ed994fd2f608c5f44fb3de274a2bb25538fe7be4
michalisantoniou6 <michalisantoniou6@gmail.com>
2015-05-04 23:56:21 -0400

    Adds location index view

---
+++ resources/views/location/index.blade.php
@@ -0,0 +1,49 @@
+@extends('app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Add location</div>
+                    <div class="panel-body">
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        @foreach( $locations as $loc )
+
+                            <form id="location-form" class="form-horizontal" role="form" method="POST" action="{{ action('LocationController@destroy', $loc->id) }}">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input name="_method" type="hidden" value="DELETE">
+
+                                <div class="form-group">
+                                    <div class="col-md-6">
+                                        <h4>{{ $loc->name }}</h4>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Delete Location
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        @endforeach
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
