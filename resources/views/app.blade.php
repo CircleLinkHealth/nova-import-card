c22593d126c96fa036ba70668672c6e3a8d316d1
michalisantoniou6 <michalisantoniou6@gmail.com>
2015-05-29 19:38:55 -0400

    Adds API Keys menu item

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,6 +35,7 @@
 				@if ( ! Auth::guest())
 					<ul class="nav navbar-nav">
 						<li><a href="{{ url('/') }}">Home</a></li>
+						<li><a href="{{ action('ApiKeyController@index') }}">API Keys</a></li>
 					</ul>
 				@endif
 
