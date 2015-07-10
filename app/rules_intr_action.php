069ddd1fc1f2a575d6c952f648615b287f2585c0
kgallo <trek23237374>
2015-07-10 13:22:40 -0400

    issue 111 - adding rules engine models w/ migrations

---
+++ app/rules_intr_action.php
@@ -0,0 +1,28 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rules_intr_action extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'rules_intr_action';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    //protected $fillable = ['duration', 'duration_unit', 'patient_id', 'provider_id', 'start_time', 'start_time_gmt', 'end_time', 'end_time_gmt', 'url_full', 'url_short', 'program_id'];
+
+}
---
+++ app/rules_intr_condition.php
@@ -0,0 +1,28 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rules_intr_condition extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'rules_intr_condition';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    //protected $fillable = ['duration', 'duration_unit', 'patient_id', 'provider_id', 'start_time', 'start_time_gmt', 'end_time', 'end_time_gmt', 'url_full', 'url_short', 'program_id'];
+
+}
