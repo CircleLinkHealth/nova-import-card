1b73d2cafbdadc9d01488d6a50f07819d9df3b94
kgallo <trek23237374>
2015-07-15 13:59:20 -0400

    fixing database migration class name conflict

--- database/migrations/2015_07_15_095438_add_pagetimer_columns.php
+++ database/migrations/2015_07_15_095438_add_pagetimer_columns.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPagetimerColumns extends Migration {
+class AddPagetimerColumns2 extends Migration {
 
 	/**
 	 * Run the migrations.
