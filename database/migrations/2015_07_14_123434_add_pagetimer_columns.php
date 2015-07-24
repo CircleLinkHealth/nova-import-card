8e7733787e516b64e2ee7ebc5b661c98962e488b
Phil Lawlor <philiplawlor@gmail.com>
2015-07-24 13:21:04 -0400

    Commented our Activity Column due to migration failure 2

--- database/migrations/2015_07_14_123434_add_pagetimer_columns.php
+++ database/migrations/2015_07_14_123434_add_pagetimer_columns.php
@@ -15,7 +15,7 @@ class AddPagetimerColumns extends Migration {
 		Schema::table('page_timer', function($table)
 		{
 			// $table->string('activity_type')->after('url_short');
-			$table->string('title')->after('activity_type');
+			// $table->string('title')->after('activity_type');
 			$table->string('query_string')->after('title');
 		});
 		Schema::table('activities', function($table)
