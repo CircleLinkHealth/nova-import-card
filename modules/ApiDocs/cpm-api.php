c608b49156149f416fbdf5bc79f7281f5dee83b9
michalisantoniou6 <michalisantoniou6@gmail.com>
2015-08-19 20:35:35 +0300

    Adds initial Swagger config files

---
+++ modules/ApiDocs/cpm-api.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host="cpm-api.dev",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="CLH Api",
+ *         description="This is CircleLink Health's Api",
+ *     ),
+ * )
+ */
