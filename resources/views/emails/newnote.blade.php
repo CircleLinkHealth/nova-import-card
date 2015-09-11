69c6905ce1f8e09058d075eee86b0e7d7839e4f7
Phil Lawlor <philiplawlor@gmail.com>
2015-09-11 09:34:09 -0400

    Changed Link to Click Here in view

--- resources/views/emails/newnote.blade.php
+++ resources/views/emails/newnote.blade.php
@@ -1,3 +1,3 @@
-<p>Please see new note for patient {{ $patient_name }}: <a href="{{ $url }}">Link</a>
+<p>Please see new note for patient {{ $patient_name }}: <a href="{{ $url }}">Click here</a>
 
 <p>The CircleLinkHealth Team
