539e64e5727d7eef3ca668df48b75c75a624f299
Michalis Antoniou <michalisantoniou6@gmail.com>
2015-09-22 11:59:15 -0400

    Fixes Password Reset

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'no-reply@circlelinkhhealth.com', 'name' => 'CircleLink Health'],
 
 	/*
 	|--------------------------------------------------------------------------
