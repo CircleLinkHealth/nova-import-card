a3261c9daa8ec4995146be508e717cfa7936e07a
Michalis Antoniou <michalisantoniou6@gmail.com>
2015-10-22 09:18:15 -0400

    Changes invalid credentials status code

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -12,80 +12,6 @@ class AuthorizationController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
 	public function login(Request $request)
 	{
 		// testing
@@ -106,8 +32,7 @@ class AuthorizationController extends Controller {
 		// attempt authorization
 		if ( ! $token = \JWTAuth::attempt($credentials) )
 		{
-			// return 401 error response
-			return response()->json(['error' => 'invalid_credentials'], 401);
+			return response()->json(['error' => 'invalid_credentials'], 400);
 		}
 		return response()->json(compact('token'));
 	}
