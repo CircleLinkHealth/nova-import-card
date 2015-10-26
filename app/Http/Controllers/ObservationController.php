5e7734ccfc64c08271ae180fd0281b8a8c416383
kgallo <trek23237374>
2015-10-26 13:45:49 -0400

    more urgent careplan feed fixes, removed timezone validation

--- app/Http/Controllers/ObservationController.php
+++ app/Http/Controllers/ObservationController.php
@@ -213,9 +213,11 @@ class ObservationController extends Controller {
 		}
 
 		// validate timezone
+		/*
 		if(strlen($params['timezone']) < 5) {
 			return response()->json(['response' => 'Error - Invalid timezone, please provide full timezone (not GMT offset)'], 500);
 		}
+		*/
 
 		$result = $observationService->storeObservationFromApp($params['user_id'], $params['parent_id'], $params['obs_value'], $params['obs_date'], $obsMessageId, $params['obs_key'], $params['timezone'], $params['source'], $params['isStartingObs']);
 
