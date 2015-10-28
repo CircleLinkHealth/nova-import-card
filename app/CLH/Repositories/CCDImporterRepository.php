353cc562a5e1208aba6e56fa1ad1fb84bff101ad
Michalis Antoniou <michalisantoniou6@gmail.com>
2015-10-28 11:54:39 -0400

    Adds CCDImporterRepo, Alters xml_ccds

---
+++ app/CLH/Repositories/CCDImporterRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\CLH\Repositories;
+
+
+use App\CLH\Repositories\WpUserRepository;
+use App\Role;
+use App\WpUser;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class CCDImporterRepository
+{
+    public function createUser()
+    {
+        $role = Role::whereName('patient')->first();
+
+        $newUserId = str_random(20);
+
+        $bag = new ParameterBag([
+            'user_email' => $newUserId . '@careplanmanager.com',
+            'user_pass' => 'whatToPutHere',
+            'user_nicename' => $newUserId,
+            'primary_blog' => '7',
+            'roles' => [$role->id],
+        ]);
+
+        return debug((new WpUserRepository())->createNewUser(new WpUser(), $bag));
+    }
+
+}
