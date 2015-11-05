33d27ddf33f13ca9cf6fdb7469488b3491391497
rohan <rohanm@circlelinkhealth.com>
2015-11-05 17:05:27 -0500

    - Fine tunes patientAddForm

--- resources/views/partials/patientAddForm.blade.php
+++ resources/views/partials/patientAddForm.blade.php
@@ -11,41 +11,38 @@
                 </div>
             </div>
             <hr />
-            {{--{!! Form::open(array('url' => 'test/form/dump', 'class' => 'form-horizontal')) !!}--}}
-            {!! Form::open(array('url' => '#', 'class' => 'form-horizontal')) !!}
+            {!! Form::open(array('url' => 'test/form/dump','method' => 'post', 'class' => 'form-horizontal')) !!}
+            {{--{!! Form::open(array('url' => '#', 'class' => 'form-horizontal')) !!}--}}
             <div class="form-group">
                 <div class="row">
-                    <div class="col-xs-1">{!! Form::label('first_name', 'First Name:') !!}</div>
-                    <label for="male">Male</label>
-                    <div class="col-xs-3">{!! Form::text('first_name', '', ['class' => 'form-control', 'required']) !!}</div>
-                    <div class="col-xs-1">{!! Form::label('last_name', 'Last Name:') !!}</div>
-                    <div class="col-xs-3">{!! Form::text('last_name', '', ['class' => 'form-control', 'required']) !!}</div>
-                    <div class="col-xs-1">{!! Form::label('email', 'Email Address:') !!}</div>
-                    <div class="col-xs-3">{!!Form::input('email', 'email', null, ['class' => 'form-control', 'required'])!!}</div>
+                    <div class="col-xs-1"><label for="first_name">First Name</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="first_name" type="text" value="" id="first_name"></div>
+                    <div class="col-xs-1"><label for="last_name">Last Name</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="last_name" type="text" value="" id="last_name"></div>
+                    <div class="col-xs-1"><label for="email">Email</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="email" type="email" value="" id="email"></div>
                 </div>
             </div>
             <div class="form-group">
                 <div class="row">
-
-                    <div class="col-xs-1">{!! Form::label('mail', 'Mailing Address:') !!}</div>
-                    <div class="col-xs-3">{!! Form::text('description', '', ['class' => 'form-control', 'required']) !!}</div>
-                    <div class="col-xs-1">{!! Form::label('mrn', 'MRN#:') !!}</div>
-                    <div class="col-xs-3">{!! Form::text('mrn', '', ['class' => 'form-control', 'required']) !!}</div>
-                    <div class="col-xs-1">{!! Form::label('CCM', 'CCM Enrollment') !!}</div>
-                    <div class="col-xs-1">{!! Form::radio('CCM', 'enrolled', true) !!} Enrolled</div>
-                    <div class="col-xs-2">{!! Form::radio('CCM', 'unenrolled') !!} Un-enrolled</div>
+                    <div class="col-xs-1"><label for="mail">Mailing Address:</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="mail" type="text" value="" id="mail"></div>
+                    <div class="col-xs-1"><label for="mrn">MRN#:</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="mrn" type="text" value="" id="mrn"></div>
+                    <div class="col-xs-1"><label for="ccm">CCM Enrollment:</label></div>
+                    <div class="col-xs-1"><input class="" required="required" name="ccm" type="radio" value="" id="enrolled">Enrolled</div>
+                    <div class="col-xs-2"><input class="" required="required" name="ccm" type="radio" value="" id="unenrolled">Un-Enrolled</div>
                 </div>
             </div>
 
             <div class="form-group">
                 <div class="row">
-                    <div class="col-xs-1">{!! Form::label('date', 'Date Of Birth:') !!}</div>
-                    <div class="col-xs-3">{!!Form::input('date', 'date', null, ['class' => 'form-control', 'placeholder' => 'Date', 'required'])!!}</div>
+                    <div class="col-xs-1"><label for="date">Date Of Birth:</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="date" type="date" value="" id="date" placeholder="date"></div>
                     <div class="col-xs-1">{!! Form::label('date', 'Preferred Contact Days:') !!}</div>
                     <div class="col-xs-3">{!! Form::select('contact_days[]', $days , null, ['class' => 'form-control', 'multiple', 'data-size' => '10', 'required']) !!}</div>
-                    <div class="col-xs-1">{!! Form::label('date', 'Preferred Contact Time:') !!}</div>
-                    <div class="col-xs-3">{!! Form::input('time', 'daily_reminder_time', null, ['class' => 'form-control', 'required']) !!}</div>
-
+                    <div class="col-xs-1"><label for="preferred_contact_time">Preferred Contact Time:</label></div>
+                    <div class="col-xs-3"><input class="form-control" required="required" name="preferred_contact_time" type="time" value="" id="preferred_contact_time" placeholder="time"></div>
                 </div>
             </div>
             <hr />
@@ -122,29 +119,32 @@
                                 <div class="row">
                                     <div class="col-xs-2"><input tabindex="1" type="checkbox" name="biometrics[]" id="{{$item->items_id}}" value="{{$item->items_id}}"><strong>{{$item->items_text}}</strong></div>
                                     <div id="{{$item->items_id . "_initial"}}">
-                                        <div class="col-xs-1">{!! Form::label('starting', 'Starting:') !!}</div>
-                                        <div class="col-xs-2">{!! Form::text('other_problems', '', ['class' => 'form-control']) !!}</div>
-                                        <div class="col-xs-1">{!! Form::label('target', 'Target:') !!}</div>
-                                        <div class="col-xs-2">{!! Form::text('other_problems', '', ['class' => 'form-control']) !!}</div>
+                                        <div class="col-xs-1">{!! Form::label($item->items_id . '_starting', 'Starting:') !!}</div>
+                                        <div class="col-xs-2">{!! Form::text($item->items_id . '_starting', '', ['class' => 'form-control']) !!}</div>
+                                        <div class="col-xs-1">{!! Form::label($item->items_id . '_target', 'Target:') !!}</div>
+                                        <div class="col-xs-2">{!! Form::text($item->items_id . '_target', '', ['class' => 'form-control']) !!}</div>
                                     </div>
                                 </div>
+
+                                <script type="text/javascript">
+                                    function valueChanged()
+                                    {
+
+                                        if($("input[id^='item->items_id']").is(":checked"))
+                                            $("#{!!$item->items_id . "_initial" !!}").show();
+                                        else
+                                            $("#{!!$item->items_id . "_initial" !!}").hide();
+                                    }
+                                </script>
+
                             @endforeach
                         </div>
                     </div>
                 </div><hr />
-                {!! Form::submit('Save [test only]') !!}
-            </div>
+                {!! Form::submit('Save') !!}
+        </div>
         </div>
     </div>
 </div>
-<script>
-    @foreach ($items['Biometrics to Monitor'] as $item)
-        $('#{!!$item->items_id!!}').change(function(){
-            if(this.checked) {
-                $('#{!!$item->items_id . "_initial"!!}'.next().show();
-            } else {
-                $(this).next().hide();
-            }
-        });
-    @endforeach
-</script>
+
+
