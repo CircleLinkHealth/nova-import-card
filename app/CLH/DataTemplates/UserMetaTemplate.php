c8eabbdb4e5de6cf4d8eca5492221dabb67794ae
kgallo <trek23237374>
2015-11-11 16:26:23 -0500

    fixed new meta keys changed clh -> qa

--- app/CLH/DataTemplates/UserMetaTemplate.php
+++ app/CLH/DataTemplates/UserMetaTemplate.php
@@ -13,8 +13,8 @@ class UserMetaTemplate extends DataTemplate
     public $admin_color = 'fresh';
     public $cur_month_activity_time = 0;
     public $show_admin_bar_front = false;
-    public $careplan_clh_approver;
-    public $careplan_clh_date;
+    public $careplan_qa_approver;
+    public $careplan_qa_date;
     public $careplan_provider_approver;
     public $careplan_provider_date;
     public $careplan_status = 'draft';
