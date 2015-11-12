be650c37eb8fc20b6222a5da2e54620ecc72f643
kgallo <trek23237374>
2015-11-12 13:07:46 -0500

    updated seeder to work with new approval meta, sudo php artisan db:seed --class=S20151025UserMeta

--- database/seeds/S20151025UserMeta.php
+++ database/seeds/S20151025UserMeta.php
@@ -9,12 +9,15 @@ class S20151025UserMeta extends Seeder {
 
     public function run()
     {
-        // careplan_approved
+        // careplan_status
 
-        // get users without careplan_approved
-        echo PHP_EOL.PHP_EOL.'Users missing careplan_approved'.PHP_EOL.PHP_EOL;
+        // get users without careplan_status
+        echo PHP_EOL.PHP_EOL.'Users missing careplan_status'.PHP_EOL.PHP_EOL;
         $users = WpUser::whereDoesntHave('meta', function ($q) {
-                $q->where('meta_key', '=', 'careplan_approved');
+            $q->where('meta_key', '=', 'careplan_status');
+        })
+            ->orWhereHas('meta', function ($q) {
+                $q->where('meta_key', '=', 'careplan_status');
                 $q->where('meta_value', '=', '');
             })
             ->get();
@@ -23,37 +26,40 @@ class S20151025UserMeta extends Seeder {
             foreach($users as $user) {
                 $meta = new CPRulesItemMeta;
                 $meta->user_id = $user->ID;
-                $meta->meta_key = 'careplan_approved';
-                $meta->meta_value = '';
-                if($user->program_id == 10) { // miller(10) = true
-                    $meta->meta_value = 'true';
-                }
+                $meta->meta_key = 'careplan_status';
+                $meta->meta_value = 'qa_approved';
                 //$meta->save();
-                echo 'added careplan_approved to '.$user->ID.PHP_EOL;
+                echo 'added careplan_status = qa_approved to '.$user->ID.PHP_EOL;
             }
         }
 
-        // careplan_approver
+        // careplan_qa_approver / careplan_qa_date
 
-        // get users without careplan_approver
-        echo PHP_EOL.PHP_EOL.'Users missing careplan_approver'.PHP_EOL.PHP_EOL;
+        // get users without careplan_qa_approver
+        echo PHP_EOL.PHP_EOL.'Users missing careplan_qa_approver'.PHP_EOL.PHP_EOL;
         $users = WpUser::whereDoesntHave('meta', function ($q) {
-            $q->where('meta_key', '=', 'careplan_approver');
-            $q->where('meta_value', '=', '');
+            $q->where('meta_key', '=', 'careplan_qa_approver');
         })
+            ->orWhereHas('meta', function ($q) {
+                $q->where('meta_key', '=', 'careplan_qa_approver');
+                $q->where('meta_value', '=', '');
+            })
             ->get();
 
         if(count($users) > 0) {
             foreach($users as $user) {
                 $meta = new CPRulesItemMeta;
                 $meta->user_id = $user->ID;
-                $meta->meta_key = 'careplan_approver';
-                $meta->meta_value = '';
-                if($user->program_id == 10) { // miller(10) = system
-                    $meta->meta_value = 'system';
-                }
+                $meta->meta_key = 'careplan_qa_approver';
+                $meta->meta_value = '1';
                 //$meta->save();
-                echo 'added careplan_approver to '.$user->ID.PHP_EOL;
+                echo 'added careplan_qa_approver = qa_approved to '.$user->ID.PHP_EOL;
+                $meta = new CPRulesItemMeta;
+                $meta->user_id = $user->ID;
+                $meta->meta_key = 'careplan_qa_date';
+                $meta->meta_value = date('Y-m-d H:i:s');
+                //$meta->save();
+                echo 'added careplan_qa_date = careplan_qa_date to '.$user->ID.PHP_EOL;
             }
         }
 
@@ -63,8 +69,11 @@ class S20151025UserMeta extends Seeder {
         echo PHP_EOL.PHP_EOL.'Users missing ccm_enabled'.PHP_EOL.PHP_EOL;
         $users = WpUser::whereDoesntHave('meta', function ($q) {
             $q->where('meta_key', '=', 'ccm_enabled');
-            $q->where('meta_value', '=', '');
         })
+            ->orWhereHas('meta', function ($q) {
+                $q->where('meta_key', '=', 'ccm_enabled');
+                $q->where('meta_value', '=', '');
+            })
             ->get();
 
         if(count($users) > 0) {
@@ -74,10 +83,9 @@ class S20151025UserMeta extends Seeder {
                 $meta->meta_key = 'ccm_enabled';
                 $meta->meta_value = 'true'; // set everyone to true
                 //$meta->save();
-                echo 'adding ccm_enabled to '.$user->ID.PHP_EOL;
+                echo 'adding ccm_enabled = true to '.$user->ID.PHP_EOL;
             }
         }
-        dd('END');
 
     }
 }
