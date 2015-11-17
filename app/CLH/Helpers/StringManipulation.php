95f0d99a625937654d71bb180c0a5952cc159fe9
Michalis Antoniou <michalisantoniou6@gmail.com>
2015-11-17 15:08:31 -0500

    Adds StringManipulation Helpers

---
+++ app/CLH/Helpers/StringManipulation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\CLH\Helpers;
+
+
+class StringManipulation
+{
+    /**
+     * Returns only numerical values in a string
+     *
+     * @param $string
+     * @return string
+     */
+    public function extractNumbers($string)
+    {
+        preg_match_all('/([\d]+)/', $string, $match);
+
+        return implode($match[0]);
+    }
+
+
+
+    public function formatPhoneNumber($string)
+    {
+        $sanitized = $this->extractNumbers($string);
+
+        if (strlen($sanitized) > 10) {
+            $sanitized = substr($sanitized, -10);
+        }
+
+        return substr($sanitized, 0, 3) . '-' . substr($sanitized, 4, 3) . '-' . substr($sanitized, 7, 4);
+
+    }
+}
