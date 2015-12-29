adfef9c0837ee726ee8cc3f53d63413e604f2cf0
rohan <rohanm@circlelinkhealth.com>
2015-12-29 02:22:20 -0500

    - fixed Progress Report Link

--- resources/views/partials/providerUI.blade.php
+++ resources/views/partials/providerUI.blade.php
@@ -105,10 +105,10 @@
                         <a href="{{ empty($patient) ? '' : '' }}">Patient Alerts</a>
                     </li>
                     <li>
-                        <a href="{{ empty($patient) ? '' : URL::route('patient.activity.index', array('patient' => $patient->ID)) }}">Progress Report</a>
+                        <a href="{{ empty($patient) ? '' : URL::route('patient.reports.progress', array('patient' => $patient->ID)) }}">Progress Report</a>
                     </li>
                     <li>
-                        <a href="{{ empty($patient) ? '' : '' }}">Patient Activity Report</a>
+                        <a href="{{ empty($patient) ? URL::route('patient.activity.providerUIIndex', array('patient' => $patient->ID)) : '#' }}">Patient Activity Report</a>
                     </li>
                     <li>
                         <a href="{{ URL::route('patient.reports.u20', array('patient' => $patient->ID)) }}">Under 20 Minute Report</a>
