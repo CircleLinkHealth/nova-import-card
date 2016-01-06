3740285acc279ec65735e8e1c2f70ff57bf9fc44
kgallo <kgallo@circlelinkhealth.com>
2016-01-06 16:02:02 -0500

    removed ccm_status from user_config and fixed admin user edit ccm status form save, was saving in the wrong place

--- resources/views/wpUsers/edit.blade.php
+++ resources/views/wpUsers/edit.blade.php
@@ -87,7 +87,7 @@
                                 <div class="form-group">
                                     <div class="row">
                                         <div class="col-xs-2">{!! Form::label('ccm_status', 'CCM Status:') !!}</div>
-                                        <div class="col-xs-4">{!! Form::select('ccm_status', array('paused' => 'paused', 'enrolled' => 'enrolled', 'withdrawn' => 'withdrawn'), $userMeta['ccm_status'], ['class' => 'form-control select-picker', 'style' => 'width:40%;']) !!}</div>
+                                        <div class="col-xs-4">{!! Form::select('ccm_status', array('paused' => 'paused', 'enrolled' => 'enrolled', 'withdrawn' => 'withdrawn'), $wpUser->ccmStatus, ['class' => 'form-control select-picker', 'style' => 'width:40%;']) !!}</div>
                                         <div class="col-xs-2">{!! Form::label('careplan_status', 'Careplan Status:') !!}</div>
                                         <div class="col-xs-4">{!! Form::select('careplan_status', array('draft' => 'draft', 'qa_approved' => 'qa_approved', 'provider_approved' => 'provider_approved'), $userMeta['careplan_status'], ['class' => 'form-control select-picker', 'style' => 'width:40%;']) !!}</div>
                                     </div>
