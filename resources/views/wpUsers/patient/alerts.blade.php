f82a1c74e5362ed49ce8630e4a9b16ffd2cb50a9
kgallo <kgallo@circlelinkhealth.com>
2016-01-17 19:21:03 -0500

    [touch: 16] -  fixed alerts page placeholder to use providerUI partial

--- resources/views/wpUsers/patient/alerts.blade.php
+++ resources/views/wpUsers/patient/alerts.blade.php
@@ -1,42 +1,17 @@
-@extends('partials.adminUI')
+@extends('partials.providerUI')
 
 @section('content')
-    <script type="text/javascript" src="{{ asset('/js/rules/rules.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('/webix/codebase/webix.css') }}" type="text/css">
-    <script src="{{ asset('/webix/codebase/webix.js') }}" type="text/javascript"></script>
-    <link href="{{ asset('/css/wpstyle.css') }}" rel="stylesheet">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="row">
-                    <div class="col-sm-8">
-                        <h2>Alerts</h2>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="pull-right" style="margin:20px;">
-                        </div>
-                    </div>
+    <div class="row" style="margin-top:60px;">
+        <div class="main-form-container-last col-lg-8 col-lg-offset-2">
+            <div class="row">
+                <div class="main-form-title col-lg-12">
+                    Alerts
                 </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">Patient Alerts</div>
-                    <div class="panel-body">
-
-                        <div class="row">
-                            <div class="main-form-container col-lg-8 col-lg-offset-2">
-                                <div class="row">
-                                    <div class="main-form-title col-lg-12">
-                                        Patient Alerts
-                                    </div>
-                                    <div class="main-form-block main-form-horizontal main-form-primary-horizontal col-md-12">
-                                        Alerts Webix
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div class="row">
+                <div class="main-form-block main-form-horizontal main-form-primary-horizontal col-md-12">
+                    Alerts Webix
                 </div>
-
-
             </div>
         </div>
     </div>
