b623b90f9ddc98ecf38616a2e47fa76b2c07bd60
rohan <rohanm@circlelinkhealth.com>
2016-01-25 18:35:35 -0500

    [touch: 54] UI Updates

--- resources/views/wpUsers/patient/select.blade.php
+++ resources/views/wpUsers/patient/select.blade.php
@@ -7,40 +7,58 @@
             text-align: center;
         }
     </style>
-    <div class="container col-md-2 col-md-offset-5">
-        <div id="bloodhound">
-            <input class="typeahead form-control" type="text" placeholder="Enter Patient Name...">
-        </div>
+    <div class="container">
+        <section class="main-form" >
+            <div class="row">
+                <div class="main-form-container col-lg-6 col-lg-offset-3 main-form-container-last" style="border-bottom: 3px solid #50b2e2">
+                    <div class="row">
+                        <div class="main-form-title">
+                            Select a Patient
+                        </div>
+                    </div>
+
+                    <div class="form-item form-item-spacing form-item--first col-sm-12 col-lg-12">
+                        <div class="ui-widget form-item form-item-spacing form-item--first col-sm-12 col-lg-12">
+                            <label for="patients">MRN, Patient Name or DOB [mm-dd-yyyy]</label>
+                            <div id="bloodhound">
+                                <input class="typeahead form-item-spacing form-control" type="text" autofocus="autofocus" placeholder="Enter Patient Name...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-    <script>
-        jQuery(document).ready(function($) {
-            var states = new Bloodhound({
-                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                // `states` is an array of state names defined in "The Basics"
-                local: {!! $data !!}
-        });
+                <script>
+                    jQuery(document).ready(function ($) {
+                        var states = new Bloodhound({
+                            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+                            queryTokenizer: Bloodhound.tokenizers.whitespace,
+                            // `states` is an array of state names defined in "The Basics"
+                            local: {!! $data !!}
 
-            states.initialize();
+                        });
 
-            $('#bloodhound .typeahead').typeahead({
-                        hint: true,
-                        highlight: true,
-                        minLength: 3
-                    },
-                    {
-                        name: 'matched-states',
-                        displayKey: 'name',
-                        source: states,
-                        templates: {
-                            empty: [
-                                '<div class="empty-message">', 'No Patients Found...', '</div>'
-                            ].join('\n'),
-                            suggestion: function (data) {
-                                return '<div><a href="' + data.link + '">' + data.name + '</a></div>';
-                            }
-                        }
+                        states.initialize();
+
+                        $('#bloodhound .typeahead').typeahead({
+                                    hint: true,
+                                    highlight: true,
+                                    minLength: 3
+                                },
+                                {
+                                    name: 'matched-states',
+                                    displayKey: 'name',
+                                    source: states,
+                                    templates: {
+                                        empty: [
+                                            '<div class="empty-message">', 'No Patients Found...', '</div>'
+                                        ].join('\n'),
+                                        suggestion: function (data) {
+                                            return '<li><a href="' + data.link + '">' + data.name + '</a></li>';
+                                        }
+                                    }
+                                });
                     });
-        });
-    </script>
+                </script>
 @stop
