f9d3cef9496b3790ae545ca76f8b287f99415bc0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-02-03 19:33:08 -0500

    Fixes consolidate ccd problem bug. code system, code system name, and code are now picked from the same section

--- app/CLH/CCD/Importer/Parsers/ProblemsParser.php
+++ app/CLH/CCD/Importer/Parsers/ProblemsParser.php
@@ -156,13 +156,12 @@ class ProblemsParser extends BaseParser implements Parser
              * ICD-10 Check
              */
             if ( in_array( $problemCodes->code_system_name, ['ICD-10', 'ICD10'] ) || $problemCodes->code_system == '2.16.840.1.113883.6.3' ) {
-                if (! empty($potentialICD10List)) {
+                if ( !empty($potentialICD10List) ) {
                     /**
                      * This is the same code as a few lines down.
                      * @todo: refactor soon
                      */
-                    foreach ($potentialICD10List as $icd10)
-                    {
+                    foreach ( $potentialICD10List as $icd10 ) {
                         foreach ( $cpmProblems as $cpmProblem ) {
                             /**
                              * Since we are doing string comparison, I10 != I10.0
@@ -182,15 +181,14 @@ class ProblemsParser extends BaseParser implements Parser
                     }
                 }
 
-            /*
-             * SNOMED Check
-             */
-                if (in_array($problemCodes->code_system_name, ['SNOMED CT']) || $problemCodes->code_system == '2.16.840.1.113883.6.96')
-                {
-                    $potentialICD10List = SnomedToICD10Map::whereSnomedCode($problemCodes->code)->lists('icd_10_code');
+                /*
+                 * SNOMED Check
+                 */
+                if ( in_array( $problemCodes->code_system_name, ['SNOMED CT'] ) || $problemCodes->code_system == '2.16.840.1.113883.6.96' ) {
+                    $potentialICD10List = SnomedToICD10Map::whereSnomedCode( $problemCodes->code )->lists( 'icd_10_code' );
                     $problemCodes->code_system_name = 'ICD-10';
                     $problemCodes->code_system = '2.16.840.1.113883.6.3';
-                    if (! empty($potentialICD10List[0])) $problemCodes->code = $potentialICD10List[0];
+                    if ( !empty($potentialICD10List[ 0 ]) ) $problemCodes->code = $potentialICD10List[ 0 ];
                 }
 
                 foreach ( $cpmProblems as $cpmProblem ) {
@@ -264,6 +262,8 @@ class ProblemsParser extends BaseParser implements Parser
      * Consolidate Problem info from BB Problem and BB Problem Translation sections.
      * Sometimes info is in problem, or translation or both.
      *
+     * Overwrite the problem section with the preferred one.
+     *
      * @param $ccdProblem
      * @return mixed
      */
@@ -271,15 +271,9 @@ class ProblemsParser extends BaseParser implements Parser
     {
         $consolidatedProblem = $ccdProblem;
 
-        if ( empty($consolidatedProblem->code) && !empty($ccdProblem->translation->code) ) {
+        if ( !empty($ccdProblem->translation->code) ) {
             $consolidatedProblem->code = $ccdProblem->translation->code;
-        }
-
-        if ( empty($consolidatedProblem->code_system) && !empty($ccdProblem->translation->code_system) ) {
             $consolidatedProblem->code_system = $ccdProblem->translation->code_system;
-        }
-
-        if ( empty($consolidatedProblem->code_system_name) && !empty($ccdProblem->translation->code_system_name) ) {
             $consolidatedProblem->code_system_name = $ccdProblem->translation->code_system_name;
         }
 
