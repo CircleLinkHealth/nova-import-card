9545a255008a1cdb9fec3f8e71c21f5390d50366
Phil Lawlor <philiplawlor@gmail.com>
2016-02-10 08:30:23 -0500

    Patient List View - Patient Name Column width fix

--- resources/views/wpUsers/patient/listing.blade.php
+++ resources/views/wpUsers/patient/listing.blade.php
@@ -57,9 +57,9 @@
                             resizeColumn:true,
                             //tooltip:true,
                             columns:[
-                                { id:"first_name",   header:["Patient Name",{content:"textFilter", placeholder:"Filter"}], template:"<a href='<?php echo URL::route('patient.summary', array('patient' => '#key#')); ?>'>#first_name# #last_name#</a>", width:150, sort:'string'},
-                                { id:"last_name",   header:["Patient Name",{content:"textFilter", placeholder:"Filter"}], template:"<a href='<?php echo URL::route('patient.summary', array('patient' => '#key#')); ?>'>#last_name#, #first_name#</a>", width:150, sort:'string'},
-                                { id:"provider",   header:["Provider",{content:"selectFilter"}],    width:105, sort:'string'},
+                                { id:"first_name",   header:["Patient Name",{content:"textFilter", placeholder:"Filter"}], template:"<a href='<?php echo URL::route('patient.summary', array('patient' => '#key#')); ?>'>#first_name# #last_name#</a>", width:200, sort:'string'},
+                                { id:"last_name",   header:["Patient Name",{content:"textFilter", placeholder:"Filter"}], template:"<a href='<?php echo URL::route('patient.summary', array('patient' => '#key#')); ?>'>#last_name#, #first_name#</a>", width:200, sort:'string'},
+                                { id:"provider",   header:["Provider",{content:"selectFilter"}],    width:200, sort:'string'},
                                 { id:"ccm_status",   header:["CCM Status",{content:"selectFilter"}],    width:105, sort:'string'},
                                 { id:"careplan_status",    header:["CarePlan Status",{content:"selectFilter", placeholder:"Filter"}], tooltip:"#tooltip#" , width:125,
                                     template: function (obj) {
