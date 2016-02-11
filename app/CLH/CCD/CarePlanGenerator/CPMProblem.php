b9c4a3dc6e3d464ce7a76252e943a86daab413ea
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-02-11 00:19:24 -0500

    Moves CPMProblem and SnomedToICDmap

---
+++ app/CLH/CCD/CarePlanGenerator/CPMProblem.php
@@ -0,0 +1,11 @@
+<?php namespace App\CLH\CCD\CarePlanGenerator;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CPMProblem extends Model {
+
+    protected $table = 'cpm_problems';
+
+	protected $guarded = [];
+
+}
