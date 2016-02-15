ed16b38561272b2eadf1f6227e7ee57b5ec6d30d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-02-15 16:29:35 -0500

    Adds Identification Strategies and Identification Manager

---
+++ app/CLH/CCD/Identifier/IdentificationStrategies/AuthorName.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\CLH\CCD\Identifier\IdentificationStrategies;
+
+
+class AuthorName extends BaseIdentificationStrategy
+{
+
+    public function identify()
+    {
+        if (empty($this->ccd->document->author->name->given[0])) return false;
+
+        if (empty($this->ccd->document->author->name->family)) return false;
+
+        $authorName = $this->ccd->document->author->name->given[0]
+                        . ' '
+                        . $this->ccd->document->author->name->family;
+
+        return empty($authorName) ? false : $authorName;
+    }
+}
---
+++ app/CLH/CCD/Identifier/IdentificationStrategies/CustodianName.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\CLH\CCD\Identifier\IdentificationStrategies;
+
+
+class CustodianName extends BaseIdentificationStrategy
+{
+
+    public function identify()
+    {
+        if (empty($this->ccd->document->custodian->name)) return false;
+
+        $custodianName = $this->ccd->document->custodian->name;
+
+        return empty($custodianName) ? false : $custodianName;
+    }
+}
