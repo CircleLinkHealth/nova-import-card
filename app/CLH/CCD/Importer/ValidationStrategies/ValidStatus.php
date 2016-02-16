845200856cd0f122ff2485a7bc8071db4fcc7936
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-02-16 03:50:50 -0500

    Better error handling

--- app/CLH/CCD/Importer/ValidationStrategies/ValidStatus.php
+++ app/CLH/CCD/Importer/ValidationStrategies/ValidStatus.php
@@ -9,6 +9,8 @@ class ValidStatus implements ValidationStrategy
 {
     public function validate($ccdItem)
     {
+        if (empty($ccdItem->status)) return false;
+
         return in_array( strtolower( $ccdItem->status ), ['active', 'chronic'] );
     }
 }
