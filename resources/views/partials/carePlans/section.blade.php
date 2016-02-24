856a47c42a269f1c6c94180eb9610a77d1b2bd4e
Phil Lawlor <philiplawlor@gmail.com>
2016-02-24 09:05:01 -0500

    [touch: 140] - Rolled back last commit to Fix CarePlan Items Alignment issue

--- resources/views/partials/carePlans/section.blade.php
+++ resources/views/partials/carePlans/section.blade.php
@@ -1,5 +1,4 @@
 <input type=hidden name="careSections[]" value="{{ $careSection->id }}">
-<div class="row">
 <div class="main-form-block main-form-horizontal main-form-primary-horizontal col-md-12 cp-section"  id="section{{ $careSection->id }}">
     @if(isset($editMode) && $editMode != false)
         @include('partials.carePlans.sectionEdit')
@@ -15,27 +14,11 @@
         <div class="collapse in" id="collapseSection{{ $careSection->id }}">
             @foreach($careSection->carePlanItems as $planItem)
                 @if ($planItem->careItem->display_name != '')
-                    <?php
-                    // column math
-                    $num_items_total = count($careSection->carePlanItems);
-                    $num_items_per_column = round($num_items_total / 2);
-                    ?>
-
-                    <!-- output column containers if needed -->
-                    @if($i == 0)
-                        <div class="form-block form-block--left col-md-6">
-                        <div class="row">';
-                    @elseif($i == $num_items_per_column)
-                        <div class="form-block form-block--right col-md-6">
-                        <div class="row">
-                    @endif
-
-                    <!-- include item -->
                     @if($i % 2 == 0)
                         @if(isset($editMode) && $editMode != false) START ROW {{ $r }} @endif
                         <div class="row">
                     @endif
-                    <div class="col-sm-12">
+                    <div class="col-sm-6" @if($i % 2 != 0) style="border-left:1px solid #ccc;" @endif>
                         {{ $planItem->ui_row_start > 0 ? '<div class="row">' : '' }}
                         {{ $planItem->ui_col_start > 0 ? '<div class="col-sm-'.$planItem->ui_col_start.'>' : '' }}
                         @include('partials.carePlans.item')
@@ -47,18 +30,10 @@
                         <?php $r++; ?>
                     @endif
                     </div>
-
-                    <!-- output column containers if needed -->
-                    @if($i==($num_items_per_column-1))
-                        </div></div>
-                    @elseif($i == ($num_items_total-1))
-                        </div></div>
-                    @endif
                 @endif
                 <?php $i++; ?>
             @endforeach
         </div>
     @endif
 
-</div>
 </div>
