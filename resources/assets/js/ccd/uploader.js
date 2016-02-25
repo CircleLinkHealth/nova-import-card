1163010a2f4a466afc19d3e60c7bc7e50c802e26
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-02-25 02:37:11 -0500

    Updates uploader FE

--- resources/assets/js/ccd/uploader.js
+++ resources/assets/js/ccd/uploader.js
@@ -15,11 +15,24 @@ var uploader = new Vue({
         buffer: 100,
         message: 'Drop CCD Records in the box below, or click on it to browse your computer for CCDs. It is recommended that you import up to 5 CCDs in one go.',
         enabled: false, // submit button enabled
-        formCss: 'display: none',
-        qaSummary: ''
+        formCssHide: true,
+        groupHide: false,
+        tableHide: true,
+        qaSummaries: [],
+        okToImport: new Array
     },
     ready: function () {
         this.watchForFileInput();
+    },
+    computed: {
+        hideImportChecked: {
+            get: function () {
+                return this.okToImport.length ? false : true;
+            },
+            set: function () {
+
+            }
+        }
     },
     methods: {
         watchForFileInput: function () {
@@ -27,7 +40,7 @@ var uploader = new Vue({
             $('input[type="radio"]').change(this.notifyFileInput.bind(this));
         },
         notifyFileInput: function (e) {
-            this.formCss = '';
+            this.formCssHide = false;
             this.message = 'Preparing CCDs for upload';
             this.progress = 0;
             var files = event.target.files;
@@ -36,134 +49,49 @@ var uploader = new Vue({
                 this.ccdRecords.append('file[]', files[i]);
             }
 
-            //this.ccdRecords.append('vendor', this.ccdVendor);
-
             this.progress += 10;
             this.message = 'CCDs are ready for upload. Please click Upload CCD Records';
             this.enabled = true;
         },
-        parseCCDwithBB: function (data) {
-            var bb = BlueButton(data);
-
-            var ccd = {
-                document : bb.data.document,
-                demographics : bb.data.demographics,
-                allergies : bb.data.allergies,
-                carePlan : bb.data.care_plan,
-                chiefComplaint : bb.data.chief_complaint,
-                encouters : bb.data.encounters,
-                functionalStatuses : bb.data.functional_statuses,
-                immunizations : bb.data.immunizations,
-                immunizationDeclines : bb.data.immunization_declines,
-                instructions : bb.data.instructions,
-                results : bb.data.results,
-                medications : bb.data.medications,
-                problems : bb.data.problems,
-                procedures : bb.data.procedures,
-                smokingStatus : bb.data.smoking_status,
-                vitals : bb.data.vitals,
-            };
-            return ccd;
-        },
         onSubmitForm: function (e) {
             e.preventDefault();
             this.enabled = false;
-            this.message = 'Uploading CCD records and checking for duplicates.';
+            this.message = 'Uploading CCD records.';
             this.progress += 20;
 
             this.ccdRecords.append('vendor', this.ccdVendor);
 
-            this.$http.post('/upload-raw-ccds', this.ccdRecords, function (data, status, request) {
+            this.$http.post('/ccds/qaimport', this.ccdRecords, function (data, status, request) {
                 this.ccdRecords = new FormData;
 
-                if (data.uploaded.length > 0) {
-                    uploader.parseAndUploadCCDs(data.uploaded);
-                }
+                this.qaSummaries = data;
 
-                if (data.duplicates.length > 0) {
-                    uploader.parseAndUploadDuplicateCCDs(data.duplicates);
-                }
+                this.groupHide = true;
+                this.tableHide = false;
 
-                console.log(data);
+                setTimeout(function () {
+                    componentHandler.upgradeDom('MaterialCheckbox');
+                }, 0);
 
-                this.qaSummary = data;
+                this.message = 'All operations completed successfully.';
+                this.progress = 100;
 
             }).error(function (data, status, request) {
                 this.message = 'ERROR! Uploading raw XML CCDs has failed. Try refreshing your browser.';
-                console.log('Data: \n' + data );
-                console.log('Status: \n' + status );
-                console.log('Request: \n' + request );
+                console.log('Data: \n' + data);
+                console.log('Status: \n' + status);
+                console.log('Request: \n' + request);
             });
         },
-        parseAndUploadCCDs: function (uploadedCCDs) {
-            this.message = 'Parsing CCDs and generating Care Plans.';
-            this.progress += 20;
-            var parsedJsonCCDs = [];
-
-            uploadedCCDs.forEach(function(ccd) {
-                var jsonCcd = uploader.parseCCDwithBB(ccd.xml);
-                var parsedCCD = {
-                    userId:ccd.userId,
-                    ccd:jsonCcd,
-                    vendor: ccd.vendor
-                };
-                parsedJsonCCDs.push(parsedCCD);
-            });
-
-            var json = JSON.stringify(parsedJsonCCDs);
-
-            this.$http.post('/upload-parsed-ccds', json, function (data, status, request) {
-                if (data) {
-                    this.message = 'Everything completed successfully.';
-                    this.progress = 100;
-                    this.enabled = true;
-
-                }
+        importCcds: function () {
+            this.$http.post('/ccds/import', { qaImportIds: this.okToImport }, function (data, status, request){
+                this.tableHide = true;
+                alert('The importer operations completed succesfully. Please refresh your browser to upload more CCDs.')
             }).error(function (data, status, request) {
-                this.message = 'ERROR! Uploading Parsed CCDs has failed. Try refreshing your browser.';
-                console.log('Data: \n' + data );
-                console.log('Status: \n' + status );
-                console.log('Request: \n' + request );
+                console.log('Data: \n' + data);
+                console.log('Status: \n' + status);
+                console.log('Request: \n' + request);
             });
-        },
-        parseAndUploadDuplicateCCDs: function (duplicates) {
-            this.message = 'Parsing duplicate CCDs and generating Care Plans.';
-            this.progress += 15;
-            var importAgain = [];
-
-            var numberOfDuplicates = duplicates.length;
-
-            if (numberOfDuplicates > 0) {
-                for (var i = 0; i < numberOfDuplicates; i++) {
-                    var duplicate = duplicates[i];
-                    if (confirm(duplicate.fileName + " has already been imported. Do you wish to re-import it?")) {
-                        var uniqueSuffix = '_*duplicate*_' + Date.now();
-                        importAgain.push({
-                            'blogId': duplicate.blogId,
-                            'xml': duplicate.ccd,
-                            'fullName': duplicate.fullName + uniqueSuffix,
-                            'dob': duplicate.dob + uniqueSuffix,
-                            'fileName': duplicate.fileName,
-                            'vendor': duplicate.vendor
-                        });
-                    }
-                }
-            }
-
-            if (importAgain.length == numberOfDuplicates) {
-                this.message = 'Uploading duplicate CCDs.';
-                this.progress += 10;
-
-                var json = JSON.stringify(importAgain);
-                this.$http.post('/upload-duplicate-raw-ccds', json, function (data, status, request) {
-                    uploader.parseAndUploadCCDs(data.uploaded);
-                }).error(function (data, status, request) {
-                    this.message = 'ERROR! Uploading duplicate raw XML CCDs has failed. Try refreshing your browser.';
-                    console.log('Data: \n' + data );
-                    console.log('Status: \n' + status );
-                    console.log('Request: \n' + request );
-                });
-            }
         }
     }
 });
--- resources/views/CCDUploader/uploader.blade.php
+++ resources/views/CCDUploader/uploader.blade.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CCD Importer</title>
 
+    <!-- Material Design Lite -->
+    <script src="https://code.getmdl.io/1.1.1/material.min.js"></script>
+
     <link rel="stylesheet" href="https://code.getmdl.io/1.1.0/material.teal-blue.min.css"/>
-    <link href="/css/animate.min.css" rel="stylesheet">
+    <!-- Material Design icon font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <style>
+        .full-width {
+            width: 100%;
+        }
+
+        .hide {
+            display: none;
+        }
+    </style>
     <link href="/img/favicon.png" rel="icon">
 
     <style>
@@ -45,43 +58,75 @@
 </nav>
 
 <div id="ccd-uploader" class="mdl-grid">
-    <div class="mdl-cell mdl-cell--12-col">
-        <mdl-progress :progress="progress" :buffer="buffer" class="mdl-cell mdl-cell--12-col"></mdl-progress>
-        <p :message="message" class="mdl-cell mdl-cell--12-col mdl-typography--text-left">@{{ message }}</p>
+
+    <div v-bind:class="{ 'hide': groupHide }" class="mdl-cell mdl-cell--12-col">
+        <div class="mdl-cell mdl-cell--12-col">
+            <mdl-progress :progress="progress" :buffer="buffer" class="mdl-cell mdl-cell--12-col"></mdl-progress>
+            <p :message="message" class="mdl-cell mdl-cell--12-col mdl-typography--text-left">@{{ message }}</p>
+        </div>
+
+        <form method="POST" v-on:submit="onSubmitForm" enctype="multipart/form-data" class="mdl-cell mdl-cell--12-col">
+
+            <div class="mdl-cell mdl-cell--12-col">
+                @if(!empty($ccdVendors))
+                    <h5>Please choose an Import profile for this CCD.</h5>
+
+                    @foreach($ccdVendors as $vendor)
+                        <mdl-radio class="mdl-cell--4-col" :checked.sync="ccdVendor" value="{{ $vendor->id }}"
+                                   required>{{ $vendor->vendor_name }}</mdl-radio>
+                    @endforeach
+                @endif
+            </div>
+
+            <div v-bind:class="{ 'hide': formCssHide }" class="mdl-cell mdl-cell--12-col">
+
+                <input type="file" id="ccd" class="dropzone" multiple>
+
+                <div class="mdl-typography--text-center mdl-cell mdl-cell--12-col">
+                    <mdl-button primary raised v-mdl-ripple-effect type="submit" :disabled="!enabled">
+                        Upload CCD Records
+                    </mdl-button>
+                </div>
+            </div>
+        </form>
     </div>
 
-    <form method="POST" v-on:submit="onSubmitForm" enctype="multipart/form-data" class="mdl-cell mdl-cell--12-col">
+    <table v-bind:class="{ 'hide': tableHide }" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp full-width">
+        <thead>
+        <tr>
+            <th class="mdl-data-table__cell--non-numeric">Import</th>
+            <th class="mdl-data-table__cell--non-numeric">Name</th>
+            <th>Allergies</th>
+            <th>Medications</th>
+            <th>Problems</th>
+        </tr>
+        </thead>
+        <tbody>
 
-        <div class="mdl-cell mdl-cell--12-col">
-            @if(!empty($ccdVendors))
-                <h5>Please choose an Import profile for this CCD.</h5>
+        <tr v-for="qaSummary in qaSummaries">
+            <td>
+                <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"
+                       for="checkbox-@{{ qaSummary.qa_output_id }}">
+                    <input v-model="okToImport" value="@{{ qaSummary.qa_output_id }}" type="checkbox"
+                           id="checkbox-@{{ qaSummary.qa_output_id }}" class="mdl-checkbox__input">
+                    <span class="mdl-checkbox__label"></span>
+                </label>
+            </td>
+            <td class="mdl-data-table__cell--non-numeric">@{{ qaSummary.name }}</td>
+            <td>@{{ qaSummary.allergies }}</td>
+            <td>@{{ qaSummary.medications }}</td>
+            <td>@{{ qaSummary.problems }}</td>
+        </tr>
+        </tbody>
+    </table>
 
-                @foreach($ccdVendors as $vendor)
-                    <mdl-radio class="mdl-cell--4-col" :checked.sync="ccdVendor" value="{{ $vendor->id }}"
-                               required>{{ $vendor->vendor_name }}</mdl-radio>
-                @endforeach
-            @endif
-        </div>
-
-        <div style="@{{ formCss }}" class="mdl-cell mdl-cell--12-col">
-
-            <input type="file" id="ccd" class="dropzone" multiple>
-
-            <div class="mdl-typography--text-center mdl-cell mdl-cell--12-col">
-                <mdl-button primary raised v-mdl-ripple-effect type="submit" :disabled="!enabled">
-                    Upload CCD Records
-                </mdl-button>
-            </div>
-        </div>
-    </form>
-    <p>@{{ qaSummary }}</p>
+    <div v-bind:class="{ 'hide': tableHide }" class="mdl-typography--text-center mdl-cell mdl-cell--12-col">
+        <mdl-button primary raised v-mdl-ripple-effect type="submit" v-on:click="importCcds" :disabled="hideImportChecked">
+            Import Checked CCDs
+        </mdl-button>
+    </div>
 
 </div>
-
-
-
-<div id="success" class="alert hide animated" role="alert"></div>
-<div id="notification" class="alert hide animated" role="alert"></div>
 
 <script src="{{ asset('/js/scripts.js') }}"></script>
 <script src="/js/uploader.js"></script>
