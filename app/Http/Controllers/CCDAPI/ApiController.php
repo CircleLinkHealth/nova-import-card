e9cc0ad603830392bf1c50e976a7dba0512ae40e
rohan <rohanm@circlelinkhealth.com>
2016-02-29 18:44:37 -0500

    - adds login, upload controllers to ApiController

--- app/Http/Controllers/CCDAPI/ApiController.php
+++ app/Http/Controllers/CCDAPI/ApiController.php
@@ -1,112 +1,125 @@
 <?php namespace App\Http\Controllers\CCDAPI;
 
+use App\CLH\CCD\Ccda;
+use App\CLH\CCD\Importer\QAImportManager;
+use App\CLH\Repositories\CCDImporterRepository;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-class ApiController extends Controller {
+class ApiController extends Controller
+{
 
-	/*
-	 * Route::post('/ehr-api/login', function(){
+    private $repo;
 
-	$credentials = \Input::only('email', 'user_pass');
-	\JWTAuth::setIdentifier('ID');
-	//$user = \JWTAuth::parseToken()->authenticate();
-	if ( ! $token = \JWTAuth::attempt($credentials) )
-	{
-		return response()->json(['error' => 'invalid_credentials'], 400);
-	}
-	return response()->json(compact('token'));
-});
+    public function __construct(CCDImporterRepository $repo)
+    {
+        $this->repo = $repo;
+    }
 
-Route::post('/ehr-api/upload', function(Request $request){
-	$user = \JWTAuth::parseToken()->authenticate();
+    //returns JWT session token
+    public function login(Request $request)
+    {
+        if (! $request->has('email') || ! $request->has('user_pass')) {
+            response()->json(['error' => 'Invalid Request'], 400);
+        }
 
-	if($user->hasRole('ccd-vendor')) {
-		$ccd = base64_decode($request['ccd_base64']);
-		//Save to CCD object or table
-		return response()->json(['message' => $ccd]);
-	}
-	return response()->json($user);
-});
-	 */
-	public function login()
-	{
-		$credentials = \Input::only('email', 'user_pass');
-		\JWTAuth::setIdentifier('ID');
-		//$user = \JWTAuth::parseToken()->authenticate();
-		if ( ! $token = \JWTAuth::attempt($credentials) )
-		{
-			return response()->json(['error' => 'invalid_credentials'], 400);
-		}
-		return response()->json(compact('token'));
-	}
+        $credentials = \Input::only('email', 'user_pass');
+        \JWTAuth::setIdentifier('ID');
+        if (!$token = \JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid Credentials'], 400);
+        }
+        return response()->json(compact('token'), 200);
+    }
 
-	public function create()
-	{
-		$user = \JWTAuth::parseToken()->authenticate();
+    public function create(Request $request)
+    {
+        $user = \JWTAuth::parseToken()->authenticate();
+        if (!$user) {
+            return response()->json(['error' => 'Invalid Token'], 400);
+        }
 
-		if($user->hasRole('ccd-vendor')) {
-			$ccd = base64_decode($request['ccd_base64']);
-			//Save to CCD object or table
-			return response()->json(['message' => $ccd]);
-		}
-		return response()->json($user);
-	}
+        if (!$request->has('ccd')) {
+            response()->json(['error' => 'No file found on the request.'], 422);
+        }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+        $blog = $user->blogId();
+        if (!$user->hasRole('ccd-vendor')) {
+            response()->json(['error' => 'Unauthorized Request'], 403);
+        }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+        $xml = base64_decode($request['ccd']);
+        $json = $this->repo->toJson($xml);
+        $ccdObj = Ccda::create([
+            'user_id' => $user->ID,
+            'vendor_id' => 1,
+            'xml' => $xml
+        ]);
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+        $ccdObj->json = $json;
+        $ccdObj->save();
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+        $importer = new QAImportManager($blog, $ccdObj);
+        $output = $importer->generateCarePlanFromCCD();
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+        $jsonCcd = json_decode($output->output, true);
+
+        return response()->json($jsonCcd);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
