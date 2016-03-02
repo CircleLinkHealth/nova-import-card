1f6efb8cc1e6197644433c4a34243ddc721a9822
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-01 21:18:11 -0500

    Adds ValidEndDateTest

---
+++ tests/Importer/ValidationStrategiesTests/ValidEndDateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 3/1/16
+ * Time: 8:59 PM
+ */
+class ValidEndDateTest extends TestCase
+{
+    public function getValidationStrategy()
+    {
+        return new \App\CLH\CCD\Importer\ValidationStrategies\ValidEndDate();
+    }
+
+    public function mockProblem($date)
+    {
+        $problem = new stdClass();
+        @$problem->date_range->end = $date;
+
+        return $problem;
+    }
+
+    public function test_null_date_returns_inactive()
+    {
+        $this->assertFalse( $this->getValidationStrategy()->validate( $this->mockProblem( null ) ) );
+    }
+
+    public function test_past_date_returns_inactive()
+    {
+        $this->assertFalse( $this->getValidationStrategy()->validate( $this->mockProblem( '12/29/2015' ) ) );
+    }
+
+    public function test_future_date_returns_active()
+    {
+        $this->assertTrue( $this->getValidationStrategy()->validate( $this->mockProblem( \Carbon\Carbon::tomorrow() ) ) );
+    }
+
+    public function test_invalid_date_returns_inactive()
+    {
+        $this->assertFalse( $this->getValidationStrategy()->validate( $this->mockProblem( 'not a date' ) ) );
+    }
+}
