0baa56eba4c0def226f7b00bbe1471bb857baf30
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-07 15:28:31 -0500

    Viewer uses node parser

--- app/Http/Controllers/CCDViewer/CCDViewerController.php
+++ app/Http/Controllers/CCDViewer/CCDViewerController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers\CCDViewer;
 
+use App\CLH\Repositories\CCDImporterRepository;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -7,49 +8,57 @@ use App\XmlCCD;
 use Illuminate\Http\Request;
 use JavaScript;
 
-class CCDViewerController extends Controller {
+class CCDViewerController extends Controller
+{
+
+    private $repo;
+
+    public function __construct(CCDImporterRepository $repo)
+    {
+        $this->repo = $repo;
+        ini_set( 'memory_limit', '-1' );
+    }
 
     public function create()
     {
-        return view('CCDViewer.old-viewer');
+        return view( 'CCDViewer.old-viewer' );
     }
 
-	public function showByUserId($userId)
+    public function showByUserId($userId)
     {
-        $ccd = XmlCCD::whereUserId($userId)->first()->ccd;
+        $ccd = XmlCCD::whereUserId( $userId )->first()->ccd;
 
-        $template = view('CCDViewer.bb-ccd-viewer', compact('ccd'))->render();
+        $template = view( 'CCDViewer.bb-ccd-viewer', compact( 'ccd' ) )->render();
 
-        return view('CCDViewer.viewer', compact('template'));
+        return view( 'CCDViewer.viewer', compact( 'template' ) );
     }
 
     public function showUploadedCcd(Request $request)
     {
-        if ($request->hasFile('uploadedCcd'))
-        {
-            $ccd = file_get_contents($request->file('uploadedCcd'));
+        if ( $request->hasFile( 'uploadedCcd' ) ) {
+            $ccd = file_get_contents( $request->file( 'uploadedCcd' ) );
 
-            $template = view('CCDViewer.bb-ccd-viewer', compact('ccd'))->render();
+            $template = view( 'CCDViewer.bb-ccd-viewer', compact( 'ccd' ) )->render();
 
-            return view('CCDViewer.viewer', compact('template'));
+            return view( 'CCDViewer.viewer', compact( 'template' ) );
         }
     }
 
     public function oldViewer(Request $request)
     {
-        if ($request->hasFile('uploadedCcd'))
-        {
-            $xml = file_get_contents($request->file('uploadedCcd'));
+        if ( $request->hasFile( 'uploadedCcd' ) ) {
+            $xml = file_get_contents( $request->file( 'uploadedCcd' ) );
 
-            return view('CCDViewer.old-viewer', compact('xml'));
+            $ccd = json_decode( $this->repo->toJson( $xml ) );
+
+            return view( 'CCDViewer.old-viewer', compact( 'ccd' ) );
         }
     }
 
     public function viewSource(Request $request)
     {
-        if ($xml = urldecode($request->input('xml')))
-        {
-            return view('CCDViewer.old-viewer', compact('xml'));
+        if ( $xml = urldecode( $request->input( 'xml' ) ) ) {
+            return view( 'CCDViewer.old-viewer', compact( 'xml' ) );
         }
     }
 
