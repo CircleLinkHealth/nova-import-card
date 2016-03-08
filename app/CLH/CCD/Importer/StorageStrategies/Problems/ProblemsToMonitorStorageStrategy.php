db89f5133eac0d395ee63ec72f3cfad154a99243
rohan <rohanm@circlelinkhealth.com>
2016-03-08 16:13:32 -0500

    - Migrates Problems to v3 cp

--- app/CLH/CCD/Importer/StorageStrategies/Problems/ProblemsToMonitorStorageStrategy.php
+++ app/CLH/CCD/Importer/StorageStrategies/Problems/ProblemsToMonitorStorageStrategy.php
@@ -15,33 +15,13 @@ class ProblemsToMonitorStorageStrategy extends BaseStorageStrategy implements St
     public function import($cpmProblemNames = [])
     {
         if (empty($cpmProblemNames)) return;
-
-        foreach ($cpmProblemNames as $cpmProblemName)
-        {
-            if ( empty($this->blogId) or empty($this->userId) ) throw new \Exception( 'UserID and BlogID are required.' );
-
-            $pcp = CPRulesPCP::whereProvId( $this->blogId )->whereSectionText( 'Diagnosis / Problems to Monitor' )->first();
-            if ( empty($pcp) ) {
-                Log::error( __METHOD__ . ' ' . __LINE__ . ' for userID ' . $this->userId . ', blogId ' . $this->blogId . ' has failed.' );
-                continue;
-            }
-            $pcpId = $pcp->pcp_id;
-
-            $rulesItem = CPRulesItem::wherePcpId( $pcpId )->whereItemsText( $cpmProblemName )->first();
-            if ( empty($rulesItem) ) {
-                Log::error( __METHOD__ . ' ' . __LINE__ . ' for userID ' . $this->userId . ', blogId ' . $this->blogId . ', ' . $cpmProblemName . 'has failed.' );
-                continue;
-            }
-            $parentItemId = $rulesItem->items_id;
-
-            //Set UI Item to Active
-            CPRulesUCP::updateOrCreate( [
-                'items_id' => $parentItemId,
-                'user_id' => $this->userId,
-                'meta_key' => 'status',
-            ], [
-                'meta_value' => 'Active',
-            ] );
+        if (empty($this->blogId) or empty($this->userId)) throw new \Exception('UserID and BlogID are required.');
+        $carePlan = CarePlan::where('program_id', '=', $this->blogId)->where('type', '=', 'Program Default')->first();
+        if(!$carePlan) {
+            return response()->json(["message" => "Careplan Not Found"]);
+        }
+        foreach ($cpmProblemNames as $cpmProblemName) {
+            $carePlan->setCareItemUserValue($this->user, $cpmProblemName,"Active");
         }
     }
 }
