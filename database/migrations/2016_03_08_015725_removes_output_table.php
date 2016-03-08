e041be181c4cca11b43b6a05669b0b973c93652e
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-08 13:34:22 -0500

    Updates migrations

---
+++ database/migrations/2016_03_08_015725_removes_output_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemovesOutputTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'q_a_import_summaries', function (Blueprint $table) {
+            DB::statement( 'SET FOREIGN_KEY_CHECKS=0;' );
+
+            $table->dropForeign( 'q_a_import_summaries_qa_output_id_foreign' );
+            $table->removeColumn( 'qa_output_id' );
+
+            Schema::dropIfExists( 'q_a_import_outputs' );
+
+            DB::statement( 'SET FOREIGN_KEY_CHECKS=1;' );
+
+            $table->unsignedInteger( 'ccda_id' )->after('id')->nullable();
+            $table->foreign( 'ccda_id' )
+                ->references( 'id' )
+                ->on( 'ccdas' )
+                ->onUpdate( 'cascade' )
+                ->onDelete( 'cascade' );
+        } );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table( 'q_a_import_summaries', function (Blueprint $table) {
+            $table->dropForeign( 'ccdas_ccda_id_foreign' );
+            $table->removeColumn( 'ccda_id' );
+        } );
+    }
+
+}
