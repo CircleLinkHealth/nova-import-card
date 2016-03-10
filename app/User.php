d077c8dcfa0ea717c0bf5bfe7561d5b5bfdf1d4e
kgallo <kgallo@circlelinkhealth.com>
2016-03-10 08:48:54 -0500

    [touch: 187] - updated AllUserScrambler seeder to pass in randomUserInfo param, one api call instead of many

--- app/User.php
+++ app/User.php
@@ -809,18 +809,20 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	}
 
 	// user data scrambler
-	public function scramble() {
+	public function scramble($randomUserInfo = false) {
 		// states array
 		$states = array('Alabama' => 'AL', 'Alaska' => 'AK', 'Arizona' => 'AZ', 'Arkansas' => 'AR', 'California' => 'CA', 'Colorado' => 'CO', 'Connecticut' => 'CT', 'Delaware' => 'DE', 'Florida' => 'FL', 'Georgia' => 'GA', 'Hawaii' => 'HI', 'Idaho' => 'ID', 'Illinois' => 'IL', 'Indiana' => 'IN', 'Iowa' => 'IA', 'Kansas' => 'KS', 'Kentucky' => 'KY', 'Louisiana' => 'LA', 'Maine' => 'ME', 'Maryland' => 'MD', 'Massachusetts' => 'MA', 'Michigan' => 'MI', 'Minnesota' => 'MN', 'Mississippi' => 'MS', 'Missouri' => 'MO', 'Montana' => 'MT', 'Nebraska' => 'NE', 'Nevada' => 'NV', 'New Hampshire' => 'NH', 'New Jersey' => 'NJ', 'New Mexico' => 'NM', 'New York' => 'NY', 'North Carolina' => 'NC', 'North Dakota' => 'ND', 'Ohio' => 'OH', 'Oklahoma' => 'OK', 'Oregon' => 'OR', 'Pennsylvania' => 'PA', 'Rhode Island' => 'RI', 'South Carolina' => 'SC', 'South Dakota' => 'SD', 'Tennessee' => 'TN', 'Texas' => 'TX', 'Utah' => 'UT', 'Vermont' => 'VT', 'Virginia' => 'VA', 'Washington' => 'WA', 'West Virginia' => 'WV', 'Wisconsin' => 'WI', 'Wyoming' => 'WY');
 
 		// Some Randomness
 		// https://randomuser.me/api/?nat=us&results=3
-		$json_string = file_get_contents("https://randomuser.me/api/?nat=us&results=1");
-		if(empty($json_string)) {
-			return false;
+		if(!$randomUserInfo) {
+			$json_string = file_get_contents("https://randomuser.me/api/?nat=us&results=1");
+			if (empty($json_string)) {
+				return false;
+			}
+			$randomUserInfo = json_decode($json_string);
+			$randomUserInfo = $randomUserInfo->results[0]->user;
 		}
-		$randomUserInfo = json_decode($json_string);
-		$randomUserInfo = $randomUserInfo->results[0]->user;
 
 		//dd($randomUserInfo);
 		// set random data
--- database/seeds/config/AllUserScrambler.php
+++ database/seeds/config/AllUserScrambler.php
@@ -14,15 +14,24 @@ class AllUserScrambler extends Seeder {
     {
         $allUsers = User::all();
         if(!empty($allUsers)) {
+            $json_string = file_get_contents("https://randomuser.me/api/?nat=us&results=".$allUsers->count());
+            if (empty($json_string)) {
+                return false;
+            }
+            $randomUsers = json_decode($json_string);
+            $u = 0;
             foreach($allUsers as $user) {
-                $role = $user->roles()->first();
-                if($role && strtolower($role->name) != 'administrator' && strtolower($role->name) != 'provider') {
-                    echo PHP_EOL;
-                    echo PHP_EOL;
-                    echo PHP_EOL.$role->name;
-                    echo PHP_EOL.$user->ID. '-' . $user->user_email;
-                    $user->scramble();
-                    echo PHP_EOL.$user->ID. '-' . $user->user_email;
+                if(isset($randomUsers->results[$u]->user) && $u < 2) {
+                    $randomUserInfo = $randomUsers->results[$u]->user;
+                    $role = $user->roles()->first();
+                    if ($role && strtolower($role->name) != 'administrator' && strtolower($role->name) != 'provider') {
+                        echo PHP_EOL.PHP_EOL;
+                        echo PHP_EOL . $role->name;
+                        echo PHP_EOL . $user->ID . '-' . $user->user_email;
+                        $user->scramble($randomUserInfo);
+                        echo PHP_EOL . $user->ID . '-' . $user->user_email;
+                        $u++;
+                    }
                 }
             }
         }
