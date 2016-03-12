cfa6f61dfaa84c9576f3e687cbc657cec724ad0d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-11 19:40:08 -0500

    Updates Medications Parsing Strategies

--- app/CLH/CCD/Importer/ParsingStrategies/Medications/ProductNameAndTextMedsListParser.php
+++ app/CLH/CCD/Importer/ParsingStrategies/Medications/ProductNameAndTextMedsListParser.php
@@ -4,6 +4,7 @@ namespace App\CLH\CCD\Importer\ParsingStrategies\Medications;
 
 
 use App\CLH\CCD\Ccda;
+use App\CLH\CCD\ImportedItems\MedicationImport;
 use App\CLH\CCD\ItemLogger\CcdMedicationLog;
 use App\CLH\Contracts\CCD\ParsingStrategy;
 use App\CLH\Contracts\CCD\ValidationStrategy;
@@ -11,6 +12,7 @@ use App\CLH\Facades\StringManipulation;
 
 class ProductNameAndTextMedsListParser implements ParsingStrategy
 {
+    use ConsolidatesMedicationInfoTrait;
 
     public function parse(Ccda $ccd, ValidationStrategy $validator = null)
     {
@@ -24,7 +26,20 @@ class ProductNameAndTextMedsListParser implements ParsingStrategy
             $medication->import = true;
             $medication->save();
 
-            $medsList[] = $medication;
+            $consMed = $this->consolidateMedicationInfo($medication);
+
+            $importedMed = (new MedicationImport())->create([
+                'ccda_id' => $ccd->id,
+                'vendor_id' => $ccd->vendor_id,
+                'ccd_medication_log_id' => $medication->id,
+                'name' => $consMed->cons_name,
+                'sig' => StringManipulation::stringDiff( $medication->cons_name, $medication->text ),
+                'code' => $consMed->cons_code,
+                'code_system' => $consMed->cons_code_system,
+                'code_system_name' => $consMed->cons_code_system_name,
+            ]);
+
+            $medsList[] = $importedMed;
 
 //            $medsList .= "\n\n";
 //            empty($medication->product->name)
--- app/CLH/CCD/Importer/ParsingStrategies/Medications/ReferenceTitleAndSigMedsListParser.php
+++ app/CLH/CCD/Importer/ParsingStrategies/Medications/ReferenceTitleAndSigMedsListParser.php
@@ -4,6 +4,7 @@ namespace App\CLH\CCD\Importer\ParsingStrategies\Medications;
 
 
 use App\CLH\CCD\Ccda;
+use App\CLH\CCD\ImportedItems\MedicationImport;
 use App\CLH\CCD\ItemLogger\CcdMedicationLog;
 use App\CLH\Contracts\CCD\ParsingStrategy;
 use App\CLH\Contracts\CCD\ValidationStrategy;
@@ -22,7 +23,21 @@ class ReferenceTitleAndSigMedsListParser implements ParsingStrategy
             $medication->import = true;
             $medication->save();
 
-            $medsList[] = $medication;
+            $consMed = $this->consolidateMedicationInfo($medication);
+
+            $importedMed = (new MedicationImport())->create([
+                'ccda_id' => $ccd->id,
+                'vendor_id' => $ccd->vendor_id,
+                'ccd_medication_log_id' => $medication->id,
+                'name' => $medication->reference_title,
+                'sig' => $medication->reference_sig,
+                'code' => $consMed->cons_code,
+                'code_system' => $consMed->cons_code_system,
+                'code_system_name' => $consMed->cons_code_system_name,
+            ]);
+
+            $medsList[] = $importedMed;
+
 //            $medsList .= $medication->reference_title;
 //
 //            empty($medication->reference_sig)
