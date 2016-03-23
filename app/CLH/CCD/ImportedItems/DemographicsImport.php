7df4b372086b5265599a5aeec063c8e8748fec7c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-22 22:09:44 -0400

    Adds soft deletes

--- app/CLH/CCD/ImportedItems/DemographicsImport.php
+++ app/CLH/CCD/ImportedItems/DemographicsImport.php
@@ -2,8 +2,18 @@
 
 use App\CLH\CCD\ItemLogger\CcdDemographicsLog;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DemographicsImport extends Model {
+
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
     protected $guarded = [];
 
