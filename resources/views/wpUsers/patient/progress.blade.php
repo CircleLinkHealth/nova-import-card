7fe4828b9c081c52c156bbe9b091b4468cc477c5
rohan <rohanm@circlelinkhealth.com>
2016-03-23 20:52:10 -0400

    [touch: 195] Fix size of title

--- resources/views/wpUsers/patient/progress.blade.php
+++ resources/views/wpUsers/patient/progress.blade.php
@@ -21,7 +21,7 @@ $provider = App\User::find($patient->getBillingProviderIDAttribute());
                     </div>
                     <div class="row">
                         <div class="col-xs-12">
-                            <h1 class="patient-summary__title patient-summary__title_7 patient-summary--careplan patient-summary--progress-report">
+                            <h1 class="patient-summary__title patient-summary__title_9 patient-summary--careplan">
                                 Progress Report:</h1>
                         </div>
                     </div>
