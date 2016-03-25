c97044e0c70be62b310bfcc07aedb0133c82cbf2
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-25 16:42:48 -0400

    Adds demo ccm time

--- app/Http/Controllers/CCDAPI/Aprima/CCMTimeController.php
+++ app/Http/Controllers/CCDAPI/Aprima/CCMTimeController.php
@@ -9,7 +9,17 @@ class CCMTimeController extends Controller {
 
 	public function getCcmTime()
 	{
-		return 'here';
-	}
+		$demo = [
+			'patientId' => 103,
+			'providerId' => 100,
+			'careEvents' => [
+				'servicePerson' => 'Bob',
+				'startingDateTime' => '2015-05-26 18:32:00',
+				'length' => '10',
+				'commentstring' => 'Call Center Contact'
+			]
+		];
 
+		return json_encode($demo, JSON_FORCE_OBJECT);
+	}
 }
