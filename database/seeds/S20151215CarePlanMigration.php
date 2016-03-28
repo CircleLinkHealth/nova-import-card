ac242c103b1a8266f827c3c9d2e2df3799d037bf
kgallo <kgallo@circlelinkhealth.com>
2016-03-27 20:24:22 -0400

    [touch: 221] - fix for care_team -> care-team, added to careplan migration seeder

--- database/seeds/S20151215CarePlanMigration.php
+++ database/seeds/S20151215CarePlanMigration.php
@@ -19,6 +19,8 @@ class S20151215CarePlanMigration extends Seeder {
 
     public function run()
     {
+
+        $this->command->info('truncate tables.....');
         DB::connection('mysql_no_prefix')->statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::connection('mysql_no_prefix')->table('care_item_user_values')->truncate();
         DB::connection('mysql_no_prefix')->table('care_item_care_plan')->truncate();
@@ -27,9 +29,14 @@ class S20151215CarePlanMigration extends Seeder {
         DB::connection('mysql_no_prefix')->table('care_plans')->truncate();
         DB::connection('mysql_no_prefix')->table('care_sections')->truncate();
         DB::connection('mysql_no_prefix')->statement('SET FOREIGN_KEY_CHECKS=1;');
+        $this->command->info('done truncating');
 
         // hack fix for bad data, missing qid for 5 programs on prod
-        DB::statement("UPDATE rules_items SET qid = 25 WHERE items_text = 'Smoking'");
+        $rows = DB::update("UPDATE rules_items SET qid = 25 WHERE items_text = 'Smoking'");
+        $this->command->info('UPDATE rules_items..... rows affected =' . $rows);
+        // fix for time tracking rules
+        $rows = DB::update("UPDATE lv_rules_intr_conditions SET value = 'care-center' WHERE value LIKE '%care_center%'");
+        $this->command->info('UPDATE lv_rules_intr_conditions..... rows affected =' . $rows);
 
         $this->call('database\seeds\S20151215CarePlanMigration1');
         $this->command->info('Part 1 Completed Successfully');
