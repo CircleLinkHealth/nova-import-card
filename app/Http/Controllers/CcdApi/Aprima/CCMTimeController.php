e17f5942a05481c1cfb08bbfba19fa7955eae215
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-03-29 15:37:09 -0400

    Updates demos

--- app/Http/Controllers/CcdApi/Aprima/CCMTimeController.php
+++ app/Http/Controllers/CcdApi/Aprima/CCMTimeController.php
@@ -10,16 +10,44 @@ class CCMTimeController extends Controller {
 	public function getCcmTime()
 	{
 		$demo = [
-			'patientId' => 103,
-			'providerId' => 100,
-			'careEvents' => [
-				'servicePerson' => 'Bob',
-				'startingDateTime' => '2015-05-26 18:32:00',
-				'length' => '10',
-				'commentstring' => 'Call Center Contact'
+			[
+				'patientId' => 103,
+				'providerId' => 100,
+				'careEvents' => [
+					[
+						'servicePerson' => 'Bob',
+						'startingDateTime' => '2015-05-26 18:32:00',
+						'length' => '10',
+						'commentstring' => 'Call Center Contact'
+					],
+					[
+						'servicePerson' => 'Marie',
+						'startingDateTime' => '2015-05-26 18:12:00',
+						'length' => '8',
+						'commentstring' => 'Blood Pressure Monitor'
+					]
+				]
+			],
+			[
+				'patientId' => 105,
+				'providerId' => 101,
+				'careEvents' => [
+					[
+						'servicePerson' => 'Mario',
+						'startingDateTime' => '2015-05-26 18:32:00',
+						'length' => '5',
+						'commentstring' => 'Measure Weigh'
+					],
+					[
+						'servicePerson' => 'John',
+						'startingDateTime' => '2015-05-26 18:12:00',
+						'length' => '2',
+						'commentstring' => 'Call Center Contact'
+					]
+				]
 			]
 		];
 
-		return json_encode($demo, JSON_FORCE_OBJECT);
+		return json_encode($demo);
 	}
 }
