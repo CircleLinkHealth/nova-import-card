9688d2b31042f4c00cd23f65c9bbd86f7c9b014d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-04-01 13:11:17 -0400

    Seeder small change

--- database/seeds/AprimaApi/AprimaUsers.php
+++ database/seeds/AprimaApi/AprimaUsers.php
@@ -31,8 +31,10 @@ class AprimaUsers extends Seeder
             ], [
                 'password' => \Hash::make( $password ),
             ] );
+
+            $relationships = DB::table('location_user')->where('user_id', '=', $user->ID)->where('location_id', '=', $loc->id)->count();
             
-            $user->locations()->attach( $loc->id );
+            if ($relationships < 1) $user->locations()->attach( $loc->id );
 
             //attach role if it's not there
             if (! $user->hasRole('aprima-api-location')) $user->attachRole( $role );
