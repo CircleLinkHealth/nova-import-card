756bb8214d7893f312e010ac1da19199078b4b58
rohan <rohanm@circlelinkhealth.com>
2016-04-12 17:21:33 -0400

    - Adds comments to Aprima Activities

--- app/Repositories/ActivityRepositoryEloquent.php
+++ app/Repositories/ActivityRepositoryEloquent.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\ActivityMeta;
 use App\CcmTimeApiLog;
 use App\User;
 use Illuminate\Support\Facades\DB;
@@ -54,6 +55,7 @@ class ActivityRepositoryEloquent extends BaseRepository implements ActivityRepos
         //Dynamically get all the tables' names since we'll probably change them soon
         $activitiesTable = (new Activity())->getTable();
         $userTable = (new User())->getTable();
+        $actMeta = (new ActivityMeta())->getTable();
 
         $activities = Activity::select(DB::raw("
                 $activitiesTable.id as id,
@@ -61,11 +63,14 @@ class ActivityRepositoryEloquent extends BaseRepository implements ActivityRepos
                 duration as length,
                 duration_unit as lengthUnit,
                 $userTable.display_name as servicePerson,
-                $activitiesTable.performed_at as startingDateTime
+                $activitiesTable.performed_at as startingDateTime,
+                $actMeta.meta_value as comment
                 "))
             ->whereProviderId($providerId)
             ->wherePatientId($patientId)
             ->join($userTable, "$userTable.ID", '=', "$activitiesTable.provider_id")
+            ->join($actMeta, "$actMeta.activity_id", '=', "$activitiesTable.id")
+            ->where("$actMeta.meta_key", 'comment')
             ->whereBetween("$activitiesTable.performed_at", [
                 $startDate, $endDate
             ]);
