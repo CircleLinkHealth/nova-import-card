b03b74b685daa93d1cecb60618ac2a9af4677ccb
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-04-18 13:29:04 -0400

    Adds CcdMedication

---
+++ app/Ccd/CcdMedication.php
@@ -0,0 +1,17 @@
+<?php namespace App\App\Ccd;
+
+use App\CLH\CCD\ItemLogger\CcdMedicationLog;
+use Illuminate\Database\Eloquent\Model;
+
+class CcdMedication extends Model {
+
+    protected $guarded = [];
+
+    protected $table = 'ccd_medications';
+
+    public function ccdLog()
+    {
+        return $this->belongsTo(CcdMedicationLog::class);
+    }
+
+}
