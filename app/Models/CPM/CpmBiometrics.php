f3cc694f3010755642ca39b0fd5d66e6f85488b5
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-04-27 17:56:27 -0400

    Adds Biometric stuff

---
+++ app/Models/CPM/CpmBiometrics.php
@@ -0,0 +1,9 @@
+<?php namespace App\Models\CPM;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CpmBiometrics extends Model {
+
+	//
+
+}
---
+++ database/seeds/Entities/CPM/CpmBiometrics.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 4/27/16
+ * Time: 5:16 PM
+ */
+class CpmBiometrics
+{
+    public function run()
+    {
+        $entities[] = [
+            'name' => 'Weight',
+            'type' => \App\Models\CPM\Biometrics\CpmWeight::class,
+        ];
+
+        $entities[] = [
+            'name' => 'Blood Pressure',
+            'type' => \App\Models\CPM\Biometrics\CpmBloodPressure::class,
+        ];
+
+        $entities[] = [
+            'name' => 'Blood Sugar',
+            'type' => \App\Models\CPM\Biometrics\CpmBloodSugar::class,
+        ];
+
+        $entities[] = [
+            'name' => 'Smoking (# per day)',
+            'type' => \App\Models\CPM\Biometrics\CpmSmoking::class,
+        ];
+
+        foreach ($entities as $entity) {
+            \App\Models\CPM\CpmBiometrics::updateOrCreate($entity);
+        }
+    }
+}
