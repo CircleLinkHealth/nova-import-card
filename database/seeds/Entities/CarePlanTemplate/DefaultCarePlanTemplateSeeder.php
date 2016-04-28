dff1b02a7e74e226810db2e566002fb3fb7e1ebb
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-04-28 18:08:23 -0400

    Seeder  [touch:277]

--- database/seeds/Entities/CarePlanTemplate/DefaultCarePlanTemplateSeeder.php
+++ database/seeds/Entities/CarePlanTemplate/DefaultCarePlanTemplateSeeder.php
@@ -15,16 +15,6 @@ class DefaultCarePlanTemplateSeeder extends \Illuminate\Database\Seeder
             'type' => \App\CarePlanTemplate::CLH_DEFAULT,
         ]);
 
-        /*
-         * Relate all CpmProblems and get their ui_sort from CarePlanItem
-         */
-        $cpmProblems = \App\Models\CPM\CpmProblem::all();
-        $cpt->cpmProblems()->sync($cpmProblems);
-
-        foreach ($cpmProblems as $problem) {
-            $cpi = \App\CarePlanItem::whereItemId($problem->care_item_id)->first();
-            $cpt->cpmProblems()->updateExistingPivot($problem->id, ['ui_sort' => $cpi->ui_sort]);
-        }
 
         /*
          * Relate all CpmLifestyles and get their ui_sort from CarePlanItem
@@ -35,6 +25,42 @@ class DefaultCarePlanTemplateSeeder extends \Illuminate\Database\Seeder
         foreach ($cpmLifestyles as $lifestyle) {
             $cpi = \App\CarePlanItem::whereItemId($lifestyle->care_item_id)->first();
             $cpt->cpmLifestyles()->updateExistingPivot($lifestyle->id, ['ui_sort' => $cpi->ui_sort]);
+        }
+
+
+        /*
+         * Relate all CpmMedicationGroups and get their ui_sort from CarePlanItem
+         */
+        $cpmMedGroup = \App\Models\CPM\CpmMedicationGroup::all();
+        $cpt->cpmMedicationGroups()->sync($cpmMedGroup);
+
+        foreach ($cpmMedGroup as $medGroup) {
+            $cpi = \App\CarePlanItem::whereItemId($medGroup->care_item_id)->first();
+            $cpt->cpmMedicationGroups()->updateExistingPivot($medGroup->id, ['ui_sort' => $cpi->ui_sort]);
+        }
+
+
+        /*
+         * Relate all CpmMisc and get their ui_sort from CarePlanItem
+         */
+        $cpmMisc = \App\Models\CPM\CpmMisc::all();
+        $cpt->cpmMiscs()->sync($cpmMisc);
+
+        foreach ($cpmMisc as $misc) {
+            $cpi = \App\CarePlanItem::whereItemId($misc->care_item_id)->first();
+            $cpt->cpmMiscs()->updateExistingPivot($misc->id, ['ui_sort' => $cpi->ui_sort]);
+        }
+
+
+        /*
+         * Relate all CpmProblems and get their ui_sort from CarePlanItem
+         */
+        $cpmProblems = \App\Models\CPM\CpmProblem::all();
+        $cpt->cpmProblems()->sync($cpmProblems);
+
+        foreach ($cpmProblems as $problem) {
+            $cpi = \App\CarePlanItem::whereItemId($problem->care_item_id)->first();
+            $cpt->cpmProblems()->updateExistingPivot($problem->id, ['ui_sort' => $cpi->ui_sort]);
         }
     }
 }
