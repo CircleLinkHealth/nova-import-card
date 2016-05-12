d0afec0fa545dbf25b4adddae432c0d09a595c84
rohan <rohanm@circlelinkhealth.com>
2016-05-12 15:45:08 -0400

    [touch: 256] Adds getProblemsWithInstructions function

--- app/Services/CPM/CpmProblemService.php
+++ app/Services/CPM/CpmProblemService.php
@@ -38,11 +38,24 @@ class CpmProblemService implements CpmModel
         return true;
     }
 
+    public function getProblemsWithInstructionsForUser(User $user){
+
+        //Get all the User's Problems
+        $problems =  $user->cpmProblems()->get()->all();
+        //For each problem, extract the instructions and
+        //store in a key value pair
+        foreach($problems as $problem){
+            $instruction = \App\Models\CPM\CpmInstruction::find($problem->pivot->cpm_instruction_id);
+            $instructions[$problem->name] = $instruction->name;
+        }
+        return $instructions;
+    }
+
     /**
      * @param User $patient
      * @return array|bool
      */
-    public function getProblemsToMonitorWithDetails(User $patient)
+    public function getDetails(User $patient)
     {
         $carePlan = $patient->service()->firstOrDefaultCarePlan($patient);
 
@@ -56,7 +69,7 @@ class CpmProblemService implements CpmModel
         //get the User's cpmProblems
         $patientProblems = $patient->cpmProblems()->get();
 
-        $intersection = $patientProblems->intersect($cptProblems)->all();
+        $intersection = $patientProblems->intersect($cptProblems)->lists('name');
 
         return $intersection;
     }
