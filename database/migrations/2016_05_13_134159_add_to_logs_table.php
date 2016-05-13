2997ee5db96289df4e548215ca3bf164ad52d2a0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-05-13 16:23:33 -0400

    Comments out migration

--- database/migrations/2016_05_13_134159_add_to_logs_table.php
+++ database/migrations/2016_05_13_134159_add_to_logs_table.php
@@ -12,19 +12,19 @@ class AddToLogsTable extends Migration
      */
     public function up()
     {
-        Schema::table('ccd_demographics_logs', function (Blueprint $table) {
-            $table->renameColumn('language', 'preferred_contact_language');
-            $table->string('consent_date');
-            $table->string('preferred_contact_timezone');
-            $table->string('study_phone_number');
-            $table->unsignedInteger('location_id');
-//not sure why this is failing come back later
-//            $table->foreign('location_id')
-//                ->references('id')
-//                ->on((new \App\Location())->getTable())
-//                ->onUpdate('cascade')
-//                ->onDelete('set null');
-        });
+//        Schema::table('ccd_demographics_logs', function (Blueprint $table) {
+//            $table->renameColumn('language', 'preferred_contact_language');
+//            $table->string('consent_date');
+//            $table->string('preferred_contact_timezone');
+//            $table->string('study_phone_number');
+//            $table->unsignedInteger('location_id');
+////not sure why this is failing come back later
+////            $table->foreign('location_id')
+////                ->references('id')
+////                ->on((new \App\Location())->getTable())
+////                ->onUpdate('cascade')
+////                ->onDelete('set null');
+//        });
     }
 
     /**
@@ -34,15 +34,15 @@ class AddToLogsTable extends Migration
      */
     public function down()
     {
-        DB::statement('set foreign_key_checks = 0');
-        Schema::table('ccd_demographics_logs', function (Blueprint $table) {
-            $table->renameColumn('preferred_contact_language', 'language');
-            $table->dropColumn('consent_date');
-            $table->dropColumn('preferred_contact_timezone');
-            $table->dropColumn('study_phone_number');
-            $table->dropColumn('location_id');
-        });
-        DB::statement('set foreign_key_checks = 1');
+//        DB::statement('set foreign_key_checks = 0');
+//        Schema::table('ccd_demographics_logs', function (Blueprint $table) {
+//            $table->renameColumn('preferred_contact_language', 'language');
+//            $table->dropColumn('consent_date');
+//            $table->dropColumn('preferred_contact_timezone');
+//            $table->dropColumn('study_phone_number');
+//            $table->dropColumn('location_id');
+//        });
+//        DB::statement('set foreign_key_checks = 1');
 
     }
 }
