383e84592c688de33b30fc798abf922e79e2d410
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-05-13 10:57:20 -0400

    Seeders [touch:277]

---
+++ database/seeds/DataMigration/CarePlanRefactoring/MigrateCcdAttributes.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 5/13/16
+ * Time: 10:56 AM
+ */
+class MigrateCcdAttributes extends \Illuminate\Database\Seeder
+{
+    public function run()
+    {
+
+    }
+}
