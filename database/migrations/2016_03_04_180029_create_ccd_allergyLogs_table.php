28dceab377d76cecacad8b6f73b4de0a2559588b
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-05-16 14:54:28 -0400

    Adds checks

--- database/migrations/2016_03_04_180029_create_ccd_allergyLogs_table.php
+++ database/migrations/2016_03_04_180029_create_ccd_allergyLogs_table.php
@@ -3,54 +3,58 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCcdAllergyLogsTable extends Migration {
+class CreateCcdAllergyLogsTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('ccd_allergy_logs', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('ccd_allergy_logs')) {
 
-			$table->unsignedInteger('ccda_id');
-			$table->foreign('ccda_id')
-				->references('id')
-				->on('ccdas')
-				->onUpdate('cascade')
-				->onDelete('cascade');
+            Schema::create('ccd_allergy_logs', function (Blueprint $table) {
+                $table->increments('id');
 
-			$table->unsignedInteger( 'vendor_id' );
-			$table->foreign( 'vendor_id' )
-				->references( 'id' )
-				->on( 'ccd_vendors' )
-				->onUpdate( 'cascade' )
-				->onDelete( 'cascade' );
+                $table->unsignedInteger('ccda_id');
+                $table->foreign('ccda_id')
+                    ->references('id')
+                    ->on('ccdas')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
 
-			$table->string('start')->nullable()->default(null);
-			$table->string('end')->nullable()->default(null);
-			$table->string('status')->nullable()->default(null);
-			$table->string('allergen_name')->nullable()->default(null);
+                $table->unsignedInteger('vendor_id');
+                $table->foreign('vendor_id')
+                    ->references('id')
+                    ->on('ccd_vendors')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
 
-			$table->boolean('import');
-			$table->boolean('invalid');
+                $table->string('start')->nullable()->default(null);
+                $table->string('end')->nullable()->default(null);
+                $table->string('status')->nullable()->default(null);
+                $table->string('allergen_name')->nullable()->default(null);
 
-			$table->softDeletes();
-			$table->timestamps();
-		});
-	}
+                $table->boolean('import');
+                $table->boolean('invalid');
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('ccd_allergy_logs');
-	}
+                $table->softDeletes();
+                $table->timestamps();
+            });
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ccd_allergy_logs');
+    }
 
 }
