0aac76e322f78e599b931f5506de68bb6e588cc4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-05-17 16:28:53 -0400

    Test

---
+++ database/migrations/2016_05_17_162209_test.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Test extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('a_test_this_is', function (Blueprint $table) {
+            $table->drop();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('another_test_this_is');
+    }
+}
