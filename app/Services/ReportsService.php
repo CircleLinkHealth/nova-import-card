eff33d4d02342376b5127bdee8003417ae9b6427
rohan <rohanm@circlelinkhealth.com>
2016-05-23 13:20:29 -0400

    [touch: 201] Adds Checks

--- app/Services/ReportsService.php
+++ app/Services/ReportsService.php
@@ -835,17 +835,15 @@ class ReportsService
             $biometric = $user->cpmBiometrics->where('name', $metric)->first();
             $biometric_values = app(config('cpmmodelsmap.biometrics')[$biometric->type])->getUserValues($user);
 
+            if(($biometric_values)){
+                if (($biometric_values['starting'] == '')) {
+                    $biometric_values['starting'] = 'TBD';
+                }
 
-                if(($biometric_values['starting'] == ''))
-                    {
-                        $biometric_values['starting'] = 'TBD';
-                    }
-
-                if(($biometric_values['target'] == ''))
-                    {
-                        $biometric_values['target'] = 'TBD';
-                    }
-
+                if (($biometric_values['target'] == '')) {
+                    $biometric_values['target'] = 'TBD';
+                }
+            }
 
             $careplanReport[$user->ID]['bio_data'][$metric]['target'] = $biometric_values['target'] . ReportsService::biometricsUnitMapping($metric);
             $careplanReport[$user->ID]['bio_data'][$metric]['starting'] = $biometric_values['starting'] . ReportsService::biometricsUnitMapping($metric);
