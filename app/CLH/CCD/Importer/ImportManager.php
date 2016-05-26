06a00cb372827d5377460a93dd70efa90f029b28
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-05-26 00:33:42 -0400

    Adds weight [touch:305]

--- app/CLH/CCD/Importer/ImportManager.php
+++ app/CLH/CCD/Importer/ImportManager.php
@@ -3,7 +3,9 @@
 namespace App\CLH\CCD\Importer;
 
 
+use App\CLH\CCD\Ccda;
 use App\CLH\CCD\ImportedItems\DemographicsImport;
+use App\CLH\CCD\Importer\StorageStrategies\Biometrics\Weight;
 use App\CLH\CCD\Importer\StorageStrategies\DefaultSections\TransitionalCare;
 use App\Models\CCD\CcdAllergy;
 use App\Models\CCD\CcdMedication;
@@ -23,13 +25,15 @@ class ImportManager
     private $problemsImport;
     private $ccdaStrategies;
     private $user;
+    private $decodedCcda;
 
     public function __construct(array $allergiesImport = null,
                                 DemographicsImport $demographicsImport,
                                 array $medicationsImport,
                                 array $problemsImport,
                                 array $strategies,
-                                User $user)
+                                User $user,
+                                Ccda $ccda)
     {
         $this->allergiesImport = $allergiesImport;
         $this->demographicsImport = $demographicsImport;
@@ -37,6 +41,7 @@ class ImportManager
         $this->problemsImport = $problemsImport;
         $this->ccdaStrategies = $strategies;
         $this->user = $user;
+        $this->decodedCcda = \GuzzleHttp\json_decode($ccda->json);
     }
 
     public function import()
@@ -166,9 +171,21 @@ class ImportManager
         /**
          * CarePlan Defaults
          */
+        
+        //Transitional Care
         $miscId = CpmMisc::whereName(CpmMisc::TRACK_CARE_TRANSITIONS)->first();
-
         $this->user->cpmMiscs()->attach($miscId->id);
+
+
+        /**
+         * Biometrics
+         */
+        
+        //Weight
+        $weightParseAndStore = new Weight($this->user->program_id, $this->user);
+        $weight = $weightParseAndStore->parse($this->decodedCcda);
+        
+        if (!empty($weight)) $weightParseAndStore->import($weight);
 
         return true;
     }
---
+++ app/CLH/CCD/Importer/StorageStrategies/Biometrics/Weight.php
@@ -0,0 +1,66 @@
+<?php namespace App\CLH\CCD\Importer\StorageStrategies\Biometrics;
+
+use App\CLH\CCD\Importer\StorageStrategies\BaseStorageStrategy;
+use App\CLH\Contracts\CCD\StorageStrategy;
+use App\Models\CPM\CpmBiometric;
+
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 5/25/16
+ * Time: 11:55 PM
+ */
+class Weight extends BaseStorageStrategy implements StorageStrategy
+{
+
+    public function import($weight)
+    {
+        $this->user->cpmWeight()->create([
+            'starting' => $weight
+        ]);
+
+        $biometric = CpmBiometric::whereName(CpmBiometric::WEIGHT)->first();
+
+        $this->user->cpmBiometrics()->attach($biometric->id);
+    }
+
+    public function parse($jsonDecodedCcda)
+    {
+        $codes = [
+            '3141-9', //body weight, measured
+        ];
+
+        $names = [
+            'weight',
+            'body weight',
+        ];
+
+        $unit = [
+            'kg',
+            'kilos',
+            'kilograms',
+            'kgs',
+        ];
+
+        $vitals = $jsonDecodedCcda->vitals;
+
+        if (empty($vitals)) return;
+
+        foreach ($vitals as $vital) {
+            foreach ($vital->results as $result) {
+                if (in_array($result->code, $codes) || in_array(strtolower($result->name), $names)) {
+                    $weight = $result->value;
+
+                    if (in_array($result->unit, $unit)) {
+                        //if it's in kg, we convert to lbs
+                        $weight = round($weight * 2.2046, 1);
+                    }
+
+                    return $weight;
+                }
+            }
+        }
+
+        return false;
+    }
+}
