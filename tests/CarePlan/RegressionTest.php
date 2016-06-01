431b9907e220550f1267ea9ea6594d698a5ba461
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-06-01 17:08:06 -0400

    Adds create provider test [touch: 347]

---
+++ tests/CarePlan/RegressionTest.php
@@ -0,0 +1,60 @@
+<?php
+use App\CLH\Repositories\UserRepository;
+use App\User;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 4/19/16
+ * Time: 4:34 PM
+ */
+class RegressionTest extends TestCase
+{
+    use \Illuminate\Foundation\Testing\DatabaseTransactions;
+
+    public function testCreateProvider()
+    {
+        $faker = Faker\Factory::create();
+
+        $firstName = $faker->firstName;
+        $lastName = $faker->lastName;
+        $email = $faker->email;
+
+        $roles = [
+            \App\Role::whereName('provider')->first()->id,
+        ];
+
+        $bag = new ParameterBag([
+            'user_email' => $email,
+            'user_pass' => 'password',
+            'display_name' => "$firstName $lastName",
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'user_login' => $faker->userName,
+            'program_id' => 9, //testdrive
+            'address' => $faker->streetAddress,
+            'address2' => '',
+            'city' => $faker->city,
+            'state' => 'AL',
+            'zip' => '12345',
+            'is_auto_generated' => true,
+            'roles' => $roles,
+        ]);
+
+        //create a user
+        $user = (new UserRepository())->createNewUser(new User(), $bag);
+
+        //check that it was created
+        $this->seeInDatabase('users', ['user_email' => $email]);
+
+        //check that the roles were created
+        foreach ($roles as $role)
+        {
+            $this->seeInDatabase('lv_role_user', [
+                'user_id' => $user->ID,
+                'role_id' => $role,
+            ]);
+        }
+    }
+}
