f83d522fb12d44ea22b48a1e8e52e9536adc71fe
Kevin Gallo <kevin.gallo@abilto.com>
2016-06-02 17:36:21 -0400

    [touch: 354] - updates to seeder S20160602UserAddressMigration to migrate data for withTrashed() deleted users

--- database/seeds/S20160602UserAddressMigration.php
+++ database/seeds/S20160602UserAddressMigration.php
@@ -30,11 +30,15 @@ class S20160602UserAddressMigration extends Seeder {
     public function migrateUserAddress()
     {
         // seed data user demographics
-        $users = User::with('meta')->where('ID', '<', '2065')->get();
+        $users = User::withTrashed()->with('meta')->where('ID', '<', '2065')->get();
         echo 'Process all role users demographics - Users found: '.$users->count().PHP_EOL;
         foreach($users as $user) {
             echo 'Processing user '.$user->ID.PHP_EOL;
             echo 'Rebuild User address / address2 / zip / status'.PHP_EOL;
+            $user->first_name = $user->getUserMetaByKey('first_name');
+            $user->last_name = $user->getUserMetaByKey('last_name');
+            $user->city = $user->getUserConfigByKey('city');
+            $user->state = $user->getUserConfigByKey('state');
             $user->address = $user->getUserConfigByKey('address');
             $user->address2 = $user->getUserConfigByKey('address2');
             $user->zip = $user->getUserConfigByKey('zip');
