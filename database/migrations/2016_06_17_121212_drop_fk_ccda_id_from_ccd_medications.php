20a72559fb1a29e41be10ab02677508d1b51642b
kgallo <kgallo@circlelinkhealth.com>
2016-06-17 19:21:20 -0400

    [touch: 306] - fixes to migration, hacky - commenting out to run on stage than will uncomment and commit

--- database/migrations/2016_06_17_121212_drop_fk_ccda_id_from_ccd_medications.php
+++ database/migrations/2016_06_17_121212_drop_fk_ccda_id_from_ccd_medications.php
@@ -18,20 +18,24 @@ class DropFkCcdaIdFromCcdMedications extends Migration
             $table->dropForeign('ccd_medications_ccd_medication_log_id_foreign');
             $table->dropForeign('ccd_medications_vendor_id_foreign');
             $table->dropForeign('medication_group_foreign');
-
+            */
+        });
+        Schema::table('ccd_medications', function ($table) {
             $table->integer('ccda_id')->nullable()->change();
             $table->integer('ccd_medication_log_id')->nullable()->change();
             $table->integer('vendor_id')->nullable()->change();
             $table->integer('medication_group_id')->nullable()->change();
-            */
         });
 
         Schema::table('ccd_problems', function ($table) {
+            /*
             $table->dropForeign('ccd_problems_ccda_id_foreign');
             $table->dropForeign('ccd_problems_ccd_problem_log_id_foreign');
             $table->dropForeign('ccd_problems_vendor_id_foreign');
             $table->dropForeign('ccd_problems_cpm_problem_id_foreign');
-
+            */
+        });
+        Schema::table('ccd_problems', function ($table) {
             $table->integer('ccda_id')->nullable()->change();
             $table->integer('ccd_problem_log_id')->nullable()->change();
             $table->integer('vendor_id')->nullable()->change();
@@ -42,7 +46,8 @@ class DropFkCcdaIdFromCcdMedications extends Migration
             $table->dropForeign('ccd_allergies_ccda_id_foreign');
             $table->dropForeign('ccd_allergies_ccd_allergy_log_id_foreign');
             $table->dropForeign('ccd_allergies_vendor_id_foreign');
-
+        });
+        Schema::table('ccd_allergies', function ($table) {
             $table->integer('ccda_id')->nullable()->change();
             $table->integer('ccd_allergy_log_id')->nullable()->change();
             $table->integer('vendor_id')->nullable()->change();
