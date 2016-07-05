0e28a1161e5a8346e383e73c5be2c507523054a2
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-07-05 18:30:20 -0400

    Adds program to CCD vendors [touch:384]

--- database/seeds/CcdVendorsTableSeeder.php
+++ database/seeds/CcdVendorsTableSeeder.php
@@ -20,6 +20,7 @@ class CcdVendorsTableSeeder extends Seeder
             0 => 
             array (
                 'id' => 1,
+                'program_id' => 16,
                 'ccd_import_routine_id' => 1,
                 'vendor_name' => 'Aprima - UPG',
                 'ehr_oid' => 638,
@@ -32,6 +33,7 @@ class CcdVendorsTableSeeder extends Seeder
             1 => 
             array (
                 'id' => 2,
+                'program_id' => 15,
                 'ccd_import_routine_id' => 2,
                 'vendor_name' => 'STI - Tabernacle',
                 'ehr_oid' => 929,
@@ -44,6 +46,7 @@ class CcdVendorsTableSeeder extends Seeder
             2 => 
             array (
                 'id' => 3,
+                'program_id' => 21,
                 'ccd_import_routine_id' => 3,
                 'vendor_name' => 'Athena - Mazhar',
                 'ehr_oid' => 564,
@@ -56,6 +59,7 @@ class CcdVendorsTableSeeder extends Seeder
             3 => 
             array (
                 'id' => 5,
+                'program_id' => 22,
                 'ccd_import_routine_id' => 4,
                 'vendor_name' => 'WRS - Purser',
                 'ehr_oid' => NULL,
@@ -68,6 +72,7 @@ class CcdVendorsTableSeeder extends Seeder
             4 => 
             array (
                 'id' => 6,
+                'program_id' => 20,
                 'ccd_import_routine_id' => 4,
                 'vendor_name' => 'WRS - Nestor',
                 'ehr_oid' => NULL,
@@ -80,6 +85,7 @@ class CcdVendorsTableSeeder extends Seeder
             5 => 
             array (
                 'id' => 7,
+                'program_id' => 14,
                 'ccd_import_routine_id' => 2,
                 'vendor_name' => 'STI - Elmwood',
                 'ehr_oid' => NULL,
@@ -92,6 +98,7 @@ class CcdVendorsTableSeeder extends Seeder
             6 => 
             array (
                 'id' => 9,
+                'program_id' => 23,
                 'ccd_import_routine_id' => 6,
                 'vendor_name' => 'Prognosis - Middletown',
                 'ehr_oid' => NULL,
@@ -104,20 +111,9 @@ class CcdVendorsTableSeeder extends Seeder
             7 => 
             array (
                 'id' => 10,
+                'program_id' => NULL,
                 'ccd_import_routine_id' => 5,
                 'vendor_name' => 'Epic - Carolina Medical Associates',
-                'ehr_oid' => NULL,
-                'doctor_name' => NULL,
-                'doctor_oid' => NULL,
-                'custodian_name' => NULL,
-                'created_at' => '0000-00-00 00:00:00',
-                'updated_at' => '0000-00-00 00:00:00',
-            ),
-            8 => 
-            array (
-                'id' => 11,
-                'ccd_import_routine_id' => 8,
-                'vendor_name' => 'Medent - Montgomery Medical',
                 'ehr_oid' => NULL,
                 'doctor_name' => NULL,
                 'doctor_oid' => NULL,
