ccaf837f4414950bf59b9bed2a1060e5f474ae72
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-07-12 17:48:53 -0400

    Removes awe-worker package

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,9 +35,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->call(function(){
-			(new PhiMail)->sendReceive();
-		})->everyFiveMinutes();
+		
 	}
 
 }
