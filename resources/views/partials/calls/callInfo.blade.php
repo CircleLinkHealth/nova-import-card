d2e04a921a22c9eaf91dd50a853cb8472ad0c768
rohan <rohanm@circlelinkhealth.com>
2016-08-08 17:53:11 -0400

    Adds seeder to update last call for patients

--- resources/views/partials/calls/callInfo.blade.php
+++ resources/views/partials/calls/callInfo.blade.php
@@ -1,4 +1,4 @@
-<div class="form-block col-md-7">
+<div class="form-block col-md-8">
     <div class="row" style="border-top: solid 2px #50b2e2;">
         <div class="new-note-item">
             <div class="form-group">
@@ -26,14 +26,14 @@
         </div>
     </div>
 </div>
-<div class="form-block col-md-5">
+<div class="form-block col-md-4">
     <div class="row" style="border-top: solid 2px #50b2e2;">
         <div class="new-note-item">
             <div class="form-group">
-                <div class="col-sm-12">
+                <div class="col-sm-12" style="padding-left: 0px">
                     <div class="form-group">
                         <div class="form-item form-item-spacing">
-                            <div class="col-sm-12" style="margin-bottom: -17px;">
+                            <div class="col-sm-12" style="margin-bottom: -17px; ">
                                 <label for="activityKey">
                                     <b>{{Carbon\Carbon::now()->format('F')}} CCM Time</b>
                                 </label>
@@ -60,7 +60,7 @@
     <div class="row" style="margin-right: 35px" >
         <div class="new-note-item">
             <div class="form-group">
-                <div class="col-sm-12" style="margin-top: -25px;">
+                <div class="col-sm-12" style="margin-top: -25px; padding-left: 0px">
                     <div class="form-group">
                         <div class="form-item form-item-spacing">
                             <div class="col-sm-12" style="margin-bottom: 9px">
@@ -68,7 +68,7 @@
                                     <b>{{Carbon\Carbon::now()->format('F')}} Call Statistics:</b>
                                 </label>
                             </div>
-                            <div class="col-sm-12">
+                            <div class="col-sm-12" style="width: 135%;">
                                 <ul class="list-group">
                                     <li class="list-group-item">
                                         <b> Successful Calls:</b> <span style="color: green"> {{$no_of_successful_calls}} </span>
@@ -80,7 +80,7 @@
                                         <b> Call Success: {{round($success_percent, 2)}}%</b>
                                     </li>
                                     <li class="list-group-item">
-                                        {{--<b> Last Successful Call: {{$patient->patientInfo->}}</b>--}}
+                                        <b> Last Successful Call Date: {{$patient->patientInfo->last_successful_contact_time == '0000-00-00' ? 'N/A' : \Carbon\Carbon::parse($patient->patientInfo->last_successful_contact_time)->toFormattedDateString()}}</b>
                                     </li>
                                 </ul>
                             </div>
