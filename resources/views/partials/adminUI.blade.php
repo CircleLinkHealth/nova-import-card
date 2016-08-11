bea5af9e0ad1ad3f1990439adaf060a8fc1e5070
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-08-11 18:01:18 -0400

    Adds menu item [deploy:hotfix]

--- resources/views/partials/adminUI.blade.php
+++ resources/views/partials/adminUI.blade.php
@@ -232,6 +232,8 @@
                             </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ URL::route('view.files.ready.to.import', []) }}">CCDs To Import</a></li>
+                                <li><a href="{{ URL::route('EthnicityReportController.getReport', []) }}">Ethnicity/Race
+                                        Report</a></li>
                                 <li><a href="{{ URL::route('MonthlyBillingReportsController.create', []) }}">Monthly
                                         Billing Report</a></li>
                                 <li><a href="{{ URL::route('admin.reports.nurseTime.index', array()) }}">Nurse Time</a>
