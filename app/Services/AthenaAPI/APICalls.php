c0bd2c85b32a1416e421daeff44af8d43f8febe5
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-08-24 13:11:43 -0400

    Adds appointments call [touch:399]

--- app/Services/AthenaAPI/APICalls.php
+++ app/Services/AthenaAPI/APICalls.php
@@ -9,28 +9,62 @@ class APICalls
     protected $key;
     protected $secret;
     protected $version;
-    protected $practiceid;
 
     public function __construct()
     {
         $this->key = env('ATHENA_KEY');
         $this->secret = env('ATHENA_SECRET');
         $this->version = env('ATHENA_VERSION');
-        $this->practiceid = env('ATHENA_PRACTISE_ID');
-        
-        $this->api = new APIConnection($this->version, $this->key, $this->secret, $this->practiceid);
+
+        $this->api = new APIConnection($this->version, $this->key, $this->secret, env('ATHENA_CLH_PRACTICE_ID'));
     }
 
-    public function getCcd($patientId, $departmentId)
+    public function getCcd($patientId, $practiceId, $departmentId = 1)
     {
-        $ccda = $this->api->GET("patients/{$patientId}/ccda", [
+        $response = $this->api->GET("patients/{$patientId}/ccda", [
             'patientid' => $patientId,
-            'practiceid' => $this->practiceid,
+            'practiceid' => $practiceId,
             'departmentid' => $departmentId,
             'purpose' => 'internal',
-            'xmloutput' => false,
+            'xmloutput' => true,
         ]);
 
-        return $ccda[0]['ccda'];
+        return $this->response($response);
+    }
+
+    public function getBookedAppointments($practiceId, $startDate, $endDate, $showInsurance = false, $limit = 1000, $departmentId = 1)
+    {
+        $response = $this->api->GET("{$practiceId}/appointments/booked", [
+            'practiceid' => $practiceId,
+            'startdate' => $startDate,
+            'enddate' => $endDate,
+            'departmentid' => $departmentId,
+            'showinsurance' => $showInsurance,
+            'limit' => $limit,
+        ]);
+
+        return $this->response($response);
+    }
+
+    /**
+     * @param $url
+     * @return bool|mixed
+     */
+    public function getNextPage($url)
+    {
+        return $this->api->GET($url);
+    }
+
+    private function response($response)
+    {
+        //check for errors
+        if (isset($response['error'])) {
+            \Log::alert(__METHOD__ . __LINE__ . 'Response logged below');
+            \Log::error($response);
+
+            abort(400, json_encode($response));
+        }
+
+        return $response;
     }
 }
