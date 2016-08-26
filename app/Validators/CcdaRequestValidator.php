4e91ecffd5252a6cc3404e711f15fb943f7b9eaa
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-08-26 14:34:51 -0400

    Adds validator

---
+++ app/Validators/CcdaRequestValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Validators;
+
+use \Prettus\Validator\Contracts\ValidatorInterface;
+use \Prettus\Validator\LaravelValidator;
+
+class CcdaRequestValidator extends LaravelValidator
+{
+
+    protected $rules = [
+        ValidatorInterface::RULE_CREATE => [
+            'patient_id' => 'required',
+            'vendor' => 'required',
+        ],
+        ValidatorInterface::RULE_UPDATE => [],
+   ];
+}
