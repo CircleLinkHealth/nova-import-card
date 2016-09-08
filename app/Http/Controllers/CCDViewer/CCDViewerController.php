7eac2c97a4a52920bea46b20fd63e24142fff9d9
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-09-08 16:36:00 -0400

    Adds view CCDA button [touch:529]

--- app/Http/Controllers/CCDViewer/CCDViewerController.php
+++ app/Http/Controllers/CCDViewer/CCDViewerController.php
@@ -15,49 +15,53 @@ class CCDViewerController extends Controller
     public function __construct(CCDImporterRepository $repo)
     {
         $this->repo = $repo;
-        ini_set( 'memory_limit', '-1' );
+        ini_set('memory_limit', '-1');
     }
 
     public function create()
     {
-        return view( 'CCDViewer.old-viewer' );
+        return view('CCDViewer.old-viewer');
     }
 
     public function showByUserId($userId)
     {
-        $ccd = Ccda::wherePatientId( $userId )->first()->ccd;
+        $ccda = Ccda::wherePatientId($userId)->first();
 
-        $template = view( 'CCDViewer.bb-ccd-viewer', compact( 'ccd' ) )->render();
+        if ($ccda) {
+            $ccd = $ccda->json;
 
-        return view( 'CCDViewer.viewer', compact( 'template' ) );
+            return view('CCDViewer.old-viewer', compact('ccd'));
+        }
+
+        abort(400, "CCDA was not found.");
     }
 
     public function showUploadedCcd(Request $request)
     {
-        if ( $request->hasFile( 'uploadedCcd' ) ) {
-            $ccd = file_get_contents( $request->file( 'uploadedCcd' ) );
+        if ($request->hasFile('uploadedCcd')) {
+            $ccd = file_get_contents($request->file('uploadedCcd'));
 
-            $template = view( 'CCDViewer.bb-ccd-viewer', compact( 'ccd' ) )->render();
+            $template = view('CCDViewer.bb-ccd-viewer', compact('ccd'))->render();
 
-            return view( 'CCDViewer.viewer', compact( 'template' ) );
+            return view('CCDViewer.viewer', compact('template'));
         }
     }
 
     public function oldViewer(Request $request)
     {
-        if ( $request->hasFile( 'uploadedCcd' ) ) {
-            $xml = file_get_contents( $request->file( 'uploadedCcd' ) );
+        if ($request->hasFile('uploadedCcd')) {
+            $xml = file_get_contents($request->file('uploadedCcd'));
 
-            $ccd = json_decode( $this->repo->toJson( $xml ) );
+            $ccd = json_decode($this->repo->toJson($xml));
 
-            return view( 'CCDViewer.old-viewer', compact( 'ccd' ) );
+            return view('CCDViewer.old-viewer', compact('ccd'));
         }
     }
 
     public function viewSource(Request $request)
     {
-        if ( $xml = urldecode( $request->input( 'xml' ) ) ) {
-            return view( 'CCDViewer.old-viewer', compact( 'xml' ) );
+        if ($xml = urldecode($request->input('xml'))) {
+            return view('CCDViewer.old-viewer', compact('xml'));
         }
     }
 
