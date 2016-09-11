6f0ac7217ee9d9c27c1a17fc08ef238144f0f8d7
rohan <rohanm@circlelinkhealth.com>
2016-09-10 20:33:16 -0400

    change to every minute

--- app/Http/Controllers/CallsCSVController.php
+++ app/Http/Controllers/CallsCSVController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Call;
+use App\Services\Calls\SchedulerService;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -14,19 +17,55 @@ class CallsCSVController extends Controller
         if ($request->hasFile('uploadedCsv')) {
             $csv = parseCsvToArray($request->file('uploadedCsv'));
 
-            $patients = array();
+            $calls = array();
             
             foreach ($csv as $patient){
 
                 $temp = User::where('first_name', $patient['Patient First Name'])
                                   ->where('last_name', $patient['Patient Last Name'])
-                                  ->pluck('id');
+                                  ->whereHas('patientInfo',function ($q) use ($patient){
+                                      $q->where('birth_date',
+                                          Carbon::parse($patient['DOB'])->toDateString()
+                                          );
+                                  })
+                                  ->first();
 
-                if(is_object($temp)) $patients[] = $temp;
+                if(is_object($temp))
+
+                    {
+
+                        $call = (new SchedulerService())->getScheduledCallForPatient($temp->ID);
+
+                        Call::updateOrCreate([
+
+                                'service' => 'phone',
+                                'status' => 'scheduled',
+
+                                'inbound_phone_number' => $patient->phone ? $patient->phone : '',
+                                'outbound_phone_number' => '',
+
+                                'inbound_cpm_id' => $patient->ID,
+                                'outbound_cpm_id' => isset($nurse_id) ? $nurse_id : '',
+
+                                'call_time' => 0,
+
+                                'is_cpm_outbound' => true
+
+                            ], [
+                                'scheduled_date' => Carbon::parse($patient['Next call date'])->toDateString(),
+                                'window_start' => Carbon::parse($patient['Call time From:'])->format('H:i'),
+                                'window_end' => Carbon::parse($patient['Call time From:'])->format('H:i'),
+                            ]);
+
+                        $calls[] = $call;
+
+                        }
+
+                    };
 
             }
 
-            return $patients;
+            return $calls;
         }
-    }
+    
 }
