9ab9739d7ea0277228a3f70258db6a6cc0fbbc8d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-09-12 13:27:37 -0400

    Small change

--- resources/views/uploadCsv.blade.php
+++ resources/views/uploadCsv.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-6">
     <div class="panel panel-default">
-        <div class="panel-heading">Upload CSV</div>
+        <div class="panel-heading">Upload Call List CSV Document</div>
 
         <div class="panel-body">
             <div class="container-fluid">
@@ -8,10 +8,10 @@
                     <div class="col-md-12">
                         <form action="{{ route('post.CallController.import') }}" method="post" enctype="multipart/form-data" target="_blank">
                             <div class="form-group">
-                                <label for="uploadedCsv">Upload Call List CSV:</label>
+                                <label for="uploadedCsv">Upload Call List .csv:</label>
                                 <input type="file" name="uploadedCsv" id="uploadedCsv" required>
 
-                                <input type="submit" class="btn btn-default" value="Upload Call List CSV" name="submit">
+                                <input type="submit" class="btn btn-default" value="Upload" name="submit">
                             </div>
                         </form>
                     </div>
