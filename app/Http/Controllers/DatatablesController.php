d59266b8f863a87c81b391d1e373fcc0fa2eae8b
kgallo <kgallo@circlelinkhealth.com>
2016-09-12 18:07:32 -0400

    [touch: 551] - adds scheduler column to call management table

--- app/Http/Controllers/DatatablesController.php
+++ app/Http/Controllers/DatatablesController.php
@@ -59,6 +59,7 @@ class DatatablesController extends Controller
                     'notes.body AS note_body',
                     'notes.performed_at AS note_datetime',
                     'calls.note_id',
+                    'calls.scheduler',
                     'patient_info.cur_month_activity_time',
                     'patient_info.last_successful_contact_time',
                     \DB::raw('DATE_FORMAT(patient_info.last_contact_time, "%Y-%m-%d") as last_contact_time'),
