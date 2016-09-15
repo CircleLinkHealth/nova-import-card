eb439c0e3dc6a5bccaab68591ff3f04212a882c0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-09-15 12:34:04 -0400

    [touch:532]

--- resources/views/partials/userheader.blade.php
+++ resources/views/partials/userheader.blade.php
@@ -1,6 +1,7 @@
 <div class="main-form-block main-form-horizontal main-form-primary-horizontal col-md-12">
 
     @role(['administrator'])
+    @if(App\Models\CCD\Ccda::wherePatientId($patient->ID)->exists())
         <div class="pull-right">
             <a href="{{ route('get.CCDViewerController.showByUserId', [ 'userId' => $patient->ID]) }}"
                class="btn btn-primary btn-xs"
@@ -9,6 +10,11 @@
                 View CCDA
             </a>
         </div>
+    @else
+        <div class="pull-right">
+            <b>CCDA not available for this patient.</b>
+        </div>
+    @endif
     @endrole
 
     <div class="row">
@@ -24,21 +30,25 @@
                             Provider: <em>No Provider Selected </em>
                         @endif
                         Location:</strong>
-                                <?= (empty($patient->getPreferredLocationName())) ?  'Not Set' : $patient->getPreferredLocationName();  ?>
+                    <?= (empty($patient->getPreferredLocationName())) ? 'Not Set' : $patient->getPreferredLocationName();  ?>
                 </span>
                 <?php
-                    // calculate display, fix bug where gmdate('i:s') doesnt work for > 24hrs
+                // calculate display, fix bug where gmdate('i:s') doesnt work for > 24hrs
                 $seconds = $patient->patientInfo()->first()->cur_month_activity_time;
                 $H = floor($seconds / 3600);
                 $i = ($seconds / 60) % 60;
                 $s = $seconds % 60;
-                $monthlyTime = sprintf("%02d:%02d:%02d",$H, $i, $s);
+                $monthlyTime = sprintf("%02d:%02d:%02d", $H, $i, $s);
                 ?>
-               <a href="{{URL::route('patient.activity.providerUIIndex', array('patient' => $patient->ID))}}"><span class="pull-right">{{
+                <a href="{{URL::route('patient.activity.providerUIIndex', array('patient' => $patient->ID))}}"><span
+                            class="pull-right">{{
                 date("F", mktime(0, 0, 0, Carbon\Carbon::now()->month, 10))
                  }} Time: {{ $monthlyTime }}</span></a></p>
             <a href="{{ URL::route('patient.summary', array('patient' => $patient->ID)) }}">
-                <span class="person-name text-big text-dark text-serif" title="{{$patient->ID}}">{{$patient->fullName}}</span></a><a href="{{ URL::route('patient.demographics.show', array('patient' => $patient->ID)) }}"><span class="glyphicon glyphicon-pencil" style="margin-right:3px;"></span></a>
+                <span class="person-name text-big text-dark text-serif"
+                      title="{{$patient->ID}}">{{$patient->fullName}}</span></a><a
+                    href="{{ URL::route('patient.demographics.show', array('patient' => $patient->ID)) }}"><span
+                        class="glyphicon glyphicon-pencil" style="margin-right:3px;"></span></a>
             <ul class="person-info-list inline-block text-medium">
                 <li class="inline-block">DOB: {{$patient->birthDate}}</li>
                 <li class="inline-block">{{$patient->gender}}</li>
@@ -46,27 +56,37 @@
                 <li class="inline-block">{{$patient->phone}}</li>
                 @if(Route::is('patient.note.create'))
                     <li class="inline-block">
-                        <select id="status" name="status" class="selectpickerX dropdownValid form-control" data-size="2" style="width: 100px">
-                            <option class="enrolled" value="enrolled" {{$patient->ccm_status == 'enrolled' ? 'selected' : ''}}> Enrolled</option>
-                            <option class="withdrawn" value="withdrawn" {{$patient->ccm_status == 'withdrawn' ? 'selected' : ''}}> Withdrawn</option>
-                            <option class="paused" value="paused" {{$patient->ccm_status == 'paused' ? 'selected' : ''}}> Paused</option>
+                        <select id="status" name="status" class="selectpickerX dropdownValid form-control" data-size="2"
+                                style="width: 100px">
+                            <option class="enrolled"
+                                    value="enrolled" {{$patient->ccm_status == 'enrolled' ? 'selected' : ''}}> Enrolled
+                            </option>
+                            <option class="withdrawn"
+                                    value="withdrawn" {{$patient->ccm_status == 'withdrawn' ? 'selected' : ''}}>
+                                Withdrawn
+                            </option>
+                            <option class="paused"
+                                    value="paused" {{$patient->ccm_status == 'paused' ? 'selected' : ''}}> Paused
+                            </option>
                         </select>
                     </li>
                 @else
-                    <li class=" inline-block {{$patient->ccm_status}}"><?= (empty($patient->ccm_status)) ?  'N/A' : ucwords($patient->ccm_status);  ?></li>
+                    <li class=" inline-block {{$patient->ccm_status}}"><?= (empty($patient->ccm_status)) ? 'N/A' : ucwords($patient->ccm_status);  ?></li>
                 @endif
             </ul>
         </div>
     </div>
     @if($patient->agentName)
-    <div class="row">
-        <div class="col-sm-12">
-            <ul class="person-info-listX inline-block text-medium">
-                <li class="inline-block">Alternate Contact: <span title="{{$patient->agentEmail}}">({{$patient->agentRelationship}}) {{$patient->agentName}}&nbsp;&nbsp;</span></li>
-                <li class="inline-block">{{$patient->agentPhone}}</li>
-            {{--</ul><div style="clear:both"></div><ul class="person-conditions-list inline-block text-medium"></ul>--}}
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="person-info-listX inline-block text-medium">
+                    <li class="inline-block">Alternate Contact: <span
+                                title="{{$patient->agentEmail}}">({{$patient->agentRelationship}}
+                            ) {{$patient->agentName}}&nbsp;&nbsp;</span></li>
+                    <li class="inline-block">{{$patient->agentPhone}}</li>
+                {{--</ul><div style="clear:both"></div><ul class="person-conditions-list inline-block text-medium"></ul>--}}
+            </div>
         </div>
-    </div>
     @endif
 
     @if(!empty($problems))
