3675d17b841fce13b03569a712a24f62febf10ab
rohan <rohanm@circlelinkhealth.com>
2016-09-22 15:22:46 -0400

    [touch: 450]  clears create

--- resources/views/admin/families/create.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="row" style="margin:20px 0px 40px 0px;">
-    <div class="col-md-8 col-md-offset-2">
-        <div class="row">
-            <div class="col-xs-4 text-right">{!! Form::label('filterUser', 'Find User:') !!}</div>
-            <div class="col-xs-8">{!! Form::select('filterUser', array('all' => 'All Users') + $users, $filterUser, ['class' => 'form-control select-picker', 'style' => 'width:80%;']) !!}</div>
-        </div>
-    </div>
-</div>
