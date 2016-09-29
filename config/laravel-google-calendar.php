99f5694f15b6c5c6f16d047c0cfbda26d57ae8b8
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-09-29 03:15:35 -0400

    Small Fix

--- config/laravel-google-calendar.php
+++ config/laravel-google-calendar.php
@@ -5,7 +5,7 @@ return [
     /**
      * Path to a json file containing the credentials of a Google Service account.
      */
-    'client_secret_json' => storage_path('app/google-calendar/credentials.json'),
+    'client_secret_json' => storage_path('google-calendar/credentials.json'),
 
     /**
      *  The id of the Google Calendar that will be used by default.
