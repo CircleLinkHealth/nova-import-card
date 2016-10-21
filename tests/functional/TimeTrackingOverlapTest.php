db497829016696157c238dcc53f8a6b02a7450a4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-10-21 17:57:57 -0400

    More testation

--- tests/functional/TimeTrackingOverlapTest.php
+++ tests/functional/TimeTrackingOverlapTest.php
@@ -152,4 +152,64 @@ class TimeTrackingOverlapTest extends TestCase
 
         $this->createActivitiesAndRunTest($create);
     }
+
+    /**
+     *  Really crazy case
+     */
+    public function testCrayCase2()
+    {
+        //Add some time so that it won't mess with the values of the previous test
+        $startTime = Carbon::now()->addMinutes(10);
+        $endTime = $startTime->copy()->addSeconds(30);
+
+        $create = new Collection([
+            [
+                $startTime,
+                $endTime,
+                $startTime->diffInSeconds($endTime),
+            ],
+            [
+                $startTime->copy()->addMinutes(1),
+                $endTime,
+                $startTime->copy()->addMinutes(1)->diffInSeconds($endTime),
+            ],
+            [
+                $startTime->copy()->addSeconds(25),
+                $endTime->copy()->subSeconds(2),
+                $startTime->copy()->addSeconds(25)->diffInSeconds($endTime->copy()->subSeconds(2)),
+            ],
+            [
+                $startTime->copy()->subSeconds(20),
+                $endTime->copy()->addSeconds(60),
+                $startTime->copy()->subSeconds(20)->diffInSeconds($endTime->copy()->addSeconds(60)),
+            ],
+            [
+                $startTime->copy()->subSeconds(10),
+                $endTime->copy()->addSeconds(10),
+                $startTime->copy()->subSeconds(10)->diffInSeconds($endTime->copy()->addSeconds(10)),
+            ],
+            [
+                $startTime->copy()->subSeconds(50),
+                $endTime,
+                $startTime->copy()->subSeconds(50)->diffInSeconds($endTime),
+            ],
+            [
+                $startTime->copy()->addSeconds(1),
+                $endTime->copy()->subSeconds(3),
+                $startTime->copy()->addSeconds(1)->diffInSeconds($endTime->copy()->subSeconds(3)),
+            ],
+            [
+                $startTime->copy()->subSeconds(1),
+                $endTime->copy()->addSeconds(75),
+                $startTime->copy()->subSeconds(1)->diffInSeconds($endTime->copy()->addSeconds(75)),
+            ],
+            [
+                $startTime->copy()->addSeconds(5),
+                $endTime->copy()->subSeconds(5),
+                $startTime->copy()->addSeconds(5)->diffInSeconds($endTime->copy()->subSeconds(5)),
+            ],
+        ]);
+
+        $this->createActivitiesAndRunTest($create);
+    }
 }
