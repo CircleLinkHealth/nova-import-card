131a2b42f4387a915740ee2b220e56fadbed409c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-10-24 16:35:21 -0400

    Changes nurse to google calendar to run every half hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -88,7 +88,7 @@ class Kernel extends ConsoleKernel
             ->dailyAt('08:00');
 
         $schedule->command('nurseSchedule:export')
-            ->everyFiveMinutes();
+            ->everyThirtyMinutes();
 
         $schedule->command('athena:getTodaysAppointments')
             ->dailyAt('01:00');
