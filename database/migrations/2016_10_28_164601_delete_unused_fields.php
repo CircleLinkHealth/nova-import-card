a48fdfb6d715184ed0869380a6d979f901143ee2
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-10-31 19:43:07 -0400

    Fix

--- database/migrations/2016_10_28_164601_delete_unused_fields.php
+++ database/migrations/2016_10_28_164601_delete_unused_fields.php
@@ -14,7 +14,7 @@ class DeleteUnusedFields extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('password');
+            $table->dropColumn('user_pass');
             $table->dropColumn('user_url');
             $table->dropColumn('user_activation_key');
         });
