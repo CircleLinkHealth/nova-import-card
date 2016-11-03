f1b8b9e943f4f629ac0744b68780970c85112cd4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-11-03 13:31:11 -0400

    Actually fixes random redirects

--- database/migrations/2016_10_28_140457_add_practice_id_to_locations.php
+++ database/migrations/2016_10_28_140457_add_practice_id_to_locations.php
@@ -21,7 +21,7 @@ class AddPracticeIdToLocations extends Migration
             }
 
             $table->unsignedInteger('practice_id')
-                ->default(8)
+                ->default(4)
                 ->after('id');
         });
 
