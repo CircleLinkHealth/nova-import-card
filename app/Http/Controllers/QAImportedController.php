352d21b6b3d1a9a623d1dc416b22854d54684a74
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-11-02 22:35:56 -0400

    Importer Fix

--- app/Http/Controllers/QAImportedController.php
+++ app/Http/Controllers/QAImportedController.php
@@ -5,7 +5,6 @@ use App\CLH\CCD\ImportedItems\AllergyImport;
 use App\CLH\CCD\ImportedItems\DemographicsImport;
 use App\CLH\CCD\ImportedItems\MedicationImport;
 use App\CLH\CCD\ImportedItems\ProblemImport;
-use App\Location;
 use App\Models\CCD\CcdVendor;
 use App\Practice;
 use App\User;
@@ -23,24 +22,15 @@ class QAImportedController extends Controller
         $problems = ProblemImport::whereCcdaId( $ccdaId )->get();
 
         $programId = $demographics->program_id;
-        $programObj = Practice::whereBlogId($programId)->first();
+        $programObj = Practice::find($programId);
 
         //get program's location
-        $locations = Location::whereNotNull( 'parent_id' )->whereId( $programObj->location_id )->first();
-
-        if (empty($locations)){
-            //means it's a parent loc
-            $locations = Location::whereParentId( $programObj->location_id )->get();
-        } else {
-            //get all locations from the same parent
-            $locations = Location::whereParentId( $locations->parent_id )->get();
-        }
+        $locations = $programObj->locations;
 
         $vendor = CcdVendor::find($demographics->vendor_id);
 
-        $providers = User::whereHas( 'roles', function ($q) {
-            $q->where( 'name', '=', 'provider' );
-        } )->whereProgramId( $programId )
+        $providers = User::ofType('provider')
+            ->whereProgramId($programId)
             ->get();
 
         $providers = $providers->map( function ($provider) {
