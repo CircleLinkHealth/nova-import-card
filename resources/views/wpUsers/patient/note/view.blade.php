14a663635e0b148a3e55e0da3caee64dcabcc8d7
rohan <rohanm@circlelinkhealth.com>
2016-11-07 14:29:43 -0500

    [touch: 679] Adds exception for Rocky Mtn dropdowns

--- resources/views/wpUsers/patient/note/view.blade.php
+++ resources/views/wpUsers/patient/note/view.blade.php
@@ -142,6 +142,14 @@
                                                             @foreach ($careteam_info as $id => $name)
                                                                 <option value="{{$id}}"> {{$name}} </option>
                                                             @endforeach
+
+                                                            <!-- rework later, quick fix ticket: 679 !-->
+                                                            @if($patient->program_id == 29)
+                                                                    <option value="2584">Tina Booze</option>
+                                                            @elseif
+                                                                    <option value="948">Patient Support</option>
+                                                            @endif
+
                                                         </select>
                                                     </div>
                                                 </div>
