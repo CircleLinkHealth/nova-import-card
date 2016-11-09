c908b5255208a351fea42b137d5f141f43dcd552
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-11-09 16:06:21 -0500

    Closes PatientSession when leaving patient page

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,9 @@
 <?php namespace App\Http\Middleware;
 
+use App\Models\PatientSession;
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\Request;
 
 class Authenticate {
 
@@ -55,7 +57,42 @@ class Authenticate {
             }
         }
 
+        $this->clearPatientSession($request);
+
 		return $next($request);
 	}
+
+    /**
+     * Determine whether to clear the patient session or not and then clear it.
+     *
+     * @param Request $request
+     */
+    protected function clearPatientSession(Request $request)
+    {
+        $patientId = $request->route('patientId') ?? $request->input('patientId');
+
+        if ($request->method() != 'GET') {
+            return;
+        }
+
+        $clearPatientSessions = preg_match('/(?<![0-9])[0-9]{2,4}(?![0-9])/', $request->getRequestUri()) == 0;
+
+        if (!empty($patientId)) {
+            $clearPatientSessions = !str_contains($request->getRequestUri(),
+                $patientId)//    && str_contains(\URL::previous(), $patientId)
+            ;
+        }
+
+        if ($clearPatientSessions) {
+            if (auth()->check()) {
+                $user = auth()->user()->id;
+            } else {
+                $user = $request->input('providerId');
+            }
+
+            $session = PatientSession::where('user_id', '=', $user)
+                ->delete();
+        }
+    }
 
 }
