0f776b0f3f70e0680b127faee5a1ea11383bde51
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-11-14 12:23:03 -0500

    Formatting changes

--- app/Services/PhiMail/PhiMail.php
+++ app/Services/PhiMail/PhiMail.php
@@ -100,8 +100,12 @@ class PhiMail
                 $srList = $c->send();
                 foreach ($srList as $sr) {
                     echo("Send to " . $sr->recipient);
-                    echo($sr->succeeded ? " succeeded id=" : "failed err=");
-                    echo($sr->succeeded ? $sr->messageId : $sr->errorText);
+                    echo($sr->succeeded
+                        ? " succeeded id="
+                        : "failed err=");
+                    echo($sr->succeeded
+                        ? $sr->messageId
+                        : $sr->errorText);
                     echo("\n");
                 }
             }
@@ -127,99 +131,103 @@ class PhiMail
 //                            ->send("Checked EMR Direct Mailbox. There where no messages. \n");
                         break;
 
-                    } else if ($cr->isMail()) {
-                        // If you are checking messages for an address group,
-                        // $cr->recipient will contain the address in that
-                        // group to which this message should be delivered.
-                        Log::info("A new message is available for " . $cr->recipient . "\n");
-                        Log::info("from " . $cr->sender . "; id "
-                            . $cr->messageId . "; #att=" . $cr->numAttachments
-                            . "\n");
+                    } else {
+                        if ($cr->isMail()) {
+                            // If you are checking messages for an address group,
+                            // $cr->recipient will contain the address in that
+                            // group to which this message should be delivered.
+                            Log::info("A new message is available for " . $cr->recipient . "\n");
+                            Log::info("from " . $cr->sender . "; id "
+                                . $cr->messageId . "; #att=" . $cr->numAttachments
+                                . "\n");
 
-                        for ($i = 0; $i <= $cr->numAttachments; $i++) {
-                            // Get content for part i of the current message.
-                            $showRes = $c->show($i);
+                            for ($i = 0; $i <= $cr->numAttachments; $i++) {
+                                // Get content for part i of the current message.
+                                $showRes = $c->show($i);
 //                            echo ("MimeType = " . $showRes->mimeType
 //                                . "; length=" . $showRes->length . "\n");
 
-                            // List all the headers. Headers are set by the
-                            // sender and may include Subject, Date, additional
-                            // addresses to which the message was sent, etc.
-                            // Do NOT use the To: header to determine the address
-                            // to which this message should be delivered
-                            // internally; use $cr->recipient instead.
+                                // List all the headers. Headers are set by the
+                                // sender and may include Subject, Date, additional
+                                // addresses to which the message was sent, etc.
+                                // Do NOT use the To: header to determine the address
+                                // to which this message should be delivered
+                                // internally; use $cr->recipient instead.
 //                            foreach ($showRes->headers as $header) {
 //                                echo ("Header: " . $header . "\n");
 //                            }
 
-                            // Process the content; for this example text data
-                            // is echoed to the console and non-text data is
-                            // written to files.
+                                // Process the content; for this example text data
+                                // is echoed to the console and non-text data is
+                                // written to files.
 
-                            if (str_contains($showRes->mimeType, 'plain')) {
-                                // ... do something with text parts ...
-                                Log::info('The plain text part of the mail');
-                                Log::info($showRes->data);
-                                self::writeDataFile(storage_path(str_random(20) . '.txt'), $showRes->data);
-                            } elseif (str_contains($showRes->mimeType, 'xml')) {
-                                //save ccd to file
-                                self::writeDataFile(storage_path(str_random(20) . '.xml'), $showRes->data);
-                                $import = $this->importCcd($cr->sender, $showRes);
+                                if (str_contains($showRes->mimeType, 'plain')) {
+                                    // ... do something with text parts ...
+                                    Log::info('The plain text part of the mail');
+                                    Log::info($showRes->data);
+                                    self::writeDataFile(storage_path(str_random(20) . '.txt'), $showRes->data);
+                                } elseif (str_contains($showRes->mimeType, 'xml')) {
+                                    //save ccd to file
+                                    self::writeDataFile(storage_path(str_random(20) . '.xml'), $showRes->data);
+                                    $import = $this->importCcd($cr->sender, $showRes);
 
-                                if (!$import) continue;
+                                    if (!$import) {
+                                        continue;
+                                    }
 
-                                $ccdas[] = $import;
+                                    $ccdas[] = $import;
+                                }
+
+                                // Display the list of attachments and associated info. This info is only
+                                // included with message part 0.
+                                for ($k = 0; $i == 0 && $k < $cr->numAttachments; $k++) {
+                                    Log::info("Attachment " . ($k + 1)
+                                        . ": " . $showRes->attachmentInfo[$k]->mimeType
+                                        . " fn:" . $showRes->attachmentInfo[$k]->filename
+                                        . " Desc:" . $showRes->attachmentInfo[$k]->description
+                                        . "\n");
+                                }
+                            }
+                            // This signals the server that the message can be safely removed from the queue
+                            // and should only be sent after all required parts of the message have been
+                            // retrieved and processed.
+                            $c->acknowledgeMessage();
+
+                            Log::info('Number of Attachments: ' . $cr->numAttachments);
+
+                            if ($cr->numAttachments > 0) {
+                                $this->notifyAdmins($cr->numAttachments);
+
+                                $message = "Checked EMR Direct Mailbox. There where {$cr->numAttachments} messages. \n";
+
+                                echo $message;
+
+                                Slack::to('#background-tasks')->send($message);
                             }
 
-                            // Display the list of attachments and associated info. This info is only
-                            // included with message part 0.
-                            for ($k = 0; $i == 0 && $k < $cr->numAttachments; $k++) {
-                                Log::info("Attachment " . ($k + 1)
-                                    . ": " . $showRes->attachmentInfo[$k]->mimeType
-                                    . " fn:" . $showRes->attachmentInfo[$k]->filename
-                                    . " Desc:" . $showRes->attachmentInfo[$k]->description
-                                    . "\n");
-                            }
-                        }
-                        // This signals the server that the message can be safely removed from the queue
-                        // and should only be sent after all required parts of the message have been
-                        // retrieved and processed.
-                        $c->acknowledgeMessage();
+                            Log::info('***************');
 
-                        Log::info('Number of Attachments: ' . $cr->numAttachments);
+                        } else {
 
-                        if ($cr->numAttachments > 0) {
-                            $this->notifyAdmins($cr->numAttachments);
-
-                            $message = "Checked EMR Direct Mailbox. There where {$cr->numAttachments} messages. \n";
-
-                            echo $message;
-
-                            Slack::to('#background-tasks')->send($message);
-                        }
-
-                        Log::info('***************');
-
-                    } else {
-
-                        // Process a status update for a previously sent message.
+                            // Process a status update for a previously sent message.
 //                        echo ("Status message for id = " . $cr->messageId . "\n");
 //                        echo ("  StatusCode = " . $cr->statusCode . "\n");
 //                        if ($cr->info != null) echo ("  Info = " . $cr->info . "\n");
-                        if ($cr->statusCode == "failed") {
-                            // ...do something about a failed message...
-                            // $cr->messageId will match the messageId returned
-                            // when you originally sent the corresponding message
-                            // See the API documentation for information about
-                            // status notification types and their meanings.
-                        }
+                            if ($cr->statusCode == "failed") {
+                                // ...do something about a failed message...
+                                // $cr->messageId will match the messageId returned
+                                // when you originally sent the corresponding message
+                                // See the API documentation for information about
+                                // status notification types and their meanings.
+                            }
 
-                        // This signals the server that the status update can be
-                        // safely removed from the queue,
-                        // i.e. it has been successfully received and processed.
-                        // Note: this is NOT the same method used to acknowledge
-                        // regular messages.
-                        $c->acknowledgeStatus();
+                            // This signals the server that the status update can be
+                            // safely removed from the queue,
+                            // i.e. it has been successfully received and processed.
+                            // Note: this is NOT the same method used to acknowledge
+                            // regular messages.
+                            $c->acknowledgeStatus();
+                        }
                     }
                 }
             }
@@ -253,11 +261,15 @@ class PhiMail
         return file_put_contents($filename, $data);
     }
 
-    public function importCcd($sender, $attachment)
-    {
+    public function importCcd(
+        $sender,
+        $attachment
+    ) {
         $atPosition = strpos($sender, '@');
 
-        if (!$atPosition) return false;
+        if (!$atPosition) {
+            return false;
+        }
 
         //get the domain of the sender's emr address to see where it came from
         $senderDomain = substr($sender, $atPosition);
@@ -267,15 +279,17 @@ class PhiMail
         $vendorMap = Ccda::EMAIL_DOMAIN_TO_VENDOR_MAP;
 
         //Map the email domain of the sender to one of our CCD Vendors, or assume carolina meds
-        $vendorId = key_exists($senderDomain, $vendorMap) ? $vendorMap[$senderDomain] : 10;
+        $vendorId = key_exists($senderDomain, $vendorMap)
+            ? $vendorMap[$senderDomain]
+            : 10;
 
         $vendor = CcdVendor::find($vendorId);
 
         $ccda = Ccda::create([
-            'user_id' => null,
+            'user_id'   => null,
             'vendor_id' => $vendorId,
-            'xml' => $attachment->data,
-            'source' => Ccda::EMR_DIRECT,
+            'xml'       => $attachment->data,
+            'source'    => Ccda::EMR_DIRECT,
         ]);
 
         $ccdaRepo = new CCDImporterRepository;
@@ -291,7 +305,7 @@ class PhiMail
         $importer->generateCarePlanFromCCD();
 
         return [
-            'id' => $ccda->id,
+            'id'       => $ccda->id,
             'fileName' => $attachment->filename,
         ];
     }
