5168ce52d505ee6e9d0f75b62c3bfe9088fc4bdb
rohan <rohanm@circlelinkhealth.com>
2016-11-21 17:49:58 -0500

    more ui updates [touch: 711]

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,7 +1,7 @@
 
     <label for="patients"></label>
     <div id="bloodhound">
-            <input style="margin-top: -9px; margin-bottom: -41px; width: 32%" class="typeahead form-item-spacing form-control" size="50" type="text" name="users"
-                   autofocus="autofocus" @if(!empty($patient->id)) placeholder="{!!$patient->fullName!!}"> @else placeholder="Please enter a Patient Name, an MRN or a Date of Birth (mm-dd-yyyy)">@endif
+            <input style="margin-top: -9px; margin-bottom: -41px; width: auto" class="typeahead form-item-spacing form-control" size="50" type="text" name="users"
+                   autofocus="autofocus" @if(!empty($patient->id)) placeholder="{!!$patient->fullName!!}"> @else placeholder="Enter a Patient Name, MRN or DOB (mm-dd-yyyy)">@endif
 
     </div>
