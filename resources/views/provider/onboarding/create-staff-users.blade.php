01227be3f6c72677c3e914667b8189cbd6d2984e
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-11-22 23:43:01 -0500

    Pre-selects no role selected [touch:632] [deploy:staging]

--- resources/views/provider/onboarding/create-staff-users.blade.php
+++ resources/views/provider/onboarding/create-staff-users.blade.php
@@ -71,7 +71,8 @@
 
                             <div class="input-field col s6">
                                 <select id="roles" v-model="newUser.role">
-                                    <option v-bind:value="{id:1, name:'No Role Selected'}" disabled>Choose a role
+                                    <option v-bind:value="{id:0, name:'No Role Selected'}" disabled selected>Choose a
+                                        role
                                     </option>
                                     <option v-bind:value="{id:1, name:'Medical Assistant'}">Medical Assistant</option>
                                     <option v-bind:value="{id:1, name:'Specialist Doctor'}">Specialist Doctor</option>
