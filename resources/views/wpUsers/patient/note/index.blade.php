f8a544bbeb46a5f5444b111e3e3fdf065e69a8c0
rohan <rohanm@circlelinkhealth.com>
2016-11-23 13:57:38 -0500

    [touch: 713] download updates

--- resources/views/wpUsers/patient/note/index.blade.php
+++ resources/views/wpUsers/patient/note/index.blade.php
@@ -198,14 +198,10 @@
                             </div>
                         @if(auth()->user()->hasRole(['administrator', 'med_assistant', 'provider']))
 
-                            <?php
-                                $title = ;
-                                ?>
-
                                 <input type="button" value="Export as PDF" class="btn btn-primary"
                                        style='margin:15px;'
                                        onclick="webix.toPDF($$(obs_alerts_dtable), {
-                                               header: 'Circlelink Health notes for <?= $patient->fullName . ", Dr. " . $patient->billingProviderName . " as of " . Carbon\Carbon::now()->toDateString()?>',
+                                               header: 'Circlelink Health notes for {!!  $patient->fullName . ", Dr. " . $patient->billingProviderName . " as of " . Carbon\Carbon::now()->toDateString() !!}',
                                                orientation:'landscape',
                                                autowidth:true,
                                                filename: 'PatientNotesReport . {{Carbon\Carbon::now()->toDateString()}}',
@@ -219,7 +215,7 @@
                                 <input type="button" value="Export as Excel" class="btn btn-primary"
                                        style='margin:15px;'
                                        onclick="webix.toExcel($$(obs_alerts_dtable), {
-                                               header:'Circlelink Health notes for <?= $patient->fullName . ", Dr. " . $patient->billingProviderName . " as of " . Carbon\Carbon::now()->toDateString()?>',
+                                               header:'Circlelink Health notes for {!! $patient->fullName . ", Dr. " . $patient->billingProviderName . " as of " . Carbon\Carbon::now()->toDateString() !!}',
                                                orientation:'landscape',
                                                autowidth:true,
                                                filename: 'PatientNotesReport . {{Carbon\Carbon::now()->toDateString()}}',
