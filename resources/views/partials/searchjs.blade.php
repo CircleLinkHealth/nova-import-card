6ba2820b181b747db8800b82298ce3d5b9730147
rohan <rohanm@circlelinkhealth.com>
2016-11-29 12:43:31 -0500

    [touch: 717] fixes

--- resources/views/partials/searchjs.blade.php
+++ resources/views/partials/searchjs.blade.php
@@ -1,25 +1,3 @@
-
-{{--<style>--}}
-
-    {{--.searchbox-label{--}}
-        {{--position: relative;--}}
-        {{--z-index: 99;--}}
-        {{--top: 20px;--}}
-        {{--color: #50b2e2;--}}
-        {{--padding-left: 10px;--}}
-        {{--font-size: 16px;--}}
-    {{--}--}}
-
-{{--</style>--}}
-
-<label class="searchbox-label" for="patients"></label>
-<div id="bloodhound">
-    <input style="margin-top: -9px; margin-bottom: -41px; width: auto;"
-           class="form-control typeahead form-item-spacing form-control" size="50" type="text" name="users"
-           autofocus="autofocus" @if(!empty($patient->id)) placeholder="{!!$patient->fullName!!}"> @else
-        placeholder="Enter a Patient Name, MRN or DOB (mm-dd-yyyy)">@endif
-
-</div>
 <script>
 
     jQuery(document).ready(function ($) {
@@ -27,7 +5,7 @@
             datumTokenizer: Bloodhound.tokenizers.obj.whitespace('search'),
             queryTokenizer: Bloodhound.tokenizers.whitespace,
             remote: {
-                url: {!! route('patients.query') !!},
+                url: '{!! route('patients.query') . '?users=%QUERY' !!}',
                 wildcard: '%QUERY'
             }
         });
