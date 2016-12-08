0d4b6880ae7568af4781e53984a3f8795eb474f2
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-12-07 20:09:47 -0500

    Adds contacts

---
+++ database/migrations/2016_12_07_165824_create_contactables_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->string('name');
+
+            $table->unsignedInteger('contactable_id');
+            $table->string('contactable_type');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactables');
+    }
+}
