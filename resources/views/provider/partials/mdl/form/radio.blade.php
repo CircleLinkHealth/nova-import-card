a6ed5e70a3330774ad3859196562408e674c817d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-12-14 22:04:51 -0500

    Adds to radio button

--- resources/views/provider/partials/mdl/form/radio.blade.php
+++ resources/views/provider/partials/mdl/form/radio.blade.php
@@ -1,9 +1,9 @@
-<input name="{{$name}}" type="radio"
-       id="{{$id ?? $name}}"
+<input name="{{$name ?? ''}}" type="radio"
+       id="{{$id ?? ''}}"
        value="{{$value}}"
 @if(isset($attributes))
     @foreach($attributes as $key => $val)
         {{ $key }}="{{ $val }}"
     @endforeach
 @endif/>
-<label for="{{$id ?? $name}}">{{$label}}</label>
+<label for="{{$id ?? ''}}">{{$label}}</label>
