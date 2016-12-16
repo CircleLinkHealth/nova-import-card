90ee8bae49b5f0fc3a0d32bcb4568a457bc023f4
rohan <rohanm@circlelinkhealth.com>
2016-12-16 15:43:32 -0500

    [touch: 639] adds clh_pppm to practices thru migration+seeder

---
+++ database/migrations/2016_12_16_152910_add_clh_pppm_to_practices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddClhPppmToPractices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('practices', function (Blueprint $table) {
+
+            $table->integer('clh_pppm')->after('display_name');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('practices', function (Blueprint $table) {
+
+            $table->dropColumn('clh_ppm');
+
+        });
+    }
+}
