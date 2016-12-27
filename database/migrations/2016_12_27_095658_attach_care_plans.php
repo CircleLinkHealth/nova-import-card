bc9f9b9bf27aeb413d06ee3f1d854d91f2301551
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2016-12-27 18:25:16 +0200

    Attach CarePlans to PatientInfo [touch:752]

---
+++ database/migrations/2016_12_27_095658_attach_care_plans.php
@@ -0,0 +1,64 @@
+<?php
+
+use App\CarePlan;
+use App\PatientInfo;
+use Illuminate\Database\Migrations\Migration;
+
+class AttachCarePlans extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $patientInfoToCarePlanMap = [
+            'provider_approver_id' => 'careplan_provider_approver',
+            'qa_approver_id'       => 'careplan_qa_approver',
+            'status'               => 'careplan_status',
+            'qa_date'              => 'careplan_qa_date',
+            'provider_date'        => 'careplan_provider_date',
+            'last_printed'         => 'careplan_last_printed',
+        ];
+
+        PatientInfo::withTrashed()
+            ->where('careplan_provider_approver', '=', 0)
+            ->update([
+                'careplan_provider_approver' => null,
+            ]);
+
+        try {
+            foreach (CarePlan::all() as $cp) {
+                $patient = PatientInfo::where('user_id', '=', $cp->patient_id)
+                    ->first();
+
+                if (!$patient) {
+                    continue;
+                }
+
+                foreach ($patientInfoToCarePlanMap as $carePlanKey => $patientInfoKey) {
+                    if ($patient->$patientInfoKey) {
+                        $cp->$carePlanKey = $patient->$patientInfoKey;
+                    }
+                    $patient->care_plan_id = $cp->id;
+                }
+
+                $cp->save();
+                $patient->save();
+            }
+        } catch (\Exception $e) {
+            Log::alert('Unable to migrate patient info with ID:' . $patient->id);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
