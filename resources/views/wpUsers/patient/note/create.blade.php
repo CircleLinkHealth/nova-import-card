a85c6cf379ba96405dd26318c42732c02a7f541d
rohan <rohanm@circlelinkhealth.com>
2017-01-03 16:48:18 -0500

    [touch: 770] Adds confirmation modal for ccm complexity toggle on Notes And Activities

--- resources/views/wpUsers/patient/note/create.blade.php
+++ resources/views/wpUsers/patient/note/create.blade.php
@@ -22,31 +22,7 @@
         }
     </style>
 
-    <div id="confirmButtonModal" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Confirm Complex CCM Patient</h4>
-                </div>
-                <div class="modal-body">
-                    <p>Please confirm patient will benefit from extra CCM care time this month.</p>
-                    <p>Friendly Reminder: A Medication Reconciliation is required for Complex CCM patients.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" name="complex_confirm" id="complex_confirm" class="btn btn-primary"
-                            data-dismiss="modal">Confirm
-                    </button>
-                    <button type="button" name="complex_cancel" id="complex_cancel" class="btn btn-danger"
-                            data-dismiss="modal">Disable
-                    </button>
-                </div>
-            </div>
-
-        </div>
-    </div>
+    @include('partials.confirm-ccm-complexity-modal')
 
     <div class="row" style="margin-top:30px;">
         <div class="main-form-container col-lg-6 col-lg-offset-3 col-md-10 col-md-offset-1"
