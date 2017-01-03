11e949e58cf5358673553c6930a04cef096d4061
rohan <rohanm@circlelinkhealth.com>
2017-01-03 16:50:43 -0500

    Algo Same Day Fix

--- app/PatientContactWindow.php
+++ app/PatientContactWindow.php
@@ -184,7 +184,7 @@ class PatientContactWindow extends Model
 
         }
 
-        $adjusted_offset = Carbon::parse($offset_date)->subDay()->toDateString();
+        $adjusted_offset = Carbon::parse($offset_date)->toDateString();
 
         foreach ($patient_windows as $window){
 
