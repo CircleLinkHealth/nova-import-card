d37ec12313d4b2fc34ac3fdd8cf60b2dec102299
rohan <rohanm@circlelinkhealth.com>
2017-01-11 19:41:38 -0500

    [touch: 742] add alternative billing calculator, UI pending

--- app/Billing/NurseInvoices/VariablePay.php
+++ app/Billing/NurseInvoices/VariablePay.php
@@ -3,16 +3,22 @@
 
 use App\Activity;
 use App\Nurse;
+use App\NurseMonthlySummary;
 use Carbon\Carbon;
 
 class VariablePay extends NurseInvoice
 {
 
     private $ccm_over_duration;
-    private $ccm_under_duration;
+    private $ccm_over_payable;
 
-    const OVER_PER_HOUR = 30;
-    const UNDER_PER_HOUR = 60;
+    private $ccm_under_duration;
+    private $ccm_under_payable;
+
+    private $report;
+
+    const OVER_PER_MINUTE = 10/60;
+    const UNDER_PER_MINUTE = 30/60;
 
     public function __construct(
         Nurse $nurse,
@@ -20,23 +26,28 @@ class VariablePay extends NurseInvoice
         Carbon $end
     ) {
         parent::__construct($nurse, $start, $end);
+
+        $day_start = Carbon::parse(Carbon::now()->firstOfMonth()->format('Y-m-d'));
+        $this->report = NurseMonthlySummary::where('nurse_id', $nurse->id)->where('month_year', $day_start)->first();
+
+        $this->ccm_over_duration = $this->report->accrued_after_ccm;
+        $this->ccm_under_duration = $this->report->accrued_towards_ccm;
+
+        $this->ccm_under_payable = ( $this->ccm_over_duration / 3600 ) * 10;
+        $this->ccm_over_payable = ( $this->ccm_under_duration / 3600 ) * 30;
+
+
     }
 
     public function activityDurationForPeriod(){
 
-        $this->ccm_over_duration = Activity::where('logger_id', $this->nurse->user_id)
-            ->where(function ($q){
-                $q->where('updated_at', '>=', $this->start)
-                    ->where('updated_at', '<=', $this->end);
-            })
-            ->sum('post_ccm_duration');
+        return [
 
-        $this->ccm_under_duration = Activity::where('logger_id', $this->nurse->user_id)
-            ->where(function ($q){
-                $q->where('updated_at', '>=', $this->start)
-                    ->where('updated_at', '<=', $this->end);
-            })
-            ->sum('pre_ccm_duration');
+            'total' => $this->ccm_under_payable + $this->ccm_over_payable,
+            'High Rate' => $this->ccm_under_payable,
+            'Low Rate' => $this->ccm_over_payable
+
+        ];
 
     }
 
@@ -56,8 +67,7 @@ class VariablePay extends NurseInvoice
 
         foreach ($activities as $activity){
 
-            $this->data[Carbon::parse($activity[0]['created_at'])->toDateString()]['ccm_over'] = $activity->sum('post_ccm_duration');
-            $this->data[Carbon::parse($activity[0]['created_at'])->toDateString()]['ccm_under'] = $activity->sum('pre_ccm_duration');
+            $this->data[Carbon::parse($activity[0]['created_at'])->toDateString()]['duration'] = $activity->sum('duration');
 
         };
 
