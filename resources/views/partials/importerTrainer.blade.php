7f4a0412ceb41e986af4354c93c551f8d695cc6d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-01-18 18:49:34 +0200

    Adds Upload Panel [touch:648]

---
+++ resources/views/partials/importerTrainer.blade.php
@@ -0,0 +1,20 @@
+<style>
+
+</style>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <form action="{{ route('make.welcome.call.list') }}" method="post" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="ccda">Just upload a CCDA to train the algo.</label>
+                    <input type="file" name="ccda" id="ccda" required>
+                    <p class="help-block">Hint: You can also drop a CSV file on this panel</p>
+
+                    <input type="submit" class="btn btn-default funky-background" value="Train" name="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
