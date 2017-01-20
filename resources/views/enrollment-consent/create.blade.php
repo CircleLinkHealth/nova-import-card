35528077fb731b3566800f2b2904b22204f99560
rohan <rohanm@circlelinkhealth.com>
2017-01-20 18:08:16 -0500

    [touch: 787] ui updates, adds 'thanks' page

--- resources/views/enrollment-consent/create.blade.php
+++ resources/views/enrollment-consent/create.blade.php
@@ -1,51 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Circle Link Health - CarePlan Manager Provider Dashboard.">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.enrollment-consent-layout')
 
-    <title>CarePlan Manager | Join </title>
-
-    <!-- Add to homescreen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="192x192" href="images/android-desktop.png">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="CarePlan Manager">
-    <link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png">
-
-    <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
-    <meta name="msapplication-TileColor" content="#3372DF">
-
-    <link rel="shortcut icon" href="images/favicon.png">
-
-
-    <link href="{{ asset('/img/favicon.png') }}" rel="icon">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.light_blue-blue.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.0.7/parsley.min.js"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-
-
-</head>
-
+@section('content')
 <style>
     .mdl-layout {
         align-items: center;
@@ -82,17 +37,6 @@
     }
 
 </style>
-
-<div class="mdl-layout mdl-js-layout">
-    <header class="mdl-layout__header">
-        <div class="mdl-layout-icon" style="overflow: visible "><img src="/img/ui/clh_logo_lt.png"
-                                                                     alt="Care Plan Manager"
-                                                                     style="position:relative;top:-15px"
-                                                                     width="50px"/></div>
-        <div class="mdl-layout__header-row">
-            <span class="mdl-layout__title" style="color: white;">CircleLink Health</span>
-        </div>
-    </header>
     <main class="mdl-layout__content">
         <div class="mdl-card mdl-shadow--6dp" style="width: 800px; align-items: center">
             <div class="mdl-card__supporting-text">
@@ -125,8 +69,7 @@
 
                     {{ csrf_field() }}
                     <div>
-                        <p class="headings" style="padding-top: 20px"> If you're visiting this web page, Dr.
-                            (doctor_name) has invited you to
+                        <p class="headings" style="padding-top: 20px"> If you're visiting this web page, your Doctor at {{ucwords($practice->name)}} has invited you to
                             his/her new personalized
                             care management program!</p>
 
@@ -208,5 +151,5 @@
 
 </script>
 
+@stop
 
-</html>
