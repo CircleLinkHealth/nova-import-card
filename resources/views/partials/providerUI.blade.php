b29a9dfad534fa36b96b0f00e52f345c80c03566
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-01-24 16:45:23 +0200

    Adds sections to providerUI [touch:615]

--- resources/views/partials/providerUI.blade.php
+++ resources/views/partials/providerUI.blade.php
@@ -48,10 +48,10 @@
             (function (i, s, o, g, r, a, m) {
                 i['GoogleAnalyticsObject'] = r;
                 i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
+                            (i[r].q = i[r].q || []).push(arguments)
+                        }, i[r].l = 1 * new Date();
                 a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
+                        m = s.getElementsByTagName(o)[0];
                 a.async = 1;
                 a.src = g;
                 m.parentNode.insertBefore(a, m)
@@ -128,6 +128,10 @@
                 @include('partials.addprovider')
             @endif
         @endif
+
+        @include('partials.footer')
+
+        @yield('scripts')
 
 </body>
 
