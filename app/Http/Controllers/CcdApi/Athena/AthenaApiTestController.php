a14f836dfd04290355c5825c9284cf822c1a74f7
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-01-26 13:46:21 +0200

    Adds get Athena Ccd by patient Id into test controller

--- app/Http/Controllers/CcdApi/Athena/AthenaApiTestController.php
+++ app/Http/Controllers/CcdApi/Athena/AthenaApiTestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\CcdApi\Athena;
 use App\ForeignId;
 use App\Http\Controllers\Controller;
 use App\Models\CCD\CcdVendor;
+use App\Services\AthenaAPI\Calls;
 use App\Services\AthenaAPI\Service;
 use Carbon\Carbon;
 
@@ -24,13 +25,25 @@ class AthenaApiTestController extends Controller
         $endDate = Carbon::today()->addDay(1);
         $startDate = $endDate->copy()->subDay(2);
 
-        foreach ($vendors as $vendor)
-        {
+        foreach ($vendors as $vendor) {
             $this->service->getAppointments(1959188, $startDate, $endDate);
             $this->service->getCcdsFromRequestQueue(5);
         }
     }
 
+    public function getCcdas(
+        array $patientIds,
+        $practiceId,
+        $departmentId
+    ) {
+        $api = new Calls();
+
+        foreach ($patientIds as $id) {
+            $ccda = $api->getCcd($id, $practiceId, $departmentId);
+            file_put_contents(storage_path('AthenaCcdas/' . $id . '.xml'), $ccda[0]['ccda']);
+        }
+
+    }
 }
 
 
