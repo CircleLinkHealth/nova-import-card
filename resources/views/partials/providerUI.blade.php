8703a7bcb132ec9dc50fb2637ba4263bb465b56a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-01-31 16:23:22 +0200

    Cleans up scripts

--- resources/views/partials/providerUI.blade.php
+++ resources/views/partials/providerUI.blade.php
@@ -18,114 +18,90 @@
     <link href="{{ asset('/css/patientsearch.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/wpstyle.css') }}" rel="stylesheet">
     <link href="{{ asset('/img/favicon.png') }}" rel="icon">
-
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 
     <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
-    <!-- Metrialize CDN -->
-{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.css" />--}}
-
 @if(!isset($isPdf))
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
-        <!-- Scripts -->
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-        <!-- http://trentrichardson.com/examples/timepicker/
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js"></script>-->
-
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                            (i[r].q = i[r].q || []).push(arguments)
-                        }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                        m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-9084372-23', 'auto');
-            ga('send', 'pageview');
-
-        </script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.0.7/parsley.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-        <script src="{{ asset('/js/idle-timer.min.js') }}"></script>
-        <script src="{{ asset('/js/scripts.js') }}"></script>
-        <script src="{{ asset('/js/bootstrap-select.min.js') }}"></script>
-        <script src="{{ asset('/js/typeahead.bundle.js') }}"></script>
-        <script src="{{ asset('/js/fab.js') }}"></script>
-    @include('partials.searchjs')
-
     <!-- http://curioussolutions.github.io/DateTimePicker/ -->
         <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/datetimepicker/latest/DateTimePicker.min.css"/>
-        <script type="text/javascript" src="//cdn.jsdelivr.net/datetimepicker/latest/DateTimePicker.min.js"></script>
-
         <link rel="stylesheet" href="{{ asset('/webix/codebase/webix.css') }}" type="text/css">
-        <script src="{{ asset('/webix/codebase/webix.js') }}" type="text/javascript"></script>
 
         <!-- select2 -->
-        <script src="{{ asset('/js/bootstrap-select.min.js') }}"></script>
         <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet"/>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
     @endif
 </head>
 <body>
+
 @if(!isset($isPdf))
-
-    @if(!empty($impersonatedUserEmail))
-        <div class="container-fluid text-center" style="background-color: #E46745; color: black; padding: 0.6%;">
-            You are impersonating user with email <b>{{ $impersonatedUserEmail }}</b>
-        </div>
-        @endif
-
     @include('partials.providerUI.primarynav')
 
     @if(!empty($patient->id))
-
         @include('partials.providerUI.patientnav')
-
     @endif
 
+    @if(!empty($patient->id))
+        @include('partials.fab')
     @endif
-
-    @if(!isset($isPdf))
-        @if(!empty($patient->id))
-            @include('partials.fab')
-        @endif
-    @endif
+@endif
 
 <!--[if lt IE 8]>
-        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a
-                href="http://browsehappy.com/">upgrade
-            your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome
-            Frame</a>
-            to improve your experience.</p>
-        <![endif]-->
-        @yield('content')
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a
+        href="http://browsehappy.com/">upgrade
+    your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome
+    Frame</a>
+    to improve your experience.</p>
+<![endif]-->
+@yield('content')
 
-        @if(!isset($isPdf))
-            <!-- PAGE TIMER START -->
-            @include('partials.providerUItimer')
-            <!-- PAGE TIMER END -->
-        @endif
+@if(!isset($isPdf))
+    <!-- PAGE TIMER START -->
+    @include('partials.providerUItimer')
+    <!-- PAGE TIMER END -->
 
-        @include('partials.footer')
+    @include('partials.footer')
 
-        @yield('scripts')
+         <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                        (i[r].q = i[r].q || []).push(arguments)
+                    }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-91176199-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="{{ asset('/js/idle-timer.min.js') }}"></script>
+    <script src="{{ asset('/js/scripts.js') }}"></script>
+    <script src="{{ asset('/js/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('/js/typeahead.bundle.js') }}"></script>
+    <script src="{{ asset('/js/fab.js') }}"></script>
+    @include('partials.searchjs')
+    <script type="text/javascript"
+            src="//cdn.jsdelivr.net/datetimepicker/latest/DateTimePicker.min.js"></script>
+    <script src="{{ asset('/webix/codebase/webix.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('/js/bootstrap-select.min.js') }}"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
+
+    @yield('scripts')
+@endif
 
 </body>
 
