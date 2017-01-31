9566adca972f48bca7e173d409d923e12d0faca1
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-01-31 17:12:49 +0200

    Updates some scripts

--- resources/views/partials/providerUI.blade.php
+++ resources/views/partials/providerUI.blade.php
@@ -28,8 +28,13 @@
         <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/datetimepicker/latest/DateTimePicker.min.css"/>
         <link rel="stylesheet" href="{{ asset('/webix/codebase/webix.css') }}" type="text/css">
 
-        <!-- select2 -->
-        <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet"/>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet"/>
+
+        {{--Prefer to load JS assets in the footer. These have to be loaded here :(--}}
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
     @endif
 </head>
 <body>
@@ -53,6 +58,7 @@
     Frame</a>
     to improve your experience.</p>
 <![endif]-->
+
 @yield('content')
 
 @if(!isset($isPdf))
@@ -66,8 +72,6 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script src="{{ asset('/js/idle-timer.min.js') }}"></script>
@@ -80,7 +84,6 @@
             src="//cdn.jsdelivr.net/datetimepicker/latest/DateTimePicker.min.js"></script>
     <script src="{{ asset('/webix/codebase/webix.js') }}" type="text/javascript"></script>
     <script src="{{ asset('/js/bootstrap-select.min.js') }}"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
 
     @yield('scripts')
 @endif
