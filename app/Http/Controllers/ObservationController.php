66885af678c4be4d65d88664fd8a20be631fc9e7
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-09 17:50:47 +0200

    Syntax changes

--- app/Http/Controllers/ObservationController.php
+++ app/Http/Controllers/ObservationController.php
@@ -34,93 +34,6 @@ class ObservationController extends Controller
         //
     }
 
-
-    /** @SWG\Post(
-     *     path="/observation",
-     *     tags={"observation"},
-     *     operationId="createObservation",
-     *     summary="Appends comment to daily state_app and creates a new observation",
-     *     description="",
-     *     consumes={"application/json", "application/xml"},
-     *     produces={"application/xml", "application/json"},
-     * @SWG\Parameter(
-     *         name="Authorization",
-     *         type="string",
-     *         in="header",
-     *         description="Token",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     * @SWG\Parameter(
-     *         name="X-Authorization",
-     *         in="header",
-     *         type="string",
-     *         description="API Key",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     * @SWG\Parameter(
-     *         name="client",
-     *         in="header",
-     *         type="string",
-     *         description="mobi/ui",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *     @SWG\Parameter(
-     *         name="parent_id",
-     *         in="body",
-     *         description="Id of the state_app record for the given day",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *     @SWG\Parameter(
-     *         name="obs_message_id",
-     *         in="body",
-     *         description="Observation Message id",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *     @SWG\Parameter(
-     *         name="obs_key",
-     *         in="body",
-     *         description="Observation Key",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *      @SWG\Parameter(
-     *         name="obs_value",
-     *         in="body",
-     *         description="Observation Value",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *     @SWG\Parameter(
-     *         name="obs_date",
-     *         in="body",
-     *         description="Created timestamp",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *     @SWG\Parameter(
-     *         name="timezone",
-     *         in="body",
-     *         description="User's timezone",
-     *         required=true,
-     *         @SWG\Schema(ref="#/definitions/Observation"),
-     *     ),
-     *     @SWG\Response(
-     *         response=201,
-     *         description="Success",
-     *     ),
-     *     @SWG\Response(
-     *         response=500,
-     *         description="Error",
-     *     ),
-     *     security={{"petstore_auth":{"write:pets", "read:pets"}}}
-     * )
-     */
-
     public function store(Request $request)
     {
         $observationService = new ObservationService;
@@ -250,38 +163,6 @@ class ObservationController extends Controller
         }
     }
 
-
-    /**
-     * @SWG\Get(
-     *     path="/observations/{id}",
-     *     description="Returns a observations based on a single id",
-     *     operationId="observations",
-     *     @SWG\Parameter(
-     *         description="id of observations to fetch",
-     *         format="int64",
-     *         in="path",
-     *         name="id",
-     *         required=true,
-     *         type="integer"
-     *     ),
-     *     produces={
-     *         "application/json",
-     *         "application/xml",
-     *         "text/html",
-     *         "text/xml"
-     *     },
-     *     @SWG\Response(
-     *         response=200,
-     *         description="pet response",
-     *         @SWG\Schema(ref="#/definitions/observations")
-     *     ),
-     *     @SWG\Response(
-     *         response="default",
-     *         description="unexpected error",
-     *         @SWG\Schema(ref="#/definitions/errorModel")
-     *     )
-     * )
-     */
     public function show($id)
     {
         //
