243886b5a282bb4cd3be4f561b79d7c0ea8699f1
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-10 20:54:47 +0200

    Adds contact card fillable

--- app/ContactCard.php
+++ app/ContactCard.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactCard extends Model
 {
+    public $fillable = [
+        'contactcardable_type',
+        'contactcardable_id',
+        'email',
+        'emr_direct',
+        'work_phone',
+        'cell_phone',
+        'home_phone',
+    ];
+
     /**
      * Get all of the owning contactCardable models.
      */
