6e541eed55a75f81456cb6141020e023c267c994
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-14 00:12:34 +0200

    provider dashboard

--- resources/views/provider/layouts/dashboard.blade.php
+++ resources/views/provider/layouts/dashboard.blade.php
@@ -2,25 +2,39 @@
 
 @section('title', 'Provider Dashboard')
 
+@section('head')
+    <link rel="stylesheet" href="{{ asset('/css/provider-dashboard.css') }}"/>
+@endsection
+
 @section('content')
     <div class="container full-width">
-
         <div class="row">
-            <div class="col s3">
-                @include('provider.partials.avatar')
-                @include('provider.navigation.default')
+            <div class="col s12">
+                @include('provider.partials.topBarHeader')
             </div>
-            <main class="col s9">
 
-                <div class="col s12">
-                    @include('provider.partials.topBarHeader')
-                </div>
+            <div class="col s12 m4 l3">
+
+                <ul id="slide-out" class="side-nav fixed">
+                    <li class="center-align">
+                        <img src="{{asset('/img/clh_logo.svg')}}" height="64" class="brand-logo">
+                    </li>
+                    <li>
+                        <div class="divider"></div>
+                    </li>
+                    <li>
+                        @include('provider.navigation.default')
+                    </li>
+                </ul>
+
+
+            </div>
+            <main class="col s12 m8 l9">
 
                 <div id="app" class="">
                     @yield('module')
                 </div>
             </main>
-
         </div>
 
     </div>
