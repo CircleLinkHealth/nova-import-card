da83b873afa92eae1a0884b14eb9a5fde2f8551c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-14 00:41:41 +0200

    Fixes layout

--- resources/views/provider/layouts/dashboard.blade.php
+++ resources/views/provider/layouts/dashboard.blade.php
@@ -9,13 +9,8 @@
 @section('content')
     <div class="container full-width">
         <div class="row">
-            <div class="col s12">
-                @include('provider.partials.topBarHeader')
-            </div>
-
-            <div class="col s12 m4 l3">
-
-                <ul id="slide-out" class="side-nav fixed">
+            <div class="col s12 m3">
+                <ul id="slide-out" class="side-nav col s12 m3" style="transform: translateX(0);">
                     <li class="center-align">
                         <img src="{{asset('/img/clh_logo.svg')}}" height="64" class="brand-logo">
                     </li>
@@ -26,12 +21,13 @@
                         @include('provider.navigation.default')
                     </li>
                 </ul>
-
-
             </div>
-            <main class="col s12 m8 l9">
+            <main class="col s12 m9 offset-3">
+                <div class="row">
+                    @include('provider.partials.topBarHeader')
+                </div>
 
-                <div id="app" class="">
+                <div id="app" class="row">
                     @yield('module')
                 </div>
             </main>
