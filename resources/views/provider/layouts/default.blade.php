5775ae1fed44aa7b8cca620d9e02ca1aa74a8a87
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-13 20:20:15 +0200

    Default layout changes

--- resources/views/provider/layouts/default.blade.php
+++ resources/views/provider/layouts/default.blade.php
@@ -33,9 +33,10 @@
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="{{ asset('/css/provider-dashboard.css') }}"/>
+
+    @yield('head')
 </head>
-<body class="onboarding-container">
+<body class="main-container">
 
 {{--This is for JS variables. Purposefully included before content.--}}
 @include('partials.footer')
