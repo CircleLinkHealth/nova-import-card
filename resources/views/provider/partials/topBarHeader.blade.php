a05a76726a89b5c2d1b0f6381b64b160eec0afb7
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-14 00:41:12 +0200

    Adds page title

--- resources/views/provider/partials/topBarHeader.blade.php
+++ resources/views/provider/partials/topBarHeader.blade.php
@@ -1,5 +1,7 @@
 <nav>
     <div class="nav-wrapper primary-color">
+        <a href="#" class="center" style="margin-left: 15px;">@yield('title')</a>
+
         <ul class="right hide-on-med-and-down">
             <li>
                 <a href="#!">
