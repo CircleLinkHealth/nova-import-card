9cfa9398f53cd4911f334bf735a6d60d1c1e0094
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-14 19:25:01 +0200

    Add provider dashboard acl

---
+++ app/Http/Middleware/ProviderDashboardACL.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Practice;
+use Closure;
+use Illuminate\Support\Facades\Route;
+
+class ProviderDashboardACL
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle(
+        $request,
+        Closure $next,
+        $role
+    ) {
+        $practiceSlug = Route::current()->getParameter('practiceSlug');
+        $practice = Practice::whereName($practiceSlug)->first();
+
+        if (auth()->user()->hasRole($role)
+            && in_array($practice->id, auth()->user()->practices->pluck('id')->all())
+        ) {
+            return $next($request);
+        }
+
+        abort(403, 'You do not have access to this page.');
+    }
+}
