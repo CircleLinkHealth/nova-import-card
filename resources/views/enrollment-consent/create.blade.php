bbbbcbb69a1dd1ec03bda558df5ca5f56fcd1cc0
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-02-14 15:26:42 -0500

    removes demographic fields on enrollment create #840

--- resources/views/enrollment-consent/create.blade.php
+++ resources/views/enrollment-consent/create.blade.php
@@ -21,126 +21,104 @@
 
             font-size: 23px;
             line-height: 30px;
+            text-align: center;
         }
 
         .info-list {
-           font-size: 20px;
-           margin-top: 10px;
-           margin-bottom: 10px;
+            margin-top: -1px;
+            margin-bottom: 0px;
+            color: #080808;
+            background: #c3cdd2;
+            padding: 18px 20px;
+            text-align: center;
+            font-size: 18px;
+            border-bottom: white 2px solid;
         }
 
-        .invalid-phone {
-            color: red;
-        }
-
-        .valid-phone {
-            color: green;
-        }
 
     </style>
 
 </head>
 
 <nav>
-    <div class="nav-wrapper">
+    <div class="nav-wrapper center">
         <div class="mdl-layout__header-row" style="background: #4fb2e2; padding-left: 10px">
-            <span class="mdl-layout__title" style="color: white;">{{$practice->display_name}}'s Personalized Care Management Program</span>
+            <span class="mdl-layout__title" style="color: white; font-size: 1.4em;">On Behalf of Dr. Mazhar’s Office</span>
         </div>
     </div>
 </nav>
 
 <div class="container">
-    <div>
-        <p class="headings" style="padding-top: 20px; color: black">“Dr. [first_name] [last_name] has invited you to their new personalized care management program for improved wellness!</p>
+    <p class="headings" style="padding-top: 20px; color: black">Mr. John Doe, Dr. Selma Mazhar has invited you to their
+        new personalized care management program for improved wellness!</p>
+</div>
 
-        <p class="flow-text info-list">- A personal care coach --registered nurse-- will do a quick phone check in about 2x a month for Dr. [Dr._first_name] [Dr._last_name] to provide support, health information and to see how you’re doing</p>
-        <p class="flow-text info-list">- You can leave a message 24/7 at (888) 729-4045 if anything comes up, and a care coach will get back to you shortly</p>
-        <p class="flow-text info-list">- Only one practice/doctor at a time can provide this program</p>
-        <p class="flow-text info-list">- You can withdraw anytime by calling (888) 729-4045</p>
-        @if(isset($has_copay))
-            <p style="font-size: 20px;" class="flow-text">- Medicare covers the program you may be responsible for a ~$8 per month co-pay</p>
-        @endif
+<div class="info">
 
-    </div>
-
-    <div class="row" id="enrollment_module">
-        <form method="post" name="enroll" id="enroll"
-              action="{{URL::route('patient.enroll.store', ['program_name' => $practice])}}"
-              class="col s12" style="padding-top: 20px;">
-            {{ csrf_field() }}
-
-            <div class="row">
-                <div class="input-field col s12 m6">
-                    <label for="first_name">First Name</label>
-                    <input placeholder="Enter First Name..." id="first_name" name="first_name" type="text">
-                </div>
-                <div class="input-field col s12 m6">
-                    <label for="last_name">Last Name</label>
-                    <input placeholder="Enter Last Name..." id="last_name" name="last_name" type="text">
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12 m6">
-                    <label class="active" for="dob">Date Of Birth</label>
-                    <input placeholder="XX-XX-XXXX" type="date" class="datepicker" name="dob" id="dob">
-                </div>
-                <div class="input-field col s12 m6">
-                    <label for="phone"><span v-bind:class="phoneValid">Phone Number</span></label>
-                    <input placeholder="XXX-XXX-XXXX" v-on:keyUp="checkPhone" v-model="phone" id="phone" type="text"
-                           name="phone">
-                </div>
-            </div>
-
-            <input type="datetime" v-model="enrolled_time" id="enrolled_time" name="enrolled_time" hidden>
-            <input type="datetime" v-model="confirmed_time" id="confirmed_time" name="confirmed_time" hidden>
-            <input type="text" id="practice_id" name="practice_id" value="{{$practice->id}}" hidden>
-
-            <div class="row right">
-                <a class="waves-effect waves-light btn modal-trigger" v-on:click="openModal" href="#confirm">Consent</a>
-            </div>
-
-
-
-
-            <div id="confirm" class="modal modal-fixed-footer">
-                <div class="modal-content">
-                    <h4 class="" style="color: #47beab">Great! Remember:</h4>
-                    <blockquote>“Great! We’ll be in touch shortly.
-                        Optionally, you can tell us the best time to reach you:</blockquote>
-                    <div class="row">
-                        <div class="col s12 m6">
-                            <select class="input-field" name="day" id="day">
-                                <option disabled selected>Select Day</option>
-                                <option value="1">Monday</option>
-                                <option value="2">Tuesday</option>
-                                <option value="3">Wednesday</option>
-                                <option value="4">Thursday</option>
-                                <option value="5">Friday</option>
-                            </select>
-                            <label class="active" for="day">Day</label>
-                        </div>
-                        <div class="col s12 m6">
-                            <select class="input-field" name="time" id="time">
-                                <option disabled selected>Select Day</option>
-                                <option value="09:00-12:00">9AM - Noon</option>
-                                <option value="12:00-15:00">Noon - 3PM</option>
-                                <option value="15:00-18:00">3PM - 6PM</option>
-                            </select>
-                            <label class="active" for="time">Time</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button id="submit" name="submit" v-on:onclick="submitForm"
-                            class="modal-action waves-effect waves-green btn-flat">Acknowledge and Exit
-                    </button>
-                </div>
-            </div>
-
-        </form>
-    </div>
+    <p class="info-list">Calls from Registered Nurses 2x Monthly to track your health</p>
+    <p class="info-list">24/7 Helpline (888) 729-4045</p>
+    <p class="info-list">Only one practice/doctor at a time can provide this program</p>
+    <p class="info-list">Withdraw anytime by calling (888) 729-4045</p>
+    @if(isset($has_copay))
+        <p style="font-size: 20px;" class="flow-text">- Medicare covers the program you may be responsible for a ~$8 per
+            month co-pay</p>
+    @endif
 
 </div>
+
+<div class="row" id="enrollment_module">
+    <form method="post" name="enroll" id="enroll"
+          action="{{URL::route('patient.enroll.store', ['program_name' => $practice])}}"
+          class="col s12" style="padding-top: 20px;">
+        {{ csrf_field() }}
+
+        <input type="datetime" v-model="enrolled_time" id="enrolled_time" name="enrolled_time" hidden>
+        <input type="datetime" v-model="confirmed_time" id="confirmed_time" name="confirmed_time" hidden>
+        <input type="text" id="practice_id" name="practice_id" value="{{$practice->id}}" hidden>
+
+        <div class="row center">
+            <a class="waves-effect waves-light btn modal-trigger" v-on:click="openModal" href="#confirm">Consent</a>
+        </div>
+
+        <div id="confirm" class="modal modal-fixed-footer">
+            <div class="modal-content">
+                <h4 class="" style="color: #47beab">Great! Remember:</h4>
+                <blockquote>“Great! We’ll be in touch shortly.
+                    Optionally, you can tell us the best time to reach you:
+                </blockquote>
+                <div class="row">
+                    <div class="col s12 m6">
+                        <select class="input-field" name="day" id="day">
+                            <option disabled selected>Select Day</option>
+                            <option value="1">Monday</option>
+                            <option value="2">Tuesday</option>
+                            <option value="3">Wednesday</option>
+                            <option value="4">Thursday</option>
+                            <option value="5">Friday</option>
+                        </select>
+                        <label class="active" for="day">Day</label>
+                    </div>
+                    <div class="col s12 m6">
+                        <select class="input-field" name="time" id="time">
+                            <option disabled selected>Select Day</option>
+                            <option value="09:00-12:00">9AM - Noon</option>
+                            <option value="12:00-15:00">Noon - 3PM</option>
+                            <option value="15:00-18:00">3PM - 6PM</option>
+                        </select>
+                        <label class="active" for="time">Time</label>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button id="submit" name="submit" v-on:onclick="submitForm"
+                        class="modal-action waves-effect waves-green btn-flat">Acknowledge and Exit
+                </button>
+            </div>
+        </div>
+
+    </form>
+</div>
+
 
 </html>
 
--- resources/views/layouts/enrollment-consent-layout.blade.php
+++ resources/views/layouts/enrollment-consent-layout.blade.php
@@ -1,51 +1,21 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Circle Link Health - CarePlan Manager Provider Dashboard.">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>CarePlan Manager | Enroll </title>
-
-    <!-- Add to homescreen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="192x192" href="images/android-desktop.png">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="CarePlan Manager">
-    <link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png">
-
-    <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
-    <meta name="msapplication-TileColor" content="#3372DF">
-
-    <link rel="shortcut icon" href="images/favicon.png">
-
-
-    <link href="{{ asset('/img/favicon.png') }}" rel="icon">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.light_blue-blue.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.0.7/parsley.min.js"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-
-</head>
-
-@yield('content')
-
-</html>
+<div class="row">
+    <div class="input-field col s12 m6">
+        <label for="first_name">First Name</label>
+        <input placeholder="Enter First Name..." id="first_name" name="first_name" type="text">
+    </div>
+    <div class="input-field col s12 m6">
+        <label for="last_name">Last Name</label>
+        <input placeholder="Enter Last Name..." id="last_name" name="last_name" type="text">
+    </div>
+</div>
+<div class="row">
+    <div class="input-field col s12 m6">
+        <label class="active" for="dob">Date Of Birth</label>
+        <input placeholder="XX-XX-XXXX" type="date" class="datepicker" name="dob" id="dob">
+    </div>
+    <div class="input-field col s12 m6">
+        <label for="phone"><span v-bind:class="phoneValid">Phone Number</span></label>
+        <input placeholder="XXX-XXX-XXXX" v-on:keyUp="checkPhone" v-model="phone" id="phone" type="text"
+               name="phone">
+    </div>
+</div>
