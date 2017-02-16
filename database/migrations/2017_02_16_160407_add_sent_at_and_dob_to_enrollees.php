08975844c15e93fb72bb3e8247985908b6b619cb
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-02-16 16:11:15 -0500

    adds dob and invite_sent_at to enrollees table migration

---
+++ database/migrations/2017_02_16_160407_add_sent_at_and_dob_to_enrollees.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSentAtAndDobToEnrollees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('enrollees', function (Blueprint $table) {
+
+            $table->date('dob')->after('phone')->nullable();
+            $table->date('invite_sent_at')->after('preferred_window')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('enrollees', function (Blueprint $table) {
+
+            $table->dropColumn('dob');
+            $table->dropColumn('invite_sent_at');
+
+        });
+    }
+}
