3261acec39da685a53d2575146a6aab65f11ebb4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-16 18:05:55 +0200

    Add new problems migration

---
+++ database/migrations/2017_02_16_104938_update_problems.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProblems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('db:seed', [
+            '--class' => AddNewProblems::class,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
