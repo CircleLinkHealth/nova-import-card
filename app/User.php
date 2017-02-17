758c98e03931251760bb8266a9ed99d407e0bd85
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-17 22:03:11 +0200

    Updates PRactice function on User

--- app/User.php
+++ app/User.php
@@ -1924,19 +1924,26 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 
     /**
      * Get the specified Practice, if it is related to this User
+     * You can pass in a practice_id, practice_slug, or  App\Practice object
      *
-     * @param $practiceId
+     * @param $practice
      *
      * @return mixed
      */
-    public function practice($practiceId)
+    public function practice($practice)
     {
-        if (is_object($practiceId)) {
-            $practiceId = $practiceId->id;
+        if (is_string($practice) && !is_int($practice)) {
+            return $this->practices()
+                ->where('name', '=', $practice)
+                ->first();
+        }
+
+        if (is_object($practice)) {
+            $practice = $practice->id;
         }
 
         return $this->practices()
-            ->where('program_id', '=', $practiceId)
+            ->where('program_id', '=', $practice)
             ->first();
     }
 
