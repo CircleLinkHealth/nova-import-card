76be9814d212410ceed7dd52b714f5ba07a6bc9a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-23 19:13:31 +0100

    Fix [touch:801]

--- resources/views/partials/sendToCareTeam.blade.php
+++ resources/views/partials/sendToCareTeam.blade.php
@@ -1,13 +1,20 @@
 <div class="form-group">
     <div class="col-sm-12">
+        <label>Send To</label>
+    </div>
+    <div class="col-sm-12">
         <input type="checkbox" id="notify-careteam" name="notify_careteam">
-        <label for="notify-careteam"><span> </span>Send to Provider/CareTeam</label>
+        <label for="notify-careteam"><span> </span>Provider/CareTeam</label>
+    </div>
+    <div class="col-sm-12">
+        <input type="checkbox" id="notify-careteam" name="notify_careteam">
+        <label for="notify-careteam"><span> </span>Provider/CareTeam</label>
     </div>
     <div class="col-sm-12">
         <div class="form-group">
-            @if(count($patient->careTeamMembers) > 0)
+            @if(count($patient->care_team_receives_alerts) > 0)
                 This will
-                notify: @foreach($patient->careTeamMembers as $carePerson){{ ($loop->first ? '' : ', ') . $carePerson->user->fullName }}@endforeach
+                notify: @foreach($patient->care_team_receives_alerts as $carePerson){{ ($loop->first ? '' : ', ') . $carePerson->user->fullName }}@endforeach
             @else
                 The CareTeam is empty. Please visit
                 <strong>{{link_to_route('patient.careplan.print', 'View CarePlan', ['patientId' => $patient->id])}}</strong>
