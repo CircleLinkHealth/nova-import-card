74852dee17c6e5c8406c70aa655be5b8df7bd41d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-23 19:15:30 +0100

    Adds CircleLink Support

--- resources/views/partials/sendToCareTeam.blade.php
+++ resources/views/partials/sendToCareTeam.blade.php
@@ -3,8 +3,8 @@
         <label>Send To</label>
     </div>
     <div class="col-sm-12">
-        <input type="checkbox" id="notify-careteam" name="notify_careteam">
-        <label for="notify-careteam"><span> </span>Provider/CareTeam</label>
+        <input type="checkbox" id="notify-circlelink-support" name="notify_circlelink_support">
+        <label for="notify-circlelink-support"><span> </span>CircleLink Support</label>
     </div>
     <div class="col-sm-12">
         <input type="checkbox" id="notify-careteam" name="notify_careteam">
@@ -14,7 +14,7 @@
         <div class="form-group">
             @if(count($patient->care_team_receives_alerts) > 0)
                 This will
-                notify: @foreach($patient->care_team_receives_alerts as $carePerson){{ ($loop->first ? '' : ', ') . $carePerson->user->fullName }}@endforeach
+                notify: @foreach($patient->care_team_receives_alerts as $carePerson){{ ($loop->first ? '' : ', ') . $carePerson->fullName }}@endforeach
             @else
                 The CareTeam is empty. Please visit
                 <strong>{{link_to_route('patient.careplan.print', 'View CarePlan', ['patientId' => $patient->id])}}</strong>
