738f6dbba4bf8d1d7c9e512ee3bdfc4345fdd58b
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-24 17:59:30 +0100

    Adds notifies on same line

--- resources/views/partials/sendToCareTeam.blade.php
+++ resources/views/partials/sendToCareTeam.blade.php
@@ -3,18 +3,20 @@
         <label>Send To</label>
     </div>
     <div class="col-sm-12">
-        <input type="checkbox" id="notify-circlelink-support" name="notify_circlelink_support" value="1">
-        <label for="notify-circlelink-support"><span> </span>CircleLink Support</label>
+        <div class="col-sm-12">
+            <input type="checkbox" id="notify-circlelink-support" name="notify_circlelink_support" value="1">
+            <label for="notify-circlelink-support"><span> </span>CircleLink Support</label>
+        </div>
     </div>
     <div class="col-sm-12">
-        <input type="checkbox" id="notify-careteam" name="notify_careteam" value="1">
-        <label for="notify-careteam"><span> </span>Provider/CareTeam</label>
-    </div>
-    <div class="col-sm-12">
-        <div class="form-group">
+        <div class="col-sm-4">
+            <input type="checkbox" id="notify-careteam" name="notify_careteam" value="1">
+            <label for="notify-careteam"><span> </span>Provider/CareTeam</label>
+        </div>
+
+        <div class="col-sm-8">
             @if(count($patient->care_team_receives_alerts) > 0)
-                This will
-                notify: @foreach($patient->care_team_receives_alerts as $carePerson){{ ($loop->first ? '' : ', ') . $carePerson->fullName }}@endforeach
+                Notifies: @foreach($patient->care_team_receives_alerts as $carePerson){{ ($loop->first ? '' : ', ') . $carePerson->fullName }}@endforeach
             @else
                 The CareTeam is empty. Please visit
                 <strong>{{link_to_route('patient.careplan.print', 'View CarePlan', ['patientId' => $patient->id])}}</strong>
