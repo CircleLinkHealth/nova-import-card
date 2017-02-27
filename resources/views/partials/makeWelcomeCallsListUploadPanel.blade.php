0b23be5c7cc5e3f7333d570290d2f3d67feb1aa9
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-27 19:08:13 +0100

    Adds filters checkboxes

--- resources/views/partials/makeWelcomeCallsListUploadPanel.blade.php
+++ resources/views/partials/makeWelcomeCallsListUploadPanel.blade.php
@@ -6,9 +6,17 @@
                 <div class="form-group">
                     <label for="patient_list">Upload *.csv patient list:</label>
                     <input type="file" name="patient_list" id="patient_list" required>
-                    <p class="help-block">Hint: You can also drop a CSV file on this panel</p>
 
-                    <input type="submit" class="btn btn-default" value="Make Calls List" name="submit">
+                    <input type="checkbox" name="filterLastEncounter" id="filterLastEncounter" checked>
+                    <label for="">filterLastEncounter</label>
+
+                    <input type="checkbox" name="filterProblems" id="filterProblems" checked>
+                    <label for="">filterProblems</label>
+
+                    <input type="checkbox" name="filterInsurance" id="filterInsurance" checked>
+                    <label for="">filterInsurance</label>
+
+                    <input type="submit" class="btn btn-default" value="Create Enrollees" name="submit">
                 </div>
             </form>
         </div>
