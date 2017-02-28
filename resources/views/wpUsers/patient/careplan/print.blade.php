5f510d3ac4c44d269ea2c8a8fcc907bb469b27cf
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-02-28 15:52:06 +0100

    Removes For for full conditions list

--- resources/views/wpUsers/patient/careplan/print.blade.php
+++ resources/views/wpUsers/patient/careplan/print.blade.php
@@ -251,7 +251,7 @@ if (isset($patient) && !empty($patient)) {
                     <div class="patient-info__subareas">
                         <div class="row">
                             <div class="col-xs-12">
-                                <h3 class="patient-summary__subtitles--subareas patient-summary--careplan">For
+                                <h3 class="patient-summary__subtitles--subareas patient-summary--careplan">@if($key != App\Models\CPM\CpmMisc::OTHER_CONDITIONS){{'For '}}@endif
                                     <?= $key ?>:</h3>
                             </div>
                             <div class="col-xs-12">
