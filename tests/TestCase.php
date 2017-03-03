abfed2aa8a98c163c94c817e342a5b30c6b0398a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-03 15:36:37 +0100

    Adds env('APP_URL') to test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,21 +1,26 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
 
-	protected $baseUrl = 'http://localhost';
-	
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    protected $baseUrl = 'http://localhost';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        if (env('APP_URL')) {
+            $this->baseUrl = env('APP_URL');
+        }
 
-		return $app;
-	}
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
 
 }
