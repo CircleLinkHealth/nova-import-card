18dccd8de5711eb6a50a3c17ed5578088390e5b0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-06 21:21:25 +0100

    Adds primary phone #45

--- app/Importer/Section/Importers/Demographics.php
+++ app/Importer/Section/Importers/Demographics.php
@@ -66,6 +66,7 @@ class Demographics extends BaseImporter
             'cell_phone'                 => $itemLog->cell_phone,
             'home_phone'                 => $itemLog->home_phone,
             'work_phone'                 => $itemLog->work_phone,
+            'primary_phone'              => $itemLog->primary_phone,
             'email'                      => strtolower(str_replace('/', '', $itemLog->email)),
             'study_phone_number'         => empty($itemLog->cell_phone)
                 ? empty($itemLog->home_phone)
