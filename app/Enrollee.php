3ba141ef0a6f9f98939bdcf949192b6bc5ba5f11
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-08 10:18:05 -0500

    adds scopes to Enrollees

--- app/Enrollee.php
+++ app/Enrollee.php
@@ -16,10 +16,10 @@ class Enrollee extends Model
 
     /**
      * STATUS TYPES:
-     *
+     * eligble, , , mailed, consented, rejected
      * eligible: just imported to enrollees table, queue of sms recipients.
-     * sms_sent: initial sms sent
-     * sms_received: patient opened link
+     * smsed: initial sms sent
+     * call_queue: patient will show up in enrollment UI
      * consented: client consented
      * ccd_obtained: medical records were imported
      * ccd_qaed: QAed, good to go for enrollment
@@ -181,6 +181,18 @@ class Enrollee extends Model
         $twilio->message($this->primary_phone,
             "Dr. $provider_name hasn’t heard from you regarding their new wellness program $emjo. Please enroll here: $link");
 
+
+    }
+
+    public function scopeToSMS($query){
+
+        return $query->where('status', 'eligible');
+
+    }
+
+    public function scopeToCall($query){
+
+        return $query->where('status', 'call_queue');
 
     }
 
