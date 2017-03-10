5f113d029fa5d2b9e42f060a5ad81b2ceb79042a
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-10 16:25:51 -0500

    mucho updates

--- app/CareAmbassadorLog.php
+++ app/CareAmbassadorLog.php
@@ -8,51 +8,48 @@ use Illuminate\Database\Eloquent\Model;
 class CareAmbassadorLog extends Model
 {
 
-    protected $fillable = [
+    protected $fillable  = [
         'care_ambassador_id',
-        'month-year',
+        'day',
         'no_enrolled',
         'no_rejected',
         'no_utc',
         'total_calls',
-        'total_time_in_system',
+        'total_time_in_system'
     ];
 
-    public static function createOrGetLogs($care_ambassador_id)
-    {
-
-        $date = Carbon::now()->format('Y-m-d');
-        $report =
-            self
-                ::where('care_ambassador_id', $care_ambassador_id)
-                ->where('month-year', $date)
-                ->first();
-
-        if ($report == null) {
-
-            return self
-                ::create([
-                    'care_ambassador_id' => $care_ambassador_id,
-                    'month-year'         => $date,
-                ]);
-
-        }
-
-        return $report;
-
-    }
-
-    public function user()
-    {
+    public function user(){
 
         return $this->belongsTo(User::class, 'care_ambassador_id');
 
     }
 
-    public function totalUniquePatientsCalled()
-    {
+    public function totalUniquePatientsCalled(){
 
         return Enrollee::where('care_ambassador_id', $this->care_ambassador_id)->count();
+
+    }
+
+    public static function createOrGetLogs($care_ambassador_id){
+
+        $date = Carbon::now()->format('Y-m-d');
+        $report =
+                self
+                    ::where('care_ambassador_id', $care_ambassador_id)
+                    ->where('day', $date)
+                    ->first();
+
+        if($report == null){
+
+            return self
+                   ::create([
+                        'care_ambassador_id' => $care_ambassador_id,
+                        'day' => $date,
+                   ]);
+
+        }
+
+        return $report;
 
     }
 
