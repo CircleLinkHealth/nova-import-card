e8fccc4225f8f945e1d0cbbc3a70753d28aef121
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-13 10:13:55 +0100

    Adds migration for bp alerts [deploy:production]

---
+++ database/migrations/2017_03_13_051156_make_bp_receive_alerts.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class MakeBpReceiveAlerts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('db:seed', [
+            '--class' => MakeBillingProviderReceiveAlerts::class,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
