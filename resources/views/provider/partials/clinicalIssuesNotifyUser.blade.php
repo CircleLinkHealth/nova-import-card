72e6d6e5618e7d9c3355a8204c520db7e8805061
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-15 11:55:20 +0100

    Changes instruction #77

--- resources/views/provider/partials/clinicalIssuesNotifyUser.blade.php
+++ resources/views/provider/partials/clinicalIssuesNotifyUser.blade.php
@@ -1,5 +1,5 @@
 <select v-select2="newUser.clinical_issues_notify.user_id" style="width: 100%;">
-    <option value="" disabled selected>Select User to notify.</option>
+    <option value="" disabled selected>Select existing Staff Member.</option>
 
     <option v-for="user in newUsers"
             value="@{{ user.id }}">@{{ user.first_name }} @{{ user.last_name }}</option>
