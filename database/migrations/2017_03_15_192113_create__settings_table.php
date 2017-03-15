f6d29257c06fc72cf0a0f7705f8da2589513b579
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-16 00:42:10 +0100

    Migration changes #90

--- database/migrations/2017_03_15_192113_create__settings_table.php
+++ database/migrations/2017_03_15_192113_create__settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotificationSettingsTable extends Migration
+class CreateSettingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateNotificationSettingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notification_settings', function (Blueprint $table) {
+        Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('notifiable_id');
-            $table->string('notifiable_type');
+            $table->unsignedInteger('settingsable_id');
+            $table->string('settingsable_type');
+            $table->boolean('auto_approve_careplans');
             $table->boolean('dm_pdf_careplan');
             $table->boolean('dm_pdf_notes');
             $table->boolean('email_careplan_approval_reminders');
@@ -34,6 +35,6 @@ class CreateNotificationSettingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notification_setings');
+        Schema::dropIfExists('settings');
     }
 }
