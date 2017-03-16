b65fcca3c178af2e6d060469b9e0fa705d8f0d42
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-16 01:42:25 +0100

    Updates HasSettings #90

--- app/Traits/HasSettings.php
+++ app/Traits/HasSettings.php
@@ -28,17 +28,15 @@ trait HasSettings
      */
     public function syncSettings(Settings $settings)
     {
+        $args = [];
+
+        foreach ($settings->getFillable() as $fieldName) {
+            $args[$fieldName] = $settings->{$fieldName};
+        }
+
         Settings::updateOrCreate([
             'settingsable_type' => self::class,
             'settingsable_id'   => $this->id,
-        ], [
-            'auto_approve_careplans'            => $settings->auto_approve_careplans,
-            'dm_pdf_careplan'                   => $settings->dm_pdf_careplan,
-            'dm_pdf_notes'                      => $settings->dm_pdf_notes,
-            'efax_pdf_careplan'                 => $settings->efax_pdf_careplan,
-            'efax_pdf_notes'                    => $settings->efax_pdf_notes,
-            'email_careplan_approval_reminders' => $settings->email_careplan_approval_reminders,
-            'email_note_was_forwarded'          => $settings->email_note_was_forwarded,
-        ]);
+        ], $args);
     }
 }
