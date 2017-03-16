525212f0148159e71775fe6f152e2954bbef9293
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-16 16:15:01 +0000

    Fix [deploy:production]

--- database/migrations/2017_03_15_195207_add_settings_for_existing_practises.php
+++ database/migrations/2017_03_15_195207_add_settings_for_existing_practises.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Practice;
-use App\Settings;
 use Illuminate\Database\Migrations\Migration;
 
 class AddSettingsForExistingPractises extends Migration
@@ -13,19 +11,19 @@ class AddSettingsForExistingPractises extends Migration
      */
     public function up()
     {
-        foreach (Practice::all() as $p) {
-            $settings = new Settings([
-                'auto_approve_careplans'            => $p->auto_approve_careplans,
-                'email_careplan_approval_reminders' => $p->send_alerts,
-                'dm_pdf_careplan'                   => true,
-                'dm_pdf_notes'                      => true,
-                'efax_pdf_careplan'                 => true,
-                'efax_pdf_notes'                    => true,
-                'email_note_was_forwarded'          => true,
-            ]);
-
-            $p->syncSettings($settings);
-        }
+//        foreach (Practice::all() as $p) {
+//            $settings = new Settings([
+//                'auto_approve_careplans'            => $p->auto_approve_careplans,
+//                'email_careplan_approval_reminders' => $p->send_alerts,
+//                'dm_pdf_careplan'                   => true,
+//                'dm_pdf_notes'                      => true,
+//                'efax_pdf_careplan'                 => true,
+//                'efax_pdf_notes'                    => true,
+//                'email_note_was_forwarded'          => true,
+//            ]);
+//
+//            $p->syncSettings($settings);
+//        }
     }
 
     /**
