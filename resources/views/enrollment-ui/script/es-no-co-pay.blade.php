f42a42d6d86f1baeb16e7c30788a69657e81dd58
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-16 22:34:50 -0400

    adds spanish scripts

---
+++ resources/views/enrollment-ui/script/es-no-co-pay.blade.php
@@ -0,0 +1,34 @@
+<p>[speak clearly and cheerfully]: Hola, [nombre de la persona que llama] estoy llamando en nombre de
+    los doctores Dr. @{{provider_name}} de la @{{practice_name}}. Los médicos le han invitado a su nuevo programa de
+    gestión de atención personalizada y es posible que haya recibido una carta a este efecto.</p>
+
+<p>Dr. @{{provider_name}} piensan que este programa sería muy útil para usted y le gustaría que se inscribiera.
+    Permítame contarle algo sobre este programa y cómo puede ayudarle a mantenerse saludable.</p>
+
+<p>Este es un nuevo programa de atención preventiva de Medicare para ayudarlo a usted y a su médico a
+    cuidar mejor de su salud. Medicare ha decidido que su enfoque de la medicina necesitaba mejoras.
+    Antes, Medicare era reactivo, a menudo esperaba hasta que los pacientes terminaban en el hospital
+    para proporcionarles atención médica, no era bueno para los pacientes y caro para Medicare.</p>
+
+<p>Ahora, Medicare está siendo proactivo: proporcionando atención entre las visitas al médico para
+    asegurarse que sus afecciones están bajo control, está tomando sus medicamentos y no tiene ningún
+    síntoma que pueda estar molestándolo.</p>
+
+<p>He aquí algunos detalles sobre el programa:</p>
+
+<p>
+<li>Un encargado de cuidado personal, una enfermera registrada, le hará una rápida llamada telefónica dos veces al mes,
+    para brindarle apoyo, atención personalizada y para ver cómo está usted
+</li>
+
+<li> También puede dejarnos un mensaje las 24 horas los 7 días de la semana y uno de nuestros encargados de atención se
+    pondrá en contacto con usted en un tiempo razonable
+</li>
+
+<li> Puede retirarse del programa en cualquier momento que desee. Solo llámenos.</li>
+
+<li> Solamente puedes ser parte de un programa de cuidados del Doctor a la vez</li>
+
+<p>¿Puede informarle a su doctor que usted aceptó inscribirse en este programa? (Recuerde que siempre
+    puede retirarse si no le gusta) [Ambassador: Please click the appropriate button based on patient’s
+    answer and follow instructions in subsequent popup forms. Thank you!] </p>
