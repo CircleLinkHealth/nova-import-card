eae639d003ccff24f104100d3c55b46db7a1216f
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-20 15:50:12 -0400

    updates

--- resources/views/partials/calls/make-twilio-call.blade.php
+++ resources/views/partials/calls/make-twilio-call.blade.php
@@ -3,52 +3,53 @@
 
 <div class="container">
 
-    <h2>Support Tickets</h2>
+    <h2>CLHCaller</h2>
 
-    <p class="lead">
-        This is the list of most recent support tickets. Click the "Call customer" button to start a phone call from
-        your browser.
-    </p>
 
-    <div class="row">
-        <div class="col-md-4 col-md-push-8">
-            <div class="panel panel-primary client-controls">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Make a call</h3>
-                </div>
-                <div class="panel-body">
-                    <p><strong>Status</strong></p>
-                    <div class="well well-sm" id="call-status">
-                        Connecting to Twilio...
+    <div id="makeCall">
+        <div class="row">
+            <div class="col-md-4 col-md-push-8">
+                <div class="panel panel-primary client-controls">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Make a call</h3>
                     </div>
+                    <div class="panel-body">
+                        <p><strong>Status</strong></p>
+                        <div class="well well-sm" id="call-status">
+                           @{{ callStatus }}
+                        </div>
 
-                    <button class="btn btn-lg btn-success answer-button" disabled>Answer call</button>
-                    <button class="btn btn-lg btn-danger hangup-button" disabled onclick="hangUp()">Hang up</button>
+                        <button class="btn btn-lg btn-success answer-button" disabled>Answer call</button>
+                        <button class="btn btn-lg btn-danger hangup-button" disabled onclick="hangUp()">Hang up</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-8 col-md-pull-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Ticket
-                        <small class="pull-right"></small>
-                    </h3>
-                </div>
-
-                <div class="panel-body">
-
-                    <div class="pull-right">
-                        <button onclick="callCustomer('+19727622642')" type="button"
-                                class="btn btn-primary btn-lg call-customer-button">
-                            <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>
-                            Call customer
-                        </button>
+            <div class="col-md-8 col-md-pull-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Make Call
+                            <small class="pull-right"></small>
+                        </h3>
                     </div>
 
-                    <p><strong>Name:</strong></p>
-                    <p><strong>Phone number:</strong> 9727622642</p>
-                    <p><strong>Description:</strong></p>
+                    <div class="panel-body">
 
+                        <div class="form-group">
+                            <label for="num">Phone Number:</label>
+                            <input v-model="toCall" id="num" type="text" class="form-control">
+                        </div>
+
+                        <div class="pull-right">
+                            <button v-on:click="call" type="button"
+                                    class="btn btn-primary btn-lg call-customer-button">
+                                <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>
+                                Call customer
+                            </button>
+                        </div>
+
+                        <p><strong>Phone number:</strong> @{{ toCall }}</p>
+
+                    </div>
                 </div>
             </div>
         </div>
@@ -59,3 +60,26 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="//static.twilio.com/libs/twiliojs/1.3/twilio.min.js"></script>
 <script src="{{ asset('js/browser-calls.js', true) }}"></script>
+<script src="https://unpkg.com/vue@2.1.3/dist/vue.js"></script>
+
+<script>
+
+    let app = new Vue({
+
+        el: "#makeCall",
+
+        data: {
+            callStatus: '',
+            toCall: ''
+
+        },
+        methods: {
+            call(){
+                callCustomer(this.toCall)
+            }
+
+        }
+
+    });
+
+</script>
