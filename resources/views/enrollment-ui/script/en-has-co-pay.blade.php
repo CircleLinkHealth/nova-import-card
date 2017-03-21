1c990f940bb28269d4a7d94571e5ac663047e91b
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-21 13:18:46 -0400

    formatting

--- resources/views/enrollment-ui/script/en-has-co-pay.blade.php
+++ resources/views/enrollment-ui/script/en-has-co-pay.blade.php
@@ -1,44 +1,54 @@
-<p><b>ENGLISH [speak clearly and cheerfully]</b>: Hi this is {{auth()->user()->fullName}} calling on behalf of
-    <b>Dr. @{{provider_name}}'s </b>office.
+<div>
+    <p><b>ENGLISH [speak clearly and cheerfully]</b>: Hi this is {{auth()->user()->fullName}} calling on behalf of
+        <b>Dr. @{{provider_name}}'s </b>office.
 
-    How are you doing today?</p>
+        How are you doing today?</p>
 
-<p>I’m fine. The doctor[s] have invited you to their new personalized care management program. You may have received a
-    letter about this. <b> Dr. @{{provider_name}}</b> think(s) that this program would be helpful to your health and would like you
-    to enroll. May I tell you a little about this program?</p>
+    <p>I’m fine. The doctor[s] have invited you to their new personalized care management program. You may have received
+        a
+        letter about this. <b> Dr. @{{provider_name}}</b> think(s) that this program would be helpful to your health and
+        would like you
+        to enroll. May I tell you a little about this program?</p>
 
-<p><i>[Patients interested usually discuss their conditions here.. be sure to listen. If patient asks where calling from,
-        use the practice name in goog sheet, e.g., CareMedica or Ferguson]</i></p>
+    <p><i>[Patients interested usually discuss their conditions here.. be sure to listen. If patient asks where calling
+            from,
+            use the practice name in goog sheet, e.g., CareMedica or Ferguson]</i></p>
 
-<p>This is a new preventative care program from Medicare to help you and your doctor take better care of your
-    health.</p>
+    <p>This is a new preventative care program from Medicare to help you and your doctor take better care of your
+        health.</p>
 
-<p>Before, Medicare was reactive, often waiting until patients were already ill to provide care, which is not great for
-    patients and expensive for Medicare.
+    <p>Before, Medicare was reactive, often waiting until patients were already ill to provide care, which is not great
+        for
+        patients and expensive for Medicare.
 
-    Now, Medicare is being proactive: providing care between doctor’s visits to prevent hospitalization, including
-    making sure your conditions are under control, you are taking your medications, and you don’t have any symptoms that
-    might be bothering you.</p>
+        Now, Medicare is being proactive: providing care between doctor’s visits to prevent hospitalization, including
+        making sure your conditions are under control, you are taking your medications, and you don’t have any symptoms
+        that
+        might be bothering you.</p>
 
-<p>It’s a free program if you are on Medicaid or have Supplemental Insurance. If not, there is a ~$8 co-pay per month.
-    Remember, similar concierge services are up to 5X more expensive. Here’s some details on the program:</p>
+    <p>It’s a free program if you are on Medicaid or have Supplemental Insurance. If not, there is a ~$8 co-pay per
+        month.
+        Remember, similar concierge services are up to 5X more expensive. Here’s some details on the program:</p>
 
-<li> A personal care coach— registered nurse-- will do a quick phone checkin about twice a month,to provide support,
-    personalized care, and to see how you’re doing
-</li>
+    <li> A personal care coach— registered nurse-- will do a quick phone checkin about twice a month,to provide support,
+        personalized care, and to see how you’re doing
+    </li>
 
-<li> The information we collect is available to your doctor, and will be shared with your doctor immediately if you
-    request, or if the nurse judges it appropriate
-</li>
+    <li> The information we collect is available to your doctor, and will be shared with your doctor immediately if you
+        request, or if the nurse judges it appropriate
+    </li>
 
-<li> You can leave a message for us 24/7 and one of our care coaches will get back to you shortly</li>
+    <li> You can leave a message for us 24/7 and one of our care coaches will get back to you shortly</li>
 
-<li> You can withdraw at any time you want. Just give us a call</li>
+    <li> You can withdraw at any time you want. Just give us a call</li>
 
-<li> You can only be part of one doctor’s care management program at a time</li>
+    <li> You can only be part of one doctor’s care management program at a time</li>
 
-<p>Can let your Dr. know you consented to enroll in this program today? (Remember you can always withdraw if you don’t
-    like it)</p>
+    <p>Can let your Dr. know you consented to enroll in this program today? (Remember you can always withdraw if you
+        don’t
+        like it)</p>
 
-<p><i> [Ambassador, Please click the appropriate button based on patient’s answer and follow instructions in subsequent
-        popup forms. Thank you!]</i></p>
+    <p><i> [Ambassador, Please click the appropriate button based on patient’s answer and follow instructions in
+            subsequent
+            popup forms. Thank you!]</i></p>
+</div>
--- resources/views/enrollment-ui/script/es-has-co-pay.blade.php
+++ resources/views/enrollment-ui/script/es-has-co-pay.blade.php
@@ -1,41 +1,51 @@
-<p><b>Speak clearly and cheerfully</b>: Hola, [nombre de la persona que llama] estoy llamando en nombre de
-    los doctores <b>Dr. @{{provider_name}}</b> de la @{{practice_name}}. Los médicos le han invitado a su nuevo programa de
-gestión de atención personalizada y es posible que haya recibido una carta a este efecto.</p>
+<div>
+    <p><b>Speak clearly and cheerfully</b>: Hola, [nombre de la persona que llama] estoy llamando en nombre de
+        los doctores <b>Dr. @{{provider_name}}</b> de la @{{practice_name}}. Los médicos le han invitado a su nuevo
+        programa de
+        gestión de atención personalizada y es posible que haya recibido una carta a este efecto.</p>
 
-<p><b>Dr. @{{provider_name}}</b> piensan que este programa sería muy útil para usted y le gustaría que se inscribiera.
-    Permítame contarle algo sobre este programa y cómo puede ayudarle a mantenerse saludable.</p>
+    <p><b>Dr. @{{provider_name}}</b> piensan que este programa sería muy útil para usted y le gustaría que se
+        inscribiera.
+        Permítame contarle algo sobre este programa y cómo puede ayudarle a mantenerse saludable.</p>
 
-<p>Este es un nuevo programa de atención preventiva de Medicare para ayudarlo a usted y a su médico a cuidar mejor de su
-    salud. Medicare ha decidido que su enfoque de la medicina necesitaba mejoras. Antes, Medicare era reactivo, a menudo
-    esperaba hasta que los pacientes terminaban en el hospital para
-    proporcionarles atención médica, no era bueno para los pacientes y caro para Medicare. Ahora, Medicare está siendo
-    proactivo: proporcionando atención entre las visitas al médico para asegurarse que sus afecciones están bajo
-    control, está tomando sus medicamentos y no tiene ningún síntoma que pueda estar molestándolo.</p>
+    <p>Este es un nuevo programa de atención preventiva de Medicare para ayudarlo a usted y a su médico a cuidar mejor
+        de su
+        salud. Medicare ha decidido que su enfoque de la medicina necesitaba mejoras. Antes, Medicare era reactivo, a
+        menudo
+        esperaba hasta que los pacientes terminaban en el hospital para
+        proporcionarles atención médica, no era bueno para los pacientes y caro para Medicare. Ahora, Medicare está
+        siendo
+        proactivo: proporcionando atención entre las visitas al médico para asegurarse que sus afecciones están bajo
+        control, está tomando sus medicamentos y no tiene ningún síntoma que pueda estar molestándolo.</p>
 
-<p>Ahora, Medicare está siendo proactivo: proporcionando atención entre las visitas al médico para
-    asegurarse que sus afecciones están bajo control, está tomando sus medicamentos y no tiene ningún
-    síntoma que pueda estar molestándolo.</p>
+    <p>Ahora, Medicare está siendo proactivo: proporcionando atención entre las visitas al médico para
+        asegurarse que sus afecciones están bajo control, está tomando sus medicamentos y no tiene ningún
+        síntoma que pueda estar molestándolo.</p>
 
-<p>Es un programa gratuito si usted está en Medicaid o tiene un seguro suplementario.Si no, hay un copago de alrededor
-    de $ 8 por mes. Recuerde, este servicio puede ahorrarle visitas a Atención de Urgencia o a la consulta del Dr.
-    conectándole a una enfermera. El valor entregado está muy por encima de los $ 8 por mes.</p>
+    <p>Es un programa gratuito si usted está en Medicaid o tiene un seguro suplementario.Si no, hay un copago de
+        alrededor
+        de $ 8 por mes. Recuerde, este servicio puede ahorrarle visitas a Atención de Urgencia o a la consulta del Dr.
+        conectándole a una enfermera. El valor entregado está muy por encima de los $ 8 por mes.</p>
 
-<p>He aquí algunos detalles sobre el programa:</p>
+    <p>He aquí algunos detalles sobre el programa:</p>
 
-<p>
-<li>Un encargado de cuidado personal, una enfermera registrada, le hará una rápida llamada telefónica dos veces al mes,
-    para brindarle apoyo, atención personalizada y para ver cómo está usted
-</li>
+    <p>
+    <li>Un encargado de cuidado personal, una enfermera registrada, le hará una rápida llamada telefónica dos veces al
+        mes,
+        para brindarle apoyo, atención personalizada y para ver cómo está usted
+    </li>
 
-<li> También puede dejarnos un mensaje las 24 horas los 7 días de la semana y uno de nuestros encargados de atención se
-    pondrá en contacto con usted en un tiempo razonable
-</li>
+    <li> También puede dejarnos un mensaje las 24 horas los 7 días de la semana y uno de nuestros encargados de atención
+        se
+        pondrá en contacto con usted en un tiempo razonable
+    </li>
 
-<li> Puede retirarse del programa en cualquier momento que desee. Solo llámenos.</li>
+    <li> Puede retirarse del programa en cualquier momento que desee. Solo llámenos.</li>
 
-<li> Solamente puedes ser parte de un programa de cuidados del Doctor a la vez</li>
+    <li> Solamente puedes ser parte de un programa de cuidados del Doctor a la vez</li>
 
-<p>¿Puede informarle a su doctor que usted aceptó inscribirse en este programa? (Recuerde que siempre
-    puede retirarse si no le gusta) </p>
-<p><i>[Ambassador: Please click the appropriate button based on patient’s
-        answer and follow instructions in subsequent popup forms. Thank you!]</i></p>
+    <p>¿Puede informarle a su doctor que usted aceptó inscribirse en este programa? (Recuerde que siempre
+        puede retirarse si no le gusta) </p>
+    <p><i>[Ambassador: Please click the appropriate button based on patient’s
+            answer and follow instructions in subsequent popup forms. Thank you!]</i></p>
+</div>
