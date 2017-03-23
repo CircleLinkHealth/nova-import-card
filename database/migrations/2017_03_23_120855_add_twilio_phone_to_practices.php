fd0e5e5eed7d1acf2eddcf8c13dd4c776c8c9086
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-23 12:11:07 -0400

    adds migration for twilio phone nums to practices

---
+++ database/migrations/2017_03_23_120855_add_twilio_phone_to_practices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTwilioPhoneToPractices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('practices', function (Blueprint $table) {
+
+            $table->string('outgoing_phone_number')->after('external_id')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('practices', function (Blueprint $table) {
+
+            $table->dropColumn('outgoing_phone_number');
+
+        });
+    }
+}
