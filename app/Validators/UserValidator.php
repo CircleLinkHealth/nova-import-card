cc68fa68f6f05ffa58dcc2685a60d32cf0427101
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-25 00:41:06 +0200

    Brings back password validator [deploy:production]

--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -13,7 +13,7 @@ class UserValidator extends LaravelValidator
             'email'      => 'required|email|unique:users,email',
             'first_name' => 'required',
             'last_name'  => 'required',
-            //            'password'   => 'required',
+            'password'   => 'required',
         ],
         ValidatorInterface::RULE_UPDATE => [
             'program_id' => 'exists:practices,id',
