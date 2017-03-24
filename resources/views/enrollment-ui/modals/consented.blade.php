25056df6e0ea1282a04f80cd85e484a13f5ee1e2
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-24 15:08:18 -0400

    bunch of copy changes

--- resources/views/enrollment-ui/modals/consented.blade.php
+++ resources/views/enrollment-ui/modals/consented.blade.php
@@ -6,39 +6,47 @@
         <div class="modal-content">
             <h4 style="color: #47beab">Awesome! Please confirm patient details:</h4>
             <blockquote style="border-left: 5px solid #26a69a;">
+                <span class="consented_title"><b>I.</b></span>
                 @if($enrollee->lang == 'ES')
-                    Ask patient: ¿Quiere quele llamemos directamente o hay alguien más con el cual quiere quenos pongamos en
+                    <b>Ask patient:</b> ¿Quiere quele llamemos directamente o hay alguien más con el cual quiere quenos
+                    pongamos en
                     contacto?
                 @else
-                    <b>Ask patient: Do you want us to call you directly or is there someone else we should contact?</b>
+                    <b>Ask patient:</b> Do you want us to call you directly or is there someone else we should contact?
                 @endif
                 <br>
-                Please confirm the patient's preferred phone number (format: XXX-XXX-XXXX):
+                Confirm the patient’s phone number(s) [format: xxx-xxx-xxxx] and select preferred number with radio button:
+                <br>
             </blockquote>
             <div class="row">
-                <div class="col s6 m3" style="padding-top: 4px;">
-                    <label for="preferred_phone" class="preferred_phone">Preferred Contact Phone</label>
-                    <select name="preferred_phone" id="preferred_phone">
-                        <option value="home" selected>Home Phone</option>
-                        <option value="cell">Cell Phone</option>
-                        <option value="other">Other Phone</option>
-                    </select>
-                </div>
-                <div class="col s6 m3 select-custom">
-                    <label for="home_phone"
+                <div class="col s6 m4 select-custom">
+                    <input name="preferred_phone" type="radio" id="home_radio" value="home"
+                           @if($enrollee->home_phone != '') checked @endif/>
+                    <label for="home_radio"
                            v-bind:class="{valid: home_is_valid, invalid: home_is_invalid}">@{{home_phone_label}}</label>
-                    <input class="input-field" name="home_phone" id="home_phone" v-model="home_phone"/>
+                    <input class="input-field" name="home_phone" id="home_phone" v-model="home_phone"
+                           v-bind:disabled="disableCall"/>
                 </div>
-                <div class="col s6 m3 select-custom">
-                    <label for="cell_phone"
+                <div class="col s6 m4 select-custom">
+                    <input name="preferred_phone" type="radio" id="cell_radio" value="cell"
+                           @if($enrollee->home_phone == '' && $enrollee->cell_phone != '') checked @endif/>
+                    <label for="cell_radio"
                            v-bind:class="{valid: cell_is_valid, invalid: cell_is_invalid}">@{{cell_phone_label}}</label>
                     <input class="input-field" name="cell_phone" id="cell_phone" v-model="cell_phone"/>
                 </div>
-                <div class="col s6 m3 select-custom">
-                    <label for="other_phone"
+                <div class="col s6 m4 select-custom">
+                    <input name="preferred_phone" type="radio" id="other_radio" value="other"
+                           @if($enrollee->home_phone == '' && $enrollee->cell_phone == '' && $enrollee->other_phone != '') checked @endif/>
+                    <label for="other_radio"
                            v-bind:class="{valid: other_is_valid, invalid: other_is_invalid}">@{{other_phone_label}}</label>
                     <input class="input-field" name="other_phone" id="other_phone" v-model="other_phone"/>
                 </div>
+            </div>
+            <div class="row">
+                <blockquote style="border-left: 5px solid #26a69a;">
+                    <span class="consented_title"><b>II.</b></span> Please confirm address and email
+                </blockquote>
+
                 <div class="col s12 m3 select-custom">
                     <label for="address" class="label">Address</label>
                     <input class="input-field" name="address" id="address" v-model="address"/>
@@ -66,9 +74,10 @@
             </div>
             <div class="row">
                 <blockquote style="border-left: 5px solid #26a69a;">
-                    Please confirm the patient's preferred contact days and times:
+                    <span class="consented_title"><b>III.</b></span> Please confirm the patient's preferred contact days and
+                    times, and any other relevant information:
                 </blockquote>
-                <div class="col s12 m6">
+                <div class="col s12 m3">
                     <label for="days[]" class="label">Day</label>
                     <select name="days[]" id="days[]" multiple>
                         <option disabled selected>Select Days</option>
@@ -79,7 +88,7 @@
                         <option value="5">Friday</option>
                     </select>
                 </div>
-                <div class="col s12 m6">
+                <div class="col s12 m3">
                     <label for="times[]" class="label">Times</label>
                     <select name="times[]" id="times[]" multiple>
                         <option disabled selected>Select Times</option>
@@ -88,25 +97,22 @@
                         <option value="15:00-18:00">3PM - 6PM</option>
                     </select>
                 </div>
-            </div>
-            <div class="row input-field">
-                <blockquote style="border-left: 5px solid #26a69a; margin: 10px 0;">
-                    Is there anything else we should know about the patient?
-                </blockquote>
-                <div class="col s12 m12 select-custom">
+                <div class="col s12 m6 select-custom">
                     <input class="materialize-textarea input-field" id="extra" name="extra"
-                           placeholder="Optional additional information" style="margin-bottom: 10px">
+                           placeholder="Optional additional information" style="margin-bottom: 10px; padding-bottom: 18px;">
                 </div>
             </div>
-            <blockquote style="border-left: 5px solid red;">
 
+            <blockquote style="border-left: 5px solid #26a69a;">
+                <span class="consented_title"><b>IV.</b></span>
                 <span style="color: red"><b>TELL PATIENT BEFORE HANGING UP!</b></span><br>
                 @if($enrollee->lang == 'ES')
                     Una enfermera registrada le llamará en breve del mismo desde el cual lo estoy llamando
-                    [number of practice]. Por favor, guárdelo para que acepte la llamada cuando suene el teléfono.
+                    @{{practice_phone}}. Por favor, guárdelo para que acepte la llamada cuando suene el teléfono.
                     ¡Me alegro de haberme conectado! ¡Que tenga un muy buen día!
                 @else
-                    A Registered Nurse will call you shortly from the same # I’m calling from, @{{practice_phone}}. Please save it so you accept the call when she/he rings. So glad we
+                    A Registered Nurse will call you shortly from the same # I’m calling from, @{{practice_phone}}.
+                    Please save it so you accept the call when she/he rings. So glad we
                     connected! Have a great day!
 
                 @endif
@@ -120,7 +126,7 @@
         </div>
         <div class="modal-footer">
             <button id="submit" name="submit" type="submit"
-                    class="modal-action waves-effect waves-light btn">Confirm and Call Next Patient
+                    class="modal-action waves-effect waves-light btn">Confirm and call next patient
             </button>
             <div v-if="onCall === true" style="text-align: center">
                 <a v-on:click="hangUp" class="waves-effect waves-light btn" style="background: red"><i
