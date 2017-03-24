d64d11f496a0056f7ccfdb141ab6d78d5ba23af5
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-24 22:46:56 +0200

    Shrinks footer

--- resources/views/patient/signup.blade.php
+++ resources/views/patient/signup.blade.php
@@ -546,30 +546,30 @@ Medications List:
         </form>
     </div>
 
-    <footer class="page-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text">Footer Content</h5>
-                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your
-                        footer
-                        content.</p>
-                </div>
-                <div class="col l4 offset-l2 s12">
-                    <h5 class="white-text">Olark live chat, maybe?</h5>
+    <footer class="page-footer" style="padding-top: 0;">
+        {{--<div class="container">--}}
+        {{--<div class="row">--}}
+        {{--<div class="col l6 s12">--}}
+        {{--<h5 class="white-text">Footer Content</h5>--}}
+        {{--<p class="grey-text text-lighten-4">You can use rows and columns here to organize your--}}
+        {{--footer--}}
+        {{--content.</p>--}}
+        {{--</div>--}}
+        {{--<div class="col l4 offset-l2 s12">--}}
+        {{--<h5 class="white-text">Olark live chat, maybe?</h5>--}}
                     {{--<ul>--}}
                     {{--<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>--}}
                     {{--<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>--}}
                     {{--<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>--}}
                     {{--<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>--}}
                     {{--</ul>--}}
-                </div>
-            </div>
-        </div>
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
         <div class="footer-copyright">
             <div class="container">
                 © 2017 CicleLink Health
-                <a class="grey-text text-lighten-4 right" href="#!">Call CLH maybe?</a>
+                {{--<a class="grey-text text-lighten-4 right" href="#!">Call CLH maybe?</a>--}}
             </div>
         </div>
     </footer>
