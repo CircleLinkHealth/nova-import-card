da948acad238609c3f37a7b6afcaf6e879c7c101
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-31 16:59:30 -0400

    adds updates to create view for nurse invoice generator

--- resources/views/billing/practice/create.blade.php
+++ resources/views/billing/practice/create.blade.php
@@ -4,13 +4,13 @@
 
     <script>
         $(document).ready(function () {
-            $(".nurses").select2();
+            $(".practices").select2();
 
         });
     </script>
 
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    {!! Form::open(array('url' => URL::route('admin.reports.nurse.generate', array()),'class' => 'form-horizontal')) !!}
+    {!! Form::open(array('url' => URL::route('practice.billing.make', array()),'class' => 'form-horizontal')) !!}
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -23,11 +23,11 @@
                             <fieldset>
 
                                 <div class="form-group">
-                                    <label class="col-md-2 control-label" for="days">
+                                    <label class="col-md-2 control-label" for="practices">
                                         Select Practices<br>
                                         Select All <kbd><kbd>cmd</kbd> + <kbd>A</kbd></kbd></label>
                                     <div class="col-md-6">
-                                        <select id="nurse" name="nurses[]" class="nurses dropdown Valid form-control" multiple required>
+                                        <select id="practices" name="practices[]" class="practices dropdown Valid form-control" multiple required>
                                             @foreach($practices as $practice)
                                                 <option value="{{$practice->id}}">{{$practice->display_name}}</option>
                                             @endforeach
@@ -39,7 +39,7 @@
                                     <label class="col-md-2 control-label" for="end_date">Month</label>
                                     <div class="col-md-6">
                                         <input class="form-control" type="date"
-                                               value="{{\Carbon\Carbon::now()->firstOfMonth()->toDateString()}}" name="start"
+                                               value="2017-03-01" name="start"
                                                id="end_date" disabled>
                                     </div>
                                 </div>
