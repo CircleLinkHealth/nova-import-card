d4f4d63ead129e98791f2956cdc9efc37acd6a85
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-31 13:17:59 -0400

    adds actor id and rejected to patient monthly summaries.

---
+++ database/migrations/2017_03_31_125045_add_rejected_to_patient_monthly_summaries.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRejectedToPatientMonthlySummaries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patient_monthly_summaries', function (Blueprint $table) {
+
+            $table->boolean('rejected')->after('approved')->default(0);
+            $table->unsignedInteger('actor_id')->after('rejected')->nullable();
+
+            $table->foreign('actor_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('patient_monthly_summaries', function (Blueprint $table) {
+
+            $table->dropColumn('rejected');
+            $table->dropColumn('actor_id');
+
+        });
+    }
+}
