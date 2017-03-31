219ff503e215d1d813f6148c0e71d94dafe244a8
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-03-31 14:38:08 -0400

    adds actice status to practices

---
+++ database/migrations/2017_03_31_141507_add_active_practice_bool_to_practices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddActivePracticeBoolToPractices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('practices', function (Blueprint $table) {
+
+            $table->boolean('active')->after('display_name')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('practices', function (Blueprint $table) {
+
+            $table->dropColumn('active');
+
+        });
+    }
+}
