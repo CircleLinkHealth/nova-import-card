987d0107b1b223a1b85d5e146f2f84603c2b5836
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-31 18:07:54 +0200

    removes Footer #103 [deploy:production]

--- resources/views/patient/signup.blade.php
+++ resources/views/patient/signup.blade.php
@@ -184,14 +184,6 @@
             </div>
         </div>
     </div>
-
-    <footer class="page-footer teal" style="padding-top: 0;">
-        <div class="footer-copyright">
-            <div class="container">
-                © 2017 CicleLink Health
-            </div>
-        </div>
-    </footer>
 @endsection
 
 @section('scripts')
