5593d2075ea55fb8409d3bed43a0e5563341b078
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-03-31 10:04:15 +0200

    More changes #103

--- resources/views/patient/signup.blade.php
+++ resources/views/patient/signup.blade.php
@@ -12,7 +12,7 @@
 
         .overlay {
             padding-top: 5%;
-            height: 410px;
+            height: 535px;
         }
 
         .header {
@@ -57,23 +57,74 @@
             </nav>
         </div>
 
-        <div class="col s12 blue lighten-1">
-            <div class="row overlay"
+        <div class="row blue lighten-1">
+
+            <div class="col s4 white">
+                {!! Form::open(['url' => route('sign-up.store'), 'method' => 'post', 'class' => 'col s12', 'id' => 'signup-form']) !!}
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="name" type="text" class="validate" required name="name">
+                        <label for="name" id="name_label">Name</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="text" type="text" class="validate"
+                               pattern="\d{3}[\-]\d{3}[\-]\d{4}" required name="phone">
+                        <label for="text" data-error="The phone number must match format xxx-xxx-xxxx">Phone Number
+                            xxx-xxx-xxxx</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">email</i>
+                        <input id="email" type="email" name="email" class="validate">
+                        <label for="email" data-error="The email must contain an @">Email</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">message</i>
+                        <textarea id="textarea1" class="materialize-textarea" name="comment"
+                                  placeholder=""></textarea>
+                        <label for="textarea1">Message (Optional)</label>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12 center">
+                            <button type="submit"
+                                    class="waves-effect waves-light btn-large scroll-to-form teal accent-3">Sign me up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                {!! Form::close() !!}
+            </div>
+
+            <div class="col s8 overlay valign-wrapper"
                  style="background: url('http://www.circlelinkhealth.com/wp-content/themes/CLH_132/images/bg.png')">
 
-                <h1 style="line-height: 110%;font-size: 1.64rem;margin: .82rem 0 .656rem;" class="white-text center"><b>WELLNESS
-                        MANAGER</b> <em>from CircleLink Health</em></h1>
+                <div class="valign">
+                    <h1 style="line-height: 110%;font-size: 1.64rem;margin: .82rem 0 .656rem;"
+                        class="white-text center"><b>WELLNESS
+                            MANAGER</b> <em>from CircleLink Health</em></h1>
 
-                <br>
-
-                <h5 class="white-text center">Let our registered nurse care coaches take the stress out of chronic
-                    condition for you <u>or</u> a loved one.</h5>
-
-                <div class="center" style="margin-top: 4%;">
-                    <h5 class="teal-text text-accent-1 center">Telephone health coaching, care plan setup and
-                        coordination with family and doctors. Just $49.99/month!</h5>
                     <br>
-                    <a class="waves-effect waves-light btn-large scroll-to-form teal accent-3">Sign me up</a>
+
+                    <h5 class="white-text center">Let our registered nurse care coaches take the stress out of chronic
+                        condition for you <u>or</u> a loved one.</h5>
+
+                    <div class="center" style="margin-top: 4%;">
+                        <h5 class="teal-text text-accent-1 center">Telephone health coaching, care plan setup and
+                            coordination with family and doctors. Just $49.99/month!</h5>
+                    </div>
                 </div>
             </div>
 
@@ -103,84 +154,11 @@
                 </div>
             </div>
         </div>
-
-        {!! Form::open(['url' => route('sign-up.store'), 'method' => 'post', 'class' => 'col s12', 'id' => 'signup-form']) !!}
-        <div class="section container">
-            <h1 class="center">Sign Up for Wellness Manager</h1>
-
-            <br>
-            <br>
-
-            <div class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">account_circle</i>
-                    <input id="name" type="text" class="validate" required name="name">
-                    <label for="name" id="name_label"> Name</label>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">phone</i>
-                    <input id="text" type="text" class="validate"
-                           pattern="\d{3}[\-]\d{3}[\-]\d{4}" required name="phone">
-                    <label for="text" data-error="The phone number must match format xxx-xxx-xxxx">Phone Number
-                        xxx-xxx-xxxx</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">email</i>
-                    <input id="email" type="email" name="email" class="validate">
-                    <label for="email" data-error="The email must contain an @">Email</label>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">message</i>
-                    <textarea id="textarea1" class="materialize-textarea" name="comment"
-                              placeholder=""></textarea>
-                    <label for="textarea1">Message (Optional)</label>
-                </div>
-
-                <div class="row">
-                    <div class="input-field col s12 center">
-                        <button class="btn-large waves-effect waves-light blue center" type="submit" name="action">
-                            Schedule a call
-                            <i class="material-icons right">send</i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {!! Form::close() !!}
-
     </div>
-
-    <footer class="page-footer teal" style="padding-top: 0;">
-        <div class="footer-copyright">
-            <div class="container">
-                © 2017 CicleLink Health
-            </div>
-        </div>
-    </footer>
-
-
 @endsection
 
 @section('scripts')
-    <script>
-        $(document).ready(function () {
-            $(".scroll-to-form").click(function () {
-                $('html, body').animate({
-                    scrollTop: $("#signup-form").offset().top
-                }, 600);
 
-                $("#name_label").trigger("click");
-            });
-        });
-    </script>
 @endsection
 
 
