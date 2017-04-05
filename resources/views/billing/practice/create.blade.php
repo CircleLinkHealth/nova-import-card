8b09fa4335a0a5b805eee9a761a9a122457d5f77
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-04-05 08:27:29 -0500

    beautification of billing invoice maker.

--- resources/views/billing/practice/create.blade.php
+++ resources/views/billing/practice/create.blade.php
@@ -14,21 +14,20 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-
                 <div class="panel panel-default">
                     <div class="panel-heading">Practice Bill Generator</div>
                     <div class="panel-body">
                         <form class="form-horizontal">
                             {{ csrf_field() }}
                             <fieldset>
-
                                 <div class="form-group">
                                     <label class="col-md-2 control-label" for="practices">
                                         Select Practices<br>
                                         Select All <kbd><kbd>cmd</kbd> + <kbd>A</kbd></kbd></label>
                                     <div class="col-md-6">
-                                        <select id="practices" name="practices[]" class="practices dropdown Valid form-control" multiple required>
-                                            @foreach($practices as $practice)
+                                        <select id="practices" name="practices[]"
+                                                class="practices dropdown Valid form-control" multiple required>
+                                            @foreach($readyToBill as $practice)
                                                 <option value="{{$practice->id}}">{{$practice->display_name}}</option>
                                             @endforeach
                                         </select>
@@ -37,29 +36,37 @@
 
                                 <div class="form-group">
                                     <label class="col-md-2 control-label" for="end_date">Month</label>
-                                    <div class="col-md-6">
+                                    <div class="col-md-4">
                                         <input class="form-control" type="date"
                                                value="2017-03-01" name="start"
                                                id="end_date" disabled>
                                     </div>
-                                </div>
-
-                                <!-- Button -->
-                                <div class="form-group">
-                                    <div class="row" style="padding-left: 12px;">
-                                        <label class="col-md-2 control-label" for="end_date"></label>
-                                        <div class="col-md-2">
-                                            <button id="submit" name="submit" value="download" class="btn btn-success">Download
-                                                Invoice (s)
-                                            </button>
-                                        </div>
+                                    <div class="col-md-2">
+                                    <button id="submit" name="submit" value="download"
+                                            class="btn btn-success">
+                                        Download Invoice (s)
+                                    </button>
                                     </div>
                                 </div>
-
-
                             </fieldset>
                         </form>
 
+                        <hr>
+
+                        <div class="row" style="padding-left: 30px;">
+
+                            List of Practices with pending QA:
+                            <ul>
+
+                                @foreach($needsQA as $practice)
+
+                                    <li><a href="#">{{$practice->display_name}}</a></li>
+
+                                @endforeach
+
+                            </ul>
+
+                        </div>
                     </div>
                 </div>
             </div>
