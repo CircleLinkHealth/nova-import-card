5250ba531a192c1da994a26c73a1393b35b02187
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-04-12 12:41:19 -0400

    adds invoice num to app config

---
+++ database/seeds/TrackBillingInvoiceNo.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TrackBillingInvoiceNo extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        \App\AppConfig::create([
+
+            'config_key' => 'billing_invoice_count',
+            'config_value' => 1163
+
+        ]);
+
+    }
+}
