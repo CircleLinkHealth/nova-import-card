045845fbb0a555bfb5ba4fbba66607456995ac13
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-04-13 12:14:38 -0400

    adds migration

---
+++ database/migrations/2017_04_13_121227_add_codes_to_monthly_billing.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCodesToMonthlyBilling extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patient_monthly_summaries', function (Blueprint $table) {
+
+            $table->text('billable_problem1_code')->after('billable_problem1');
+            $table->text('billable_problem2_code')->after('billable_problem2');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('patient_monthly_summaries', function (Blueprint $table) {
+
+            $table->dropColumn('billable_problem1_code');
+            $table->dropColumn('billable_problem2_code');
+
+        });
+    }
+}
