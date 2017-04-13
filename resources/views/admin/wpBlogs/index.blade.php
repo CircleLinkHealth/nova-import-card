fc5dce48e4617ed7c88162abcd11c5992ed2cea9
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-04-13 10:16:44 -0400

    shows only active programs

--- resources/views/admin/wpBlogs/index.blade.php
+++ resources/views/admin/wpBlogs/index.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-10 col-md-offset-1">
                 <div class="row">
                     <div class="col-sm-8">
-                        <h1>Programs</h1>
+                        <h1>Practice</h1>
                     </div>
                     @if(Entrust::can('programs-manage'))
                         <div class="col-sm-4">
@@ -19,7 +19,7 @@
                     @endif
                 </div>
                 <div class="panel panel-default">
-                    <div class="panel-heading">Programs</div>
+                    <div class="panel-heading">Practice</div>
                     <div class="panel-body">
                         @include('errors.errors')
                         <table class="table table-striped">
