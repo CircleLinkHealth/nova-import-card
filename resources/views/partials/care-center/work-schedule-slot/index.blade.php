51490cc5435e7dc2f5dd497f1442509e167c3378
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-04-26 19:12:11 +0300

    Displays holidays this week #67

--- resources/views/partials/care-center/work-schedule-slot/index.blade.php
+++ resources/views/partials/care-center/work-schedule-slot/index.blade.php
@@ -18,26 +18,34 @@
             <dl class="dl-horizontal">
                 <dt>{{ucfirst($day)}}</dt>
                 <dd>
-                    @foreach($windows as $window)
-                        @if (strcasecmp(clhDayOfWeekToDayName($window->day_of_week), $day) == 0)
-                            <div class="col-md-2 list-group-item text-center">
-                                <b>
-                                    {{ Carbon\Carbon::parse($window->window_time_start)->format('H:i') }}
-                                </b> -
-                                <b>
-                                    {{ Carbon\Carbon::parse($window->window_time_end)->format('H:i') }}
-                                </b>
+                    @if (in_array($day, $holidaysThisWeek))
+                        <div class="col-md-12 list-group-item text-center">
+                            <b>
+                                HOLIDAY!
+                            </b>
+                        </div>
+                    @else
+                        @foreach($windows as $window)
+                            @if (strcasecmp(clhDayOfWeekToDayName($window->day_of_week), $day) == 0)
+                                <div class="col-md-2 list-group-item text-center">
+                                    <b>
+                                        {{ Carbon\Carbon::parse($window->window_time_start)->format('H:i') }}
+                                    </b> -
+                                    <b>
+                                        {{ Carbon\Carbon::parse($window->window_time_end)->format('H:i') }}
+                                    </b>
 
-                                &nbsp;
+                                    &nbsp;
 
-                                <a href="{{ route('care.center.work.schedule.destroy', $window->id) }}"
-                                   onclick="return confirm('Are you sure you want to delete this slot?')"
-                                   id="delete-window-{{$window->id}}">
-                                    <i class="glyphicon glyphicon-trash"></i>
-                                </a>
-                            </div>
-                        @endif
-                    @endforeach
+                                    <a href="{{ route('care.center.work.schedule.destroy', $window->id) }}"
+                                       onclick="return confirm('Are you sure you want to delete this slot?')"
+                                       id="delete-window-{{$window->id}}">
+                                        <i class="glyphicon glyphicon-trash"></i>
+                                    </a>
+                                </div>
+                            @endif
+                        @endforeach
+                    @endif
                 </dd>
             </dl>
         </div>
