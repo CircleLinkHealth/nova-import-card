449e8fa84eab357e91bb47b371aa3e895075f90e
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-05-02 15:21:28 -0400

    invoice mail view

---
+++ resources/views/billing/practice/mail.blade.php
@@ -0,0 +1,24 @@
+<p>Thank you for choosing CircleLink for your CCM program!</p>
+
+<p>An invoice for the recently past month is attached, and please click <a href='{{$link}}'>here</a> to see a billing
+    report with information needed to bill Medicare.</p>
+
+<p>We also have some billing tips below.</p>
+
+<p>Please let us know if you need anything else.</p>
+
+<p>Thanks! <br>
+
+    CircleLink Team</p>
+
+<p>Chronic Care Management Billing Tips:<br>
+<ul>
+    <li><b>Date of Service:</b> We recommend the last business day of billing month
+    <li><b>Billing Location:</b> Billing provider’s location. CMS: “where the billing practitioner would furnish a
+        face-to-face
+        office visit with the patient”
+    <li><b>Include a Chronic Condition:</b> The billing report [“billing report” links to billing report] has a
+        condition for
+        each patient that we recommend submitting with your claim to Medicare
+    <li><b>Billing Provider:</b> Primary care provider of patient. Always use an MD if possible
+</ul> </p>
