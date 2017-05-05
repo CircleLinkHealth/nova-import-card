5df1abb36078120edd2f202d880ebba144f01881
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-05-05 14:05:49 -0400

    updates email copy

--- resources/views/billing/practice/mail.blade.php
+++ resources/views/billing/practice/mail.blade.php
@@ -1,14 +1,12 @@
 <p>Thank you for choosing CircleLink for your CCM program!</p>
 
-<p>A CircleLink invoice for the most recent month is attached.</p>
+<p>A CircleLink invoice for the most recent month is attached and a billing report for Medicare can be downloaded <a href="{{$link}}"> here</a>. (Login required) </p>
 
-<p>For a billing report with patient information Medicare will request, please click <a href="{{$link}}"> here </a>  to download. Note:
-    login required (authorized users only).</p>
+<p><i>FYI your username is your practice email address, and you can obtain or reset passwords by clicking “Lost/Need a password...?” per this screenshot:</i></p>
 
-<p>As a reminder, your username is your practice/health organization e-mail address and you can obtain or reset your
-    password by clicking “Lost/Need a password...?”.</p>
+<p style="text-align: center"><img style="width: 295px" src="{{ $message->embed(asset('/img/forgot.png')) }}"></p>
 
-<p>We also have some billing tips below.</p>
+<p>Finally, we have some billing tips below.</p>
 
 <p>Please let us know if you need anything else!</p>
 
@@ -23,10 +21,9 @@
     <li><b>Billing Location:</b> Billing provider’s location. CMS: “where the billing practitioner would furnish a
         face-to-face
         office visit with the patient”
-    <li><b>Include a Chronic Condition:</b> The <a href="{{$link}}"> billing report </a> has a
+    <li><b>Include a Chronic Condition:</b> The <a href="{{$link}}">billing report</a> has a
         condition for
         each patient that we recommend submitting with your claim to Medicare
     <li><b>Billing Provider:</b> Primary care provider of patient. Always use an MD if possible
 </ul>
 </p>
-
