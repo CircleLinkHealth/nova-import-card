968af8f644edc29deee3494e4113efd211c7797e
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-05-08 18:38:08 +0300

    Fixes first checkbox of #176

--- resources/assets/js/components/CareTeam/care-person.js
+++ resources/assets/js/components/CareTeam/care-person.js
@@ -55,7 +55,7 @@ var carePerson = Vue.component('carePerson', {
                 patientId: this.patientId,
             }).then(function (response) {
                 this.$set('care_person.id', response.data.carePerson.id);
-                this.$set('care_person.formatted_type', response.data.carePerson.type);
+                // this.$set('care_person.formatted_type', response.data.carePerson.type);
                 $("#editCareTeamModal-" + id).modal('hide');
 
                 if (response.data.oldBillingProvider) {
