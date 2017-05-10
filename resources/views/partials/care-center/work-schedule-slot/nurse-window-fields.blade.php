7589de56ed4f17d1d5559561c2c64a1f107a6f44
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-05-10 15:19:44 +0300

    Fixes select box #179

--- resources/views/partials/care-center/work-schedule-slot/nurse-window-fields.blade.php
+++ resources/views/partials/care-center/work-schedule-slot/nurse-window-fields.blade.php
@@ -3,7 +3,11 @@
     <div class="col-md-4">
         <select class="form-control" name="day_of_week" required>
             @foreach(weekDays() as $key => $day)
-                <option value="{{carbonToClhDayOfWeek($key)}}">{{$day}}</option>
+                @if(!isset($window))
+                    <option value="{{carbonToClhDayOfWeek($key)}}">{{$day}}</option>
+                @else
+                    <option value="{{carbonToClhDayOfWeek($key)}}" {{$window->day_of_week == carbonToClhDayOfWeek($key) ? 'selected' : '' }}>{{$day}}</option>
+                @endif
             @endforeach
         </select>
     </div>
@@ -12,7 +16,7 @@
         <input class="form-control" name="window_time_start"
                type="time" data-field="time"
                id="window_time_start"
-               value="{{ old('window_time_start') ? old('window_time_start') : '09:00' }}"
+               value="{{ old('window_time_start') ? old('window_time_start') : isset($window) ? $window->window_time_start : '09:00' }}"
                required>
     </div>
 
@@ -20,7 +24,7 @@
         <input class="form-control" name="window_time_end"
                type="time" data-field="time"
                id="window_time_end"
-               value="{{ old('window_time_end') ?old('window_time_end') : '17:00' }}"
+               value="{{ old('window_time_end') ? old('window_time_end') : isset($window) ? $window->window_time_end : '17:00' }}"
                required>
     </div>
 
