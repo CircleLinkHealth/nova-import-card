404ec47c4301f8e4aae1c8a87129d39853aee62b
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-05-17 13:29:32 +0300

    Adds down method #203

--- database/migrations/2017_05_16_164957_rename_tab_data_fields.php
+++ database/migrations/2017_05_16_164957_rename_tab_data_fields.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class RenameTabDataFields extends Migration
 {
@@ -28,7 +28,9 @@ class RenameTabDataFields extends Migration
     public function down()
     {
         Schema::table('tabular_medical_records', function (Blueprint $table) {
-            //
+            $table->renameColumn('allergies_string', 'allergies');
+            $table->renameColumn('medications_string', 'medications');
+            $table->renameColumn('problems_string', 'problems');
         });
     }
 }
