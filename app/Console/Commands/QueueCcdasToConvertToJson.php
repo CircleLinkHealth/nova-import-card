3b87b1471d3e0eb7abbba8cd4a3462769457b13d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-05-30 18:00:22 +0300

    Bump the rate up ti 500 per minute #234

--- app/Console/Commands/QueueCcdasToConvertToJson.php
+++ app/Console/Commands/QueueCcdasToConvertToJson.php
@@ -41,7 +41,7 @@ class QueueCcdasToConvertToJson extends Command
     {
         $ccdas = Ccda::where('json', '=', '')
             ->orWhereNull('json')
-            ->take(100)
+            ->take(500)
             ->get()
             ->map(function ($ccda) {
                 dispatch(new ConvertCcdaToJson($ccda));
