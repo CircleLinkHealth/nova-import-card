56f84db29b5cda501e29c0754a972d194cd42b98
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-06-06 00:07:19 +0300

    Major optimization #234

--- app/Console/Commands/QueueCcdaToDetermineEnrollmentEligibility.php
+++ app/Console/Commands/QueueCcdaToDetermineEnrollmentEligibility.php
@@ -45,15 +45,7 @@ class QueueCcdaToDetermineEnrollmentEligibility extends Command
 
         $ccdas = Ccda::where([
             ['status', '=', Ccda::DETERMINE_ENROLLEMENT_ELIGIBILITY],
-//            ['referring_provider_name', '=', 'Angela Snow'],
         ])->whereNotNull('mrn')->take(500)->get(['id', 'referring_provider_name'])
-            //            ->whereIn('referring_provider_name', [
-//                'Michael Alexander',
-//                'Juan Perez',
-//                'Bradley Chastant MD',
-//                'Bradley Chastant',
-//                'Angela Snow',
-//            ])
             ->map(function ($ccda) {
 //                $job = (new DetermineCcdaEnrollmentEligibility($ccda))->onQueue('ccda-determine-eligibility');
                 dispatch(new DetermineCcdaEnrollmentEligibility($ccda));
