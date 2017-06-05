1a1ca37f72f894bd538378bdd04506dc3a93fc5a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-06-06 00:53:32 +0300

    Try 3000/min #234

--- app/Console/Commands/QueueCcdasToProcess.php
+++ app/Console/Commands/QueueCcdasToProcess.php
@@ -42,7 +42,7 @@ class QueueCcdasToProcess extends Command
     {
         $ccdas = Ccda::where('status', '=', Ccda::DETERMINE_ENROLLEMENT_ELIGIBILITY)
             ->whereNull('mrn')
-            ->take(1000)
+            ->take(3000)
             ->get(['id'])
             ->map(function ($ccda) {
                 $job = (new ProcessCcda($ccda))
