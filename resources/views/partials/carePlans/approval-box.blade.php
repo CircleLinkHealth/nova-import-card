d589f382a633a796f6c8c85d2a2d1f3ded2e50f5
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-06-23 18:17:15 +0300

    Breaks up approval box into partial #343

---
+++ resources/views/partials/carePlans/approval-box.blade.php
@@ -0,0 +1,12 @@
+@if($patient->carePlan->provider_approver_id && $patient->carePlan->provider_date)
+    <div class="col-xs-12">
+        <div class="pull-right print-row text-right">
+            Approved on: {{$patient->carePlan->provider_date->format('m/d/Y')}}
+            at {{$patient->carePlan->provider_date->setTimezone($patient->timezone ?? 'America/New_York')->format('H:i')}} {{$patient->carePlan->provider_date->setTimezone($patient->timezone ?? 'America/New_York')->format('T')}}
+        </div>
+        <br><br>
+        <div class="pull-right print-row text-right">
+            Approved by: {{App\User::find($patient->carePlan->provider_approver_id)->fullName}}
+        </div>
+    </div>
+@endif
