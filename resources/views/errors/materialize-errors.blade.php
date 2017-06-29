d13f76573387160a350725d83e9d41e791f014ec
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-06-29 16:37:55 +0300

    Material Errors improvements #200

--- resources/views/errors/materialize-errors.blade.php
+++ resources/views/errors/materialize-errors.blade.php
@@ -1,10 +1,10 @@
 @if (isset($errors))
     @if (count($errors) > 0)
         <div class="red lighten-5 red-text text-darken-5" style="padding: 5%;">
-            <strong>We have some regarding with your input.</strong><br><br>
+            <strong>We have some concerns regarding with your input.</strong><br><br>
             <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                    <li style="padding-bottom: 2%;">{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
