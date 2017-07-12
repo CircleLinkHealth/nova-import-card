993bb7af4f91c3b235bacaefdbafd78d3604a3f7
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-12 15:20:31 +0300

    Cleans up search template #192

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,17 +1,3 @@
-
-{{--<style>--}}
-
-    {{--.searchbox-label{--}}
-        {{--position: relative;--}}
-        {{--z-index: 99;--}}
-        {{--top: 20px;--}}
-        {{--color: #50b2e2;--}}
-        {{--padding-left: 10px;--}}
-        {{--font-size: 16px;--}}
-    {{--}--}}
-
-{{--</style>--}}
-
 <label class="searchbox-label" for="patients"></label>
 <div id="bloodhound">
     <input style="margin-top: -9px; margin-bottom: -41px; width: auto;"
@@ -20,42 +6,45 @@
         placeholder="Enter a Patient Name, MRN or DOB (mm-dd-yyyy)">@endif
 
 </div>
-<script>
 
-    jQuery(document).ready(function ($) {
-        var pat = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('search'),
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            remote: {
-                url: {!! route('patients.query') !!},
-                wildcard: '%QUERY'
-            }
+@section('scripts')
+    <script>
+
+        jQuery(document).ready(function ($) {
+            var pat = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('search'),
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                remote: {
+                    url: {!! route('patients.query') !!},
+                    wildcard: '%QUERY'
+                }
+            });
+
+            pat.initialize();
+
+            $('#bloodhound .typeahead').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 3
+            }, {
+                source: pat.ttAdapter(),
+                // This will be appended to "tt-dataset-" to form the class name of the suggestion menu.
+                name: 'User_list',
+                // the key from the array we want to display (name,id,email,etc...)
+                displayKey: 'hint',
+                limit: 50,
+                templates: {
+                    empty: [
+                        '<div class="empty-message">No Patients Found</div>'
+                    ]
+                }
+            });
+
+            $('#bloodhound .typeahead').on('typeahead:selected', function (e, datum) {
+                window.location.href = datum.link;
+                datum.val(datum.name);
+            });
         });
 
-        pat.initialize();
-
-        $('#bloodhound .typeahead').typeahead({
-            hint: true,
-            highlight: true,
-            minLength: 3
-        },{
-            source: pat.ttAdapter(),
-            // This will be appended to "tt-dataset-" to form the class name of the suggestion menu.
-            name: 'User_list',
-            // the key from the array we want to display (name,id,email,etc...)
-            displayKey: 'hint',
-            limit: 50,
-            templates: {
-                empty: [
-                    '<div class="empty-message">No Patients Found</div>'
-                ]
-            }
-        });
-
-        $('#bloodhound .typeahead').on('typeahead:selected', function (e, datum) {
-            window.location.href = datum.link;
-            datum.val(datum.name);
-        });
-    });
-
-</script>
+    </script>
+@endsection
