6606a6e1e1b119c29371252854f1b276d88a48d9
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-12 13:46:34 +0300

    Adds variables #192

--- resources/views/wpUsers/patient/careplan/print/careteam.blade.php
+++ resources/views/wpUsers/patient/careplan/print/careteam.blade.php
@@ -1,4 +1,11 @@
-@include('vue-templates.care-team')
+{{--Declare any variables the component may need here--}}
+{{--In this case I need routes to be able to delete multiple components--}}
+<meta name="provider-destroy-route" content="{{ route('care-team.destroy', ['id'=>'']) }}">
+
+<meta name="provider-update-route" content="{{ route('care-team.update', ['id'=>'']) }}">
+<meta name="providers-search" content="{{ route('providers.search') }}">
+<meta name="created_by" content="{{auth()->user()->id}}">
+<meta name="patient_id" content="{{$patient->id}}">
 
 <care-team v-bind:care-team-collection="careTeamCollection"></care-team>
 
