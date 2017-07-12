2884814b856056514d171f1339882d069e1745c4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-12 13:48:15 +0300

    migrate to select2 component #192

--- resources/views/provider/partials/forwardCareplanApprovalEmails.blade.php
+++ resources/views/provider/partials/forwardCareplanApprovalEmails.blade.php
@@ -1,8 +1,4 @@
-<select v-select2="newUser.forward_careplan_approval_emails_to.user_id" style="width: 100%;">
+<select2 :options="newUsers" :selected="newUser.forward_careplan_approval_emails_to.user_id" v-model="newUser.forward_careplan_approval_emails_to.user_id" style="width: 100%;">
     <option value="" v-bind:selected="!newUser.forward_careplan_approval_emails_to.user_id">Select existing Staff Member.
     </option>
-
-    <option v-for="user in newUsers" v-if="user.id != newUser.id"
-            v-bind:selected="newUser.forward_careplan_approval_emails_to.user_id == user.id"
-            value="@{{ user.id }}">@{{ user.first_name }} @{{ user.last_name }}</option>
-</select>
+</select2>
