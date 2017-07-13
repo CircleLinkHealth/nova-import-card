09ecf1f4e3ec975cb628b66234d19440f5c4863d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 07:59:35 +0300

    removes component #192

--- public/js/app-provider-ui.js
+++ public/js/app-provider-ui.js
@@ -21705,112 +21705,9 @@ function applyToTag (styleElement, obj) {
 
 /***/ }),
 /* 36 */,
-/* 37 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var disposed = false
-var Component = __webpack_require__(8)(
-  /* script */
-  __webpack_require__(38),
-  /* template */
-  __webpack_require__(39),
-  /* styles */
-  null,
-  /* scopeId */
-  null,
-  /* moduleIdentifier (server only) */
-  null
-)
-Component.options.__file = "/Users/michalis/Code/CLH/cpm-api/resources/assets/js/components/src/material-select.vue"
-if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key.substr(0, 2) !== "__"})) {console.error("named exports are not supported in *.vue files.")}
-if (Component.options.functional) {console.error("[vue-loader] material-select.vue: functional components are not supported with templates, they should use render functions.")}
-
-/* hot reload */
-if (false) {(function () {
-  var hotAPI = require("vue-hot-reload-api")
-  hotAPI.install(require("vue"), false)
-  if (!hotAPI.compatible) return
-  module.hot.accept()
-  if (!module.hot.data) {
-    hotAPI.createRecord("data-v-8d31995c", Component.options)
-  } else {
-    hotAPI.reload("data-v-8d31995c", Component.options)
-  }
-  module.hot.dispose(function (data) {
-    disposed = true
-  })
-})()}
-
-module.exports = Component.exports
-
-
-/***/ }),
-/* 38 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-//
-//
-//
-//
-
-/* harmony default export */ __webpack_exports__["default"] = ({
-    props: ['value'],
-    watch: {
-        value: function value(_value) {
-
-            this.reload(_value);
-        }
-    },
-    methods: {
-        reload: function reload(value) {
-
-            var select = $(this.$el);
-
-            select.val(value || this.value);
-            select.material_select('destroy');
-            select.material_select();
-        }
-    },
-    mounted: function mounted() {
-
-        var vm = this;
-        var select = $(this.$el);
-
-        select.val(this.value).on('change', function () {
-
-            vm.$emit('input', this.value);
-        });
-
-        select.material_select();
-    },
-    updated: function updated() {
-
-        this.reload();
-    },
-    destroyed: function destroyed() {
-
-        $(this.$el).material_select('destroy');
-    }
-});
-
-/***/ }),
-/* 39 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
-  return _c('select', [_vm._t("default")], 2)
-},staticRenderFns: []}
-module.exports.render._withStripped = true
-if (false) {
-  module.hot.accept()
-  if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-8d31995c", module.exports)
-  }
-}
-
-/***/ }),
+/* 37 */,
+/* 38 */,
+/* 39 */,
 /* 40 */,
 /* 41 */,
 /* 42 */,
@@ -22915,7 +22812,6 @@ window.Vue.use(__WEBPACK_IMPORTED_MODULE_0_vue_form___default.a, {
 });
 
 Vue.component('createCarePerson', __webpack_require__(69));
-Vue.component('materialSelect', __webpack_require__(37));
 Vue.component('select2', __webpack_require__(45));
 Vue.component('fab', __webpack_require__(79));
 
@@ -24756,7 +24652,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 //                                });
                 //                            }
 
-                //                            $("#successModal-" + id).modal();
+                //                            $("#successModal-" + id).modal({    backdrop: 'static',    keyboard: false});
 
                 //HACK to replace select2 with newly added provider on appointments page
                 var carePerson = response.data.carePerson;
--- resources/assets/js/app-provider-ui.js
+++ resources/assets/js/app-provider-ui.js
@@ -20,7 +20,6 @@ window.Vue.use(VueForm, {
 });
 
 Vue.component('createCarePerson', require('./components/CareTeam/create-care-person.vue'));
-Vue.component('materialSelect', require('./components/src/material-select.vue'));
 Vue.component('select2', require('./components/src/select2'));
 Vue.component('fab', require('./components/fab.vue'));
 
