c9d1bc4d2c4f72566aa09c7dda9f95f2a9245f87
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 17:39:48 +0300

    vue edits #192

--- resources/views/partials/fab.blade.php
+++ resources/views/partials/fab.blade.php
@@ -5,7 +5,7 @@
       content="{{ URL::route('patient.activity.create', ['patient' => $patient->id]) }}">
 <meta name="route.patient.appointment.create"
       content="{{ URL::route('patient.appointment.create', ['patientId' => $patient->id]) }}">
-<meta name="provider-update-route" content="{{ route('user.care-team.update', ['userId' => '##userId##', 'id'=>'']) }}">
+<meta name="provider-update-route" content="{{ route('user.care-team.update', ['userId' => $patient->id, 'id'=>'']) }}">
 <meta name="patient_id" content="{{$patient->id}}">
 
 <fab></fab>
