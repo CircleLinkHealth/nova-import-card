1bfd0348a93ffeab1ff3b154a04eab5e609b4b3f
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 03:33:00 +0300

    Provider is probably unusable now #192

--- resources/assets/js/components/provider-ui.vue
+++ resources/assets/js/components/provider-ui.vue
@@ -1,12 +1,9 @@
 <template>
     <div>
-        <p>
-            <i v-on:click="createCarePerson" class="mini-action-button__icon material-icons">contact_mail</i>
-        </p>
-        <p class="mini-action-button__text--hide">Add Care Person</p>
 
 
-        <component :is="currentModal" :model="editedUser"></component>
+
+
     </div>
 </template>
 
@@ -15,27 +12,6 @@
     import { showForm } from '../store/actions'
 
     export default {
-        data() {
-            return {
-                currentModal: '',
-                editedUser: null,
-                showModal: false
-            }
-        },
 
-        mounted() {
-            console.log('component-provider-ui')
-        },
-
-        methods: Object.assign(
-            mapActions(['showForm']),
-            {
-                createCarePerson() {
-                    this.editedUser = {}
-                    this.currentModal = 'create-care-person'
-                    this.showForm(true)
-                }
-            }
-        )
     }
 </script>
