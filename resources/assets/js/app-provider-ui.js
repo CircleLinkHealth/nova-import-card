1f237cf700e8e5500fcad1e4ff94d0b8dda3673f
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 03:34:49 +0300

    Adds provider ui app #192

--- resources/assets/js/app-provider-ui.js
+++ resources/assets/js/app-provider-ui.js
@@ -1,29 +1,31 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-window.Vue.config.debug = true
-
-import store from './store/index'
-
-Vue.component('createCarePerson', require('./components/CareTeam/create-care-person.vue'));
-Vue.component('providerUi', require('./components/provider-ui.vue'));
 
 var getUrl = window.location;
 var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
 window.axios.defaults.baseURL = $('meta[name="base-url"]').attr('content');
 
+import VueForm from "vue-form";
+import store from "./store";
+
+window.Vue.config.debug = store.state.debug
+
+window.Vue.use(VueForm, {
+    inputClasses: {
+        valid: 'form-control-success',
+        invalid: 'form-control-danger'
+    }
+});
+
+Vue.component('createCarePerson', require('./components/CareTeam/create-care-person.vue'));
+Vue.component('materialSelect', require('./components/src/material-select.vue'));
+Vue.component('select2', require('./components/src/select2'));
+Vue.component('fab', require('./components/fab.vue'));
+
 window.App = new Vue({
     el: '#app',
-
-    store,
-
-    mounted() {
-
-    },
-
-    created () {
-
-    }
+    store
 });
 
