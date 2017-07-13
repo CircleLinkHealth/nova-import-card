0c431f548240f2625d10b11002b96180fe706ddf
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 03:33:37 +0300

    Moved to fab component template #192

--- resources/views/partials/fab.blade.php
+++ resources/views/partials/fab.blade.php
@@ -1,55 +1,6 @@
-<style>
-    @media print {
-        .hidden-print, .hidden-print * {
-            display: none !important;
-        }
-    }
-</style>
+<meta name="route.patient.note.create" content="{{ URL::route('patient.note.create', ['patient' => $patient->id]) }}">
+<meta name="route.patient.observation.create" content="{{ URL::route('patient.observation.create', ['patient' => $patient->id]) }}">
+<meta name="route.patient.activity.create" content="{{ URL::route('patient.activity.create', ['patient' => $patient->id]) }}">
+<meta name="route.patient.appointment.create" content="{{ URL::route('patient.appointment.create', ['patientId' => $patient->id]) }}">
 
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-<section class="FAB hidden-print">
-    <div class="FAB__mini-action-button hidden-print">
-        <div class="mini-action-button--hide mini-action-button hidden-print">
-            <a href="{{  URL::route('patient.note.create', array('patient' => $patient->id)) }}">
-                <i class="mini-action-button__icon material-icons">speaker_notes</i>
-            </a>
-            <p class="mini-action-button__text--hide">Add Note</p>
-        </div>
-
-        <div class="mini-action-button--hide mini-action-button">
-            <a href="{{ URL::route('patient.observation.create', array('patient' => $patient->id)) }}">
-                <i class="mini-action-button__icon material-icons">timeline</i>
-            </a>
-            <p class="mini-action-button__text--hide">Add Observation</p>
-        </div>
-
-        @if(!auth()->user()->hasRole('care-center')) {
-        <div class="mini-action-button--hide mini-action-button">
-            <a href="{{ URL::route('patient.activity.create', array('patient' => $patient->id)) }}">
-                <i class="mini-action-button__icon material-icons">local_hospital</i>
-            </a>
-            <p class="mini-action-button__text--hide">Add Offline Activity</p>
-        </div>
-        @endif
-
-        <div class="mini-action-button--hide mini-action-button">
-            <a href="{{ URL::route('patient.appointment.create', array('patientId' => $patient->id)) }}">
-                <i class="mini-action-button__icon material-icons">today</i>
-            </a>
-            <p class="mini-action-button__text--hide">Add Appointment</p>
-        </div>
-
-        <div id="showAddCarePersonModal" class="mini-action-button--hide mini-action-button">
-            <provider-ui></provider-ui>
-        </div>
-
-    </div>
-    <div class="FAB__action-button hidden-print">
-        <i class="action-button__icon material-icons hidden-print">add</i>
-    </div>
-</section>
-
-@section('scripts')
-    {{--<script src="/js/view-care-plan.js"></script>--}}
-@endsection
+<fab></fab>
