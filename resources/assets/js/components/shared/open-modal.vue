25879c76d1442fe205c4fdd10cbc39a481ee0875
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-14 01:55:02 +0300

    adds open modal #192

---
+++ resources/assets/js/components/shared/open-modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <component-proxy v-if="name" :name="name" :props="props"></component-proxy>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex'
+    import {openModal} from '../../store/getters';
+
+    export default {
+        computed: Object.assign(
+            mapGetters({
+                openModal: 'openModal'
+            }),
+            {
+                name() {
+                    return _.isNull(this.openModal.name) ? false : this.openModal.name;
+                },
+                props(){
+                    return _.isNil(this.openModal.props) ? {'show':true} : Object.assign(this.openModal.props, {'show':true})
+                }
+            }
+        ),
+    }
+</script>
