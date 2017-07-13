df60179e706ef7f4bbb003dc8fbf5fd086fb02fb
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 03:29:30 +0300

    Adds getCurrentUserAction #192

--- resources/assets/js/store/actions.js
+++ resources/assets/js/store/actions.js
@@ -1,7 +1,19 @@
+import userProfile from '../api/user-profile'
+
 export const cancelForm = (context) => {
     context.commit('CLEAR_FORM');
 }
 
 export const showForm = (context) => {
     context.commit('SET_FORM_SHOW', true);
+}
+
+export const getCurrentUser = ({ commit }) => {
+    userProfile.getCurrentUser(user => {
+        if (!user) {
+            commit('LOGOUT_USER');
+            return
+        }
+        commit('LOGIN_USER', user);
+    })
 }
