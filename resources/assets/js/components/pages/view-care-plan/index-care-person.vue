dcf0cf421005b6781aac97e7c45885558de648e9
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 17:40:49 +0300

    care person improvements #192

--- resources/assets/js/components/pages/view-care-plan/index-care-person.vue
+++ resources/assets/js/components/pages/view-care-plan/index-care-person.vue
@@ -1,22 +1,19 @@
 <template>
-    <div>
-        WORKING!
-    </div>
-    <!--<div v-show="care_person.user.first_name && care_person.user.last_name">-->
-        <!--<div class="col-md-7"><p style="margin-left: -10px;"><strong>{{care_person.formatted_type}}-->
-            <!--: </strong>{{care_person.user.first_name}} {{care_person.user.last_name}}-->
-            <!--<em>{{care_person.user.primaryRole}}</em></p></div>-->
-        <!--<div class="col-md-3"><p v-if="care_person.alert">Receives Alerts</p></div>-->
-        <!--<div class="col-md-2">-->
-            <!--<button :id="'deleteCareTeamMember-' + care_person.id"-->
+    <div v-show="carePerson.user.first_name && carePerson.user.last_name">
+        <div class="col-md-7"><p style="margin-left: -10px;"><strong>{{carePerson.formatted_type}}
+            : </strong>{{carePerson.user.first_name}} {{carePerson.user.last_name}}
+            <em>{{carePerson.user.primaryRole}}</em></p></div>
+        <div class="col-md-3"><p v-if="carePerson.alert">Receives Alerts</p></div>
+        <div class="col-md-2">
+            <!--<button :id="'deleteCareTeamMember-' + carePerson.id"-->
                     <!--class="btn btn-xs btn-danger problem-delete-btn"-->
-                    <!--v-on:click.stop.prevent="deleteCarePerson(care_person, index)"><span> <i-->
+                    <!--v-on:click.stop.prevent="deleteCarePerson(carePerson, index)"><span> <i-->
                     <!--class="glyphicon glyphicon-remove"></i> </span></button>-->
             <!--<button class="btn btn-xs btn-primary problem-edit-btn"-->
-                    <!--v-on:click.stop.prevent="editCarePerson(care_person)"><span> <i-->
+                    <!--v-on:click.stop.prevent="editCarePerson(carePerson)"><span> <i-->
                     <!--class="glyphicon glyphicon-pencil"></i> </span></button>-->
-        <!--</div>-->
-    <!--</div>-->
+        </div>
+    </div>
 </template>
 
 <script>
