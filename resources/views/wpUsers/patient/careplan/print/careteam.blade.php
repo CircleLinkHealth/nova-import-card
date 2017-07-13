cb2ffa889e195fc098d8109df55d3a715f7e123e
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-13 03:39:39 +0300

    comments out view-care-plan #192

--- resources/views/wpUsers/patient/careplan/print/careteam.blade.php
+++ resources/views/wpUsers/patient/careplan/print/careteam.blade.php
@@ -10,5 +10,5 @@
 <care-team v-bind:care-team-collection="careTeamCollection"></care-team>
 
 @section('scripts')
-    <script src="/js/view-care-plan.js"></script>
+    {{--<script src="/js/view-care-plan.js"></script>--}}
 @endsection
