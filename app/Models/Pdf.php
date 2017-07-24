2a7897c446afbd99d2a150c26ff1c74f2af683e0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-24 17:16:22 +0300

    Adds PDF soft deletes #407

--- app/Models/Pdf.php
+++ app/Models/Pdf.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use App\CarePlan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pdf extends Model
 {
+    use SoftDeletes;
+
     /**
      * Get all of the owning pdfable models.
      */
