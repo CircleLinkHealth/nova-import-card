b2e15c61e3387ef1c1895c632786502e5471d76c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-26 00:07:12 +0300

    Compiled minified assets

--- resources/views/partials/care-center/work-schedule-slot/nurse-window-fields.blade.php
+++ resources/views/partials/care-center/work-schedule-slot/nurse-window-fields.blade.php
@@ -1,17 +1,6 @@
-<style>
-    .work-schedule-label {
-        line-height: 4rem;
-        font-weight: bold !important;
-    }
-
-    .minimum-padding {
-        padding: 0 1px 0 1px;
-    }
-</style>
-
 <div class="form-group">
 
-    <div class="col-md-4">
+    <div class="col-md-2">
         <select class="form-control" name="day_of_week" required>
             @foreach(weekDays() as $key => $day)
                 @if(!isset($window))
@@ -21,6 +10,25 @@
                 @endif
             @endforeach
         </select>
+    </div>
+
+    <div class="col-md-2">
+        <div class="row">
+            <div class="col-md-4 minimum-padding">
+                <label class="work-schedule-label">for</label>
+            </div>
+            <div class="col-md-8 minimum-padding">
+                <input class="form-control" name="window_time_start"
+                       type="text" data-field="time"
+                       id="window_time_start"
+                       @if (isset($window))
+                       value="{{$window->window_time_start}}"
+                       @else
+                       value="{{ old('window_time_start') ? old('window_time_start') :  '09:00' }}"
+                       @endif
+                       required>
+            </div>
+        </div>
     </div>
 
     <div class="col-md-3">
