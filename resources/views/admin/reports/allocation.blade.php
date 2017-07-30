4917ef823b26b157383901d4fbeafb6ba320ec5c
Rohan Maheshwari <rohanm@circlelinkhealth.com>
2017-07-29 20:34:50 -0400

    fixes font colors

--- resources/views/admin/reports/allocation.blade.php
+++ resources/views/admin/reports/allocation.blade.php
@@ -46,10 +46,10 @@
                                     <div class="">
                                         @foreach($nurses as $nurse => $count)
                                             @if(!($count['Scheduled'] == 0 && $count['Actual Made'] == 0))
-                                                <div class=""><h5>{{$nurse}}</h5>
-                                                    <span>Sch: {{$count['Scheduled']}}</span><br/>
+                                                <div class=""><h5 style="font-size: 17px; font-weight: 500;">{{$nurse}}</h5>
+                                                    <span style="color: gray;">Sch: {{$count['Scheduled']}}</span><br/>
                                                     @if(Carbon\Carbon::parse($date)->toDateString() < \Carbon\Carbon::today()->toDateString())
-                                                    <span>Made: {{$count['Actual Made']}}</span>
+                                                    <span style="color: gray;">Made: {{$count['Actual Made']}}</span>
                                                     @endif
                                                 </div>
                                             @endif
