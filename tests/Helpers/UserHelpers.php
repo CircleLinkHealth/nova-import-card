864d4a9382c2ee75ae7fcd701dd06e390e02ca49
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-07-30 22:47:11 +0300

    Upgrade UserHelpers #418

--- tests/Helpers/UserHelpers.php
+++ tests/Helpers/UserHelpers.php
@@ -86,18 +86,18 @@ trait UserHelpers
         $user->locations()->sync($locations);
 
         foreach ($locations as $locId) {
-            $this->seeInDatabase('location_user', [
+            $this->assertDatabaseHas('location_user', [
                 'location_id' => $locId,
                 'user_id'     => $user->id,
             ]);
         }
 
         //check that it was created
-        $this->seeInDatabase('users', ['email' => $email]);
+        $this->assertDatabaseHas('users', ['email' => $email]);
 
         //check that the roles were created
         foreach ($roles as $role) {
-            $this->seeInDatabase('lv_role_user', [
+            $this->assertDatabaseHas('lv_role_user', [
                 'user_id' => $user->id,
                 'role_id' => $role,
             ]);
