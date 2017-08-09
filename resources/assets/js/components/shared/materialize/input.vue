f7bfeaca9ceaab410552f9e579d555222e697be3
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-08-09 12:21:50 +0300

    Adds input component #411

---
+++ resources/assets/js/components/shared/materialize/input.vue
@@ -0,0 +1,35 @@
+<script>
+    import {mapGetters, mapActions} from 'vuex'
+    import {errors} from '../../../store/getters'
+
+    export default {
+        props: {
+            name: String,
+            value: String,
+            label: String
+        },
+        computed: Object.assign(
+            mapGetters({
+                errors: 'errors'
+            }),
+        ),
+
+        methods: {
+            updateValue(value) {
+                this.$emit('input', value)
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <input :id="name" name="name" :class="{invalid: errors.get(name)}" @input="updateValue($event.target.value)"
+               :value="value">
+
+        <label :class="{active: value}" :for="name" :data-error="errors.get(name)"
+               data-success="">
+            {{label}}
+        </label>
+    </div>
+</template>
