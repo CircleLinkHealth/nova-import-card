bae2993ec633431f7ffb3b458d1ab1a213dda626
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-08-09 12:22:10 +0300

    Moving to page components - adds manage locations #411

---
+++ resources/assets/js/components/pages/provider-admin-panel/manage-practice-locations.vue
@@ -0,0 +1,34 @@
+<template>
+    <!--<component-proxy v-if="componentName" :name="componentName" :props="props" @update-view="updateView"></component-proxy>-->
+    <component :is="componentName" :location="props" @update-view="updateView"></component>
+</template>
+
+<script>
+    import ComponentProxy from '../../shared/component-proxy.vue'
+    import UpdateLocation from '../../practice/lodations/update.vue'
+    import CreateLocations from '../../practice/lodations/create.vue'
+    import IndexLocations from '../../practice/lodations/index.vue'
+
+    export default {
+        components: {
+            UpdateLocation,
+            IndexLocations,
+            CreateLocations,
+            ComponentProxy
+        },
+
+        data() {
+            return {
+                componentName: 'index-locations',
+                props: {}
+            }
+        },
+
+        methods: {
+            updateView(componentName, props) {
+                this.componentName = componentName
+                this.props = props
+            }
+        }
+    }
+</script>
