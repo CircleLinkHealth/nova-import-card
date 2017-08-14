218aad852affb2685429cfa3692cd9d97e4efd1c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-08-14 20:31:37 +0200

    Breaks it up into jobs #439

--- app/Console/Commands/EmailWeeklyReports.php
+++ app/Console/Commands/EmailWeeklyReports.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\EmailWeeklyPracticeReport;
+use App\Jobs\EmailWeeklyProviderReport;
 use App\Practice;
 use App\Reports\Sales\Practice\SalesByPracticeReport;
 use App\Reports\Sales\Provider\SalesByProviderReport;
@@ -47,110 +49,18 @@ class EmailWeeklyReports extends Command
      */
     public function handle()
     {
+        $testerEmail = null;
+
+        if ($this->argument('email')) {
+            $testerEmail = $this->argument('email');
+        }
+
         $startRange = Carbon::now()->setTime(0, 0, 0)->subWeek();
         $endRange = Carbon::now()->setTime(0, 0, 0);
 
         foreach ($this->activePractices as $practice) {
-            $this->sendPracticeReport($practice, $startRange, $endRange);
-            $this->sendProviderReport($practice, $startRange, $endRange);
-        }
-    }
-
-    public function sendProviderReport($practice, $startRange, $endRange) {
-        if ($this->argument('email')) {
-            $testerEmail = $this->argument('email');
-        }
-
-        $providers_for_practice = $practice->getProviders($practice->id);
-
-        //handle providers
-        foreach ($providers_for_practice as $provider) {
-
-            $providerData =
-                (new SalesByProviderReport(
-                    $provider,
-                    SalesByProviderReport::SECTIONS,
-                    $startRange,
-                    $endRange
-                ))
-                    ->data(true);
-
-            $providerData['name'] = $provider->display_name;
-            $providerData['start'] = $startRange->toDateString();
-            $providerData['end'] = $endRange->toDateString();
-            $providerData['isEmail'] = true;
-
-
-            $subjectProvider = 'Dr. ' . $provider->last_name . '\'s CCM Weekly Summary';
-
-            if (isset($testerEmail)) {
-                Mail::send('sales.by-provider.report', ['data' => $providerData], function ($message) use (
-                    $provider,
-                    $subjectProvider,
-                    $testerEmail
-                ) {
-                    $message->from('notifications@careplanmanager.com', 'CircleLink Health');
-                    $message->to($testerEmail)->subject($subjectProvider);
-                });
-            } else {
-                Mail::send('sales.by-provider.report', ['data' => $providerData], function ($message) use (
-                    $provider,
-                    $subjectProvider
-                ) {
-                    $message->from('notifications@careplanmanager.com', 'CircleLink Health');
-                    $message->to($provider->email)->subject($subjectProvider);
-                });
-            }
-
-//                Slack::to('#background-tasks')
-//                    ->send("The CPMbot just sent the provider's summary for $practice->display_name to $provider->fullName");
-
-        }
-    }
-
-    public function sendPracticeReport(Practice $practice, $startRange, $endRange) {
-        if ($this->argument('email')) {
-            $testerEmail = $this->argument('email');
-        }
-
-        $subjectPractice = $practice->display_name . '\'s CCM Weekly Summary';
-
-        $practiceData = (new SalesByPracticeReport(
-            $practice,
-            SalesByPracticeReport::SECTIONS,
-            $startRange,
-            $endRange
-
-        ))->data(true);
-
-        $practiceData['name'] = $practice->display_name;
-        $practiceData['start'] = $startRange->toDateString();
-        $practiceData['end'] = $endRange->toDateString();
-        $practiceData['isEmail'] = true;
-
-        if ($practice->weekly_report_recipients != null) {
-
-            $organizationSummaryRecipients = explode(', ', trim($practice->weekly_report_recipients));
-
-            if (isset($testerEmail)) {
-                $organizationSummaryRecipients = [$testerEmail];
-            }
-
-            //handle leads
-            foreach ($organizationSummaryRecipients as $recipient) {
-
-                Mail::send('sales.by-practice.report', ['data' => $practiceData], function ($message) use (
-                    $recipient,
-                    $subjectPractice
-                ) {
-                    $message->from('notifications@careplanmanager.com', 'CircleLink Health');
-                    $message->to($recipient)->subject($subjectPractice);
-                });
-
-//                    Slack::to('#background-tasks')
-//                        ->send("The CPMbot just sent the organization weekly summary for $practice->display_name to $recipient");
-
-            }
+            dispatch(new EmailWeeklyPracticeReport($practice, $startRange, $endRange, $testerEmail));
+            dispatch(new EmailWeeklyProviderReport($practice, $startRange, $endRange, $testerEmail));
         }
     }
 }
