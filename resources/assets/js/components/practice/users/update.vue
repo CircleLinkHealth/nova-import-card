97a7ed70c03048153a8f22fc238056fa1f00a5a3
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-08-23 14:57:30 +0200

    More users work #412

--- resources/assets/js/components/practice/users/update.vue
+++ resources/assets/js/components/practice/users/update.vue
@@ -23,6 +23,55 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div id="edit-staff-form">
+                <div class="row">
+                    <div class="input-field col s4">
+                        <v-input type="text" label="First Name" v-model="formData.first_name" name="first_name"
+                                 required></v-input>
+                    </div>
+
+                    <div class="input-field col s4">
+                        <v-input type="text" label="Last Name" v-model="formData.last_name" name="last_name"
+                                 required></v-input>
+                    </div>
+
+                    <div class="input-field col s4">
+                        <material-select v-model="formData.role" name="role" id="role"
+                                         :class="isValid(formData.role)">
+                            <option v-for="option in roleOptions" :value="option.value"
+                                    v-text="option.name"></option>
+                        </material-select>
+
+                        <label for="role">Role</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s4">
+                        <v-input type="number" label="Phone Number" v-model="formData.phone_number" name="phone_number"
+                                 required></v-input>
+                    </div>
+
+                    <div class="input-field col s4">
+                        <v-input type="number" label="Phone Extension" v-model="formData.phone_extension"
+                                 name="phone_extension"
+                                 required></v-input>
+                    </div>
+
+                    <div class="input-field col s4">
+                        <material-select v-model="formData.phone_type" name="phone_type" id="phone_type"
+                                         :class="isValid(formData.phone_type)">
+                            <option v-for="option in phoneTypes" :value="option.value"
+                                    v-text="option.name"></option>
+                        </material-select>
+
+                        <label for="phone_type">Phone Type</label>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -101,9 +150,54 @@
         data() {
             return {
                 formData: {
-
+                    role: '',
+                    locations: [],
+                    grandAdminRights: false,
+                    sendBillingReports: false,
+                    emr_direct_address: '',
+                    phone_number: '',
+                    phone_extension: '',
+                    phone_type: '',
+                    forward_alerts_to: {
+                        who: 'billing_provider',
+                        user_id: ''
+                    },
+                    forward_careplan_approval_emails_to: {
+                        who: 'billing_provider',
+                        user_id: ''
+                    },
                 },
                 formState: {},
+                roleOptions: [{
+                    name: 'Medical Assistant',
+                    value: 'med_assistant'
+                }, {
+                    name: 'Office Admin',
+                    value: 'office_admin'
+                }, {
+                    name: 'Provider',
+                    value: 'provider'
+                }, {
+                    name: 'Registered Nurse',
+                    value: 'registered-nurse'
+                }, {
+                    name: 'Specialist',
+                    value: 'specialist'
+                }],
+                phoneTypes: [
+                    {
+                        name: 'Home',
+                        value: 1
+                    },
+                    {
+                        name: 'Mobile',
+                        value: 2
+                    },
+                    {
+                        name: 'Work',
+                        value: 3
+                    }
+                ]
             }
         },
     }
