9a46c8d421ba1b944807d9443d7bfb4eaf17eb2d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-09-20 18:00:59 +0200

    Disables emr direct [deploy:worker]

--- app/Services/PhiMail/PhiMail.php
+++ app/Services/PhiMail/PhiMail.php
@@ -12,6 +12,7 @@ class PhiMail
 
     public function __construct()
     {
+        return;
         $phiMailServer = env('EMR_DIRECT_MAIL_SERVER');
         $phiMailPort = env('EMR_DIRECT_PORT');
         $phiMailUser = env('EMR_DIRECT_USER');
