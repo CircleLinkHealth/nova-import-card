c49f77b64f7400e4d9a633fb2bef29548ff8fc54
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-12 21:47:19 +0200

    Improves migration to add diabetes 1 and 2 #714

--- database/migrations/2017_10_12_120226_add_diabetes_type_2.php
+++ database/migrations/2017_10_12_120226_add_diabetes_type_2.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CarePlanTemplate;
 use App\Models\CPM\CpmProblem;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -14,15 +15,31 @@ class AddDiabetesType2 extends Migration
      */
     public function up()
     {
-        $diabetes1 = CpmProblem::create([
+        $diabetes1 = CpmProblem::updateOrCreate([
             'name' => 'Diabetes Type 1',
             'default_icd_10_code' => 'E10.8',
         ]);
 
-        $diabetes2 = CpmProblem::create([
+        $diabetes2 = CpmProblem::updateOrCreate([
             'name' => 'Diabetes Type 2',
             'default_icd_10_code' => 'E11.8',
         ]);
+
+        $diabetes = CpmProblem::whereName('Diabetes')
+            ->with('cpmInstructions')
+            ->first();
+
+        $defaultCarePlan = CarePlanTemplate::where('type', '=', 'CLH Default')->first();
+
+        if (count($diabetes->cpmInstructions) > 0) {
+            $defaultCarePlan->cpmProblems()->attach($diabetes1->id, [
+                'cpm_instruction_id' => $diabetes->cpmInstructions[0]->id,
+            ]);
+
+            $defaultCarePlan->cpmProblems()->attach($diabetes2->id, [
+                'cpm_instruction_id' => $diabetes->cpmInstructions[0]->id,
+            ]);
+        }
     }
 
     /**
