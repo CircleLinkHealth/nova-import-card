df00c2a3dfcb7ebe57232fefb35b8a613fa31c73
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-21 03:19:54 +0200

    Removes Log:: [deploy:worker]

--- app/Services/PhiMail/PhiMail.php
+++ app/Services/PhiMail/PhiMail.php
@@ -205,17 +205,17 @@ class PhiMail
                 // If you are checking messages for an address group,
                 // $message->recipient will contain the address in that
                 // group to which this message should be delivered.
-                Log::critical("A new message is available for " . $message->recipient . "\n");
-                Log::critical("from " . $message->sender . "; id "
-                    . $message->messageId . "; #att=" . $message->numAttachments
-                    . "\n");
+//                Log::critical("A new message is available for " . $message->recipient . "\n");
+//                Log::critical("from " . $message->sender . "; id "
+//                    . $message->messageId . "; #att=" . $message->numAttachments
+//                    . "\n");
 
                 for ($i = 0; $i <= $message->numAttachments; $i++) {
                     // Get content for part i of the current message.
                     $showRes = $this->connector->show($i);
 
-                    Log::critical("MimeType = " . $showRes->mimeType
-                        . "; length=" . $showRes->length . "\n");
+//                    Log::critical("MimeType = " . $showRes->mimeType
+//                        . "; length=" . $showRes->length . "\n");
 
                     // List all the headers. Headers are set by the
                     // sender and may include Subject, Date, additional
@@ -223,9 +223,9 @@ class PhiMail
                     // Do NOT use the To: header to determine the address
                     // to which this message should be delivered
                     // internally; use $messagerecipient instead.
-                    foreach ($showRes->headers as $header) {
-                        Log::critical("Header: " . $header . "\n");
-                    }
+//                    foreach ($showRes->headers as $header) {
+//                        Log::critical("Header: " . $header . "\n");
+//                    }
 
                     // Process the content; for this example text data
                     // is echoed to the console and non-text data is
@@ -233,8 +233,8 @@ class PhiMail
 
                     if (str_contains($showRes->mimeType, 'plain')) {
                         // ... do something with text parts ...
-                        Log::critical('The plain text part of the mail');
-                        Log::critical($showRes->data);
+//                        Log::critical('The plain text part of the mail');
+//                        Log::critical($showRes->data);
                         self::writeDataFile(storage_path(str_random(20) . '.txt'), $showRes->data);
                     } elseif (str_contains($showRes->mimeType, 'xml')) {
                         //save ccd to file
@@ -250,20 +250,20 @@ class PhiMail
 
                     // Display the list of attachments and associated info. This info is only
                     // included with message part 0.
-                    for ($k = 0; $i == 0 && $k < $message->numAttachments; $k++) {
-                        Log::critical("Attachment " . ($k + 1)
-                            . ": " . $showRes->attachmentInfo[$k]->mimeType
-                            . " fn:" . $showRes->attachmentInfo[$k]->filename
-                            . " Desc:" . $showRes->attachmentInfo[$k]->description
-                            . "\n");
-                    }
+//                    for ($k = 0; $i == 0 && $k < $message->numAttachments; $k++) {
+//                        Log::critical("Attachment " . ($k + 1)
+//                            . ": " . $showRes->attachmentInfo[$k]->mimeType
+//                            . " fn:" . $showRes->attachmentInfo[$k]->filename
+//                            . " Desc:" . $showRes->attachmentInfo[$k]->description
+//                            . "\n");
+//                    }
                 }
                 // This signals the server that the message can be safely removed from the queue
                 // and should only be sent after all required parts of the message have been
                 // retrieved and processed.
                 $this->connector->acknowledgeMessage();
 
-                Log::critical('Number of Attachments: ' . $message->numAttachments);
+//                Log::critical('Number of Attachments: ' . $message->numAttachments);
 
                 if ($message->numAttachments > 0) {
                     $this->notifyAdmins($message->numAttachments);
@@ -275,7 +275,7 @@ class PhiMail
                     sendSlackMessage('#background-tasks', $message);
                 }
 
-                Log::critical('***************');
+//                Log::critical('***************');
             }
         } catch (\Exception $e) {
             $this->handleException($e);
