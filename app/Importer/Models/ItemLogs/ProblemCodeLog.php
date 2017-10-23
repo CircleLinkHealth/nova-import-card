ef00dda23e7f7f840495b6da5ba8beaf1f55427e
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-23 19:48:48 +0200

    Adds ccd problem codes logs model #781

---
+++ app/Importer/Models/ItemLogs/ProblemCodeLog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Importer\Models\ItemLogs;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProblemCodeLog extends Model
+{
+    protected $table = 'ccd_problem_code_logs';
+
+    public $fillable = [
+        'ccd_problem_log_id',
+        'code_system_name',
+        'code_system_oid',
+        'code',
+    ];
+}
