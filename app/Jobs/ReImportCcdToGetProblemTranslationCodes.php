b49e68df7821ab0b1f451c333bebb7386f56c28b
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-24 02:19:36 +0200

    Adds NGDC correction queue job #781

---
+++ app/Jobs/ReImportCcdToGetProblemTranslationCodes.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Jobs;
+
+use App\CLH\Repositories\CCDImporterRepository;
+use App\Importer\Loggers\Ccda\CcdToLogTranformer;
+use App\Models\CCD\Problem;
+use App\Models\MedicalRecords\Ccda;
+use App\Models\ProblemCode;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ReImportCcdToGetProblemTranslationCodes implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $patient;
+    private $transformer;
+    private $repo;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $patient)
+    {
+        $this->patient = $patient;
+        $this->repo = new CCDImporterRepository();
+        $this->transformer = new CcdToLogTranformer();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $ccda = Ccda::select(['id', 'patient_id', 'xml'])
+            ->where('patient_id', '=', $this->patient->id)
+            ->first();
+
+        if (!$ccda) {
+            return;
+        }
+
+        $parsed = json_decode($this->repo->toJson($ccda->xml));
+
+        $ccdProblems = Problem::where('patient_id', '=', $this->patient->id)->get();
+
+        $problems = collect($parsed->problems)->map(function ($prob) use ($ccdProblems) {
+            $ccdProblem = $ccdProblems->where('name', '=', $prob->name)
+//                ->where('code', '=', $prob->code)
+//                ->where('code_system', '=', $prob->code_system)
+                ->first();
+
+            foreach ($prob->translations as $translation) {
+                if (!$translation->code_system_name) {
+                    $translation->code_system_name = getProblemCodeSystemName($translation);
+
+                    if (!$translation->code_system_name) {
+                        continue;
+                    }
+                }
+
+                ProblemCode::updateOrCreate([
+                    'problem_id'       => $ccdProblem->id,
+                    'code_system_name' => $translation->code_system_name,
+                    'code_system_oid'  => $translation->code_system,
+                    'code'             => $translation->code,
+                ]);
+            }
+        });
+    }
+}
