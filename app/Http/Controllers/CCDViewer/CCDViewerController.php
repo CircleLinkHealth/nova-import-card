19e7854d54c6a5fb3e2cd1cffd51c2686b6160e6
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-24 14:29:48 +0200

    removes $ccd->json references #781

--- app/Http/Controllers/CCDViewer/CCDViewerController.php
+++ app/Http/Controllers/CCDViewer/CCDViewerController.php
@@ -27,7 +27,7 @@ class CCDViewerController extends Controller
         $ccda = Ccda::find($ccdaId);
 
         if ($ccda) {
-            $ccd = json_decode($ccda->json);
+            $ccd = json_decode((new CCDImporterRepository())->toJson($ccda->xml));
 
             return view('CCDViewer.old-viewer', compact('ccd'));
         }
@@ -40,7 +40,7 @@ class CCDViewerController extends Controller
         $ccda = Ccda::wherePatientId($userId)->first();
 
         if ($ccda) {
-            $ccd = json_decode($ccda->json);
+            $ccd = json_decode((new CCDImporterRepository())->toJson($ccda->xml));
 
             return view('CCDViewer.old-viewer', compact('ccd'));
         }
