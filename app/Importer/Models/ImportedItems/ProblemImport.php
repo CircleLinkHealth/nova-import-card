2065f5c085ac29b505601ae821b01fa633d56b32
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-24 12:42:03 +0200

    Improvements #781

--- app/Importer/Models/ImportedItems/ProblemImport.php
+++ app/Importer/Models/ImportedItems/ProblemImport.php
@@ -11,47 +11,4 @@ class ProblemImport extends Model {
     {
         return $this->belongsTo(ProblemLog::class, 'ccd_problem_log_id');
     }
-
-    /**
-     * Gets the kind of code for a problem (ICD 9, ICD10, Snomed)
-     *
-     * @return bool|string
-     */
-    public function getCodeType()
-    {
-        /*
-         * ICD-9 Check
-         */
-        if ((str_contains(strtolower($this->code_system_name), 'icd')
-                && str_contains(strtolower($this->code_system_name), '9'))
-            || $this->code_system == '2.16.840.1.113883.6.103'
-        ) {
-            return 'icd_9_code';
-        }
-
-        /*
-       * ICD-10 Check
-       */
-        if ((str_contains(strtolower($this->code_system_name), 'icd')
-                && str_contains(strtolower($this->code_system_name), '10'))
-            || in_array($this->code_system, [
-                '2.16.840.1.113883.6.3',
-                '2.16.840.1.113883.6.4',
-            ])
-        ) {
-            return 'icd_10_code';
-        }
-
-
-        /*
-             * SNOMED Check
-             */
-        if (str_contains(strtolower($this->code_system_name), 'snomed')
-            || $this->code_system == '2.16.840.1.113883.6.96'
-        ) {
-            return 'snomed_code';
-        }
-
-        return false;
-    }
 }
