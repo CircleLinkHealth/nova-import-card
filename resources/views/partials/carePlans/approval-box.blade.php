dad14f0969a0720db5bac9f2c8b87aea30990751
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-25 17:59:47 +0200

    Fix [deploy:production]

--- resources/views/partials/carePlans/approval-box.blade.php
+++ resources/views/partials/carePlans/approval-box.blade.php
@@ -3,7 +3,8 @@
         <div class="pull-right print-row text-right">
             Approved on {{$patient->carePlan->provider_date->format('m/d/Y')}}
             at {{$patient->carePlan->provider_date->setTimezone($patient->timezone ?? 'America/New_York')->format('g:i A')}} {{$patient->carePlan->provider_date->setTimezone($patient->timezone ?? 'America/New_York')->format('T')}}
-            by {{App\User::find($patient->carePlan->provider_approver_id)->fullName}}
+            by {{App\User::withTrashed()->find($patient->carePlan->provider_approver_id)->fullName}}
         </div>
     </div>
 @endif
+
