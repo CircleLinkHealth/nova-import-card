cd9f2d4989e60db8bd260b2a306b2a7a8237a669
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-10-25 14:38:38 +0200

    Adds CarePlanApproval mailable #775

---
+++ app/Console/Commands/SendCarePlanApprovalReminderTestEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\CarePlanApprovalReminder;
+use App\User;
+use Illuminate\Console\Command;
+use Mail;
+
+class SendCarePlanApprovalReminderTestEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:emailCPApprovalReminderTest {userId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a test CarePlan approval reminder email to the given User.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $testerEmail = $this->argument('userId');
+
+        $user = User::findOrFail($testerEmail);
+
+        Mail::send(new CarePlanApprovalReminder($user));
+    }
+}
