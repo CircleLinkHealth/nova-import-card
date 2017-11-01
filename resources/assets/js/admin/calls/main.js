0f2d913a7238e9ff54fc4458216450ac10563a64
Ikechi Michael <mykehell123@gmail.com>
2017-11-01 16:28:57 +0100

    chore: now it shows the table ... yaay

--- resources/assets/js/admin/calls/main.js
+++ resources/assets/js/admin/calls/main.js
@@ -2,12 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import CallMgmtApp from './app'
+import VueResource from 'vue-resource'
+import { ClientTable, Event } from 'vue-tables-2'
 
+
+Vue.use(VueResource)
+Vue.use(ClientTable, {}, false)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 const App = new Vue({
   el: '#call-mgmt-app',
-  template: '<CallMgmtApp/>',
-  components: { CallMgmtApp }
+  template: '<call-mgmt-app/>',
+  components: { 
+    'call-mgmt-app': CallMgmtApp, 
+    'v-client-table': ClientTable
+  }
 })
