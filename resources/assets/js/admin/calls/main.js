9a4b12b0959876b952ca31538f74c696b34fe633
Ikechi Michael <mykehell123@gmail.com>
2017-11-01 16:29:53 +0100

    chore: removed unnecessary comments

--- resources/assets/js/admin/calls/main.js
+++ resources/assets/js/admin/calls/main.js
@@ -1,16 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import CallMgmtApp from './app'
 import VueResource from 'vue-resource'
 import { ClientTable, Event } from 'vue-tables-2'
 
-
 Vue.use(VueResource)
 Vue.use(ClientTable, {}, false)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 const App = new Vue({
   el: '#call-mgmt-app',
   template: '<call-mgmt-app/>',
