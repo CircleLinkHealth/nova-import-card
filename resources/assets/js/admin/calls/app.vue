bddd33c754fc8ef7f20704e5c0b7d43118ff16bd
Ikechi Michael <mykehell123@gmail.com>
2017-11-01 17:38:52 +0100

    feat: render call data (not complete)

--- resources/assets/js/admin/calls/app.vue
+++ resources/assets/js/admin/calls/app.vue
@@ -11,21 +11,39 @@
       name: 'CallMgmtApp',
       data() {
         return {
-          columns: ['id','name','age'],
-          tableData: [
-            {id:1, name:"John",age:"20"},
-            {id:2, name:"Jane",age:"24"},
-            {id:3, name:"Susan",age:"16"},
-            {id:4, name:"Chris",age:"55"},
-            {id:5, name:"Dan",age:"40"}
-          ],
+          columns: ['ID', 'Nurse','Patient','Status', 'Practice', 'Last Call Status', 'Next Call', 'Call Time Start', 'Call Time End', 'Time Zone', 'Preferred Call Days', 'Last Call', 'CCM Time'],
+          tableData: [],
           options: {
           // see the options API
           }
         }
       },
       mounted() {
-        this.$http.get(rootUrl('api/admin/calls')).then((data) => console.log(data))
+        this.$http.get(rootUrl('api/admin/calls')).then((result) => result.data).then(result => {
+          const calls = result.data;
+          calls.forEach(call => {
+            call.getNurse = () => ((call.inbound_user && call.inbound_user.nurse_info) ?
+                                            call.inbound_user : 
+                                      (call.outbound_user && call.outbound_user.nurse_info) ?
+                                            call.outbound_user : 
+                                            null)
+            call.getPatient = () => ((call.inbound_user && call.inbound_user.patient_info) ?
+                                            call.inbound_user : 
+                                      (call.outbound_user && call.outbound_user.patient_info) ?
+                                            call.outbound_user : 
+                                            null)
+          })
+          const tableCalls = calls.map(call => ({
+                                ID: call.id,
+                                Nurse: (call.getNurse() || {}).full_name,
+                                Patient: (call.getPatient() || {}).full_name,
+                                Status: call.status,
+                                Practice: (call.getNurse() || {}).primary_practice_id
+                              }))
+          this.tableData = this.tableData.concat(tableCalls)
+          console.log(calls);
+          console.log(this.tableData);
+        })
       }
   }
 </script>
