88f6eddae2c619ff100c3d8d6add2020b425f79f
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-01 22:10:05 +0100

    Adds dates #687 [deploy:production]

--- resources/views/admin/jobsCompleted/manage.blade.php
+++ resources/views/admin/jobsCompleted/manage.blade.php
@@ -12,6 +12,8 @@
                 <div class="alert alert-success" role="alert">
                     {{$cache['message']}}
                     <a href="{{route('get.cached.vue.by.key', ['key' => $cache['key']])}}" class="alert-link">Go to page</a>
+                    <p class="text-right">Requested at {{$cache['created_at']}}</p>
+                    <p class="text-right">Expires at {{$cache['expires_at']}}</p>
                 </div>
             @endif
         @endforeach
