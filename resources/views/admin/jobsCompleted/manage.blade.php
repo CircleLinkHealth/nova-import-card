8df3215008c9f86cba4a7018aec43067cd30762d
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-03 03:01:06 +0100

    Improves look [deploy:worker]

--- resources/views/admin/jobsCompleted/manage.blade.php
+++ resources/views/admin/jobsCompleted/manage.blade.php
@@ -1,21 +1,57 @@
 @extends('partials.adminUI')
 
+@push('styles')
+<style>
+    .job-completed-card {
+        background-color: #fafafa;
+        color: #212121;
+        padding: 3rem;
+        border-radius: 2rem;
+        font-family: 'Roboto', arial, 'Noto Sans Japanese', sans-serif;
+        margin: 4rem;
+    }
+
+    .job-completed-card-title {
+        color: #ff5723;
+        border-bottom: 1px solid #e6e6e6;
+        padding: 1rem;
+    }
+
+    .job-completed-card-footer {
+        font-size: 12px;
+        margin: 40px 0 0 0;
+    }
+</style>
+@endpush
+
 @section('content')
     <div class="container">
-        @foreach(auth()->user()->cachedViews() as $cache)
-            @if(!$cache['key'])
-                <div class="alert alert-danger" role="alert">
-                    <strong>Error!</strong>{{$cache['message']}}
-                </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach(auth()->user()->cachedViews() as $cache)
+                    @if(!$cache['key'])
+                        <div class="alert alert-danger" role="alert">
+                            <strong>Error!</strong>{{$cache['message']}}
+                        </div>
 
-            @else
-                <div class="alert alert-success" role="alert">
-                    {{$cache['message']}}
-                    <a href="{{route('get.cached.vue.by.key', ['key' => $cache['key']])}}" class="alert-link">Go to page</a>
-                    <p class="text-right">Requested at {{$cache['created_at']}}</p>
-                    <p class="text-right">Expires at {{$cache['expires_at']}}</p>
-                </div>
-            @endif
-        @endforeach
+                    @else
+                        <div class="job-completed-card col-md-12 text-center">
+                            <h3 class="job-completed-card-title">
+                                {{$cache['title'] ?? ''}}
+                            </h3>
+                            <a href="{{route('get.cached.vue.by.key', ['key' => $cache['key']])}}" class="alert-link">Go to
+                                page</a>
+
+                            <h5>{{$cache['message'] ?? ''}}</h5>
+
+                            <p class="job-completed-card-footer">
+                                <span class="pull-left">created: <strong>{{$cache['created_at']}}</strong></span>
+                                <span class="pull-right">expires: <strong>{{$cache['expires_at']}}</strong></span>
+                            </p>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
