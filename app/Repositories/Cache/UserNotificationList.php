20209531b66834e23e22ee289d8e1a5b21492ef2
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-03 18:30:28 +0100

    Adds cached user notifications list #876

---
+++ app/Repositories/Cache/UserNotificationList.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 11/03/2017
+ * Time: 2:14 AM
+ */
+
+namespace App\Repositories\Cache;
+
+
+use App\Constants;
+use App\User;
+use Carbon\Carbon;
+
+class UserNotificationList
+{
+    private $userId;
+    private $viewHashKey;
+
+    public function __construct($user, $viewHashKey = null)
+    {
+        $this->userId = is_a(User::class, $user)
+            ? $user->id
+            : $user;
+        $this->viewHashKey = $viewHashKey;
+    }
+
+    /**
+     * Add a Notification to the User's Notification List
+     *
+     * @param string $title
+     * @param string $description
+     * @param null $link
+     */
+    public function push($title = '', $description = '', $link = null)
+    {
+        \Redis::rpush(
+            $this->userHashKey(),
+            $this->userCachedNotificationFactory($title, $description, $link)
+        );
+    }
+
+    /**
+     * Get the hash key for the give User's cached views list
+     *
+     *
+     * @return string
+     */
+    public function userHashKey()
+    {
+        return str_replace('{$userId}', $this->userId, Constants::CACHED_USER_NOTIFICATIONS);
+    }
+
+    /**
+     * Create a User view
+     *
+     * @param $title
+     * @param string $description
+     * @param null $link
+     *
+     * @return array
+     *
+     */
+    public function userCachedNotificationFactory($title, $description = '', $link = null)
+    {
+        return json_encode([
+            'created_at'  => Carbon::now()->toDateTimeString(),
+            'expires_at'  => Carbon::now()->addWeek()->toDateTimeString(),
+            'title'       => $title,
+            'description' => $description,
+            'link'        => $link,
+        ]);
+    }
+
+    /**
+     * Returns the count of the User's Notifications
+     *
+     * @return mixed
+     */
+    public function count()
+    {
+        return \Redis::llen($this->userHashKey());
+    }
+
+    /**
+     * Get the User's cached views
+     *
+     * @param int $start
+     * @param int $end
+     *
+     * @return static
+     */
+    public function all($start = 0, $end = -1)
+    {
+        return collect(\Redis::lrange($this->userHashKey(), $start, $end))->map(function ($json) {
+            $cache = json_decode($json, true);
+
+            $now = Carbon::now();
+            $expires = Carbon::parse($cache['expires_at']);
+
+            if ($now->greaterThan($expires) || !\Cache::has($cache['key'])) {
+                \Redis::lrem($this->userHashKey(), 0, $json);
+
+                return false;
+            }
+
+            return $cache;
+        })
+            ->filter()
+            ->reverse();
+    }
+}
