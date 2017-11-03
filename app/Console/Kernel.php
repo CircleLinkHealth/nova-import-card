2fff052abe345269bf04299be939233c3bd6585f
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-03 19:27:54 +0100

    Small change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,37 +1,9 @@
 <?php namespace App\Console;
 
 use App\Algorithms\Calls\ReschedulerHandler;
-use App\Console\Commands\Athena\GetAppointments;
-use App\Console\Commands\Athena\GetCcds;
-use App\Console\Commands\CheckEmrDirectInbox;
-use App\Console\Commands\EmailRNDailyReport;
-use App\Console\Commands\EmailsProvidersToApproveCareplans;
-use App\Console\Commands\EmailWeeklyReports;
-use App\Console\Commands\ExportNurseSchedulesToGoogleCalendar;
-use App\Console\Commands\GeneratePatientReports;
-use App\Console\Commands\ImportLGHInsurance;
-use App\Console\Commands\ImportNurseScheduleFromGoogleCalendar;
-use App\Console\Commands\Inspire;
-use App\Console\Commands\MapSnomedToCpmProblems;
-use App\Console\Commands\ProcessCcdaLGHMixup;
-use App\Console\Commands\QueueCcdasToConvertToJson;
-use App\Console\Commands\QueueCcdasToProcess;
-use App\Console\Commands\QueueCcdaToDetermineEnrollmentEligibility;
-use App\Console\Commands\QueueGenerateNurseDailyReport;
-use App\Console\Commands\QueueGenerateNurseInvoices;
-use App\Console\Commands\QueueMakeWelcomeCallsList;
-use App\Console\Commands\QueueSendAuditReports;
-use App\Console\Commands\RecalculateCcmTime;
-use App\Console\Commands\ReImportCcdsToGetTranslations;
-use App\Console\Commands\ResetCcmTime;
-use App\Console\Commands\SendCarePlanApprovalReminderTestEmail;
-use App\Console\Commands\SplitMergedCcdas;
 use App\Services\Calls\SchedulerService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
-//use EnrollmentSMSSender;
-
 
 class Kernel extends ConsoleKernel
 {
@@ -146,7 +118,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
         require base_path('routes/console.php');
     }
 }
