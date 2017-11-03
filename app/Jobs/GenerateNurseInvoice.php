73f73f5f6718a277eda7be903765bc20d47fb9c0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-03 01:30:06 +0100

    Adds generate nurse invoice

--- app/Jobs/GenerateNurseInvoice.php
+++ app/Jobs/GenerateNurseInvoice.php
@@ -4,13 +4,13 @@ namespace App\Jobs;
 
 use App\Billing\NurseMonthlyBillGenerator;
 use App\Nurse;
-use App\User;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
 
 class GenerateNurseInvoice implements ShouldQueue
 {
@@ -21,7 +21,8 @@ class GenerateNurseInvoice implements ShouldQueue
     private $variablePay;
     private $addNotes;
     private $addTime;
-    private $requestor;
+    private $requestors;
+    private $key;
 
     /**
      * Create a new job instance.
@@ -29,18 +30,18 @@ class GenerateNurseInvoice implements ShouldQueue
      * @param array $nurseUserIds
      * @param Carbon $startDate
      * @param Carbon $endDate
+     * @param Collection|int $requestors
      * @param bool $variablePay
      * @param int $addTime
      * @param string $addNotes
-     * @param User $requestor
      */
     public function __construct(array $nurseUserIds,
                                 Carbon $startDate,
                                 Carbon $endDate,
+                                $requestors,
                                 bool $variablePay = false,
                                 int $addTime = 0,
-                                string $addNotes = '',
-                                User $requestor)
+                                string $addNotes = '')
     {
         $this->nurses = Nurse::whereIn('user_id', $nurseUserIds)->get();
         $this->startDate = $startDate;
@@ -48,7 +49,10 @@ class GenerateNurseInvoice implements ShouldQueue
         $this->variablePay = $variablePay;
         $this->addTime = $addTime;
         $this->addNotes = $addNotes;
-        $this->requestor = $requestor;
+        $this->requestors = is_a($requestors, Collection::class)
+            ? $requestors
+            : collect($requestors);
+        $this->key = 'view' . str_random('20');
     }
 
     /**
@@ -77,48 +81,92 @@ class GenerateNurseInvoice implements ShouldQueue
             $links[$nurse->user_id]['name'] = $generator['name'];
         }
 
-        $key = 'view' . str_random('20');
-
 
         if (empty($links) && empty($data)) {
-            \Redis::rpush("user{$this->requestor->id}views", [
-                'key'        => $key,
-                'created_at' => Carbon::now()->toDateTimeString(),
-                'expires_at' => Carbon::now()->addWeek()->toDateTimeString(),
-                'view'       => false,
-                'message'    => 'There was an error when compiling the reports. Please try again, and if the error persists, notify CLH.',
-                'data'       => [],
-            ]);
+            $this->storeFailResponse();
 
             return;
         }
 
-        \Cache::put($key, [
-            'key'        => $key,
-            'view'       => 'billing.nurse.list',
-            'message'    => 'The Nurse Invoices you requested are ready!',
+        $this->storeViewInCache('billing.nurse.list', [
+            'invoices' => $links,
+            'data'     => $data,
+            'month'    => Carbon::parse($this->startDate)->format('F'),
+        ]);
+
+        $this->storeSuccessResponse();
+
+    }
+
+    /**
+     * Store a fail response in the requesting User's view cache, basically notifying the User that this job failed.
+     */
+    public function storeFailResponse()
+    {
+        $this->requestors->map(function ($userId) {
+            $message = 'There was an error when compiling the reports. Please try again, and if the error persists, notify CLH.';
+
+            \Redis::rpush($this->getHashKeyForUser($userId), $this->userCachedNotificationFactory($message));
+        });
+    }
+
+    /**
+     * Get the hash key for the give User's cached views list
+     *
+     * @param $userId
+     *
+     * @return string
+     */
+    public function getHashKeyForUser($userId)
+    {
+        return "user:{$userId}:views";
+    }
+
+    /**
+     * Create a User view
+     *
+     * @param $message
+     * @param bool $view
+     * @param array $data
+     *
+     * @return array
+     */
+    public function userCachedNotificationFactory($message)
+    {
+        return json_encode([
+            'key'        => $this->key,
             'created_at' => Carbon::now()->toDateTimeString(),
             'expires_at' => Carbon::now()->addWeek()->toDateTimeString(),
-            'data'       => [
-                'invoices' => $links,
-                'data'     => $data,
-                'month'    => Carbon::parse($this->startDate)->format('F'),
-            ],
+            'message'    => $message,
+        ]);
+    }
+
+    /**
+     * Store a fail response in the requesting User's view cache, basically notifying the User that this job failed.
+     */
+    public function storeViewInCache($view, $data)
+    {
+        \Cache::put($this->key, [
+            'view'       => $view,
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'expires_at' => Carbon::now()->addWeek()->toDateTimeString(),
+            'data'       => $data,
         ], 11000);
+    }
 
-        $month = Carbon::parse($this->startDate)->format('F');
+    /**
+     * Store a success response in the requesting User's view cache, basically notifying the User that this job was
+     * completed successfully.
+     */
+    public function storeSuccessResponse()
+    {
+        $message = 'Nurse Invoices';
 
-        \Redis::rpush("user{$this->requestor->id}views", json_encode([
-            'key'        => $key,
-            'created_at' => Carbon::now()->toDateTimeString(),
-            'expires_at' => Carbon::now()->addWeek()->toDateTimeString(),
-            'view'       => 'billing.nurse.list',
-            'message'    => 'The Nurse Invoices you requested are ready!',
-            'data'       => [
-                'invoices' => $links,
-                'data'     => $data,
-                'month'    => $month,
-            ],
-        ]));
+        $this->requestors->map(function ($userId) use ($message) {
+            \Redis::rpush(
+                $this->getHashKeyForUser($userId),
+                $this->userCachedNotificationFactory($message)
+            );
+        });
     }
 }
