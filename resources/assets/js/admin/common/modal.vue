a72f0fa25881db2c2e96d6feef6523ad72b66c59
Ikechi Michael <mykehell123@gmail.com>
2017-11-10 13:08:33 +0100

    doc: common components documentation

--- resources/assets/js/admin/common/modal.vue
+++ resources/assets/js/admin/common/modal.vue
@@ -39,51 +39,7 @@
 
 <script>
     /**
-     * This is a generic modal component
-     * 
-     * It depends on an EventBus component called `Event` ... feel free to replace this
-     * 
-     * Trigger with:
-     * 
-     * ```
-     * Event.$emit('modal:show', { title: 'My Modal Title', 'body': 'Hello Modal Everyone', footer: 'Cool Modal!' })
-     * ```
-     * 
-     * You can pass a name prop to the modal to differentiate it from others.
-     * 
-     * In that case, the trigger script key becomes 'modal-<name>:show` where you replace `<name>` with whatever :name value you passed to the component
-     * 
-     * You can choose to not show title or footer by passing the `no-title` and `no-footer` props which take boolean values
-     * 
-     * You may specify the templates for title, body and footer via templates. 
-     * 
-     * For Body,
-     * 
-     * <template> ... body html here ... </template>
-     * 
-     * For title,
-     * 
-     * <template slot='title'> ... title html here ... </template>
-     * 
-     * For footer,
-     * 
-     * <template slot='footer'> ... footer html here ... </template>
-     * 
-     * For custom behavior, use the template slots and pass the :info prop to the modal component.
-     * 
-     * Within the template slots, you can use scope to props e.g.
-     * 
-     * <modal :no-title="true" :no-footer="true" :info="selectNursesModalInfo">
-            <template scope="props">
-                <select class="form-control" @change="props.info.onChange">
-                    <option value="">Pick a Nurse</option>
-                    <option value="1">Nurse N RN</option>
-                    <option value="2">Kathryn Alchalabi RN</option>
-                </select>
-            </template>
-       </modal>
-
-     * Where [selectNursesModalInfo] is an object that contains the `onChange` callback
+     See README.md for docs
      */
     import { Event } from 'vue-tables-2'
 
