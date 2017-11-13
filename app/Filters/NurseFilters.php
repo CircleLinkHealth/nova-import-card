522a96f54f2e7060883e6f227ca22813d3389794
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-13 18:23:05 +0200

    Adds more filters

--- app/Filters/NurseFilters.php
+++ app/Filters/NurseFilters.php
@@ -16,28 +16,77 @@ class NurseFilters extends QueryFilters
      */
     public function active($status = 'active')
     {
+        return $this->status($status);
+    }
+
+    /**
+     * Scope nurses by status.
+     *
+     * @param string $status
+     *
+     * @return Builder
+     */
+    public function status($status = 'active')
+    {
         return $this->builder->where('status', '=', $status);
     }
 
     /**
      * Get the states the nurse is licenced in.
+     * By default the and operator is selected, which menas that only nurses that include all states will be included.
+     *
+     * @param string $states Comma delimited State Codes. Example: 'NJ, NY, GA'
+     * @param string $operator Can 'and' or 'or'
      *
      * @return Builder
      */
-    public function states($states = null)
+    public function states($states = null, $operator = 'and')
     {
         if ( ! $states) {
             return $this->builder->with('states');
         }
 
-        if (!is_array($states)) {
+        if (str_contains($states, ',')) {
+            $states = explode(',', $states);
+        }
+
+        if ( ! is_array($states)) {
             $states = [$states];
         }
 
-        $this->builder->whereHas('states', function ($q) use ($states) {
-            $q->whereIn('code', $states);
-        })->with('states');
+        if ($operator == 'and') {
+            foreach ($states as $state) {
+                $this->builder->whereHas('states', function ($q) use ($state) {
+                    $q->where('code', $state);
+                });
+            }
+        }
+
+        if ($operator == 'or') {
+            $this->builder->whereHas('states', function ($q) use ($states) {
+                $q->whereIn('code', $states);
+            });
+        }
+
+        $this->builder->with('states');
     }
 
+    /**
+     * Get nurses that are licenced in any of the states provided.
+     *
+     * @param string $states Comma delimited State Codes. Example: 'NJ, NY, GA'
+     * @param string $operator Can 'and' or 'or'
+     *
+     * @return Builder
+     */
+    public function statesOr($states = null, $operator = 'or')
+    {
+        return $this->states($states, $operator);
+    }
+
+    public function windows()
+    {
+        return $this->builder->with('windows');
+    }
 
 }
