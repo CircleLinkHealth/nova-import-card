f456533209599ce9bf0808e43f6085b3a7c11b86
Ikechi Michael <mykehell123@gmail.com>
2017-11-14 13:51:45 +0100

    chore: created ApiController to differ from Controller

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProfileController extends Controller
+class ProfileController extends ApiController
 {
     /**
      * Display a listing of the resource.
