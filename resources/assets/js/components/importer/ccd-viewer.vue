a15db7bb9b8809ce6c8f6f38d58ff272915c44a8
Ikechi Michael <mykehell123@gmail.com>
2017-11-20 18:11:54 +0100

    feat: created ccd-records-viewer

---
+++ resources/assets/js/components/importer/ccd-viewer.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script>
+    import { ClientTable } from 'vue-tables-2'
+
+    export default {
+        name: 'ccd-viewer'
+    }
+</script>
+
+<style>
+    
+</style>
