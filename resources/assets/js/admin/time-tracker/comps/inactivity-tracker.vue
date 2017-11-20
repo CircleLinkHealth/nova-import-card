6ff7a6bdf6ee1445f44684e08d2b14370106cb1e
Ikechi Michael <mykehell123@gmail.com>
2017-11-20 18:43:20 +0100

    fix: change styling of modal to be like previous

--- resources/assets/js/admin/time-tracker/comps/inactivity-tracker.vue
+++ resources/assets/js/admin/time-tracker/comps/inactivity-tracker.vue
@@ -36,7 +36,7 @@
                 this.interval = setInterval(
                     function() {
                         this.endTime = new Date();
-                        const ALERT_INTERVAL = 120;
+                        const ALERT_INTERVAL = 20;
                         const LOGOUT_INTERVAL = 300;
                         if (this.totalSeconds && ((this.totalSeconds % ALERT_INTERVAL) === 0)) {
                             /**
