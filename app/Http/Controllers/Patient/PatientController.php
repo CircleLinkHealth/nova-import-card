cfef2b2dfd6cb19e08c5f85a96526de5bf180387
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-22 20:09:23 +0200

    Disable tt on dashboard

--- app/Http/Controllers/Patient/PatientController.php
+++ app/Http/Controllers/Patient/PatientController.php
@@ -39,12 +39,16 @@ class PatientController extends Controller
         }
 
         return view('wpUsers.patient.dashboard',
-            array_merge(
-                compact([
-                    'pendingApprovals',
-                    'nurse',
-                    'showPatientsPendingApprovalBox',
-                ]), $patientsPendingApproval
+            array_merge([
+                'disableTimeTracking' => true,
+            ],
+                array_merge(
+                    compact([
+                        'pendingApprovals',
+                        'nurse',
+                        'showPatientsPendingApprovalBox',
+                    ]),
+                    $patientsPendingApproval)
             )
         );
     }
