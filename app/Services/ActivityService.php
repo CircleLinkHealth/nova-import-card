14a41de4d8cfaf48c57f30d050bc5e94d36488f1
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-22 16:18:18 +0200

    Removes unused functions

--- app/Services/ActivityService.php
+++ app/Services/ActivityService.php
@@ -10,41 +10,6 @@ use Illuminate\Support\Collection;
 class ActivityService
 {
     /**
-     * Get total activity for a range of two Carbon dates.
-     *
-     * @param $userId
-     * @param Carbon $from
-     * @param Carbon $to
-     *
-     * @return mixed
-     */
-    public function getTotalActivityTimeForRange(
-        $userId,
-        Carbon $from,
-        Carbon $to
-    ) {
-        $acts = new Collection(DB::table('lv_activities')
-                                 ->select(DB::raw('id,provider_id,logged_from,DATE(performed_at), type, SUM(duration) as duration'))
-                                 ->whereBetween('performed_at', [
-                                     $from,
-                                     $to,
-                                 ])
-                                 ->where('patient_id', $userId)
-                                 ->where(function ($q) {
-                                     $q->where('logged_from', 'activity')
-                                       ->Orwhere('logged_from', 'manual_input')
-                                       ->Orwhere('logged_from', 'pagetimer');
-                                 })
-                                 ->groupBy(DB::raw('provider_id, DATE(performed_at),type'))
-                                 ->orderBy('performed_at', 'desc')
-                                 ->get());
-
-        return $acts->map(function ($act) {
-            return $act->duration;
-        })->sum();
-    }
-
-    /**
      * Process activity time for month
      *
      * @param array $userIds
@@ -73,49 +38,5 @@ class ActivityService
 
             (new PatientMonthlySummary())->updateCCMInfoForPatient($info, $ccmTime);
         }
-    }
-
-    public function getTotalActivityTimeForMonth(
-        $userId,
-        $month = false,
-        $year = false
-    ) {
-        // if no month, set to current month
-        if ( ! $month) {
-            $month = date('m');
-        }
-        if ( ! $year) {
-            $year = date('Y');
-        }
-
-        $time                = Carbon::createFromDate($year, $month, 15);
-        $start               = $time->startOfMonth()->format('Y-m-d') . ' 00:00:00';
-        $end                 = $time->endOfMonth()->format('Y-m-d') . ' 23:59:59';
-        $month_selected      = $time->format('m');
-        $month_selected_text = $time->format('F');
-        $year_selected       = $time->format('Y');
-
-        $acts = DB::table('lv_activities')
-                  ->select(DB::raw('id,provider_id,logged_from, performed_at, type, SUM(duration) as duration'))
-                  ->whereBetween('performed_at', [
-                      $start,
-                      $end,
-                  ])
-                  ->where('patient_id', $userId)
-                  ->where(function ($q) {
-                      $q->where('logged_from', 'activity')
-                        ->Orwhere('logged_from', 'manual_input')
-                        ->Orwhere('logged_from', 'pagetimer');
-                  })
-                  ->groupBy(DB::raw('provider_id, performed_at,type'))
-                  ->orderBy('performed_at', 'desc')
-                  ->get();
-
-        $totalDuration = 0;
-        foreach ($acts as $act) {
-            $totalDuration = ($totalDuration + $act->duration);
-        }
-
-        return $totalDuration;
     }
 }
