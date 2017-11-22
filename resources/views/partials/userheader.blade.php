e0855d0fafc0065bc1c87b65f1c0449125268627
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-11-22 20:38:31 +0200

    Adds view composer #749

--- resources/views/partials/userheader.blade.php
+++ resources/views/partials/userheader.blade.php
@@ -1,33 +1,3 @@
-<?php
-// calculate display, fix bug where gmdate('i:s') doesnt work for > 24hrs
-
-$seconds = optional($patient->patientInfo)->cur_month_activity_time ?? 0;
-
-$H = floor($seconds / 3600);
-$i = ($seconds / 60) % 60;
-$s = $seconds % 60;
-$monthlyTime = sprintf("%02d:%02d:%02d", $H, $i, $s);
-$ccm_above = false;
-
-$ccm_complex = false;
-if ($patient->patientInfo) {
-    $ccm_complex = $patient->patientInfo->isCCMComplex() ?? false;
-}
-
-if ($seconds > 1199 && !$ccm_complex) {
-    $ccm_above = true;
-} elseif ($seconds > 3599 && $ccm_complex) {
-    $ccm_above = true;
-}
-
-$provider = optional($patient->billingProviderUser())->fullName ?? 'No Provider Selected';
-
-$location = empty($patient->getPreferredLocationName())
-    ? 'Not Set'
-    : $patient->getPreferredLocationName();
-
-?>
-
 <div class="main-form-block main-form-horizontal main-form-primary-horizontal col-md-12" style="padding-bottom:9px">
     <div class="row">
         <div class="col-sm-12">
