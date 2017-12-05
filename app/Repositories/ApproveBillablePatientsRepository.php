df493577add4e0514efc23a089db4e4bd8a8c947
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-12-05 18:53:31 +0100

    Adds Repository #1010

---
+++ app/Repositories/ApproveBillablePatientsRepository.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 12/05/2017
+ * Time: 6:39 PM
+ */
+
+namespace App\Repositories;
+
+
+use App\User;
+use Carbon\Carbon;
+
+class ApproveBillablePatientsRepository
+{
+    public function billablePatients($practiceId, Carbon $date)
+    {
+        $month = $date->firstOfMonth()->toDateString();
+
+        return User::with([
+            'ccdProblems'      => function ($query) {
+                $query->whereNotNull('cpm_problem_id');
+            },
+            'billableProblems',
+            'patientSummaries' => function ($query) use ($month) {
+                $query->where('month_year', $month)
+                      ->where('ccm_time', '>=', 1200);
+            },
+            'cpmProblems',
+            'patientInfo',
+        ])
+                   ->has('patientInfo')
+                   ->whereHas('patientSummaries', function ($query) use ($month) {
+                       $query->where('month_year', $month)
+                             ->where('ccm_time', '>=', 1200)
+                             ->with(['billableProblem1', 'billableProblem2']);
+                   })
+                   ->ofType('participant')
+                   ->where('program_id', '=', $practiceId)
+                   ->get();
+    }
+}
