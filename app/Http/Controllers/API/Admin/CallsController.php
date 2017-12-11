7221e980b105ce10d5b57bdfb0789731535956c7
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-12-11 14:55:50 +0100

    Brings api routes back to web.php #853

--- app/Http/Controllers/API/Admin/CallsController.php
+++ app/Http/Controllers/API/Admin/CallsController.php
@@ -355,65 +355,6 @@ class CallsController extends ApiController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param string $ids
@@ -421,7 +362,7 @@ class CallsController extends ApiController
      * @return \Illuminate\Http\Response
      *
      */
-    public function destroy($ids)
+    public function deleteCalls($ids)
     {
         if (str_contains($ids, ',')) {
             $ids = explode(',', $ids);
