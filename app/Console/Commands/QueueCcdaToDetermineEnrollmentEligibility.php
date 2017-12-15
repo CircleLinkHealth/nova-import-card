3bd5be8f29a086ed9583a0e850a905d047b5cea4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-12-15 15:02:21 +0200

    #1042 bump rate up to 2000 [deploy:worker]

--- app/Console/Commands/QueueCcdaToDetermineEnrollmentEligibility.php
+++ app/Console/Commands/QueueCcdaToDetermineEnrollmentEligibility.php
@@ -46,7 +46,7 @@ class QueueCcdaToDetermineEnrollmentEligibility extends Command
             ['status', '=', Ccda::DETERMINE_ENROLLEMENT_ELIGIBILITY],
         ])->whereNotNull('mrn')
                     ->inRandomOrder()
-                    ->take(30)
+                    ->take(2000)
                     ->get(['id', 'practice_id'])
                     ->map(function ($ccda) {
                         //lgh
