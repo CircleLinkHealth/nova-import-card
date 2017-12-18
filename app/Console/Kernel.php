ac48198ba8eae714365e4ec4bf957da7353dcf21
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-12-18 18:12:20 +0200

    #1044 removes job from  scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -65,9 +65,6 @@ class Kernel extends ConsoleKernel
         $schedule->command('athena:getAppointments')
             ->dailyAt('23:00');
 
-//        $schedule->command('athena:getPatientIdFromAppointments')
-//            ->dailyAt('23:30');
-
         $schedule->command('athena:getCcds')
             ->everyThirtyMinutes();
 
