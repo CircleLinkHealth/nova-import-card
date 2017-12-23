c8ccf9bc57e30a06f611a48d674d9a6904ed4e3e
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-12-23 14:24:46 +0200

    small change [deploy:production]

--- resources/views/partials/providerUI/primarynav.blade.php
+++ resources/views/partials/providerUI/primarynav.blade.php
@@ -31,7 +31,7 @@
             <ul class="nav navbar-nav">
                 @if (!isset($patient))
                     <li data-monthly-time="{{$monthlyTime}}" style="padding-top: 15px; padding-bottom: 15px; line-height: 20px;">
-                        <time-tracker ref="TimeTrackerApp" :info="timeTrackerInfo" :hide-tracker="true" :no-live-count="{{isset($noLiveCountTimeTracking) ? true : false}}"></time-tracker>
+                        <time-tracker ref="TimeTrackerApp" :info="timeTrackerInfo" :hide-tracker="true" :no-live-count="{{isset($noLiveCountTimeTracking)}}"></time-tracker>
                     </li>
                 @endif
                 <li>
