b72c02c169d522d0464b476b9010661f7d46e2f5
Ikechi Michael <mykehell123@gmail.com>
2017-12-26 10:21:38 +0100

    feat: create ccd problem repo and service

---
+++ app/Repositories/CcdProblemRepository.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 12/07/2017
+ * Time: 12:32 PM
+ */
+
+namespace App\Repositories;
+
+
+use App\User;
+use App\Patient;
+use App\Models\CCD\Problem;
+use Prettus\Repository\Contracts\RepositoryInterface;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class CcdProblemRepository extends BaseRepository implements RepositoryInterface
+{
+    public function model()
+    {
+        return Problem::class;
+    }
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+    public function validator()
+    {
+        return null;
+    }
+
+    public function count() {
+        $this->applyCriteria();
+        $this->applyScope();
+
+        $result = $this->model->select('name', DB::raw('count(*) as total'))->groupBy('name')->pluck('total')->count();
+
+        $this->resetModel();
+        $this->resetScope();
+
+        return $result;
+    }
+}
---
+++ app/Services/CCD/CcdProblemService.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 5/3/16
+ * Time: 2:19 PM
+ */
+
+namespace App\Services\CPM;
+
+use App\User;
+use App\Repositories\UserRepositoryEloquent;
+use App\Repositories\CcdProblemRepository;
+
+class CcdProblemService
+{
+    private $problemRepo;
+    private $userRepo;
+
+    public function __construct(CcdProblemRepository $problemRepo, UserRepositoryEloquent $userRepo) {
+        $this->problemRepo = $problemRepo;
+        $this->userRepo = $userRepo;
+    }
+
+    public function repo() {
+        return $this->problemRepo;
+    }
+}
