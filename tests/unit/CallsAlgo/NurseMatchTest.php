2a88409928282cc7e1d6781140add3e5946f01aa
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2017-12-28 21:23:37 +0200

    fixes nurse match test

--- tests/unit/CallsAlgo/NurseMatchTest.php
+++ tests/unit/CallsAlgo/NurseMatchTest.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Tests\Calls;
+namespace Tests\Unit\CallsAlgo;
 
-use Tests\TestCase;
 use App\Algorithms\Calls\CallAlgoHelper;
-use App\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\Helpers\UserHelpers;
+use Tests\TestCase;
 
 class NurseMatchTest extends TestCase
 {
@@ -18,35 +19,36 @@ class NurseMatchTest extends TestCase
     private $prediction = [];
     private $matchArray = [];
 
-    use UserHelpers, CallAlgoHelper;
+    use DatabaseTransactions,
+        UserHelpers,
+        CallAlgoHelper;
 
     public function testNursesMatchTest()
     {
-
         //init mock algo predictions
-        $this->prediction['date'] = '2016-12-19';
+        $this->prediction['date']         = '2016-12-19';
         $this->prediction['window_start'] = '09:00:00';
-        $this->prediction['window_end'] = '17:00:00';
+        $this->prediction['window_end']   = '17:00:00';
 
         $this->practice = \App\Practice::create([
 
-            'name' => 'program' . \Carbon\Carbon::now()->secondsSinceMidnight()
+            'name' => 'program' . Carbon::now()->secondsSinceMidnight(),
 
         ]);
 
         //create main nurse
-        $nurse = $this->createUser($this->practice->id, 'care-center');
-        $this->nurse = $nurse->nurseInfo;
+        $nurse                      = $this->createUser($this->practice->id, 'care-center');
+        $this->nurse                = $nurse->nurseInfo;
         $this->prediction['Nurse1'] = $nurse->fullName;
 
         //create nurse with matching window
-        $nurse2 = $this->createUser($this->practice->id, 'care-center');
-        $this->nurse2 = $nurse2->nurseInfo;
+        $nurse2                     = $this->createUser($this->practice->id, 'care-center');
+        $this->nurse2               = $nurse2->nurseInfo;
         $this->prediction['Nurse2'] = $nurse2->fullName;
 
 
-        $patient = $this->createUser($this->practice->id, 'participant');
-        $this->patient = $patient->patientInfo;
+        $patient                     = $this->createUser($this->practice->id, 'participant');
+        $this->patient               = $patient->patientInfo;
         $this->prediction['Patient'] = $patient->fullName;
 
 
@@ -57,19 +59,17 @@ class NurseMatchTest extends TestCase
 
         $this->createWindowForNurse(
             $this->nurse,
-            Carbon\Carbon::parse('2016-12-17 08:00:00'),
-            Carbon\Carbon::parse('2016-12-17 11:00:00')
+            Carbon::parse('2016-12-17 08:00:00'),
+            Carbon::parse('2016-12-17 11:00:00')
         );
 
         $this->createWindowForNurse(
             $this->nurse2,
-            Carbon\Carbon::parse('2016-12-21 08:00:00'),
-            Carbon\Carbon::parse('2016-12-21 11:00:00')
+            Carbon::parse('2016-12-21 08:00:00'),
+            Carbon::parse('2016-12-21 11:00:00')
         );
 
         $this->findNurse();
-
-        dd($this->prediction);
 
         $this->assertTrue($this->prediction['nurse'] == $this->nurse->user_id);
     }
@@ -80,8 +80,8 @@ class NurseMatchTest extends TestCase
         for ($i = 1; $i < 6; $i++) {
             $windows[] = $this->createWindowForPatient(
                 $this->patient,
-                Carbon\Carbon::parse('10:00:00'),
-                Carbon\Carbon::parse('17:00:00'),
+                Carbon::parse('10:00:00'),
+                Carbon::parse('17:00:00'),
                 $i
             );
         }
