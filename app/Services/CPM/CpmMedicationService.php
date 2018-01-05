cc35130e65b2516a35fa0b123dbd5fa95260aeb1
Ikechi Michael <mykehell123@gmail.com>
2018-01-05 12:13:40 +0100

    feat: create services for medication and its groups

---
+++ app/Services/CPM/CpmMedicationService.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 5/3/16
+ * Time: 2:20 PM
+ */
+
+namespace App\Services\CPM;
+
+use App\User;
+use App\Repositories\CpmMedicationRepository;
+
+class CpmMedicationService
+{
+    private $medicationRepo;
+
+    public function __construct(CpmMedicationRepository $medicationRepo) {
+        $this->medicationRepo = $medicationRepo;
+    }
+
+    public function repo() {
+        return $this->medicationRepo;
+    }
+}
