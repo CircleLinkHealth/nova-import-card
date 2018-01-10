aea75b26bee09b79eadfe2bcb777bae48d2dc7af
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-01-10 21:23:08 +0200

    #1078 adds route

--- routes/web.php
+++ routes/web.php
@@ -663,6 +663,11 @@ Route::group(['middleware' => 'auth'], function () {
             'as'   => 'get.athena.ccdas',
         ]);
 
+        Route::get('print/patients/letters/paused', [
+            'uses' => 'ReportsController@pausedPatientsLetterPrintList',
+            'as'   => 'get.print.paused.letters'
+        ]);
+
         /**
          * LOGGER
          */
