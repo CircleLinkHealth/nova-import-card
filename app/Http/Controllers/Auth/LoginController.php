677d92811f4f54cf2e0ae6477dbaf46a5aafc4a4
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-01-15 19:20:40 +0200

    #1145 prevent login or show warning if browser is IE

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Jenssegers\Agent\Agent;
 
 class LoginController extends Controller
 {
@@ -19,7 +21,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        login as traitLogin;
+    }
 
     /**
      * Where to redirect users after login.
@@ -39,8 +43,33 @@ class LoginController extends Controller
     public function __construct(Request $request)
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
 
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return void
+     */
+    public function login(Request $request)
+    {
         $this->usernameOrEmail($request);
+        $loginResponse = $this->traitLogin($request);
+
+        if ( ! $this->validateBrowserCompatibility()) {
+            $this->sendInvalidBrowserResponse();
+        }
+
+        return $loginResponse;
     }
 
     /**
@@ -52,13 +81,13 @@ class LoginController extends Controller
      */
     public function usernameOrEmail(Request $request)
     {
-        if (!$request->filled('email')) {
+        if ( ! $request->filled('email')) {
             return false;
         }
 
         $request->merge(array_map('trim', $request->input()));
 
-        if (!str_contains($request->input('email'), '@')) {
+        if ( ! str_contains($request->input('email'), '@')) {
             $this->username = 'username';
 
             $request->merge([
@@ -68,12 +97,38 @@ class LoginController extends Controller
     }
 
     /**
-     * Get the login username to be used by the controller.
+     * Check whether the user is using a supported browser.
      *
-     * @return string
+     * @return bool
      */
-    public function username()
+    public function validateBrowserCompatibility()
     {
-        return $this->username;
+        $agent = new Agent();
+
+        if ( ! $agent->isChrome()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function sendInvalidBrowserResponse()
+    {
+        $messages = [
+            'invalid-browser' => "I'm sorry, you may be using a version of IE that we don't support. We recommend you use Chrome. If you must use IE, please use IE11 or later.",
+        ];
+
+        if (auth()->user()->hasRole('care-center')) {
+            auth()->logout();
+
+            $messages = [
+                'invalid-browser-force-switch' => 'Care Coaches are required to use Chrome. Please switch to Chrome and try logging in again.',
+            ];
+        }
+
+        throw ValidationException::withMessages($messages);
     }
 }
