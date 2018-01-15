d25d441c7138c9c03bb3c0646eb8a5547c84fb99
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-01-15 21:02:02 +0200

    Revert "fixes tester email [deploy:worker]"
    This reverts commit 8205386

--- app/Console/Commands/EmailWeeklyReports.php
+++ app/Console/Commands/EmailWeeklyReports.php
@@ -46,10 +46,10 @@ class EmailWeeklyReports extends Command
      */
     public function handle()
     {
-        $testerEmail = null;
+        $tester = null;
 
         if ($this->argument('testerUserId')) {
-            $testerEmail = User::findOrFail($this->argument('testerUserId'))->email;
+            $tester = User::findOrFail($this->argument('testerUserId'));
         }
 
         if ($this->argument('practiceId')) {
@@ -60,10 +60,10 @@ class EmailWeeklyReports extends Command
         $endRange = Carbon::now()->endOfDay();
 
         if (isset($onlyForPractice)) {
-            dispatch(new EmailWeeklyPracticeReport($onlyForPractice, $startRange, $endRange, $testerEmail));
+            dispatch(new EmailWeeklyPracticeReport($onlyForPractice, $startRange, $endRange, $tester));
 
             if ($this->option('provider')) {
-                dispatch(new EmailWeeklyProviderReport($onlyForPractice, $startRange, $endRange, $testerEmail));
+                dispatch(new EmailWeeklyProviderReport($onlyForPractice, $startRange, $endRange, $tester));
             }
 
             return;
@@ -71,11 +71,11 @@ class EmailWeeklyReports extends Command
 
         foreach ($this->activePractices as $practice) {
             if ($practice->settings->first() && $practice->settings->first()->email_weekly_report && $this->option('provider')) {
-                dispatch(new EmailWeeklyProviderReport($practice, $startRange, $endRange, $testerEmail));
+                dispatch(new EmailWeeklyProviderReport($practice, $startRange, $endRange, $tester));
             }
 
             if ($this->option('practice')) {
-                dispatch(new EmailWeeklyPracticeReport($practice, $startRange, $endRange, $testerEmail));
+                dispatch(new EmailWeeklyPracticeReport($practice, $startRange, $endRange, $tester));
             }
         }
     }
