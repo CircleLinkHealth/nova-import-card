bcbd5309de6a8ee56abb630dec04519d44fb1a3c
Ikechi Michael <mykehell123@gmail.com>
2018-01-15 09:48:04 +0100

    feat: sort appointments in descending order by date

--- resources/assets/js/components/careplan/appointments.vue
+++ resources/assets/js/components/careplan/appointments.vue
@@ -95,7 +95,7 @@
                     const pagination = response.data
                     console.log('appointments:get-appointments', pagination)
                     this.appointments = this.appointments.concat(pagination.data.map(this.setupAppointment))
-                    this.appointments = this.appointments.sort((a, b) => a.at > b.at ? 1 : -1)
+                    this.appointments.sort((a, b) => a.at < b.at ? 1 : -1)
                     if (pagination.to < pagination.total) return this.getAppointments(page + 1)
                 }).catch(err => {
                     console.error('appointments:get-appointments', err)
