60c17216d7dc5fc638162cd833407d8e0048b9c9
kakoushias <kakoushias@gmail.com>
2018-01-16 23:43:07 +0200

    #1209 edits migrations/adds new codes

--- database/migrations/2018_01_11_170424_attach_data_to_chargeable_services_table.php
+++ database/migrations/2018_01_11_170424_attach_data_to_chargeable_services_table.php
@@ -15,13 +15,44 @@ class AttachDataToChargeableServicesTable extends Migration
     {
         $service = ChargeableService::updateOrCreate([
             'code'        => 'CPT99490',
-            'description' => 'CPT for 99490 is defined as "clinical staff time directed by a physician or other Qualified Health Care Provider (QHCP)".',
-            'amount'      => null,
+            'description' => 'CCCM Services over 20 mins (1 month).',
+            'amount'      => 29,
         ]);
 
         foreach (Practice::active()->get() as $practice) {
             $practice->chargeableServices()->attach($service->id);
         }
+
+        ChargeableService::updateOrCreate([
+            'code'        => 'CPT 99487',
+            'description' => 'Complex CCM over 60 mins (1 month)',
+            'amount'      => 70,
+        ]);
+
+        ChargeableService::updateOrCreate([
+            'code'        => 'CPT 99489',
+            'description' => 'Complex CCM additional 30 mins (1 month)',
+            'amount'      => 35,
+        ]);
+
+        ChargeableService::updateOrCreate([
+            'code'        => 'CPT 99484',
+            'description' => 'Behavioural Health Services over 20 mins (1 month)',
+            'amount'      => 33,
+        ]);
+
+        ChargeableService::updateOrCreate([
+            'code'        => 'G0506',
+            'description' => 'Enrollment in office & Care Planning by Provider',
+            'amount'      => 9.99,
+        ]);
+
+        ChargeableService::updateOrCreate([
+            'code'        => 'G0511',
+            'description' => 'FQHC / RHC General Care Management (1 month)',
+            'amount'      => 41,
+        ]);
+
     }
 
     /**
