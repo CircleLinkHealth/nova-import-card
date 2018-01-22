153bd374abb63cd2e0fdbe17c8f0aca2af937346
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-01-22 17:30:21 +0200

    Adds cloud storage drive

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,14 @@ return [
             'root'   => storage_path('app'),
         ],
 
+        'cloud' => [
+            'driver' => 's3',
+            'key'    => env('S3_CPM_STORAGE_KEY'),
+            'secret' => env('S3_CPM_STORAGE_SECRET'),
+            'region' => env('S3_CPM_STORAGE_REGION'),
+            'bucket' => env('S3_CPM_STORAGE_BUCKET_NAME'),
+        ],
+
         'public' => [
             'driver'     => 'local',
             'root'       => storage_path('app/public'),
