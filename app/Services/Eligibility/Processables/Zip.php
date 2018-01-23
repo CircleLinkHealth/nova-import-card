add519a0b6aec573e119222e7110bcd18589abf0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-01-23 12:37:02 +0200

    change xml perms to 775 [deploy:worker]

--- app/Services/Eligibility/Processables/Zip.php
+++ app/Services/Eligibility/Processables/Zip.php
@@ -11,9 +11,7 @@ namespace App\Services\Eligibility\Processables;
 
 use App\Jobs\CheckCcdaEnrollmentEligibility;
 use App\Jobs\ProcessCcda;
-use App\Jobs\ProcessEligibilityProcessable;
 use App\Models\MedicalRecords\Ccda;
-use App\Services\AthenaAPI\DetermineEnrollmentEligibility;
 use Carbon\Carbon;
 use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
@@ -45,9 +43,40 @@ class Zip extends BaseProcessable
             $deleted = \Storage::disk('storage')->delete($filePath);
 
             ProcessCcda::withChain([
-                new CheckCcdaEnrollmentEligibility($ccda->id, $this->practice, $this->filterLastEncounter, $this->filterInsurance, $this->filterProblems)
+                new CheckCcdaEnrollmentEligibility($ccda->id, $this->practice, $this->filterLastEncounter,
+                    $this->filterInsurance, $this->filterProblems),
             ])->dispatch($ccda);
         }
+    }
+
+    /**
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     * @throws \Exception
+     */
+    public function queue()
+    {
+        if (is_a($this->getFile(), UploadedFile::class) || is_a($this->getFile(), File::class)) {
+            $date     = Carbon::now();
+            $relDir   = "{$date->toDateString()}/unzip/{$this->practice->name}/{$date->toTimeString()}";
+            $dir      = storage_path($relDir);
+            $fileName = 'unzip-' . $this->practice->name . '-' . Carbon::now()->toTimeString() . '.zip';
+
+            $dirPerms = mkdir($dir, 0775, true);
+
+            $this->getFile()->move($dir, $fileName);
+
+            $changed = \Storage::disk('storage')->setVisibility("$relDir/$fileName", 'public');
+
+            $this->setFile("$dir/$fileName");
+
+            $changeFileNamePerms = chmod("$dir/$fileName", 0775);
+
+            $this->relativeDirectory = $relDir;
+
+            $this->unzip();
+        }
+
+        parent::queue();
     }
 
     /**
@@ -72,38 +101,12 @@ class Zip extends BaseProcessable
         $zip = ZipFacade::open($path);
         $zip->extract($dir);
 
+        foreach (glob("$dir/*xml") as $filePath) {
+            $changeFilePerms = chmod($filePath, 0775);
+        }
+
         $deleted = \Storage::disk('storage')->delete(str_replace(storage_path(), '', $path));
 
         return $dir;
-    }
-
-    /**
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
-     * @throws \Exception
-     */
-    public function queue()
-    {
-        if (is_a($this->getFile(), UploadedFile::class) || is_a($this->getFile(), File::class)) {
-            $date     = Carbon::now();
-            $relDir   = "{$date->toDateString()}/unzip/{$this->practice->name}/{$date->toTimeString()}";
-            $dir      = storage_path($relDir);
-            $fileName = 'unzip-' . $this->practice->name . '-' . Carbon::now()->toTimeString() . '.zip';
-
-            $dirPerms = mkdir($dir, 0775, true);
-
-            $this->getFile()->move($dir, $fileName);
-
-            $changed = \Storage::disk('storage')->setVisibility("$relDir/$fileName",'public');
-
-            $this->setFile("$dir/$fileName");
-
-            $changeFileNamePerms = chmod("$dir/$fileName", 0775);
-
-            $this->relativeDirectory = $relDir;
-
-            $this->unzip();
-        }
-
-        parent::queue();
     }
 }
