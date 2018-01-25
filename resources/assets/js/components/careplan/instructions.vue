20c0516c6b42b413989306f59e9785043932fa4b
Ikechi Michael <mykehell123@gmail.com>
2018-01-25 11:52:54 +0100

    chore: show unique problems with duplicate names in instructions component

--- resources/assets/js/components/careplan/instructions.vue
+++ resources/assets/js/components/careplan/instructions.vue
@@ -61,7 +61,7 @@
         },
         computed: {
             cpmProblemsWithInstructions() {
-                return this.cpmProblems.filter(problem => problem.instruction.name).concat(this.ccdProblems.filter(problem => problem.instruction.name))
+                return this.cpmProblems.filter(problem => problem.instruction.name).concat(this.ccdProblems.filter(problem => problem.instruction.name)).distinct(problem => problem.name)
             }
         },
         methods: {
