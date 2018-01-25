945e54a363b86268fcc727e9f7d320c533163a5b
Ikechi Michael <mykehell123@gmail.com>
2018-01-25 13:04:39 +0100

    feat: make sure health-goal enabled status does not change if confirm() fails #1166

--- resources/assets/js/components/careplan/health-goals.vue
+++ resources/assets/js/components/careplan/health-goals.vue
@@ -56,7 +56,7 @@
         mixins: [ CareplanMixin ],
         computed: {
             goalsForListing () {
-                return this.goals.filter(goal => goal.enabled)
+                return this.goals.filter(goal => goal.enabled && goal.active())
             }
         },
         data() {
@@ -83,8 +83,9 @@
                     goal.info.created_at = new Date(goal.info.created_at)
                     goal.info.updated_at = new Date(goal.info.updated_at)
                     goal.info.monitor_changes_for_chf = goal.info.monitor_changes_for_chf || false
-                    goal.start = () => Number((goal.info.starting || '') || '0')
-                    goal.end = () => Number((goal.info.target || '') || '0')
+                    goal.start = () => goal.info.starting
+                    goal.end = () => goal.info.target
+                    goal.active = () => !!(goal.info.starting && goal.info.target)
                     
                     if (goal.start() > goal.end()) {
                         goal.info.verb = 'Decrease'
