19dc1333d03550ced23ae295801a31c7d23d06fb
Ikechi Michael <mykehell123@gmail.com>
2018-02-01 13:57:04 +0100

    feat: care-team issues #1166

--- resources/assets/js/api/care-person.js
+++ resources/assets/js/api/care-person.js
@@ -1,14 +1,22 @@
 export default {
-    destroyCarePerson (cb, ecb = null, carePerson) {
+    destroyCarePerson (cb, ecb = () => ({}), carePerson) {
         window.axios.delete('user/' + carePerson.user_id + '/care-team/' + carePerson.id).then(
             (resp) => cb(carePerson),
-            (resp) => ecb(resp.data)
+            (resp) => {
+                if (typeof(ecb) == 'function') {
+                    ecb(resp.data)
+                }
+            }
         );
     },
-    updateCarePerson (cb, ecb = null, carePerson) {
+    updateCarePerson (cb, ecb = () => ({}), carePerson) {
         window.axios.patch('user/' + carePerson.user_id + '/care-team/' + carePerson.id, carePerson).then(
             (resp) => cb(carePerson),
-            (resp) => ecb(resp.data)
+            (resp) => {
+                if (typeof(ecb) == 'function') {
+                    ecb(resp.data)
+                }
+            }
         );
     },
 }
