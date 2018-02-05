54cf482934a136b548f4da6681701f71bd76cc90
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-02-05 15:39:14 +0200

    #1168 seed chargeable services with saas prices

--- database/migrations/2018_01_11_170424_attach_data_to_chargeable_services_table.php
+++ database/migrations/2018_01_11_170424_attach_data_to_chargeable_services_table.php
@@ -15,42 +15,44 @@ class AttachDataToChargeableServicesTable extends Migration
     {
         $service = ChargeableService::updateOrCreate([
             'code'        => 'CPT 99490',
+        ], [
             'description' => 'CCM Services over 20 mins (1 month).',
-            'amount'      => 29,
+            'amount'      => 42.21,
         ]);
-
-        foreach (Practice::active()->get() as $practice) {
-            $practice->chargeableServices()->attach($service->id);
-        }
 
         ChargeableService::updateOrCreate([
             'code'        => 'CPT 99487',
+        ], [
             'description' => 'Complex CCM over 60 mins (1 month)',
-            'amount'      => 70,
+            'amount'      => 92.66,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'CPT 99489',
+        ], [
             'description' => 'Complex CCM additional 30 mins (1 month)',
-            'amount'      => 35,
+            'amount'      => 46.87,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'CPT 99484',
+        ], [
             'description' => 'Behavioural Health Services over 20 mins (1 month)',
-            'amount'      => 33,
+            'amount'      => 48.00,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'G0506',
+        ], [
             'description' => 'Enrollment in office & Care Planning by Provider',
-            'amount'      => 9.99,
+            'amount'      => 62.00,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'G0511',
+        ], [
             'description' => 'FQHC / RHC General Care Management (1 month)',
-            'amount'      => 41,
+            'amount'      => 61.00,
         ]);
 
     }
