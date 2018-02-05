bea014b5964e7e9ecf897f2f74c50fe46964c2d3
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-02-05 18:10:32 +0200

    #1168 fix

--- resources/views/partials/adminUI.blade.php
+++ resources/views/partials/adminUI.blade.php
@@ -246,7 +246,7 @@
                                     SaaS Accounts<span class="caret"></span>
                                 </a>
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ route('locations.index', []) }}">Create New</a></li>
+                                    <li><a href="{{ route('saas-accounts.create', []) }}">Create New</a></li>
                                 </ul>
                             </li>
 
