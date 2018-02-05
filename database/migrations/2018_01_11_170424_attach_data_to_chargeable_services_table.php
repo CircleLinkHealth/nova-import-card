9a69c42cf885cbe207b8fce8ccb2e0ff63f150d9
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-02-05 18:47:08 +0200

    #1168 makes prices null

--- database/migrations/2018_01_11_170424_attach_data_to_chargeable_services_table.php
+++ database/migrations/2018_01_11_170424_attach_data_to_chargeable_services_table.php
@@ -17,42 +17,42 @@ class AttachDataToChargeableServicesTable extends Migration
             'code'        => 'CPT 99490',
         ], [
             'description' => 'CCM Services over 20 mins (1 month).',
-            'amount'      => 42.21,
+            'amount'      => null,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'CPT 99487',
         ], [
             'description' => 'Complex CCM over 60 mins (1 month)',
-            'amount'      => 92.66,
+            'amount'      => null,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'CPT 99489',
         ], [
             'description' => 'Complex CCM additional 30 mins (1 month)',
-            'amount'      => 46.87,
+            'amount'      => null,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'CPT 99484',
         ], [
             'description' => 'Behavioural Health Services over 20 mins (1 month)',
-            'amount'      => 48.00,
+            'amount'      => null,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'G0506',
         ], [
             'description' => 'Enrollment in office & Care Planning by Provider',
-            'amount'      => 62.00,
+            'amount'      => null,
         ]);
 
         ChargeableService::updateOrCreate([
             'code'        => 'G0511',
         ], [
             'description' => 'FQHC / RHC General Care Management (1 month)',
-            'amount'      => 61.00,
+            'amount'      => null,
         ]);
 
     }
