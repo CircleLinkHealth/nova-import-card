585bb8bd2f05c1b623245d2c0a9698afffd252cd
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-02-09 19:30:32 +0200

    fixes [deploy:worker-master]

--- app/Services/ApproveBillablePatientsService.php
+++ app/Services/ApproveBillablePatientsService.php
@@ -42,7 +42,7 @@ class ApproveBillablePatientsService
     public function patientsToApprove($practiceId, Carbon $month)
     {
         $summaries = $this->approvePatientsRepo->billablePatients($practiceId, $month)
-                                         ->paginate();
+                                         ->paginate(10);
         $summaries->getCollection()->transform(function ($u) {
             return $this->patientSummaryRepo->attachBillableProblems($u,
                 $u->patientSummaries->first());
