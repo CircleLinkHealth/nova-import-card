51408958c9753fcb8707fc65019a15b356a304a7
Ikechi Michael <mykehell123@gmail.com>
2018-03-27 20:51:09 +0100

    feat: add logout activity to lv_page_timer #1613

---
+++ app/Listeners/UserLoggedOut.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Listeners;
+
+use App\PageTimer;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Events\Logout;
+use Carbon\Carbon;
+
+class UserLoggedOut
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        $user = $event->user;
+        if ($user) {
+            $activity = new PageTimer();
+            $activity->duration = 0;
+            $activity->billable_duration = 0;
+            $activity->duration_unit = 'seconds';
+            $activity->activity_type = 'logout';
+            $activity->title = 'Logout';
+            $activity->url_short = '/auth/logout/';
+            $activity->url_full = url()->current();
+            $activity->patient_id = $user->id;
+            $activity->start_time = Carbon::now();
+            $activity->end_time = Carbon::now();
+            $activity->program_id = $user->program_id;
+            $activity->save();
+        }
+        else {
+            dd($event);
+            throw new \Exception('event:logout: user not found');
+        }
+    }
+}
