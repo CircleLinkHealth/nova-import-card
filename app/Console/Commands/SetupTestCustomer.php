60ff1f7ef2492537a1eee3a5626b1cfe127130c2
kakoushias <kakoushias@gmail.com>
2018-04-12 14:56:37 +0300

    #1640 refactors UserHelpers and SetupTestCustomer traits, creates command to create Customer data, changes default amount of patients to 50.

---
+++ app/Console/Commands/SetupTestCustomer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Tests\Helpers\SetupTestCustomer as SetupTestCustomerTrait;
+
+class SetupTestCustomer extends Command
+{
+    use SetupTestCustomerTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:setupTestCustomer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a Practice, a Location, a Provider and 50 Patients with various ccm_status, careplan(draft), 5 cpmProblems, chargeable service, patientMonthlySummaries and activities. Everything is related to eachother.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->createTestCustomerData();
+
+    }
+}
