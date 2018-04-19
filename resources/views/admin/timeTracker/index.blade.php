6401b279fa0653b3a058cb4e52402049d167f429
Ikechi Michael <mykehell123@gmail.com>
2018-04-19 01:31:58 +0100

    feat: create page to view time-tracker events #1781

---
+++ resources/views/admin/timeTracker/index.blade.php
@@ -0,0 +1,18 @@
+@extends('partials.adminUI')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Time Tracker Events</h1>
+        </div>
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
