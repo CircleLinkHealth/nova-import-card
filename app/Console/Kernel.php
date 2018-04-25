47f593d83c40a38718490e49bf71ff55806ddd02
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-04-25 19:39:14 +0300

    delete calls every five minutes [deploy:worker]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(SyncFamilialCalls::class)->dailyAt('00:30');
 
         //Removes All Scheduled Calls for patients that are withdrawn
-        $schedule->command(RemoveScheduledCallsForWithdrawnAndPausedPatients::class)->everyMinute()->withoutOverlapping();
+        $schedule->command(RemoveScheduledCallsForWithdrawnAndPausedPatients::class)->everyFiveMinutes();
 
 //        $schedule->command(EmailWeeklyReports::class, ['--practice', '--provider'])
 //                 ->weeklyOn(1, '10:00');
