d590955c63a7dac16d9643f22976004a72cd31e3
kakoushias <kakoushias@gmail.com>
2018-04-27 16:03:52 +0300

    #1826 small js change (var to let(

--- resources/views/admin/observations/dashboard/index.blade.php
+++ resources/views/admin/observations/dashboard/index.blade.php
@@ -82,7 +82,7 @@
                 </table>
                 <script>
                     function myFunction() {
-                        var input, filter, table, tr, td, i;
+                        let input, filter, table, tr, td, i;
                         input = document.getElementById("myInput");
                         filter = input.value.toUpperCase();
                         table = document.getElementById("myTable");
