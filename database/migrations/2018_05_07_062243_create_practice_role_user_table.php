ef9842ed7c7369b2d19018d49e08eefb86f643a0
Ikechi Michael <mykehell123@gmail.com>
2018-05-08 16:03:08 +0100

    feat: nullables for test_it_assigns_calls_to_a_nurse

--- database/migrations/2018_05_07_062243_create_practice_role_user_table.php
+++ database/migrations/2018_05_07_062243_create_practice_role_user_table.php
@@ -15,10 +15,10 @@ class CreatePracticeRoleUserTable extends Migration {
 		Schema::create('practice_role_user', function(Blueprint $table)
 		{
 			$table->integer('user_id')->unsigned()->index('practice_user_user_id_foreign');
-			$table->integer('program_id')->unsigned()->index('lv_program_user_program_id_foreign');
+			$table->integer('program_id')->unsigned()->index('lv_program_user_program_id_foreign')->nullable();
 			$table->integer('role_id')->unsigned()->nullable()->index('practice_user_role_id_foreign');
-			$table->boolean('has_admin_rights')->nullable();
-			$table->boolean('send_billing_reports')->nullable();
+			$table->boolean('has_admin_rights')->default(false);
+			$table->boolean('send_billing_reports')->default(false);
 			$table->unique(['user_id','role_id','program_id'], 'practice_user_user_id_role_id_program_id_unique');
 		});
 	}
