26a2140ca7ea8d057b41bea0579d0fddc17d6e15
Ikechi Michael <mykehell123@gmail.com>
2018-05-09 10:31:08 +0100

    chore: add nullables

--- database/migrations/2018_05_07_062243_create_care_plans_table.php
+++ database/migrations/2018_05_07_062243_create_care_plans_table.php
@@ -20,8 +20,8 @@ class CreateCarePlansTable extends Migration {
 			$table->integer('provider_approver_id')->unsigned()->nullable()->index('patient_care_plans_provider_approver_id_foreign');
 			$table->integer('qa_approver_id')->unsigned()->nullable()->index('patient_care_plans_qa_approver_id_foreign');
 			$table->integer('care_plan_template_id')->unsigned()->index('patient_care_plans_care_plan_template_id_foreign');
-			$table->text('type', 65535);
-			$table->text('status', 65535);
+			$table->text('type', 65535)->nullable();
+			$table->text('status', 65535)->nullable();
 			$table->dateTime('qa_date')->default('0000-00-00 00:00:00');
 			$table->dateTime('provider_date')->default('0000-00-00 00:00:00');
 			$table->integer('first_printed_by')->unsigned()->nullable()->index('care_plans_first_printed_by_foreign');
