f98ca9507dd9566c445d9c64056ec4c060ff796b
kakoushias <kakoushias@gmail.com>
2018-05-09 15:57:06 +0300

    #1909 fixes NoteForwardedTest

--- database/migrations/2018_05_07_062243_create_notes_table.php
+++ database/migrations/2018_05_07_062243_create_notes_table.php
@@ -17,11 +17,11 @@ class CreateNotesTable extends Migration {
 			$table->increments('id');
 			$table->integer('patient_id')->unsigned()->index();
 			$table->integer('author_id')->unsigned()->index('notes_author_id_foreign');
-			$table->text('body', 65535);
-			$table->boolean('isTCM');
-			$table->boolean('did_medication_recon');
+			$table->text('body', 65535)->nullable();
+			$table->boolean('isTCM')->default(0);
+			$table->boolean('did_medication_recon')->default(0);
 			$table->timestamps();
-			$table->text('type', 65535);
+			$table->text('type', 65535)->nullable();
 			$table->dateTime('performed_at')->nullable();
 			$table->integer('logger_id')->unsigned()->nullable()->index('notes_logger_id_foreign');
 		});
--- tests/unit/NoteForwardedTest.php
+++ tests/unit/NoteForwardedTest.php
@@ -4,30 +4,39 @@ namespace Tests\Unit;
 
 use App\Notifications\NoteForwarded;
 use App\Practice;
-use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Notification;
+use Tests\Helpers\UserHelpers;
 use Tests\TestCase;
 
 class NoteForwardedTest extends TestCase
 {
     use DatabaseTransactions,
-        WithoutMiddleware;
+        WithoutMiddleware,
+        UserHelpers;
+
+    protected $practice;
+    protected $patient;
+    protected $nurse;
+    protected $admin;
 
     public function test_it_sends_notifications()
     {
-        $practice                           = Practice::find(8);
+        $practice                           = $this->practice;
         $settings                           = $practice->cpmSettings();
         $settings->email_note_was_forwarded = true;
         $settings->efax_pdf_notes           = true;
         $settings->dm_pdf_notes             = true;
         $settings->save();
 
-        $auth = User::find(357);
+        //admin
+        $auth = $this->admin;
         auth()->login($auth);
 
-        $note = User::findOrFail(874)->notes->first();
+        //participant
+        $note = $this->patient->notes()->first();
 
         Notification::fake();
 
@@ -35,7 +44,8 @@ class NoteForwardedTest extends TestCase
 
         $recipients = collect();
         $recipients = $note->patient->care_team_receives_alerts;
-        $recipients->push(User::find(948));
+        //care center
+        $recipients->push($this->nurse);
 
         $recipients->map(function ($user) {
             Notification::assertSentTo(
@@ -43,5 +53,31 @@ class NoteForwardedTest extends TestCase
                 NoteForwarded::class
             );
         });
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->practice = factory(Practice::class)->create();
+        $this->patient  = $this->createUser($this->practice->id, 'participant');
+        $this->admin    = $this->createUser($this->practice->id, 'administrator');
+
+        $this->nurse = $this->createUser($this->practice->id, 'care-center');
+
+        $carePerson = $this->patient->careTeamMembers()->create([
+            'member_user_id' => $this->nurse->id,
+            'type'           => 'member',
+            'alert'          => 1,
+
+        ]);
+
+        $this->patient->notes()->create([
+            'author_id'    => $this->nurse->id,
+            'body'         => 'test',
+            'logger_id'    => $this->nurse->id,
+            'performed_at' => Carbon::now(),
+            'type'         => 'Patient Consented',
+        ]);
     }
 }
