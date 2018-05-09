a1e0426bee2a87a2ed120e4abb6d62df2484e819
Ikechi Michael <mykehell123@gmail.com>
2018-05-09 12:40:41 +0100

    fix: CarePlanApprovalReminderTest

--- tests/unit/CarePlanApprovalReminderTest.php
+++ tests/unit/CarePlanApprovalReminderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Practice;
 use App\CarePlan;
 use App\Notifications\CarePlanApprovalReminder;
 use App\Patient;
@@ -19,13 +20,16 @@ class CarePlanApprovalReminderTest extends TestCase
 
     private $provider;
     private $patient;
+    private $practice;
 
     protected function setUp()
     {
         parent::setUp();
+
+        $this->practice = factory(Practice::class)->create();
         
-        $this->provider = $this->createUser(8, 'provider');
-        $this->patient = $this->createUser(8, 'participant');
+        $this->provider = $this->createUser($this->practice->id, 'provider');
+        $this->patient = $this->createUser($this->practice->id, 'participant');
 
         $this->patient->billing_provider_id = $this->provider->id;
         $this->patient->ccm_status = Patient::ENROLLED;
