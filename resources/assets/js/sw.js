affd82a095dd7720df28a1a1cea2f5936a7a6b2b
Ikechi Michael <mykehell123@gmail.com>
2018-05-14 13:23:24 +0100

    feat: change path

--- resources/assets/js/sw.js
+++ resources/assets/js/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://unpkg.com/workbox-sw@2.1.1')
+importScripts('https://unpkg.com/workbox-sw@2.1.1/build/importScripts/workbox-sw.prod.v2.1.1.js')
 
 const workboxSW = new self.WorkboxSW();
 workboxSW.precache([]);
