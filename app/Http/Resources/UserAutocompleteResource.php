768819f3dc1ed33cc37d563c9951cc86201cd205
Ikechi Michael <mykehell123@gmail.com>
2018-05-23 12:54:27 +0100

    feat: make sure patient-users autocomplete contains careplan status #1782

--- app/Http/Resources/UserAutocompleteResource.php
+++ app/Http/Resources/UserAutocompleteResource.php
@@ -18,7 +18,8 @@ class UserAutocompleteResource extends Resource
         return [
             'id' => $this->id,
             'name' => $this->name() ?? $this->display_name,
-            'program_id' => $this->program_id
+            'program_id' => $this->program_id,
+            'status'    => optional($this->carePlan()->first())->status
         ];
     }
 }
