1b13d670e02693c88190becb04710d620b535b57
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-06-05 13:06:32 +0200

    File change

--- config/snappy.php
+++ config/snappy.php
@@ -3,7 +3,7 @@
 return [
     'pdf' => [
         'enabled' => true,
-        'binary'  => base_path('bin/wkhtmltopdf/wkhtmltopdf'),
+        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => [
             'collate' => true,
