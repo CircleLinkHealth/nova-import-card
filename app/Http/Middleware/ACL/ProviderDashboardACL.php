447ddeda5268c99dc9bf9bd0f698f207c769b490
kakoushias <kakoushias@gmail.com>
2018-07-23 18:41:41 +0300

    #CPM-27 Adds new Role for MDAlly

--- app/Http/Middleware/ACL/ProviderDashboardACL.php
+++ app/Http/Middleware/ACL/ProviderDashboardACL.php
@@ -26,7 +26,7 @@ class ProviderDashboardACL
 
 
         //CLH Admins can see everything
-        if (auth()->user()->hasRole(['administrator', 'saas-admin'])) {
+        if (auth()->user()->hasRole(['administrator', 'saas-admin', 'mdally'])) {
             return $next($request);
         }
 
