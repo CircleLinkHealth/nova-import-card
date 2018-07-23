5b20df0b7894f1790b921baa8722675be19bd4e2
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-07-23 23:37:09 +0300

    Adds comment

--- app/ChargeableService.php
+++ app/ChargeableService.php
@@ -4,7 +4,13 @@ namespace App;
 
 class ChargeableService extends \App\BaseModel
 {
+    /**
+     * When a Patient consents to receive Care from CLH, they consent to these Chargeable Services, if consent date is
+     * after 7/23/2018. If consent date is before 7/23/2018, patient was consented to the same services except for 'CPT
+     * 99484'.
+     */
     const DEFAULT_CHARGEABLE_SERVICE_CODES = [
+        'CPT 99484',
         'CPT 99490',
         'CPT 99487',
         'CPT 99489',
