2f90c35e7dc61e98c02f91042825709c804be305
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-07-25 16:42:05 +0300

    Adds Roles/Permissions migration

---
+++ database/migrations/2018_07_25_090653_roles_and_permissions_changes.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class RolesAndPermissionsChanges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('db:seed', [
+            '--class' => 'RolesPermissionsSeeder',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
