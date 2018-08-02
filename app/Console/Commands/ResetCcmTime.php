7bca8919bd6154dbb6211bbd5b20f10b68c222a1
subzero10 <pagcosma@gmail.com>
2018-08-02 14:56:46 +0300

    Command ResetCCMTime also creates a PatientMonthlySummary now (with zeroed values).

--- app/Console/Commands/ResetCcmTime.php
+++ app/Console/Commands/ResetCcmTime.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\AppConfig;
 use App\Patient;
+use App\PatientMonthlySummary;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 
@@ -44,10 +45,47 @@ class ResetCcmTime extends Command
 
         $lastReset = $appConfigs->where('config_key', 'cur_month_ccm_time_last_reset')->first();
 
+        /*
         Patient::withTrashed()
             ->update([
                 'cur_month_activity_time' => '0',
             ]);
+        */
+
+        Patient::withTrashed()
+               ->chunk(200, function (Patient $patient) {
+                   $patient->cur_month_activity_time = 0;
+                   $patient->save();
+
+                   $summary = PatientMonthlySummary::where('patient_id', '=', $patient->user_id)
+                                                   ->orderBy('id', 'desc')->first();
+
+                   //if we have already summary for this month, then we skip this
+                   if ($summary && Carbon::today()->isSameMonth($summary->month_year)) {
+                       return;
+                   }
+
+                   if ($summary) {
+                       //clone record
+                       $newSummary = $summary->replicate();
+                   } else {
+                       $newSummary             = new PatientMonthlySummary();
+                       $newSummary->patient_id = $patient->user_id;
+                   }
+
+                   $newSummary->month_year             = Carbon::today();
+                   $newSummary->total_time             = 0;
+                   $newSummary->ccm_time               = 0;
+                   $newSummary->bhi_time               = 0;
+                   $newSummary->no_of_calls            = 0;
+                   $newSummary->no_of_successful_calls = 0;
+                   $newSummary->is_ccm_complex         = 0;
+                   $newSummary->approved               = 0;
+                   $newSummary->rejected               = 0;
+                   $newSummary->actor_id               = null;
+                   $newSummary->needs_qa               = null;
+                   $newSummary->save();
+               });
 
         $lastReset->config_value = Carbon::now();
         $lastReset->save();
