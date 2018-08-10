e5a7f84c177a0bac0a799b35f1ae01e4233f00e4
kakoushias <kakoushias@gmail.com>
2018-08-10 15:11:52 +0300

    CPM-46 fixes new column filter and sorting

--- app/Filters/PatientFilters.php
+++ app/Filters/PatientFilters.php
@@ -4,12 +4,11 @@ namespace App\Filters;
 
 use App\CarePerson;
 use App\CarePlan;
-use App\Practice;
-use App\ProviderInfo;
 use App\Patient;
+use App\Practice;
 use App\Repositories\PatientReadRepository;
-use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class PatientFilters extends QueryFilters
 {
@@ -21,70 +20,83 @@ class PatientFilters extends QueryFilters
         parent::__construct($request);
     }
 
-    public function name($term) {
+    public function name($term)
+    {
         return $this->builder->where('users.display_name', 'LIKE', "%$term%");
     }
 
-    public function provider($provider) {
+    public function provider($provider)
+    {
         return $this->builder->whereHas('billingProvider', function ($query) use ($provider) {
             $query->whereHas('user', function ($q) use ($provider) {
                 $q->where('display_name', $provider)->orWhere('id', $provider);
             });
         });
     }
-    
-    public function practice($id) {
+
+    public function practice($id)
+    {
         return $this->builder->whereHas('primaryPractice', function ($query) use ($id) {
             $query->where('id', $id);
         });
     }
-    
-    public function careplanStatus($status) {
+
+    public function careplanStatus($status)
+    {
         return $this->builder->whereHas('carePlan', function ($query) use ($status) {
             $query->where('status', $status)->orWhere('status', 'LIKE', '%\"status\":\"' . $status . '\"%');
         });
     }
-    
-    public function ccmStatus($status) {
+
+    public function ccmStatus($status)
+    {
         return $this->builder->whereHas('patientInfo', function ($query) use ($status) {
             $query->where('ccm_status', $status);
         });
     }
-    
-    public function dob($date) {
+
+    public function dob($date)
+    {
         return $this->builder->whereHas('patientInfo', function ($query) use ($date) {
             $query->where('birth_date', 'LIKE', '%' . $date . '%');
         });
     }
-    
-    public function phone($phone) {
+
+    public function phone($phone)
+    {
         return $this->builder->whereHas('phoneNumbers', function ($query) use ($phone) {
             $query->where('number', 'LIKE', '%' . $phone . '%');
         });
     }
-    
-    public function age($age) {
+
+    public function age($age)
+    {
         $year = Carbon::now()->subYear($age)->format('Y');
+
         return $this->builder->whereHas('patientInfo', function ($query) use ($year) {
-           $query->where('birth_date', '>=', "$year-01-01")->where('birth_date', '<=', "$year-12-31");
+            $query->where('birth_date', '>=', "$year-01-01")->where('birth_date', '<=', "$year-12-31");
         });
     }
-    
-    public function registeredOn($on) {
+
+    public function registeredOn($on)
+    {
         return $this->builder->where('users.created_at', 'LIKE', '%' . $on . '%');
     }
-    
-    public function lastReading($reading) {
+
+    public function lastReading($reading)
+    {
         return $this->builder->whereHas('lastObservation', function ($query) use ($reading) {
             return $query->where('obs_date', 'LIKE', $reading . '%');
         });
     }
 
-    public function sort_name($type = null) {
+    public function sort_name($type = null)
+    {
         return $this->builder->orderBy('users.display_name', $type);
     }
-    
-    public function sort_provider($type = null) {
+
+    public function sort_provider($type = null)
+    {
         $careTeamTable = (new CarePerson())->getTable();
 
         return $this->builder
@@ -95,8 +107,9 @@ class PatientFilters extends QueryFilters
             ->join('users as providers', 'providers.id', '=', "$careTeamTable.member_user_id")
             ->orderBy("providers.display_name", $type);
     }
-    
-    public function sort_practice($type = null) {
+
+    public function sort_practice($type = null)
+    {
         $practicesTable = (new Practice())->getTable();
 
         return $this->builder
@@ -106,58 +119,101 @@ class PatientFilters extends QueryFilters
             ->orderBy("$practicesTable.display_name", $type)
             ->groupBy('users.id');
     }
-    
-    public function sort_ccmStatus($type = null) {
+
+    public function sort_ccmStatus($type = null)
+    {
         $patientTable = (new Patient())->getTable();
-        return $this->builder->select('users.*')->with('patientInfo')->join($patientTable, 'users.id', '=', "$patientTable.user_id")->orderBy("$patientTable.ccm_status", $type)->groupBy('users.id');
-    }
-    
-    public function sort_careplanStatus($type = null) {
-        $careplanTable = (new CarePlan())->getTable();
-        return $this->builder->select('users.*')->with('carePlan')->join($careplanTable, 'users.id', '=', "$careplanTable.user_id")->orderBy("$careplanTable.status", $type)->groupBy('users.id');
-    }
-    
-    public function sort_dob($type = null) {
-        $patientTable = (new Patient())->getTable();
-        return $this->builder->select('users.*')->with('patientInfo')->join($patientTable, 'users.id', '=', "$patientTable.user_id")->orderBy("$patientTable.birth_date", $type)->groupBy('users.id');
-    }
-    
-    public function sort_age($type = null) {
-        return $this->sort_dob((!$type || $type == 'asc') ? 'desc' : 'asc');
-    }
-    
-    public function sort_registeredOn($type = null) {
-        return $this->builder->orderBy('users.created_at', $type);
-    }
-    
-    public function sort_ccm($type = null) {
-        $patientTable = (new Patient())->getTable();
-        return $this->builder->select('users.*')->with('patientInfo')->join($patientTable, 'users.id', '=', "$patientTable.user_id")
-                    ->orderBy("$patientTable.cur_month_activity_time", $type)->groupBy('users.id');
+
+        return $this->builder->select('users.*')->with('patientInfo')->join($patientTable, 'users.id', '=',
+            "$patientTable.user_id")->orderBy("$patientTable.ccm_status", $type)->groupBy('users.id');
     }
 
-    public function excel() {
+    public function sort_careplanStatus($type = null)
+    {
+        $careplanTable = (new CarePlan())->getTable();
+
+        return $this->builder->select('users.*')->with('carePlan')->join($careplanTable, 'users.id', '=',
+            "$careplanTable.user_id")->orderBy("$careplanTable.status", $type)->groupBy('users.id');
+    }
+
+    public function sort_dob($type = null)
+    {
+        $patientTable = (new Patient())->getTable();
+
+        return $this->builder->select('users.*')->with('patientInfo')->join($patientTable, 'users.id', '=',
+            "$patientTable.user_id")->orderBy("$patientTable.birth_date", $type)->groupBy('users.id');
+    }
+
+    public function sort_age($type = null)
+    {
+        return $this->sort_dob(( ! $type || $type == 'asc')
+            ? 'desc'
+            : 'asc');
+    }
+
+    public function sort_registeredOn($type = null)
+    {
+        return $this->builder->orderBy('users.created_at', $type);
+    }
+
+    public function sort_ccm($type = null)
+    {
+        $patientTable = (new Patient())->getTable();
+
+        return $this->builder->select('users.*')->with('patientInfo')->join($patientTable, 'users.id', '=',
+            "$patientTable.user_id")
+                             ->orderBy("$patientTable.cur_month_activity_time", $type)->groupBy('users.id');
+    }
+
+    public function excel()
+    {
         return true;
     }
 
-    public function isExcel() {
+    public function isExcel()
+    {
         return isset($this->filters()['excel']);
     }
 
-    public function csv() {
+    public function csv()
+    {
         return true;
     }
 
-    public function isCsv() {
+    public function isCsv()
+    {
         return isset($this->filters()['csv']);
     }
 
-    public function autocomplete() {
-        return $this->builder->select([ 'users.id', 'users.display_name', 'users.program_id' ]);
+    public function autocomplete()
+    {
+        return $this->builder->select(['users.id', 'users.display_name', 'users.program_id']);
     }
 
-    public function isAutocomplete() {
+    public function isAutocomplete()
+    {
         return isset($this->filters()['autocomplete']);
+    }
+
+    public function ccmStatusDate($date)
+    {
+        return $this->builder->whereHas('patientInfo', function ($query) use ($date) {
+            $query->where('date_paused', 'LIKE', "%$date%")
+                  ->orWhere('date_withdrawn', 'LIKE', "%$date%")
+                  ->orWhere('date_unreachable', 'LIKE', "%$date%");
+        });
+    }
+
+    public function sort_ccmStatusDate($type = null)
+    {
+        $patientTable = (new Patient())->getTable();
+        return $this->builder->select('users.*')
+                             ->with('patientInfo')->join($patientTable, 'users.id', '=', "$patientTable.user_id")
+                             ->orderBy("$patientTable.date_paused", $type)
+                             ->orderBy("$patientTable.date_withdrawn", $type)
+                             ->orderBy("$patientTable.date_unreachable", $type)
+                             ->groupBy('users.id');
+
     }
 
     public function globalFilters(): array
