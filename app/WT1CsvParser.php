316962cb35479977915575446f50a8483edc9ac0
subzero10 <pagcosma@gmail.com>
2018-11-02 11:44:54 +0200

    CPM-143 parse latest CSV - problems, allergies, medications, DOS

--- app/WT1CsvParser.php
+++ app/WT1CsvParser.php
@@ -62,17 +62,6 @@ class WT1CsvParser
 
         $entry = $this->patients[$patientId];
 
-        //exit if problem already processed
-        if (isset($entry['problems'])) {
-            $problems   = $entry['problems'];
-            $rowProblem = $row['description'];
-            foreach ($problems as $problem) {
-                if ($problem['name'] === $rowProblem) {
-                    return;
-                }
-            }
-        }
-
         if ( ! isset($entry['insurance_plans'])) {
             $entry['insurance_plans'] = []; //we want this to be translated to { "primary" : {}, "secondary": {} }
         }
@@ -91,8 +80,7 @@ class WT1CsvParser
 
         $entry['patient_id'] = $patientId;
 
-        //medicalrecordnumber was in first csv
-        $entry['mrn'] = $this->getValue($row, 'medicalrecordnumber') ?? $this->getValue($row, 'mrn');
+        $entry['mrn'] = $this->getValue($row, 'mrn');
 
         $entry['last_name']      = $this->getValue($row, 'lastname');
         $entry['first_name']     = $this->getValue($row, 'firstname');
@@ -106,76 +94,127 @@ class WT1CsvParser
         $entry['primary_phone']  = $this->getValue($row, 'phonehome');
         $entry['cell_phone']     = $this->getValue($row, 'phonecell');
 
-        //datecreated was in first csv - not found in latest csv
-        $entry['last_visit'] = $this->getValue($row, 'encounterdate') ?? $this->getValue($row, 'datecreated');
-        
+        $entry['last_visit'] = $this->getValue($row, 'dos');
+
         $entry['preferred_provider'] = $this->getProviderValue($row);
 
-        //FamHxAllergies,FamHxDiabetesII,FamHxCOPD,FamHxTuberculosis,FamHxHTN,FamHxHeartDisease,FamHxStroke,FamHxBleeding,FamHxMigraine,FamHxPsychiatric,FamHxDepression,FamHxAlcoholAbuse,FamHxArthritis,FamHxOsteoporosis,FamHxCancer,FamHxColonPolyps,FamHxColonCancer,FamHxKidneyDisease,FamHxLiverDisease,FamHxGERD,FamHxHeartAttack,FamHxHighChol,FamHxThyroidDisease,FamHxOther,FamHxAsthma,FamHxBreastCancer,FamHxCervicalCancer,FamHxLungCancer,FamHxOtherCancer,FamHxProstateCancer,FamHxDementia,FamHxHearingLoss,FamHxHuntingtons,FamHxParkinsons,FamHxSeizures,FamHxGlaucoma,FamHxMacularDeg
-        //ReportedDiabetes,ReportedOverweight,ReportedPoorVision,ReportedHearing,ReportedDizziness,ReportedFalls,ReportedForgetfullness,ReportedDepression,ReportedSleeping,ReportedJointPain,ReportedHeart,ReportedHighBP,ReportedCAD,ReportedStroke,ReportedOsteoporosis,ReportedArthritis,ReportedADDADHD,ReportedAnxity,ReportedAspergers,ReportedBackPain,ReportedBiPolar,ReportedCancer,ReportedCHDCF,ReportedDementia,ReportedGERD,ReportingHearingProb,ReportedAfib,ReportedAngina,ReportedAtherosclerosis,ReportedCardiomyopathy,ReportedHeartOther,ReportedPAD,ReportedPVD,ReportedValveDisease,ReportedHepatitis,ReportedHIVAIDs,ReportedHxHeartAttack,ReportedIncontinence,ReportedInpatientLastMonth,ReportedInpatientLastYear,ReportedKidneyTrans,ReportedMovementDis,ReportedOCD,ReportedOtherMH,ReportedRA,ReportedSchizophrenia,ReportedSeizures,ReportedStrokeIsc,ReportedStrokeTIA,ReportedStrokeHem,ReportedUrinary,ReportedAsthma,ReportedCOPD,ReportedAsthmaCOPD,ReportedOther1,ReportedOther2,ReportedOther3,ReportedOther4,ReportedOther5,MacularLeftEye,MacularRightEye
-        //ServingsNuts,ServingsFruit,ServingsFish,ServingsMeat,ServingsVegetables,ServingsBeverages,ServingsFriedFood,ServingsSaturatedFat
-        //ROSWeightGain,ROSWeightLoss,ROSMalaise,ROSFever,ROSChills,ROSFatigue,ROSWeakness,ROSAllergies,ROSAppetite,ROSDizziness,ROSInsomnia,ROSSOB,ROSWheezing,ROSVertigo,ROSBackPain,ROSJointPain,ROSProbSleep,ROSForgetful,ROSVisionProb,ROSHearingProb,ROSFalling,ROSSnoring,ROSChoking,ROSTremors,ROSCough,ROSEyePain,ROSHalos,ROSBleeding,ROSLegPain,ROSChestPain,ROSNumbness,ROSStiffness,ROSSwelling,ROSOther
-        //FCSitting,FCStanding,FCLifting,FCCarrying,FCPushing,FCPulling,FCBending,FCStooping,FCSquatting,FCKneeling,FCReaching,FCHandUse
-        //SAHandrails,SASlippery,SADriving,SALocking,SAFamily,SAFriends
-        //MCIEye,MCIWalking,MCITouch,MCIClock,MCILocation,MCICopy,MCI3Step,MCIListofWords,MCIWritten,MCIWrite,MCICount
-        //DSHappyFamily,DSOutlook,DSDepression,DSSadness,DSActivities,DSWeight,DSAppetite,DSSleeping,DSAgitation,DSThought,DSDetails,DSMovements,DSFatigue,DSWorthlessness,DSConcentration,DSDecisions,DSSuicide,DSPHQ9Score
-        //AllergyAnaphylaxis,AllergyCannotUseEpiPen,AllergyFood,AllergyVenom,AllergyInhalants,AllergyMedications,AllergyOther,AllergyHasEpi,AllergyNoEpi,AllergyCanUseEpi
-        //FallRiskNumberofFalls,FallRiskSafeHabits,FallRiskDiffProblems,FallRiskGait,FallRiskConcerned,FallRiskFellLastYear,FallRiskFellLastMonth,FallRiskFellInjury,
-        //Allergy1,Allergy2,Allergy3,Allergy4,Allergy5
-        //MedsNoMeds,MedsDiabetes,MedsSleep,MedsDepression,MedsPain,MedsHeart,MedsBP,MedsThyroid,MedsCholesterol,MedsReflux,MedsAntiSeizure,MedsThinner,MedsRespiratory,MedsAllergy,MedsAntibiotics,MedsAsprin,MedsCorticosteroid,MedsAllergyMeds,MedsCalcium,MedsVitamins,MedsOther1,MedsOther2,MedsOther3,MedsOther4,MedsOther5,Medications1,Medications2,Medications3,Medications4,Medications5,Medications6,Medications7,Medications8,Medications9,Medications10,Medications11,Medications12,Medications13,Medications14,Medications15,Medications16,Medications17,Medications18,Medications19,Medications20
-        //Vaccine1,Vaccine2,Vaccine3,Vaccine4,Vaccine5,Vaccine6
-        //ADLBathing,ADLInhalerusage,ADLFeedingself,ADLInOutCar,ADLDressing,ADLInOutBed,ADLPutOnTakeOffClothes,ADLInOutTub,ADLDryPowderInhaler,ADLHousekeeping,ADLDifficultyManaging,ADLUseOfPhone,ADLAdministerMeds,ADLManageMoney,ADLShopping,ADLUsesPillOrganizer
-        //AdvDirWantsSpeakPhys,AdvDirHasSpokenPhys,AdvDirAdvancedDirectives
-        //ObsHasSeenProv2Mo,ObsHasNotSeenProv2Mo,ObsTakingMedsAsRx,ObsNotTakingMedsAsRx,ObsPtComplaintsNew,ObsPtComplaintsNotNew,ObsPtDiscussedDepression,ObsPtNotDiscussedDepression,ObsPtDiscussedOutlook,ObsPtNotDiscussedOutlook
-        //RVRFDiabetes,RVRFCHD,RVRFStroke,RVRFColorectal,RVRFOsteoporosis,RVRFDepression,RVRFCognitive,RVRFFunctional,RVRFProstate,RVRFCOPD
+        $entry['insurance_plans'] = $this->getInsurances($row);
 
-        $entry['insurance_plans']['primary'] = [
-            "plan" => "Medicare",
-        ];
-
-        //PrimaryIns,PrimaryInsPol,SecondaryIns,SecondaryInsPol
-//        $entry['insurance_plans']['primary']   = [
-//            "plan"           => "Test Insurance",
-//            "group_number"   => "",
-//            "policy_number"  => "TEST1234",
-//            "insurance_type" => "Medicaid",
-//        ];
-//        $entry['insurance_plans']['secondary'] = [
-//            "plan"           => "Test Medicare",
-//            "group_number"   => "",
-//            "policy_number"  => "123455",
-//            "insurance_type" => "Medicare",
-//        ];
-
-        $entry['problems'][] = [
-            "name" => $row['description'],
-        ];
-
-//        $entry['problems'][]    = [
-//            "name"       => "Chronic Obstructive Pulmonary Disease",
-//            "code_type"  => "ICD9",
-//            "code"       => "496",
-//            "start_date" => "07-30-2013",
-//        ];
-
-//        $entry['medications'][] = [
-//            "name"       => "Avinza 30 mg oral capsule, ER multiphase 24 hr",
-//            "sig"        => "take 1 capsule by oral route daily for 30 days",
-//            "start_date" => "2014-03-11",
-//        ];
-
-//        $entry['allergies'][] = [
-//            "name" => "Animal Dander",
-//        ];
+        $entry['problems']    = $this->getMergedList($row, $entry['problems'], 'reported');
+        $entry['allergies']   = $this->getMergedList($row, $entry['allergies'], 'allergy');
+        $entry['medications'] = $this->getMergedList($row, $entry['medications'], 'meds');
 
         $this->patients[$patientId] = $entry;
     }
 
-    private function getProviderValue($row, $default = null) {
+    private function getInsurances($row)
+    {
+        $result    = [];
+        $primary   = $this->getPrimaryInsurance($row);
+        $secondary = $this->getSecondaryInsurance($row);
+
+        if ($primary) {
+            $result['primary'] = $primary;
+        }
+        if ($secondary) {
+            $result['secondary'] = $secondary;
+        }
+        return $result;
+
+    }
+
+    private function getPrimaryInsurance($row)
+    {
+
+        /**
+         * "plan"           => "Test Medicare",
+         * "group_number"   => "",
+         * "policy_number"  => "123455",
+         * "insurance_type" => "Medicare",
+         */
+
+        $plan = $this->getValue($row, 'primaryins');
+        if ( ! $plan) {
+            return null;
+        }
+
+        $result                  = [];
+        $result["plan"]          = $plan;
+        $result["policy_number"] = $this->getValue($row, 'primaryinspol');
+        return $result;
+    }
+
+    private function getSecondaryInsurance($row)
+    {
+        $plan = $this->getValue($row, 'secondaryins');
+        if ( ! $plan) {
+            return null;
+        }
+
+        $result                  = [];
+        $result["plan"]          = $plan;
+        $result["policy_number"] = $this->getValue($row, 'secondaryinspol');
+        return $result;
+    }
+
+    private function getMergedList($row, $currentList, $fieldName)
+    {
+        $rowList = $this->getListFromFields($row, $fieldName);
+
+        foreach ($rowList as $fieldValue) {
+
+            $found = false;
+            foreach ($currentList as $entry) {
+                if (strcasecmp($entry['name'], $fieldValue) === 0) {
+                    $found = true;
+                    break;
+                }
+            }
+
+            if ( ! $found) {
+                $currentList[] = [
+                    'name' => $fieldValue,
+                ];
+            }
+        }
+        return $currentList;
+    }
+
+    private function getListFromFields($row, $fieldName)
+    {
+        $lookingFor = $fieldName;
+        $result     = [];
+        foreach ($row as $key => $value) {
+
+            $isFieldFound = strcasecmp(substr($key, 0, strlen($lookingFor)), $lookingFor) === 0;
+            if ( ! $isFieldFound) {
+                continue;
+            }
+
+            $fieldValue = str_replace($lookingFor, "", $key);
+            //ignore {FieldName}None
+            if (strcasecmp($fieldValue, 'none') === 0) {
+                continue;
+            }
+
+            //ignore MedsNoMeds
+            if (strcasecmp($key, 'MedsNoMeds') === 0) {
+                continue;
+            }
+
+            if ($this->getBoolValue($row, $key)) {
+                $result[] = $fieldValue;
+            }
+        }
+        return $result;
+    }
+
+    private function getProviderValue($row, $default = null)
+    {
         $firstName  = $this->getValue($row, 'providerfirstname', '');
         $middleName = $this->getValue($row, 'providermiddlename', '');
         $lastName   = $this->getValue($row, 'providerlastname', '');
-        $result = '';
+        $result     = '';
         if ( ! empty($firstName)) {
             $result .= $firstName;
             if ( ! empty($middleName)) {
@@ -214,5 +253,23 @@ class WT1CsvParser
         return $row[$key];
     }
 
+    /**
+     * Returns true if value is Y, false if N.
+     * All other fields return false.
+     *
+     * @param $row
+     * @param $key
+     * @param bool $default
+     *
+     * @return bool|null
+     */
+    private function getBoolValue($row, $key, $default = false)
+    {
+        $val = $this->getValue($row, $key, $default);
+        if ($val === false) {
+            return $val;
+        }
+        return $val === 'Y';
+    }
 
 }
