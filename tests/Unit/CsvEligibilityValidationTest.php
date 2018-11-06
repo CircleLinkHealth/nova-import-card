807d60d3893b013cb7c2f312acd7efb52240b10f
kakoushias <kakoushias@gmail.com>
2018-11-06 20:32:06 +0200

    #CPM-414 Adds assertions

--- tests/Unit/CsvEligibilityValidationTest.php
+++ tests/Unit/CsvEligibilityValidationTest.php
@@ -8,8 +8,6 @@ use App\Services\CCD\ProcessEligibilityService;
 use App\Services\Eligibility\Csv\CsvPatientList;
 use Tests\Helpers\UserHelpers;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CsvEligibilityValidationTest extends TestCase
 {
@@ -28,7 +26,7 @@ class CsvEligibilityValidationTest extends TestCase
     {
         $csv = getcwd() . '/Tests/Feature/EligibleCsvFormat/Numbered_Fields_1.csv';
 
-        $patients   = parseCsvToArray($csv);
+        $patients = parseCsvToArray($csv);
 
         $csvPatientList = new CsvPatientList(collect($patients));
         $isValid        = $csvPatientList->guessValidator();
@@ -37,9 +35,9 @@ class CsvEligibilityValidationTest extends TestCase
 
         $batch = $this->service->createSingleCSVBatch($patients, $this->practice->id, false, false, true);
 
-        //assert batch
+
         $this->assertDatabaseHas('eligibility_batches', [
-            'id' => $batch->id
+            'id' => $batch->id,
         ]);
 
         $result = $this->service->processCsvForEligibility($batch);
@@ -48,12 +46,14 @@ class CsvEligibilityValidationTest extends TestCase
             $batch->status = EligibilityBatch::STATUSES['processing'];
             $batch->save();
         }
+        $this->assertEquals(EligibilityBatch::STATUSES['processing'], $batch->getStatus());
 
-        //assert batch created
-        //assert jobs created
-//        $this->assertAttributeCount();
+        $this->assertTrue($batch->hasJobs());
+
+        $jobs = $batch->eligibilityJobs();
+        $this->assertEquals(6, $jobs->count());
+
         //assert jobs have no errors
-        //assert errors empty
 //        $this->assertAttributeEmpty();
 
 
@@ -63,7 +63,7 @@ class CsvEligibilityValidationTest extends TestCase
     {
         $csv = getcwd() . '/Tests/Feature/EligibleCsvFormat/Single_Fields_1.csv';
 
-        $patients   = parseCsvToArray($csv);
+        $patients = parseCsvToArray($csv);
 
         $csvPatientList = new CsvPatientList(collect($patients));
         $isValid        = $csvPatientList->guessValidator();
@@ -71,28 +71,33 @@ class CsvEligibilityValidationTest extends TestCase
         $this->assertTrue($isValid);
         $batch = $this->service->createSingleCSVBatch($patients, $this->practice->id, false, false, true);
 
+        $this->assertDatabaseHas('eligibility_batches', [
+            'id' => $batch->id,
+        ]);
+
         $result = $this->service->processCsvForEligibility($batch);
 
         if ($result) {
             $batch->status = EligibilityBatch::STATUSES['processing'];
             $batch->save();
         }
-        //assert batch
         $this->assertDatabaseHas('eligibility_batches', [
-            'id' => $batch->id
+            'id' => $batch->id,
         ]);
 
-        $this->assertTrue(true);
+        $this->assertEquals(EligibilityBatch::STATUSES['processing'], $batch->getStatus());
 
-        //assert batch created
-        //assert jobs created
-        //assert jobs have no errors
+        $this->assertTrue($batch->hasJobs());
+
+        $jobs = $batch->eligibilityJobs();
+        $this->assertEquals(5, $jobs->count());
+
     }
 
     protected function setUp()
     {
         parent::setUp();
-        $this->service = new ProcessEligibilityService();
+        $this->service  = new ProcessEligibilityService();
         $this->practice = factory(Practice::class)->create();
 
     }
