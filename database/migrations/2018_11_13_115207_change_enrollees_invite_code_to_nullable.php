db7ece189e746a73e2f5f19972595cf022c3e3f2
kakoushias <kakoushias@gmail.com>
2018-11-13 20:08:41 +0200

    #414 requested changes to test, migration for enrollees table

---
+++ database/migrations/2018_11_13_115207_change_enrollees_invite_code_to_nullable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeEnrolleesInviteCodeToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('enrollees', function($table)
+        {
+            $table->text('invite_code')->nullable(true)->change();
+            $table->string('status')->nullable(true)->change();
+            $table->integer('attempt_count')->nullable(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('enrollees', function($table)
+        {
+            $table->text('invite_code')->nullable(false)->change();
+            $table->string('status')->nullable(false)->change();
+            $table->integer('attempt_count')->nullable(false)->change();
+        });
+    }
+}
