944f5ae66caeacc3c79e2e2e7eb0f7dfdd0a6308
subzero10 <pagcosma@gmail.com>
2018-11-14 17:50:18 +0200

    CPM-457 bug fixes, refactor and input box for other number

--- resources/assets/js/components/call-number.vue
+++ resources/assets/js/components/call-number.vue
@@ -4,22 +4,36 @@
         <div>{{log}}</div>
         <template v-if="!waiting">
             <div class="row">
-                <div class="col-sm-10">
-                    <select2 class="form-control" v-model="selectedNumber" :disabled="hasActiveCall">
+                <div class="col-sm-12">
+                    <select2 class="form-control" v-model="dropdownNumber" :disabled="onPhone">
                         <option v-for="(number, index) in numbers" :key="index" :value="number">{{number}}</option>
+                        <option :key="numbers ? numbers.length : 0" value="other">Other</option>
                     </select2>
                 </div>
 
-                <div class="col-sm-2" style="margin-top: 5px">
-                    <button class="btn btn-circle" @click="toggleCallMessage(selectedNumber)"
-                            :class="onPhone[selectedNumber] ? 'btn-danger': 'btn-success'"
-                            :disabled="!validPhone(selectedNumber) || !selectedNumber">
-                        <i class="fa fa-fw fa-phone" :class="onPhone[selectedNumber] ? 'fa-close': 'fa-phone'"></i>
+
+                <div v-if="dropdownNumber === 'other'" class="col-sm-12" style="margin-top: 5px">
+                    <label for="other-number">Please input a 10 digit US Phone Number</label>
+                    <div class="input-group">
+                        <span class="input-group-addon">+1</span>
+                        <input id="other-number" name="other-number"
+                               maxlength="10" minlength="10"
+                               class="form-control" type="tel"
+                               title="10-digit US Phone Number" placeholder="1234567890"
+                               v-model="otherNumber" :disabled="onPhone"/>
+                    </div>
+                </div>
+
+                <div class="col-sm-12" style="margin-top: 5px">
+                    <button class="btn btn-circle" @click="toggleCallMessage()"
+                            :class="onPhone ? 'btn-danger': 'btn-success'"
+                            :disabled="!validNumber">
+                        <i class="fa fa-fw fa-phone" :class="onPhone ? 'fa-close': 'fa-phone'"></i>
                     </button>
-                    <button class="btn btn-circle btn-default" v-if="onPhone[selectedNumber]"
-                            @click="toggleMuteMessage(selectedNumber)">
+                    <button class="btn btn-circle btn-default" v-if="onPhone"
+                            @click="toggleMuteMessage()">
                         <i class="fa fa-fw"
-                           :class="muted[selectedNumber] ? 'fa-microphone-slash': 'fa-microphone'"></i>
+                           :class="muted ? 'fa-microphone-slash': 'fa-microphone'"></i>
                     </button>
                 </div>
             </div>
@@ -46,43 +60,54 @@
         ],
         data() {
             return {
-                hasActiveCall: false,
                 waiting: false,
-                muted: [],
-                onPhone: [],
+                muted: false,
+                onPhone: false,
                 log: 'Initializing',
                 connection: null,
                 //twilio device
                 device: null,
-                selectedNumber: this.numbers[0] ? this.numbers[0] : null
+                dropdownNumber: this.numbers[0] ? this.numbers[0] : null,
+                otherNumber: null
+            }
+        },
+        computed: {
+            selectedNumber() {
+                return this.dropdownNumber === 'other' ? ('+1' + this.otherNumber) : this.dropdownNumber;
+            },
+            validNumber() {
+                if (!this.selectedNumber) {
+                    return false;
+                }
+                if (isNaN(this.selectedNumber.substring(1))) {
+                    return false;
+                }
+                return this.selectedNumber.toString().length === 12;
             }
         },
         methods: {
-            validPhone: function (number) {
-                // return /^([0-9]|#|\*)+$/.test(number.replace(/[-()\s]/g,''));
-                return true;
-            },
-            // Handle muting
-            toggleMute: function (number) {
-                const value = !this.muted[number];
-                this.muted[number] = value;
+
+            toggleMute: function () {
+                const value = !this.muted;
+                this.muted = value;
                 this.device.activeConnection().mute(value);
             },
 
-            toggleMuteMessage: function (number) {
-                const action = this.muted[number] ? "call_unmuted" : "call_muted";
-                this.toggleMute(number);
-                sendRequest(action, {number: {value: number, muted: self.muted[number]}})
+            toggleMuteMessage: function () {
+                const action = this.muted ? "call_unmuted" : "call_muted";
+                this.toggleMute();
+                sendRequest(action, {number: {value: this.selectedNumber, muted: self.muted}})
                     .then(() => {
 
                     })
                     .catch(err => console.error(err));
             },
 
-            toggleCallMessage: function (number) {
-                const action = this.onPhone[number] ? "call_ended" : "call_started";
+            toggleCallMessage: function () {
+                const number = this.selectedNumber;
+                const action = this.onPhone ? "call_ended" : "call_started";
                 this.toggleCall(number);
-                sendRequest(action, {number: {value: number, muted: self.muted[number]}})
+                sendRequest(action, {number: {value: number, muted: self.muted}})
                     .then(() => {
 
                     })
@@ -92,29 +117,33 @@
             // Make an outbound call with the current number,
             // or hang up the current call
             toggleCall: function (number) {
-                if (!this.onPhone[number]) {
-                    this.hasActiveCall = true;
-                    this.muted[number] = false;
-                    this.onPhone[number] = true;
+                if (!this.onPhone) {
+                    this.muted = false;
+                    this.onPhone = true;
+                    const isUnlisted = this.dropdownNumber === 'other';
                     // make outbound call with current number
-                    this.connection = this.device.connect({To: number});
+                    this.connection = this.device.connect({To: number, IsUnlistedNumber: isUnlisted});
                     this.log = 'Calling ' + number;
                     EventBus.$emit('tracker:call-mode:enter');
                 } else {
-                    this.hasActiveCall = false;
-                    this.muted[number] = false;
-                    this.onPhone[number] = false;
+                    this.muted = false;
+                    this.onPhone = false;
                     // hang up call in progress
                     this.device.disconnectAll();
                     EventBus.$emit('tracker:call-mode:exit');
                 }
             },
             resetPhoneState: function () {
-                self.hasActiveCall = false;
-                self.numbers.forEach(x => {
-                    self.onPhone[x] = false;
-                    self.muted[x] = false;
-                });
+
+                if (self.onPhone) {
+                    sendRequest("call_ended", {number: {value: self.selectedNumber, muted: false}})
+                        .then(() => {
+
+                        })
+                        .catch(err => console.error(err));
+                }
+                self.onPhone = false;
+                self.muted = false;
             },
             setTwilio: function (onDone) {
                 self.waiting = true;
@@ -166,21 +195,6 @@
                         self.log = 'Could not fetch token, see console.log';
                     });
             },
-            getActiveCalNumber() {
-                let number = null;
-                for (let i in self.onPhone) {
-
-                    if (!self.onPhone.hasOwnProperty(i)) {
-                        continue;
-                    }
-
-                    if (self.onPhone[i]) {
-                        number = i;
-                        break;
-                    }
-                }
-                return number;
-            },
             registerBroadcastChannelHandlers: function () {
                 registerHandler("call_status", (msg) => {
                     let status = null;
@@ -191,24 +205,23 @@
                     }
                     else {
                         status = self.device.status();
-                        if (status === "ready") {
-                            number = self.getActiveCalNumber();
+                        if (status === "ready" && self.onPhone) {
+                            number = self.selectedNumber;
                         }
                     }
                     return Promise.resolve({
                         status: status,
                         number: number ? {
                             value: number,
-                            muted: self.muted[number]
+                            muted: self.muted
                         } : null
                     });
                 });
 
                 function endCallHandler(msg) {
 
-                    const number = self.getActiveCalNumber();
-                    if (number) {
-                        self.toggleCall(number);
+                    if (self.onPhone) {
+                        self.toggleCall(self.selectedNumber);
                     }
 
                     //resolve the promise but also close the window
@@ -223,9 +236,8 @@
 
                 function muteHandler(msg) {
 
-                    const number = self.getActiveCalNumber();
-                    if (number) {
-                        self.toggleMute(number);
+                    if (self.onPhone) {
+                        self.toggleMute();
                     }
                     return Promise.resolve({});
                 }
@@ -239,22 +251,25 @@
 
             self = this;
             this.resetPhoneState();
-            this.registerBroadcastChannelHandlers();
+            //this.registerBroadcastChannelHandlers();
 
             window.onbeforeunload = function (event) {
-                let number = self.getActiveCalNumber();
-                if (number) {
-                    self.toggleCallMessage(number);
+                if (self.onPhone) {
+                    self.toggleCallMessage();
                 }
                 sendRequest("calls_page_closed", {})
-                    .then((msg) => {})
-                    .catch((err) => {});
+                    .then((msg) => {
+                    })
+                    .catch((err) => {
+                    });
             };
 
             //just handling the case of refresh
             sendRequest("calls_page_opened", {})
-                .then((msg) => {})
-                .catch((err) => {});
+                .then((msg) => {
+                })
+                .catch((err) => {
+                });
         },
         mounted() {
             self.setTwilio(() => {
