a44feaf6883dc5ee1a42f2318a8d46c48f9aae0a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-11-19 20:55:25 +0100

    CPM-427: Adds authy fields to users table

---
+++ database/migrations/2018_11_16_090431_add_authy_id.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAuthyId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('country_code')->nullable()->after('id');
+            $table->string('phone_number')->nullable()->after('id');
+            $table->string('authy_status')->default('unverified')->after('id');
+            $table->string('authy_id')->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('country_code');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('authy_status');
+            $table->dropColumn('authy_id');
+        });
+    }
+}
