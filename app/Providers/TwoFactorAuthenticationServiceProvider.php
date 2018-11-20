7e883c740fc25d14a6a98c5dde92b61a38be050a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-11-20 11:55:43 +0100

    CPM-427: Renames 2FA service

--- app/Providers/TwoFactorAuthenticationServiceProvider.php
+++ app/Providers/TwoFactorAuthenticationServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Contracts\TwoFactorAuthentication;
+use App\Contracts\TwoFactorAuthenticationApi;
 use Authy\AuthyApi;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,7 +25,7 @@ class TwoFactorAuthenticationServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(TwoFactorAuthentication::class, function() {
+        $this->app->bind(TwoFactorAuthenticationApi::class, function() {
             return new AuthyApi(config('services.authy.api_key'));
         });
     }
