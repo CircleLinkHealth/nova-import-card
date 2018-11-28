8280dffec4b605080a5a67ddf5b4a7c368e7fb98
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-11-28 05:20:34 +0100

    CPM-427: Improfes 2Fa middleware

--- app/Http/Middleware/HasPassed2FA.php
+++ app/Http/Middleware/HasPassed2FA.php
@@ -13,12 +13,14 @@ class HasPassed2FA
      */
     protected $except = [
         'user.2fa.show.token.form',
+        'user.inactivity-logout',
         'user.logout',
         'user.2fa.one-touch-request.create',
         'user.2fa.one-touch-request.check',
         'user.2fa.token.sms',
         'user.2fa.token.voice',
         'user.2fa.token.verify',
+        'user.settings.manage',
     ];
 
     /**
@@ -31,22 +33,36 @@ class HasPassed2FA
      */
     public function handle($request, Closure $next)
     {
-        if ( ! isAllowedToSee2FA()) {
-            return $next($request);
-        }
 //        if (app()->environment(['local'])) {
-//            \Session::put('authy_verified', true);
+//            \Session::put('authy_status', 'approved');
 //            return $next($request);
 //        }
 
-        if ( ! in_array(\Route::currentRouteName(), $this->except)
-             && optional(auth()->user())->is_authy_enabled
-             && session('authy_status') != 'approved') {
+        $user         = optional(auth()->user());
+        $currentRoute = \Route::currentRouteName();
+
+        if (in_array(\Route::currentRouteName(), $this->except)) {
+            return $next($request);
+        }
+
+        if ($user->hasRole('administrator') && ! $user->is_authy_enabled && $currentRoute != 'user.settings.manage') {
+            return redirect()->route('user.settings.manage');
+        }
+
+        if ( ! isAllowedToSee2FA() || ! $user->is_authy_enabled) {
+            if ($currentRoute == 'user.2fa.show.token.form') {
+                return redirect()->back();
+            }
+
+            return $next($request);
+        }
+
+        if (session('authy_status') != 'approved') {
             return redirect()->route('user.2fa.show.token.form');
         }
 
         if ($this->hasPassed2FA() && \Route::currentRouteName() == 'user.2fa.show.token.form') {
-            return redirect()->to('/');
+            return redirect()->back();
         }
 
         return $next($request);
