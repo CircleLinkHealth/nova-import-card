d4107cf751c3b5dd66bdf94989deefbc6b825677
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2018-12-05 00:48:47 +0100

    Fixes PDF library bug. Related to: Mark patients as unreachable every 5 calls, not just when unreachable calls == 5 [deploy:worker]
    Ref: https://clh-it.slack.com/archives/C0EPMB8DT/p1543347691001600

--- config/snappy.php
+++ config/snappy.php
@@ -1,16 +1,43 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+$debianPdfLib = '/usr/local/bin/wkhtmltopdf';
+$localPdfLib  = base_path('bin/wkhtmltopdf/wkhtmltopdf-amd64');
+
+if (file_exists($debianPdfLib)) {
+    $pdfBinary = $debianPdfLib;
+} elseif (file_exists($localPdfLib)) {
+    $pdfBinary = $localPdfLib;
+} else {
+    throw new \Exception('wkhtmltopdf binary not found.', 500);
+}
+
+//Img Binary
+$debianImgLib   = '/usr/local/bin/wkhtmltoimage';
+$composerImgLib = base_path('bin/wkhtmltoimage/wkhtmltoimage-amd64');
+
+if (file_exists($debianImgLib)) {
+    $imgBinary = $debianImgLib;
+} elseif (file_exists($composerImgLib)) {
+    $imgBinary = $composerImgLib;
+} else {
+    throw new \Exception('wkhtmltoimage binary not found.', 500);
+}
+
 return [
     'pdf' => [
         'enabled' => true,
-        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        'binary'  => $pdfBinary,
         'timeout' => false,
         'options' => [
-            'collate' => true,
+            'collate'                => true,
             'disable-external-links' => true,
             'disable-internal-links' => true,
             'enable-smart-shrinking' => true,
-
+            
             'margin-bottom' => '1cm',
             'margin-left'   => '1cm',
             'margin-right'  => '1cm',
@@ -19,11 +46,11 @@ return [
             'page-width'    => '5mm',
             'zoom'          => 0.8,
         ],
-        'env'     => [],
+        'env' => [],
     ],
     'image' => [
         'enabled' => true,
-        'binary'  => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage'),
+        'binary'  => $imgBinary,
         'timeout' => false,
         'options' => [],
         'env'     => [],
