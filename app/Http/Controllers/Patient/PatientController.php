3cf49c2a1a39a332945e74528753e76fd9ac7cab
Pangratios Cosma <pagcosma@gmail.com>
2018-12-05 18:37:06 +0200

    CPM-459 [wip] - new branch for Twilio

--- app/Http/Controllers/Patient/PatientController.php
+++ app/Http/Controllers/Patient/PatientController.php
@@ -66,21 +66,21 @@ class PatientController extends Controller
         $searchTerms = explode(' ', $input['users']);
 
         $query = User::intersectPracticesWith(auth()->user())
-                     ->ofType('participant')
-                     ->with(['primaryPractice', 'patientInfo', 'phoneNumbers']);
+            ->ofType('participant')
+            ->with(['primaryPractice', 'patientInfo', 'phoneNumbers']);
 
         foreach ($searchTerms as $term) {
             $query->where(function ($q) use ($term) {
                 $q->where('first_name', 'like', "%${term}%")
-                  ->orWhere('last_name', 'like', "%${term}%")
-                  ->orWhere('id', 'like', "%${term}%")
-                  ->orWhereHas('patientInfo', function ($query) use ($term) {
-                      $query->where('mrn_number', 'like', "%${term}%")
-                            ->orWhere('birth_date', 'like', "%${term}%");
-                  })
-                  ->orWhereHas('phoneNumbers', function ($query) use ($term) {
-                      $query->where('number', 'like', "%${term}%");
-                  });
+                    ->orWhere('last_name', 'like', "%${term}%")
+                    ->orWhere('id', 'like', "%${term}%")
+                    ->orWhereHas('patientInfo', function ($query) use ($term) {
+                        $query->where('mrn_number', 'like', "%${term}%")
+                          ->orWhere('birth_date', 'like', "%${term}%");
+                    })
+                    ->orWhereHas('phoneNumbers', function ($query) use ($term) {
+                        $query->where('number', 'like', "%${term}%");
+                    });
             });
         }
 
@@ -97,7 +97,7 @@ class PatientController extends Controller
             $programObj = Practice::find($d->program_id);
 
             $patients[$i]['program'] = $programObj->display_name ?? '';
-            $patients[$i]['hint']    = $patients[$i]['name'] . ' DOB:' . $patients[$i]['dob'] . ' [' . $patients[$i]['program'] . "] MRN: {$patients[$i]['mrn']} ID: {$d->id} PRIMARY PHONE: {$d->getPrimaryPhone()}";
+            $patients[$i]['hint']    = $patients[$i]['name'].' DOB:'.$patients[$i]['dob'].' ['.$patients[$i]['program']."] MRN: {$patients[$i]['mrn']} ID: {$d->id} PRIMARY PHONE: {$d->getPrimaryPhone()}";
             ++$i;
         }
 
@@ -107,8 +107,8 @@ class PatientController extends Controller
     public function showCallPatientPage(Request $request, $patientId)
     {
         $user = User::with('phoneNumbers')
-                    ->where('id', $patientId)
-                    ->firstOrFail();
+            ->where('id', $patientId)
+            ->firstOrFail();
 
         $phoneNumbers = $user->phoneNumbers->mapWithKeys(function ($p) {
             return [$p->type => $p->number];
@@ -149,8 +149,8 @@ class PatientController extends Controller
             ) {
                 return CarePlan::QA_APPROVED == $user->getCarePlanStatus();
             });
-            $patientsPendingApproval        = $this->formatter->patientListing($patients);
-            $pendingApprovals               = $patients->count();
+            $patientsPendingApproval = $this->formatter->patientListing($patients);
+            $pendingApprovals        = $patients->count();
         }
         $noLiveCountTimeTracking = true;
 
@@ -204,7 +204,7 @@ class PatientController extends Controller
     {
         $storageDirectory = 'storage/pdfs/patients/';
         $datetimePrefix   = date('Y-m-dH:i:s');
-        $fileName         = $storageDirectory . $datetimePrefix . '-patient-list.pdf';
+        $fileName         = $storageDirectory.$datetimePrefix.'-patient-list.pdf';
         $file             = $this->pdfService->createPdfFromView('wpUsers.patient.listing-pdf', [
             'patients' => $this->formatter->patients(),
         ], [
@@ -283,7 +283,7 @@ class PatientController extends Controller
     {
         // get number of approvals
         $patients = User::intersectPracticesWith(auth()->user())
-                        ->with('phoneNumbers', 'patientInfo', 'careTeamMembers')->whereHas('roles', function ($q) {
+            ->with('phoneNumbers', 'patientInfo', 'careTeamMembers')->whereHas('roles', function ($q) {
                 $q->where('name', '=', 'participant');
             })->get()->pluck('fullNameWithId', 'id')->all();
 
@@ -306,24 +306,24 @@ class PatientController extends Controller
 
         $wpUser = User::with([
             'primaryPractice',
-            'ccdProblems'  => function ($q) {
+            'ccdProblems' => function ($q) {
                 $q->with('cpmProblem.cpmInstructions')
-                  ->whereNotNull('cpm_problem_id');
+                    ->whereNotNull('cpm_problem_id');
             },
             'observations' => function ($q) {
                 $q->where('obs_unit', '!=', 'invalid')
-                  ->where('obs_unit', '!=', 'scheduled')
-                  ->with([
-                      'meta',
-                      'question.careItems',
-                  ])
-                  ->orderBy('obs_date', 'desc')
-                  ->take(100);
+                    ->where('obs_unit', '!=', 'scheduled')
+                    ->with([
+                        'meta',
+                        'question.careItems',
+                    ])
+                    ->orderBy('obs_date', 'desc')
+                    ->take(100);
             },
             'patientSummaries',
         ])
-                      ->where('id', $patientId)
-                      ->first();
+            ->where('id', $patientId)
+            ->first();
 
         if ( ! $wpUser) {
             return response('User not found', 401);
@@ -466,17 +466,17 @@ class PatientController extends Controller
                     $alertLevel = $observation->alert_level;
                 }
                 // lastly format json
-                $observation_json[$section] .= "{ obs_key:'" . $observation->obs_key . "', " .
-                                               "description:'" . str_replace(
+                $observation_json[$section] .= "{ obs_key:'".$observation->obs_key."', ".
+                                               "description:'".str_replace(
                                                    '_',
                                                    ' ',
                                                    $observation->description
-                                               ) . "', " .
-                                               "obs_value:'" . $observation->obs_value . "', " .
-                                               "dm_alert_level:'" . $alertLevel . "', " .
-                                               "obs_unit:'" . $observation->obs_unit . "', " .
-                                               "obs_message_id:'" . $observation->obs_message_id . "', " .
-                                               "comment_date:'" . Carbon::parse($observation->obs_date)->format('m-d-y h:i:s A') . "', " . '},';
+                                               )."', ".
+                                               "obs_value:'".$observation->obs_value."', ".
+                                               "dm_alert_level:'".$alertLevel."', ".
+                                               "obs_unit:'".$observation->obs_unit."', ".
+                                               "obs_message_id:'".$observation->obs_message_id."', ".
+                                               "comment_date:'".Carbon::parse($observation->obs_date)->format('m-d-y h:i:s A')."', ".'},';
                 ++$o;
             }
             $observation_json[$section] .= '],';
