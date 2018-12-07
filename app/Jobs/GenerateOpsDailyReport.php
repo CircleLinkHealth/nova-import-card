add2080a4bcce48ac5c1904d63fa18eff3bb6fac
kakoushias <kakoushias@gmail.com>
2018-12-07 18:27:09 +0200

    #OPS- retries and date changes

--- app/Jobs/GenerateOpsDailyReport.php
+++ app/Jobs/GenerateOpsDailyReport.php
@@ -24,6 +24,11 @@ class GenerateOpsDailyReport implements ShouldQueue
     private $date;
 
     /**
+     * @var Carbon
+     */
+    private $fromDate;
+
+    /**
      * Create a new job instance.
      *
      * @param Carbon|null $date
@@ -35,7 +40,27 @@ class GenerateOpsDailyReport implements ShouldQueue
         }
 
         $this->date = $date;
+
+        //If the job was not run between 23:30-23:59 we need to get revisions from 23:30, 2 days before.
+        //Example: we need data for 12/5 23:30 - 12/6 23:30 (time the report was supposed to run). If the job runs at 12/7 04:25,
+        //we need fromDate = date->subDay(2)->setTimeFromTimeString('23:30')
+        //
+        //Even though this will make the report more accurate, it still makes the report not agree with the next day report (if that was ran at the designated time.
+        //Example: if the report gets data for 12/5 23:30 - 12/7 02:30, the next day report will get data for 12/6 23:30 - 12/7 23:30.
+        //Thus changes between 12/6 23:30 and 12/7 2:30 will be calculated in both reports, making (total added/lost patients and prior day totals have potential discrepancies)
+        if ($this->date->copy()->format('H:i:s') >= '00:00:00' && $this->date->copy()->format('H:i:s') <= '23:29:00'){
+            $this->fromDate = $this->date->copy()->subDay(2)->setTimeFromTimeString('23:30');
+        }else{
+            $this->fromDate = $this->date->copy()->subDay()->setTimeFromTimeString('23:30');
+        }
     }
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 3;
 
     /**
      * Execute the job.
@@ -59,7 +84,7 @@ class GenerateOpsDailyReport implements ShouldQueue
                                          'patientInfo.revisionHistory' => function ($r) {
                                              $r->where('key', 'ccm_status')
                                                ->where('created_at', '>=',
-                                                   $this->date->copy()->subDay()->setTimeFromTimeString('23:30'));
+                                                   $this->fromDate);
                                          },
                                      ]);
                                  },
