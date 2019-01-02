f0d4ead16f57c2dd317dd729d22995088f8848b4
kakoushias <kakoushias@gmail.com>
2019-01-02 02:37:42 +0200

    #CPM-477 adds command to unassign care ambassadors from enrollees daily

---
+++ app/Jobs/ResetAssignedCareAmbassadorsFromEnrollees.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enrollee;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class ResetAssignedCareAmbassadorsFromEnrollees implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Enrollee::whereNotIn('status', ['ineligible', 'consented'])
+            ->update(['care_ambassador_id' => null]);
+    }
+}
