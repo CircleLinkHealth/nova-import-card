f438bbf1fee7be744780f9e96da03b2c3b917795
Antonis Antoniou <kountouris7@gmail.com>
2019-01-05 13:34:18 +0200

    Revert "CPM-461 Improve Manual Condition Entry / Condition Search com1"
    This reverts commit 961e53e

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -82,7 +82,7 @@ class Handler extends ExceptionHandler
 
         return parent::render($request, $e);
     }
-
+    
     /**
      * Report or log an exception.
      *
@@ -90,14 +90,13 @@ class Handler extends ExceptionHandler
      *
      * @param \Exception $e
      *
-     * @throws Exception
-     *
      * @return mixed|void
+     * @throws Exception
      */
     public function report(Exception $e)
     {
         parent::report($e);
-
+        
         if ( ! $this->shouldReport($e)) {
             return;
         }
