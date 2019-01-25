87fca43d7451ce5219d4b16356389770fae60c9d
Antonis Antoniou <kountouris7@gmail.com>
2019-01-25 23:30:01 +0200

    CPM 559 - Create link with token

---
+++ database/migrations/2019_01_25_194329_create_awv_invitation_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAwvInvitationLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('awv_invitation_links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('patient_user_id');
+            $table->string('patient_name');
+            $table->date('birth_date');
+            $table->unsignedInteger('survey_id');
+            //$table->string('token');
+            $table->string('link_token')->unique();
+            $table->boolean('is_expired')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('awv_invitation_links');
+    }
+}
