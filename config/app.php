72e024fb6c6ff1a9471efc638983d98bbeb87811
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-02-18 11:51:50 +0100

    Removes debugbar from being loaded

--- config/app.php
+++ config/app.php
@@ -284,7 +284,6 @@ return [
         'Inspiring' => Illuminate\Foundation\Inspiring::class,
 
         'DataTables'     => Yajra\DataTables\Facades\DataTables::class,
-        'Debugbar'       => Barryvdh\Debugbar\Facade::class,
         'Excel'          => \Maatwebsite\Excel\Facades\Excel::class,
         'Form'           => \Collective\Html\FormFacade::class,
         'Html'           => \Collective\Html\HtmlFacade::class,
