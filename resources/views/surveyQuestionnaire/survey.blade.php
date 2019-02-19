3200fbb722e912834d4bad7f4f211aab3bd66c33
Antonis Antoniou <kountouris7@gmail.com>
2019-02-19 13:33:10 +0200

    cpm 502 - Vuejs

--- resources/views/surveyQuestionnaire/survey.blade.php
+++ resources/views/surveyQuestionnaire/survey.blade.php
@@ -1,17 +1,22 @@
-<div id="app">
-    <div class="container">
-        <h2>Survey Questions</h2>
-        <form method="POST" action="{{route('saveSurveyAnswer')}}">
-            {{csrf_field()}}
-            <div class="form-group">
-                <div class="col-md-8">
-                    <label for="answer">How you doing?</label><br>
-                    <input id="answer" type="text" class="validate" name="answer" required>
-                    <input type="hidden" name="link_token" value="{{$urlToken}}">
-                </div>
-
-                <button class="btn btn-primary" type="submit">Next</button>
-            </div>
-        </form>
+<div class="container">
+    <div id="app">
+        <survey-form></survey-form>
     </div>
+
+
+    {{--  <h2>Survey Questions</h2>
+      <form method="POST" action="{{route('saveSurveyAnswer')}}">
+          {{csrf_field()}}
+          <div class="form-group">
+              <div class="col-md-8">
+                  <label for="answer">How you doing?</label><br>
+                  <input id="answer" type="text" class="validate" name="answer" required>
+                  <input type="hidden" name="link_token" value="{{$urlToken}}">
+              </div>
+
+              <button class="btn btn-primary" type="submit">Next</button>
+          </div>
+      </form>--}}
+    <script src="{{asset('js/app.js')}}"></script>
 </div>
+
