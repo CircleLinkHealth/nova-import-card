b5be3833a4a7ff19885cbaa57c578f9bfa963d98
Pangratios Cosma <pagcosma@gmail.com>
2019-03-12 09:15:44 +0200

    fix migration name, must match file name

--- database/migrations/2019_03_08_114239_update_careplan_print_list_view_table.php
+++ database/migrations/2019_03_08_114239_update_careplan_print_list_view_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateCareplanPrintListView extends Migration
+class UpdateCareplanPrintListViewTable extends Migration
 {
     /**
      * Run the migrations.
