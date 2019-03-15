f94612fd6ca8aadde3b8576541b4925561bdcec6
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-03-15 11:32:06 +0100

    Getting somewhere

--- Config/config.php
+++ Config/config.php
@@ -1,12 +1,30 @@
 <?php
 
 return [
-    'name' => 'Raygun',
-    'apiKey' => 'yourapikey',
-
+    'enable_crash_reporting' => env('ENABLE_RAYGUN_CRASH_REPORTING', false),
+    
+    'enable_real_user_monitoring' => env('ENABLE_RAYGUN_REAL_USER_MONITORING', false),
+    
+    'apiKey'    => env('RAYGUN_API_KEY', 'yourapikey'),
+    
     /**
      * WARNING: PHP `exec()` must be enabled for this. IF it's not enabled, it default to fals at runtime.
      */
-    'async' => false,
-    'debugMode'=> false
+    'async'     => env('ENABLE_RAYGUN_CRASH_REPORTING_ASYNC_MODE', false),
+    
+    'debugMode' => env('ENABLE_RAYGUN_CRASH_REPORTING_DEBUG_MODE', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Logger Notify Level
+    |--------------------------------------------------------------------------
+    |
+    | This sets the level at which a logged message will trigger a notification
+    | to Bugsnag.  By default this level will be 'debug'.
+    |
+    | Must be one of the Psr\Log\LogLevel levels from the Psr specification.
+    |
+    */
+
+    'logger_notify_level' => env('RAYGUN_CRASH_REPORTING_LOGGER_LEVEL', 'debug'),
 ];
--- Providers/RaygunServiceProvider.php
+++ Providers/RaygunServiceProvider.php
@@ -2,11 +2,22 @@
 
 namespace CircleLinkHealth\Raygun\Providers;
 
+use CircleLinkHealth\Raygun\LaravelLogger;
+use CircleLinkHealth\Raygun\Facades\Raygun;
+use CircleLinkHealth\Raygun\MultiLogger;
+use CircleLinkHealth\Raygun\PsrLogger\MultiLogger as BaseMultiLogger;
+use CircleLinkHealth\Raygun\PsrLogger\RaygunLogger;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory;
-
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Log\LogManager;
+use Monolog\Handler\PsrHandler;
+use Monolog\Logger;
 use Raygun4php\RaygunClient;
+use Illuminate\Contracts\Logging\Log;
+use Psr\Log\LoggerInterface;
+
 
 class RaygunServiceProvider extends ServiceProvider
 {
@@ -16,7 +27,7 @@ class RaygunServiceProvider extends ServiceProvider
      * @var bool
      */
     protected $defer = false;
-
+    
     /**
      * Boot the application events.
      *
@@ -28,9 +39,9 @@ class RaygunServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->registerFactories();
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
-
+    
     /**
      * Register the service provider.
      *
@@ -38,13 +49,68 @@ class RaygunServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerConfig();
+        
         $this->app->register(RouteServiceProvider::class);
+        
+        if (true === Config::get('cpm-module-raygun.enable_crash_reporting')) {
+            $this->app->singleton(
+                'raygun',
+                function ($app) {
+                    $client = new RaygunClient(
+                        Config::get('cpm-module-raygun.apiKey'),
+                        $this->shouldEnableAsync(),
+                        Config::get('cpm-module-raygun.debugMode')
+                    );
+                    
+                    if (\Auth::check()) {
+                        $authUSer = \Auth::user();
+                        
+                        $client->SetUser(
+                            $user = $authUSer->id,
+                            $firstName = $authUSer->first_name,
+                            $fullName = $authUSer->display_name,
+                            $email = $authUSer->email
+                        );
+                    }
+                    
+                    return $client;
+                }
+            );
     
-        $this->app->singleton('raygun', function($app){
-            return new RaygunClient(Config::get('laravel-raygun.apiKey'), $this->shouldEnableAsync(), Config::get('laravel-raygun.debugMode'));
-        });
+            $this->app->singleton(
+                'raygun.logger', function (Container $app) {
+                $config = $app->config->get('cpm-module-raygun');
+                $logger = interface_exists(Log::class) ? new LaravelLogger($app['raygun'], $app['events']) : new RaygunLogger($app['raygun']);
+                if (isset($config['logger_notify_level'])) {
+                    $logger->setNotifyLevel($config['logger_notify_level']);
+                }
+        
+                return $logger;
+            });
+    
+            $this->app->singleton('raygun.multi', function (Container $app) {
+                return interface_exists(Log::class) ? new MultiLogger([$app['log'], $app['raygun.logger']]) : new BaseMultiLogger([$app['log'], $app['raygun.logger']]);
+            });
+    
+            if ($this->app['log'] instanceof LogManager) {
+                $this->app['log']->extend('raygun', function (Container $app, array $config) {
+                    $handler = new PsrHandler($app['raygun.logger']);
+            
+                    return new Logger('raygun', [$handler]);
+                });
+            }
+            $this->app->alias('raygun.logger', interface_exists(Log::class) ? LaravelLogger::class : RaygunLogger::class);
+            $this->app->alias('raygun.multi', interface_exists(Log::class) ? MultiLogger::class : BaseMultiLogger::class);
+    
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Raygun', Raygun::class);
+    
+            $this->app->alias('raygun.logger', Log::class);
+            $this->app->alias('raygun.logger', LoggerInterface::class);
+        }
     }
-
+    
     /**
      * Register config.
      *
@@ -52,14 +118,18 @@ class RaygunServiceProvider extends ServiceProvider
      */
     protected function registerConfig()
     {
-        $this->publishes([
-            __DIR__.'/../Config/config.php' => config_path('raygun.php'),
-        ], 'config');
+        $this->publishes(
+            [
+                __DIR__.'/../Config/config.php' => config_path('cpm-module-raygun.php'),
+            ],
+            'config'
+        );
         $this->mergeConfigFrom(
-            __DIR__.'/../Config/config.php', 'raygun'
+            __DIR__.'/../Config/config.php',
+            'cpm-module-raygun'
         );
     }
-
+    
     /**
      * Register views.
      *
@@ -68,18 +138,30 @@ class RaygunServiceProvider extends ServiceProvider
     public function registerViews()
     {
         $viewPath = resource_path('views/modules/raygun');
-
+        
         $sourcePath = __DIR__.'/../Resources/views';
-
-        $this->publishes([
-            $sourcePath => $viewPath
-        ],'views');
-
-        $this->loadViewsFrom(array_merge(array_map(function ($path) {
-            return $path . '/modules/raygun';
-        }, \Config::get('view.paths')), [$sourcePath]), 'raygun');
+        
+        $this->publishes(
+            [
+                $sourcePath => $viewPath,
+            ],
+            'views'
+        );
+        
+        $this->loadViewsFrom(
+            array_merge(
+                array_map(
+                    function ($path) {
+                        return $path.'/modules/raygun';
+                    },
+                    \Config::get('view.paths')
+                ),
+                [$sourcePath]
+            ),
+            'raygun'
+        );
     }
-
+    
     /**
      * Register translations.
      *
@@ -88,26 +170,26 @@ class RaygunServiceProvider extends ServiceProvider
     public function registerTranslations()
     {
         $langPath = resource_path('lang/modules/raygun');
-
+        
         if (is_dir($langPath)) {
             $this->loadTranslationsFrom($langPath, 'raygun');
         } else {
-            $this->loadTranslationsFrom(__DIR__ .'/../Resources/lang', 'raygun');
+            $this->loadTranslationsFrom(__DIR__.'/../Resources/lang', 'raygun');
         }
     }
-
+    
     /**
      * Register an additional directory of factories.
-     * 
+     *
      * @return void
      */
     public function registerFactories()
     {
-        if (! app()->environment('production')) {
-            app(Factory::class)->load(__DIR__ . '/../Database/factories');
+        if ( ! app()->environment('production')) {
+            app(Factory::class)->load(__DIR__.'/../Database/factories');
         }
     }
-
+    
     /**
      * Get the services provided by the provider.
      *
@@ -129,7 +211,7 @@ class RaygunServiceProvider extends ServiceProvider
     {
         $disabled = explode(',', ini_get('disable_functions'));
         
-        return function_exists($func) && !in_array($func, $disabled);
+        return function_exists($func) && ! in_array($func, $disabled);
     }
     
     private function shouldEnableAsync()
