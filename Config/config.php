fc019442cc78f51da5e02e75d7557169b6a6b939
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-03-15 12:24:58 +0100

    adds comments in config file

--- Config/config.php
+++ Config/config.php
@@ -1,18 +1,31 @@
 <?php
 
 return [
-    'enable_crash_reporting' => env('ENABLE_RAYGUN_CRASH_REPORTING', false),
-    
-    'enable_real_user_monitoring' => env('ENABLE_RAYGUN_REAL_USER_MONITORING', false),
-    
+    /*
+   |--------------------------------------------------------------------------
+   | API Key
+   |--------------------------------------------------------------------------
+   |
+   | You can find your API key on your Raygun dashboard, under "Appplication Settings".
+   |
+   | This api key points the Raygun notifier to the project in your account
+   | which should receive your application's uncaught exceptions.
+   |
+   */
     'apiKey'    => env('RAYGUN_API_KEY', 'yourapikey'),
-    
-    /**
-     * WARNING: PHP `exec()` must be enabled for this. IF it's not enabled, it default to fals at runtime.
-     */
-    'async'     => env('ENABLE_RAYGUN_CRASH_REPORTING_ASYNC_MODE', false),
-    
-    'debugMode' => env('ENABLE_RAYGUN_CRASH_REPORTING_DEBUG_MODE', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Crash Reporting
+    |--------------------------------------------------------------------------
+    |
+    | Use Crash Reporting feature in this application.
+    |
+    | This will send exceptions and logs to Raygun, and they will appear under Crash Reporting.
+    | Defaults to false.
+    |
+    */
+    'enable_crash_reporting' => env('ENABLE_RAYGUN_CRASH_REPORTING', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -20,11 +33,50 @@ return [
     |--------------------------------------------------------------------------
     |
     | This sets the level at which a logged message will trigger a notification
-    | to Bugsnag.  By default this level will be 'debug'.
+    | to Raygun.
     |
     | Must be one of the Psr\Log\LogLevel levels from the Psr specification.
+    | Defaults to 'debug'.
     |
     */
-
     'logger_notify_level' => env('RAYGUN_CRASH_REPORTING_LOGGER_LEVEL', 'debug'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Crash Reporting Async Mode
+    |--------------------------------------------------------------------------
+    |
+    | Send data to Crash Reporting asynchronously
+    |
+    | WARNING: PHP `exec()` must be enabled for this. IF it's not enabled, it default to fals at runtime.
+    | Defaults to false.
+    |
+    */
+    'async'     => env('ENABLE_RAYGUN_CRASH_REPORTING_ASYNC_MODE', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Raygun Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    | Defaults to false.
+    |
+    */
+    'debugMode' => env('ENABLE_RAYGUN_CRASH_REPORTING_DEBUG_MODE', false),
+
+    /*
+   |--------------------------------------------------------------------------
+   | Enable Real User Monitoring
+   |--------------------------------------------------------------------------
+   |
+   | Use Real User Monitoring feature in this application.
+   |
+   | This will send monitoring data to Raygun, and it will appear under Real User Monitoring.
+   | Defaults to false.
+   |
+   */
+    'enable_real_user_monitoring' => env('ENABLE_RAYGUN_REAL_USER_MONITORING', false),
 ];
