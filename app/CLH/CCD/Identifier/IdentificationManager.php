07cd3c6271be242d4af8b81e43ac3eb27fe32ad0
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-03-16 02:32:54 +0100

    formatting changes

--- app/CLH/CCD/Identifier/IdentificationManager.php
+++ app/CLH/CCD/Identifier/IdentificationManager.php
@@ -16,11 +16,11 @@ class IdentificationManager extends BaseIdentificationStrategy
         'doctor_oid'     => null,
         'ehr_oid'        => null,
     ];
-
+    
     public function identify()
     {
         $identifierMap = \config('ccdimportervendoridentifiermap');
-
+        
         /*
          * Extracts Identifier Values from the CCD.
          * This function calls all the Identifiers from config/ccdimportervendoridentifiermap
@@ -30,18 +30,18 @@ class IdentificationManager extends BaseIdentificationStrategy
                 if ( ! empty($this->matchedIdentifiers[$field])) {
                     continue 2;
                 }
-                $this->matchedIdentifiers[$field] = ( new $identifier['class']($this->ccd) )->identify();
+                $this->matchedIdentifiers[$field] = (new $identifier['class']($this->ccd))->identify();
             }
         }
-
+        
         //this will get rid of empty entries
         $filteredIdentifiers = array_filter($this->matchedIdentifiers);
-
+        
         if ( ! $filteredIdentifiers) {
             // all identifier values are false
             return false;
         }
-
+        
         return $filteredIdentifiers;
     }
 }
