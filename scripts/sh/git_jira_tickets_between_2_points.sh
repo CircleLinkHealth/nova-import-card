2b81b3af8b785c23edbebd9730b4a7bfa5a01b6c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-03-18 22:15:25 +0100

    CPM-781: More Logging

--- scripts/sh/git_jira_tickets_between_2_points.sh
+++ scripts/sh/git_jira_tickets_between_2_points.sh
@@ -6,3 +6,6 @@ TO=$2
 # Show unique Jira Issues worked on since the provided tag.
 # Jira Issues begin with CPM-
 git log --pretty=oneline $FROM...$TO  | perl -ne '{ /(CPM)-(\d+)/ && print "$1-$2\n" }' | sort | uniq
+
+# if perls is not available, use
+# git log --pretty=oneline $FROM...$TO | grep "CPM-" | sort | uniq
