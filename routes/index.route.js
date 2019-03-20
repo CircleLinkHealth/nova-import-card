17a9e24f34083871a52410387b487ef56738837f
Pangratios Cosma <pagcosma@gmail.com>
2019-03-20 17:34:55 +0200

    CPM-783 Restart at 2am (#28)
    * CPM-783 Restart at 2am
    
    * CPM-783 comment out console.debug
    
    * CPM-783 show uptime in index route
    
    * CPM-783 show uptime in index route
    
    * CPM-783 make sure its 2am!

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 if (process.env.NODE_ENV !== 'production') {
-  const swaggerSpec = require('../swagger')
+    const swaggerSpec = require('../swagger')
 
-  router.get('/swagger.json', function(req, res) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(swaggerSpec);
-  });
+    router.get('/swagger.json', function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
 
-  /* GET home page. */
-  require('express-swagger-ui')({
-    app       : app,
-    swaggerUrl: '/swagger.json',  // this is the default value 
-    localPath : '/'       // this is the default value 
-  });
+    /* GET home page. */
+    require('express-swagger-ui')({
+        app: app,
+        swaggerUrl: '/swagger.json',  // this is the default value
+        localPath: '/'       // this is the default value
+    });
 }
 else {
-  router.get('/', (req, res) => {
-    res.send({ message: 'Time Tracker' })
-  })
+    router.get('/', (req, res) => {
+        res.send({message: 'Time Tracker', uptime: Math.floor(process.uptime())})
+    })
 }
 
 module.exports = router;
