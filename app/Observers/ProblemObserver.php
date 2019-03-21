e0f9bb06f92dfdaa71b282d6bfa7e21d3286da38
kakoushias <kakoushias@gmail.com>
2019-03-21 13:10:34 +0200

    CPM-790 account for if only cpm_problem_id is dirty

--- app/Observers/ProblemObserver.php
+++ app/Observers/ProblemObserver.php
@@ -32,16 +32,7 @@ class ProblemObserver
 
         $storage = new ProblemsToMonitor($patient->program_id, $patient);
 
-        $problems           = $patient->ccdProblems;
-        $problemsToActivate = [];
-
-        foreach ($problems as $problem) {
-            if (empty($problem->cpm_problem_id)) {
-                continue;
-            }
-
-            $problemsToActivate[] = $problem->cpm_problem_id;
-        }
+        $problemsToActivate = $this->getProblemsToActivate($patient);
 
         $storage->import(array_unique($problemsToActivate), true);
     }
@@ -54,13 +45,45 @@ class ProblemObserver
     public function saved(Problem $problem)
     {
         if ($problem->isDirty('cpm_problem_id')) {
-            $patient = User::find($problem->patient_id);
+            //exclude generic diabetes
+            $diabetes = CpmProblem::where('name', 'Diabetes')->first();
+
+            $patient = User::with([
+                'ccdProblems' => function ($p) use ($diabetes) {
+                    $p->where('cpm_problem_id', '!=', $diabetes->id);
+                },
+            ])
+                ->where('id', $problem->patient_id)->first();
 
             if ($patient) {
                 $storage = new ProblemsToMonitor($patient->program_id, $patient);
 
-                $storage->import($problem->cpm_problem_id);
+                // If only the cpm_problem_id is changed, we sync relationships (symptoms, lifestyles etc)  for all problems again,
+                if ( ! $problem->isDirty(['patient_id', 'name'])) {
+                    $problemsToActivate = $this->getProblemsToActivate($patient);
+
+                    $storage->import(array_unique($problemsToActivate), true);
+                } else {
+                    // else we are just importing the new problem's relationships.
+                    $storage->import($problem->cpm_problem_id);
+                }
             }
         }
+    }
+
+    protected function getProblemsToActivate($patient)
+    {
+        $problems           = $patient->ccdProblems;
+        $problemsToActivate = [];
+
+        foreach ($problems as $problem) {
+            if (empty($problem->cpm_problem_id)) {
+                continue;
+            }
+
+            $problemsToActivate[] = $problem->cpm_problem_id;
+        }
+
+        return $problemsToActivate;
     }
 }
