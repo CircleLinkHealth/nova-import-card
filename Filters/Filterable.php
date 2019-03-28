7c20aeb26402c19628aed05a81496a7578167c19
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-03-29 00:56:18 +0545

    Add filterable

---
+++ Filters/Filterable.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\Core\Filters;
+
+use App\Filters\QueryFilters;
+use Illuminate\Database\Eloquent\Builder;
+
+trait Filterable
+{
+    /**
+     * Filter a result set.
+     *
+     * @param Builder      $query
+     * @param QueryFilters $filters
+     *
+     * @return Builder
+     */
+    public function scopeFilter($query, QueryFilters $filters)
+    {
+        return $filters->apply($query);
+    }
+}
