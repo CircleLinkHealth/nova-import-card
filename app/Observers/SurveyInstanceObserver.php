770717fa67259b19286c54f36325685f76548d81
kakoushias <kakoushias@gmail.com>
2019-03-28 17:45:03 +0200

    CPM-505 removes observer code (replaced by event/listener)

--- app/Observers/SurveyInstanceObserver.php
+++ app/Observers/SurveyInstanceObserver.php
@@ -17,19 +17,7 @@ class SurveyInstanceObserver
     public function saved(SurveyInstance $instance)
     {
 
-        $x =1;
-        if ($instance->pivot->status === SurveyInstance::COMPLETED) {
-            $otherInstance = SurveyInstance::isCompleted()
-                                           ->where('user_id', $instance->user_id)
-                                           ->where('start_date', $instance->start_date)
-                                           ->where('end_date', $instance->end_date)
-                                           ->where('survey_id', '!=', $instance->survey_id)
-                                           ->first();
-
-            if ($otherInstance) {
-                GenerateProviderReport::dispatch($instance->user_id, $instance->start_date)->onQueue('high');
-            }
-        }
+        //replaced by event and listener, keeping class in case we need
 
     }
 }
