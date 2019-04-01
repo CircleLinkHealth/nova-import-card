146b654749769b1be05d856c7fc1e9fa0981dc63
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-04-01 17:26:52 +0545

    Adds ide helpers

--- Entities/AuthyUser.php
+++ Entities/AuthyUser.php
@@ -9,6 +9,34 @@ namespace CircleLinkHealth\TwoFA\Entities;
 use CircleLinkHealth\Core\Entities\BaseModel;
 use CircleLinkHealth\Customer\Entities\User;
 
+/**
+ * CircleLinkHealth\TwoFA\Entities\AuthyUser
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int $is_authy_enabled
+ * @property string|null $authy_id
+ * @property string|null $authy_method
+ * @property string|null $country_code
+ * @property string|null $phone_number
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Venturecraft\Revisionable\Revision[] $revisionHistory
+ * @property-read \CircleLinkHealth\Customer\Entities\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereAuthyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereAuthyMethod($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereCountryCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereIsAuthyEnabled($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\TwoFA\Entities\AuthyUser whereUserId($value)
+ * @mixin \Eloquent
+ */
 class AuthyUser extends BaseModel
 {
     protected $fillable = [
