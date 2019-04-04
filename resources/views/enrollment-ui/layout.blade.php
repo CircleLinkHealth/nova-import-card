b46337e513fa7f12978db47f555a837d37ce53cd
kakoushias <kakoushias@gmail.com>
2019-04-04 03:02:06 +0300

    CPM-802 reverts changes, ui broke

--- resources/views/enrollment-ui/layout.blade.php
+++ resources/views/enrollment-ui/layout.blade.php
@@ -19,15 +19,10 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
-
-
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
-
+<!-- Dropdown Structure -->
 <ul id="dropdown1" class="dropdown-content">
     <li><a href="{{ url('/auth/logout') }}">
             Logout
