2f54af8e3302940912f54380589e2c7b35e3a514
kakoushias <kakoushias@gmail.com>
2019-04-04 16:33:25 +0300

    CPM-806 adds front-end

--- resources/assets/js/components/enrollment/dashboard.vue
+++ resources/assets/js/components/enrollment/dashboard.vue
@@ -380,7 +380,7 @@
                     <div class="row">
                         <div class="col s12 m12">
                             <label for="utc-reason" class="label">Reason:</label>
-                            <select name="reason" id="utc-reason" required>
+                            <select v-model="utc_reason" name="reason" id="utc-reason" required>
                                 <option value="voicemail">Left A Voicemail</option>
                                 <option value="disconnected">Disconnected Number</option>
                                 <option value="requested callback">Requested Call At Other Time</option>
@@ -391,6 +391,11 @@
                         <div class="col s6 m12 select-custom">
                             <label for="utc_reason_other" class="label">If you selected other, please specify:</label>
                             <input class="input-field" name="reason_other" id="utc_reason_other"/>
+                        </div>
+
+                        <div v-show="utc_requested_callback" class="col s6 m12 select-custom">
+                            <label for="utc_callback" class="label">Patient Requests Callback On:</label>
+                            <input type="date" name="utc_callback" id="utc_callback">
                         </div>
 
                     </div>
@@ -632,7 +637,10 @@
             },
             cell_is_invalid: function () {
                 return !this.validatePhone(this.cell_phone)
-            }
+            },
+            utc_requested_callback(){
+                return this.utc_reason === 'requested callback';
+            },
         },
         data: function () {
             return {
@@ -662,6 +670,7 @@
                 callStatus: 'Summoning Calling Gods...',
                 toCall: '',
                 isSoftDecline: false,
+                utc_reason: '',
                 callError: null,
                 consentedUrl: rootUrl('enrollment/consented'),
                 utcUrl: rootUrl('enrollment/utc'),
