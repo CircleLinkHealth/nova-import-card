28d170e71558b6619907077dad5b67578b62f825
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-04-08 13:34:35 +0545

    fix

--- .deploybot/after_new_version_is_uploaded_no_build_commands.sh
+++ .deploybot/after_new_version_is_uploaded_no_build_commands.sh
@@ -9,9 +9,6 @@ BRANCH=$4
 RELEASE_ID=$5
 PREVIOUS_REVISION=$6
 
-# uncompress tarfile
-tar -xzf $RELEASE/release.tar.gz
-
 # Create a shared storage directory and symlink it to the project root
 if [ ! -d "$SHARED/storage" ]; then
   mkdir -p $SHARED/storage
