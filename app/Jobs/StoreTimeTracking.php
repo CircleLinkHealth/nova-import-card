236912ff6c41bfb4952d6e7fce037e16c7a52db8
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-04-12 11:17:50 +0545

    Removes unnecessary function. Some time tracking optimizations

--- app/Jobs/StoreTimeTracking.php
+++ app/Jobs/StoreTimeTracking.php
@@ -1,20 +1,23 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 namespace App\Jobs;
 
-use CircleLinkHealth\TimeTracking\Entities\Activity;
 use App\Algorithms\Invoicing\AlternativeCareTimePayableCalculator;
-use CircleLinkHealth\Customer\Entities\Nurse;
-use CircleLinkHealth\TimeTracking\Entities\PageTimer;
 use App\Services\ActivityService;
-use CircleLinkHealth\Customer\Entities\User;
 use Carbon\Carbon;
+use CircleLinkHealth\Customer\Entities\Nurse;
+use CircleLinkHealth\Customer\Entities\User;
+use CircleLinkHealth\TimeTracking\Entities\Activity;
+use CircleLinkHealth\TimeTracking\Entities\PageTimer;
 use Illuminate\Bus\Queueable;
-use Illuminate\Http\Request;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 class StoreTimeTracking implements ShouldQueue
@@ -24,7 +27,7 @@ class StoreTimeTracking implements ShouldQueue
      * @var array
      */
     protected $params;
-    
+
     /**
      * Create a new job instance.
      *
@@ -37,26 +40,24 @@ class StoreTimeTracking implements ShouldQueue
 
     /**
      * Execute the job.
-     *
-     * @return void
      */
     public function handle()
     {
         $patientId  = $this->params->get('patientId');
         $providerId = $this->params->get('providerId', null);
-    
+
+        $isBhi = User::isBhiChargeable()
+            ->where('id', $patientId)
+            ->exists();
+
         foreach ($this->params->get('activities', []) as $activity) {
             $duration = $activity['duration'];
-        
+
             $startTime = Carbon::createFromFormat('Y-m-d H:i:s', $activity['start_time']);
             $endTime   = $startTime->copy()->addSeconds($duration);
-        
+
             $redirectTo = $this->params->get('redirectLocation', null);
-        
-            $isBhi = User::isBhiChargeable()
-                         ->where('id', $patientId)
-                         ->exists();
-        
+
             $newActivity                    = new PageTimer();
             $newActivity->redirect_to       = $redirectTo;
             $newActivity->billable_duration = $duration;
@@ -67,52 +68,52 @@ class StoreTimeTracking implements ShouldQueue
             $newActivity->start_time        = $startTime->toDateTimeString();
             $newActivity->end_time          = $endTime->toDateTimeString();
             $is_behavioral                  = isset($activity['is_behavioral'])
-                ? (bool)$activity['is_behavioral'] && $isBhi
+                ? (bool) $activity['is_behavioral'] && $isBhi
                 : $isBhi;
-            $newActivity->url_full          = $activity['url'];
-            $newActivity->url_short         = $activity['url_short'];
-            $newActivity->program_id        = $this->params->get('programId');
-            $newActivity->ip_addr           = $this->params->get('ipAddr');
-            $newActivity->activity_type     = $activity['name'];
-            $newActivity->title             = $activity['title'];
-            $newActivity->user_agent        = $this->params->get('userAgent', null);
+            $newActivity->url_full      = $activity['url'];
+            $newActivity->url_short     = $activity['url_short'];
+            $newActivity->program_id    = $this->params->get('programId');
+            $newActivity->ip_addr       = $this->params->get('ipAddr');
+            $newActivity->activity_type = $activity['name'];
+            $newActivity->title         = $activity['title'];
+            $newActivity->user_agent    = $this->params->get('userAgent', null);
             $newActivity->save();
-        
+
             $activityId = null;
-        
+
             if ($newActivity->billable_duration > 0) {
                 $activityId = $this->addPageTimerActivities($newActivity, $is_behavioral);
             }
-        
+
             if ($activityId) {
                 $this->handleNurseLogs($activityId);
             }
         }
     }
-    
+
     private function addPageTimerActivities(PageTimer $pageTimer, $is_behavioral = false)
     {
         // check params to see if rule exists
         $params = [];
-        
+
         //user
         $user = User::find($pageTimer->provider_id);
-        
-        if (( ! (bool)$user->isCCMCountable()) || (0 == $pageTimer->patient_id)) {
+
+        if (( ! (bool) $user->isCCMCountable()) || (0 == $pageTimer->patient_id)) {
             return false;
         }
-        
+
         // activity param
         $params['activity'] = $pageTimer->activity_type;
-        
+
         $omitted_routes = [
             'patient.activity.create',
             'patient.activity.providerUIIndex',
             'patient.reports.progress',
         ];
-        
+
         $is_ommited = in_array($pageTimer->title, $omitted_routes);
-        
+
         if ( ! $is_ommited) {
             $activityParams                  = [];
             $activityParams['type']          = $params['activity'];
@@ -125,43 +126,42 @@ class StoreTimeTracking implements ShouldQueue
             $activityParams['logged_from']   = 'pagetimer';
             $activityParams['logger_id']     = $pageTimer->provider_id;
             $activityParams['page_timer_id'] = $pageTimer->id;
-            
-            // if rule exists, create activity
-            $activityId = Activity::createNewActivity($activityParams);
-            
+
+            $activity = Activity::create($activityParams);
+
             app(ActivityService::class)->processMonthlyActivityTime([$pageTimer->patient_id]);
-            
+
             $pageTimer->processed = 'Y';
-            
+
             $pageTimer->save();
-            
-            return $activityId;
+
+            return $activity->id;
         }
-        
+
         // update pagetimer
         $pageTimer->processed = 'Y';
-        
+
         $pageTimer->save();
-        
+
         return false;
     }
-    
+
     private function handleNurseLogs($activityId)
     {
         $activity = Activity::with('patient.patientInfo')
-                            ->find($activityId);
-        
+            ->find($activityId);
+
         if ( ! $activity) {
             return;
         }
-        
+
         $nurse = Nurse::whereUserId($activity->provider_id)
-                      ->first();
-        
+            ->first();
+
         if ( ! $nurse) {
             return;
         }
-        
+
         $alternativePayComputer = new AlternativeCareTimePayableCalculator($nurse);
         $alternativePayComputer->adjustNursePayForActivity($activity);
     }
