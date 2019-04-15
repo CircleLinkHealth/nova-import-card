05df657ed4ecd5f177a5447c4ad20af194437827
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-04-15 22:33:13 +0545

    Adds TimeTrackedPerDayView

---
+++ app/TimeTrackedPerDayView.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TimeTrackedPerDayView extends Model
+{
+    protected $table = 'time_tracked_per_day_view';
+}
