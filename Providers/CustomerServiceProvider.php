d23ac5d51146716e52aac186d9bda41ab31076ec
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-04-29 19:47:54 +0200

    Adds cerberus config

--- Providers/CustomerServiceProvider.php
+++ Providers/CustomerServiceProvider.php
@@ -16,7 +16,7 @@ class CustomerServiceProvider extends ServiceProvider
      * @var bool
      */
     protected $defer = false;
-
+    
     /**
      * Boot the application events.
      *
@@ -28,9 +28,9 @@ class CustomerServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->registerFactories();
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
-
+    
     /**
      * Register the service provider.
      *
@@ -40,10 +40,13 @@ class CustomerServiceProvider extends ServiceProvider
     {
         $this->app->register(RouteServiceProvider::class);
         $this->app->bind(DatabaseNotification::class, \CircleLinkHealth\Core\Entities\DatabaseNotification::class);
-        $this->app->bind(HasDatabaseNotifications::class, \CircleLinkHealth\Core\Traits\HasDatabaseNotifications::class);
+        $this->app->bind(
+            HasDatabaseNotifications::class,
+            \CircleLinkHealth\Core\Traits\HasDatabaseNotifications::class
+        );
         $this->app->bind(Notifiable::class, \CircleLinkHealth\Core\Traits\Notifiable::class);
     }
-
+    
     /**
      * Register config.
      *
@@ -51,14 +54,30 @@ class CustomerServiceProvider extends ServiceProvider
      */
     protected function registerConfig()
     {
-        $this->publishes([
-            __DIR__.'/../Config/config.php' => config_path('customer.php'),
-        ], 'config');
+        $this->publishes(
+            [
+                __DIR__.'/../Config/config.php' => config_path('customer.php'),
+            ],
+            'config'
+        );
         $this->mergeConfigFrom(
-            __DIR__.'/../Config/config.php', 'customer'
+            __DIR__.'/../Config/config.php',
+            'customer'
+        );
+        
+        $this->publishes(
+            [
+                __DIR__.'/../Config/cerberus.php' => config_path('cerberus.php'),
+            ],
+            'cerberus'
+        );
+        
+        $this->mergeConfigFrom(
+            __DIR__.'/../Config/cerberus.php',
+            'cerberus'
         );
     }
-
+    
     /**
      * Register views.
      *
@@ -67,18 +86,30 @@ class CustomerServiceProvider extends ServiceProvider
     public function registerViews()
     {
         $viewPath = resource_path('views/modules/customer');
-
+        
         $sourcePath = __DIR__.'/../Resources/views';
-
-        $this->publishes([
-            $sourcePath => $viewPath
-        ],'views');
-
-        $this->loadViewsFrom(array_merge(array_map(function ($path) {
-            return $path . '/modules/customer';
-        }, \Config::get('view.paths')), [$sourcePath]), 'customer');
+        
+        $this->publishes(
+            [
+                $sourcePath => $viewPath,
+            ],
+            'views'
+        );
+        
+        $this->loadViewsFrom(
+            array_merge(
+                array_map(
+                    function ($path) {
+                        return $path.'/modules/customer';
+                    },
+                    \Config::get('view.paths')
+                ),
+                [$sourcePath]
+            ),
+            'customer'
+        );
     }
-
+    
     /**
      * Register translations.
      *
@@ -87,26 +118,26 @@ class CustomerServiceProvider extends ServiceProvider
     public function registerTranslations()
     {
         $langPath = resource_path('lang/modules/customer');
-
+        
         if (is_dir($langPath)) {
             $this->loadTranslationsFrom($langPath, 'customer');
         } else {
-            $this->loadTranslationsFrom(__DIR__ .'/../Resources/lang', 'customer');
+            $this->loadTranslationsFrom(__DIR__.'/../Resources/lang', 'customer');
         }
     }
-
+    
     /**
      * Register an additional directory of factories.
-     * 
+     *
      * @return void
      */
     public function registerFactories()
     {
-        if (! app()->environment('production')) {
-            app(Factory::class)->load(__DIR__ . '/../Database/factories');
+        if ( ! app()->environment('production')) {
+            app(Factory::class)->load(__DIR__.'/../Database/factories');
         }
     }
-
+    
     /**
      * Get the services provided by the provider.
      *
