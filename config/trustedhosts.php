6ec78e3612d6dafbabcd7489d40c60fc8b37aa3a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-04-29 19:50:19 +0200

    small changes

--- config/trustedhosts.php
+++ config/trustedhosts.php
@@ -13,6 +13,7 @@ return [
         'circlelink-awv-1.medstack.net',
         '54.156.98.138', //awv ip
         '18.205.240.109', //worker ip
+        'cl20190401201618660100000002-1125925038.us-east-1.elb.amazonaws.com', //load balancer
     ]
     : [
         '.*ngrok.io',
@@ -22,5 +23,8 @@ return [
         'circlelink-production.medstack.net',
         'circlelink-worker.medstack.net',
         'circlelink-awv-1.medstack.net',
+        '54.156.98.138', //awv ip
+        '18.205.240.109', //worker ip
+        'cl20190401201618660100000002-1125925038.us-east-1.elb.amazonaws.com', //load balancer
     ],
 ];
