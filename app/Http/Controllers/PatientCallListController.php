cc410d584213f94e5e67531809092f22963ef174
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-06 20:06:41 +0200

    CPM-915

--- app/Http/Controllers/PatientCallListController.php
+++ app/Http/Controllers/PatientCallListController.php
@@ -52,7 +52,7 @@ class PatientCallListController extends Controller
             $calls->where('status', '=', $filterStatus);
         }
 
-        $calls->orderByRaw('FIELD(type, "Call Back") desc, scheduled_date asc, call_time_start asc');
+        $calls->orderByRaw('FIELD(type, "Call Back") desc, scheduled_date asc, call_time_start asc, call_time_end asc');
 
         $calls = $calls->get();
 
