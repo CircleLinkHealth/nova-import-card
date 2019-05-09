f378cbdd3418794d7800cee06de49bc4a16d8317
subzero10 <pagcosma@gmail.com>
2019-05-09 23:45:35 +0300

    CPM-874 PatientContactWindowUpdatedEvent. PhpCSFixer formatting.

---
+++ Events/PatientContactWindowUpdatedEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\Customer\Events;
+
+use CircleLinkHealth\Customer\Entities\PatientContactWindow;
+
+/**
+ * Class PatientContactWindowUpdatedEvent.
+ */
+class PatientContactWindowUpdatedEvent
+{
+    
+    /**
+     * @var PatientContactWindow[] $window
+     */
+    public $windows;
+    
+    public function __construct(iterable $windows)
+    {
+        $this->windows = $windows;
+    }
+}
