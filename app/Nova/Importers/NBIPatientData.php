3ee6d8bd931b2ed164e536011d6cdd3d53503fea
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-17 19:31:12 +0200

    CPM-968: Adds values to table and importer

--- app/Nova/Importers/NBIPatientData.php
+++ app/Nova/Importers/NBIPatientData.php
@@ -41,6 +41,32 @@ class NBIPatientData implements OnEachRow, WithChunkReading, WithValidation, Wit
     }
 
     /**
+     * Returns null if value means N/A or equivalent. Otherwise returns the value passed to it.
+     *
+     * @param string $value
+     *
+     * @return string|null
+     */
+    public function nullOrValue(string $value)
+    {
+        return ! (empty($value) || in_array($value, $this->nullValues()))
+            ?: $value;
+    }
+
+    /**
+     * If the value of a cell is any of these we shall consider it null.
+     *
+     * @return array
+     */
+    public function nullValues()
+    {
+        return [
+            'NA: In CPM',
+            'N/A',
+        ];
+    }
+
+    /**
      * @param Row $row
      */
     public function onRow(Row $row)
@@ -51,7 +77,15 @@ class NBIPatientData implements OnEachRow, WithChunkReading, WithValidation, Wit
             [
                 'mrn' => $row['mrn'],
             ],
-            $row
+            [
+                'dob'                 => $this->nullOrValue($row['dob']),
+                'first_name'          => $this->nullOrValue($row['first_name']),
+                'last_name'           => $this->nullOrValue($row['last_name']),
+                'mrn'                 => $this->nullOrValue($row['mrn']),
+                'primary_insurance'   => $this->nullOrValue($row['primary_insurance']),
+                'provider'            => $this->nullOrValue($row['provider']),
+                'secondary_insurance' => $this->nullOrValue($row['secondary_insurance']),
+            ]
         );
     }
 
