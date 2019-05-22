7dd022f70c6d6774d9cd5529af60753088711999
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-22 18:30:29 +0200

    CPM-980 Limit index to care coaches

--- app/Nova/CareCoachUser.php
+++ app/Nova/CareCoachUser.php
@@ -12,6 +12,26 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 class CareCoachUser extends NovaUser
 {
     /**
+     * Indicates if the resource should be displayed in the sidebar.
+     *
+     * @var bool
+     */
+    public static $displayInNavigation = true;
+
+    /**
+     * Build an "index" query for the given resource.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @param \Illuminate\Database\Eloquent\Builder   $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->careCoaches();
+    }
+
+    /**
      * Build a "relatable" query for the given resource.
      *
      * This query determines which instances of the model may be attached to other resources.
