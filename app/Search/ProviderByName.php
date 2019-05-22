17799ed3a9d246a34f7fda1c2f2060fc84cdfba7
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-22 13:17:11 +0200

    Adds base search class

--- app/Search/ProviderByName.php
+++ app/Search/ProviderByName.php
@@ -6,71 +6,14 @@
 
 namespace App\Search;
 
-use App\Contracts\ScoutSearch;
 use CircleLinkHealth\Customer\Entities\User;
 
-class ProviderByName implements ScoutSearch
+class ProviderByName extends BaseScoutSearch
 {
     /**
      * The name of this search.
      */
     const SEARCH_NAME = 'search_provider_by_name';
-
-    /**
-     * Two weeks in minutes.
-     */
-    const TWO_WEEKS = 21600;
-
-    /**
-     * How long to store in cache for.
-     *
-     * @return int
-     */
-    public function duration(): int
-    {
-        return self::TWO_WEEKS;
-    }
-
-    /**
-     * Search using given term.
-     *
-     * @param string $term
-     *
-     * @return mixed
-     */
-    public function find(string $term)
-    {
-        return \Cache::tags($this->tags())
-            ->remember(
-                self::key($term),
-                $this->duration(),
-                function () use ($term) {
-                    return $this->query($term);
-                }
-                     );
-    }
-
-    /**
-     * Essentially a static wrapper for find so that we can do `ProviderByName::first($term)`.
-     *
-     * @param string $term
-     *
-     * @return mixed
-     */
-    public static function first(string $term)
-    {
-        return (new static())->find($term);
-    }
-
-    /**
-     * @param string $term
-     *
-     * @return string
-     */
-    public function key(string $term)
-    {
-        return "{$this->name()}_$term";
-    }
 
     /**
      * The name of this search. Will be used in cache keys, tags.
