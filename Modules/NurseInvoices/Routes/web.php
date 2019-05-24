78df3f01fbf9952133b5f627ede5539273343b34
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-24 15:31:16 +0200

    Removes routes

--- Modules/NurseInvoices/Routes/web.php
+++ Modules/NurseInvoices/Routes/web.php
@@ -3,7 +3,3 @@
 /*
  * This file is part of CarePlan Manager by CircleLink Health.
  */
-
-Route::prefix('nurseinvoices')->group(function () {
-    Route::get('/', 'NurseInvoicesController@index');
-});
