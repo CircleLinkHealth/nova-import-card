26c5127766764b4f9582f8dd9f2647ede58e25cf
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-29 21:52:29 +0200

    Adds migration

---
+++ Modules/NurseInvoices/Database/Migrations/2019_05_29_154427_nurse_invoice_final_edits.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use App\AppConfig;
+use CircleLinkHealth\NurseInvoices\ValueObjects\NurseInvoiceDisputeDeadline;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class NurseInvoiceFinalEdits extends Migration
+{
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        //Add default dispute deadline
+        AppConfig::updateOrCreate([
+            'config_key' => NurseInvoiceDisputeDeadline::NURSE_INVOICE_DISPUTE_SUBMISSION_DEADLINE_KEY,
+        ], [
+            'config_value' => NurseInvoiceDisputeDeadline::DEFAULT_NURSE_INVOICE_DISPUTE_SUBMISSION_DEADLINE_DAY_AND_TIME,
+        ]);
+
+        Schema::table('nurse_invoices', function (Blueprint $table) {
+            $table->dropColumn('sent_to_accountant_at');
+        });
+
+        Schema::table('nurse_invoices', function (Blueprint $table) {
+            $table->timestamp('sent_to_accountant_at')->nullable()->after('month_year');
+        });
+    }
+}
