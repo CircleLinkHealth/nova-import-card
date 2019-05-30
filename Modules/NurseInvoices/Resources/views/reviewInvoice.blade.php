e5097f1356667fe57d4416441b90d80bb4b84660
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-30 18:55:25 +0200

    Adds nurse approved message

--- Modules/NurseInvoices/Resources/views/reviewInvoice.blade.php
+++ Modules/NurseInvoices/Resources/views/reviewInvoice.blade.php
@@ -45,6 +45,12 @@
                 @else
                     @if (auth()->user()->shouldShowInvoiceReviewButton())
                         <dispute-nurse-invoice invoice-id="{{$invoiceId}}"></dispute-nurse-invoice>
+                    @elseif ($invoice->is_nurse_approved)
+                        <div class="row">
+                            <div class="col-md-12 alert alert-success">
+                                <h4>You approved this invoice on {{presentDate($invoice->nurse_approved_at, true, true, true)}}</h4>
+                            </div>
+                        </div>
                     @endif
                 @endisset
             </div>
