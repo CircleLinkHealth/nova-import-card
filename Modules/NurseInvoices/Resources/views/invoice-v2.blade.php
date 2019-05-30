ef9d698149f83913983cac794eeb019c30d3eef5
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-30 20:44:23 +0200

    CPM-1023 Change invoice format

--- Modules/NurseInvoices/Resources/views/invoice-v2.blade.php
+++ Modules/NurseInvoices/Resources/views/invoice-v2.blade.php
@@ -1,22 +1,4 @@
-@isset($isPdf)
-    <link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
-@endisset
-
-<style>
-    .pdf-line {
-        display: block;
-        padding-bottom: 5px;
-        padding-top: 5px;
-    }
-
-    .cross-out {
-        text-decoration: line-through;
-    }
-
-    .display-inline-block {
-        display: inline-block;
-    }
-</style>
+<link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
 
 <div class="page-header">
     <h1>CircleLink Health
@@ -27,72 +9,132 @@
 </div>
 
 
-<dl class="dl-horizontal">
-    <h4>
-        <div class="pdf-line">
-            <b>Base Salary @if($changedToFixedRateBecauseItYieldedMore)
-                    <small>(highest amount used)</small>@endif:</b>
-            @if(is_array($formattedBaseSalary))
-                <span class="display-inline-block">{{$formattedBaseSalary['high']}}</span>
-                <span class="display-inline-block cross-out"><small>{{$formattedBaseSalary['low']}}</small></span>
-            @else
-                <span class="display-inline-block">formattedBaseSalary}}</span>
-            @endif
-        </div>
+<div class="row">
+    <div class="col-md-10">
+        <table class="table borderless invoice-totals-table">
+            <tr class="borderless">
+                <th></th>
+                <th class="text-bold-invoice">Amount ($)</th>
+                <th>Note</th>
+            </tr>
+            <tr class="borderless">
+                <td class="text-bold-invoice">Base Pay:</td>
+                <td>${{$baseSalary}}</td>
+                <td>
+                    @if($changedToFixedRateBecauseItYieldedMore)
+                        <span>Highest Total Used:</span>
+                    @endif
+                    @if(is_array($formattedBaseSalary))
+                        <span class="display-inline-block">{{$formattedBaseSalary['high']}}</span>
+                        <span class="display-inline-block"><small
+                                    class="cross-out">{{$formattedBaseSalary['low']}}</small></span>
+                    @else
+                        <span class="display-inline-block">{{$formattedBaseSalary}}</span>
+                    @endif
+                </td>
+            </tr>
+            <tr class="borderless">
+                <td class="text-bold-invoice">Extra Time:</td>
+                <td>${{$addedTimeAmount}}</td>
+                <td>
+                    @if($hasAddedTime)
+                        <span>{{$addedTime}} hours @ {{$nurseHourlyRate}}/hr</span>
+                    @endif
+                </td>
+            </tr>
+            <tr class="borderless">
+                <td class="text-bold-invoice">Bonuses:</td>
+                <td>${{$bonus}}</td>
+                <td></td>
+            </tr>
+            <tr class="borderless">
+                <td class="text-bold-invoice">Total Due:</td>
+                <td style="border-top: 2px solid black !important;">
+                    {{$formattedInvoiceTotalAmount}}
+                </td>
+                <td>
 
-        @if($hasAddedTime)
-            <div class="pdf-line"><b>Extras:</b> <span>${{$addedTimeAmount}} ({{$addedTime}}
-                    Hours at {{$nurseHourlyRate}}/hr)@if($bonus), Cash Bonuses: ${{$bonus}} @endif</span></div>
-        @endif
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
 
-        <div class="pdf-line"><b><u>Invoice Total</u>:</b> <span><b>{{$formattedInvoiceTotalAmount}}</b></span></div>
-    </h4>
-</dl>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered">
+            <tr>
+                <th style="width: 25%">Date</th>
+                @if(!$variablePay)
+                    <th style="width: 25%">Minutes</th>
+                @endif
 
-<table class="table table-bordered">
-    <tr>
-        <th style="width: 25%">Date</th>
-        @if(!$variablePay)
-            <th style="width: 25%">Minutes</th>
-        @endif
+                <th style="width: 25%">Total Hours</th>
+                @if($variablePay)
+                    <th style="width: 25%">CCM Hours (${{$nurseHighRate}}/Hour)</th>
+                    <th style="width: 25%">CCM Hours (${{$nurseLowRate}}/Hour)</th>
+                @endif
+            </tr>
 
-        <th style="width: 25%">Total Hours</th>
-        @if($variablePay)
-            <th style="width: 25%">CCM Hours (${{$nurseHighRate}}/Hour)</th>
-            <th style="width: 25%">CCM Hours (${{$nurseLowRate}}/Hour)</th>
-        @endif
-    </tr>
+            <tr>
+                <td><b>Total Hours</b></td>
 
-    <tr>
-        <td><b>Total Hours</b></td>
+                @if(!$variablePay)
+                    <td>{{$systemTimeInMinutes}}</td>
+                @endif
 
-        @if(!$variablePay)
-            <td>{{$systemTimeInMinutes}}</td>
-        @endif
-
-        <td>{{$systemTimeInHours}}</td>
+                <td>{{$systemTimeInHours}}</td>
 
 
-        @if($variablePay)
-            <td>{{$totalTimeTowardsCcm}}</td>
-            <td>{{$totalTimeAfterCcm}}</td>
-        @endif
-    </tr>
+                @if($variablePay)
+                    <td>{{$totalTimeTowardsCcm}}</td>
+                    <td>{{$totalTimeAfterCcm}}</td>
+                @endif
+            </tr>
 
-    @foreach($timePerDay as $date => $row)
-        <tr>
-            <td><b>{{$date}}</b></td>
+            @foreach($timePerDay as $date => $row)
+                <tr>
+                    <td><b>{{$date}}</b></td>
 
-            @if(!$variablePay)
-                <td>{{$row['minutes']}}</td>
-            @endif
+                    @if(!$variablePay)
+                        <td>{{$row['minutes']}}</td>
+                    @endif
 
-            <td>{{$row['hours']}}</td>
+                    <td>{{$row['hours']}}</td>
 
-            @if($variablePay)
-                <td>{{$row['towards']}}</td>
-                <td>{{$row['after']}}</td>
-            @endif
-        </tr>
-    @endforeach
-</table>
+                    @if($variablePay)
+                        <td>{{$row['towards']}}</td>
+                        <td>{{$row['after']}}</td>
+                    @endif
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+
+@push('styles')
+    <style>
+        body {
+            background: #fff;
+            color: #333;
+            line-height: 1.3em;
+            font-weight: 300;
+        }
+
+        .borderless > td, .borderless > th, .borderless > tr, .borderless > td > tr, .borderless > td > tr > th, .table.borderless > tbody > tr > th {
+            border: none !important;
+        }
+
+        .cross-out {
+            text-decoration: line-through;
+        }
+
+        .display-inline-block {
+            display: inline-block;
+        }
+
+        .text-bold-invoice {
+            font-weight: bold;
+        }
+    </style>
+@endpush
