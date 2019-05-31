7fbea88c0a908d044fcca458c753cb5b8be13b7a
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-31 20:34:37 +0200

    Send dispute reminder

--- Modules/NurseInvoices/Console/Commands/SendDisputeReminder.php
+++ Modules/NurseInvoices/Console/Commands/SendDisputeReminder.php
@@ -7,33 +7,34 @@
 namespace CircleLinkHealth\NurseInvoices\Console\Commands;
 
 use App\Notifications\InvoiceReminder;
-use Carbon\Carbon;
 use CircleLinkHealth\NurseInvoices\Entities\NurseInvoice;
 use CircleLinkHealth\NurseInvoices\Helpers\NurseInvoiceDisputeDeadline;
+use CircleLinkHealth\NurseInvoices\Traits\DryRunnable;
+use CircleLinkHealth\NurseInvoices\Traits\TakesMonthAndUsersAsInputArguments;
 use Illuminate\Console\Command;
 
 class SendDisputeReminder extends Command
 {
+    use DryRunnable;
+    use TakesMonthAndUsersAsInputArguments;
+
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Reminder to review invoices';
+    protected $description = 'Send the LAN (Last Approval Notification) to the nurses for invoices of the given month.';
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'nurseinvoices:dispute-reminder {month? : Month to generate the invoice for in YYYY-MM format. Defaults to previous month.} {userIds?* : Space separated. Leave empty to send to all}';
+    protected $signature = 'nurseinvoices:lan';
 
     /**
-     * Create a new command instance.
+     * @var \Carbon|null
      */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    private $deadlineInstance;
 
     /**
      * Execute the console command.
@@ -42,46 +43,61 @@ class SendDisputeReminder extends Command
      */
     public function handle()
     {
-        $month = $this->argument('month') ?? null;
+        $count = 0;
 
-        if ($month) {
-            $month = Carbon::createFromFormat('Y-m', $month)->startOfMonth();
-        } else {
-            $month = Carbon::now()->subMonth()->startOfMonth();
-        }
-
-        $userIds = (array) $this->argument('userIds') ?? [];
-
-        $deadline = NurseInvoiceDisputeDeadline::forInvoiceOfMonth($month);
-
-        NurseInvoice::with('nurse.user')
-            ->when(
-                ! empty($userIds),
-                function ($q) use ($userIds) {
-                    $q->whereHas(
-                        'nurse.user',
-                        function ($q) use ($userIds) {
-                            $q->whereIn('id', $userIds);
-                        }
-                            );
-                }
-                    )
-            ->where('month_year', $month)
-            ->undisputed()
-            ->notApproved()
+        $this->invoices()
             ->chunk(
                 20,
-                function ($invoices) use ($deadline, $month) {
+                function ($invoices) use ($count) {
                     foreach ($invoices as $invoice) {
-                        $tz = $invoice->nurse->user->timezone ?? 'America/New_York';
-
-                        $this->warn("Sending notification to {$invoice->nurse->user->getFullName()}");
-                        $invoice->nurse->user->notify(new InvoiceReminder($deadline->copy()->setTimezone($tz), $month));
-                        $this->info("Sent notification to {$invoice->nurse->user->getFullName()}");
+                        $this->sendInvoice($invoice);
+                        ++$count;
                     }
                 }
-                    );
+             );
 
-        $this->info('Command finished!');
+        $this->info("Command finished! Sent $count Notifications.");
+    }
+
+    public function invoices()
+    {
+        return NurseInvoice::with('nurse.user')
+            ->has('nurse.user')
+            ->when(
+                ! empty($userIds),
+                function ($q) {
+                    $q->whereHas('nurse.user', function ($q) {
+                        $q->whereIn('id', $this->usersIds());
+                    });
+                }
+                           )
+            ->where('month_year', $this->month())
+            ->undisputed()
+            ->notApproved();
+    }
+
+    /**
+     * Returns a new instance of the deadline.
+     *
+     * @return \Carbon\Carbon
+     */
+    private function deadline()
+    {
+        if ( ! $this->deadlineInstance) {
+            $this->deadlineInstance = NurseInvoiceDisputeDeadline::forInvoice($this->month());
+        }
+
+        return $this->deadlineInstance->copy();
+    }
+
+    private function sendInvoice($invoice)
+    {
+        $this->warn("Sending notification to {$invoice->nurse->user->getFullName()}");
+
+        $invoice->nurse->user->notify(
+            new InvoiceReminder($this->deadline()->setTimezone($invoice->nurse->user->timezone ?? 'America/New_York'), $this->month())
+        );
+
+        $this->info("Sent notification to {$invoice->nurse->user->getFullName()}");
     }
 }
