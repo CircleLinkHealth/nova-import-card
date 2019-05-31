0f12f3e12aac5aa7b278ebab1e5144e672fc249c
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-05-31 17:51:18 +0200

    Adds TakesMonthAndUsersAsInputArguments trait

---
+++ Modules/NurseInvoices/Traits/TakesMonthAndUsersAsInputArguments.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\NurseInvoices\Traits;
+
+use Carbon\Carbon;
+use Symfony\Component\Console\Input\InputArgument;
+
+trait TakesMonthAndUsersAsInputArguments
+{
+    public $monthInstance;
+
+    /**
+     * @return Carbon
+     */
+    public function defaultMonth()
+    {
+        return Carbon::now()->subMonth()->startOfMonth();
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function month()
+    {
+        if ( ! $this->monthInstance) {
+            $input = $this->argument('month') ?? null;
+
+            $this->monthInstance = $input ? Carbon::createFromFormat('Y-m', $input)->startOfMonth() : $this->defaultMonth();
+        }
+
+        return $this->monthInstance;
+    }
+
+    /**
+     * @return array
+     */
+    public function usersIds()
+    {
+        return (array) $this->argument('userIds') ?? [];
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['month', InputArgument::OPTIONAL, 'Month to generate the invoice for in YYYY-MM format. Defaults to previous month.'],
+            ['userIds', InputArgument::IS_ARRAY|InputArgument::OPTIONAL, 'Users to run the command for. Leave empty to send to all.'],
+        ];
+    }
+}
