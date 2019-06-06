3e273d0ed2afe4603d25dba4d601dc89c87d360d
Pangratios Cosma <pagcosma@gmail.com>
2019-06-07 00:37:25 +0300

    CPM-999 upcomingHolidays returns companyHolidays as well, which have id == null. Cannot be deleted by nurse.

--- resources/views/partials/care-center/holiday-schedule/index.blade.php
+++ resources/views/partials/care-center/holiday-schedule/index.blade.php
@@ -6,6 +6,7 @@
     <div class="row">
         <ul class="list-group col-md-5">
             @foreach($holidays as $holiday)
+
                 <li class="list-group-item" style="padding: 20px;">
                     <div class="row">
                         <div class="col-md-11">
@@ -15,16 +16,20 @@
                             </b>
                         </div>
 
-                        <div class="col-md-1">
-                            <a href="{{ route('care.center.work.schedule.holiday.destroy', $holiday->id) }}"
-                               onclick="return confirm('Are you sure you want to delete this slot?')"
-                               id="delete-window-{{$holiday->id}}">
-                                <i class="glyphicon glyphicon-trash"></i>
-                            </a>
-                        </div>
+                        <!-- if it does not have an id, it is a company holiday -->
+                        @if ($holiday->id)
+                            <div class="col-md-1">
+                                <a href="{{ route('care.center.work.schedule.holiday.destroy', $holiday->id) }}"
+                                   onclick="return confirm('Are you sure you want to delete this slot?')"
+                                   id="delete-window-{{$holiday->id}}">
+                                    <i class="glyphicon glyphicon-trash"></i>
+                                </a>
+                            </div>
+                        @endif
 
                     </div>
                 </li>
+
             @endforeach
         </ul>
     </div>
