cbb29918119c56fce5073ad68908412faa9bc061
kakoushias <kakoushias@gmail.com>
2019-06-09 22:49:31 +0300

    CPM-822 checks if Nurse is on holiday

--- app/Services/NursesAndStatesDailyReportService.php
+++ app/Services/NursesAndStatesDailyReportService.php
@@ -152,7 +152,7 @@ class NursesAndStatesDailyReportService
             'nurse_full_name' => $nurse->getFullName(),
             'systemTime'      => $systemTime,
             'actualHours'     => round((float) ($systemTime / 3600), 2),
-            'committedHours'  => round(
+            'committedHours'  => $nurse->nurseInfo->isOnHoliday($date) ? 0 : round(
                 (float) $nurseWindows->where(
                     'day_of_week',
                     carbonToClhDayOfWeek($date->dayOfWeek)
@@ -213,7 +213,7 @@ class NursesAndStatesDailyReportService
                 round(
                     (float) (100 * (
                         (floatval($this->successfulCallsMultiplier) * $data['successful']) + (floatval(
-                                $this->unsuccessfulCallsMultiplier
+                            $this->unsuccessfulCallsMultiplier
                                                                                                   ) * $data['unsuccessful'])
                         ) / $data['actualHours'])
                 )
@@ -391,8 +391,8 @@ class NursesAndStatesDailyReportService
                 NursesAndStatesDailyReportService::LAST_COMMITTED_DAYS_TO_GO_BACK
             )
                 ->sortBy(function ($date) {
-                                      return $date;
-                                  });
+                    return $date;
+                });
         } catch (\Exception $e) {
             //todo: Log exception
         }
@@ -439,19 +439,19 @@ class NursesAndStatesDailyReportService
     {
         return \DB::table('calls')
             ->select(
-                      \DB::raw('DISTINCT inbound_cpm_id as patient_id'),
-                      \DB::raw(
+                \DB::raw('DISTINCT inbound_cpm_id as patient_id'),
+                \DB::raw(
                           'GREATEST(patient_monthly_summaries.ccm_time, patient_monthly_summaries.bhi_time)/60 as patient_time'
                       ),
-                      \DB::raw(
+                \DB::raw(
                           "({$this->timeGoal} - (GREATEST(patient_monthly_summaries.ccm_time, patient_monthly_summaries.bhi_time)/60)) as patient_time_left"
                       ),
-                      'no_of_successful_calls as successful_calls'
+                'no_of_successful_calls as successful_calls'
                   )
             ->leftJoin('users', 'users.id', '=', 'calls.inbound_cpm_id')
             ->leftJoin('patient_monthly_summaries', 'users.id', '=', 'patient_monthly_summaries.patient_id')
             ->whereRaw(
-                      "(
+                "(
 (
 DATE(calls.scheduled_date) >= DATE('{$date->copy()->startOfMonth()->toDateString()}')
 AND
