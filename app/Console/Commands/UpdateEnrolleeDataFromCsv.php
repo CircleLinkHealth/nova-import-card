d6d59f7d3f3e50a88bf7278f34bc3d91872b878e
kakoushias <kakoushias@gmail.com>
2019-06-12 17:07:16 +0300

    adds/removes comments

--- app/Console/Commands/UpdateEnrolleeDataFromCsv.php
+++ app/Console/Commands/UpdateEnrolleeDataFromCsv.php
@@ -47,8 +47,6 @@ class UpdateEnrolleeDataFromCsv extends Command
      */
     public function handle()
     {
-//        $contents = collect(Storage::drive('google')
-//                                   ->listContents('/', true));
         $file = collect(Storage::drive('google')
             ->listContents('/', true))
             ->where('filename', '=', 'English Enrollment Records All Time - Sheet2')
@@ -97,37 +95,38 @@ class UpdateEnrolleeDataFromCsv extends Command
         Enrollee::whereIn('id', $csv->pluck('Eligible_Patient_ID')->toArray())
             ->orWhereIn('eligibility_job_id', $csv->pluck('Eligibility_Job_ID')->toArray())
             ->chunk(200, function ($enrollees) use (&$csv) {
-                    $enrollees->each(function (Enrollee $e) use ($csv) {
-                        $row = $csv->filter(function ($row) use ($e) {
-                            //We need either the enrollee id, or the eligibility job id
-                            if (array_key_exists('Eligible_Patient_ID', $row)) {
-                                return $row['Eligible_Patient_ID'] == $e->id;
-                            }
-                            if (array_key_exists('Eligibility_Job_ID', $row)) {
-                                return $row['Eligibility_Job_ID'] == $e->eligibility_job_id;
-                            }
+                $enrollees->each(function (Enrollee $e) use ($csv) {
+                    $row = $csv->filter(function ($row) use ($e) {
+                        //We need either the enrollee id, or the eligibility job id
+                        if (array_key_exists('Eligible_Patient_ID', $row)) {
+                            return $row['Eligible_Patient_ID'] == $e->id;
+                        }
+                        if (array_key_exists('Eligibility_Job_ID', $row)) {
+                            return $row['Eligibility_Job_ID'] == $e->eligibility_job_id;
+                        }
 
-                            return false;
-                        })
+                        return false;
+                    })
                             //use last to get the last row for that patient from the csv (latest updates for that patient)
-                            ->last();
+                        ->last();
 
-                        if ($row) {
-                            $e = $this->setEnrolleeStatus($e, $row);
-                            if (array_key_exists('Call_Date', $row) && ! empty($row['Call_Date'])) {
-                                $date = preg_split("/[.|\/]/", $row['Call_Date']);
-                                if (3 == count($date)) {
-                                    try {
-                                        $e->last_attempt_at = Carbon::parse("{$date[0]}/{$date[1]}/{$date[2]}");
-                                    } catch (\Exception $exception) {
-                                        //do nothing, date provided in csv is invalid
-                                    }
+                    if ($row) {
+                        $e = $this->setEnrolleeStatus($e, $row);
+                        if (array_key_exists('Call_Date', $row) && ! empty($row['Call_Date'])) {
+                            //this is a hack helping us parse dates from the csv that combine dots and dashes. It makes alot of assumptions that in other cases may be wrong. Fix if needed.
+                            $date = preg_split("/[.|\/]/", $row['Call_Date']);
+                            if (3 == count($date)) {
+                                try {
+                                    $e->last_attempt_at = Carbon::parse("{$date[0]}/{$date[1]}/{$date[2]}");
+                                } catch (\Exception $exception) {
+                                    //do nothing, date provided in csv is invalid
                                 }
                             }
-                            $e->save();
                         }
-                    });
+                        $e->save();
+                    }
                 });
+            });
 
         $localDisk->delete($fileName);
     }
