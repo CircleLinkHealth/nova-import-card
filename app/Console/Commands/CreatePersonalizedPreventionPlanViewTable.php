0ec256f5bb209d1ff0330f5031ff375e70ebad13
Pangratios Cosma <pagcosma@gmail.com>
2019-06-13 12:10:31 +0300

    CPM-1101 progress

--- app/Console/Commands/CreatePersonalizedPreventionPlanViewTable.php
+++ app/Console/Commands/CreatePersonalizedPreventionPlanViewTable.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class PersonalizedPreventionPlan extends Command
+class CreatePersonalizedPreventionPlanViewTable extends Command
 {
     /**
      * The name and signature of the console command.
--- app/Jobs/GeneratePersonalizedPreventionPlanJob.php
+++ app/Jobs/GeneratePersonalizedPreventionPlanJob.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class PersonalizedPreventionPlan implements ShouldQueue
+class GeneratePersonalizedPreventionPlanJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
