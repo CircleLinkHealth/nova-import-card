21e972ac763dc0a09ff068d7d4c0ab328bca8235
kakoushias <kakoushias@gmail.com>
2019-06-18 20:11:33 +0300

    Applies fix, fixes moth

--- app/Console/Commands/EmailRNDailyReport.php
+++ app/Console/Commands/EmailRNDailyReport.php
@@ -125,7 +125,8 @@ class EmailRNDailyReport extends Command
                             'caseLoadNeededToComplete'     => $reportDataForNurse['caseLoadNeededToComplete'],
                             'hoursCommittedRestOfMonth'    => $reportDataForNurse['hoursCommittedRestOfMonth'],
                             'surplusShortfallHours'        => $reportDataForNurse['surplusShortfallHours'],
-                            'projectedHoursLeftInMoth'     => $reportDataForNurse['projectedHoursLeftInMoth'],
+                            'projectedHoursLeftInMonth'    => $reportDataForNurse['projectedHoursLeftInMonth'],
+                            'avgHoursWorkedLast10Sessions' => $reportDataForNurse['avgHoursWorkedLast10Sessions'],
                             'totalEarningsThisMonth'       => $totalEarningsThisMonth,
                             'totalTimeInSystemOnGivenDate' => $totalTimeInSystemOnGivenDate,
                             'totalTimeInSystemThisMonth'   => $totalTimeInSystemThisMonth,
@@ -176,7 +177,7 @@ class EmailRNDailyReport extends Command
             'hoursCommittedRestOfMonth',
             'surplusShortfallHours',
             'nextUpcomingWindow',
-            'projectedHoursLeftInMoth',
+            'projectedHoursLeftInMonth',
             'avgHoursWorkedLast10Sessions',
         ], $report);
     }
