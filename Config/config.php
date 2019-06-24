1b8e2edba3035555bed0e1a73a54fce4d4278ccc
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-06-24 14:47:51 +0300

    moves some helpers to core

--- Config/config.php
+++ Config/config.php
@@ -6,4 +6,8 @@
 
 return [
     'name' => 'Core',
+    
+    
+    'is_production_env'   => env('IS_PRODUCTION_SERVER', false),
+    'is_queue_worker_env' => env('IS_QUEUE_WORKER_SERVER', false),
 ];
