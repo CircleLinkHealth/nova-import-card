83b97dd4140a3b9ca9c58307efede3e8944de2bb
Antonis Antoniou <kountouris7@gmail.com>
2019-06-25 17:03:37 +0300

    CPM-1085- space fixes

--- resources/views/partials/providerUI/bhi-notification-banner.blade.php
+++ resources/views/partials/providerUI/bhi-notification-banner.blade.php
@@ -1,53 +1,78 @@
-<div id="legacy-bhi-banner" class="alert alert-warning alert-dismissible notification-banner" role="alert"
-     style="background-color: #fefac0;">
+<div class="modal-mask">
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+        <div id="legacy-bhi-banner" class="alert alert-light" role="alert">
 
-                <span class="banner-title">THIS PATIENT IS ELIGIBLE FOR BEHAVIORAL HEALTH SERVICES, PLEASE ASK: </span>
-                <p class="banner-body">
-                    "DR. {{($user->billingProviderUser())->getFullName()}} would like us to spend a bit more time on these
-                    calls, including covering behavioral and other health topics, and possibly looping in specialists
-                    only if needed. If you don’t have supplemental insurance, there’s an $8 additional copay".
+            <button type="button" class="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                Close
+            </button>
 
-                    <br>
-                    <br>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="alert-heading">
+                        <div class="title">
+                            <span class="glyphicon glyphicon-exclamation-sign"></span>
+                            THIS PATIENT IS ELIGIBLE FOR BEHAVIORAL HEALTH SERVICES, PLEASE ASK:
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="message">
+                        DR. {{($user->billingProviderUser())->getFullName()}} would like us to spend a bit more time on
+                        these
+                        calls, including covering behavioral and other health topics, and possibly looping in
+                        specialists
+                        only if needed.
+                        <div class="space"></div>
+                        If you don’t have supplemental insurance, there’s an $8 additional copay.
+                        <div class="space"></div>
+                        <strong>Did patient consent?</strong>
+                        <div class="space"></div>
+                    </div>
+                </div>
 
-                    Did patient consent?
+{{--@todo:buttons dance all overwhen hover - must fix--}}
+                <div class="options">
+                    <div class="col-md-12">
+                    {{ Form::open(['url' => route('legacy-bhi.store', [$user->program_id, $user->id]), 'style' => 'display: inline-block;', 'class' => 'legacy-bhi-decision-form']) }}
+                    <input type="hidden" name="decision" value="1">
+                    <input type="submit"
+                           value="Consented"
+                           class="alert-success alert"
+                           style="margin-left: 25px;"
+                           data-toggle="tooltip"
+                           data-placement="top"
+                           title="Confirm that the patient has consented to receiving BHI services"
+                           onclick="return confirm('Please confirm that the patient has consented to receiving BHI services.')">
+                    {{ Form::close() }}
 
-                    <br>
-                    <br>
-                </p>
-            </div>
-        </div>
+                    {{ Form::open(['url' => route('legacy-bhi.store', [$user->program_id, $user->id]), 'style' => 'display: inline-block;', 'class' => 'legacy-bhi-decision-form']) }}
+                    <input type="hidden" name="decision" value="2">
+                    <input type="submit"
+                           value="Not Now"
+                           class="alert-warning alert legacy-bhi-consent-not-now-button"
+                           style="margin-left: 25px;"
+                           data-toggle="tooltip"
+                           data-placement="top"
+                           title="Hide this notice until this patient's next scheduled call day"
+                           onclick="return confirm('Clicking OK will hide the BHI notice until the next day this patient has a scheduled call.')">
+                    {{ Form::close() }}
 
-        <div class="row">
-            <div class="col-md-12">
-                {{ Form::open(['url' => route('legacy-bhi.store', [$user->program_id, $user->id]), 'style' => 'display: inline-block;', 'class' => 'legacy-bhi-decision-form']) }}
-                <input type="hidden" name="decision" value="1">
-                <input type="submit" value="Consented" class="alert-success alert"
-                       style="margin-left: 25px;"
-                       onclick="return confirm('Please confirm that the patient has consented to receiving BHI services.')">
-                {{ Form::close() }}
-
-                {{ Form::open(['url' => route('legacy-bhi.store', [$user->program_id, $user->id]), 'style' => 'display: inline-block;', 'class' => 'legacy-bhi-decision-form']) }}
-                <input type="hidden" name="decision" value="2">
-                <input type="submit" value="Not Now" class="alert-warning alert legacy-bhi-consent-not-now-button"
-                       style="margin-left: 25px;"
-                       onclick="return confirm('Clicking OK will hide the BHI notice until the next day this patient has a scheduled call.')">
-                {{ Form::close() }}
-
-                {{ Form::open(['url' => route('legacy-bhi.store', [$user->program_id, $user->id]), 'style' => 'display: inline-block;', 'class' => 'legacy-bhi-decision-form']) }}
-                <input type="hidden" name="decision" value="0">
-                <input type="submit" value="Not Consented" class="alert-danger alert"
-                       style="margin-left: 25px;"
-                       onclick="return confirm('Please confirm that the patient has NOT consented to receiving BHI services.')">
-                {{ Form::close() }}
+                    {{ Form::open(['url' => route('legacy-bhi.store', [$user->program_id, $user->id]), 'style' => 'display: inline-block;', 'class' => 'legacy-bhi-decision-form']) }}
+                    <input type="hidden" name="decision" value="0">
+                    <input type="submit" value="Not Consented" class="alert-danger alert"
+                           style="margin-left: 25px;"
+                           data-toggle="tooltip"
+                           data-placement="top"
+                           title="Confirm that the patient has NOT consented to receiving BHI services"
+                           onclick="return confirm('Please confirm that the patient has NOT consented to receiving BHI services.')">
+                    {{ Form::close() }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
 
 @push('scripts')
     <script>
@@ -55,5 +80,69 @@
             event.preventDefault();
             $('#legacy-bhi-banner').hide();
         });
+
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+            $('.close').click(function (e) {
+                $(".modal-mask, .load-hidden-bhi").hide();
+            });
+        });
     </script>
+@endpush
+
+@push('styles')
+    <style>
+        #legacy-bhi-banner {
+            position: fixed;
+            z-index: 1;
+            padding-top: 100px;
+            left: 15%;
+            top: 14%;
+            width: 70%;
+            height: 60%;
+            overflow: auto;
+            border: #000000;
+            background-color: whitesmoke;
+            border-radius: 2px;
+            box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+            transition: all .3s ease;
+            font-family: Helvetica, Arial, sans-serif;
+            color: #000;
+        }
+
+        .modal-mask {
+            position: fixed;
+            z-index: -1;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, .5);
+            display: table;
+            transition: opacity .3s ease;
+        }
+
+        .space {
+            margin-top: 2%;
+        }
+
+        .message {
+            font-size: medium;
+            line-height: 1.6;
+            text-align: center;
+        }
+
+        .title {
+            font-size: medium;
+            letter-spacing: 1px;
+            text-align: center;
+            font-weight: bold;
+        }
+
+        .options {
+            position: relative;
+            bottom: 5%;
+            left: 29%;
+        }
+    </style>
 @endpush
