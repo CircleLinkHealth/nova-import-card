a2f52ec8c8dfbb58ab418b33af2990346c58348f
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-06-28 01:14:28 +0300

    small chagnes

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 if ( ! function_exists('isProductionEnv')) {
     /**
      * Returns whether or not this is a Production server, ie. used by real users.
