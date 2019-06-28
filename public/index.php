fa490a4e88fca30d6f337a83d098bb1b664e3a47
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-06-28 04:13:49 +0300

    composer

--- public/index.php
+++ public/index.php
@@ -1,10 +1,7 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
  */
 
 define('LARAVEL_START', microtime(true));
