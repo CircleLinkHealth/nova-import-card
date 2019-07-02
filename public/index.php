f460da2f77cea863141448e117f8d04b3aa08f6f
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-07-02 16:51:44 +0300

    Add session vars later

--- public/index.php
+++ public/index.php
@@ -20,30 +20,34 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-$n = (new Dotenv\Dotenv(__DIR__.'/../', '.redis-creds'))->load();
+$app = require_once __DIR__.'/../bootstrap/app.php';
+//$app->register(Illuminate\Session\SessionServiceProvider::class);
+//define('LOAD_APP', microtime(true) - LARAVEL_START);
 
-$redis = new Predis\Client([
-    'host'     => $n[0],
-    'password' => 'NULL' === $n[1] ? null : $n[1],
-    'port'     => $n[2],
-]);
-
-if ($keys = $redis->keys("*{$_SERVER['REQUEST_URI']}*")) {
-    if (is_array($keys) && 1 == count($keys)) {
-        $r = $redis->get($keys[0]);
-
-        if ($r) {
-            try {
-                $response = (new CircleLinkHealth\ResponseCache\ResponseSerializer())->unserialize($r);
-//                @todo: load session module and replace csrf token
-//                (new \CircleLinkHealth\ResponseCache\Replacers\CsrfTokenReplacer())->replaceInCachedResponse($response);
-                
-                return $response->send();
-            } catch (CircleLinkHealth\ResponseCache\Exceptions\CouldNotUnserialize $e) {
-            }
-        }
-    }
-}
+//$n = (new Dotenv\Dotenv(__DIR__.'/../', '.redis-creds'))->load();
+//
+//$redis = new Predis\Client([
+//    'host'     => $n[0],
+//    'password' => 'NULL' === $n[1] ? null : $n[1],
+//    'port'     => $n[2],
+//]);
+//
+//if ($keys = $redis->keys("*{$_SERVER['REQUEST_URI']}*")) {
+//    if (is_array($keys) && 1 == count($keys)) {
+//        $r = $redis->get($keys[0]);
+//
+//        if ($r) {
+//            try {
+//                $response = (new CircleLinkHealth\ResponseCache\ResponseSerializer())->unserialize($r);
+////                @todo: load session module and replace csrf token
+////                (new \CircleLinkHealth\ResponseCache\Replacers\CsrfTokenReplacer())->replaceInCachedResponse($response);
+//
+//                return $response->send();
+//            } catch (CircleLinkHealth\ResponseCache\Exceptions\CouldNotUnserialize $e) {
+//            }
+//        }
+//    }
+//}
 
 /*
  |--------------------------------------------------------------------------
@@ -79,11 +83,7 @@ if ($keys = $redis->keys("*{$_SERVER['REQUEST_URI']}*")) {
 | the responses back to the browser and delight our users.
 |
 */
-
-define('LOAD_APP', microtime(true) - LARAVEL_START);
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-define('LARAVEL_REQUIRE_APP', microtime(true) - LOAD_APP - LARAVEL_START);
+//define('LARAVEL_REQUIRE_APP', microtime(true) - LOAD_APP - LARAVEL_START);
 
 $app->alias('request', 'App\SafeRequest');
 
@@ -100,19 +100,19 @@ $app->alias('request', 'App\SafeRequest');
 */
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-define('LARAVEL_BEFORE_KERNEL', microtime(true) - LARAVEL_REQUIRE_APP - LOAD_APP - LARAVEL_START);
+//define('LARAVEL_BEFORE_KERNEL', microtime(true) - LARAVEL_REQUIRE_APP - LOAD_APP - LARAVEL_START);
 
 $response = $kernel->handle(
     $request = App\SafeRequest::capture()
 );
 
-define('LARAVEL_AFTER_KERNEL', microtime(true) - LARAVEL_BEFORE_KERNEL - LARAVEL_REQUIRE_APP - LOAD_APP - LARAVEL_START);
+//define('LARAVEL_AFTER_KERNEL', microtime(true) - LARAVEL_BEFORE_KERNEL - LARAVEL_REQUIRE_APP - LOAD_APP - LARAVEL_START);
 
 if (config('responsecache.enabled') && ! $request->isMethodCacheable()) {
     app('invalidate-cache')->flushCandidates();
 }
 
-define('LARAVEL_AFTER_RESPONCE_CACHE', microtime(true) - LARAVEL_BEFORE_KERNEL - LARAVEL_REQUIRE_APP - LOAD_APP - LARAVEL_START - LARAVEL_AFTER_KERNEL);
+//define('LARAVEL_AFTER_RESPONCE_CACHE', microtime(true) - LARAVEL_BEFORE_KERNEL - LARAVEL_REQUIRE_APP - LOAD_APP - LARAVEL_START - LARAVEL_AFTER_KERNEL);
 
 //dd([
 //       'LARAVEL_START' => LARAVEL_START,
