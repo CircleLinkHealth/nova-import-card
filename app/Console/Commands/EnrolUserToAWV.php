018bfdfcb19b6cdad1416d4576d69fa996c55d9f
Pangratios Cosma <pagcosma@gmail.com>
2019-07-06 12:25:49 +0300

    CPM-1245 command to enrol users to AWV (add userid to users_surveys table for both HRA and VITALS)

---
+++ app/Console/Commands/EnrolUserToAWV.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\SurveyInvitationLinksService;
+use App\Survey;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class EnrolUserToAWV extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'enrol:user {userId} {forYear?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Enrol a user to AWV Surveys (HRA and Vitals).';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param SurveyInvitationLinksService $service
+     *
+     * @return mixed
+     */
+    public function handle(SurveyInvitationLinksService $service)
+    {
+        $userId = $this->argument('userId');
+
+        $user = User
+            ::with([
+                'surveyInstances' => function ($query) {
+                    $query->ofSurvey(Survey::HRA)->current();
+                },
+            ])
+            ->where('id', '=', $userId)
+            ->first();
+
+        if ( ! $user) {
+            $this->warn("Could not find user with id $userId");
+
+            return;
+        }
+
+        $forYear = $this->argument('forYear');
+        if ( ! $forYear) {
+            $forYear = Carbon::now()->year;
+        }
+
+        if ( ! $user->surveyInstances->isEmpty()) {
+            $this->warn("User[$userId] is already enrolled to AWV for year $forYear");
+
+            return;
+        }
+
+        try {
+            $service->enrolUser($user, $forYear);
+            $this->info("Done");
+        } catch (\Exception $e) {
+            $this->warn($e);
+        }
+    }
+}
