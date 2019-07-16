989e86f1f9944f45d9808fd5389b9d19ba379322
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-07-16 23:25:06 +0300

    fix

--- .deploybot/after_new_version_is_uploaded_no_build_commands.sh
+++ .deploybot/after_new_version_is_uploaded_no_build_commands.sh
@@ -37,7 +37,7 @@ fi
 # laravel needs these to run, and git does not clone empty folders
 mkdir -p $RELEASE/storage/framework/{framework,sessions,views,cache}
 
-composer dump-autoload —classmap-authoritative
+composer dump-autoload --classmap-authoritative
 
 # Run migrations
 php artisan migrate --force
