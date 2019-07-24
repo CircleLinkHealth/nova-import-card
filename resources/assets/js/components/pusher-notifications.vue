93a29331b359d60ab88bd39ffd46895ee6db87c5
Antonis Antoniou <kountouris7@gmail.com>
2019-07-24 15:49:08 +0300

    CPM-1035 - progress on notifications dropdown2 + redirect to route

--- resources/assets/js/components/pusher-notifications.vue
+++ resources/assets/js/components/pusher-notifications.vue
@@ -2,12 +2,12 @@
     <div>
         <div class="dropdown-toggle top-nav-item" data-toggle="dropdown" role="button"
              aria-expanded="false" style="padding: inherit;"><i class="glyphicon glyphicon-bell"></i> Notifications
-            <span v-show="shouldShowCount" class="badge badge-secondary"
-                  style="background-color: #e46745;">{{count}}</span></div>
+            <span v-show="shouldShowCount" class="badge badge-secondary">{{count}}</span></div>
 
-        <ul class="dropdown-menu" role="menu" style="background: white !important;">
+        <ul class="dropdown-menu" role="menu">
+            <li class="dropdown-header">NOTIFICATIONS</li>
             <li v-for="notification in notifications">
-                <a>{{notification.sender_id}} {{notification.subject}} {{notification.patient_id}}</a>
+                <a @click="redirectToSource()">{{notification.sender_id}} {{notification.subject}} {{notification.patient_id}}</a>
             </li>
         </ul>
     </div>
@@ -17,7 +17,7 @@
 
     export default {
         name: "pusher-notifications",
-
+        components: {},
         props: [
             'user'
         ],
@@ -40,7 +40,12 @@
             },
 
         },
-        methods: {},
+        methods: {
+            redirectToSource(){
+                window.location.href = '/';
+
+            }
+        },
 
         created() {
             axios.get('/addendum-notifications')
@@ -55,8 +60,37 @@
             });
         }
     }
+
 </script>
 
 <style scoped>
+    .dropdown-menu {
+        cursor: pointer;
+        word-spacing: 5px;
+        background: #ffffff;
+        padding-bottom: unset;
+        overflow-y: scroll;
+        max-height: 480%;
+    }
+
+    .dropdown-menu li > a {
+        padding-top: 4%;
+        padding-bottom: 4%;
+        color: #90949c;
+        font-family: Helvetica, Arial, sans-serif;
+        background-color: #ffffff;
+        border-bottom: 1px solid #90949c;
+    }
+
+    .badge-secondary {
+        background-color: #e46745;
+    }
+
+    .dropdown-header {
+        color: #90949c;
+        cursor: default;
+        font-weight: bold;
+        border-bottom: 1px solid #90949c;
+    }
 
 </style>
