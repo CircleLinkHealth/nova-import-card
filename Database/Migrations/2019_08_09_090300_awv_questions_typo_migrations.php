00f573735dc7389a901c816e1f72cb209725519d
Antonis Antoniou <kountouris7@gmail.com>
2019-08-09 16:06:15 +0300

    CPM-1415 migration file typo

--- Database/Migrations/2019_08_09_090300_awv_questions_typo_migrations.php
+++ Database/Migrations/2019_08_09_090300_awv_questions_typo_migrations.php
@@ -13,7 +13,22 @@ class AwvQuestionsTypoMigrations extends Migration
      */
     public function up()
     {
-        //
+        $q14 = \App\Question::where('body', '=', 'How often do you exerise?')
+            ->first();
+
+        $q5a = \App\Question::where('body', '=', 'Word Recall (1 point for each word spontaneously recalled without cueing)')
+            ->first();
+
+        if ($q14) {
+            $q14->body = 'How often do you exercise?';
+            $q14->save();
+        }
+
+        if ($q5a) {
+            $q5a->body = 'Word Recall (1 point for each word spontaneously recalled without cueing) 
+                http://mini-cog.com/wp-content/uploads/2015/12/Universal-Mini-Cog-Form-011916.pdf';
+            $q5a->save();
+        }
     }
 
     /**
