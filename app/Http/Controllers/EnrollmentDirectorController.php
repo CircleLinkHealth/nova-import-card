859a8fe3c907dd4ca811312e79775d271c47d352
kakoushias <kakoushias@gmail.com>
2019-08-22 14:40:12 +0300

    CPM-1430 applies fix

--- app/Http/Controllers/EnrollmentDirectorController.php
+++ app/Http/Controllers/EnrollmentDirectorController.php
@@ -43,14 +43,24 @@ class EnrollmentDirectorController extends Controller
 
     public function assignCareAmbassadorToEnrollees(UpdateMultipleEnrollees $request)
     {
-        $enrolleeIds  = implode(',', $request->input('enrolleeIds'));
+        //using this to input a '?' in WHERE IN for each id
+        $inQuery = implode(',', array_fill(0, count($request->input('enrolleeIds')), '?'));
+
         $toCall       = Enrollee::TO_CALL;
         $softDeclined = Enrollee::SOFT_REJECTED;
 
-        DB::statement('UPDATE enrollees
+        //using this to create an extra binding for each number
+        // - otherwise if we pass the imploded array as a string,
+        // it will cast it as an integer, taking only the 1st id of the comma-delimited array
+        $bindings = array_merge(
+            [$request->input('ambassadorId'), $softDeclined, $toCall],
+            $request->input('enrolleeIds')
+        );
+
+        DB::statement("UPDATE enrollees
 SET
     care_ambassador_user_id = ?, status = CASE WHEN status = ? THEN ? ELSE status END
-WHERE id IN (?)', [$request->input('ambassadorId'), $softDeclined, $toCall, $enrolleeIds]);
+WHERE id IN ({$inQuery})", $bindings);
 
         return response()->json([], 200);
     }
