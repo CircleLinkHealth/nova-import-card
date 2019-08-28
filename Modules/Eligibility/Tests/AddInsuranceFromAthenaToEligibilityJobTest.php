a06e36cc81c501598a59ab461a01c9df9b0ecd1f
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-08-29 00:58:50 +0300

    Adds Test

---
+++ Modules/Eligibility/Tests/AddInsuranceFromAthenaToEligibilityJobTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\Eligibility\Tests;
+
+use App\EligibilityJob;
+use App\Models\MedicalRecords\Ccda;
+use App\TargetPatient;
+use CircleLinkHealth\Eligibility\Tests\Fakers\AthenaApiResponses;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Tests\TestCase;
+
+class AddInsuranceFromAthenaToEligibilityJobTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * A basic test example.
+     */
+    public function test_it_adds_insurance_from_athena()
+    {
+        $successfulApiResponse = AthenaApiResponses::getPatientInsurances();
+        //Mock
+        $athena = \Mockery::mock(\CircleLinkHealth\Eligibility\Contracts\AthenaApiImplementation::class);
+        $athena->shouldReceive('getPatientInsurances')->once()->andReturn($successfulApiResponse);
+
+        //Test
+        $eligibilityJob = new EligibilityJob();
+        $targetPatient  = new TargetPatient();
+        $ccda           = new Ccda();
+        $decorator      = new \CircleLinkHealth\Eligibility\Decorators\AddInsuranceFromAthenaToEligibilityJob($athena);
+
+        //Assert
+        $eligibilityJob = $decorator->addInsurancesFromAthena($eligibilityJob, $targetPatient, $ccda);
+
+        $this->assertInstanceOf(EligibilityJob::class, $eligibilityJob);
+    }
+}
