cb04c6ac88ca6eb80b78e5be7fc4a10cb867f647
Michalis Antoniou <mantoniou@circlelinkhealth.com>
2019-08-29 10:36:05 +0300

    More docs

--- Modules/Eligibility/Contracts/Checkable.php
+++ Modules/Eligibility/Contracts/Checkable.php
@@ -11,21 +11,28 @@ use App\EligibilityJob;
 use App\TargetPatient;
 
 /**
- * Medical Records which can be checked for Eligibility can implement this contract.
+ * A "Checkable" is an object encapsulating behavior and data necessary to determine whether the patient whose materials are encapsulated by the "Checkable" is eligible to receive service from CLH.
  *
  * Interface Checkable
  */
 interface Checkable extends HasMedicalRecord
 {
     /**
+     * Creates an EligibilityJob using the MedicalRecord provided to the Checkable, and stores it on the instance.
+     *
      * @return EligibilityJob
      */
     public function createAndProcessEligibilityJobFromMedicalRecord(): EligibilityJob;
 
+    /**
+     * Get the Eligibility Job stored on the instance.
+     *
+     * @return EligibilityJob
+     */
     public function getEligibilityJob(): EligibilityJob;
 
     /**
-     * This Model holds the data we need to make a request to an EHR API to get patient data.
+     * This Model holds the data we need to make a request to an EHR API to get patient data. This includes data such as the the patient/practice/department ID in the EHR,.
      *
      * @return TargetPatient
      */
