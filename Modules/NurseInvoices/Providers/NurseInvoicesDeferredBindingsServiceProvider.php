7da853a01f6e8740ebd0e8a5827eecbdfc2fde84
kakoushias <kakoushias@gmail.com>
2019-09-26 18:29:21 +0300

    CPM-1510 ditches DI from alligator class

--- Modules/NurseInvoices/Providers/NurseInvoicesDeferredBindingsServiceProvider.php
+++ Modules/NurseInvoices/Providers/NurseInvoicesDeferredBindingsServiceProvider.php
@@ -6,14 +6,10 @@
 
 namespace CircleLinkHealth\NurseInvoices\Providers;
 
-use Carbon\Carbon;
-use CircleLinkHealth\Customer\Entities\User;
-use CircleLinkHealth\NurseInvoices\AggregatedTotalTimePerNurse;
 use CircleLinkHealth\NurseInvoices\Console\Commands\GenerateMonthlyInvoicesForNonDemoNurses;
 use CircleLinkHealth\NurseInvoices\Console\Commands\SendMonthlyNurseInvoiceLAN;
 use CircleLinkHealth\NurseInvoices\Console\Commands\SendResolveInvoiceDisputeReminder;
 use CircleLinkHealth\NurseInvoices\Console\SendMonthlyNurseInvoiceFAN;
-use CircleLinkHealth\NurseInvoices\TotalTimeAggregator;
 use Illuminate\Support\ServiceProvider;
 
 class NurseInvoicesDeferredBindingsServiceProvider extends ServiceProvider
@@ -33,7 +29,6 @@ class NurseInvoicesDeferredBindingsServiceProvider extends ServiceProvider
     public function provides()
     {
         return [
-            AggregatedTotalTimePerNurse::class,
             GenerateMonthlyInvoicesForNonDemoNurses::class,
             SendMonthlyNurseInvoiceFAN::class,
             SendMonthlyNurseInvoiceLAN::class,
@@ -46,44 +41,6 @@ class NurseInvoicesDeferredBindingsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(AggregatedTotalTimePerNurse::class, function ($app, array $args) {
-            if (empty($args)) {
-                $userIds = User::ofType('care-center')
-                    ->with(
-                                   [
-                                       'nurseInfo' => function ($info) {
-                                           $info->with(
-                                               [
-                                                   'windows',
-                                                   'holidays',
-                                                   'workhourables',
-                                               ]
-                                           );
-                                       },
-                                   ]
-                               )
-                    ->whereHas(
-                                   'nurseInfo',
-                                   function ($info) {
-                                       $info->where('status', 'active')
-                                           ->when(isProductionEnv(), function ($info) {
-                                                $info->where('is_demo', false);
-                                            });
-                                   }
-                               )
-                    ->pluck('id')
-                    ->all();
-                $startDate = Carbon::yesterday()->startOfDay();
-                $endDate = Carbon::yesterday()->endOfDay();
-            } else {
-                $userIds = $args[0];
-                $startDate = $args[1];
-                $endDate = $args[2];
-            }
-
-            return new AggregatedTotalTimePerNurse(new TotalTimeAggregator(parseIds($userIds), $startDate, $endDate));
-        });
-
         if ($this->app->runningInConsole()) {
             $this->commands(
                 [
