7b6e700b2e76cdeaf5b97b3a228f6520592b57bc
kakoushias <kakoushias@gmail.com>
2019-10-03 01:26:15 +0300

    CPM-1613 additions

--- app/Nova/Importers/PracticeStaff.php
+++ app/Nova/Importers/PracticeStaff.php
@@ -6,13 +6,18 @@
 
 namespace App\Nova\Importers;
 
+use App\CLH\Repositories\UserRepository;
 use App\Search\PracticeByName;
+use CircleLinkHealth\Customer\Entities\Role;
+use CircleLinkHealth\Customer\Entities\User;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Row;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Validator;
 
 class PracticeStaff implements OnEachRow, WithChunkReading, WithValidation, WithHeadingRow
 {
@@ -50,10 +55,23 @@ class PracticeStaff implements OnEachRow, WithChunkReading, WithValidation, With
      */
     public function onRow(Row $row)
     {
-        //set arguements to create users
-        $x = 1;
+        $validator = $this->validateRow($row);
 
-        //create user through UserRepository
+        if ($validator->fails()) {
+            return;
+        }
+
+        $user = $this->createUser($row);
+
+        if ( ! $user) {
+            return;
+        }
+
+        $this->attachPhone($user, $row);
+
+        $this->attachEmrDirectAddress($user, $row);
+
+        $this->attachLocation($user, $row);
     }
 
     public function rules(): array
@@ -71,12 +89,73 @@ class PracticeStaff implements OnEachRow, WithChunkReading, WithValidation, With
             $practice = PracticeByName::first($array[0]);
 
             if ( ! $practice) {
-                throw new \Exception('Practice not found. Please make sure that the file name is a valid Practice name.', 500);
+                throw new \Exception(
+                    'Practice not found. Please make sure that the file name is a valid Practice name.',
+                    500
+                );
             }
 
             return $practice;
         }
 
         throw new \Exception('Something went wrong. File not found.', 500);
+    }
+
+    private function attachEmrDirectAddress($user, $row)
+    {
+        if (empty($row['emr_direct_address'])) {
+            return;
+        }
+    }
+
+    private function attachLocation($user, $row)
+    {
+        if (empty($row['locations'])) {
+            return;
+        }
+    }
+
+    private function attachPhone($user, $row)
+    {
+        if (empty($row['phone_number'])) {
+            return;
+        }
+    }
+
+    private function createUser($row)
+    {
+        //get role, use Scout?
+        $role = Role::where('display_name', $row['role'])->first();
+
+        if ( ! $role) {
+            return null;
+        }
+
+        $bag = new ParameterBag([
+            'email'             => $row['email'],
+            'password'          => str_random(),
+            'display_name'      => $row['first_name'].' '.$row['last_name'],
+            'first_name'        => $row['first_name'],
+            'last_name'         => $row['last_name'],
+            'username'          => $row['email'],
+            'program_id'        => $this->practice->id,
+            'is_auto_generated' => true,
+            'roles'             => [$role->id],
+        ]);
+
+        return (new UserRepository())->createNewUser(new User(), $bag);
+    }
+
+    private function validateRow($row)
+    {
+        return Validator::make(
+            $row,
+            [
+                'email'      => 'required|email',
+                'first_name' => 'required',
+                'last_name'  => 'required',
+                'role'       => 'required',
+            ]
+        );
     }
 }
