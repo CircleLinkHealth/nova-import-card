a304f3f016f1a60140d0248e3aba24a042ae8337
kakoushias <kakoushias@gmail.com>
2019-10-15 01:09:27 +0300

    CPM-1613 removes ugly comments

--- nova-components/ImportPracticeStaffCsv/src/CLHImportCardController.php
+++ nova-components/ImportPracticeStaffCsv/src/CLHImportCardController.php
@@ -10,12 +10,20 @@ use Sparclex\NovaImportCard\ImportController;
 
 class CLHImportCardController
 {
+    /**
+     * We need a post request made to Sparclex\NovaImportCard\ImportController@handle
+     * but we cannot redirect to a POST request.
+     *
+     *This is a hacky workaround.
+     *
+     * @param ImportPracticeStaffCsvNovaRequest $request
+     *
+     * @return array
+     */
     public function handle(ImportPracticeStaffCsvNovaRequest $request)
     {
-        //not possible to redirect to POST
         $controller = new ImportController();
 
         return $controller->handle($request);
-//        return redirect()->action('\Sparclex\NovaImportCard\ImportController@handle', ['request' => $request]);
     }
 }
