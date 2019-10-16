3d79db314c393a7ca8d8676654e4666a3133080c
kakoushias <kakoushias@gmail.com>
2019-10-16 23:27:10 +0300

    CPM-1668 merges 1518 to get mail contract and job, adds notification and mailable

---
+++ app/Mail/TrixMailable.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TrixMailable extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    protected $content;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param mixed $content
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $x = 1;
+
+        return $this->view('patient.patient-email')
+            ->with([
+                'content' => $this->content,
+            ])
+            ->from('no-replyg@circlelinkhealth.com', 'CircleLink Health')
+            ->subject('You have received a mail from Care Plan Manager');
+    }
+}
---
+++ app/Notifications/SendPatientEmail.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App\Notifications;
+
+use App\Mail\TrixMailable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class SendPatientEmail extends Notification
+{
+    use Queueable;
+
+    protected $content;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param mixed $content
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new TrixMailable($this->content))
+            ->to($notifiable->email);
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+}
