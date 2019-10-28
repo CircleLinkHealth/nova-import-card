fe61ef724de57800f0710e34defb2b34fbdc2fde
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-10-28 17:11:03 +0200

    Subscribe to AWV redis

---
+++ app/Console/Commands/SubscribeToRedisAWVChannel.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Redis;
+
+class SubscribeToRedisAWVChannel extends Command
+{
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Subscribe to a Redis channel';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'redis:subscribe';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Redis::subscribe(['test-channel'], function ($message) {
+            echo $message;
+
+            \Log::channel('logdna')->info('Recording test-channel message', [
+                'batch_id' => \Carbon::now()->toDateTimeString(),
+            ]);
+        });
+    }
+}
