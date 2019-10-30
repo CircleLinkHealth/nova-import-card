6454a637422b329cbcc21f9a3a88c4d43e5e32cf
Pangratios Cosma <pagcosma@gmail.com>
2019-10-30 13:23:15 +0200

    CPM-1641 css improvements | progress

--- resources/views/reports/ppp.blade.php
+++ resources/views/reports/ppp.blade.php
@@ -37,11 +37,12 @@ function getStringValue($val, $default = '')
     <link href="{{ asset('css/pppReport.css') }}" rel="stylesheet">
 
     <div class="container report">
+
         <div class="report-title">
-            <h3>Patient Info</h3>
+            <div>Patient Info</div>
             <hr>
         </div>
-        <div>
+        <div class="report-data">
             Patient Name: <span style="color: #50b2e2">{{$patient->display_name}}</span> <br>
             Date of Birth (records): <strong>{{$patient->patientInfo->birth_date}}</strong><br>
             Age (self-reported): <strong>{{getStringValue($patientPppData->answers_for_eval['age'])}}</strong> <br>
@@ -49,12 +50,12 @@ function getStringValue($val, $default = '')
             City, State, Zip: <strong>{{$patient->city}}, {{$patient->state}}, {{$patient->zip}}</strong> <br>
             Provider: <strong>{{$patient->getBillingProviderName()}}</strong>
         </div>
+
         <div class="report-title">
-            <br>
-            <h3>Vitals</h3>
+            <div>Vitals</div>
             <hr>
         </div>
-        <div>
+        <div class="report-data">
             Weight: <strong>{{getStringValue($patientPppData->answers_for_eval['weight'])}} </strong><br>
             Height: <strong>{{getStringValue($patientPppData->answers_for_eval['height']['feet'])}}
                 ' {{getStringValue($patientPppData->answers_for_eval['height']['inches'])}}' </strong><br>
@@ -63,16 +64,15 @@ function getStringValue($val, $default = '')
             <strong>{{getStringValue($patientPppData->answers_for_eval['blood_pressure']['first_metric'])}}
                 / {{getStringValue($patientPppData->answers_for_eval['blood_pressure']['second_metric'])}}</strong><br>
         </div>
-        <br>
-        <div class="suggested-list">
-            <div class="report-title col-md-7 no-padding">
-                <h3>Suggested CheckList</h3>
-            </div>
-            <div class="side-title col-md-5 no-padding">
-                Ask your doctor about:
-            </div>
-        </div>
 
+        <div class="suggested-list">
+            <span class="report-title">
+                Suggested CheckList
+            </span>
+            <span class="side-title">
+                Ask your doctor about:
+            </span>
+        </div>
 
         <hr>
 
@@ -102,43 +102,49 @@ function getStringValue($val, $default = '')
         <br>
         <div class="row">
             <div class="col">
-                <div class="health-advice-title">
-                    <h3>Personalized Health Advice</h3>
-                </div>
-                <hr>
-
                 @foreach($personalizedHealthAdvices as $key => $tasks)
-                    @if(array_filter($tasks['tasks']))
-                        <br>
-                        <div class="recommendation-title">
-                            <div class="image {{$tasks['image']}}"></div> {{$tasks['title']}}
-                        </div>
-                    @endif
-                    <br>
-                    @foreach($tasks['tasks'] as $key => $recommendations)
-                        @if(! empty($recommendations) && isset($recommendations['qualitative_trigger']))
-                            <div class="recommendations-area">
-                                <div><strong>{{$recommendations['qualitative_trigger']}}</strong></div>
-                                <div>{{$recommendations['task_body']}}</div>
-                                <br>
-                                @if (is_array($recommendations['recommendation_body']))
-                                    @foreach($recommendations['recommendation_body'] as $recBodyItem)
-                                        <ul>
-                                            <li style="font-weight: 400; margin-left: 7%;"><i>{{$recBodyItem}}</i>
-                                            </li>
-                                        </ul>
-                                    @endforeach
-                                @else
-                                    <ul>
-                                        <li style="font-weight: 400; margin-left: 7%;">
-                                            <i>{{$recommendations['recommendation_body']}}</i>
-                                        </li>
-                                    </ul>
-                                @endif
-                                <br>
+                    <div class="avoid-page-break">
+
+                        <!-- keeping this here so it sticks with the first item in case it goes over a page break -->
+                        @if($loop->index === 0)
+                            <div class="health-advice-title">
+                                <div>Personalized Health Advice</div>
+                            </div>
+                            <hr>
+                        @endif
+
+                        @if(array_filter($tasks['tasks']))
+                            <br>
+                            <div class="recommendation-title">
+                                <div class="image {{$tasks['image']}}"></div> {{$tasks['title']}}
                             </div>
                         @endif
-                    @endforeach
+                        <br>
+                        @foreach($tasks['tasks'] as $key => $recommendations)
+                            @if(! empty($recommendations) && isset($recommendations['qualitative_trigger']))
+                                <div class="recommendations-area">
+                                    <div><strong>{{$recommendations['qualitative_trigger']}}</strong></div>
+                                    <div>{{$recommendations['task_body']}}</div>
+                                    <br>
+                                    @if (is_array($recommendations['recommendation_body']))
+                                        @foreach($recommendations['recommendation_body'] as $recBodyItem)
+                                            <ul>
+                                                <li style="font-weight: 400; margin-left: 7%;"><i>{{$recBodyItem}}</i>
+                                                </li>
+                                            </ul>
+                                        @endforeach
+                                    @else
+                                        <ul>
+                                            <li style="font-weight: 400; margin-left: 7%;">
+                                                <i>{{$recommendations['recommendation_body']}}</i>
+                                            </li>
+                                        </ul>
+                                    @endif
+                                    <br>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
                 @endforeach
             </div>
         </div>
