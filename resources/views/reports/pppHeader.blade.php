f4dc37f7fb1168811054e9f83ea7cf8e6f7da3d3
Pangratios Cosma <pagcosma@gmail.com>
2019-10-31 22:00:39 +0200

    CPM-1641 hide header on first page

--- resources/views/reports/pppHeader.blade.php
+++ resources/views/reports/pppHeader.blade.php
@@ -25,8 +25,41 @@
             border-bottom: 1px solid rgba(0, 0, 0, .1);
         }
     </style>
+
+    <script>
+        function subst() {
+            var vars = {};
+
+            // explode the URL query string
+            var x = document.location.search.substring(1).split('&');
+
+            // loop through each query string segment and get keys/values
+            for (var i in x)
+            {
+                var z = x[i].split('=',2);
+                vars[z[0]] = unescape(z[1]);
+            }
+
+            // an array of all the parameters passed into the footer file
+            var x = ['frompage', 'topage', 'page', 'webpage', 'section', 'subsection', 'subsubsection'];
+
+            // each page will have an element with class 'section' from the body of the footer HTML
+            var y = document.getElementsByClassName('report-header');
+            for(var j = 0; j < y.length; j++)
+            {
+                // if current page equals total pages
+                // if (vars[x[2]] == vars[x[1]])
+
+                //if current page is the first
+                if (vars[x[2]] == 1)
+                {
+                    y[j].style.display = "none";
+                }
+            }
+        }
+    </script>
 </head>
-<body>
+<body onload="subst()">
 <div class="report-header">
     Wellness Visit PPP for&nbsp;<span class="patient-name">{{$patientName}}</span>&nbsp;(DOB: {{$patientDob}})
 </div>
