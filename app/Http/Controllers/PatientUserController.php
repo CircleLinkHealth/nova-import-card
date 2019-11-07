f31ce4fc63b75b7c53991f68d5cf5bfc1b05da9c
kakoushias <kakoushias@gmail.com>
2019-11-07 02:57:25 +0200

    CPM-1733 cleanup

--- app/Http/Controllers/PatientUserController.php
+++ app/Http/Controllers/PatientUserController.php
@@ -26,23 +26,21 @@ class PatientUserController extends Controller
     public function viewCareplan(
         Request $request
     ) {
-        $patient = User::with(PatientCareplanRelations::get())->findOrFail(auth()->user()->id);
+        $patient = User::with(PatientCareplanRelations::get())
+            ->findOrFail(auth()->user()->id);
 
-        $careplan = $this->reportFormatter->formatDataForViewPrintCareplanReport($patient);
-        $careplan = $careplan[$patient->id];
-
-        //change check, make cleaner
-        if (empty($careplan)) {
-            throw new \Exception("Could not get CarePlan info for CarePlan with ID: {$patient->id}");
-        }
+        $careplan = collect($this->reportFormatter->formatDataForViewPrintCareplanReport($patient))->first();
 
         return view(
             'wpUsers.patient.careplan.print-patient',
             [
-                'careplans'    => [$patient->id => $careplan],
-                'problemNames' => $careplan['problem'],
-                'careTeam'     => $patient->careTeamMembers,
-                'data'         => $this->careplanService->careplan($patient->id),
+                'patient'       => $patient,
+                'careplan'      => $careplan,
+                'problemNames'  => $careplan['problem'],
+                'careTeam'      => $patient->careTeamMembers,
+                'data'          => $this->careplanService->careplan($patient->id),
+                'billingDoctor' => $patient->billingProviderUser(),
+                'regularDoctor' => $patient->regularDoctorUser(),
             ]
         );
     }
--- resources/views/wpUsers/patient/careplan/print-patient.blade.php
+++ resources/views/wpUsers/patient/careplan/print-patient.blade.php
@@ -6,12 +6,6 @@ $today = \Carbon\Carbon::now()->toFormattedDateString();
 @section('title', 'Care Plan View/Print')
 @section('activity', 'Care Plan View/Print')
 @section('content')
-    @foreach($careplans as $id => $careplan)
-        <?php
-        $patient       = \CircleLinkHealth\Customer\Entities\User::find($id);
-        $billingDoctor = $patient->billingProviderUser();
-        $regularDoctor = $patient->regularDoctorUser();
-        ?>
         <div class="container">
             <section class="patient-summary">
                 <div class="patient-info__main">
@@ -21,6 +15,4 @@ $today = \Carbon\Carbon::now()->toFormattedDateString();
             </section>
         </div>
         <div class="row pb-before"></div>
-
-    @endforeach
 @stop
