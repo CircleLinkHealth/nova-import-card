bb91064cc0251e19c01d8076788ae0d5cd0f69eb
kakoushias <kakoushias@gmail.com>
2019-11-08 16:35:19 +0200

    CPM-1733 improves error messages

--- app/Http/Middleware/CheckPatientUserData.php
+++ app/Http/Middleware/CheckPatientUserData.php
@@ -20,16 +20,23 @@ class CheckPatientUserData
      */
     public function handle($request, Closure $next)
     {
-        if ( ! auth()->user()->hasRole('participant')) {
+        $loggedUser = auth()->user();
+
+        if ( ! $loggedUser->hasRole('participant')) {
             auth()->logout();
 
             return redirect('login')->withErrors(['This page can be accessed only by patients.']);
         }
 
-        if ( ! auth()->user()->carePlan) {
+        if ( ! $loggedUser->carePlan) {
+            /** @var RaygunLogger $raygunLogger */
+            $raygunLogger = app('raygun.logger');
+            if ($raygunLogger) {
+                $raygunLogger->error("Care Plan for patient user with id: {$loggedUser->id} not found");
+            }
             auth()->logout();
 
-            return redirect('login')->withErrors(['Care Plan does not exist. Please contact CircleLink Health Support.']);
+            return redirect('login')->withErrors(['careplan-error' => "[402] There was an error retrieving your Care Plan and we are investigating the issue. <br> If the problem persists, please contact CircleLink Health Support at <a href='mailto:contact@circlelinkhealth.com'>contact@circlelinkhealth.com</a>."]);
         }
 
         return $next($request);
