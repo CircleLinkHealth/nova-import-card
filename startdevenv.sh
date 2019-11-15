a3b4d38f48b98a52ef6e2f3aa89d81947417e479
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-11-15 10:02:33 +0900

    Small changes

--- startdevenv.sh
+++ startdevenv.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+# Reset BASH time counter
+SECONDS=0
+
 docker-compose up --build --remove-orphans -d
 
 docker-compose exec app npm run build-dev
 
-echo 'Done! http://localhost:8085/'
+docker-compose exec app php artisan db:seed --class=TestSuiteSeeder
+
+ELAPSED="Elapsed: $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+
+echo "Time elapsed: $ELAPSED seconds."
+
+echo "Done! You may head to: http://localhost:8085"
