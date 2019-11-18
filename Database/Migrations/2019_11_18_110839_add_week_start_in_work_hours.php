3e3345e0ed1d5933ca2cf49716589906e03a6b95
Antonis Antoniou <kountouris7@gmail.com>
2019-11-18 20:25:52 +0200

    CPM-1658 - cant save date

---
+++ Database/Migrations/2019_11_18_110839_add_week_start_in_work_hours.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWeekStartInWorkHours extends Migration
+{
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('work_hours', function (Blueprint $table) {
+            $table->dropColumn('work_week_start');
+        });
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('work_hours', function (Blueprint $table) {
+            $table->date('work_week_start');
+        });
+    }
+}
