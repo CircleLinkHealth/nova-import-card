28be1dcf28f94095191263482e1701c90df46759
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-11-19 16:44:33 +0900

    Removes userMeta

--- app/Http/Controllers/Patient/PatientCareplanController.php
+++ app/Http/Controllers/Patient/PatientCareplanController.php
@@ -460,7 +460,6 @@ class PatientCareplanController extends Controller
             compact(
                 [
                     'patient',
-                    'userMeta',
                     'userConfig',
                     'states',
                     'locations',
