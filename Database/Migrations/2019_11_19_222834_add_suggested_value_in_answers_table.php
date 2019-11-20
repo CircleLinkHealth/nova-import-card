ac576c1dbfecc0e8f117735d29ed9aaf695df0fe
Pangratios Cosma <pagcosma@gmail.com>
2019-11-20 21:53:32 +0200

    CPM-1475 command to calculate answer suggestions, job to calculate answer suggestions, answer calculator class, dispatch job to calculate answer suggestions when patient is enrolled, migration suggested_value in answers, UI to show suggested answer if available

---
+++ Database/Migrations/2019_11_19_222834_add_suggested_value_in_answers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSuggestedValueInAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->json('value')
+                  ->nullable(true)
+                  ->default(null)
+                  ->change();
+
+            $table->json('suggested_value')
+                  ->nullable(true)
+                  ->default(null)
+                  ->after('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->json('value')->nullable(false)->change();
+
+            if (Schema::hasColumn('answers', 'suggested_value')) {
+                $table->dropColumn('suggested_value');
+            }
+        });
+
+    }
+}
