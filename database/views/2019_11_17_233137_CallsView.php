56c22da5389de74e0e815b1c7320600f03b6c133
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-11-22 23:50:28 +0900

    Fixes views

--- database/views/2019_11_17_233137_CallsView.php
+++ database/views/2019_11_17_233137_CallsView.php
@@ -30,17 +30,37 @@ class CallsView extends BaseSqlView
             c.id,
             c.is_manual,
             c.status,
-            if(c.type = 'call' or c.type is null, 'call', c.sub_type) as type,
+            CASE WHEN c.type = 'call' or c.type is null
+                THEN 'call'
+                ELSE c.sub_type
+            END
+            as type,
             u2.nurse_id,
             u2.nurse,
             u1.patient_id,
             u1.patient,
             c.scheduled_date,
             u4.last_call,
-            if (u5.ccm_time is null, 0, u5.ccm_time) as ccm_time,
-            if (u5.bhi_time is null, 0, u5.bhi_time) as bhi_time,
-            if (u5.no_of_calls is null, 0, u5.no_of_calls) as no_of_calls,
-            if (u5.no_of_successful_calls is null, 0, u5.no_of_successful_calls) as no_of_successful_calls,
+            CASE WHEN u5.ccm_time is null
+                THEN 0
+                ELSE u5.ccm_time
+            END
+            as ccm_time,
+            CASE WHEN u5.bhi_time is null
+                THEN 0
+                ELSE u5.bhi_time
+            END
+            as bhi_time,
+            CASE WHEN u5.no_of_calls is null
+                THEN 0
+                ELSE u5.no_of_calls
+            END
+            as no_of_calls,
+            CASE WHEN u5.no_of_successful_calls is null
+                THEN 0
+                ELSE u5.no_of_successful_calls
+            END
+            as no_of_successful_calls,
             u7.practice_id,
             u7.practice,
             u1.timezone,
@@ -48,9 +68,20 @@ class CallsView extends BaseSqlView
             c.window_end as call_time_end,
             c.asap,
             u6.preferred_call_days,
-            if(pccm.id is null, false, true) as is_ccm,
-            if(pbhi.id is null, false, true) as is_bhi,
-            if(u3.scheduler is null, c.scheduler, u3.scheduler) as scheduler,
+            CASE WHEN pccm.id is null
+                THEN false
+                ELSE true
+            END
+            as is_ccm,
+            CASE WHEN pbhi.id is null
+                THEN false
+                ELSE true
+            END
+            as is_bhi,
+            CASE WHEN u3.scheduler is null
+                THEN c.scheduler
+                ELSE u3.scheduler
+            END as scheduler,
             u8.billing_provider,
             c.attempt_note,
             u4.general_comment,
@@ -61,9 +92,10 @@ class CallsView extends BaseSqlView
             calls c
             left join (select u.id as patient_id, CONCAT(u.display_name) as patient, u.timezone from users u where u.deleted_at is null) as u1 on c.inbound_cpm_id = u1.patient_id
 
-            left join (select u.id as nurse_id, CONCAT(u.first_name, ' ', u.last_name, ' ', (if (u.suffix is null, '', u.suffix))) as nurse from users u where u.deleted_at is null) as u2 on c.outbound_cpm_id = u2.nurse_id
 
-            left join (select u.id as scheduler_id, u.display_name as `scheduler` from users u where u.deleted_at is null) as u3 on c.scheduler = u3.scheduler_id
+            left join (select u.id as nurse_id, CONCAT(u.first_name, ' ', u.last_name, ' ', (CASE WHEN u.suffix is null THEN '' ELSE u.suffix END)) as nurse from users u where u.deleted_at is null) as u2 on c.outbound_cpm_id = u2.nurse_id
+
+            left join (select u.id as scheduler_id, u.display_name as scheduler from users u where u.deleted_at is null) as u3 on c.scheduler::INT = u3.scheduler_id
             
             left join (select pi.user_id as patient_id, pi.last_contact_time as last_call, pi.no_call_attempts_since_last_success, pi.general_comment, pi.ccm_status from patient_info pi where pi.deleted_at is null and pi.ccm_status in ('enrolled', 'paused')) as u4 on c.inbound_cpm_id = u4.patient_id
             
@@ -78,8 +110,8 @@ class CallsView extends BaseSqlView
             left join patients_ccm_view pccm on c.inbound_cpm_id = pccm.id
             
             left join (select pbp.user_id as patient_id, u.display_name as billing_provider from users u join (select pctm.user_id, pctm.member_user_id from users u 		left join patient_care_team_members pctm on u.id = pctm.user_id where pctm.type = 'billing_provider') pbp on pbp.member_user_id = u.id) u8 on c.inbound_cpm_id = u8.patient_id
-            
-            left join (select pi.patient_user_id as patient_id, u.id as patient_nurse_id, CONCAT(u.first_name, ' ', u.last_name, ' ', (if (u.suffix is null, '', u.suffix))) as patient_nurse from users u join patients_nurses pi on u.id = pi.nurse_user_id where u.deleted_at is null) as u9 on c.inbound_cpm_id = u9.patient_id
+
+            left join (select pi.patient_user_id as patient_id, u.id as patient_nurse_id, CONCAT(u.first_name, ' ', u.last_name, ' ', (CASE WHEN u.suffix is null THEN '' ELSE u.suffix END)) as patient_nurse from users u join patients_nurses pi on u.id = pi.nurse_user_id where u.deleted_at is null) as u9 on c.inbound_cpm_id = u9.patient_id
          
         WHERE
             c.scheduled_date is not null
