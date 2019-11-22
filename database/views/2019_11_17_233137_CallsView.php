195d52843824258ee38d9ddb53af5242761c2751
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-11-23 00:01:51 +0900

    Fixes calls view

--- database/views/2019_11_17_233137_CallsView.php
+++ database/views/2019_11_17_233137_CallsView.php
@@ -19,9 +19,7 @@ class CallsView extends BaseSqlView
      */
     public function createSqlView(): bool
     {
-        $startOfMonthQuery = 'mysql' === config('database.connections')[config('database.default')]['driver']
-            ? "DATE_ADD(DATE_ADD(LAST_DAY(CONVERT_TZ(UTC_TIMESTAMP(),'UTC','America/New_York')), INTERVAL 1 DAY), INTERVAL - 1 MONTH)"
-            : "date('now','start of month')"; //sqlite
+        $startOfMonthQuery = "cast(date_trunc('month', current_date) as date)";
 
         return \DB::statement("
         CREATE VIEW {$this->getViewName()}
@@ -101,9 +99,9 @@ class CallsView extends BaseSqlView
             
             left join (select pms.patient_id, pms.ccm_time, pms.bhi_time, pms.no_of_successful_calls, pms.no_of_calls from patient_monthly_summaries pms where month_year = ${startOfMonthQuery}) u5 on c.inbound_cpm_id = u5.patient_id
             
-			left join (select pi.user_id, GROUP_CONCAT(pcw.day_of_week) as preferred_call_days from patient_info pi left join patient_contact_window pcw on pi.id = pcw.patient_info_id where pi.deleted_at is null group by pi.user_id) as u6 on c.inbound_cpm_id = u6.user_id
+			left join (select pi.user_id, string_agg(pcw.day_of_week::TEXT, ',') as preferred_call_days from patient_info pi left join patient_contact_window pcw on pi.id = pcw.patient_info_id where pi.deleted_at is null group by pi.user_id) as u6 on c.inbound_cpm_id = u6.user_id
 			
-			left join (select u.id as user_id, p.id as practice_id, p.display_name as practice from practices p join users u on u.program_id = p.id where p.active = 1) u7 on c.inbound_cpm_id = u7.user_id
+			left join (select u.id as user_id, p.id as practice_id, p.display_name as practice from practices p join users u on u.program_id = p.id where p.active = true) u7 on c.inbound_cpm_id = u7.user_id
 			       
             left join patients_bhi_chargeable_view pbhi on c.inbound_cpm_id = pbhi.id
             
