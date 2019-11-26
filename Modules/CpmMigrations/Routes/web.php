c91da19d88dfb2fdfcde840f1051f404e5d33e27
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-11-27 03:53:00 +0900

    Removes CpmMigrations from CPm Modules

--- Modules/CpmMigrations/Routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * This file is part of CarePlan Manager by CircleLink Health.
- */
-
-Route::prefix('cpmmigrations')->group(function () {
-    Route::get('/', 'CpmMigrationsController@index');
-});
