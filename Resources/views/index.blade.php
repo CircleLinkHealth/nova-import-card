449ef0b8610eba7a2c1d57da5d953856c9d052a4
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-11-27 04:10:58 +0900

    all files

--- Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('cpmmigrations::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('cpmmigrations.name') !!}
-    </p>
-@stop
