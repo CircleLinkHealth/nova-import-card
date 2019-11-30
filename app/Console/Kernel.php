48e3bd5c5d720f576f7b5be364b8faedfa95e120
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-12-01 04:16:33 +0900

    Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,14 +27,13 @@ use App\Console\Commands\QueueSendAuditReports;
 use App\Console\Commands\RemoveScheduledCallsForWithdrawnAndPausedPatients;
 use App\Console\Commands\RescheduleMissedCalls;
 use App\Console\Commands\ResetPatients;
+use App\Console\Commands\RunScheduler;
 use App\Console\Commands\SendCarePlanApprovalReminders;
 use App\Console\Commands\TuneScheduledCalls;
-use Carbon\Carbon;
 use CircleLinkHealth\NurseInvoices\Console\Commands\GenerateMonthlyInvoicesForNonDemoNurses;
 use CircleLinkHealth\NurseInvoices\Console\Commands\SendMonthlyNurseInvoiceLAN;
 use CircleLinkHealth\NurseInvoices\Console\Commands\SendResolveInvoiceDisputeReminder;
 use CircleLinkHealth\NurseInvoices\Console\SendMonthlyNurseInvoiceFAN;
-use CircleLinkHealth\NurseInvoices\Helpers\NurseInvoiceDisputeDeadline;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -44,6 +43,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        RunScheduler::class,
     ];
 
     /**
@@ -69,10 +69,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        if ( ! isQueueWorkerEnv()) {
-            return;
-        }
-
         $schedule->command('horizon:snapshot')->everyFiveMinutes()->onOneServer();
 
         $schedule->command(DetermineTargetPatientEligibility::class)
@@ -142,7 +138,23 @@ class Kernel extends ConsoleKernel
         $schedule->command(CareplanEnrollmentAdminNotification::class)
             ->dailyAt('07:00')
             ->withoutOverlapping()->onOneServer();
-        
+
+//        $schedule->command('ccda:determineEligibility')
+//                 ->everyFiveMinutes()
+//                 ->withoutOverlapping()->onOneServer();
+
+//        $schedule->command('ccda:toJson')
+//            ->everyMinute()
+//            ->withoutOverlapping()->onOneServer();
+
+//        $schedule->command('ccda:process')
+//            ->everyMinute()
+//            ->withoutOverlapping()->onOneServer();
+
+        //every 2 hours
+//        $schedule->command('ccdas:split-merged')
+//            ->cron('0 */2 * * *')->onOneServer();
+
         $schedule->command(QueueSendAuditReports::class)
             ->monthlyOn(1, '02:00')->onOneServer();
 
@@ -164,8 +176,9 @@ class Kernel extends ConsoleKernel
         $schedule->command(GenerateMonthlyInvoicesForNonDemoNurses::class)->dailyAt('00:10')->onOneServer();
         $schedule->command(SendMonthlyNurseInvoiceFAN::class)->monthlyOn(1, '08:30')->onOneServer();
 
-        $sendReminderAt = NurseInvoiceDisputeDeadline::for(Carbon::now()->subMonth())->subHours(36);
-        $schedule->command(SendMonthlyNurseInvoiceLAN::class)->monthlyOn($sendReminderAt->day, $sendReminderAt->format('H:i'))->onOneServer();
+        $schedule->command(SendMonthlyNurseInvoiceLAN::class)->everyMinute()->when(function () {
+            return SendMonthlyNurseInvoiceLAN::shouldSend();
+        })->onOneServer();
 
         $schedule->command(SendResolveInvoiceDisputeReminder::class)->dailyAt('08:35')->skip(function () {
             return SendResolveInvoiceDisputeReminder::shouldSkip();
