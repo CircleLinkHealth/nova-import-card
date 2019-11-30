fb461fceb695949c7310145fe4965d7e75a9a4f3
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-12-01 04:32:53 +0900

    makes master heroku even more deployable

--- app/Console/Commands/PostDeploymentTasks.php
+++ app/Console/Commands/PostDeploymentTasks.php
@@ -7,6 +7,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class PostDeploymentTasks extends Command
 {
@@ -38,9 +39,14 @@ class PostDeploymentTasks extends Command
      */
     public function handle()
     {
+        if (app()->environment(['local', 'testing'])) {
+            echo 'Not running because env is '.app()->environment().PHP_EOL;
+
+            return;
+        }
+
         collect(
             [
-                'nova:publish',
                 'view:clear',
                 'route:cache',
                 'config:cache',
@@ -51,21 +57,9 @@ class PostDeploymentTasks extends Command
             ]
         )->each(
             function ($command) {
-                if ( ! isQueueWorkerEnv() && in_array(
-                    $command,
-                    [
-                        'horizon:terminate',
-                        'queue:restart',
-                    ]
-                    )) {
-                    //@todo: start using envoyer scripts
-                    //Do not run Queue commands on production, as Worker now takes care of Prod jobs
-                    return;
-                }
-
                 $this->output->note("Running ${command}");
 
-                \Artisan::call($command);
+                Artisan::call($command);
 
                 $this->output->success("Finished running ${command}");
             }
