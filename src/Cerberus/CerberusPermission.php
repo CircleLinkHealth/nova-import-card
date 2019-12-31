1e5f16605c2f9a20257e8008ea3dfcd0df13e8c9
Michalis Antoniou <michalisantoniou6@gmail.com>
2019-12-31 10:08:49 -0600

    IDE Changes

--- src/Cerberus/CerberusPermission.php
+++ src/Cerberus/CerberusPermission.php
@@ -6,6 +6,26 @@
  *
  * @license MIT
  * @package Michalisantoniou6\Cerberus
+ * @property int $id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\CircleLinkHealth\Customer\Entities\Role[] $roles
+ * @property-read int|null $roles_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\CircleLinkHealth\Customer\Entities\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusPermission whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 
 use Michalisantoniou6\Cerberus\Contracts\CerberusPermissionInterface;
--- src/Cerberus/CerberusRole.php
+++ src/Cerberus/CerberusRole.php
@@ -6,6 +6,26 @@
  *
  * @license MIT
  * @package Michalisantoniou6\Cerberus
+ * @property int $id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\CircleLinkHealth\Customer\Entities\Permission[] $perms
+ * @property-read int|null $perms_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\CircleLinkHealth\Customer\Entities\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Michalisantoniou6\Cerberus\CerberusRole whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 
 use Michalisantoniou6\Cerberus\Contracts\CerberusRoleInterface;
