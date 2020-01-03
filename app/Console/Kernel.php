8d444105721aec4058d53ac598b724a8e62ab36d
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-01-03 11:24:44 -0600

    Changes schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -80,9 +80,9 @@ class Kernel extends ConsoleKernel
 
         $schedule->command(RescheduleMissedCalls::class)->dailyAt('00:01')->onOneServer();
 
-        $schedule->command(TuneScheduledCalls::class)->dailyAt('00:05')->onOneServer();
-
         $schedule->command(CheckEnrolledPatientsForScheduledCalls::class)->dailyAt('00:10')->onOneServer();
+
+        $schedule->command(TuneScheduledCalls::class)->dailyAt('00:15')->onOneServer();
 
         //family calls will be scheduled in RescheduleMissedCalls
         //$schedule->command(SyncFamilialCalls::class)->dailyAt('00:30')->onOneServer();
@@ -101,7 +101,7 @@ class Kernel extends ConsoleKernel
             ->dailyAt('22:30')->onOneServer();
 
         $schedule->command(QueueResetAssignedCareAmbassadorsFromEnrollees::class)
-            ->dailyAt('00:30')->onOneServer();
+            ->dailyAt('00:40')->onOneServer();
 
         $schedule->command(GetCcds::class)
             ->dailyAt('03:00')->onOneServer();
@@ -124,7 +124,7 @@ class Kernel extends ConsoleKernel
             ->cron('45 0 1 * *')->onOneServer();
 
         $schedule->command(AttachBillableProblemsToLastMonthSummary::class)
-            ->dailyAt('00:15')->onOneServer();
+            ->dailyAt('00:25')->onOneServer();
 
 //        $schedule->command(
 //            SendCareCoachInvoices::class,
@@ -169,7 +169,7 @@ class Kernel extends ConsoleKernel
 //                 ->everyThirtyMinutes()
 //                 ->withoutOverlapping()->onOneServer();
 
-        $schedule->command(NursesPerformanceDailyReport::class)->dailyAt('00:05')->onOneServer();
+        $schedule->command(NursesPerformanceDailyReport::class)->dailyAt('00:03')->onOneServer();
 
         $schedule->command(OverwriteNBIImportedData::class)->everyThirtyMinutes()->onOneServer();
 
