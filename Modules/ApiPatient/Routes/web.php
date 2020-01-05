623613b08923522077ccbbca2a50169bf6dba8fb
kakoushias <kakoushias@gmail.com>
2020-01-05 20:45:55 +0200

    CPM-1838 adds permission

--- Modules/ApiPatient/Routes/web.php
+++ Modules/ApiPatient/Routes/web.php
@@ -26,7 +26,7 @@ Route::group(['prefix' => 'api'], function () {
                         Route::delete('ccd/{ccdProblemId}', 'CcdProblemController@destroy')->middleware('permission:patientProblem.delete');
 
                         //todo: add permissions
-                        Route::post('attest-summary-problems', 'AttestedConditionsController@update');
+                        Route::post('attest-summary-problems', 'AttestedConditionsController@update')->middleware('permission:attestedProblems.update,attestedProblems.delete');
                     }
                 );
 
