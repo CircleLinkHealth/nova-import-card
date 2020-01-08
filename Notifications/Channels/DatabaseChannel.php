57c2ba1ca9b59cbe6ab933a6a3004eae23057a74
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-01-08 11:39:33 -0600

    Refactoring

--- Notifications/Channels/DatabaseChannel.php
+++ Notifications/Channels/DatabaseChannel.php
@@ -6,68 +6,33 @@
 
 namespace CircleLinkHealth\Core\Notifications\Channels;
 
-use Carbon\Carbon;
 use CircleLinkHealth\Core\Entities\AnonymousNotifiable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\DB;
-use RuntimeException;
+use Illuminate\Notifications\Channels\DatabaseChannel as LaravelDatabaseChannel;
 
-class DatabaseChannel
+class DatabaseChannel extends LaravelDatabaseChannel
 {
     /**
-     * Send the given notification.
+     * Build an array payload for the DatabaseNotification Model.
      *
-     * @param mixed $notifiable
-     * @param \Illuminate\Notifications\Notification $notification
-     *
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function send($notifiable, Notification $notification)
-    {
-
-        $args = [
-            'id'      => $notification->id,
-            'type'    => get_class($notification),
-            'data'    => $this->getData($notifiable, $notification),
-            'read_at' => null,
-        ];
-
-        if (method_exists($notification, 'getAttachment')) {
-            $args['attachment_id']   = $notification->getAttachment()->id;
-            $args['attachment_type'] = get_class($notification->getAttachment());
-        }
-
-        if (is_a($notifiable, AnonymousNotifiable::class)) {
-            $args['notifiable_id']   = $notifiable->id;
-            $args['notifiable_type'] = AnonymousNotifiable::class;
-        }
-        return $notifiable->routeNotificationFor('database')->create($args);
-    }
-
-    /**
-     * Get the data for the notification.
-     *
-     * @param mixed $notifiable
-     * @param \Illuminate\Notifications\Notification $notification
-     *
-     * @throws \RuntimeException
-     *
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
      * @return array
      */
-    protected function getData($notifiable, Notification $notification)
+    protected function buildPayload($notifiable, Notification $notification)
     {
-        if (method_exists($notification, 'toDatabase')) {
-            return is_array($data = $notification->toDatabase($notifiable))
-                ? $data
-                : $data->data;
+        $payload = parent::buildPayload($notifiable, $notification);
+        
+        if (method_exists($notification, 'getAttachment')) {
+            $payload['attachment_id']   = $notification->getAttachment()->id;
+            $payload['attachment_type'] = get_class($notification->getAttachment());
         }
-
-        if (method_exists($notification, 'toArray')) {
-            return $notification->toArray($notifiable);
+    
+        if ($notifiable instanceof AnonymousNotifiable) {
+            $payload['notifiable_id']   = $notifiable->id;
+            $payload['notifiable_type'] = AnonymousNotifiable::class;
         }
-
-        throw new RuntimeException(
-            'Notification is missing toDatabase / toArray method.'
-        );
+        
+        return $payload;
     }
 }
