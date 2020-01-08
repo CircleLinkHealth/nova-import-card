497e51d0fa93f4a1a0492bd5eda423530ded5492
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-01-07 18:22:22 -0600

    Return more CCDA sections

--- nodejs/lib/parser.ts
+++ nodejs/lib/parser.ts
@@ -14,10 +14,19 @@ export function parse(xmlBody: string): BluebuttonParseResponse {
                 type: parsedCcd.type,
                 document: parsedCcd.data.document,
                 allergies: parsedCcd.data.allergies,
+                care_plan: parsedCcd.data.care_plan,
+                chief_complaint: parsedCcd.data.chief_complaint,
                 demographics: parsedCcd.data.demographics,
+                encounters: parsedCcd.data.encounters,
+                functional_statuses: parsedCcd.data.functional_statuses,
+                immunizations: parsedCcd.data.immunizations,
+                instructions: parsedCcd.data.instructions,
+                results: parsedCcd.data.results,
                 medications: parsedCcd.data.medications,
                 payers: parsedCcd.data.payers,
                 problems: parsedCcd.data.problems,
+                procedures: parsedCcd.data.procedures,
+                smoking_status: parsedCcd.data.smoking_status,
                 vitals: parsedCcd.data.vitals
             }
         };
