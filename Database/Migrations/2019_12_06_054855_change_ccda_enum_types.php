c3f89898fbb81f57148c4aa2ed2268d1fbe59ae9
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-01-13 22:17:18 -0600

    All pending

--- Database/Migrations/2019_12_06_054855_change_ccda_enum_types.php
+++ Database/Migrations/2019_12_06_054855_change_ccda_enum_types.php
@@ -4,7 +4,7 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
-use App\Models\MedicalRecords\Ccda;
+use CircleLinkHealth\CarePlanModels\Entities\Ccda;
 use Illuminate\Database\Migrations\Migration;
 
 class ChangeCcdaEnumTypes extends Migration
