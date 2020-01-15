289711bb9786f27bf07938b87699b4801774617c
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-01-15 00:52:50 -0600

    trial

--- config/database.php
+++ config/database.php
@@ -61,6 +61,12 @@ if (getenv('REDIS_URL')) {
     putenv('REDIS_PASSWORD='.$redisUrl['pass']);
 }
 
+$mysqlDBName = env('DB_DATABASE', null);
+
+if (!$mysqlDBName) {
+    $mysqlDBName = snake_case(getenv('HEROKU_BRANCH'));
+}
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -105,7 +111,7 @@ return [
             'driver'         => 'mysql',
             'host'           => env('DB_HOST', '127.0.0.1'),
             'port'           => env('DB_PORT', '3306'),
-            'database'       => env('DB_DATABASE', 'forge'),
+            'database'       => $mysqlDBName,
             'username'       => env('DB_USERNAME', 'forge'),
             'password'       => env('DB_PASSWORD', ''),
             'unix_socket'    => env('DB_SOCKET', ''),
