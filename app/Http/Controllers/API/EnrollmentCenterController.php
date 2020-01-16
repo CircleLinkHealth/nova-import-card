9d70fa46dd13356116df53cd1a067e67ffa6374a
kakoushias <kakoushias@gmail.com>
2020-01-16 20:22:41 +0200

    CPM-1792 adds things

--- app/Http/Controllers/API/EnrollmentCenterController.php
+++ app/Http/Controllers/API/EnrollmentCenterController.php
@@ -13,23 +13,8 @@ class EnrollmentCenterController extends ApiController
 {
     public function getSuggestedFamilyMembers($enrolleeId)
     {
-        $suggestedFamilyMembers = EnrolleeFamilyMemberService::get($enrolleeId)
-                                                             ->map(function (Enrollee $e) {
-                                                                 return [
-                                                                     'id'         => $e->id,
-                                                                     'first_name' => $e->first_name,
-                                                                     'last_name'  => $e->last_name,
-                                                                     'phones'     => [
-                                                                         'value' => $e->getPhonesAsString(),
-                                                                     ],
-                                                                     'addresses'  => [
-                                                                         'value' => $e->getAddressesAsString(),
-                                                                     ],
-                                                                 ];
-                                                             });
-
         return $this->json([
-            'suggested_family_members' => $suggestedFamilyMembers,
+            'suggested_family_members' => EnrolleeFamilyMemberService::get($enrolleeId),
         ]);
     }
 }
--- app/Services/Enrollment/EnrolleeFamilyMemberService.php
+++ app/Services/Enrollment/EnrolleeFamilyMemberService.php
@@ -7,20 +7,27 @@
 namespace App\Services\Enrollment;
 
 
+use App\Http\Requests\Request;
 use CircleLinkHealth\Eligibility\Entities\Enrollee;
 
 class EnrolleeFamilyMemberService
 {
+    /**
+     * @var integer
+     */
     protected $enrolleeId;
 
+    /**
+     * @var Enrollee
+     */
     protected $enrollee;
 
-    public function __construct($enrolleeId)
+    public function __construct(integer $enrolleeId)
     {
         $this->enrolleeId = $enrolleeId;
     }
 
-    public static function get($enrolleeId)
+    public static function get(integer $enrolleeId)
     {
 
         return (new static($enrolleeId))->generate();
@@ -32,8 +39,8 @@ class EnrolleeFamilyMemberService
 
         $query = $this->constructQuery();
 
-        return $query->take(20)
-                     ->get();
+        return $this->formatForView($query->take(20)
+                     ->get());
     }
 
     private function getModel()
@@ -43,7 +50,6 @@ class EnrolleeFamilyMemberService
 
     private function constructQuery()
     {
-
         $phonesQuery = Enrollee::where('id', '!=', $this->enrolleeId)
                                ->searchPhones($this->enrollee->getPhonesE164AsString());
 
@@ -51,6 +57,35 @@ class EnrolleeFamilyMemberService
                                   ->searchAddresses($this->enrollee->getAddressesAsString());
 
         return $phonesQuery->union($addressesQuery);
+    }
+
+    private function formatForView($family){
+        return $family->map(function (Enrollee $e) {
+            return [
+                'id'         => $e->id,
+                'first_name' => $e->first_name,
+                'last_name'  => $e->last_name,
+                'phones'     => [
+                    'value' => $e->getPhonesAsString(),
+                ],
+                'addresses'  => [
+                    'value' => $e->getAddressesAsString(),
+                ],
+            ];
+        });
+    }
+
+    public static function attach($enrolleeId, Request $request)
+    {
+        if (! $request->has('confirmed_family_members')) {
+            return false;
+        }
+
+        return (new static($enrolleeId))->attachFamilyMembers($request);
+    }
+
+    private function attachFamilyMembers(Request $request){
+        $this->enrollee->attachFamilyMembers($request->input('confirmed_family_members'));
     }
 
 }
