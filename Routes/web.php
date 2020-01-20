bbb9f2b857736a9c36dac6769f4d776b002094e5
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-01-20 13:39:38 -0600

    When will the shuffling stop

--- Routes/web.php
+++ Routes/web.php
@@ -5,5 +5,46 @@
  */
 
 Route::prefix('eligibility')->group(function () {
-    Route::get('/', 'EligibilityController@index');
+    Route::post('process-eligibility/drive/', [
+        'uses' => 'ProcessEligibilityController@fromGoogleDrive',
+        'as'   => 'process.eligibility.google.drive',
+    ])->middleware(['auth', 'role:administrator']);
+    
+    Route::get(
+        'process-eligibility/local-zip-from-drive/{dir}/{practiceName}/{filterLastEncounter}/{filterInsurance}/{filterProblems}',
+        [
+            'uses' => 'ProcessEligibilityController@fromGoogleDriveDownloadedLocally',
+            'as'   => 'process.eligibility.local.zip',
+        ]
+    )->middleware(['auth', 'role:administrator']);
+    
+    
+    Route::group([
+                     'middleware' => [
+                         'auth',
+                         'permission:admin-access',
+                     ],
+                     'prefix' => 'admin',
+                 ], function () {
+        Route::get(
+            'eligible-lists/phoenix-heart',
+            'WelcomeCallListController@makePhoenixHeartCallList'
+        )->middleware('permission:batch.create');
+    
+        Route::post('make-welcome-call-list', [
+            'uses' => 'WelcomeCallListController@makeWelcomeCallList',
+            'as'   => 'make.welcome.call.list',
+        ])->middleware('permission:batch.create');
+    });
+    
+});
+
+Route::group([
+                 'middleware' => [
+                     'auth',
+                     'permission:ccd-import',
+                 ],
+                 'prefix' => 'ccd-importer',
+             ], function () {
+    Route::post('demographics', 'DemographicsImportsController@store');
 });
