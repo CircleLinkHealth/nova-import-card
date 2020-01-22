80935498f69b0cd106a118fa166a3faa7bbbe6e1
Antonis Antoniou <kountouris7@gmail.com>
2020-01-22 21:47:50 +0200

    CPM-1919_ReEnrollement - sending SMS and EMAIL

---
+++ Database/Migrations/2020_01_22_143515_email_subscriptions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EmailSubscriptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unsubscriptions_notification_mail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedSmallInteger('user_id');
+            $table->string('notification_type');
+            $table->date('unsubscribed_at');
+            $table->string('channel');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
