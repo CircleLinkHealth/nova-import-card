180f82a1c52a87752bf3e70ef8ec95a12a405092
Antonis Antoniou <kountouris7@gmail.com>
2020-01-29 23:48:42 +0200

    CPM-1763_V2 Merged Development

--- app/CallView.php
+++ app/CallView.php
@@ -44,7 +44,6 @@ use CircleLinkHealth\Core\Filters\Filterable;
  * @property string|null $ccm_status
  * @property string|null $patient_nurse_id
  * @property string|null $patient_nurse
- *
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView filter(\App\Filters\QueryFilters $filters)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView newQuery()
@@ -76,15 +75,11 @@ use CircleLinkHealth\Core\Filters\Filterable;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView whereTimezone($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView whereType($value)
  * @mixin \Eloquent
- *
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView whereCcmStatus($value)
- *
  * @property int $asap
- *
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView whereAsap($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView wherePatientNurse($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\CallView wherePatientNurseId($value)
- *
  * @property \CircleLinkHealth\Revisionable\Entities\Revision[]|\Illuminate\Database\Eloquent\Collection $revisionHistory
  * @property int|null                                                                                    $revision_history_count
  */
