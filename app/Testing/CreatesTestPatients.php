5f46005f6c13c2e669224a19cf9f900e453e3cf8
kakoushias <kakoushias@gmail.com>
2020-02-03 17:12:06 +0200

    CPM-1838 fixes patient seeder

---
+++ app/Testing/CreatesTestPatients.php
@@ -0,0 +1,150 @@
+<?php
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App\Testing;
+
+
+use App\CLH\Repositories\UserRepository;
+use Carbon\Carbon;
+use CircleLinkHealth\Customer\Entities\CarePerson;
+use CircleLinkHealth\Customer\Entities\Practice;
+use CircleLinkHealth\Customer\Entities\Role;
+use CircleLinkHealth\Customer\Entities\User;
+use CircleLinkHealth\SharedModels\Entities\CpmProblem;
+use Faker\Factory as Faker;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+abstract class CreatesTestPatients
+{
+    protected $faker;
+
+    public function __construct()
+    {
+        $this->faker = Faker::create();
+    }
+
+    abstract protected function data();
+
+    /**
+     * @param int $i
+     *
+     * Generate dummy medication names for users
+     *
+     * @return array
+     */
+    protected function testMedications($i = 25)
+    {
+        $medications = [];
+        while ($i > 0) {
+            $medications[] = ['name' => 'med' . ' ' . $i];
+            --$i;
+        }
+
+        return $medications;
+    }
+
+    protected function getPracticeId()
+    {
+        $demoPractice = Practice::whereName('demo');
+
+        return $demoPractice->exists()
+            ? $demoPractice->first()->id
+            : Practice::firstOrFail()->id;
+    }
+
+    protected function getProvider($id = null): User
+    {
+        if ($id && User::whereId($id)->exists()) {
+            return User::find($id);
+        }
+
+        return User::ofType('provider')->firstOrFail();
+    }
+
+    public function create()
+    {
+        $repo     = new UserRepository();
+        $role     = Role::whereName('participant')->firstOrFail();
+        $problems = CpmProblem::get();
+
+        foreach ($this->data() as $patientData) {
+
+            $this->deleteUsersIfExist($patientData['email']);
+
+            $patientData['roles'] = [$role->id];
+            $bag                  = new ParameterBag($patientData);
+
+            $user = $repo->createNewUser(new User(), $bag);
+
+            $this->createAndAttachProblems($problems, $user, $patientData);
+
+            $user->ccdMedications()->createMany($this->testMedications($patientData['medications']));
+
+            $user->careTeamMembers()->create([
+                'member_user_id' => $patientData['billing_provider_id'],
+                'type'           => CarePerson::BILLING_PROVIDER,
+            ]);
+
+            $this->createSummaries($user);
+        }
+    }
+
+    private function createAndAttachProblems($problems, User $user, $patientData)
+    {
+
+        $userProblems = in_array('all', $patientData['conditions'])
+            ? $problems
+            : $problems->whereIn('name', $patientData['conditions']);
+
+        $ccdProblems = [];
+
+        foreach ($userProblems as $problem) {
+            $ccdProblems[] = [
+                'is_monitored'   => 1,
+                'name'           => $problem->name,
+                'cpm_problem_id' => $problem->id,
+            ];
+        }
+        $user->ccdProblems()->createMany($ccdProblems);
+    }
+
+    private function createSummaries($user)
+    {
+        $now = Carbon::now();
+
+        $user->load('ccdProblems');
+
+        $problem1Id = $user->ccdProblems->random()->id;
+        $problem2Id = $user->ccdProblems->where('id', '!=', $problem1Id)->random()->id;
+
+        for ($i = 9; $i > 0; $i--) {
+            $user->patientSummaries()->updateOrCreate([
+                'month_year' => $now->copy()->firstOfMonth()->subMonth($i),
+            ], [
+                'total_time'             => 1400,
+                'ccm_time'               => 1400,
+                'no_of_calls'            => 2,
+                'no_of_successful_calls' => 1,
+                'needs_qa'               => 1,
+                'problem_1'              => $problem1Id,
+                'problem_2'              => $problem2Id,
+                'approved'               => 1,
+                'actor_id'               => User::ofType('administrator')->first()->id,
+            ]);
+        }
+    }
+
+    private function deleteUsersIfExist($email)
+    {
+        $users = User::whereEmail($email)->get();
+
+        if ($users->count() > 0) {
+            foreach ($users as $user) {
+                $user->patientSummaries()->delete();
+                $user->forceDelete();
+            }
+        }
+    }
+}
