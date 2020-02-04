d85a63366b8bea237a453566a876fe2b8ba27217
Pangratios Cosma <pagcosma@gmail.com>
2020-02-04 08:56:06 +0200

    CPM-1857 navbar dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,71 +16,10 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
-        <div class="container">
-            <a class="navbar-title" href="{{ url('/') }}">
-                <img src="{{ url('/images/clh_logo_lt.png') }}"
-                     alt="CarePlan Manager" width='50px'
-                     style="position:relative;">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                @if(!Route::is('login'))
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Provider Login') }}</a>
-                            </li>
-                        @else
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                  style="display: none;">
-                                @csrf
-                            </form>
-                        <!--
-                    dropdown not working for some reason
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->display_name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                            </a>
-                        </div>
-                    </li>
--->
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" style="cursor:default">
-                                    {{ Auth::user()->display_name }}
-                                </a>
-                            </li>
-                            <li>
-                                <a class="nav-link" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                            </li>
-                        @endguest
-                    </ul>
-                @endif
-            </div>
-        </div>
-    </nav>
+    <navigation user-display-name="{{ optional(Auth::user())->display_name }}"
+                :is-login-route="@json(Route::is('login'))"
+                :is-guest="@json(Auth::guest())">
+    </navigation>
 
     <main class="py-4">
         @yield('content')
