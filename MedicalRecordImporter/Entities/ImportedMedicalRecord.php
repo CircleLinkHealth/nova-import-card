8c49ca62f1299a94d55c082b5961e85ef1067897
kakoushias <kakoushias@gmail.com>
2020-02-10 20:37:54 +0200

    CPM-1937 adds FPDF changes pdf class

--- MedicalRecordImporter/Entities/ImportedMedicalRecord.php
+++ MedicalRecordImporter/Entities/ImportedMedicalRecord.php
@@ -21,6 +21,7 @@ use CircleLinkHealth\Customer\Entities\Patient;
 use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Customer\Entities\User;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
 /**
  * CircleLinkHealth\Eligibility\MedicalRecordImporter\Entities\ImportedMedicalRecord.
@@ -74,6 +75,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class ImportedMedicalRecord extends \CircleLinkHealth\Core\Entities\BaseModel implements ImportedMedicalRecordInterface
 {
+    use HasMediaTrait;
     use MedicalRecordIdAndTypeTrait;
     use SoftDeletes;
 
