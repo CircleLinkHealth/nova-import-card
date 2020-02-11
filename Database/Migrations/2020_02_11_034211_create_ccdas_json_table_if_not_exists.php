38bd448f22ea98ce6579fe3ca90497854c5f1f8c
Pangratios Cosma <pagcosma@gmail.com>
2020-02-11 10:56:24 +0200

    create ccdas-json table if not exists

---
+++ Database/Migrations/2020_02_11_034211_create_ccdas_json_table_if_not_exists.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCcdasJsonTableIfNotExists extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if ( ! Schema::hasTable('ccdas-json')) {
+            Schema::create('ccdas-json', function (Blueprint $table) {
+                $table->bigIncrements('id')->unsigned();
+                $table->integer('ccda_id')->nullable(false);
+                $table->json('result')->nullable(true);
+                $table->string('error')->nullable(true);
+                $table->string('status')->nullable(false);
+                $table->integer('duration_seconds')->nullable(false)->default(0);
+                $table->timestamps();
+            });
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ccdas-json');
+    }
+}
