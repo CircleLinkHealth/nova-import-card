53343be4ee32862fa1253396e2caa4ddf898f19b
kakoushias <kakoushias@gmail.com>
2020-02-11 13:21:10 +0200

    CPM-1937 parse pdf string

--- app/OCR/UPGPdfCarePlan.php
+++ app/OCR/UPGPdfCarePlan.php
@@ -6,9 +6,7 @@
 namespace App\OCR;
 
 
-use CircleLinkHealth\Customer\Entities\Media;
 use setasign\Fpdi\Fpdi;
-use setasign\Fpdi\PdfReader\PdfReader;
 use Spatie\PdfToText\Pdf;
 
 
@@ -35,36 +33,100 @@ class UPGPdfCarePlan
 
     private function getText()
     {
-        //crop PDF
-        //set .env var and config var
-
         $pdf = new Fpdi();
 
         $pageCount = $pdf->setSourceFile($this->filePath);
         $pdf->AddPage();
 
-        for ($n = 1; $n <= $pageCount; $n ++){
+        for ($n = 1; $n <= $pageCount; $n++) {
             $tplId = $pdf->importPage($n);
 
             $pdf->useTemplate($tplId, 0, -60, 210, 400);
             $pdf->AddPage();
         }
 
-
         $pdf->Output(storage_path('testarw2.pdf'), 'F');
 
-
-        Pdf::getText(storage_path('testarw2.pdf'), '/usr/local/bin/pdftotext', ['layout', 'nopgbrk']);
+        //set .env var and config var
+        $this->string = Pdf::getText(storage_path('testarw2.pdf'), '/usr/local/bin/pdftotext', ['layout', 'nopgbrk']);
 
         return $this;
     }
 
     private function parseString()
     {
-        $this->carePlan = $this->string;
-
 //        $this->carePlan = explode('\n', $this->string);
-//        $this->carePlan = preg_split("/[\n]/", $this->string);
+        $array = collect(preg_split("/[\n]/", $this->string))->filter()->values()->all();
+
+        $carePlan = [];
+
+        //to add details dynamically for each checkpoint make these arrays
+        $checkpoints = [
+            [
+                'First Name:',
+                'first_name',
+                'uc_words',
+            ],
+            ['Last Name:', 'last_name', 'uc_words'],
+            ['Visit Date:', 'visit_date', 'date'],
+            ['Medical Record #:', 'mrn', 'int'],
+            ['Address:', 'address'],
+            ['Date of Birth:', 'dob', 'date'],
+            ['Sex:', 'sex'],
+            ['Phones:', 'phones', ['Home:', 'Cell:', 'Other:']],
+            ['Dx:', 'conditions'],
+            ['Active Problems:', 'instructions'],
+            ['Services Ordered:', 'chargeable_services'],
+        ];
+
+        $currentCheckpoint = 0;
+        for ($n = 0; $n <= count($array); $n++) {
+            //check if string is empty
+            if ( ! isset($array[$n])) {
+                //can i do this?
+                break;
+            }
+
+            $string = $array[$n];
+
+            if (empty(trim($string))) {
+                //can i do this?
+                continue;
+            }
+
+            $checkpoint = $checkpoints[$currentCheckpoint];
+
+            //add key as name
+            $checkpointKey = $checkpoint[0];
+
+            //remove
+            if (str_contains($string, $checkpointKey)) {
+                $string = trim(str_replace($checkpointKey, ' ', $string));
+
+                if (empty($string)) {
+                    continue;
+                }
+            }
+            // checkpoints will be the set sections - to know where to put the variable data. At the end or start of the loop recognize if we are going to next checkpoint by checking the next?
+
+            //concat later? merge strings? (give other key) basically make all items arrays that contain all the values at the first level
+            $carePlan[$checkpoint[1]][] = $string;
+
+            $nextCheckpoint = $currentCheckpoint + 1;
+            if (isset($array[$n + 1]) && isset($checkpoints[$nextCheckpoint])) {
+                if (str_contains($array[$n + 1], $checkpoints[$nextCheckpoint][0])) {
+                    $currentCheckpoint = $nextCheckpoint;
+                }
+            }
+
+            //then we talk about transforming, recognizing categorization and validating within a certain section
+
+            //when removing checkpoint key, trim string, check if empty, and proceed
+        }
+
+        $this->carePlan = $carePlan;
+
+        $x = 1;
 
         return $this;
     }
@@ -73,42 +135,13 @@ class UPGPdfCarePlan
     {
 
 
-
-
-//        $pageCount = $pdf->setSourceFile($this->filePath);
-//
-//        $width = $pdf->GetPageWidth();
-//        $height = 0;
-//
-//        $_x = $x = 10;
-//        $_y = $y = 10;
-//
-//        $pdf->AddPage();
-//        for ($n = 1; $n <= $pageCount; $n++) {
-//            $pageId = $pdf->importPage($n);
-//
-//            $size = $pdf->useImportedPage($pageId, $x, $y, $width);
-//            $pdf->Rect($x, $y, $size['width'], $size['height']);
-//            $height = max($height, $size['height']);
-//            if ($n % 2 == 0) {
-//                $y += $height + 10;
-//                $x = $_x;
-//                $height = 0;
-//            } else {
-//                $x += $width + 10;
-//            }
-//
-//
-//                $pdf->AddPage();
-//                $x = $_x;
-//                $y = $_y;
-//
-//        }
-
-
-
         return $this->string;
 
+    }
+
+    private function prepareField($field)
+    {
+        return ucwords(strtolower(trim($field)));
     }
 
 }
