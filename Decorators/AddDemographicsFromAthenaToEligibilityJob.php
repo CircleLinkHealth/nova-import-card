8da6a6d95bc9098289b508a81ec21144a8eef8fc
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-02-13 09:58:49 -0600

    Fixes

--- Decorators/AddDemographicsFromAthenaToEligibilityJob.php
+++ Decorators/AddDemographicsFromAthenaToEligibilityJob.php
@@ -36,6 +36,8 @@ class AddDemographicsFromAthenaToEligibilityJob
         TargetPatient $targetPatient,
         Ccda $ccda
     ): EligibilityJob {
+        $data                         = $eligibilityJob->data;
+        
         if ( ! array_key_exists('patient_demographics', $eligibilityJob->data)) {
             $demographics = $this->athenaApiImplementation->getDemographics(
                 $targetPatient->ehr_patient_id,
@@ -43,23 +45,24 @@ class AddDemographicsFromAthenaToEligibilityJob
             );
             
             if (is_array($demographics) && array_key_exists(0, $demographics)) {
-                $data = $eligibilityJob->data;
                 $data['patient_demographics'] = $demographics[0];
             }
         }
         
-        if ($provId = $data['patient_demographics']['primaryproviderid']) {
-            $provider = $this->athenaApiImplementation->getProvider($targetPatient->ehr_practice_id, $provId);
+        if ( ! array_key_exists('provider', $eligibilityJob->data) && array_key_exists('patient_demographics', $eligibilityJob->data)) {
+            if ($provId = $data['patient_demographics']['primaryproviderid']) {
+                $provider = $this->athenaApiImplementation->getProvider($targetPatient->ehr_practice_id, $provId);
+            }
+            
+            if (is_array($provider) && array_key_exists(0, $provider)) {
+                $data['referring_provider_name'] = $ccda->referring_provider_name = $provider[0]['displayname'];
+                $data['provider']                = $provider[0];
+                $ccda->save();
+            }
+            
+            $eligibilityJob->data = $data;
+            $eligibilityJob->save();
         }
-        
-        if (is_array($provider) && array_key_exists(0, $provider)) {
-            $data['referring_provider_name'] = $ccda->referring_provider_name = $provider[0]['displayname'];
-            $data['provider'] = $provider[0];
-            $ccda->save();
-        }
-    
-        $eligibilityJob->data = $data;
-        $eligibilityJob->save();
         
         return $eligibilityJob;
     }
