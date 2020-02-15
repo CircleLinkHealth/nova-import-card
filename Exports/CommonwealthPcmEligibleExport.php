66886f34c3d96c920033c7cd797a66232f36c179
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-02-15 17:08:32 -0600

    changes query size

--- Exports/CommonwealthPcmEligibleExport.php
+++ Exports/CommonwealthPcmEligibleExport.php
@@ -12,8 +12,9 @@ use App\Exports\PracticeReports\PracticeReportInterface;
 use CircleLinkHealth\Eligibility\Entities\EligibilityJob;
 use CircleLinkHealth\Eligibility\Entities\PcmProblem;
 use Illuminate\Database\Eloquent\Builder;
+use Maatwebsite\Excel\Concerns\WithCustomQuerySize;
 
-class CommonwealthPcmEligibleExport extends PracticeReportInterface
+class CommonwealthPcmEligibleExport extends PracticeReportInterface implements WithCustomQuerySize
 {
     const COMMONWEALTH_PAIN_PRACTICE_ID = 232;
     
@@ -134,5 +135,19 @@ class CommonwealthPcmEligibleExport extends PracticeReportInterface
     public function mediaCollectionName(): string
     {
         return 'pcm_eligible_patients_report_from_all_batches';
+    }
+    
+    /**
+     * Queued exportables are processed in chunks; each chunk being a job pushed to the queue by the QueuedWriter.
+     * In case of exportables that implement the FromQuery concern, the number of jobs is calculated by dividing the $query->count() by the chunk size.
+     * Depending on the implementation of the query() method (eg. When using a groupBy clause), this calculation might not be correct.
+     *
+     * When this is the case, you should use this method to provide a custom calculation of the query size.
+     *
+     * @return int
+     */
+    public function querySize(): int
+    {
+        return 50;
     }
 }
