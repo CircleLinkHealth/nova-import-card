ff3af91a19882f5ed6f4d3e5d901d96f07337825
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-02-17 01:01:27 -0600

    Makes PdfMediable

--- app/Services/PhiMail/Incoming/Handlers/Pdf.php
+++ app/Services/PhiMail/Incoming/Handlers/Pdf.php
@@ -9,15 +9,43 @@
 namespace App\Services\PhiMail\Incoming\Handlers;
 
 
+use App\Exports\PracticeReports\Mediable;
 use Carbon\Carbon;
 
-class Pdf extends BaseHandler
+class Pdf extends BaseHandler implements Mediable
 {
     public function handle()
     {
-        $path = storage_path('dm_id_'.$this->dm->id.'_attachment_'.Carbon::now()->toAtomString()).'.pdf';
+        $this->storeAsMedia();
+    }
+    
+    public function mediaCollectionName() : string
+    {
+        return "dm_{$this->dm->id}_attachments_pdf";
+    }
+    
+    public function filename() : string
+    {
+        return 'dm_id_'.$this->dm->id.'_attachment_'.Carbon::now()->toAtomString().'.pdf';
+    }
+    
+    private function storeAsMedia()
+    {
+        $path = $this->fullPath();
+    
         file_put_contents($path, $this->showRes->data);
+    
         $this->dm->addMedia($path)
-                 ->toMediaCollection("dm_{$this->dm->id}_attachments_pdf");
+                 ->toMediaCollection($this->mediaCollectionName());
+    }
+    
+    /**
+     * Get the fullpath.
+     *
+     * @return string
+     */
+    public function fullPath(): string
+    {
+        return storage_path($this->filename());
     }
 }
