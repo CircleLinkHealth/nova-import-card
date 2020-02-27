224ef92efbbd8466eb474b4af198af841626d1a3
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-02-27 17:53:42 -0600

    CPM-2078: Create PCM List

--- Decorators/PcmChargeableServices.php
+++ Decorators/PcmChargeableServices.php
@@ -23,28 +23,29 @@ class PcmChargeableServices implements MedicalRecordDecorator
         $data = $eligibilityJob->data;
         $pcmProblems = [];
     
-        $problems = EligibilityChecker::getProblemsForEligibility($eligibilityJob);
-    
-        if ($problems) {
-            $eligibilityJob->loadMissing('batch');
-            
-            /** @var Problem $p */
-            foreach ($problems as $p) {
-                if ( ! is_a($p, Problem::class)) {
-                    continue;
-                }
-            
-                $pcmProblemId = $this->matchPcmProblem($eligibilityJob, $p);
-            
-                if ($pcmProblemId) {
-                    $pcmProblems[] = $pcmProblemId->id;
-                }
-            
-                if ( ! empty($pcmProblems = array_unique(array_filter($pcmProblems)))) {
-                    $this->addPcmCodeAndProblems($data, $pcmProblems);
-                    $eligibilityJob->data = $data;
-                }
+        $eligibilityJob->loadMissing('batch');
+        
+        /** @var Problem $p */
+        foreach (EligibilityChecker::getProblemsForEligibility($eligibilityJob) as $p) {
+            if ( ! is_a($p, Problem::class)) {
+                continue;
             }
+        
+            $pcmProblemId = $this->matchPcmProblem($eligibilityJob, $p);
+        
+            if ($pcmProblemId) {
+                $pcmProblems[] = $pcmProblemId->id;
+            }
+        }
+        
+        if (empty($pcmProblems)) {
+            return $eligibilityJob;
+        }
+    
+        $this->addPcmCodeAndProblems($data, $pcmProblems);
+        
+        if ($eligibilityJob->isDirty()) {
+            $eligibilityJob->save();
         }
         
         return $eligibilityJob;
