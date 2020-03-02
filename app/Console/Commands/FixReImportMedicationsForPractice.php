a03d9511c1609b3b8a58d1ba732c9b6d1a75b3b1
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-02 17:01:30 -0600

    CPM-2087

--- app/Console/Commands/FixReImportMedicationsForPractice.php
+++ app/Console/Commands/FixReImportMedicationsForPractice.php
@@ -1,34 +1,32 @@
 <?php
 
-/*
- * This file is part of CarePlan Manager by CircleLink Health.
- */
-
 namespace App\Console\Commands;
 
-use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Eligibility\MedicalRecordImporter\CarePlanHelper;
 use CircleLinkHealth\Eligibility\MedicalRecordImporter\Entities\ImportedMedicalRecord;
 use CircleLinkHealth\Eligibility\MedicalRecordImporter\Sections\Medications;
 use Illuminate\Console\Command;
 
-class AddPracticesToSearchDriver extends Command
+class FixReImportMedicationsForPractice extends Command
 {
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Imports data from Practices table to search driver.';
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'search:import-practices';
+    protected $signature = 'reimport:medications {practiceId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'reImport Medications for all patients for a practice.';
 
     /**
      * Create a new command instance.
+     *
+     * @return void
      */
     public function __construct()
     {
@@ -42,7 +40,7 @@ class AddPracticesToSearchDriver extends Command
      */
     public function handle()
     {
-        ImportedMedicalRecord::wherePracticeId(228)->with('patient')->has('patient')->chunkById(50, function ($imrs){
+        ImportedMedicalRecord::wherePracticeId($this->argument('practiceId'))->with('patient')->has('patient')->chunkById(50, function ($imrs){
             foreach ($imrs as $imr) {
                 $this->warn("fixing patient:{$imr->patient->id}");
                 $m = (new Medications($imr->medical_record_id, $imr->medical_record_type, $imr))->import($imr->medical_record_id, $imr->medical_record_type, $imr);
