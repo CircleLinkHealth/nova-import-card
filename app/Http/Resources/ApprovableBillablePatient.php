e38d0b82053b74872278ebfabdcf237672edb094
kakoushias <kakoushias@gmail.com>
2020-03-04 00:20:43 +0200

    CPM-2079 fix for this month

--- app/Http/Resources/ApprovableBillablePatient.php
+++ app/Http/Resources/ApprovableBillablePatient.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Resources\Json\Resource;
 
 class ApprovableBillablePatient extends Resource
 {
+    const ATTACH_DEFAULT_PROBLEMS_FOR_MONTH = '2020-03-01';
+
     public function allCcdProblems(User $patient)
     {
         return $patient->ccdProblems->map(function ($prob) {
@@ -44,24 +46,38 @@ class ApprovableBillablePatient extends Resource
             'patient' => $this->patient->id,
         ]);
 
-
-        $attestedBhiProblems = [];
-        if ($this->hasServiceCode('CPT 99484')) {
-            $attestedBhiProblems = $this->attestedProblems->where('cpmProblem.is_behavioral', '=', true)->pluck('id');
-        }
-
         $status = $this->closed_ccm_status;
         if (null == $status) {
             $status = $this->patient->patientInfo->ccm_status;
         }
 
-        if (Carbon::parse($this->month_year)->lt(Carbon::parse(PatientMonthlySummary::DATE_ATTESTED_CONDITIONS_ENABLED)) && $this->attestedProblems()->count() == 0) {
-            $attestedProblems = collect([
+        $attestedProblems = $this->attestedProblems->filter(function ($p){
+            return !! $p->icd10Code();
+        });
+
+        $shouldAttachDefaultProblems = Carbon::parse($this->month_year)->lte(Carbon::parse(self::ATTACH_DEFAULT_PROBLEMS_FOR_MONTH));
+
+        //get Ccm attested problems
+        if ($shouldAttachDefaultProblems && $attestedProblems->where('cpmProblem.is_behavioral', '=', false)->count() == 0) {
+            $attestedCcmProblems = collect([
                 optional($this->billableProblem1)->id,
                 optional($this->billableProblem2)->id,
             ])->filter()->toArray();
         } else {
-            $attestedCcmProblems = $this->attestedProblems->where('cpmProblem.is_behavioral', '=', false)->pluck('id');
+            $attestedCcmProblems = $attestedProblems->where('cpmProblem.is_behavioral', '=', false)->pluck('id');
+        }
+
+        $attestedBhiProblems = [];
+        //get Bhi attested Problems
+        if ($this->hasServiceCode('CPT 99484')) {
+            if ($shouldAttachDefaultProblems && $attestedProblems->where('cpmProblem.is_behavioral', '=', true)->count() == 0) {
+                $bhiProblem = $this->billableBhiProblems()->first();
+                $attestedBhiProblems = collect([
+                    $bhiProblem ? $bhiProblem->id : null
+                ])->filter()->toArray();
+            } else {
+                $attestedBhiProblems = $attestedProblems->where('cpmProblem.is_behavioral', '=', true)->pluck('id');
+            }
         }
 
         return [
