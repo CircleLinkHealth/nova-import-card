f3871d6fe8f17abd2ff3e042c4ada7d8d7e80e3f
Antonis Antoniou <kountouris7@gmail.com>
2020-03-12 19:06:26 +0200

    CPM-2128_FIXES_V3_review_after_michalis

--- resources/views/admin/reports/nursePerformance.blade.php
+++ resources/views/admin/reports/nursePerformance.blade.php
@@ -105,13 +105,11 @@
 
                         const hrsDefsColumn = 'td:eq(11)';
                         const hrsDeficit = parseInt(data['Hrs Deficit or Surplus']);
-
-                        if (hrsDeficit !== 0) {
-                            if (hrsDeficit < 0) {
-                                $(hrsDefsColumn, row).css('color', '#FA5353');
-                            } else {
-                                $(hrsDefsColumn, row).css('color', '#32C132');
-                            }
+                        if (hrsDeficit < 0) {
+                            $(hrsDefsColumn, row).css('color', '#FA5353');
+                        }
+                        if (hrsDeficit > 0) {
+                            $(hrsDefsColumn, row).css('color', '#32C132');
                         }
                     },
                     "columnDefs": [
