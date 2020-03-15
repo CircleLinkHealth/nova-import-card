6403ae5213920f50412bfa3abc0557122aa578f0
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-14 22:40:48 -0600

    cosmetic changes

--- resources/views/partials/viewCcdaButton.blade.php
+++ resources/views/partials/viewCcdaButton.blade.php
@@ -21,7 +21,7 @@
 
     @if(auth()->check() && reimportingPatientsIsEnabledForUser(auth()->id()))
         <div style="padding: 10px 0;">
-            <a href="{{ route('medical-record.patient.reimport', ['userId' => $patient->id, 'clearCcda' => 'on']) }}"
+            <a href="{{ route('medical-record.patient.reimport', ['userId' => $patient->id, 'flushCcd' => 'on']) }}"
                class="btn btn-danger btn-xs"
                style="font-size: 15px"
                onclick="return confirm('This will delete all Problems, Medications, Insurances, and Allergies, and re-import them using Importer (v3 beta). If something looks off, or Problems are imported wrongly due to CPM having false ICD codes. ICD codes were corrected, please continue.')"
