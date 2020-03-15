e83ac9048db1ce731740db543fa89aaaf16e0d2b
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-14 22:27:35 -0600

    Delete reimport button

--- resources/views/partials/viewCcdaButton.blade.php
+++ resources/views/partials/viewCcdaButton.blade.php
@@ -20,23 +20,13 @@
     @endif
 
     @if(auth()->check() && reimportingPatientsIsEnabledForUser(auth()->id()))
-        <div style="padding-top: 10px;">
-            <a href="{{ route('medical-record.patient.reimport', ['userId' => $patient->id]) }}"
-               class="btn btn-info btn-xs"
-               style="font-size: 15px"
-               onclick="return confirm('Importer (v3) (beta): CPM will search for the most recent medical record and reimport the patient. Only do this if the patient did not import correctly. CPM will notify you once reimporting finishes.')"
-            >
-                Reimport (beta)
-            </a>
-        </div>
-
         <div style="padding: 10px 0;">
             <a href="{{ route('medical-record.patient.reimport', ['userId' => $patient->id, 'clearCcda' => 'on']) }}"
                class="btn btn-danger btn-xs"
                style="font-size: 15px"
-               onclick="return confirm('Importer (v3) (beta): CPM will clear Problems, Medications, Insurances, and Allergies. You typically want to do that in scenarios such as when Problems are imported wrongly due to CPM having false ICD codes. ICD codes were corrected, and now we want to re-import.')"
+               onclick="return confirm('This will delete all Problems, Medications, Insurances, and Allergies, and re-import them using Importer (v3 beta). If something looks off, or Problems are imported wrongly due to CPM having false ICD codes. ICD codes were corrected, please continue.')"
             >
-                Clear and Reimport (beta)
+                Reimport (beta)
             </a>
         </div>
     @endif
