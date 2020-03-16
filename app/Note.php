0cdad9cd819c3388913d30f5fd807a67b752b05f
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-16 11:50:52 -0600

    Adds correct problems and uses mix

--- app/Note.php
+++ app/Note.php
@@ -41,6 +41,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property \CircleLinkHealth\Customer\Entities\User                        $patient
  * @property \CircleLinkHealth\Customer\Entities\User                        $program
  * @property string                                                          $status
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereAuthorId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereBody($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereCreatedAt($value)
@@ -53,8 +54,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereUpdatedAt($value)
  * @mixin \Eloquent
+ *
  * @property \CircleLinkHealth\Customer\Entities\User|null                                               $logger
  * @property \CircleLinkHealth\Revisionable\Entities\Revision[]|\Illuminate\Database\Eloquent\Collection $revisionHistory
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note emergency($yes = true)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note filter(\App\Filters\QueryFilters $filters)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note forwarded(\Carbon\Carbon $from = null, \Carbon\Carbon $to = null, $excludePatientSupport = true)
@@ -63,9 +66,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note patientPractice($practiceId)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note query()
+ *
  * @property string|null                                                                                                     $summary_type
  * @property string|null                                                                                                     $deleted_at
  * @property \CircleLinkHealth\Core\Entities\DatabaseNotification[]|\Illuminate\Notifications\DatabaseNotificationCollection $notifications
+ *
  * @method static bool|null forceDelete()
  * @method static \Illuminate\Database\Query\Builder|\App\Note onlyTrashed()
  * @method static bool|null restore()
@@ -75,10 +80,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereSummaryType($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Note withTrashed()
  * @method static \Illuminate\Database\Query\Builder|\App\Note withoutTrashed()
+ *
  * @property int|null $addendums_count
  * @property int|null $notifications_count
  * @property int|null $revision_history_count
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Note whereSuccessStory($value)
+ *
  * @property int $success_story
  */
 class Note extends \CircleLinkHealth\Core\Entities\BaseModel implements PdfReport, AttachableToNotification
@@ -306,8 +314,9 @@ class Note extends \CircleLinkHealth\Core\Entities\BaseModel implements PdfRepor
      * Create a PDF of this resource and return the path to it.
      *
      * @param null $scale
+     * @param bool $renderHtml
      */
-    public function toPdf($scale = null): string
+    public function toPdf($scale = null, $renderHtml = false): string
     {
         $fileName = Carbon::today()->toDateString().'-'.$this->patient->id.'.pdf';
         $filePath = storage_path('pdfs/notes/'.$fileName);
@@ -316,9 +325,20 @@ class Note extends \CircleLinkHealth\Core\Entities\BaseModel implements PdfRepor
             return $filePath;
         }
         $problems = $this->patient
-            ->cpmProblems
+            ->ccdProblems
+            ->where('is_monitored', true)
             ->pluck('name')
             ->all();
+
+        if ($renderHtml) {
+            return view('pdfs.note', [
+                'patient'  => $this->patient,
+                'problems' => $problems,
+                'sender'   => $this->author,
+                'note'     => $this,
+                'provider' => $this->patient->billingProviderUser(),
+            ]);
+        }
 
         $pdf = app('snappy.pdf.wrapper');
         $pdf->loadView('pdfs.note', [
