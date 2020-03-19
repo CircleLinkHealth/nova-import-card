533b439892b93d46c16bcf3000df06069682a3a5
Pangratios Cosma <pagcosma@gmail.com>
2020-03-19 13:57:08 +0200

    rollback

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::group([
 
 Route::group([
     'prefix'     => 'manage-patients',
-    'middleware' => ['auth', 'permission:patient.read,patient.update'],
+    'middleware' => ['auth', 'permission:vitals-survey-complete'],
 ], function () {
 
     Route::get('', [
@@ -141,7 +141,7 @@ Route::group([
 
     Route::group([
         'prefix'     => 'vitals',
-        'middleware' => ['permission:patient.read,patient.update'],
+        'middleware' => ['auth', 'permission:vitals-survey-complete'],
     ], function () {
         Route::get('{patientId}', [
             'uses' => 'VitalsSurveyController@getCurrentSurvey',
