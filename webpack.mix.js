71da4392c69b4ed9c9b5968469d806d9dd2e2571
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-24 20:55:15 -0600

    deletes more files

--- webpack.mix.js
+++ webpack.mix.js
@@ -77,7 +77,6 @@ mix.combine([
     'bower_components/bootstrap/dist/js/bootstrap.js',
     'bower_components/bootstrap-select/dist/js/bootstrap-select.js',
     'public/js/typeahead.bundle.js',
-    'public/js/jquery.datetimepicker.full.min.js',
 ], 'public/compiled/js/issue-688.js');
 /** end fixing issue 688 */
 
@@ -85,9 +84,7 @@ mix.combine([
 mix.combine([
     'bower_components/jquery/dist/jquery.js',
     'bower_components/jquery-ui/jquery-ui.js',
-    'public/js/jquery.datetimepicker.full.min.js',
     'bower_components/jquery-idletimer/dist/idle-timer.js',
-    'public/js/jquery-ui-timepicker.min.js',
     'bower_components/parsleyjs/dist/parsley.js',
     'bower_components/bootstrap-select/dist/js/bootstrap-select.js',
     'bower_components/select2/dist/js/select2.js',
