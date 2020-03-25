d978e525d82418a38ae5aa24538ed58ca18461f7
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-24 23:17:30 -0600

    delete more files

--- webpack.mix.js
+++ webpack.mix.js
@@ -85,7 +85,6 @@ mix.combine([
     'bower_components/jquery/dist/jquery.js',
     'bower_components/jquery-ui/jquery-ui.js',
     'bower_components/jquery-idletimer/dist/idle-timer.js',
-    'bower_components/parsleyjs/dist/parsley.js',
     'bower_components/bootstrap-select/dist/js/bootstrap-select.js',
     'bower_components/select2/dist/js/select2.js',
     'bower_components/bootstrap/dist/js/bootstrap.js'
