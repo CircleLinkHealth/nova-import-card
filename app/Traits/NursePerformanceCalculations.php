1cc6cccbc828ea447b4079c8b56944d8ff8629aa
Antonis Antoniou <kountouris7@gmail.com>
2020-03-26 20:28:46 +0200

    nurse performance metric fix2

--- app/Traits/NursePerformanceCalculations.php
+++ app/Traits/NursePerformanceCalculations.php
@@ -28,11 +28,10 @@ trait NursePerformanceCalculations
     {
         $totalCCMtimeOnCompletedPatients = $this->queryMonthlyCompletedPatient($patientsForMonth)->sum('patient_time');
 
-        if (0 === $totalMonthlyCompletedPatientsOfNurse) {
-            $totalMonthlyCompletedPatientsOfNurse = 1;
-        }
+        return 0 === $totalMonthlyCompletedPatientsOfNurse
+            ? 0
+            : round((float)($totalCCMtimeOnCompletedPatients / $totalMonthlyCompletedPatientsOfNurse), '2');
 
-        return round((float)($totalCCMtimeOnCompletedPatients / $totalMonthlyCompletedPatientsOfNurse), '2');
     }
 
     /**
