4fcedc055edef1f3e4bfc776b9ff51ca25f18c82
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-03-26 20:36:22 -0600

    lockfile and fix uno

--- resources/views/layouts/provider.blade.php
+++ resources/views/layouts/provider.blade.php
@@ -10,7 +10,6 @@
 
     <title>CarePlanManager - @yield('title')</title>
 
-    <link href="{{ mix('/compiled/css/stylesheet.css') }}" rel="stylesheet">
     <link href="{{ mix('/css/patientsearch.css') }}" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css" integrity="sha256-/oZ7h/Jkj6AfibN/zTWrCoba0L+QhP9Tf/ZSgyZJCnY=" crossorigin="anonymous" />
