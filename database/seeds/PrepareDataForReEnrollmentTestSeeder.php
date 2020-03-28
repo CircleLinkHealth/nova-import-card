df441ba7023d553de1b56b539264d8981bd6c13b
Antonis Antoniou <kountouris7@gmail.com>
2020-03-28 23:04:14 +0200

    cpm-1919 -

--- database/seeds/PrepareDataForReEnrollmentTestSeeder.php
+++ database/seeds/PrepareDataForReEnrollmentTestSeeder.php
@@ -21,31 +21,31 @@ class PrepareDataForReEnrollmentTestSeeder extends Seeder
      */
     public function run()
     {
-//        $faker     = Factory::create();
-//        $mothStart = Carbon::parse(now())->copy()->startOfMonth()->toDateTimeString();
-//        $monthEnd  = Carbon::parse($mothStart)->copy()->endOfMonth()->toDateTimeString();
-//
-//        $unreachablePatients = User::with('patientInfo')
-//            ->whereDoesntHave('enrollmentInvitationLink')
-//            ->whereHas('patientInfo', function ($patient) use ($mothStart, $monthEnd) {
-//                $patient->where('ccm_status', self::CCM_STATUS_UNREACHABLE)->where([
-//                    ['date_unreachable', '>=', $mothStart],
-//                    ['date_unreachable', '<=', $monthEnd],
-//                ]);
-//            })->exists();
-//
-//        if ( ! $unreachablePatients) {
-//            $n     = 1;
-//            $limit = 5;
-//            while ($n <= $limit) {
-//                $user = $this->createUser(8, 'participant', self::CCM_STATUS_UNREACHABLE);
-//                $user->patientInfo()->update([
-//                    'birth_date' => $faker->date(),
-//                ]);
-//                ++$n;
+        $faker     = Factory::create();
+        $mothStart = Carbon::parse(now())->copy()->startOfMonth()->toDateTimeString();
+        $monthEnd  = Carbon::parse($mothStart)->copy()->endOfMonth()->toDateTimeString();
+
+        $unreachablePatients = User::with('patientInfo')
+            ->whereDoesntHave('enrollmentInvitationLink')
+            ->whereHas('patientInfo', function ($patient) use ($mothStart, $monthEnd) {
+                $patient->where('ccm_status', self::CCM_STATUS_UNREACHABLE)->where([
+                    ['date_unreachable', '>=', $mothStart],
+                    ['date_unreachable', '<=', $monthEnd],
+                ]);
+            })->exists();
+
+        if ( ! $unreachablePatients) {
+            $n     = 1;
+            $limit = 5;
+            while ($n <= $limit) {
+                $user = $this->createUser(8, 'participant', self::CCM_STATUS_UNREACHABLE);
+                $user->patientInfo()->update([
+                    'birth_date' => $faker->date(),
+                ]);
+                ++$n;
 //                $this->command->info("$n Patients");
-//            }
-//        }
+            }
+        }
 
 //        $this->command->info('Finished but no patients needed to be create');
     }
