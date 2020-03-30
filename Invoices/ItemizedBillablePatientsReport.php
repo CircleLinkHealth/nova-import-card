2ae67e917fd9f42ce955bd9b66d299b67dcae46a
kakoushias <kakoushias@gmail.com>
2020-03-30 21:10:42 +0300

    CPM-2205 fix

--- Invoices/ItemizedBillablePatientsReport.php
+++ Invoices/ItemizedBillablePatientsReport.php
@@ -8,7 +8,6 @@ use App\Repositories\BillablePatientsEloquentRepository;
 use App\Repositories\PatientSummaryEloquentRepository;
 use App\ValueObjects\PatientReportData;
 use Carbon\Carbon;
-use CircleLinkHealth\Customer\Entities\ChargeableService;
 use CircleLinkHealth\Customer\Entities\PatientMonthlySummary;
 use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Customer\Entities\User;
@@ -33,7 +32,7 @@ class ItemizedBillablePatientsReport
      * @var string
      */
     protected $practiceName;
-    
+
     /**
      * ItemizedBillablePatientsReport constructor.
      *
@@ -47,13 +46,13 @@ class ItemizedBillablePatientsReport
         $this->month        = $month;
         $this->practiceName = $practiceName;
     }
-    
+
     public function toArrayForCsv(): array
     {
         $billablePatientsRepo = app(BillablePatientsEloquentRepository::class);
-        
+
         $data = [];
-        
+
         $billablePatientsRepo->billablePatients($this->practiceId, $this->month)
                              ->whereHas(
                                  'patientSummaries',
@@ -79,7 +78,7 @@ class ItemizedBillablePatientsReport
                                      $summaries->each(
                                          function (User $patientUser) use (&$data) {
                                              $summary = $patientUser->patientSummaries->sortByDesc('id')->first();
-                        
+
                                              $patientData = new PatientReportData();
                                              $patientData->setCcmTime(
                                                  round($summary->ccm_time / 60, 2)
@@ -96,58 +95,59 @@ class ItemizedBillablePatientsReport
                                                  $patientUser->billingCodes($this->month)
                                              );
                                              $patientData->setCcmProblemCodes(
-                                                 $this->getCcmAttestedConditions($summary)
+                                                 $this->formatProblemCodesForReport($summary->ccmAttestedProblems()->filter())
                                              );
-                                             $patientData->setBhiCodes($this->getBhiAttestedConditions($summary));
+
+                                             $patientData->setBhiCodes($this->formatProblemCodesForReport($summary->bhiAttestedProblems()->filter()));
+
                                              $patientData->setLocationName($patientUser->getPreferredLocationName());
-                        
+
                                              $newRow = [
                                                  'Provider Name'       => $patientData->getProvider(),
                                                  'Location'            => $patientData->getLocationName(),
                                                  'Patient Name'        => $patientData->getName(),
                                                  'DOB'                 => $patientData->getDob(),
-                                                 'Billing Code(s)'        => $patientData->getBillingCodes(),
+                                                 'Billing Code(s)'     => $patientData->getBillingCodes(),
                                                  'CCM Mins'            => $patientData->getCcmTime(),
                                                  'BHI Mins'            => $patientData->getBhiTime(),
                                                  'CCM Problem Code(s)' => $patientData->getCcmProblemCodes(),
                                                  'BHI Code(s)'         => $patientData->getBhiCodes(),
                                              ];
-                        
-                                             if ($patientUser->primaryPractice->hasAWVServiceCode(
-                                                 ) && $awvSummary = $patientUser->patientAWVSummaries->sortByDesc(
+
+                                             if ($patientUser->primaryPractice->hasAWVServiceCode() && $awvSummary = $patientUser->patientAWVSummaries->sortByDesc(
                                                      'id'
                                                  )->first()) {
                                                  $patientData->setAwvDate($awvSummary->billable_at);
                                                  $newRow['AWV Date'] = $patientData->getAwvDate();
                                              };
-                        
+
                                              $data[] = $newRow;
                                          }
                                      );
                                  }
                              );
-        
+
         return $data;
     }
-    
+
     private function practice()
     {
         if ( ! $this->practice) {
             $this->practice = Practice::findOrFail($this->practiceId);
         }
-        
+
         return $this->practice;
     }
-    
+
     public function toArray()
     {
         $data          = [];
         $data['name']  = $this->practiceName;
         $data['month'] = $this->month->toDateString();
-        
+
         $repo                 = app(PatientSummaryEloquentRepository::class);
         $billablePatientsRepo = app(BillablePatientsEloquentRepository::class);
-        
+
         $billablePatientsRepo->billablePatientSummaries($this->practiceId, $this->month, true)
                              ->where('approved', '=', true)
                              ->with(
@@ -164,7 +164,7 @@ class ItemizedBillablePatientsReport
                                      $summaries->each(
                                          function (PatientMonthlySummary $summary) use (&$data, $repo) {
                                              $u = $summary->patient;
-                        
+
                                              $patientData = new PatientReportData();
                                              $patientData->setCcmTime(round($summary->ccm_time / 60, 2));
                                              $patientData->setBhiTime(round($summary->bhi_time / 60, 2));
@@ -173,21 +173,21 @@ class ItemizedBillablePatientsReport
                                              $patientData->setPractice($u->program_id);
                                              $patientData->setProvider($u->getBillingProviderName());
                                              $patientData->setBillingCodes($u->billingCodes($this->month));
-                        
+
                                              $patientData->setCcmProblemCodes(
                                                  $this->formatProblemCodesForReport($summary->ccmAttestedProblems()->filter())
                                              );
-                        
+
                                              $patientData->setBhiCodes($this->formatProblemCodesForReport($summary->bhiAttestedProblems()->filter()));
-                        
+
                                              $patientData->setLocationName($u->getPreferredLocationName());
-                        
+
                                              $data['patientData'][$u->id] = $patientData;
                                          }
                                      );
                                  }
                              );
-        
+
         $data['patientData'] = array_key_exists('patientData', $data)
             ? collect($data['patientData'])->sortBy(
                 function ($data) {
@@ -195,7 +195,7 @@ class ItemizedBillablePatientsReport
                 }
             )
             : null;
-        
+
         $awvPatients = User::ofType('participant')
                            ->ofPractice($this->practiceId)
                            ->whereHas(
@@ -219,19 +219,19 @@ class ItemizedBillablePatientsReport
                                function ($patients) use (&$data) {
                                    foreach ($patients as $u) {
                                        $summary = $u->patientAWVSummaries->first();
-                    
+
                                        $patientData = new PatientReportData();
                                        $patientData->setName($u->getFullName());
                                        $patientData->setDob($u->getBirthDate());
                                        $patientData->setPractice($u->program_id);
                                        $patientData->setProvider($u->getBillingProviderName());
                                        $patientData->setAwvDate($summary->billable_at);
-                    
+
                                        $data['awvPatientData'][$u->id] = $patientData;
                                    }
                                }
                            );
-        
+
         $data['awvPatientData'] = array_key_exists('awvPatientData', $data)
             ? collect($data['awvPatientData'])->sortBy(
                 function ($data) {
@@ -239,7 +239,7 @@ class ItemizedBillablePatientsReport
                 }
             )
             : null;
-        
+
         return $data;
     }
 
