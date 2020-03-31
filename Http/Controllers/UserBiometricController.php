d2fbbd25f4f61e89f6ae04ad931ab08b44068d64
Antonis Antoniou <kountouris7@gmail.com>
2020-03-31 14:02:53 +0300

    CPM-2209 Allow empty starting biometric value

--- Http/Controllers/UserBiometricController.php
+++ Http/Controllers/UserBiometricController.php
@@ -7,8 +7,8 @@
 namespace CircleLinkHealth\ApiPatient\Http\Controllers;
 
 use App\Services\CPM\CpmBiometricService;
-use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class UserBiometricController extends Controller
 {
@@ -16,7 +16,7 @@ class UserBiometricController extends Controller
      * @var CpmBiometricService
      */
     protected $biometricUserService;
-    
+
     /**
      * UserBiometricController constructor.
      *
@@ -26,68 +26,68 @@ class UserBiometricController extends Controller
     {
         $this->biometricUserService = $biometricUserService;
     }
-    
+
     public function store($userId, Request $request)
     {
-        $biometricId             = $request->input('biometric_id');
-        $starting                = $request->input('starting');
-        $target                  = $request->input('target');
-        $systolic_high_alert     = $request->input('systolic_high_alert');
-        $systolic_low_alert      = $request->input('systolic_low_alert');
-        $diastolic_high_alert    = $request->input('diastolic_high_alert');
-        $diastolic_low_alert     = $request->input('diastolic_low_alert');
-        $high_alert              = $request->input('high_alert');
-        $low_alert               = $request->input('low_alert');
-        $starting_a1c            = $request->input('starting_a1c');
+        $biometricId = $request->input('biometric_id');
+        $starting = $request->input('starting');
+        $target = $request->input('target');
+        $systolic_high_alert = $request->input('systolic_high_alert');
+        $systolic_low_alert = $request->input('systolic_low_alert');
+        $diastolic_high_alert = $request->input('diastolic_high_alert');
+        $diastolic_low_alert = $request->input('diastolic_low_alert');
+        $high_alert = $request->input('high_alert');
+        $low_alert = $request->input('low_alert');
+        $starting_a1c = $request->input('starting_a1c');
         $monitor_changes_for_chf = $request->input('monitor_changes_for_chf');
-        $result                  = null;
+        $result = null;
         if ($biometricId) {
             switch ($biometricId) {
                 case 1:
                     $result = $this->biometricUserService->addPatientWeight($userId, $biometricId, [
-                        'starting'                => $starting,
-                        'target'                  => $target,
+                        'starting' => $starting === null ? '' : $starting,
+                        'target' => $target,
                         'monitor_changes_for_chf' => $monitor_changes_for_chf,
                     ]);
                     break;
                 case 2:
                     $result = $this->biometricUserService->addPatientBloodPressure($userId, $biometricId, [
-                        'starting'             => $starting,
-                        'target'               => $target,
+                        'starting' => $starting === null ? '' : $starting,
+                        'target' => $target,
                         'diastolic_high_alert' => $diastolic_high_alert,
-                        'diastolic_low_alert'  => $diastolic_low_alert,
-                        'systolic_high_alert'  => $systolic_high_alert,
-                        'systolic_low_alert'   => $systolic_low_alert,
+                        'diastolic_low_alert' => $diastolic_low_alert,
+                        'systolic_high_alert' => $systolic_high_alert,
+                        'systolic_low_alert' => $systolic_low_alert,
                     ]);
                     break;
                 case 3:
                     $result = $this->biometricUserService->addPatientBloodSugar($userId, $biometricId, [
-                        'starting'     => $starting,
-                        'target'       => $target,
-                        'high_alert'   => $high_alert,
-                        'low_alert'    => $low_alert,
+                        'starting' => $starting === null ? '' : $starting,
+                        'target' => $target,
+                        'high_alert' => $high_alert,
+                        'low_alert' => $low_alert,
                         'starting_a1c' => $starting_a1c,
                     ]);
                     break;
                 default:
                     $result = $this->biometricUserService->addPatientSmoking($userId, $biometricId, [
-                        'starting' => $starting,
-                        'target'   => $target,
+                        'starting' => $starting === null ? '' : $starting,
+                        'target' => $target,
                     ]);
                     break;
             }
         } else {
             return \response('"biometric_id" is important');
         }
-        
+
         return response()->json($result);
     }
-    
+
     public function show($userId)
     {
         return response()->json($this->biometricUserService->patientBiometrics($userId));
     }
-    
+
     public function destroy($userId, $id)
     {
         return response()->json($this->biometricUserService->removePatientBiometric($userId, $id));
