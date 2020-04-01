ceef3e3e624b12d918f8a82cc57387c4a8a2c190
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-01 15:00:38 -0600

    importer improvements

--- app/Http/Controllers/ImporterController.php
+++ app/Http/Controllers/ImporterController.php
@@ -16,12 +16,12 @@ use Laracasts\Utilities\JavaScript\JavaScriptFacade as JavaScript;
 class ImporterController extends Controller
 {
     private $repo;
-
+    
     public function __construct(CCDImporterRepository $repo)
     {
         $this->repo = $repo;
     }
-
+    
     /**
      * Show the form to upload CCDs.
      *
@@ -31,43 +31,57 @@ class ImporterController extends Controller
     {
         return view('saas.importer.create');
     }
-
+    
     public function getImportedRecords()
     {
-        return ImportedMedicalRecord::whereNull('imported')->with('demographics')
-            ->with('practice')
-            ->with(['location' => function ($q) {
-                                        $q->select([
-                                            'id',
-                                            'practice_id',
-                                            'is_primary',
-                                            'name',
-                                        ]);
-                                    }])
-            ->with([
-                'billingProvider' => function ($q) {
-                                        $q->select([
-                                            'id',
-                                            'saas_account_id',
-                                            'program_id',
-                                            'display_name',
-                                            'first_name',
-                                            'last_name',
-                                            'suffix',
-                                        ]);
-                                    },
-                'nurseUser' => function ($q) {
-                    $q->select([
-                                   'id',
-                                   'saas_account_id',
-                                   'program_id',
-                                   'display_name',
-                                   'first_name',
-                                   'last_name',
-                                   'suffix',
-                               ]);
-                }])
-            ->when(
+        return ImportedMedicalRecord::whereNull('imported')
+                                    ->with('demographics')
+                                    ->with('practice')
+                                    ->with(
+                                        [
+                                            'location' => function ($q) {
+                                                $q->select(
+                                                    [
+                                                        'id',
+                                                        'practice_id',
+                                                        'is_primary',
+                                                        'name',
+                                                    ]
+                                                );
+                                            },
+                                        ]
+                                    )
+                                    ->with(
+                                        [
+                                            'billingProvider' => function ($q) {
+                                                $q->select(
+                                                    [
+                                                        'id',
+                                                        'saas_account_id',
+                                                        'program_id',
+                                                        'display_name',
+                                                        'first_name',
+                                                        'last_name',
+                                                        'suffix',
+                                                    ]
+                                                );
+                                            },
+                                            'nurseUser'       => function ($q) {
+                                                $q->select(
+                                                    [
+                                                        'id',
+                                                        'saas_account_id',
+                                                        'program_id',
+                                                        'display_name',
+                                                        'first_name',
+                                                        'last_name',
+                                                        'suffix',
+                                                    ]
+                                                );
+                                            },
+                                        ]
+                                    )
+                                    ->when(
                                         ! auth()->user()->isAdmin(),
                                         function ($q) {
                                             $q->whereHas(
@@ -78,22 +92,22 @@ class ImporterController extends Controller
                                             );
                                         }
                                     )
-            ->get()
+                                    ->get()
             //where not in UPG + G0506
             //where media. where id = upg, custom_properties->mrn = imr.mrn, finished_processing()
-            ->transform(
-                                        function (ImportedMedicalRecord $summary) {
-                                            $mr = $summary->medicalRecord();
-
-                                            if ( ! $mr) {
-                                                return false;
-                                            }
-
-                                            if (upg0506IsEnabled()) {
-                                                $isUpg0506Incomplete = false;
-
-                                                if ($mr instanceof Ccda) {
-                                                    $isUpg0506Incomplete = Ccda::whereHas(
+                                    ->transform(
+                function (ImportedMedicalRecord $summary) {
+                    $mr = $summary->medicalRecord();
+                    
+                    if ( ! $mr) {
+                        return false;
+                    }
+                    
+                    if (upg0506IsEnabled()) {
+                        $isUpg0506Incomplete = false;
+                        
+                        if ($mr instanceof Ccda) {
+                            $isUpg0506Incomplete = Ccda::whereHas(
                                 'media',
                                 function ($q) {
                                     $q->where('custom_properties->is_upg0506_complete', '!=', 'true');
@@ -104,38 +118,38 @@ class ImporterController extends Controller
                                     $q->where('from', 'like', '%@upg.ssdirect.aprima.com');
                                 }
                             )->where('id', $mr->id)->exists();
-                                                }
-
-                                                if ($isUpg0506Incomplete) {
-                                                    return false;
-                                                }
-                                            }
-
-                                            if ( ! $summary->billing_provider_id) {
-                                                $mr = $mr->guessPracticeLocationProvider();
-
-                                                $summary->billing_provider_id = $mr->getBillingProviderId();
-
-                                                if ($summary->isDirty('billing_provider_id')) {
-                                                    $summary->load('billingProvider');
-                                                }
-
-                                                if ( ! $summary->location_id) {
-                                                    $summary->location_id = $mr->getLocationId();
-                                                    $summary->load('location');
-                                                }
-
-                                                if ( ! $summary->practice_id) {
-                                                    $summary->practice_id = $mr->getPracticeId();
-                                                    $summary->load('practice');
-                                                }
-
-                                                if ($summary->isDirty()) {
-                                                    $summary->save();
-                                                }
-                                            }
-
-                                            $providers = $mr->providers()->where(
+                        }
+                        
+                        if ($isUpg0506Incomplete) {
+                            return false;
+                        }
+                    }
+                    
+                    if ( ! $summary->billing_provider_id) {
+                        $mr = $mr->guessPracticeLocationProvider();
+                        
+                        $summary->billing_provider_id = $mr->getBillingProviderId();
+                        
+                        if ($summary->isDirty('billing_provider_id')) {
+                            $summary->load('billingProvider');
+                        }
+                        
+                        if ( ! $summary->location_id) {
+                            $summary->location_id = $mr->getLocationId();
+                            $summary->load('location');
+                        }
+                        
+                        if ( ! $summary->practice_id) {
+                            $summary->practice_id = $mr->getPracticeId();
+                            $summary->load('practice');
+                        }
+                        
+                        if ($summary->isDirty()) {
+                            $summary->save();
+                        }
+                    }
+                    
+                    $providers = $mr->providers()->where(
                         [
                             ['first_name', '!=', null],
                             ['last_name', '!=', null],
@@ -146,22 +160,22 @@ class ImporterController extends Controller
                             return $m->first_name.$m->last_name;
                         }
                     );
-
-                                            $summary['flag'] = false;
-
-                                            if ($providers->count(
+                    
+                    $summary['flag'] = false;
+                    
+                    if ($providers->count(
                         ) > 1 || ! $mr->location_id || ! $mr->location_id || ! $mr->billing_provider_id) {
-                                                $summary['flag'] = true;
-                                            }
-
-                                            $summary->checkDuplicity();
-
-                                            return $summary;
-                                        }
-                                    )->filter()
-            ->values();
+                        $summary['flag'] = true;
+                    }
+                    
+                    $summary->checkDuplicity();
+                    
+                    return $summary;
+                }
+            )->filter()
+                                    ->values();
     }
-
+    
     public function handleCcdFilesUpload(Request $request)
     {
         ini_set('upload_max_filesize', '50M');
@@ -172,18 +186,18 @@ class ImporterController extends Controller
         if ( ! $request->hasFile('file')) {
             return response()->json('No file found', 400);
         }
-
+        
         //example: http://cpm.clh.test/ccd-importer?source=importer_awv
         $source = $this->getSource($request);
-
+        
         $ccdas = [];
         foreach ($request->file('file') as $file) {
             \Log::warning("reading file $file");
-
+            
             $xml = file_get_contents($file);
-    
+            
             \Log::info("finished reading file $file");
-    
+            
             $ccda = Ccda::create(
                 [
                     'user_id' => auth()->user()->id,
@@ -191,38 +205,19 @@ class ImporterController extends Controller
                     'source'  => $source ?? Ccda::IMPORTER,
                 ]
             );
-
+            
             ImportCcda::dispatch($ccda, true);
             $ccdas[] = $ccda->id;
         }
-
+        
         return $ccdas;
     }
-
-    /**
-     * Show all QASummaries that are related to a CCDA.
-     */
-    public function index()
-    {
-        //get rid of orphans
-        $delete = ImportedMedicalRecord::whereNull('medical_record_id')->delete();
-
-        $importedRecords = $this::getImportedRecords();
-
-        JavaScript::put(
-            [
-                'importedMedicalRecords' => $importedRecords,
-            ]
-        );
-
-        return view('CCDUploader.uploadedSummary');
-    }
-
+    
     public function records()
     {
         return $this::getImportedRecords();
     }
-
+    
     /**
      * Show the form to upload CCDs.
      *
@@ -232,41 +227,41 @@ class ImporterController extends Controller
     {
         return view('CCDUploader.uploader-remix')->with('shouldUseNewVersion', $request->has('v3'));
     }
-
+    
     /**
      * Receives XML files, saves them in DB, and returns them JSON Encoded.
      *
+     * @return string
      * @throws \Exception
      *
-     * @return string
      */
     public function uploadRawFiles(Request $request)
     {
         $this::handleCcdFilesUpload($request);
-
+        
         return redirect()->route('view.files.ready.to.import');
     }
-
+    
     /**
      * Route: /api/ccd-importer/import-medical-records.
      *
      * Receives XML and XLSX files, saves them in DB, and returns them JSON Encoded
      *
+     * @return string
      * @throws \Exception
      *
-     * @return string
      */
     public function uploadRecords(Request $request)
     {
         $ccdas = $this::handleCcdFilesUpload($request);
-
+        
         if ( ! $request->has('json')) {
             return redirect()->route('import.ccd.remix');
         }
-
+        
         return response()->json(['ccdas' => $ccdas]);
     }
-
+    
     /**
      * The source of the importer can be submitted through
      * the url. In this case we are checking if available through
@@ -280,19 +275,19 @@ class ImporterController extends Controller
         if ($request->has('source')) {
             return $request->input('source');
         }
-
+        
         if (empty($_SERVER['HTTP_REFERER'])) {
             return null;
         }
-
+        
         $url   = $_SERVER['HTTP_REFERER'];
         $parts = parse_url($url);
         if (empty($parts['query'])) {
             return null;
         }
-
+        
         parse_str($parts['query'], $query);
-
+        
         return $query['source'] ?? null;
     }
 }
