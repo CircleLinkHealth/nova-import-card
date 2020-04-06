9516c8c5b8540fbcdba86ea53793546b3d2ef153
kakoushias <kakoushias@gmail.com>
2020-04-06 23:38:40 +0300

    Billing - include all problems if patient does not have BHI code

--- Invoices/ItemizedBillablePatientsReport.php
+++ Invoices/ItemizedBillablePatientsReport.php
@@ -98,7 +98,7 @@ class ItemizedBillablePatientsReport
                                              );
                                              $patientData->setCcmProblemCodes($this->getCcmAttestedConditions($summary));
 
-                                             $patientData->setAllCcmProblemCodes($this->getAllCcmConditions($patientUser));
+                                             $patientData->setAllCcmProblemCodes($this->getAllCcmConditions($patientUser, $summary));
 
                                              $patientData->setBhiCodes($this->getBhiAttestedConditions($summary));
 
@@ -195,7 +195,7 @@ class ItemizedBillablePatientsReport
 
                                              $patientData->setBhiCodes($this->getBhiAttestedConditions($summary));
 
-                                             $patientData->setAllCcmProblemCodes($this->getAllCcmConditions($u));
+                                             $patientData->setAllCcmProblemCodes($this->getAllCcmConditions($u, $summary));
 
                                              $patientData->setAllBhiCodes($this->getAllBhiConditions($u, $summary));
 
@@ -276,13 +276,16 @@ class ItemizedBillablePatientsReport
         return $this->formatProblemCodesForReport($summary->bhiAttestedProblems()->filter());
     }
 
-    private function getAllCcmConditions(User $patient)
+    private function getAllCcmConditions(User $patient, PatientMonthlySummary $summary)
     {
-        return $this->formatProblemCodesForReport(
-            $patient->ccdProblems->where('cpm_problem_id', '!=',
-                genericDiabetes()->id)->where('cpmProblem.is_behavioral', '=',
-                false)
-        );
+        $problems = $patient->ccdProblems->where('cpm_problem_id', '!=',
+            genericDiabetes()->id);
+
+        if ( $summary->hasServiceCode(ChargeableService::BHI)) {
+           $problems = $problems>where('cpmProblem.is_behavioral', '=', false);
+        }
+
+        return $this->formatProblemCodesForReport($problems);
     }
 
     private function getAllBhiConditions(User $patient, PatientMonthlySummary $summary)
