dae8cbfa5cbf292b21e5dea304ecc98ed3914277
Pangratios Cosma <pagcosma@gmail.com>
2020-04-08 10:18:50 +0300

    CPM-2241 wrap with try catch

--- app/Observers/PracticeObserver.php
+++ app/Observers/PracticeObserver.php
@@ -17,17 +17,17 @@ class PracticeObserver
     public function created(Practice $practice)
     {
         $updated = User::withTrashed()
-            ->where('saas_account_id', $practice->saas_account_id)
-            ->where('auto_attach_programs', true)
-            ->with('roles')
-            ->has('roles')
-            ->whereDoesntHave('practices', function ($q) use ($practice) {
-                $q->where('practices.id', $practice->id);
-            })
-            ->get()
-            ->map(function ($user) use ($practice) {
-                $user->attachRoleForSite($user->roles->first()->id, $practice->id);
-            });
+                       ->where('saas_account_id', $practice->saas_account_id)
+                       ->where('auto_attach_programs', true)
+                       ->with('roles')
+                       ->has('roles')
+                       ->whereDoesntHave('practices', function ($q) use ($practice) {
+                           $q->where('practices.id', $practice->id);
+                       })
+                       ->get()
+                       ->map(function ($user) use ($practice) {
+                           $user->attachRoleForSite($user->roles->first()->id, $practice->id);
+                       });
     }
 
     /**
@@ -38,5 +38,18 @@ class PracticeObserver
         if ( ! $practice->saas_account_id && ! auth()->guest()) {
             $practice->saas_account_id = auth()->user()->saas_account_id;
         }
+    }
+
+    public function saved(Practice $practice)
+    {
+        User::where('auto_attach_programs', '=', 1)
+            ->where('saas_account_id', '=', $practice->saas_account_id)
+            ->each(function (User $user) use ($practice) {
+                $role = $user->practiceOrGlobalRole();
+                if ( ! $role) {
+                    return;
+                }
+                $user->attachRoleForSite($role->id, $practice->id);
+            });
     }
 }
--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -30,35 +30,7 @@ class UserObserver
      */
     public function saved(User $user)
     {
-        if ($user->auto_attach_programs) {
-            $practiceIds = $user->saasAccount
-                ->practices
-                ->map(function ($practice) use ($user) {
-                    try {
-                        $role = $user->practiceOrGlobalRole();
 
-                        if ( ! $role) {
-                            return false;
-                        }
-
-                        $user->attachRoleForSite($role->id, $practice->id);
-
-                        return $practice->id;
-                    } catch (\Exception $e) {
-                        //check if this is a mysql exception for unique key constraint
-                        if ($e instanceof QueryException) {
-                            //                    @todo:heroku query to see if it exists, then attach
-
-                            $errorCode = $e->errorInfo[1];
-                            if (1062 == $errorCode) {
-                                return false;
-                            }
-                        }
-
-                        throw $e;
-                    }
-                });
-        }
     }
 
     /**
