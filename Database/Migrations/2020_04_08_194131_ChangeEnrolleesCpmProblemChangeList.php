c0704b39276bf311876649f75d6aaee0df98aeed
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-08 22:05:17 -0500

    removes fk checks

--- Database/Migrations/2020_04_08_194131_ChangeEnrolleesCpmProblemChangeList.php
+++ Database/Migrations/2020_04_08_194131_ChangeEnrolleesCpmProblemChangeList.php
@@ -16,11 +16,15 @@ class ChangeEnrolleesCpmProblemChangeList extends Migration
             $table->dropForeign('enrollees_cpm_problem_1_foreign');
             $table->dropForeign('enrollees_cpm_problem_2_foreign');
         });
-        
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+    
         Schema::table('enrollees', function (Blueprint $table) {
             $table->foreign('cpm_problem_1')->references('id')->on('cpm_problems')->onUpdate('CASCADE')->onDelete('SET NULL');
             $table->foreign('cpm_problem_2')->references('id')->on('cpm_problems')->onUpdate('CASCADE')->onDelete('SET NULL');
         });
+    
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 
     /**
