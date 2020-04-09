93aa8bbde9265c39c6c1740594058cedfd7b6103
Pangratios Cosma <pagcosma@gmail.com>
2020-04-09 10:22:22 +0300

    uncomment tests

--- tests/Unit/CheckUserTotalTimeTrackedTest.php
+++ tests/Unit/CheckUserTotalTimeTrackedTest.php
@@ -17,47 +17,47 @@ class CheckUserTotalTimeTrackedTest extends TestCase
     use TimeHelpers;
     use UserHelpers;
 
-//    public function test_it_does_not_raise_alert_for_total_time_in_last_7_days()
-//    {
-//        $practice = factory(Practice::class)->create();
-//        $nurse    = $this->createUser($practice->id, 'care-center');
-//
-//        $hoursPerDay = 4;
-//
-//        //33.6
-//        $maxAllowed = 7 * $hoursPerDay * UserTotalTimeChecker::getThresholdForWeek();
-//
-//        $start = now()->subDay(7);
-//        $sub6  = now()->subDay(6);
-//        $sub5  = now()->subDay(5);
-//        $sub4  = now()->subDay(4);
-//        $sub3  = now()->subDay(3);
-//        $sub2  = now()->subDay(2);
-//        $end   = now()->subDay(1);
-//
-//        //7 days -> 28 hours committed
-//        $this->addWorkHours($nurse, $start, $hoursPerDay);
-//        $this->addWorkHours($nurse, $sub6, $hoursPerDay);
-//        $this->addWorkHours($nurse, $sub5, $hoursPerDay);
-//        $this->addWorkHours($nurse, $sub4, $hoursPerDay);
-//        $this->addWorkHours($nurse, $sub3, $hoursPerDay);
-//        $this->addWorkHours($nurse, $sub2, $hoursPerDay);
-//        $this->addWorkHours($nurse, $end, $hoursPerDay);
-//
-//        //32 hours CPM time - Not above max (33.6)
-//        $this->addTime($nurse, null, 240, false, false, false, $start);
-//        $this->addTime($nurse, null, 240, false, false, false, $sub6);
-//        $this->addTime($nurse, null, 240, false, false, false, $sub5);
-//        $this->addTime($nurse, null, 240, false, false, false, $sub4);
-//        $this->addTime($nurse, null, 240, false, false, false, $sub3);
-//        $this->addTime($nurse, null, 240, false, false, false, $sub2);
-//        $this->addTime($nurse, null, 480, false, false, false, $end);
-//
-//        $checker = new UserTotalTimeChecker($start, $end, true, $nurse->id);
-//        $alerts  = $checker->check();
-//        $this->assertFalse($alerts->has('daily'));
-//        $this->assertFalse($alerts->has('weekly'));
-//    }
+    public function test_it_does_not_raise_alert_for_total_time_in_last_7_days()
+    {
+        $practice = factory(Practice::class)->create();
+        $nurse    = $this->createUser($practice->id, 'care-center');
+
+        $hoursPerDay = 4;
+
+        //33.6
+        $maxAllowed = 7 * $hoursPerDay * UserTotalTimeChecker::getThresholdForWeek();
+
+        $start = now()->subDay(7);
+        $sub6  = now()->subDay(6);
+        $sub5  = now()->subDay(5);
+        $sub4  = now()->subDay(4);
+        $sub3  = now()->subDay(3);
+        $sub2  = now()->subDay(2);
+        $end   = now()->subDay(1);
+
+        //7 days -> 28 hours committed
+        $this->addWorkHours($nurse, $start, $hoursPerDay);
+        $this->addWorkHours($nurse, $sub6, $hoursPerDay);
+        $this->addWorkHours($nurse, $sub5, $hoursPerDay);
+        $this->addWorkHours($nurse, $sub4, $hoursPerDay);
+        $this->addWorkHours($nurse, $sub3, $hoursPerDay);
+        $this->addWorkHours($nurse, $sub2, $hoursPerDay);
+        $this->addWorkHours($nurse, $end, $hoursPerDay);
+
+        //32 hours CPM time - Not above max (33.6)
+        $this->addTime($nurse, null, 240, false, false, false, $start);
+        $this->addTime($nurse, null, 240, false, false, false, $sub6);
+        $this->addTime($nurse, null, 240, false, false, false, $sub5);
+        $this->addTime($nurse, null, 240, false, false, false, $sub4);
+        $this->addTime($nurse, null, 240, false, false, false, $sub3);
+        $this->addTime($nurse, null, 240, false, false, false, $sub2);
+        $this->addTime($nurse, null, 480, false, false, false, $end);
+
+        $checker = new UserTotalTimeChecker($start, $end, true, $nurse->id);
+        $alerts  = $checker->check();
+        $this->assertFalse($alerts->has('daily'));
+        $this->assertFalse($alerts->has('weekly'));
+    }
 
     public function test_it_does_not_raise_alert_for_total_time_less_than_8_hours_in_a_day()
     {
@@ -97,25 +97,25 @@ class CheckUserTotalTimeTrackedTest extends TestCase
         $this->assertTrue($alerts->get('daily')->has($nurse->id));
     }
 
-//    public function test_it_raises_alert_for_total_time_over_8_hours_in_a_day()
-//    {
-//        $practice = factory(Practice::class)->create();
-//        $nurse    = $this->createUser($practice->id, 'care-center');
-//
-//        $this->addTime($nurse, null, 120, false, false);
-//        $this->addTime($nurse, null, 120, false, false);
-//        $this->addTime($nurse, null, 120, false, false);
-//        $this->addTime($nurse, null, 120, false, false);
-//        $this->addTime($nurse, null, 30, false, false);
-//
-//        $start = now()->startOfDay();
-//        $end   = now()->endOfDay();
-//
-//        $checker = new UserTotalTimeChecker($start, $end, false, $nurse->id);
-//        $alerts  = $checker->check();
-//        $this->assertTrue($alerts->has('daily'));
-//        $this->assertTrue($alerts->get('daily')->has($nurse->id));
-//    }
+    public function test_it_raises_alert_for_total_time_over_8_hours_in_a_day()
+    {
+        $practice = factory(Practice::class)->create();
+        $nurse    = $this->createUser($practice->id, 'care-center');
+
+        $this->addTime($nurse, null, 120, false, false);
+        $this->addTime($nurse, null, 120, false, false);
+        $this->addTime($nurse, null, 120, false, false);
+        $this->addTime($nurse, null, 120, false, false);
+        $this->addTime($nurse, null, 30, false, false);
+
+        $start = now()->startOfDay();
+        $end   = now()->endOfDay();
+
+        $checker = new UserTotalTimeChecker($start, $end, false, $nurse->id);
+        $alerts  = $checker->check();
+        $this->assertTrue($alerts->has('daily'));
+        $this->assertTrue($alerts->get('daily')->has($nurse->id));
+    }
 
     public function test_it_raises_alert_for_total_time_over_max_in_last_7_days()
     {
