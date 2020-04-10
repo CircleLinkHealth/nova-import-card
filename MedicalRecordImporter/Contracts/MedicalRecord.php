5e83dbd8f0f7bda2632f6cc42f405e8658a45643
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-10 16:57:12 -0500

    Importer v3: Medication Importer

--- MedicalRecordImporter/Contracts/MedicalRecord.php
+++ MedicalRecordImporter/Contracts/MedicalRecord.php
@@ -70,13 +70,6 @@ interface MedicalRecord
      */
     const QA = 'qa';
 
-    /**
-     * Transform the data into MedicalRecordSectionLogs, so that they can be fed to the Importer.
-     *
-     * @return ItemLog|MedicalRecord
-     */
-    public function createLogs(): MedicalRecord;
-
     public function getBillingProviderId(): ?int;
 
     public function getDocumentCustodian(): string;
@@ -84,11 +77,6 @@ interface MedicalRecord
     public function getId(): ?int;
 
     public function getLocationId(): ?int;
-
-    /**
-     * Get the Transformer.
-     */
-    public function getLogger(): MedicalRecordLogger;
 
     /**
      * Get the User to whom this record belongs to, if one exists.
@@ -103,46 +91,4 @@ interface MedicalRecord
      * Guess Practice, Location and Billing Provider.
      */
     public function guessPracticeLocationProvider(): MedicalRecord;
-
-    /**
-     * Handles importing a MedicalRecordForEligibilityCheck for QA.
-     *
-     * @return ImportedMedicalRecord
-     */
-    public function import();
-
-    /**
-     * Import Allergies for QA.
-     */
-    public function importAllergies(): MedicalRecord;
-
-    /**
-     * Import Demographics for QA.
-     */
-    public function importDemographics(): MedicalRecord;
-
-    /**
-     * Import Document for QA.
-     */
-    public function importDocument(): MedicalRecord;
-
-    /**
-     * Import Insurance Policies for QA.
-     */
-    public function importInsurance(): MedicalRecord;
-
-    /**
-     * Import Medications for QA.
-     */
-    public function importMedications(): MedicalRecord;
-
-    /**
-     * Import Problems for QA.
-     */
-    public function importProblems(): MedicalRecord;
-
-    /**
-     * Import Providers for QA.
-     */
-    public function importProviders(): MedicalRecord;
 }
