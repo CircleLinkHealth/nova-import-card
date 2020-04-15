b7eb6c6048842a6fbe332f97788e2018429a53c6
Pangratios Cosma <pagcosma@gmail.com>
2020-04-15 21:55:47 +0300

    CPM-2072 time tracker for ca panel

---
+++ Database/Migrations/2020_04_15_092421_add_enrollee_id_in_lv_page_timer_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEnrolleeIdInLvPageTimerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lv_page_timer', function (Blueprint $table) {
+            $table->unsignedInteger('enrollee_id')
+                  ->after('patient_id')
+                  ->nullable()
+                  ->default(null);
+
+            $table->foreign('enrollee_id')
+                  ->references('id')
+                  ->on('enrollees')
+                  ->onUpdate('no action')
+                  ->onDelete('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('lv_page_timer', 'enrollee_id')) {
+            Schema::table('lv_page_timer', function (Blueprint $table) {
+                $table->dropForeign(['enrollee_id']);
+                $table->dropColumn('enrollee_id');
+            });
+        }
+    }
+}
