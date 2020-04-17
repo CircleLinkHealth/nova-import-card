89778d95563415041c5d336cf828ebc63830cb90
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-16 21:15:45 -0500

    CPM-2278

---
+++ AppConfig/StandByNurseUser.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michalis
+ * Date: 11/16/19
+ * Time: 5:36 AM
+ */
+
+namespace CircleLinkHealth\Customer\AppConfig;
+
+
+use CircleLinkHealth\Core\Entities\AppConfig;
+
+class StandByNurseUser
+{
+    const STAND_BY_NURSE_USER_ID_NOVA_KEY = 'stand_by_nurse_user_id';
+    
+    public static function id() {
+        return \Cache::remember(self::STAND_BY_NURSE_USER_ID_NOVA_KEY, 2, function () {
+            return AppConfig::where('config_key', '=', self::STAND_BY_NURSE_USER_ID_NOVA_KEY)
+                            ->firstOrFail()->config_value;
+        });
+    }
+}
