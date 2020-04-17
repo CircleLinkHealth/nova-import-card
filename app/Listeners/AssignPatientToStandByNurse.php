4410a0ba62d8489d20a7df0a741384dc77c07ff6
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-16 21:34:38 -0500

    CPM-2278

---
+++ app/Listeners/AssignPatientToStandByNurse.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CarePlanWasQAApproved;
+use CircleLinkHealth\Customer\AppConfig\StandByNurseUser;
+use CircleLinkHealth\Customer\Entities\PatientNurse;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AssignPatientToStandByNurse
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(CarePlanWasQAApproved $event)
+    {
+        if (! $standByNurseId = StandByNurseUser::id()) {
+            return;
+        }
+        
+        PatientNurse::updateOrCreate(
+            ['patient_user_id' => $event->patient->id],
+            [
+                'patient_user_id'         => $event->patient->id,
+                'nurse_user_id'           => $standByNurseId,
+                'temporary_nurse_user_id' => null,
+                'temporary_from'          => null,
+                'temporary_to'            => null,
+            ]
+        );
+    }
+}
