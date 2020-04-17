d31a3c5a8ca098a025150c9b28192f4153162722
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-16 21:29:19 -0500

    CPM-2278 make stand by user optional

--- AppConfig/StandByNurseUser.php
+++ AppConfig/StandByNurseUser.php
@@ -11,6 +11,12 @@ namespace CircleLinkHealth\Customer\AppConfig;
 
 use CircleLinkHealth\Core\Entities\AppConfig;
 
+/**
+ * A stand by nurse User is used by ops team to assign all care plans after CarePlan Approval by a CLH admin.
+ *
+ * Class StandByNurseUser
+ * @package CircleLinkHealth\Customer\AppConfig
+ */
 class StandByNurseUser
 {
     const STAND_BY_NURSE_USER_ID_NOVA_KEY = 'stand_by_nurse_user_id';
