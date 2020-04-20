e40b1227b73615b6dfceb151702e8182dcd7d795
kakoushias <kakoushias@gmail.com>
2020-04-20 20:57:35 +0300

    CPM-2256 cs

--- Traits/MySQLSearchable.php
+++ Traits/MySQLSearchable.php
@@ -1,20 +1,18 @@
 <?php
+
 /*
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
 namespace CircleLinkHealth\Core\Traits;
 
-
 /**
- * Trait MySQLSearchable
- * @package CircleLinkHealth\Core\Traits
+ * Trait MySQLSearchable.
  */
 trait MySQLSearchable
 {
-
     /**
-     * Available full-text modes for MySQL
+     * Available full-text modes for MySQL.
      *
      * @var array
      */
@@ -24,68 +22,18 @@ trait MySQLSearchable
     ];
 
     /**
-     * Replaces spaces with full text search wildcards
-     *
-     * @param string $term
-     * @param bool $shouldRequireAll
-     * @param bool $shouldRequireIntegers
-     *
-     * @return string
-     */
-    protected function fullTextWildcards(
-        string $term,
-        $mode,
-        bool $shouldRequireAll = true,
-        bool $shouldRequireIntegers = true
-    ) {
-        // removing symbols used by MySQL
-        $reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~'];
-        $term            = str_replace($reservedSymbols, '', $term);
-
-        $words = explode(' ', $term);
-
-        if ($mode === 'BOOLEAN') {
-            foreach ($words as $key => $word) {
-
-                /*
-                 * applying + operator (required word) only big words
-                 * because smaller ones are not indexed by mysql
-                 */
-                if (strlen($word) >= 3) {
-                    if ($shouldRequireIntegers && ! $shouldRequireAll) {
-                        if (is_numeric($word)) {
-                            $word = '+' . $word;
-                        }
-                    }
-
-                    if ($shouldRequireAll) {
-                        $word = '+' . $word;
-                    }
-                    $words[$key] = $word . '*';
-                }
-            }
-        }
-
-
-        $searchTerm = implode(' ', $words);
-
-        return $searchTerm;
-    }
-
-    /**
      * Scope a query that matches a full text search of term.
-     * Can be built upon
+     * Can be built upon.
      *
      * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param array $columns
-     * @param string $term
+     * @param string                                $mode
+     * @param bool                                  $shouldRequireAll
+     * @param bool                                  $shouldRequireIntegers
+     * @param mixed                                 $shouldIncludeRelevanceScore
      *
-     * @param string $mode
-     * @param bool $shouldRequireAll
-     * @param bool $shouldRequireIntegers
+     * @throws \Exception
      *
      * @return \Illuminate\Database\Eloquent\Builder
-     * @throws \Exception
      */
     public function scopeMySQLSearch(
         $query,
@@ -102,16 +50,62 @@ trait MySQLSearchable
 
         $columns = implode(',', $columns);
 
-        if ($shouldIncludeRelevanceScore){
-            $query->selectRaw("*, MATCH ({$columns}) AGAINST (? IN {$mode} MODE)*100 as relevance_score",
-                [$this->fullTextWildcards($term, $shouldRequireAll, $shouldRequireIntegers)]);
+        if ($shouldIncludeRelevanceScore) {
+            $query->selectRaw(
+                "*, MATCH ({$columns}) AGAINST (? IN {$mode} MODE)*100 as relevance_score",
+                [$this->fullTextWildcards($term, $shouldRequireAll, $shouldRequireIntegers)]
+            );
         }
 
-
-        $query->whereRaw("MATCH ({$columns}) AGAINST (? IN {$mode} MODE)",
-            $this->fullTextWildcards($term, $shouldRequireAll, $shouldRequireIntegers));
+        $query->whereRaw(
+            "MATCH ({$columns}) AGAINST (? IN {$mode} MODE)",
+            $this->fullTextWildcards($term, $shouldRequireAll, $shouldRequireIntegers)
+        );
 
         return $query;
+    }
+
+    /**
+     * Replaces spaces with full text search wildcards.
+     *
+     * @param mixed $mode
+     *
+     * @return string
+     */
+    protected function fullTextWildcards(
+        string $term,
+        $mode,
+        bool $shouldRequireAll = true,
+        bool $shouldRequireIntegers = true
+    ) {
+        // removing symbols used by MySQL
+        $reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~'];
+        $term            = str_replace($reservedSymbols, '', $term);
+
+        $words = explode(' ', $term);
+
+        if ('BOOLEAN' === $mode) {
+            foreach ($words as $key => $word) {
+                /*
+                 * applying + operator (required word) only big words
+                 * because smaller ones are not indexed by mysql
+                 */
+                if (strlen($word) >= 3) {
+                    if ($shouldRequireIntegers && ! $shouldRequireAll) {
+                        if (is_numeric($word)) {
+                            $word = '+'.$word;
+                        }
+                    }
+
+                    if ($shouldRequireAll) {
+                        $word = '+'.$word;
+                    }
+                    $words[$key] = $word.'*';
+                }
+            }
+        }
+
+        return implode(' ', $words);
     }
 
     private function validateMode($mode)
