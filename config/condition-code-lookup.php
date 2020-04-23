873f5718eb13908fb6f31a14904af6c34536a724
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-22 23:36:00 -0500

    small changes

--- config/condition-code-lookup.php
+++ config/condition-code-lookup.php
@@ -4,16 +4,6 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
-$mysqlDBName = env('CCL_DB_DATABASE', env('DB_DATABASE', 'nothing'));
-
-if ('nothing' === $mysqlDBName) {
-    $mysqlDBName = snake_case(getenv('HEROKU_BRANCH'));
-}
-
-if (getenv('CI')) {
-    $mysqlDBName = getenv('HEROKU_TEST_RUN_ID');
-}
-
 return [
     'name'     => 'ConditionCodeLookup',
     'api_urls' => [
@@ -27,11 +17,6 @@ return [
         ],
     ],
     'cache' => [
-        'enable'   => env('CCL_CACHE_ENABLE', false),
-        'host'     => env('CCL_DB_HOST', env('DB_HOST', '127.0.0.1')),
-        'port'     => env('CCL_DB_PORT', env('DB_PORT', '3306')),
-        'database' => $mysqlDBName,
-        'username' => env('CCL_DB_USERNAME', env('DB_USERNAME', 'forge')),
-        'password' => env('CCL_DB_PASSWORD', env('DB_PASSWORD', '')),
+        'enable' => env('CCL_CACHE_ENABLE', false),
     ],
 ];
