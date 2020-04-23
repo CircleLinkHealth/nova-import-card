8b850da4dc7758f643e91bba175ae3841d0efd49
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-22 20:47:29 -0500

    Adds code lookup for problems without name

--- Console/ReimportPatientMedicalRecord.php
+++ Console/ReimportPatientMedicalRecord.php
@@ -70,7 +70,7 @@ class ReimportPatientMedicalRecord extends Command
 
         \Log::debug("ReimportPatientMedicalRecord:user_id:{$user->id}");
 
-        DB::transaction(function (User $user) {
+        DB::transaction(function () use ($user) {
             if ( ! $user->hasCcda()) {
                 $this->attemptCreateCcdaFromMrTemplate($user);
             }
