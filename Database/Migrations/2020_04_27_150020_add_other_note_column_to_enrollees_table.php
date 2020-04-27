2cf6a425b25d0a06995456f5169f4ffbfaf4e19a
kakoushias <kakoushias@gmail.com>
2020-04-27 22:04:44 +0300

    add other note on enrollees table

---
+++ Database/Migrations/2020_04_27_150020_add_other_note_column_to_enrollees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOtherNoteColumnToEnrolleesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('enrollees', 'other_note')) {
+            Schema::table('enrollees', function (Blueprint $table) {
+                $table->string('text')->nullable()->after('last_call_outcome_reason');
+            });
+        }
+        
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('enrollees', 'other_note')) {
+            Schema::table('enrollees', function (Blueprint $table) {
+                $table->dropColumn('other_note');
+            });
+        }
+    }
+}
