6e959efeab9c9bc73dc0267f4be87d7356136a2a
Antonis Antoniou <kountouris7@gmail.com>
2020-04-28 13:24:09 +0300

    CPM-2316_new_work_flow

--- AppConfig/SeesAutoQAButton.php
+++ AppConfig/SeesAutoQAButton.php
@@ -1,13 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michalis
- * Date: 11/16/19
- * Time: 5:36 AM
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
  */
 
 namespace CircleLinkHealth\Customer\AppConfig;
-
 
 use CircleLinkHealth\Core\Entities\AppConfig;
 
@@ -15,13 +12,13 @@ use CircleLinkHealth\Core\Entities\AppConfig;
  * A stand by nurse User is used by ops team to assign all care plans after CarePlan Approval by a CLH admin.
  *
  * Class StandByNurseUser
- * @package CircleLinkHealth\Customer\AppConfig
  */
 class SeesAutoQAButton
 {
     const SEES_AUTO_QA_BUTTON = 'sees_auto_qa_button';
-    
-    public static function userId(int $userId) {
+
+    public static function userId(int $userId)
+    {
         return \Cache::remember(self::SEES_AUTO_QA_BUTTON, 2, function () use ($userId) {
             return optional(AppConfig::where('config_key', '=', self::SEES_AUTO_QA_BUTTON)->where('config_value', '=', $userId)->first())->config_value;
         });
