f844b20860192e93025ec3d059154d6a80433ada
Antonis Antoniou <kountouris7@gmail.com>
2020-04-29 01:47:54 +0300

    CPM-1919_last name

--- resources/views/enrollment-consent/enrollmentInvitation.blade.php
+++ resources/views/enrollment-consent/enrollmentInvitation.blade.php
@@ -27,7 +27,7 @@
                         {{$dateLetterSent}}
                     </div>
                     <div class="letter-head">
-                        Dear {{$userEnrollee->first_name . ' ' . $userEnrollee->last_name}},
+                        Dear {{$userEnrollee->last_name}},
                     </div>
                     <div class="letter-body">
                         <div class="body">
