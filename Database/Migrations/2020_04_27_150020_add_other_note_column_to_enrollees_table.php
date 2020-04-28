ecf3c0b5457ebdb355261bda5d34191a69f7c98f
Pangratios Cosma <pagcosma@gmail.com>
2020-04-28 22:05:07 +0300

    linting

--- Database/Migrations/2020_04_27_150020_add_other_note_column_to_enrollees_table.php
+++ Database/Migrations/2020_04_27_150020_add_other_note_column_to_enrollees_table.php
@@ -1,26 +1,15 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class AddOtherNoteColumnToEnrolleesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        if (!Schema::hasColumn('enrollees', 'other_note')) {
-            Schema::table('enrollees', function (Blueprint $table) {
-                $table->string('other_note')->nullable()->after('last_call_outcome_reason');
-            });
-        }
-        
-    }
-    
     /**
      * Reverse the migrations.
      *
@@ -31,6 +20,20 @@ class AddOtherNoteColumnToEnrolleesTable extends Migration
         if (Schema::hasColumn('enrollees', 'other_note')) {
             Schema::table('enrollees', function (Blueprint $table) {
                 $table->dropColumn('other_note');
+            });
+        }
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if ( ! Schema::hasColumn('enrollees', 'other_note')) {
+            Schema::table('enrollees', function (Blueprint $table) {
+                $table->string('other_note')->nullable()->after('last_call_outcome_reason');
             });
         }
     }
