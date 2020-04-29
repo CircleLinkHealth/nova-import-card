d0bb975e59b8adbaf5d32643d0943f3324a4a3e1
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-04-29 17:06:15 -0500

    Adds revisionable mysql indexes

---
+++ Database/Migrations/2020_04_29_171035_OptimizeSlowRevisionableQueries.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class OptimizeSlowRevisionableQueries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('revisions', function (Blueprint $table){
+//            $table->index('updated_at');
+            $table->index(['updated_at', 'is_phi']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
