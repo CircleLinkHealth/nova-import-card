636abf8339cb897941241c167ed3def8d29381d6
kakoushias <kakoushias@gmail.com>
2020-05-02 20:39:09 +0300

    CA - fix CA director assigned bug

--- app/Filters/EnrolleeFilters.php
+++ app/Filters/EnrolleeFilters.php
@@ -19,8 +19,10 @@ class EnrolleeFilters extends QueryFilters
     public function attempt_count($count)
     {
         if (empty($count)) {
-            return $this->builder->where('attempt_count', '<', 3)
-                ->orWhere('attempt_count', null);
+            return $this->builder->where(function ($subQuery) {
+                $subQuery->where('attempt_count', '<', 3)
+                    ->orWhere('attempt_count', null);
+            });
         }
 
         return $this->builder->where('attempt_count', 'like', '%'.$count.'%');
