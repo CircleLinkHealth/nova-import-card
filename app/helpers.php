41d4493a93c96cf0a07c55e437c5494ccd2c1ace
Pangratios Cosma <pagcosma@gmail.com>
2020-05-04 11:02:15 +0300

    procfile for review apps

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,15 @@
 <?php
 
-use Carbon\Carbon;
-use Illuminate\Support\Collection;
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
 
-if (! function_exists('is_json')) {
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+if ( ! function_exists('is_json')) {
     /**
      * Determine whether the given string is json.
      *
@@ -30,13 +36,9 @@ if (! function_exists('is_json')) {
     }
 }
 
-if (! function_exists('sortSurveyQuestions')) {
+if ( ! function_exists('sortSurveyQuestions')) {
     /**
      * Sort survey questions using the pivot table, taking into account sub_order.
-     *
-     * @param \Illuminate\Support\Collection $questions
-     *
-     * @return \Illuminate\Support\Collection
      */
     function sortSurveyQuestions(Collection $questions): Collection
     {
@@ -63,8 +65,7 @@ if (! function_exists('sortSurveyQuestions')) {
     }
 }
 
-if (! function_exists('getCpmCallerToken')) {
-
+if ( ! function_exists('getCpmCallerToken')) {
     /**
      * naive authentication for the CPM Caller Service.
      * @return string
@@ -75,7 +76,7 @@ if (! function_exists('getCpmCallerToken')) {
     }
 }
 
-if (! function_exists('getStringValueFromAnswer')) {
+if ( ! function_exists('getStringValueFromAnswer')) {
     function getStringValueFromAnswer($val, $default = '')
     {
         if (empty($val)) {
@@ -102,25 +103,151 @@ if (! function_exists('getStringValueFromAnswer')) {
     }
 }
 
-if (! function_exists('sendSlackMessage')) {
+if ( ! function_exists('sendSlackMessage')) {
     /**
      * Sends a message to Slack.
      *
      * @param Illuminate\Notifications\Notification $notification - must have a toSlack method
-     * @param bool $force - in case you really want the message to go to slack (testing | debugging)
+     * @param bool                                  $force        - in case you really want the message to go to slack (testing | debugging)
      */
     function sendSlackMessage(Illuminate\Notifications\Notification $notification, $force = false)
     {
-        $isProduction = config('app.env') === 'production';
-        if (! $force && ! $isProduction) {
+        $isProduction = 'production' === config('app.env');
+        if ( ! $force && ! $isProduction) {
             return;
         }
         $endpoint = env('SLACK_DEFAULT_ENDPOINT', null);
-        if (! $endpoint) {
+        if ( ! $endpoint) {
             return;
         }
         $notifiable = new \Illuminate\Notifications\AnonymousNotifiable();
         $notifiable->route('slack', $endpoint);
         $notifiable->notify($notification);
+    }
+}
+
+if ( ! function_exists('isOnSqlite')) {
+    /**
+     * Is the app running on sqlite?
+     *
+     * @return bool
+     */
+    function isOnSqlite()
+    {
+        return 'sqlite' === strtolower(config('database.default'));
+    }
+}
+
+if ( ! function_exists('extractNumbers')) {
+    /**
+     * Returns only numerical values in a string.
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    function extractNumbers($string)
+    {
+        preg_match_all('/([\d]+)/', $string, $match);
+
+        return implode($match[0]);
+    }
+}
+
+if ( ! function_exists('formatPhoneNumberE164')) {
+    /**
+     * Formats a string of numbers as a phone number delimited by dashes as such: xxx-xxx-xxxx.
+     *
+     * @param $string
+     * @param mixed $countryCode
+     *
+     * @return string
+     */
+    function formatPhoneNumberE164(
+        $string,
+        $countryCode = '1'
+    ) {
+        $sanitized = extractNumbers($string);
+
+        if (strlen($sanitized) < 10) {
+            return '';
+        }
+
+        if (strlen($sanitized) > 10) {
+            $sanitized = substr($sanitized, -10);
+        }
+
+        return '+'.$countryCode.$sanitized;
+    }
+}
+
+if ( ! function_exists('parseIds')) {
+    /**
+     * Get all of the IDs from the given mixed value.
+     *
+     * @param mixed $value
+     *
+     * @return array
+     */
+    function parseIds($value)
+    {
+        if (empty($value)) {
+            return [];
+        }
+
+        if ($value instanceof Model) {
+            return [$value->getKey()];
+        }
+
+        if ($value instanceof EloquentCollection) {
+            return $value->modelKeys();
+        }
+
+        if (is_array($value)) {
+            $value = collect($value);
+        }
+
+        if ($value instanceof Collection) {
+            return $value->map(
+                function ($el) {
+                    $id = parseIds($el);
+
+                    return $id[0] ?? null;
+                }
+            )->values()->toArray();
+        }
+
+        if (is_string($value) && Str::contains($value, ',')) {
+            return explode(',', $value);
+        }
+
+        return array_filter((array) $value);
+    }
+}
+
+if ( ! function_exists('isCpm')) {
+    function isCpm()
+    {
+        return 'CarePlan Manager' === config('app.name');
+    }
+}
+
+if ( ! function_exists('validateUsPhoneNumber')) {
+    /**
+     * @param string
+     * @param mixed $phoneNumber
+     */
+    function validateUsPhoneNumber($phoneNumber): bool
+    {
+        $validator = \Validator::make(
+            [
+                'number' => (new \CircleLinkHealth\Core\StringManipulation())->formatPhoneNumberE164($phoneNumber),
+            ],
+            [
+                'number' => ['required', \Illuminate\Validation\Rule::phone()->country(['US'])],
+            ]
+        );
+
+        return $validator->passes();
     }
 }
