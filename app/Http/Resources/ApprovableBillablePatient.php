ea2a3c094141e9dd7c671207b60b493be2773a33
kakoushias <kakoushias@gmail.com>
2020-05-05 20:33:26 +0300

    Add filter

--- app/Http/Resources/ApprovableBillablePatient.php
+++ app/Http/Resources/ApprovableBillablePatient.php
@@ -47,7 +47,7 @@ class ApprovableBillablePatient extends Resource
         if (null == $status) {
             $status = $this->patient->patientInfo->getCcmStatusForMonth(Carbon::parse($this->month_year));
         }
-        $problems = $this->allCcdProblems($this->patient)->unique('code')->values();
+        $problems = $this->allCcdProblems($this->patient)->unique('code')->filter()->values();
 
         return [
             'id'       => $this->patient->id,
