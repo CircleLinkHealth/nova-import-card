baeb01a42c47679608d5b47af79e6b73285146c4
Antonis Antoniou <kountouris7@gmail.com>
2020-05-07 13:15:46 +0300

    CPM-1919_5 Nova enrollees dashboard

--- resources/views/enrollment-consent/practiceLogo.blade.php
+++ resources/views/enrollment-consent/practiceLogo.blade.php
@@ -1,8 +1,7 @@
  <div class="practice-logo">
         {{--    temporary till we get the logos--}}
         <img style="height: 120px;"
-             src="{{asset('img/logos/CommonWealth')}}"
+             src="https://www.zilliondesigns.com/images/portfolio/healthcare-hospital/iStock-471629610-Converted.png"
              alt="Practice logo image missing">
 
-{{--https://www.zilliondesigns.com/images/portfolio/healthcare-hospital/iStock-471629610-Converted.png--}}
     </div>
