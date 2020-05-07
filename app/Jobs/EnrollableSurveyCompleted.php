1f24108cf91ea4a4a79fde602d31b523115e1a04
Antonis Antoniou <kountouris7@gmail.com>
2020-05-07 19:30:28 +0300

    CPM-1919_5_changes on texts

--- app/Jobs/EnrollableSurveyCompleted.php
+++ app/Jobs/EnrollableSurveyCompleted.php
@@ -222,7 +222,6 @@ class EnrollableSurveyCompleted implements ShouldQueue
     {
         $user->delete();
         ImportConsentedEnrollees::dispatch([$enrollee->id]);
-        $user->forceDelete();
     }
 
     public function reEnrollUnreachablePatient(User $user)
