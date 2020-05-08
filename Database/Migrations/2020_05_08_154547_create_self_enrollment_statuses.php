38ffa8fdc0181e3ce21e3ccd17d4c9524a3d0aaf
Antonis Antoniou <kountouris7@gmail.com>
2020-05-08 23:03:49 +0300

    CPM-1919_migratoin fix on eligibility

---
+++ Database/Migrations/2020_05_08_154547_create_self_enrollment_statuses.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSelfEnrollmentStatuses extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('self_enrollment_statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('enrollee_id');
+            $table->unsignedInteger('enrollee_user_id')->nullable();
+            $table->string('awv_survey_status')->nullable();
+            $table->boolean('logged_in')->default(false);
+            $table->timestamps();
+        });
+    }
+}
