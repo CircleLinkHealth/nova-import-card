1fc8b126993d2bbc5eb0656057b361d2a8a6410c
kakoushias <kakoushias@gmail.com>
2020-05-09 15:10:04 +0300

    fix

--- Entities/SelfEnrollmentStatus.php
+++ Entities/SelfEnrollmentStatus.php
@@ -8,6 +8,23 @@ namespace CircleLinkHealth\Eligibility\Entities;
 
 use CircleLinkHealth\Core\Entities\BaseModel;
 
+/**
+ * CircleLinkHealth\Eligibility\Entities\SelfEnrollmentStatus.
+ *
+ * @property int                                                                                         $id
+ * @property int                                                                                         $enrollee_id
+ * @property int|null                                                                                    $enrollee_user_id
+ * @property string|null                                                                                 $awv_survey_status
+ * @property int                                                                                         $logged_in
+ * @property \Illuminate\Support\Carbon|null                                                             $created_at
+ * @property \Illuminate\Support\Carbon|null                                                             $updated_at
+ * @property \CircleLinkHealth\Revisionable\Entities\Revision[]|\Illuminate\Database\Eloquent\Collection $revisionHistory
+ * @property int|null                                                                                    $revision_history_count
+ * @method   static                                                                                      \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\Eligibility\Entities\SelfEnrollmentStatus newModelQuery()
+ * @method   static                                                                                      \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\Eligibility\Entities\SelfEnrollmentStatus newQuery()
+ * @method   static                                                                                      \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\Eligibility\Entities\SelfEnrollmentStatus query()
+ * @mixin \Eloquent
+ */
 class SelfEnrollmentStatus extends BaseModel
 {
     protected $fillable = [
