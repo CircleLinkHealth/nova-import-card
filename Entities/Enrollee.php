846052e9c2eb674c78c174e205abca7116c68d6f
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-10 20:59:55 -0500

    Adds comment

--- Entities/Enrollee.php
+++ Entities/Enrollee.php
@@ -314,7 +314,7 @@ class Enrollee extends BaseModel
         'batch_id',
         'eligibility_job_id',
 
-        'medical_record_type',
+        'medical_record_type', //deprecated => do not use
         'medical_record_id',
 
         'user_id',
