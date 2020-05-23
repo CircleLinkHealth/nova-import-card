3f3fce7310c8f668195c7f0f2a795ed302f340ec
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-23 07:11:18 -0500

    Import App

--- app/Jobs/SendSelfEnrollmentReminders.php
+++ app/Jobs/SendSelfEnrollmentReminders.php
@@ -18,6 +18,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\App;
 
 class SendSelfEnrollmentReminders implements ShouldQueue
 {
