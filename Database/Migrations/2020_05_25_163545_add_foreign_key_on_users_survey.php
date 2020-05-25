cb9dcee8c40d4c0ffec221c2d441b80e734f6e96
Antonis Antoniou <kountouris7@gmail.com>
2020-05-25 21:25:26 +0300

    adds_foreign_key_on users_surveys

---
+++ Database/Migrations/2020_05_25_163545_add_foreign_key_on_users_survey.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use CircleLinkHealth\Customer\Entities\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyOnUsersSurvey extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users_surveys', function (Blueprint $table) {
+            $table->dropForeign('users_surveys_user_id_foreign');
+        });
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users_surveys', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->change();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+}
