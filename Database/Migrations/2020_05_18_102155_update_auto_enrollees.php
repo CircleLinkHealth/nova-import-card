dc6880e6fe43cf00937f41659c28c4d4d3b807ca
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-25 11:54:41 -0500

    Removes query from CreateSurveyOnlyUserFromEnrollee.php

--- Database/Migrations/2020_05_18_102155_update_auto_enrollees.php
+++ Database/Migrations/2020_05_18_102155_update_auto_enrollees.php
@@ -4,7 +4,7 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
-use App\SelfEnrollment\Jobs\CreateUsersFromEnrollees;
+use App\SelfEnrollment\Jobs\CreateSurveyOnlyUserFromEnrollee;
 use CircleLinkHealth\Eligibility\Entities\Enrollee;
 use Illuminate\Database\Migrations\Migration;
 
@@ -29,14 +29,10 @@ class UpdateAutoEnrollees extends Migration
         Enrollee::whereNull('user_id')
             ->where('status', Enrollee::QUEUE_AUTO_ENROLLMENT)
             ->select('id')
-            ->get()
-            ->chunk(100)->each(function ($col) {
-                $arr = $col
-                    ->map(function ($item) {
-                        return $item->id;
-                    })
-                    ->toArray();
-                CreateUsersFromEnrollees::dispatch($arr);
+            ->chunk(100, function ($enrollees) {
+                $enrollees->each(function ($enrollee) {
+                    CreateSurveyOnlyUserFromEnrollee::dispatch($enrollee);
+                });
             });
     }
 }
