ffaf47c7604b3ff7c4c0ee0b2444108137e86880
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-26 16:49:11 -0500

    Add batch id before making it a foreign key

--- Database/Migrations/2020_05_25_050420_add_foreign_key_to_enrollment_invitation_links.php
+++ Database/Migrations/2020_05_25_050420_add_foreign_key_to_enrollment_invitation_links.php
@@ -4,6 +4,9 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
+use App\EnrollmentInvitationsBatch;
+use CircleLinkHealth\Customer\Entities\User;
+use CircleLinkHealth\Eligibility\Entities\Enrollee;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -29,6 +32,26 @@ class AddForeignKeyToEnrollmentInvitationLinks extends Migration
      */
     public function up()
     {
+        CircleLinkHealth\Customer\EnrollableInvitationLink\EnrollableInvitationLink::chunk(100, function ($links) {
+            foreach ($links as $link) {
+                if (User::class === $link->enrollable_type) {
+                    $practiceId = User::find($link->invitationable_id)->program_id;
+                } elseif (Enrollee::class === $link->enrollable_type) {
+                    $practiceId = Enrollee::find($link->invitationable_id);
+                }
+
+                $batch = Cache::remember("temp_{$practiceId}_{$link->button_color}_initial_batch", 2, function () use ($practiceId, $link) {
+                    return EnrollmentInvitationsBatch::firstOrCreate([
+                        'practice_id' => $practiceId,
+                        'type'        => "Initial:{$link->button_color}",
+                    ]);
+                });
+
+                $link->batch_id = $batch->id;
+                $link->save();
+            }
+        });
+
         Schema::table('enrollables_invitation_links', function (Blueprint $table) {
             $table->unsignedInteger('batch_id');
             $table->foreign('batch_id')
