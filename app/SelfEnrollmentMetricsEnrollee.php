64242fc533d2fb7f310dc3362bdbb2f467f59cf6
Antonis Antoniou <kountouris7@gmail.com>
2020-05-27 14:09:53 +0300

    Sql View Query

---
+++ app/SelfEnrollmentMetricsEnrollee.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App;
+
+use CircleLinkHealth\Core\Entities\SqlViewModel;
+
+/**
+ * App\SelfEnrollmentMetricsEnrollee.
+ *
+ * @property string|null $color
+ * @method   static      \Illuminate\Database\Eloquent\Builder|\App\SelfEnrollmentMetricsEnrollee newModelQuery()
+ * @method   static      \Illuminate\Database\Eloquent\Builder|\App\SelfEnrollmentMetricsEnrollee newQuery()
+ * @method   static      \Illuminate\Database\Eloquent\Builder|\App\SelfEnrollmentMetricsEnrollee query()
+ * @mixin \Eloquent
+ */
+class SelfEnrollmentMetricsEnrollee extends SqlViewModel
+{
+    protected $table = 'self_enrollment_metrics_enrollee';
+}
