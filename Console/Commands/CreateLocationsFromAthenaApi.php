74c2b9d2ecb919e976e6c63e9f244789bd1d9fb4
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-28 19:00:37 -0500

    Adds activation command

--- Console/Commands/CreateLocationsFromAthenaApi.php
+++ Console/Commands/CreateLocationsFromAthenaApi.php
@@ -9,6 +9,7 @@ namespace CircleLinkHealth\Customer\Console\Commands;
 use CircleLinkHealth\Customer\Entities\Ehr;
 use CircleLinkHealth\Customer\Entities\Location;
 use CircleLinkHealth\Customer\Entities\Practice;
+use CircleLinkHealth\Customer\Entities\Role;
 use CircleLinkHealth\Eligibility\Contracts\AthenaApiImplementation;
 use Illuminate\Console\Command;
 
@@ -25,7 +26,7 @@ class CreateLocationsFromAthenaApi extends Command
      *
      * @var string
      */
-    protected $signature = 'athena:fetch-locations {cpmPracticeId : The practice ID from CPM}';
+    protected $signature = 'athena:fetch-locations {cpmPracticeId : The practice ID from CPM} {--fetch : Fetch locations from Athena and update or create them in CPM} {--activate : Restore locations we have pulled patients from.}';
     /**
      * @var AthenaApiImplementation
      */
@@ -49,6 +50,32 @@ class CreateLocationsFromAthenaApi extends Command
      * @return mixed
      */
     public function handle()
+    {
+        if ($this->option('fetch')) {
+            $this->warn('Fetching locations from Athena');
+            $this->importAthenaLocations();
+        }
+    
+        if ($this->option('activate')) {
+            $this->warn('Activating locations from where we have patients');
+            $this->activateLocationsWeHavePatientsFrom();
+        }
+
+        $this->comment('Done!');
+    }
+
+    public function practice()
+    {
+        if (is_null($this->practice)) {
+            $this->practice = Practice::with('locations')->whereHas('ehr', function ($q) {
+                $q->where('id', Ehr::athena()->firstOrFail()->id);
+            })->whereNotNull('external_id')->findOrFail($this->argument('cpmPracticeId'));
+        }
+
+        return $this->practice;
+    }
+
+    private function importAthenaLocations()
     {
         collect($this->api->getDepartments($this->practice()->external_id)['departments'] ?? [])->each(function ($aLoc) {
             $cpmLocs = $this->practice()->locations;
@@ -74,23 +101,33 @@ class CreateLocationsFromAthenaApi extends Command
                 'timezone'       => $aLoc['timezonename'],
                 'postal_code'    => $aLoc['zip'],
             ]);
-            
+
             if (true === $softDeleteTillAdminApproves) {
                 $cpmLocation->delete();
             }
         });
-        
-        $this->comment('Done!');
     }
-
-    public function practice()
+    
+    private function activateLocationsWeHavePatientsFrom()
     {
-        if (is_null($this->practice)) {
-            $this->practice = Practice::with('locations')->whereHas('ehr', function ($q) {
-                $q->where('id', Ehr::athena()->firstOrFail()->id);
-            })->whereNotNull('external_id')->findOrFail($this->argument('cpmPracticeId'));
-        }
-
-        return $this->practice;
+        Location::onlyTrashed()->where('practice_id', $this->practice()->id)->whereIn('external_department_id', function ($q) {
+            $q->select('ehr_department_id')
+                ->from('target_patients')
+                ->where('practice_id', $this->practice()->id)
+                ->whereNotNull('user_id')
+                ->whereIn('user_id', function ($q) {
+                    $q->select('user_id')
+                        ->from('practice_role_user')
+                        ->where('program_id', $this->practice()->id)
+                        ->whereIn('role_id', Role::whereIn('name', ['participant', 'survey-only'])->pluck('id')->all())
+                        ->distinct()
+                    ;
+                })
+                ->distinct()
+            ;
+        })->get()->each(function (Location $location){
+            $this->warn("Restoring location[$location->id]");
+            $location->restore();
+        });
     }
 }
