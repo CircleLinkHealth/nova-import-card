a0a52dd7f49f477f3784cf17a4804737a011307b
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-29 12:21:22 -0500

    Lintissimo

--- Database/Migrations/2020_05_29_131845_ChangeDefaultNumberOfMonthlyCallsToOne.php
+++ Database/Migrations/2020_05_29_131845_ChangeDefaultNumberOfMonthlyCallsToOne.php
@@ -1,23 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class ChangeDefaultNumberOfMonthlyCallsToOne extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('patient_info', function (Blueprint $table){
-            $table->integer('preferred_calls_per_month')->default(1)->change();
-        });
-    }
-
     /**
      * Reverse the migrations.
      *
@@ -25,6 +17,17 @@ class ChangeDefaultNumberOfMonthlyCallsToOne extends Migration
      */
     public function down()
     {
-        //
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patient_info', function (Blueprint $table) {
+            $table->integer('preferred_calls_per_month')->default(1)->change();
+        });
     }
 }
