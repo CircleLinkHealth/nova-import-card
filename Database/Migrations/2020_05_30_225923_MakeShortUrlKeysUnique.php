663cfa1a3a5d369b4baff111b207b1e16e5c8a62
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-05-30 22:02:42 -0500

    Make short_url keys unique

---
+++ Database/Migrations/2020_05_30_225923_MakeShortUrlKeysUnique.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeShortUrlKeysUnique extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('short_urls', function (Blueprint $table) {
+            $table->unique('url_key');
+            $table->unique('default_short_url');
+        });
+    }
+}
