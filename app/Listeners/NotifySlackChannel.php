43399f6ef8e4694ebf134b2fd575888b02e7c7ef
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-06-02 11:37:59 -0500

    Adds link to batch in Slack message

--- app/Listeners/NotifySlackChannel.php
+++ app/Listeners/NotifySlackChannel.php
@@ -9,6 +9,7 @@ namespace App\Listeners;
 use App\DirectMailMessage;
 use App\Services\PhiMail\Events\DirectMailMessageReceived;
 use App\Services\PhiMail\IncomingMessageHandler;
+use CircleLinkHealth\Eligibility\Entities\EligibilityBatch;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Str;
@@ -16,6 +17,9 @@ use Illuminate\Support\Str;
 class NotifySlackChannel implements ShouldQueue
 {
     use InteractsWithQueue;
+
+    const ELIGIBILITY_PROCESSING_PURPOSE = 'Eligibility Processing';
+    const IMPORTING_PURPOSE              = 'Importing';
 
     /**
      * Create the event listener.
@@ -54,8 +58,8 @@ class NotifySlackChannel implements ShouldQueue
 
         if ($practiceName = $dm->ccdas->pluck('practice.display_name')->filter()->values()->first()) {
             $purpose = Str::contains(strtolower($dm->body), strtolower(IncomingMessageHandler::KEYWORD_TO_PROCESS_FOR_ELIGIBILITY))
-                ? 'Eligibility Processing'
-                : 'Importing';
+                ? self::ELIGIBILITY_PROCESSING_PURPOSE
+                : self::IMPORTING_PURPOSE;
 
             $greeting .= " from $practiceName, containing CCDA(s) for $purpose";
         }
@@ -64,9 +68,18 @@ class NotifySlackChannel implements ShouldQueue
 
         $messageLink = route('direct-mail.show', [$dm->id]);
 
+        $message = "$greeting \n Click this link to go to the message {$messageLink}.";
+
+        if (self::ELIGIBILITY_PROCESSING_PURPOSE === $purpose
+            && $runningBatch = EligibilityBatch::where('practice_id', $dm->ccdas->pluck('practice.id')->filter()->values()->first())
+                ->where('status', EligibilityBatch::RUNNING)
+                ->first()) {
+            $message .= "\n Click this link to see the running eligibility batch ".route('eligibility.batch.show', [$runningBatch->id]);
+        }
+
         sendSlackMessage(
             '#ccd-file-status',
-            "$greeting \n Click this link to go to the message {$messageLink}."
+            $message
         );
     }
 }
