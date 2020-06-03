a68dd14fa16958eb51b2d15949b6532217a67eb9
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-06-03 14:23:53 -0500

    Fixes command

--- Entities/Chargeable.php
+++ Entities/Chargeable.php
@@ -8,6 +8,21 @@ namespace Modules\Customer\Entities;
 
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
 
+/**
+ * Modules\Customer\Entities\Chargeable.
+ *
+ * @property int                             $chargeable_service_id
+ * @property int                             $chargeable_id
+ * @property string                          $chargeable_type
+ * @property float|null                      $amount
+ * @property int                             $is_fulfilled
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|\Modules\Customer\Entities\Chargeable newModelQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|\Modules\Customer\Entities\Chargeable newQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|\Modules\Customer\Entities\Chargeable query()
+ * @mixin \Eloquent
+ */
 class Chargeable extends MorphPivot
 {
     protected $table = 'chargeables';
