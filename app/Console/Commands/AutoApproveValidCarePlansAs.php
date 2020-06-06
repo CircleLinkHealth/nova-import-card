b8fb5288939bb070af36bc3b1250755c81da04cd
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-06-05 19:28:12 -0500

    Load fresh careplan

--- app/Console/Commands/AutoApproveValidCarePlansAs.php
+++ app/Console/Commands/AutoApproveValidCarePlansAs.php
@@ -115,9 +115,7 @@ class AutoApproveValidCarePlansAs extends Command
         if ($needsQA && $this->option('reimport')) {
             $this->warn('reimporting user:'.$patient->id);
             $this->reimport($patient->id, $approver->id);
-            if (is_null($patient->carePlan)) {
-                $patient->load('carePlan');
-            }
+            $patient = $patient->fresh('carePlan');
             $needsQA = $patient->carePlan->validator()->fails();
         }
 
