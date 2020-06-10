0cb4cffeafeeffe74ca293eb43aaf41c3659d252
Pangratios Cosma <pagcosma@gmail.com>
2020-06-10 10:26:52 +0300

    bug fix - error check

--- app/Observers/CallObserver.php
+++ app/Observers/CallObserver.php
@@ -51,48 +51,51 @@ class CallObserver
     public function saved(Call $call)
     {
         if ($call->isDirty('status')) {
-            $patient = User::ofType('participant')
+            $patient = User::withTrashed()
+                ->ofType('participant')
                 ->where('id', $call->inbound_cpm_id)
                 ->orWhere('id', $call->outbound_cpm_id)
                 ->first();
 
-            if ($this->shouldApproveCarePlan($call)) {
-                $this->approveCarePlan($patient);
-            }
+            if ($patient) {
+                if ($this->shouldApproveCarePlan($call)) {
+                    $this->approveCarePlan($patient);
+                }
 
-            $date = Carbon::parse($call->updated_at);
+                $date = Carbon::parse($call->updated_at);
 
-            $this->activityService->processMonthlyActivityTime($patient->id, $date);
+                $this->activityService->processMonthlyActivityTime($patient->id, $date);
 
-            /*
-             * this is done in updateCallLogs, which is called on demand when needed
+                /*
+                 * this is done in updateCallLogs, which is called on demand when needed
 
-            $start = $date->copy()->startOfMonth();
-            $end   = $date->copy()->endOfMonth();
+                $start = $date->copy()->startOfMonth();
+                $end   = $date->copy()->endOfMonth();
 
-            $no_of_calls = Call::where(function ($q) {
-                $q->whereNull('type')
-                    ->orWhere('type', '=', 'call')
-                    ->orWhere('sub_type', '=', 'Call Back');
-            })
-                ->where(function ($q) use ($patient) {
-                    $q->where('outbound_cpm_id', $patient->id)
-                        ->orWhere('inbound_cpm_id', $patient->id);
+                $no_of_calls = Call::where(function ($q) {
+                    $q->whereNull('type')
+                        ->orWhere('type', '=', 'call')
+                        ->orWhere('sub_type', '=', 'Call Back');
                 })
-                ->where('called_date', '>=', $start)
-                ->where('called_date', '<=', $end)
-                ->whereIn('status', ['reached', 'not reached'])
-                ->get();
+                    ->where(function ($q) use ($patient) {
+                        $q->where('outbound_cpm_id', $patient->id)
+                            ->orWhere('inbound_cpm_id', $patient->id);
+                    })
+                    ->where('called_date', '>=', $start)
+                    ->where('called_date', '<=', $end)
+                    ->whereIn('status', ['reached', 'not reached'])
+                    ->get();
 
-            $no_of_successful_calls = $no_of_calls->where('status', 'reached')->count();
+                $no_of_successful_calls = $no_of_calls->where('status', 'reached')->count();
 
-            $summary = PatientMonthlySummary::where('patient_id', $patient->id)
-                ->where('month_year', $date->startOfMonth())
-                ->update([
-                    'no_of_calls'            => $no_of_calls->count(),
-                    'no_of_successful_calls' => $no_of_successful_calls,
-                ]);
-            */
+                $summary = PatientMonthlySummary::where('patient_id', $patient->id)
+                    ->where('month_year', $date->startOfMonth())
+                    ->update([
+                        'no_of_calls'            => $no_of_calls->count(),
+                        'no_of_successful_calls' => $no_of_successful_calls,
+                    ]);
+                */
+            }
         }
 
         if ('reached' === $call->status || 'done' === $call->status) {
