b8aea91d16ce9b0bbd2b954aa901561f52de3c4c
Pangratios Cosma <pagcosma@gmail.com>
2020-06-15 17:21:55 +0300

    ROAD-109 bug fixes

--- Jobs/ProcessSendGridMailStatusCallbackJob.php
+++ Jobs/ProcessSendGridMailStatusCallbackJob.php
@@ -50,11 +50,11 @@ class ProcessSendGridMailStatusCallbackJob implements ShouldQueue
                     $event['smtp-id'],
                     false,
                     [
-                        'event_id'   => $event['sg_event_id'] ?? null,
-                        'message_id' => $event['sg_message_id'] ?? null,
-                        'value'      => $event['event'],
-                        'details'    => $event['timestamp'],
-                        'email'      => $event['email'],
+                        'sg_event_id'   => $event['sg_event_id'] ?? null,
+                        'sg_message_id' => $event['sg_message_id'] ?? null,
+                        'value'         => $event['event'],
+                        'details'       => $event['timestamp'],
+                        'email'         => $event['email'],
                     ],
                 );
             } else {
