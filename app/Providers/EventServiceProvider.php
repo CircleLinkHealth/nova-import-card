2a19deae671f93c3cc317d88cc9ab4420c4e3c35
Pangratios Cosma <pagcosma@gmail.com>
2020-06-15 14:30:29 +0300

    ROAD-109 notification status update

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -8,9 +8,15 @@ namespace App\Providers;
 
 use App\Events\SurveyInstancePivotSaved;
 use App\Listeners\GeneratePatientReports;
+use CircleLinkHealth\Core\Listeners\LogFailedNotification;
+use CircleLinkHealth\Core\Listeners\LogSentMailNotification;
+use CircleLinkHealth\Core\Listeners\LogSentNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Notifications\Events\NotificationFailed;
+use Illuminate\Notifications\Events\NotificationSent;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,6 +31,15 @@ class EventServiceProvider extends ServiceProvider
         ],
         SurveyInstancePivotSaved::class => [
             GeneratePatientReports::class,
+        ],
+        MessageSent::class => [
+            LogSentMailNotification::class,
+        ],
+        NotificationSent::class => [
+            LogSentNotification::class,
+        ],
+        NotificationFailed::class => [
+            LogFailedNotification::class,
         ],
     ];
 
