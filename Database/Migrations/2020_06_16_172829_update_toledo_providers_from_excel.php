20f97de525461ce7097792527957ddb02e77c3e4
Antonis Antoniou <kountouris7@gmail.com>
2020-06-17 00:32:51 +0300

    command and migration

---
+++ Database/Migrations/2020_06_16_172829_update_toledo_providers_from_excel.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateToledoProvidersFromExcel extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Illuminate\Support\Facades\Artisan::call('update:toledoProviders');
+    }
+}
