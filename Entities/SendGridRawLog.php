36713c0e2800cba67286294e80a4f0ef476c9e3e
Pangratios Cosma <pagcosma@gmail.com>
2020-06-17 15:17:44 +0300

    lint

--- Entities/SendGridRawLog.php
+++ Entities/SendGridRawLog.php
@@ -6,6 +6,20 @@
 
 namespace CircleLinkHealth\Core\Entities;
 
+/**
+ * CircleLinkHealth\Core\Entities\SendGridRawLog.
+ *
+ * @property int                                                                                         $id
+ * @property mixed|null                                                                                  $events
+ * @property \Illuminate\Support\Carbon|null                                                             $created_at
+ * @property \Illuminate\Support\Carbon|null                                                             $updated_at
+ * @property \CircleLinkHealth\Revisionable\Entities\Revision[]|\Illuminate\Database\Eloquent\Collection $revisionHistory
+ * @property int|null                                                                                    $revision_history_count
+ * @method   static                                                                                      \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\Core\Entities\SendGridRawLog newModelQuery()
+ * @method   static                                                                                      \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\Core\Entities\SendGridRawLog newQuery()
+ * @method   static                                                                                      \Illuminate\Database\Eloquent\Builder|\CircleLinkHealth\Core\Entities\SendGridRawLog query()
+ * @mixin \Eloquent
+ */
 class SendGridRawLog extends BaseModel
 {
     protected $casts = [
