be79942e40ae8e4fd984b9e9dd56deeb2be22f48
Antonis Antoniou <kountouris7@gmail.com>
2020-06-18 14:57:55 +0300

    Adds postal_code to seeder and Updates from dev

--- Database/Migrations/2020_06_17_035707_create_sendgrid_raw_logs_table.php
+++ Database/Migrations/2020_06_17_035707_create_sendgrid_raw_logs_table.php
@@ -1,11 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateSendgridRawLogsTable extends Migration
 {
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sendgrid_raw_logs');
+    }
+
     /**
      * Run the migrations.
      *
@@ -18,15 +32,5 @@ class CreateSendgridRawLogsTable extends Migration
             $table->json('events')->nullable();
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('sendgrid_raw_logs');
     }
 }
