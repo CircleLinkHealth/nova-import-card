8cbf631dde83135f3121302820cbef1b83e0b2d1
Pangratios Cosma <pagcosma@gmail.com>
2020-06-23 20:00:04 +0300

    ROAD-187 new AppConfig.php

--- Entities/AppConfig.php
+++ Entities/AppConfig.php
@@ -6,6 +6,8 @@
 
 namespace CircleLinkHealth\Core\Entities;
 
+use Illuminate\Support\Collection;
+
 /**
  * CircleLinkHealth\Core\Entities\AppConfig.
  *
@@ -43,18 +45,73 @@ class AppConfig extends BaseModel
     protected $table = 'app_config';
 
     /**
+     * @var Collection|null
+     */
+    private static $config = null;
+
+    private static function setup()
+    {
+        if (static::$config) {
+            return;
+        }
+
+        static::$config = static::all()->map(function ($item) {
+            return [
+                'config_key'   => $item->config_key,
+                'config_value' => $item->config_value,
+            ];
+        });
+    }
+
+    /**
      * Returns the AppConfig value for the given key.
      *
      * @param null $default
      *
-     * @return string|null
+     * @return string|string[]|null
      */
     public static function pull(string $key, $default = null)
     {
-        $conf = static::whereConfigKey($key)->first();
+        static::setup();
+        $conf = static::$config
+            ->where('config_key', '=', $key);
 
-        return $conf
-            ? $conf->config_value
-            : $default;
+        $len = $conf->count();
+        if (1 === $len) {
+            return $conf->first()['config_value'];
+        }
+
+        if ($len > 1) {
+            return $conf
+                ->map(function ($item) {
+                    return $item['config_value'];
+                })->toArray();
+        }
+
+        return $default;
+    }
+
+    public static function set(string $key, $value)
+    {
+        $conf = AppConfig::updateOrCreate(
+            [
+                'config_key' => $key,
+            ],
+            [
+                'config_value' => $value,
+            ]
+        );
+
+        if ($conf) {
+            static::setup();
+            static::$config->push([
+                'config_key'   => $conf->config_key,
+                'config_value' => $conf->config_value,
+            ]);
+
+            return $conf->config_value;
+        }
+
+        return null;
     }
 }
