4e97623022552f4bbe57491ef40dfadbf88b01fe
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-06-25 13:57:13 -0500

    ROAD-138 Adds PhaxioNotificationStatusUpdateJob.php

---
+++ Jobs/PhaxioNotificationStatusUpdateJob.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\Core\Jobs;
+
+use App\FaxLog;
+use CircleLinkHealth\Core\Entities\DatabaseNotification;
+
+class PhaxioNotificationStatusUpdateJob extends NotificationStatusUpdateJob
+{
+    const CHANNEL_NAME = 'phaxio';
+    /**
+     * @var DatabaseNotification
+     */
+    private $notification;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(DatabaseNotification $notification, FaxLog $log, array $props = [])
+    {
+        parent::__construct($notification->id, self::CHANNEL_NAME, array_merge($props, [
+            'value'          => $log->status,
+            'details'        => $log->fax_id,
+            'event_type'     => $log->event_type,
+            'fax_id'         => $log->fax_id,
+            'cpm_fax_log_id' => $log->id,
+        ]));
+        $this->notification = $notification;
+    }
+
+    protected function getIdentifier()
+    {
+        return $this->notification->id;
+    }
+
+    protected function getNotification()
+    {
+        return $this->notification;
+    }
+}
