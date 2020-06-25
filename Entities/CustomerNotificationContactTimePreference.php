83104faf86f4a9ab611ca6be663b58dd57657cb5
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-06-25 14:58:27 -0500

    ROAD-138 Adds cache key

--- Entities/CustomerNotificationContactTimePreference.php
+++ Entities/CustomerNotificationContactTimePreference.php
@@ -33,6 +33,12 @@ use CircleLinkHealth\Core\Entities\BaseModel;
  */
 class CustomerNotificationContactTimePreference extends BaseModel
 {
+    const AUDIT_REPORTS_FAXES_PER_HOUR = 'audit_report_faxes_per_hour';
+
+    const VALID_CACHE_KEY_OPTIONS = [
+        self::AUDIT_REPORTS_FAXES_PER_HOUR,
+    ];
+
     protected $fillable = [
         'contactable_type',
         'contactable_id',
@@ -46,6 +52,15 @@ class CustomerNotificationContactTimePreference extends BaseModel
         'is_enabled',
         'max_per_hour',
     ];
+
+    public function cacheKey(string $cacheKeyOption)
+    {
+        if ( ! in_array($cacheKeyOption, self::VALID_CACHE_KEY_OPTIONS)) {
+            throw new \InvalidArgumentException("`$cacheKeyOption` is not a valid option. Valid options are ".json_encode(self::VALID_CACHE_KEY_OPTIONS));
+        }
+
+        return $cacheKeyOption.$this->contactable_type.$this->contactable_id.now()->startOfHour()->format('Y-m-d H:i');
+    }
 
     /**
      * Always defaults to the app timezone, ie 'America/New_York'.
