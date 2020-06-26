c1c4dd8270b1ffe46fa3eba56ce27585832a7364
Pangratios Cosma <pagcosma@gmail.com>
2020-06-26 15:29:01 +0300

    ROAD-195 lint

--- app/Http/Controllers/PostmarkController.php
+++ app/Http/Controllers/PostmarkController.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 namespace App\Http\Controllers;
 
 use CircleLinkHealth\Core\Jobs\ProcessPostmarkMailStatusCallbackJob;
@@ -10,7 +14,7 @@ class PostmarkController extends Controller
     public function statusCallback(Request $request)
     {
         ProcessPostmarkMailStatusCallbackJob::dispatch($request->all());
-    
+
         return response()->json([]);
     }
 }
