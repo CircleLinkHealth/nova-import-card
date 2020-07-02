2966a6d07ba20ad98e9b5fadf7d4112728da25a4
kakoushias <kakoushias@gmail.com>
2020-07-02 21:03:42 +0300

    hotfix - make pms approved field boolean, not nullable, and default to 0

---
+++ Database/Migrations/2020_07_02_133907_change_patient_monthly_summaries_approved_column_to_boolean_type.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePatientMonthlySummariesApprovedColumnToBooleanType extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('patient_monthly_summaries', 'approved')) {
+            Schema::table('patient_monthly_summaries', function (Blueprint $table) {
+                $table->boolean('approved')->nullable(0)->default(0)->change();
+            });
+        }
+    }
+}
