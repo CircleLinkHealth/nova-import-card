ded604f6cd394268843430766edc8ff81d29f23e
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-07-11 12:55:54 -0500

    Lessen ScheduledTasks frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -158,10 +158,10 @@ class Kernel extends ConsoleKernel
             CreateApprovableBillablePatientsReport::class,
             ['--reset-actor', now()->startOfMonth()->toDateString()]
         )
-            ->twiceDaily(12, 16);
+            ->dailyAt('23:00');
 
         $schedule->command(CountPatientMonthlySummaryCalls::class, [now()->startOfMonth()->toDateString()])
-            ->twiceDaily(7, 21);
+            ->dailyAt('21:00');
 
 //        $schedule->command(
 //            SendCareCoachInvoices::class,
