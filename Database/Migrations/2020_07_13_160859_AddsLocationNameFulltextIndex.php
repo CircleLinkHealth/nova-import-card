cd1bf640c6775b321629c5da7b434ed03000acc7
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-07-13 15:20:56 -0500

    Adds location name full index

---
+++ Database/Migrations/2020_07_13_160859_AddsLocationNameFulltextIndex.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddsLocationNameFulltextIndex extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //for CCD Location Search
+        DB::statement('ALTER TABLE locations ADD FULLTEXT full(name)');
+    }
+}
