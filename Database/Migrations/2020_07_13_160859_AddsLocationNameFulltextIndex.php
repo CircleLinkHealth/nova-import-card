b3a7cda13cd2b45f03d56f33c601a1dd38d3f9bb
Pangratios Cosma <pagcosma@gmail.com>
2020-07-14 14:50:21 +0300

    ROAD-98 sms inbound route, table for twilio inbound sms

--- Database/Migrations/2020_07_13_160859_AddsLocationNameFulltextIndex.php
+++ Database/Migrations/2020_07_13_160859_AddsLocationNameFulltextIndex.php
@@ -5,8 +5,6 @@
  */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class AddsLocationNameFulltextIndex extends Migration
 {
---
+++ Database/Migrations/2020_07_14_072253_create_twilio_inbound_sms_table.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTwilioInboundSmsTable extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('twilio_inbound_sms');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twilio_inbound_sms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->json('data');
+
+            $table->string('from')
+                ->virtualAs('JSON_UNQUOTE(data->"$.From")')
+                ->index();
+
+            $table->string('to')
+                ->virtualAs('JSON_UNQUOTE(data->"$.To")')
+                ->index();
+
+            $table->string('body')
+                ->virtualAs('JSON_UNQUOTE(data->"$.Body")')
+                ->index();
+
+            $table->timestamps();
+        });
+    }
+}
