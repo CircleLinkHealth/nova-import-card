ddc6845884e5b97bd92769eac177395782c40387
kakoushias <kakoushias@gmail.com>
2020-07-15 21:43:16 +0200

    ROAD-218 fix bug

--- app/Services/Enrollment/PracticeKPIs.php
+++ app/Services/Enrollment/PracticeKPIs.php
@@ -6,7 +6,6 @@
 
 namespace App\Services\Enrollment;
 
-use CircleLinkHealth\Customer\Entities\CareAmbassador;
 use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Eligibility\Entities\Enrollee;
 use CircleLinkHealth\TimeTracking\Entities\PageTimer;
@@ -86,140 +85,11 @@ class PracticeKPIs
             ->setLaborRate()
             ->setAcqCost()
             ->toArray();
-        $data = [];
-
-        $data['name'] = $this->practice->display_name;
-
-        $data['unique_patients_called'] = Enrollee::where('practice_id', $this->practice->id)
-            ->whereNotNull('care_ambassador_user_id')
-            ->where('last_attempt_at', '>=', $this->start)
-            ->where('last_attempt_at', '<=', $this->end)
-            //status are needed here for the sake of end-user seeing numbers. E.g a stat is not shown for an ineligible patient, so don't count in totals
-            ->whereIn('status', [
-                Enrollee::UNREACHABLE,
-                Enrollee::CONSENTED,
-                Enrollee::ENROLLED,
-                Enrollee::REJECTED,
-                Enrollee::SOFT_REJECTED,
-            ])
-            ->count();
-
-        $data['consented'] = Enrollee::where('practice_id', $this->practice->id)
-            ->whereNotNull('care_ambassador_user_id')
-            ->where('last_attempt_at', '>=', $this->start)
-            ->where('last_attempt_at', '<=', $this->end)
-            ->whereIn('status', [Enrollee::CONSENTED, Enrollee::ENROLLED])
-            ->count();
-
-        $data['utc'] = Enrollee::where('practice_id', $this->practice->id)
-            ->whereNotNull('care_ambassador_user_id')
-            ->where('last_attempt_at', '>=', $this->start)
-            ->where('last_attempt_at', '<=', $this->end)
-            ->where('status', Enrollee::UNREACHABLE)
-            ->count();
-
-        $data['hard_declined'] = Enrollee::where('practice_id', $this->practice->id)
-            ->whereNotNull('care_ambassador_user_id')
-            ->where('last_attempt_at', '>=', $this->start)
-            ->where('last_attempt_at', '<=', $this->end)
-            ->where('status', Enrollee::REJECTED)
-            ->count();
-
-        $data['soft_declined'] = Enrollee::where('practice_id', $this->practice->id)
-            ->whereNotNull('care_ambassador_user_id')
-            ->where('last_attempt_at', '>=', $this->start)
-            ->where('last_attempt_at', '<=', $this->end)
-            ->where('status', Enrollee::SOFT_REJECTED)
-            ->count();
-
-        $total_time = Enrollee
-                ::where('practice_id', $this->practice->id)
-                    ->where('last_attempt_at', '>=', $this->start)
-                    ->where('last_attempt_at', '<=', $this->end)
-                    ->sum('total_time_spent');
-
-        $data['labor_hours'] = secondsToHMS($total_time);
-
-        $data['incomplete_3_attempts'] = Enrollee
-                ::where('practice_id', $this->practice->id)
-                    ->where('last_attempt_at', '>=', $this->start)
-                    ->where('last_attempt_at', '<=', $this->end)
-                    ->where('attempt_count', '>=', 3)
-                    ->whereNotIn(
-                        'status',
-                        [Enrollee::UNREACHABLE, Enrollee::SOFT_REJECTED, Enrollee::REJECTED]
-                    )
-                    ->count();
-
-        $enrollers = Enrollee::select(DB::raw('care_ambassador_user_id, sum(total_time_spent) as total'))
-            ->where('practice_id', $this->practice->id)
-            ->where('last_attempt_at', '>=', $this->start)
-            ->where('last_attempt_at', '<=', $this->end)
-            ->groupBy('care_ambassador_user_id')
-            ->pluck('total', 'care_ambassador_user_id');
-
-        $data['total_cost'] = 0;
-
-        foreach ($enrollers as $enrollerId => $time) {
-            if (empty($enrollerId)) {
-                continue;
-            }
-
-            $enroller = CareAmbassador::where('user_id', $enrollerId)->first();
-            if ( ! $enroller) {
-                continue;
-            }
-            $data['total_cost'] += $enroller->hourly_rate * $time / 3600;
-        }
-
-        if ($data['unique_patients_called'] > 0 && $data['consented'] > 0) {
-            $data['conversion'] = number_format(
-                $data['consented'] / $data['unique_patients_called'] * 100,
-                2
-            ).'%';
-        } else {
-            $data['conversion'] = 'N/A';
-        }
-
-        if ($data['total_cost'] > 0 && $data['consented'] > 0) {
-            $data['acq_cost'] = '$'.number_format(
-                $data['total_cost'] / $data['consented'],
-                2
-            );
-        } else {
-            $data['acq_cost'] = 'N/A';
-        }
-
-        if ($data['total_cost'] > 0 && $total_time > 0) {
-            $data['labor_rate'] = '$'.number_format(
-                $data['total_cost'] / ($total_time / 3600),
-                2
-            );
-        } else {
-            $data['labor_rate'] = 'N/A';
-        }
-
-        $data['total_cost'] = '$'.number_format($data['total_cost'], 2);
-
-        return $data;
-    }
-
-    private function setAcqCost()
-    {
-        $totalCostAndConsentedExist = $this->totalCost > 0 && $this->consented > 0;
-        $this->acqCost              = $totalCostAndConsentedExist ?
-            '$'.number_format(
-                $this->totalCost / $this->consented,
-                2
-            )
-       : 'N/A';
-
-        return $this;
     }
 
     private function setConsented()
     {
-        $this->consented = $this->practiceEnrollees->whereIn('status', [
+        $this->consented = $this->practiceEnrollees->whereIn('enrollee_status', [
             Enrollee::CONSENTED,
             Enrollee::ENROLLED,
         ])
@@ -245,7 +115,7 @@ class PracticeKPIs
 
     private function setHardDeclined()
     {
-        $this->hardDeclined = $this->practiceEnrollees->where('status', Enrollee::REJECTED)
+        $this->hardDeclined = $this->practiceEnrollees->where('enrollee_status', Enrollee::REJECTED)
             ->count();
 
         return $this;
@@ -255,7 +125,7 @@ class PracticeKPIs
     {
         $this->incompleteThreeAttempts = $this->practiceEnrollees->where('enrollee_attempt_count', '>=', 3)
             ->whereNotIn(
-                'status',
+                'enrollee_status',
                 [Enrollee::UNREACHABLE, Enrollee::SOFT_REJECTED, Enrollee::REJECTED]
             )->count();
 
@@ -312,7 +182,7 @@ class PracticeKPIs
 
     private function setSoftDeclined()
     {
-        $this->softDeclined = $this->practiceEnrollees->where('status', Enrollee::SOFT_REJECTED)
+        $this->softDeclined = $this->practiceEnrollees->where('enrollee_status', Enrollee::SOFT_REJECTED)
             ->count();
 
         return $this;
@@ -342,7 +212,7 @@ class PracticeKPIs
 
     private function setUtc()
     {
-        $this->utc = $this->practiceEnrollees->where('status', Enrollee::UNREACHABLE)
+        $this->utc = $this->practiceEnrollees->where('enrollee_status', Enrollee::UNREACHABLE)
             ->count();
 
         return $this;
