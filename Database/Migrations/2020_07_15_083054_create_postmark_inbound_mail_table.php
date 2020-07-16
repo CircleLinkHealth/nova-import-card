11be4533537047644e975e00b538eb29a181606d
Pangratios Cosma <pagcosma@gmail.com>
2020-07-16 12:16:24 +0300

    ROAD-98 postmark_inbound_mail table

---
+++ Database/Migrations/2020_07_15_083054_create_postmark_inbound_mail_table.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostmarkInboundMailTable extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postmark_inbound_mail');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postmark_inbound_mail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->json('data');
+
+            $table->string('from')
+                ->virtualAs('JSON_UNQUOTE(data->"$.From")')
+                ->index();
+
+            $table->string('to')
+                ->virtualAs('JSON_UNQUOTE(data->"$.To")')
+                ->index();
+
+            $table->string('body')
+                ->virtualAs('JSON_UNQUOTE(data->"$.HtmlBody")')
+                ->index();
+
+            $table->timestamps();
+        });
+    }
+}
