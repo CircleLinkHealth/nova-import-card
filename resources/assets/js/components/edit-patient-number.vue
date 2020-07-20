0a2542df9566c554acc230e69b4e4e8de49a2882
Antonis Antoniou <kountouris7@gmail.com>
2020-07-21 01:08:09 +0300

    Road - 222 Edit Patient Profile phone number vue.js

--- resources/assets/js/components/edit-patient-number.vue
+++ resources/assets/js/components/edit-patient-number.vue
@@ -1,31 +1,29 @@
 <template>
-    <div class="col-xs-9 no-padding">
+    <div class="phone-numbers">
         <div class="input-group">
             <template v-if="true" v-for="number in phoneNumbers">
-                <a class="input-group-addon">+1</a>
-                <input name="number"
-                       class="form-control" type="tel"
-                       title="10-digit US Phone Number" placeholder="2345678901"
-                       :value="number.number"
-                       />
+                <div style="display: inline-flex; padding-right: 5px;">
+                    <input name="type"
+                           class="form-control phone-type" type="text"
+                           title="Please select telephone type" placeholder="Home"
+                           :value="number.type"
+                           :disabled="inputDisabled"/>
+                </div>
+              <div style="display: inline-flex; padding-bottom: 10px;">
+                  <span class="input-group-addon" style="padding-right: 26px; padding-top: 10px;">+1</span>
+                  <input name="number"
+                         class="form-control phone-number" type="tel"
+                         title="10-digit US Phone Number" placeholder="2345678901"
+                         :value="number.number"
+                         :disabled="inputDisabled"/>
+                  <a class="glyphicon glyphicon-pencil edit-phone"></a>
+                  <a class="glyphicon glyphicon-remove remove-phone"></a>
+              </div>
+                <br>
             </template>
-            <template v-else>
-                <input v-for="number in phoneNumbers" name="patient-unlisted-number"
-                       maxlength="10" minlength="10"
-                       class="form-control" type="tel"
-                       title="10-digit US Phone Number" placeholder="2345678901"
-                       :value="number.number"
-                       />
-            </template>
+
         </div>
     </div>
-<!--    <div>-->
-<!--       <ul>-->
-<!--           <li v-for="number in phoneNumbers">-->
-<!--              {{number.type}}: {{number.number}}-->
-<!--           </li>-->
-<!--       </ul>-->
-<!--    </div>-->
 </template>
 
 <script>
@@ -43,12 +41,32 @@
 
         data(){
             return {
-                phoneNumber:''
+                phoneNumber:'',
+                inputDisabled:true,
             }
         }
     }
 </script>
 
 <style scoped>
+.phone-numbers{
+    float: left;
+}
+    .phone-type{
+        max-width: 80px;
+        text-align: center;
+    }
+    .edit-phone{
+        padding-left: 10px;
+        padding-top: 5px;
+        color: #50b2e2;
+        cursor: pointer;
+    }
+    .remove-phone{
+        padding-left: 10px;
+        padding-top: 5px;
+        color: red;
+        cursor: pointer;
+    }
 
 </style>
