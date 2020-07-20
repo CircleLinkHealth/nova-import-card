475f7f0544e089cab826bbe193345396e0ed9f72
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-07-20 14:12:35 -0500

    composer

---
+++ app/Console/Commands/FixDuplicateMedications.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App\Console\Commands;
+
+use CircleLinkHealth\Customer\Entities\User;
+use Illuminate\Console\Command;
+
+class FixDuplicateMedications extends Command
+{
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove duplicate Medications from all patients.';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fix:duplicate-medications';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::ofType('participant')
+            ->whereIn('id', function ($q) {
+                $q->select('patient_id')
+                    ->from('ccd_medications')
+                    ->groupBy('patient_id', 'name')
+                    ->havingRaw('COUNT(*) > 1');
+            })->orderByDesc('id')
+            ->with('ccdMedications')
+            ->chunk(50, function ($users) {
+                foreach ($users as $user) {
+                    $deleted = $user->ccdMedications()->whereNotIn('id', $user->ccdMedications->unique('name')->pluck('id')->all())->delete();
+                    $this->info("Deleted $deleted medications for patient_user[{$user->id}]");
+                }
+            });
+    }
+}
