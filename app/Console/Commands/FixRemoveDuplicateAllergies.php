d22ffa727b9dc306463ccdac8df6ddb1c4823829
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-07-20 14:43:37 -0500

    Adds FixRemoveDuplicateAllergies.php

--- app/Console/Commands/FixRemoveDuplicateAllergies.php
+++ app/Console/Commands/FixRemoveDuplicateAllergies.php
@@ -9,20 +9,20 @@ namespace App\Console\Commands;
 use CircleLinkHealth\Customer\Entities\User;
 use Illuminate\Console\Command;
 
-class FixDuplicateMedications extends Command
+class FixRemoveDuplicateAllergies extends Command
 {
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Remove duplicate Medications from all patients.';
+    protected $description = 'Remove duplicate Allergies from all patients.';
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'fix:duplicate-medications';
+    protected $signature = 'fix:duplicate-allergies';
 
     /**
      * Execute the console command.
@@ -34,18 +34,18 @@ class FixDuplicateMedications extends Command
         User::ofType('participant')
             ->whereIn('id', function ($q) {
                 $q->select('patient_id')
-                    ->from('ccd_medications')
-                    ->groupBy('patient_id', 'name')
+                    ->from('ccd_allergies')
+                    ->groupBy('patient_id', 'allergen_name')
                     ->havingRaw('COUNT(*) > 1');
             })->orderByDesc('id')
-            ->with('ccdMedications')
-            ->chunk(200, function ($users) {
+            ->with('ccdAllergies')
+            ->chunk(100, function ($users) {
                 \DB::transaction(function () use ($users) {
                     foreach ($users as $user) {
-                        $deleted = $user->ccdMedications()->whereNotIn('id', $user->ccdMedications->unique(function ($m) {
-                            return str_replace(' ', '', strtolower($m->name));
+                        $deleted = $user->ccdAllergies()->whereNotIn('id', $user->ccdAllergies->unique(function ($m) {
+                            return str_replace(' ', '', strtolower($m->allergen_name));
                         })->pluck('id')->all())->delete();
-                        $this->info("Deleting $deleted medications for patient_user[{$user->id}]");
+                        $this->info("Deleting $deleted allergies for patient_user[{$user->id}]");
                     }
                 });
             });
