05d333f363756f18adb5671a86a1d9e50b943e69
Antonis Antoniou <kountouris7@gmail.com>
2020-07-22 21:05:03 +0300

    Road - 251 - UI letter

--- Database/Seeders/GenerateCalvaryClinicLetter.php
+++ Database/Seeders/GenerateCalvaryClinicLetter.php
@@ -14,6 +14,8 @@ use Illuminate\Database\Seeder;
 
 class GenerateCalvaryClinicLetter extends Seeder
 {
+    const UI_REQUESTS = 'ui_requests';
+
     /**
      * Run the database seeds.
      *
@@ -29,24 +31,24 @@ class GenerateCalvaryClinicLetter extends Seeder
         $calvaryPractice = $this->getPractice();
 
         $bodyPageOne = "
-<p>$practiceName has invested in a new Personalized Care Program to help patients get care at home, which is especially important given current events, and I'm inviting you to join.<li></p>
+<p>$practiceName has invested in a new Personalized Care Program to help patients get care at home, which is especially important given current events, and I'm inviting you to join.</p>
 
-<p>You are getting this invitation because you're eligible according to Medicare guidelines, and we believe you will benefit from it greatly, particularly during this pandemic.<li></p>
+<p>You are getting this invitation because you're eligible according to Medicare guidelines, and we believe you will benefit from it greatly, particularly during this pandemic.</p>
 
-<p>Here's how it works:<li><br /><br /><li></p>
-<ul>
-<li>You'll get monthly calls from a Registered Nurse Care Coach to help you manage your health conditions, so you can stay as active and healthy as you can be.<li><br /><br /><li></li>
-<li>By staying healthy in between office visits, you'll be less likely to need extra/expensive medical care, including visits to the ER or the hospital, which helps reduce your medical bills. <li><br /><br /><li></li>
-<li>You can avoid being on hold when you need something: your nurse can help with prescription refills, appointment scheduling, transportation assistance, and any general questions.<li><br /><br /><li></li>
-<li>You can disenroll at any time. This is a voluntary program meant to provide assistance and benefits outside of our physical office.
-</ul>
-<p>What's the Cost?<li></p>
-<p>The program is covered by Medicare. If you have Medicaid or a supplemental insurance, it will likely cover the copay, which means you'll have $0 out-of-pocket costs. In addition, during this crisis, your Dr. may waive co-pays for this kind of remote care. Medicare has invested in this program because it saves them money by keeping people like you healthy.<li></p>
+<p>Here's how it works:</p>
 
-<p>What's Next?<li></p>
-<p>In a few days, you'll get a call from one of our care coordinators from $practiceNumber. They'll be happy to answer your questions, and help you get started if you decide to join during that call.<li></p>
+<li>You'll get monthly calls from a Registered Nurse Care Coach to help you manage your health conditions, so you can stay as active and healthy as you can be.</li>
+<li>By staying healthy in between office visits, you'll be less likely to need extra/expensive medical care, including visits to the ER or the hospital, which helps reduce your medical bills.</li>
+<li>You can avoid being on hold when you need something: your nurse can help with prescription refills, appointment scheduling, transportation assistance, and any general questions.</li>
+<li>You can disenroll at any time. This is a voluntary program meant to provide assistance and benefits outside of our physical office.</li>
 
-<p>I look forward to having you join this program to continue keeping you healthy between office visits.<li></p>
+<p>What's the Cost?</p>
+<p>The program is covered by Medicare. If you have Medicaid or a supplemental insurance, it will likely cover the copay, which means you'll have $0 out-of-pocket costs. In addition, during this crisis, your Dr. may waive co-pays for this kind of remote care. Medicare has invested in this program because it saves them money by keeping people like you healthy.</p>
+
+<p>What's Next?</p>
+<p>In a few days, you'll get a call from one of our care coordinators from $practiceNumber. They'll be happy to answer your questions, and help you get started if you decide to join during that call.</p>
+
+<p>I look forward to having you join this program to continue keeping you healthy between office visits.</p>
 <p>Sincerely,</p>
 <p>$customerSignaturePic<br />$signatoryName<br/></p>";
 
@@ -65,6 +67,17 @@ class GenerateCalvaryClinicLetter extends Seeder
                         ],
                     ]
                 ),
+                self::UI_REQUESTS => json_encode([
+                    'logo_position'        => 'text-align:right',
+                    'extra_address_header' => [
+                        $calvaryPractice->name => [
+                            'address_line_1',
+                            'city',
+                            'state',
+                            'postal_code', // zip
+                        ],
+                    ],
+                ]),
             ]
         );
     }
