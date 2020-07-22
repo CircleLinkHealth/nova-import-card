93e762d6c65547cc8db2f8fc236eaf400f354b0d
Antonis Antoniou <kountouris7@gmail.com>
2020-07-22 19:50:43 +0300

    Road - 251_Calvary Letter

---
+++ resources/views/enrollment-letters/calvaryMedicalClinicLetter.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.EnrollmentSurvey.enrollmentLetterMaster')
+@section('title', 'Calvary Invitation')
+@section('activity', 'Calvary Invitation')
+@section('content')
+    <div class="container">
+        <div class="letter-view">
+            @if(!$hideButtons)
+                <div class="header-buttons">
+                    @include('enrollment-letters.enrollableInvitationButtons')
+                </div>
+            @endif
+
+            @include('enrollment-letters.headers.calvary')
+
+            @include('enrollment-letters.baseLetter')
+
+            <div class="logo" style="margin-bottom: 10px">
+                @include('enrollment-letters.practiceLogo')
+            </div>
+
+
+            @if(!$hideButtons)
+                <div class="header-buttons">
+                    @include('enrollment-letters.enrollableInvitationButtons')
+                </div>
+            @endif
+        </div>
+    </div>
+    </div>
+
+
+@endsection
+
+<style>
+    .logo {
+        text-align: center;
+    }
+
+    .request-info-href {
+        padding: 10px;
+        text-decoration: underline;
+        font-size: 17px;
+    }
+
+    .letter {
+        width: fit-content;
+        max-height: 590px;
+        overflow-y: scroll;
+    }
+
+    /*.headers {
+        padding-top: 10px;
+    }*/
+
+    .letter-sent {
+        float: right;
+        font-weight: 500;
+        margin-right: 8em;
+        font-size: 20px;
+    }
+
+    .letter-head {
+        font-family: Tahoma;
+        padding-bottom: 15px;
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .letter-body {
+        font-family: Tahoma;
+        text-align: left;
+        font-size: 20px;
+    }
+
+    .letter-view {
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
+    .header {
+        padding-bottom: 30px;
+    }
+
+    .header-buttons{
+        text-align: center;
+    }
+
+    @media (max-width: 490px) {
+        /*.letter-view {
+            width: 504px;
+        }*/
+
+        .letter {
+            width: fit-content;
+            min-height: 1073px;
+            overflow-y: scroll;
+        }
+
+        .letter-sent {
+            margin-right: 2em;
+        }
+
+    }
+
+</style>
