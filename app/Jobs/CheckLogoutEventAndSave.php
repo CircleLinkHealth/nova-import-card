e0a5875a6d78cd317356ecf0b93394a2a5815de1
Antonis Antoniou <kountouris7@gmail.com>
2020-07-27 18:09:02 +0300

    CheckLogoutEventAndSave refactor

--- app/Jobs/CheckLogoutEventAndSave.php
+++ app/Jobs/CheckLogoutEventAndSave.php
@@ -6,14 +6,13 @@
 
 namespace App\Jobs;
 
-use App\Services\LoginLogoutActivityService;
+use App\LoginLogout;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Collection;
 
 class CheckLogoutEventAndSave implements ShouldQueue
 {
@@ -25,41 +24,39 @@ class CheckLogoutEventAndSave implements ShouldQueue
      * @var Carbon
      */
     private $date;
+    /**
+     * @var int
+     */
+    private $id;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(Carbon $date)
+    public function __construct(Carbon $date, int $id)
     {
         $this->date = $date;
-    }
-
-    public function checkLogoutEvent()
-    {
-        $yesterdaysEvents = $this->getYesterdaysEvents();
-        foreach ($yesterdaysEvents as $event) {
-            if (null === $event->logout_time) {
-                $this->saveLogoutEvent($event);
-            }
-        }
-    }
-
-    public function getEndTimeOfLatestActivityAfterLogin($event)
-    {
-        //@todo: If it is the last activity(page_timer) for the day, then where to set the logout from?
-        $latestActivityAfterLogin = $event->activities->sortBy('end_time')->where('end_time', '>=', $event->login_time)
-            ->where('end_time', '<=', Carbon::parse($event->login_time)->endOfDay())
-            ->first();
-
-        return Carbon::parse($latestActivityAfterLogin->end_time)->toDateTime();
+        $this->id   = $id;
     }
 
     /**
-     * @return Collection
+     * @return \DateTime|null
      */
-    public function getYesterdaysEvents()
+    public function getEndTimeOfLatestActivityAfterLogin(LoginLogout $event)
     {
-        return LoginLogoutActivityService::yesterdaysActivity($this->date);
+        //@todo: If it is the last activity(page_timer) for the day, then where to set the logout from?
+        $latestActivityAfterLogin = $event->activities->sortBy('end_time')
+            ->where('end_time', '>=', $event->login_time)
+            ->where('end_time', '<=', Carbon::parse($event->login_time)->endOfDay())
+            ->first();
+
+        if (is_null($latestActivityAfterLogin)) {
+            // We can exit loop or create a logout time at the end of day?
+            //            return Carbon::parse($event->login_time)->copy()->endOfDay()->toDateTime();
+
+            return null;
+        }
+
+        return $latestActivityAfterLogin->end_time;
     }
 
     /**
@@ -67,22 +64,47 @@ class CheckLogoutEventAndSave implements ShouldQueue
      */
     public function handle()
     {
-        $this->checkLogoutEvent();
+        /** @var LoginLogout $loginEventWithActivities */
+        $loginEventWithActivities = $this->eventWithActivities();
+
+        if (is_null($loginEventWithActivities)) {
+            return;
+        }
+
+        /** @var Carbon $latestActivityEndTime */
+        $latestActivityEndTime = $this->getEndTimeOfLatestActivityAfterLogin($loginEventWithActivities);
+
+        // If is null then exit loop.
+        if (is_null($latestActivityEndTime)) {
+            return;
+        }
+
+        if (empty($loginEventWithActivities->logout_time)) {
+            $this->saveLogoutEvent($loginEventWithActivities, $latestActivityEndTime);
+        }
     }
 
-    /**
-     * @param $event
-     * @param Carbon $date
-     */
-    public function saveLogoutEvent($event)
+    public function saveLogoutEvent(LoginLogout $event, Carbon $latestActivityEndTime)
     {
-        $endTime = $this->getEndTimeOfLatestActivityAfterLogin($event);
         try {
-            $event->logout_time = $endTime;
-            $event->was_edited  = true;
+            $event->logout_time     = $latestActivityEndTime;
+            $event->was_edited      = true;
+            $event->duration_in_sec = $latestActivityEndTime->diffInSeconds($event->login_time);
             $event->save();
         } catch (\Exception $exception) {
-            \Log::error(`Couldnt get end_time from page_timer table, for logout event $event->id`);
+            \Log::error("Could not get end_time from page_timer table, for logout event $event->id");
         }
+    }
+
+    private function eventWithActivities()
+    {
+        return LoginLogout::with([
+            'activities' => function ($activity) {
+                $activity->whereBetween('end_time', [$this->date->copy()->startOfDay(), $this->date->copy()->endOfDay()]);
+            },
+        ])->where('id', $this->id)
+            ->whereHas('activities', function ($activity) {
+                $activity->whereBetween('end_time', [$this->date->copy()->startOfDay(), $this->date->copy()->endOfDay()]);
+            })->first();
     }
 }
