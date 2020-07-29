ce1be74316548397da173f5ef5a646ba466ec10e
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-07-28 20:30:59 -0500

    Fix

--- resources/views/enrollment-letters/practiceLogo.blade.php
+++ resources/views/enrollment-letters/practiceLogo.blade.php
@@ -1,16 +1,18 @@
-<div class="practice-logo">
-    <img src="{{$practiceLogoSrc}}"
-         alt="{{$practiceDisplayName}}"/>
-</div>
+@if(isset($practiceLogoSrc) && isset($practiceDisplayName))
+    <div class="practice-logo">
+        <img src="{{$practiceLogoSrc}}"
+             alt="{{$practiceDisplayName}}"/>
+    </div>
+@endif
 <style>
     .practice-logo img{
         height: 120px;
     }
 
     @media (max-width: 490px) {
-    .practice-logo img{
-        height: 90px;
-    }
+        .practice-logo img{
+            height: 90px;
+        }
 
     }
 
