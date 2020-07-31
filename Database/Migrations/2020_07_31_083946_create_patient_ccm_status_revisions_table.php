c6d0d4accc0ad1f4cb12b9b0d1e27df736f6223a
kakoushias <kakoushias@gmail.com>
2020-07-31 14:52:18 +0200

    ROAD-218 adds patient Ccm status revisions table

---
+++ Database/Migrations/2020_07_31_083946_create_patient_ccm_status_revisions_table.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientCcmStatusRevisionsTable extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_ccm_status_revisions');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient_ccm_status_revisions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('patient_info_id')->nullable();
+            $table->unsignedInteger('patient_user_id')->nullable();
+            $table->string('old_ccm_status')->nullable();
+            $table->string('new_ccm_status')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('patient_info_id')
+                ->references('id')
+                ->on('patient_info')
+                ->onDelete('set null');
+
+            $table->foreign('patient_user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+    }
+}
