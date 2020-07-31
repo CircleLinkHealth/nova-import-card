1aace5d33492dde7778823776cbec3b077febcab
kakoushias <kakoushias@gmail.com>
2020-07-31 20:03:02 +0200

    ROAD-267 add index

---
+++ Database/Migrations/2020_07_31_140148_add_indexes_to_patient_ccm_status_revisions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexesToPatientCcmStatusRevisionsTable extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patient_ccm_status_revisions', function (Blueprint $table) {
+            $table->index(['created_at'], 'created_at_index');
+        });
+    }
+}
