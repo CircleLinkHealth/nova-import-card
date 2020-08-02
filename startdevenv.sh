fceb65f70d9d06f0b83aa0e45d960df21c9f3c6a
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-02 12:11:20 -0500

    Separate build commands for greater flexibility

--- startdevenv.sh
+++ startdevenv.sh
@@ -8,16 +8,24 @@ SECONDS=0
 
 docker-compose --env-file=.env up --build --remove-orphans -d
 
+docker-compose exec app composer install
+
 if [[ ! -z "$DB_NAME" ]]; then
     echo "CREATING DB $DB_NAME"
     docker-compose exec app php artisan mysql:createdb $DB_NAME
 fi
 
-docker-compose exec app npm run build-dev
+docker-compose exec app php artisan migrate
+
+docker-compose exec app php artisan migrate:views
 
 if [[ ! -z "$DB_NAME" ]]; then
     docker-compose exec app php artisan db:seed --class=TestSuiteSeeder
 fi
+
+docker-compose exec app npm install
+
+docker-compose exec app npm run bower_install
 
 ELAPSED="Elapsed: $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 
