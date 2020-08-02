7c8989c63a04e13ca574e6338bc337d071b3db7b
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-02 12:25:04 -0500

    small changes

--- startdevenv.sh
+++ startdevenv.sh
@@ -2,24 +2,31 @@
 
 set -e
 
-DB_NAME=$1
+# If this is argument is passed, the script will create a new DB and seed it
+# example:
+# bash startdevenv.sh cpm_tests
+NEW_DB_NAME=$1
+
 # Reset BASH time counter
 SECONDS=0
 
+# Fire up the container
 docker-compose --env-file=.env up --build --remove-orphans -d
 
 docker-compose exec app composer install
 
-if [[ ! -z "$DB_NAME" ]]; then
-    echo "CREATING DB $DB_NAME"
-    docker-compose exec app php artisan mysql:createdb $DB_NAME
+# Create a database if a DB name was passed to the script
+if [[ ! -z "$NEW_DB_NAME" ]]; then
+    echo "CREATING DB $NEW_DB_NAME"
+    docker-compose exec app php artisan mysql:createdb $NEW_DB_NAME
 fi
 
 docker-compose exec app php artisan migrate
 
 docker-compose exec app php artisan migrate:views
 
-if [[ ! -z "$DB_NAME" ]]; then
+# Seed the database if a DB name was passed to the script
+if [[ ! -z "$NEW_DB_NAME" ]]; then
     docker-compose exec app php artisan db:seed --class=TestSuiteSeeder
 fi
 
