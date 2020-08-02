215c54172ef525c3e4731e714f121a3a43c8fcc2
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-02 11:21:29 -0500

    Adds DB name and changes port to 8080

--- startdevenv.sh
+++ startdevenv.sh
@@ -2,19 +2,25 @@
 
 set -e
 
+DB_NAME=$1
 # Reset BASH time counter
 SECONDS=0
 
 docker-compose --env-file=.env up --build --remove-orphans -d
 
-docker-compose exec app php artisan mysql:createdb cpm_tests
+if [[ ! -z "$DB_NAME" ]]; then
+    echo "CREATING DB $DB_NAME"
+    docker-compose exec app php artisan mysql:createdb $DB_NAME
+fi
 
 docker-compose exec app npm run build-dev
 
-docker-compose exec app php artisan db:seed --class=TestSuiteSeeder
+if [[ ! -z "$DB_NAME" ]]; then
+    docker-compose exec app php artisan db:seed --class=TestSuiteSeeder
+fi
 
 ELAPSED="Elapsed: $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 
 echo "Time elapsed: $ELAPSED seconds."
 
-echo "Done! You may head to: http://localhost:8085"
+echo "Done! You may head to: http://localhost:8080"
