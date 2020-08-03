b47501cf97a85be111f16af95ae8455658f1b4a9
kakoushias <kakoushias@gmail.com>
2020-08-03 22:33:31 +0200

    ROAD-218 add command, add to migrations

--- Database/Migrations/2020_07_21_175131_addCaLoadingTimeToEnrollees.php
+++ Database/Migrations/2020_07_21_175131_addCaLoadingTimeToEnrollees.php
@@ -24,47 +24,6 @@ class AddCaLoadingTimeToEnrollees extends Migration
      */
     public function up()
     {
-        \CircleLinkHealth\Customer\Entities\User::ofType('care-ambassador')
-            ->with(['assignedEnrollees' => function ($e) {
-                $e->whereNotNull('last_attempt_at');
-            },
-                'pageTimersAsProvider' => function ($pt) {
-                    $pt->whereNull('enrollee_id');
-                }, ])
-            ->chunk(2, function ($caUsers) {
-                foreach ($caUsers as $ca) {
-                    $enrollees = $ca->assignedEnrollees->all();
-
-                    if (empty($enrollees)) {
-                        continue;
-                    }
-
-                    $enrolleesCount = count($enrollees);
-
-                    $loadingTimers = $ca->pageTimersAsProvider->all();
-
-                    if (empty($loadingTimers)) {
-                        continue;
-                    }
-
-                    $loadingTimersCount = count($loadingTimers);
-
-                    $i = 0;
-                    $enrolleeIndex = 0;
-                    while ($i < $loadingTimersCount) {
-                        $timer = $loadingTimers[$i];
-
-                        $enrolleeIndex = $enrolleeIndex < $enrolleesCount ? $enrolleeIndex : $enrolleeIndex - $enrolleesCount;
-
-                        $enrollee = $enrollees[$enrolleeIndex];
-
-                        $timer->enrollee_id = $enrollee->id;
-                        $timer->save();
-
-                        ++$i;
-                        ++$enrolleeIndex;
-                    }
-                }
-            });
+        \Illuminate\Support\Facades\Artisan::call('ca:unassignedTimeToEnrollees');
     }
 }
