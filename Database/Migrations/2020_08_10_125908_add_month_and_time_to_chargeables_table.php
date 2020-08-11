25b94edd3e2309ffba4ff17bbdddfac672158fe3
kakoushias <kakoushias@gmail.com>
2020-08-11 23:02:47 +0200

    billing - add and rename fields on chargeables table

--- Database/Migrations/2020_08_10_125908_add_month_and_time_to_chargeables_table.php
+++ Database/Migrations/2020_08_10_125908_add_month_and_time_to_chargeables_table.php
@@ -18,7 +18,7 @@ class AddMonthAndTimeToChargeablesTable extends Migration
     public function down()
     {
         Schema::table('chargeables', function (Blueprint $table) {
-            $table->dropColumn(['month_year', 'time', 'status']);
+            $table->dropColumn(['chargeable_month', 'chargeable_time', 'status', 'patient_closed_ccm_status', 'id']);
         });
     }
 
@@ -31,11 +31,13 @@ class AddMonthAndTimeToChargeablesTable extends Migration
     {
         Schema::table('chargeables', function (Blueprint $table) {
             $table->primary('id');
-            $table->date('month_year')->nullable()->after('amount');
-            $table->unsignedInteger('time')->default(0)->after('month_year');
-            $table->string('status')->nullable()->after('time');
-            //maybe use a relationship count query?
-//            $table->unsignedInteger('no_of_successful_calls')->default(0)->after('time');
+            $table->date('chargeable_month')->nullable()->after('chargeable_type');
+            $table->unsignedInteger('chargeable_time')->default(0)->after('chargeable_month');
+            $table->string('status')->nullable()->after('chargeable_time');
+            $table->string('patient_closed_ccm_status')->nullable()->after('chargeable_time');
+            //todo: use computed columns
+//            $table->unsignedInteger('no_of_successful_calls')->default(0)->after('chargeable_time');
+//            $table->unsignedInteger('no_of_calls')->default(0)->after('chargeable_time');
         });
     }
 }
