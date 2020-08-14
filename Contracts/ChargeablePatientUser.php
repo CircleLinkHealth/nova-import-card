424c0add548c274b0efd69ef556ad73f5a32454f
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-13 23:29:20 -0500

    Small Changes

--- Contracts/ChargeablePatientUser.php
+++ Contracts/ChargeablePatientUser.php
@@ -10,16 +10,20 @@ use Carbon\Carbon;
 
 interface ChargeablePatientUser
 {
-    // Check if there is an entry in ChargeableMonthlySummaries where there is a fulfilled chargeable service
-    public function processBilling(Carbon $monthYear);
-    
-    // At the beginning or end of the month. Should we attach this chargeable service to this patient and attempt to fulfill it throughout the month?
-    public function shouldAttach(Carbon $monthYear);
-    public function isAttached(Carbon $monthYear);
     public function attach(Carbon $monthYear);
-    
+
+    public function fulfill(Carbon $monthYear);
+
+    public function isAttached(Carbon $monthYear);
+
     // At any point in time we check if this service has been fulfilled
     public function isFulfilled(Carbon $monthYear);
+
+    // Check if there is an entry in ChargeableMonthlySummaries where there is a fulfilled chargeable service
+    public function processBilling(Carbon $monthYear);
+
+    // At the beginning or end of the month. Should we attach this chargeable service to this patient and attempt to fulfill it throughout the month?
+    public function shouldAttach(Carbon $monthYear);
+
     public function shouldFulfill(Carbon $monthYear);
-    public function fulfill(Carbon $monthYear);
 }
