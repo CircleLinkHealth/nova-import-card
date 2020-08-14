45535b8b91861f8f84561d94b8ecffdfc44f894d
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-14 01:23:03 -0500

    Adds Phaux Job

---
+++ Jobs/AttachChargeableServicesToPatient.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\CcmBilling\Jobs;
+
+use CircleLinkHealth\Customer\Entities\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AttachChargeableServicesToPatient implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+    
+    /**
+     * @var User
+     */
+    private User $patientUser;
+    
+    public function __construct(User $patientUser)
+    {
+        $this->patientUser = $patientUser;
+    }
+    
+    public function handle()
+    {
+    }
+}
