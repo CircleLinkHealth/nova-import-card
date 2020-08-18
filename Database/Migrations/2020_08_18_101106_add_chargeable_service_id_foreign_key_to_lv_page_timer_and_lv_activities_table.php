76eef9b1f8b9cc72e6fba58a0c6ad08842b03228
kakoushias <kakoushias@gmail.com>
2020-08-18 16:21:22 +0200

    ROAD-288 add cs id foreign on activities and page timers

---
+++ Database/Migrations/2020_08_18_101106_add_chargeable_service_id_foreign_key_to_lv_page_timer_and_lv_activities_table.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddChargeableServiceIdForeignKeyToLvPageTimerAndLvActivitiesTable extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lv_page_timer', function (Blueprint $table) {
+            $table->dropForeign(['chargeable_service_id']);
+
+            $table->dropColumn('chargeable_service_id');
+        });
+
+        Schema::table('lv_activities', function (Blueprint $table) {
+            $table->dropForeign(['chargeable_service_id']);
+
+            $table->dropColumn('chargeable_service_id');
+        });
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lv_page_timer', function (Blueprint $table) {
+            $table->unsignedInteger('chargeable_service_id')->nullable()->after('provider_id');
+
+            $table->foreign('chargeable_service_id')
+                ->references('id')
+                ->on('chargeable_services')
+                ->onDelete('set null');
+        });
+
+        Schema::table('lv_activities', function (Blueprint $table) {
+            $table->unsignedInteger('chargeable_service_id')->nullable()->after('comment_id');
+
+            $table->foreign('chargeable_service_id')
+                ->references('id')
+                ->on('chargeable_services')
+                ->onDelete('set null');
+        });
+    }
+}
