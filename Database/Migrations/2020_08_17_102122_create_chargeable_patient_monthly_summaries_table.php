c45327fd11404d5a1b298fdfb1b57fa5ee644722
kakoushias <kakoushias@gmail.com>
2020-08-18 16:09:24 +0200

    ROAD-288 fix migrations

--- Database/Migrations/2020_08_17_102122_create_chargeable_patient_monthly_summaries_table.php
+++ Database/Migrations/2020_08_17_102122_create_chargeable_patient_monthly_summaries_table.php
@@ -30,23 +30,23 @@ class CreateChargeablePatientMonthlySummariesTable extends Migration
         Schema::create('chargeable_patient_monthly_summaries', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('patient_user_id');
-            $table->unsignedInteger('chargeable_service_id');
+            $table->unsignedInteger('chargeable_service_id')->nullable();
             $table->date('chargeable_month');
             $table->unsignedInteger('actor_id')->nullable();
             $table->boolean('is_fulfilled')->default(0);
             $table->timestamps();
 
-            $table->foreign('patient_user_id')
+            $table->foreign('patient_user_id', 'c_p_m_s_patient_user_id_foreign')
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade');
 
-            $table->foreign('chargeable_service_id')
+            $table->foreign('chargeable_service_id', 'c_p_m_s_c_s_id_foreign')
                 ->references('id')
                 ->on('chargeable_services')
                 ->onDelete('set null');
 
-            $table->foreign('actor_id')
+            $table->foreign('actor_id', 'c_p_m_s_actor_id_foreign')
                 ->references('id')
                 ->on('users')
                 ->onDelete('set null');
--- Database/Migrations/2020_08_17_105736_create_chargeable_location_monthly_summaries_table.php
+++ Database/Migrations/2020_08_17_105736_create_chargeable_location_monthly_summaries_table.php
@@ -30,18 +30,18 @@ class CreateChargeableLocationMonthlySummariesTable extends Migration
         Schema::create('chargeable_location_monthly_summaries', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('location_id');
-            $table->unsignedInteger('chargeable_service_id');
+            $table->unsignedInteger('chargeable_service_id')->nullable();
             $table->date('chargeable_month');
             $table->decimal('amount')->default(0);
             $table->boolean('is_locked')->default(0);
             $table->timestamps();
 
-            $table->foreign('location_id')
+            $table->foreign('location_id', 'c_l_m_s_location_id_foreign')
                 ->references('id')
                 ->on('locations')
-                ->onDelete('set null');
+                ->onDelete('cascade');
 
-            $table->foreign('chargeable_service_id')
+            $table->foreign('chargeable_service_id', 'c_l_m_s_chargeable_service_id_foreign')
                 ->references('id')
                 ->on('chargeable_services')
                 ->onDelete('set null');
