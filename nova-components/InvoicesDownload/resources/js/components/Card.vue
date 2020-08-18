00f1501aabfbaaa2c3b4a7072cb7510671a13e42
kakoushias <kakoushias@gmail.com>
2020-08-18 13:44:22 +0200

    fix js error validation

--- nova-components/InvoicesDownload/resources/js/components/Card.vue
+++ nova-components/InvoicesDownload/resources/js/components/Card.vue
@@ -98,8 +98,11 @@ export default {
                 this.loading = false;
 
             }).catch(error => {
-                console.log(error);
-                this.$toasted.error(error.response.data.message);
+                let msg = 'There has been an error.';
+                if (error.response && error.response.data && error.response.data.message) {
+                    msg = error.response.data.message;
+                }
+                this.$toasted.error(msg);
                 this.loading = false;
             });
         },
