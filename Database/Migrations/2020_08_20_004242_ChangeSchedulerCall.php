dcb1913bddd79fff05258381e72dab3ee0afbc9e
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-19 23:56:51 -0500

    Index changes

---
+++ Database/Migrations/2020_08_20_004242_ChangeSchedulerCall.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeSchedulerCall extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('calls', function (Blueprint $table) {
+            $table->string('scheduler')->change();
+            $table->index(['type', 'status', 'scheduled_date']);
+        });
+
+        Schema::table('ccd_problems', function (Blueprint $table) {
+            $table->index(['id','is_monitored', 'patient_id']);
+        });
+    }
+}
