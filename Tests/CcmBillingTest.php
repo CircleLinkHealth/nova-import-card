b24347fced86a2b64843a906ea202022337591c9
kakoushias <kakoushias@gmail.com>
2020-08-20 20:42:51 +0200

    ROAD-288 clean

--- Tests/CcmBillingTest.php
+++ Tests/CcmBillingTest.php
@@ -16,27 +16,26 @@ use CircleLinkHealth\CcmBilling\Processors\Patient\CCM;
 use CircleLinkHealth\CcmBilling\Processors\Patient\MonthlyProcessor;
 use CircleLinkHealth\CcmBilling\Repositories\LocationProcessorEloquentRepository;
 use CircleLinkHealth\CcmBilling\Repositories\PracticeProcessorEloquentRepository;
-use CircleLinkHealth\CcmBilling\Tests\Fakes\FakeMonthlyBillingProcessor;
+use CircleLinkHealth\CcmBilling\Tests\Fakes\Repositories\Patient\Fake as FakePatientRepository;
 use CircleLinkHealth\CcmBilling\ValueObjects\AvailableServiceProcessors;
 use CircleLinkHealth\CcmBilling\ValueObjects\PatientMonthlyBillingStub;
 use CircleLinkHealth\Customer\Entities\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Mockery;
 use Tests\TestCase;
-use CircleLinkHealth\CcmBilling\Tests\Fakes\Repositories\Patient\Fake as FakePatientRepository;
 
 class BillingProcessorsIntegrationTest extends TestCase
 {
     public function test_it_fetches_approvable_patients_for_location()
     {
         $locationId = 5;
-        $monthYear  = now()->startOfMonth();
-        $pageSize   = 100;
-        $fakeUsers  = factory(User::class, 5)->make()->transform(fn ($user) => new ApprovablePatient($user));
-
-        $repoMock      = Mockery::mock(LocationProcessorEloquentRepository::class);
+        $monthYear = now()->startOfMonth();
+        $pageSize = 100;
+        $fakeUsers = factory(User::class, 5)->make()->transform(fn($user) => new ApprovablePatient($user));
+        
+        $repoMock = Mockery::mock(LocationProcessorEloquentRepository::class);
         $paginatorMock = Mockery::mock(LengthAwarePaginator::class);
-
+        
         $paginatorMock
             ->shouldReceive('first')
             ->andReturn(false);
@@ -49,24 +48,24 @@ class BillingProcessorsIntegrationTest extends TestCase
             ->with($locationId, $monthYear, $pageSize)
             ->once()
             ->andReturn($paginatorMock);
-
-        $biller   = new Location($repoMock);
+        
+        $biller = new Location($repoMock);
         $response = $biller->fetchApprovablePatients($locationId, $monthYear, $pageSize);
-
+        
         $this->assertTrue($response instanceof ApprovablePatientCollection);
         $this->assertTrue($response->collection->count() === $fakeUsers->count());
     }
-
+    
     public function test_it_fetches_approvable_patients_for_practice()
     {
         $practiceId = 5;
-        $monthYear  = now()->startOfMonth();
-        $pageSize   = 100;
-        $fakeUsers  = factory(User::class, 5)->make()->transform(fn ($user) => new ApprovablePatient($user));
-
-        $repoMock      = Mockery::mock(PracticeProcessorEloquentRepository::class);
+        $monthYear = now()->startOfMonth();
+        $pageSize = 100;
+        $fakeUsers = factory(User::class, 5)->make()->transform(fn($user) => new ApprovablePatient($user));
+        
+        $repoMock = Mockery::mock(PracticeProcessorEloquentRepository::class);
         $paginatorMock = Mockery::mock(LengthAwarePaginator::class);
-
+        
         $paginatorMock
             ->shouldReceive('first')
             ->andReturn(false);
@@ -79,46 +78,46 @@ class BillingProcessorsIntegrationTest extends TestCase
             ->with($practiceId, $monthYear, $pageSize)
             ->once()
             ->andReturn($paginatorMock);
-
-        $biller   = new Practice($repoMock);
+        
+        $biller = new Practice($repoMock);
         $response = $biller->fetchApprovablePatients($practiceId, $monthYear, $pageSize);
-
+        
         $this->assertTrue($response instanceof ApprovablePatientCollection);
         $this->assertTrue($response->collection->count() === $fakeUsers->count());
     }
-
+    
     public function test_it_processes_patient_chargeable_services_at_the_start_of_month()
     {
         //for a given Patient User
         //of  a given Practice
         //of a specific preferred Location
-
+        
         //attempt to process CS for the current (on the start of) month
         //1. take user problems
         //2. take Location enable CS? (QUESTION)
         //3. (4.)Call Patient CS Processors individually
         //    a) BHI->attach check should attach, return bool
         //    b) Is fulfilled - it would probably deduce/return false
-
+        
         //mock to job to recieve patient and month
         // X determines current CSs/summary
         //
-
+        
         // WE NEED PATIENT REPOSITORY
         // PATIENT PROCESSOR - returns collection of Classes (CS individual processors) ->getChargeableServicesForMonth()
         // ccdProblems.cpmProblems(.chargeableServices)
         // locationChargeableService
         
         FakePatientRepository::fake();
-    
+        
         $patient = factory(User::class)->make();
         
         $availableCs = new AvailableServiceProcessors();
         
         
-        $availableCs->setCcm( $ccmProcessor = new CCM());
-        $availableCs->setBhi( $bhiProcessor = new BHI());
-
+        $availableCs->setCcm($ccmProcessor = new CCM());
+        $availableCs->setBhi($bhiProcessor = new BHI());
+        
         $stub = new PatientMonthlyBillingStub();
         $stub->setAvailableServiceProcessors($availableCs);
         
@@ -127,11 +126,12 @@ class BillingProcessorsIntegrationTest extends TestCase
         //array will contain chargeable name with cpm problem id
         $stub->setPatientProblems(collect([]));
         $stub->setPatientId(1);
-
+        
         $fakeProcessor = new MonthlyProcessor();
-
+        
         $fakeProcessor->process($stub);
         
+        //todo:ask - normally this method should also be in the contract?
         FakePatientRepository::assertChargeableSummaryCreated(1, $ccmProcessor->code(), $startOfMonth);
         FakePatientRepository::assertChargeableSummaryCreated(1, $bhiProcessor->code(), $startOfMonth);
     }
