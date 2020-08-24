3d8cf353dd0f0e861fa0fa627a56ea59cd105cfc
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-23 23:14:45 -0500

    attempt fix

--- database/seeds/PrepareDataForReEnrollmentTestSeeder.php
+++ database/seeds/PrepareDataForReEnrollmentTestSeeder.php
@@ -126,19 +126,5 @@ class PrepareDataForReEnrollmentTestSeeder extends Seeder
             ]);
             ++$n;
         }
-
-//        $n     = 1;
-//        $limit = 5;
-//        while ($n <= $limit) {
-//            $user = $this->createUser($practice->id, 'participant', self::CCM_STATUS_UNREACHABLE);
-//            $user->phoneNumbers()->update(['number' => $phoneTester]);
-//            $user->update(['email' => $faker->unique()->safeEmail]);
-//            $user->patientInfo()->update([
-//                'birth_date'       => \Carbon\Carbon::parse('1901-01-01'),
-//                'date_unreachable' => now(),
-//            ]);
-//            ++$n;
-////            There is PatientObesrver
-//        }
     }
 }
