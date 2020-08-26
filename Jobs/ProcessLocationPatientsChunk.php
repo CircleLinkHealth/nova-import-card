074b66f9601d4325b93d98cf21803f7648614a22
kakoushias <kakoushias@gmail.com>
2020-08-26 15:02:22 +0200

    ROAD-288 change class into contract

--- Jobs/ProcessLocationPatientsChunk.php
+++ Jobs/ProcessLocationPatientsChunk.php
@@ -10,10 +10,23 @@ use App\Contracts\ChunksEloquentBuilder;
 use Carbon\Carbon;
 use CircleLinkHealth\CcmBilling\ValueObjects\AvailableServiceProcessors;
 use CircleLinkHealth\Customer\Entities\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
-class ProcessLocationPatientsChunk extends ChunksEloquentBuilder
+class ProcessLocationPatientsChunk implements ChunksEloquentBuilder, ShouldQueue
 {
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
     protected AvailableServiceProcessors $availableServiceProcessors;
+
+    protected Builder $builder;
 
     protected Carbon $chargeableMonth;
 
@@ -36,5 +49,14 @@ class ProcessLocationPatientsChunk extends ChunksEloquentBuilder
         $this->builder->get()->each(function (User $patient) {
             ProcessPatientMonthlyServices::dispatch($patient, $this->availableServiceProcessors);
         });
+    }
+
+    public function setBuilder(int $offset, int $limit, Builder $builder): self
+    {
+        $this->builder = $builder
+            ->offset($offset)
+            ->limit($limit);
+
+        return $this;
     }
 }
