c2ece2db767b2422bbf6ec5e338ca631d49f6365
kakoushias <kakoushias@gmail.com>
2020-08-29 22:14:28 +0200

    ROAD-288 migration command job to migrate location CS from chargeables

---
+++ Database/Migrations/2020_08_29_160303_seed_location_summaries_from_chargeables_table_data.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+class SeedLocationSummariesFromChargeablesTableData extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('billing:migrate-location-services');
+    }
+}
