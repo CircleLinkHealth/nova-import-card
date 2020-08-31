6b0e559d0e054c782de35b80aa8cd95304434653
Pangratios Cosma <pagcosma@gmail.com>
2020-08-31 21:48:04 +0300

    bug fix

--- TimeSlots.php
+++ TimeSlots.php
@@ -8,12 +8,12 @@ namespace CircleLinkHealth\Nurseinvoices;
 
 class TimeSlots
 {
-    public int $after20;
-    public int $after30;
-    public int $after40;
-    public int $after60;
-    public int $towards20;
-    public int $towards30;
+    public int $after20   = 0;
+    public int $after30   = 0;
+    public int $after40   = 0;
+    public int $after60   = 0;
+    public int $towards20 = 0;
+    public int $towards30 = 0;
 
     /**
      * TimeSlots constructor.
