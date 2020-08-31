575683cd9da099e426253da12cefa12cb1d0292f
kakoushias <kakoushias@gmail.com>
2020-09-01 00:34:11 +0200

    ROAD-288 add sql view model

---
+++ Entities/ChargeablePatientMonthlySummaryView.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\CcmBilling\Entities;
+
+use CircleLinkHealth\Core\Entities\SqlViewModel;
+
+class ChargeablePatientMonthlySummaryView extends SqlViewModel
+{
+    protected $table = 'chargeable_patient_monthly_summaries_view';
+}
