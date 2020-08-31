f2f9e996d4e6ba6d46ee96a60172453f76f8c231
Pangratios Cosma <pagcosma@gmail.com>
2020-08-31 14:57:38 +0300

    ROAD-287 - StoreTimeTracking.php: add chargeable service id to activity - Remove ServerInsights.php dashboard, add its components to main dashboard - Improve tests with pcm problems -

---
+++ TimeSlots.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\Nurseinvoices;
+
+class TimeSlots
+{
+    public int $after20;
+    public int $after30;
+    public int $after40;
+    public int $after60;
+    public int $towards20;
+    public int $towards30;
+
+    /**
+     * TimeSlots constructor.
+     */
+    public function __construct()
+    {
+    }
+}
