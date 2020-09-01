7eb8e669cb66ae0816c247ab09c4d29309cfa995
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-08-31 21:32:55 -0500

    Removing validation logic from form requests. CreateCall method looks more like service class material

--- app/Http/Requests/CreateMultiCallRequest.php
+++ app/Http/Requests/CreateMultiCallRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateMultiCallRequest extends FormRequest
 {
-    use Traits\ValidatesNewCall;
     /**
      * Determine if the user is authorized to make this request.
      *
--- app/Http/Requests/CreateNewCallRequest.php
+++ app/Http/Requests/CreateNewCallRequest.php
@@ -13,10 +13,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateNewCallRequest extends FormRequest
 {
-    use ValidatesNewCall;
-
-    private User $patientUser;
-
     /**
      * Determine if the user is authorized to make this request.
      *
