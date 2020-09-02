7426301929502d669d8f364f7b9cd0c1af5afde9
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-01 22:39:35 -0500

    Use action workflow instead of CLH Importer

--- app/Nova/Importers/SupplementalPatientDataImporter.php
+++ app/Nova/Importers/SupplementalPatientDataImporter.php
@@ -6,57 +6,76 @@
 
 namespace App\Nova\Importers;
 
-use App\EligiblePatientView;
-use App\Nova\Actions\ReimportCcda;
-use Carbon\Carbon;
 use CircleLinkHealth\Customer\Entities\Location;
-use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Eligibility\CcdaImporter\CcdaImporterWrapper;
 use CircleLinkHealth\Eligibility\CcdaImporter\Tasks\ImportPatientInfo;
-use CircleLinkHealth\Eligibility\Entities\Enrollee;
 use CircleLinkHealth\Eligibility\Entities\SupplementalPatientData;
-use CircleLinkHealth\Eligibility\Jobs\ImportConsentedEnrollees;
-use CircleLinkHealth\SharedModels\Entities\Ccda;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithValidation;
 
-class SupplementalPatientDataImporter implements ToModel, WithChunkReading, WithValidation, WithHeadingRow, ShouldQueue
+class SupplementalPatientDataImporter implements ToModel, WithChunkReading, WithHeadingRow, ShouldQueue
 {
     use Importable;
 
-    protected $attributes;
+    /**
+     * @var int
+     */
+    private $practiceId;
 
-    protected $modelClass;
-    protected $resource;
-
-    protected $rules;
-
-    public function __construct($resource, $attributes, $rules, $modelClass)
+    /**
+     * Medications constructor.
+     */
+    public function __construct(int $practiceId)
     {
-        $this->resource   = $resource;
-        $this->attributes = $attributes;
-        $this->rules      = $rules;
-        $this->modelClass = $modelClass;
+        ini_set('upload_max_filesize', '200M');
+        ini_set('post_max_size', '200M');
+        ini_set('max_input_time', 900);
+        ini_set('max_execution_time', 900);
+
+        $this->practiceId = $practiceId;
+    }
+
+    public function batchSize(): int
+    {
+        return 400;
     }
 
     public function chunkSize(): int
     {
-        return 200;
-    }
-
-    public function getPractice(): Practice
-    {
-        return $this->resource->fields->getFieldValue('practice');
+        return 400;
     }
 
     public function model(array $row)
     {
-        $this->persistRow($row);
+        $args = [
+            'dob' => optional(
+                ImportPatientInfo::parseDOBDate($this->nullOrValue($row['dob']))
+            )->toDateString(),
+            'first_name'          => $this->nullOrValue($row['first_name']),
+            'last_name'           => $this->nullOrValue($row['last_name']),
+            'mrn'                 => $this->nullOrValue($row['mrn']),
+            'primary_insurance'   => $this->nullOrValue($row['primary_insurance']),
+            'secondary_insurance' => $this->nullOrValue($row['secondary_insurance']),
+            'provider'            => $this->nullOrValue($row['provider']),
+            'location'            => $this->nullOrValue($row['location']),
+            'practice_id'         => $this->practiceId,
+        ];
+
+        $args['location_id'] = optional(
+            Location::where('practice_id', $args['practice_id'])->where(
+                'name',
+                $args['location']
+            )->first()
+        )->id;
+
+        $args['billing_provider_user_id'] = optional(
+            CcdaImporterWrapper::searchBillingProvider($args['provider'], $args['practice_id'])
+        )->id;
+
+        return new SupplementalPatientData($args);
     }
 
     /**
@@ -87,213 +106,5 @@ class SupplementalPatientDataImporter implements ToModel, WithChunkReading, With
             '#N/A',
             '-',
         ];
-    }
-
-    public function rules(): array
-    {
-        return $this->rules;
-    }
-
-    /**
-     * Subtracts 100 years off date if it's after 1/1/2000.
-     *
-     * @return Carbon
-     */
-    private function correctCenturyIfNeeded(Carbon &$date)
-    {
-        //If a DOB is after 2000 it's because at some point the date incorrectly assumed to be in the 2000's, when it was actually in the 1900's. For example, this date 10/05/04.
-        $cutoffDate = Carbon::createFromDate(2000, 1, 1);
-
-        if ($date->gte($cutoffDate)) {
-            $date->subYears(100);
-        }
-
-        return $date;
-    }
-
-    private function persistRow(array $row)
-    {
-        ini_set('upload_max_filesize', '200M');
-        ini_set('post_max_size', '200M');
-        ini_set('max_input_time', 900);
-        ini_set('max_execution_time', 900);
-
-        $args = [
-            'dob' => optional(
-                ImportPatientInfo::parseDOBDate($this->nullOrValue($row['dob']))
-            )->toDateString(),
-            'first_name'          => $this->nullOrValue($row['first_name']),
-            'last_name'           => $this->nullOrValue($row['last_name']),
-            'mrn'                 => $this->nullOrValue($row['mrn']),
-            'primary_insurance'   => $this->nullOrValue($row['primary_insurance']),
-            'secondary_insurance' => $this->nullOrValue($row['secondary_insurance']),
-            'provider'            => $this->nullOrValue($row['provider']),
-            'location'            => $this->nullOrValue($row['location']),
-            'practice_id'         => $this->getPractice()->id,
-        ];
-
-        $args['location_id'] = optional(
-            Location::where('practice_id', $args['practice_id'])->where(
-                'name',
-                $args['location']
-            )->first()
-        )->id;
-
-        $args['billing_provider_user_id'] = optional(
-            CcdaImporterWrapper::searchBillingProvider($args['provider'], $args['practice_id'])
-        )->id;
-
-        if ( ! empty($args['mrn']) && ! empty($args['first_name']) && ! empty($args['last_name'])) {
-            return tap(
-                SupplementalPatientData::updateOrCreate(
-                    [
-                        'mrn'         => $row['mrn'],
-                        'practice_id' => $this->getPractice()->id,
-                    ],
-                    $args
-                ),
-                function ($spd) use ($row) {
-                    if ( ! array_key_exists('import_now', $row)) {
-                        return $spd;
-                    }
-
-                    if ( ! ('y' === strtolower(
-                        $row['import_now']
-                    ) && $spd->practice_id && $spd->first_name && $spd->last_name && $spd->mrn)) {
-                        return $spd;
-                    }
-
-                    $query = Enrollee::with(
-                        [
-                            'ccda' => function ($q) {
-                                $q->select(['id', 'location_id', 'practice_id', 'billing_provider_id', 'patient_id']);
-                            },
-                        ]
-                    );
-
-                    if ( ! ($enrollee = $query->where('practice_id', $spd->practice_id)->where(
-                        'first_name',
-                        $spd->first_name
-                    )->where('last_name', $spd->last_name)->where('mrn', $spd->mrn)->first())) {
-                        $jobId = EligiblePatientView::where('mrn', $spd->mrn)->where(
-                            'last_name',
-                            $spd->last_name
-                        )->where('first_name', $spd->first_name)->where('practice_id', $spd->practice_id)->value(
-                            'eligibility_job_id'
-                        );
-
-                        if ( ! $jobId) {
-                            return $spd;
-                        }
-
-                        $enrollee = $query->firstOrNew(
-                            [
-                                'practice_id' => $spd->practice_id,
-                                'first_name'  => $spd->first_name,
-                                'last_name'   => $spd->last_name,
-                                'mrn'         => $spd->mrn,
-                            ],
-                            [
-                                'eligibility_job_id' => $jobId,
-                                'dob'                => $spd->dob,
-                            ]
-                        );
-                    }
-
-                    $ccda = $enrollee->ccda;
-
-                    if ( ! $ccda) {
-                        $ccda = Ccda::where('practice_id', $spd->practice_id)->where(
-                            'patient_mrn',
-                            $spd->mrn
-                        )->where('patient_last_name', $spd->last_name)->select(
-                            ['id', 'location_id', 'practice_id', 'billing_provider_id', 'patient_id']
-                        )->first();
-                        if ($ccda) {
-                            $enrollee->medical_record_id = $ccda->id;
-                        }
-                    }
-
-                    if ( ! $enrollee->location_id && $spd->location_id) {
-                        $enrollee->location_id = $spd->location_id;
-                    }
-
-                    if ( ! $enrollee->provider_id && $spd->billing_provider_user_id) {
-                        $enrollee->provider_id = $spd->billing_provider_user_id;
-                    }
-
-                    if ($ccda && ! $enrollee->user_id && $ccda->patient_id) {
-                        $enrollee->user_id = $ccda->patient_id;
-                    }
-
-                    if ($enrollee->isDirty()) {
-                        $enrollee->save();
-                    }
-
-                    if ($ccda && ! $ccda->location_id && $spd->location_id) {
-                        $ccda->location_id = $spd->location_id;
-                    }
-
-                    if ($ccda && ! $ccda->billing_provider_id && $spd->billing_provider_user_id) {
-                        $ccda->billing_provider_id = $spd->billing_provider_user_id;
-                    }
-
-                    if (optional($ccda)->isDirty()) {
-                        $ccda->save();
-                    }
-
-                    if ($enrollee->user_id) {
-                        ReimportCcda::for($enrollee->user_id, auth()->id());
-
-                        return;
-                    }
-
-                    return ImportConsentedEnrollees::dispatch([$enrollee->id]);
-                }
-            );
-        }
-    }
-
-    private function validateDob($dob)
-    {
-        if ( ! $dob) {
-            return false;
-        }
-
-        $validator = \Validator::make(['dob' => $dob], ['dob' => 'required|filled|date']);
-
-        if ($validator->fails()) {
-            return false;
-        }
-
-        try {
-            $date = Carbon::parse($dob);
-
-            if ($date->isToday()) {
-                return false;
-            }
-
-            return $this->correctCenturyIfNeeded($date);
-        } catch (\InvalidArgumentException $e) {
-            if (Str::contains($dob, '/')) {
-                $delimiter = '/';
-            } elseif (Str::contains($dob, '-')) {
-                $delimiter = '-';
-            }
-            $date = explode($delimiter, $dob);
-
-            if (count($date) < 3) {
-                throw new \Exception("Invalid date $dob");
-            }
-
-            $year = $date[2];
-
-            if (2 == strlen($year)) {
-                //if date is two digits we are assuming it's from the 1900s
-                $year = (int) $year + 1900;
-            }
-
-            return Carbon::createFromDate($year, $date[0], $date[1]);
-        }
     }
 }
