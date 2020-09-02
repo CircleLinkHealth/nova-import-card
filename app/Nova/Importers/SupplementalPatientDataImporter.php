f103ff9b863abe7a9b5a449467483687eab6caa1
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-01 22:56:06 -0500

    This may actually fix it

--- app/Nova/Importers/SupplementalPatientDataImporter.php
+++ app/Nova/Importers/SupplementalPatientDataImporter.php
@@ -12,11 +12,11 @@ use CircleLinkHealth\Eligibility\CcdaImporter\Tasks\ImportPatientInfo;
 use CircleLinkHealth\Eligibility\Entities\SupplementalPatientData;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\Importable;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class SupplementalPatientDataImporter implements ToModel, WithChunkReading, WithHeadingRow, ShouldQueue
+class SupplementalPatientDataImporter implements WithChunkReading, WithHeadingRow, ShouldQueue, OnEachRow
 {
     use Importable;
 
@@ -48,36 +48,6 @@ class SupplementalPatientDataImporter implements ToModel, WithChunkReading, With
         return 400;
     }
 
-    public function model(array $row)
-    {
-        $args = [
-            'dob' => optional(
-                ImportPatientInfo::parseDOBDate($this->nullOrValue($row['dob']))
-            )->toDateString(),
-            'first_name'          => $this->nullOrValue($row['first_name']),
-            'last_name'           => $this->nullOrValue($row['last_name']),
-            'mrn'                 => $this->nullOrValue($row['mrn']),
-            'primary_insurance'   => $this->nullOrValue($row['primary_insurance']),
-            'secondary_insurance' => $this->nullOrValue($row['secondary_insurance']),
-            'provider'            => $this->nullOrValue($row['provider']),
-            'location'            => $this->nullOrValue($row['location']),
-            'practice_id'         => $this->practiceId,
-        ];
-
-        $args['location_id'] = optional(
-            Location::where('practice_id', $args['practice_id'])->where(
-                'name',
-                $args['location']
-            )->first()
-        )->id;
-
-        $args['billing_provider_user_id'] = optional(
-            CcdaImporterWrapper::searchBillingProvider($args['provider'], $args['practice_id'])
-        )->id;
-
-        return new SupplementalPatientData($args);
-    }
-
     /**
      * Returns null if value means N/A or equivalent. Otherwise returns the value passed to it.
      *
@@ -106,5 +76,41 @@ class SupplementalPatientDataImporter implements ToModel, WithChunkReading, With
             '#N/A',
             '-',
         ];
+    }
+
+    public function onRow($row)
+    {
+        $args = [
+            'dob' => optional(
+                ImportPatientInfo::parseDOBDate($this->nullOrValue($row['dob']))
+            )->toDateString(),
+            'first_name'          => $this->nullOrValue($row['first_name']),
+            'last_name'           => $this->nullOrValue($row['last_name']),
+            'mrn'                 => $this->nullOrValue($row['mrn']),
+            'primary_insurance'   => $this->nullOrValue($row['primary_insurance']),
+            'secondary_insurance' => $this->nullOrValue($row['secondary_insurance']),
+            'provider'            => $this->nullOrValue($row['provider']),
+            'location'            => $this->nullOrValue($row['location']),
+            'practice_id'         => $this->practiceId,
+        ];
+
+        $args['location_id'] = optional(
+            Location::where('practice_id', $args['practice_id'])->where(
+                'name',
+                $args['location']
+            )->first()
+        )->id;
+
+        $args['billing_provider_user_id'] = optional(
+            CcdaImporterWrapper::searchBillingProvider($args['provider'], $args['practice_id'])
+        )->id;
+
+        return SupplementalPatientData::updateOrCreate(
+            [
+                'mrn'         => $args['mrn'],
+                'practice_id' => $args['practice_id'],
+            ],
+            $args
+        );
     }
 }
