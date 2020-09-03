bcc87a633ea7684136f59426efc8d30e3e5c48ba
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-03 00:13:25 -0500

    Try this out

--- config/app.php
+++ config/app.php
@@ -211,8 +211,6 @@ return [
         App\Providers\TwilioClientServiceProvider::class,
         \Collective\Html\HtmlServiceProvider::class,
 
-        CircleLinkHealth\ApiPatient\Providers\ApiPatientServiceProvider::class,
-
         App\View\Composers\ProviderUITimerComposer::class,
         App\View\Composers\FabComposer::class,
         App\View\Composers\SAAS\Admin\ManageInternalUser::class,
