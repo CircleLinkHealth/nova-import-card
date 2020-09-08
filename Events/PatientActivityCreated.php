0551cdebe8b3db63fe8236fc23485c7aaedbf281
kakoushias <kakoushias@gmail.com>
2020-09-08 19:00:11 +0200

    ROAD-288 - add event

---
+++ Events/PatientActivityCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\CcmBilling\Events;
+
+use App\Contracts\PatientEvent;
+use Illuminate\Queue\SerializesModels;
+
+class PatientActivityCreated implements PatientEvent
+{
+    use SerializesModels;
+
+    protected int $patientUserId;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(int $patientUserId)
+    {
+        $this->patientUserId = $patientUserId;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+
+    public function getPatientId(): int
+    {
+        return $this->patientUserId;
+    }
+}
