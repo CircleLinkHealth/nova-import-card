00054af406ee0371d02f2c9cca6bf409554c476f
Pangratios Cosma <pagcosma@gmail.com>
2020-09-10 17:54:56 +0300

    ROAD-275 fix tests

--- tests/Feature/SamlTest.php
+++ tests/Feature/SamlTest.php
@@ -6,32 +6,27 @@
 
 namespace Tests\Feature;
 
-use App\User;
 use PHPUnit\Util\Xml;
-use Tests\TestCase;
+use Tests\CustomerTestCase;
 
-class SamlTest extends TestCase
+class SamlTest extends CustomerTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
 
-        User::whereEmail('admin@example.org')->update([
-            'skip_browser_checks' => true,
-        ]);
+        $admin                      = $this->superadmin();
+        $admin->skip_browser_checks = true;
+        $admin->save();
     }
 
     /**
      * TODO: this should be a DUSK test.
-     *
-     * 1. Login as admin through SAML
-     * 2. Go to PAM page
-     * 3. Logout.
      */
     public function test_should_add_saml_request_input_in_form()
     {
         $encodedReq = 'fVFBbsIwEPxK5DuJSWgBiyClRVWRQEUk7aG3xWyIpcROvU7b59cJINEL0l6smfHMzi4ImroVWecqvcevDskFv02tSQxAyjqrhQFSJDQ0SMJJkWfbjYhDLlprnJGmZjeS%2BwogQuuU0Sx4MVbi4JuyEmpCFqxXKYNxmQAey4cEH2NI%2BOTI5zgpDyCniRxDUk5lPJtJ7OlEHa41OdAuZTGP%2BYjP%2FRR8Jvwk808WrPxCSkNvmbLKuVZEkWybUNZV6DwW1eakfJrsGuzZaOoatDnabyXxfb8568gLqe0loYKGKvMjgTCUpolAEguC3aWMJ6WPSp%2Fu93A4k0i8FsVutHvLC%2F9D8IGWhqCewpaLvk4xLGmX62ybXzwX0S1wfv2%2F4PIP';
-        $url        = 'http://cpm.clh.test/login?SAMLRequest='.$encodedReq;
+        $url        = url('login?SAMLRequest='.$encodedReq);
         $resp       = $this->get($url);
         $resp->assertOk();
         $resp->assertViewIs('auth.login');
@@ -39,17 +34,13 @@ class SamlTest extends TestCase
         self::assertStringContainsString('<input type="hidden" id="SAMLRequest" name="SAMLRequest" value="', $content);
     }
 
-    /**
-     * 1. Login as admin through normal CPM login
-     * 2. Go to PAM page
-     * 3. Logout.
-     */
     public function test_should_login_normally()
     {
-        $this->post(route('login'), [
-            'email'    => 'admin@example.org',
-            'password' => 'hello',
-        ])->assertRedirect(route('home'));
+        $resp = $this->post(route('login'), [
+            'email'    => $this->superadmin()->email,
+            'password' => 'password',
+        ]);
+        $resp->assertRedirect(route('home'));
     }
 
     public function test_should_login_through_idp()
@@ -57,9 +48,10 @@ class SamlTest extends TestCase
         $encodedReq = 'fVFBbsIwEPxK5DuJSWgBiyClRVWRQEUk7aG3xWyIpcROvU7b59cJINEL0l6smfHMzi4ImroVWecqvcevDskFv02tSQxAyjqrhQFSJDQ0SMJJkWfbjYhDLlprnJGmZjeS%2BwogQuuU0Sx4MVbi4JuyEmpCFqxXKYNxmQAey4cEH2NI%2BOTI5zgpDyCniRxDUk5lPJtJ7OlEHa41OdAuZTGP%2BYjP%2FRR8Jvwk808WrPxCSkNvmbLKuVZEkWybUNZV6DwW1eakfJrsGuzZaOoatDnabyXxfb8568gLqe0loYKGKvMjgTCUpolAEguC3aWMJ6WPSp%2Fu93A4k0i8FsVutHvLC%2F9D8IGWhqCewpaLvk4xLGmX62ybXzwX0S1wfv2%2F4PIP';
         $resp       = $this->post(route('login'), [
             'SAMLRequest' => urldecode($encodedReq),
-            'email'       => 'admin@example.org',
-            'password'    => 'hello',
-        ])->assertOk();
+            'email'       => $this->superadmin()->email,
+            'password'    => 'password',
+        ]);
+        $resp->assertOk();
         $content = $resp->getContent();
         self::assertStringContainsString('<input type="hidden" name="SAMLResponse" value="', $content);
     }
