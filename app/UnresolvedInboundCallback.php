f9773c1c154f3a3cecf5936776c53231220a4726
antoniskountouris <kountouris7@gmail.com>
2020-09-11 14:20:38 +0300

    road - 332 model

---
+++ app/UnresolvedInboundCallback.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UnresolvedInboundCallback extends Model
+{
+    protected $table = 'unresolved_postmark_inbound_callbacks';
+    
+    
+}
