05fe2ee98b94e552f403f310dd354d4551ed98d7
antoniskountouris <kountouris7@gmail.com>
2020-09-14 00:44:28 +0300

    road - 333 - migration + model

---
+++ app/UnresolvedPostmarkCallback.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\UnresolvedPostmarkCallback.
+ *
+ * @property int                             $id
+ * @property int                             $postmark_id
+ * @property int|null                        $user_id
+ * @property string|null                     $deleted_at
+ * @property mixed                           $suggestions
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \App\PostmarkInboundMail        $inboundPostmark
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|UnresolvedPostmarkCallback newModelQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|UnresolvedPostmarkCallback newQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|UnresolvedPostmarkCallback query()
+ * @mixin \Eloquent
+ */
+class UnresolvedPostmarkCallback extends Model
+{
+    public function inboundPostmark()
+    {
+        return $this->belongsTo(PostmarkInboundMail::class, 'postmark_id');
+    }
+}
