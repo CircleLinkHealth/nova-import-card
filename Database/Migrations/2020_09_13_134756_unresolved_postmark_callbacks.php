b942c8dbdd2798af1748174bc488f2610cce3269
antoniskountouris <kountouris7@gmail.com>
2020-09-14 00:44:27 +0300

    road - 333 - migration + model

---
+++ Database/Migrations/2020_09_13_134756_unresolved_postmark_callbacks.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class UnresolvedPostmarkCallbacks extends Migration
+{
+    use SoftDeletes;
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('unresolved_postmark_callbacks');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unresolved_postmark_callbacks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('postmark_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->json('suggestions');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('postmark_id')
+                ->references('id')
+                ->on('postmark_inbound_mail')
+                ->onDelete('cascade');
+        });
+    }
+}
