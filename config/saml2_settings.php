b9bdf0a8877849e9b29e81b019c7caa29e08a9af
Pangratios Cosma <pagcosma@gmail.com>
2020-09-17 10:31:11 +0300

    ROAD-275 passing tests

--- config/saml2_settings.php
+++ config/saml2_settings.php
@@ -10,7 +10,7 @@ return $settings = [
      * Separate routes will be automatically registered for each IDP specified with IDP name as prefix
      * Separate config file saml2/<idpName>_idp_settings.php should be added & configured accordingly
      */
-    'idpNames' => ['samltest'],
+    'idpNames' => ['samltest', 'testing'],
 
     /*
      * If 'useRoutes' is set to true, the package defines five new routes for reach entry in idpNames:
