286859ef974b27311ac1fae49daebbde4d0d6012
antoniskountouris <kountouris7@gmail.com>
2020-09-17 18:03:23 +0300

    Road - 333 sql view

--- Database/Views/2020_09_14_151439_UnresolvedPostmarkCallbackView.php
+++ Database/Views/2020_09_14_151439_UnresolvedPostmarkCallbackView.php
@@ -13,19 +13,35 @@ class UnresolvedPostmarkCallbackView extends BaseSqlView
      */
     public function createSqlView(): bool
     {
+        $x = 'not_enrolled';
+//        call_id exists and call.created_at is greater than pstomark craeted_at as resolved
+        
         return \DB::statement("
         CREATE VIEW {$this->getViewName()}
         AS
         SELECT
-        upc.postmark_id,
+        upc.postmark_id as postmark_id,
         upc.user_id as patient_user_id,
-        c.id
+        upc.unresolved_reason,
+        
+        CASE WHEN c.created_at > upc.created_at
+        AND c.sub_type = 'Call Back'
+        THEN c.id
+        END as call_id,
+        
+        CASE WHEN c.id IS NOT NULL
+        AND c.created_at > upc.created_at
+        AND c.sub_type = 'Call Back'
+        THEN true
+        ELSE false
+        END as resolved
         
         FROM
             unresolved_postmark_callbacks upc
             
             left join calls c on upc.user_id = c.inbound_cpm_id
-    
+        
+            
       ");
     }
 
