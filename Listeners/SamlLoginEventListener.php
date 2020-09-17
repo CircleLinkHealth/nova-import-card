099d2d4e3dc40d3bf78eaef5d0786c8d495d505b
Pangratios Cosma <pagcosma@gmail.com>
2020-09-17 10:31:11 +0300

    ROAD-275 passing tests

--- Listeners/SamlLoginEventListener.php
+++ Listeners/SamlLoginEventListener.php
@@ -8,8 +8,8 @@ namespace CircleLinkHealth\SamlSp\Listeners;
 
 use Aacotroneo\Saml2\Events\Saml2LoginEvent;
 use CircleLinkHealth\SamlSp\Entities\SamlUser;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class SamlLoginEventListener
 {
@@ -34,7 +34,13 @@ class SamlLoginEventListener
         /** @var string $idp */
         $idp        = $event->getSaml2Idp();
         $attributes = $event->getSaml2User()->getAttributesWithFriendlyName();
-        $idpUserId  = $attributes['uid'];
+        if ( ! isset($attributes['uid'])) {
+            Log::warning('Could not find uid in attributes of Saml2User');
+
+            return;
+        }
+
+        $idpUserId = $attributes['uid'];
 
         /** @var SamlUser $samlUser */
         $samlUser = SamlUser::with('cpmUser')
--- Tests/SamlSpTest.php
+++ Tests/SamlSpTest.php
@@ -6,11 +6,13 @@
 
 namespace CircleLinkHealth\SamlSp\Tests;
 
-use App\Traits\Tests\UserHelpers;
-use CircleLinkHealth\Customer\Entities\Practice;
-use CircleLinkHealth\Customer\Entities\User;
+use Aacotroneo\Saml2\Events\Saml2LogoutEvent;
+use Aacotroneo\Saml2\Saml2Auth;
+use Aacotroneo\Saml2\Saml2User;
 use CircleLinkHealth\SamlSp\Console\RegisterSamlUserMapping;
 use CircleLinkHealth\SamlSp\Entities\SamlUser;
+use Mockery;
+use OneLogin\Saml2\Auth as OneLogin_Saml2_Auth;
 use Tests\CustomerTestCase;
 
 class SamlSpTest extends CustomerTestCase
@@ -22,17 +24,69 @@ class SamlSpTest extends CustomerTestCase
 
     public function test_it_authenticates_saml_user_from_saml_request()
     {
-        self::assertTrue(false);
+        $this->setUpMocks();
+        \Artisan::call(RegisterSamlUserMapping::class, [
+            'cpmUserId' => $this->superadmin()->id,
+            'idp'       => 'testing',
+            'idpUserId' => 'testUser',
+        ]);
+
+        $route = route('saml2_acs', [
+            'idpName' => 'testing',
+        ]);
+        $this->post($route, [
+            'SAMLResponse' => '',
+        ]);
+        self::assertAuthenticatedAs($this->superadmin());
     }
 
     public function test_it_authenticates_saml_user_from_saml_request_and_redirects_to_target_url()
     {
-        self::assertTrue(false);
+        $this->setUpMocks();
+        \Artisan::call(RegisterSamlUserMapping::class, [
+            'cpmUserId' => $this->superadmin()->id,
+            'idp'       => 'testing',
+            'idpUserId' => 'testUser',
+        ]);
+
+        $route = route('saml2_acs', [
+            'idpName' => 'testing',
+        ]);
+        $targetRoute = route('admin.patientCallManagement.v2.index');
+        $resp        = $this->post($route, [
+            'RelayState'   => $targetRoute,
+            'SAMLResponse' => '',
+        ]);
+        $resp->assertRedirect($targetRoute);
+
+        self::assertAuthenticatedAs($this->superadmin());
     }
 
     public function test_it_logs_out_saml_user_from_saml_request()
     {
-        self::assertTrue(false);
+        $this->setUpMocks();
+        \Artisan::call(RegisterSamlUserMapping::class, [
+            'cpmUserId' => $this->superadmin()->id,
+            'idp'       => 'testing',
+            'idpUserId' => 'testUser',
+        ]);
+
+        $route = route('saml2_acs', [
+            'idpName' => 'testing',
+        ]);
+        $this->post($route, [
+            'SAMLResponse' => '',
+        ]);
+        self::assertAuthenticatedAs($this->superadmin());
+
+        $route = route('saml2_sls', [
+            'idpName' => 'testing',
+        ]);
+        $this->get($route, [
+            'SAMLResponse' => '',
+        ]);
+        event(new Saml2LogoutEvent('testing'));
+        self::assertFalse(self::isAuthenticated());
     }
 
     public function test_it_maps_cpm_user_with_saml_user()
@@ -48,5 +102,26 @@ class SamlSpTest extends CustomerTestCase
             ->first();
         self::assertNotNull($samlUser);
         self:self::assertEquals($this->superadmin()->id, $samlUser->cpm_user_id);
+    }
+
+    private function setUpMocks()
+    {
+        $repo = Mockery::mock(Saml2Auth::class);
+        $this->instance(Saml2Auth::class, $repo);
+
+        $repo->shouldReceive('acs')
+            ->andReturnNull();
+        
+        $repo->shouldReceive('sls')
+            ->andReturnNull();
+
+        $oneLoginAuth = Mockery::mock(OneLogin_Saml2_Auth::class);
+        $oneLoginAuth->shouldReceive('getAttributesWithFriendlyName')
+            ->andReturn(['uid' => 'testUser']);
+        $repo->shouldReceive('getSaml2User')
+            ->andReturn(new Saml2User($oneLoginAuth));
+
+        $repo->shouldReceive('getIntendedUrl')
+            ->andReturnNull();
     }
 }
