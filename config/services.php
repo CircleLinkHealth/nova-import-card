031106251bd3df78325b6a0df7ec947a0bed27de
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 13:39:28 -0500

    bunch

--- config/services.php
+++ config/services.php
@@ -29,5 +29,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+    'cpm-app' => [
+        'url' => env('CPM_APP_URL', null),
+    ],
 
 ];
