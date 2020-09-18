a9ca97bdfd3bfcc965fe0cf83067a8f094f5ac33
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 17:31:53 -0500

    few changes

--- Resources/assets/js/components/dispute-invoice.vue
+++ Resources/assets/js/components/dispute-invoice.vue
@@ -47,9 +47,9 @@
     </div>
 </template>
 <script>
-    import LoaderComponent from '../../../../../SharedVueComponents/Resources/assets/js/components/loader';
-    import {rootUrl} from "../../../../../SharedVueComponents/Resources/assets/js/app.config.js";
-    import Errors from "../../../../../SharedVueComponents/Resources/assets/js/components/src/Errors";
+    import LoaderComponent from '../../../../../Sharedvuecomponents/Resources/assets/js/components/loader';
+    import {rootUrl} from "../../../../../Sharedvuecomponents/Resources/assets/js/app.config.js";
+    import Errors from "../../../../../Sharedvuecomponents/Resources/assets/js/components/src/Errors";
 
     export default {
         props: [
--- Resources/assets/js/components/nurseInvoiceDailyDispute.vue
+++ Resources/assets/js/components/nurseInvoiceDailyDispute.vue
@@ -84,9 +84,9 @@
 </template>
 
 <script>
-    import LoaderComponent from '../../../../../SharedVueComponents/Resources/assets/js/components/loader'
+    import LoaderComponent from '../../../../../Sharedvuecomponents/Resources/assets/js/components/loader'
     import {mapActions} from 'vuex'
-    import {addNotification} from '../../../../../SharedVueComponents/Resources/assets/js/store/actions'
+    import {addNotification} from '../../../../../Sharedvuecomponents/Resources/assets/js/store/actions'
 
 
     export default {
