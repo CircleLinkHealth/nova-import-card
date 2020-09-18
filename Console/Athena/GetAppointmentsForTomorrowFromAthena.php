5dee31defd6a4411d58eb2fc04a9f0f956b348dc
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 00:31:54 -0500

    Moves nova to admin app

--- Console/Athena/GetAppointmentsForTomorrowFromAthena.php
+++ Console/Athena/GetAppointmentsForTomorrowFromAthena.php
@@ -6,7 +6,7 @@
 
 namespace CircleLinkHealth\Eligibility\Console\Athena;
 
-use App\Services\AthenaAPI\CreateAndPostPdfCareplan;
+use CircleLinkHealth\Eligibility\Services\AthenaAPI\CreateAndPostPdfCareplan;
 use Carbon\Carbon;
 use CircleLinkHealth\Customer\Entities\Ehr;
 use CircleLinkHealth\Customer\Entities\Practice;
