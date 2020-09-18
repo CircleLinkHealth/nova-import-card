f983d7081ac0c2fc15438438be9c77053f266b63
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 15:40:30 -0500

    moves shared vue components

---
+++ Resources/assets/js/components/pages/provider-admin-panel/manage-practice-locations.vue
@@ -0,0 +1,29 @@
+<template>
+    <component :is="componentName" :location="props" @update-view="updateView"></component>
+</template>
+
+<script>
+    import UpdateLocation from '../../../../../../../../resources/assets/js/components/practice/lodations/update.vue'
+    import IndexLocations from '../../../../../../../../resources/assets/js/components/practice/lodations/index.vue'
+
+    export default {
+        components: {
+            UpdateLocation,
+            IndexLocations
+        },
+
+        data() {
+            return {
+                componentName: 'index-locations',
+                props: {}
+            }
+        },
+
+        methods: {
+            updateView(componentName, props) {
+                this.componentName = componentName
+                this.props = props
+            }
+        }
+    }
+</script>
---
+++ Resources/assets/js/components/pages/provider-admin-panel/manage-practice-staff.vue
@@ -0,0 +1,34 @@
+<template>
+    <component :is="componentName" :editedStaffMember="props" :practice-settings="practiceSettings" @update-view="updateView"></component>
+</template>
+
+<script>
+    import UpdateStaff from '../../../../../../../../resources/assets/js/components/practice/users/update.vue'
+    import IndexStaff from '../../../../../../../../resources/assets/js/components/practice/users/index.vue'
+
+    export default {
+
+        props: [
+            'practiceSettings'
+        ],
+
+        components: {
+            UpdateStaff,
+            IndexStaff
+        },
+
+        data() {
+            return {
+                componentName: 'index-staff',
+                props: {},
+            }
+        },
+
+        methods: {
+            updateView(componentName, props) {
+                this.componentName = componentName
+                this.props = props
+            }
+        }
+    }
+</script>
