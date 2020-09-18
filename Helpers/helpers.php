e3bce0f6b8ee6e059790ab66e12e34a0a63fbdf1
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 13:17:11 -0500

    Moves function

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -58,3 +58,29 @@ if ( ! function_exists('upg0506IsEnabled')) {
         return 'true' === $val;
     }
 }
+
+if ( ! function_exists('getEhrReportWritersFolderUrl')) {
+    function getEhrReportWritersFolderUrl()
+    {
+        //this is to make local environments faster for devs
+        //comment out this if section to use the feature
+        if (app()->environment('local')) {
+            return null;
+        }
+        
+        $key = 'ehr_report_writers_folder_url';
+        
+        return \Cache::remember($key, 2, function () use ($key) {
+            return AppConfig::pull($key, null);
+        });
+
+//        Commenting out due to Heroku migration
+//        $dir = getGoogleDirectoryByName('ehr-data-from-report-writers');
+//
+//        if ( ! $dir) {
+//            return null;
+//        }
+//
+//        return "https://drive.google.com/drive/folders/{$dir['path']}";
+    }
+}
