a128b22829b1daae8dd989e802c69091e6d379ff
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 01:42:32 -0500

    moves views

--- resources/views/admin/medicationGroupsMaps/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('partials.adminUI')
-
-@section('content')
-    <meta name="medication.groups.maps.store" content="{{route('medication-groups-maps.store',  [], false)}}">
-    <meta name="medication.groups.maps.destroy" content="{{route('medication-groups-maps.destroy', [''], false)}}">
-    <cpm-medication-groups-maps-settings medication-groups-maps="{{$maps}}" medication-groups="{{$medicationGroups}}"></cpm-medication-groups-maps-settings>
-@endsection
