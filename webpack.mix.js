fd2bca8fb86b0fc008435d7df53e82f9e605fdd9
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 15:26:22 -0500

    moves shared vue components

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,8 @@ require('laravel-mix-merge-manifest');
 
 mix.setPublicPath('../../public').mergeManifest();
 
-mix.js('resources/assets/js/app-provider-admin-panel-ui.js', 'public/compiled/js').sourceMaps();
-mix.js('resources/assets/js/app-clh-admin-ui.js', 'public/compiled/js').sourceMaps();
+mix.js('Resources/assets/js/app-provider-admin-panel-ui.js', 'public/compiled/js').sourceMaps();
+mix.js('Resources/assets/js/app-clh-admin-ui.js', 'public/compiled/js').sourceMaps();
 
 if (mix.inProduction()) {
     mix.version();
