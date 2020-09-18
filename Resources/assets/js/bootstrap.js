35dc79cf4f130e6fcb6a3a90a4439c979a577815
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-18 17:27:06 -0500

    few changes

---
+++ Resources/assets/js/bootstrap.js
@@ -0,0 +1,21 @@
+window._ = require('lodash');
+window.Popper = require('popper.js').default;
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {
+}
+
+require('./bootstrap-axios');
+require('../../../../../resources/assets/js/logger-logdna').init();
+require('../../../../../resources/assets/js/pusher-echo').init();
+
+
