808ee04e5066db3de4c3e7140498e20295ab4b76
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-21 00:34:25 -0500

    moves helpers.php

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -91,3 +91,46 @@ if ( ! function_exists('isSelfEnrollmentTestModeEnabled')) {
         return filter_var(AppConfig::pull('testing_enroll_sms', true), FILTER_VALIDATE_BOOLEAN);
     }
 }
+
+if ( ! function_exists('isAllowedToSee2FA')) {
+    function isAllowedToSee2FA(User $user = null)
+    {
+        $twoFaEnabled = (bool) config('auth.two_fa_enabled');
+        if ( ! $twoFaEnabled) {
+            return false;
+        }
+        
+        if ( ! $user) {
+            $user = auth()->user();
+        }
+        
+        if ( ! $user || $user->isParticipant()) {
+            return false;
+        }
+        
+        return $user->isAdmin() || isTwoFaEnabledForPractice($user->program_id);
+    }
+}
+
+if ( ! function_exists('isTwoFaEnabledForPractice')) {
+    /**
+     * Key: two_fa_enabled_practices
+     * Default: false.
+     *
+     * @param mixed $practiceId
+     */
+    function isTwoFaEnabledForPractice($practiceId): bool
+    {
+        $key = 'two_fa_enabled_practices';
+        $val = AppConfig::pull($key, null);
+        if (null === $val) {
+            AppConfig::set($key, '');
+            
+            $twoFaEnabledPractices = [];
+        } else {
+            $twoFaEnabledPractices = explode(',', $val);
+        }
+        
+        return in_array($practiceId, $twoFaEnabledPractices);
+    }
+}
