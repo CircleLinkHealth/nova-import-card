18b523f67562729892fcceb393e8366197b761ba
kakoushias <kakoushias@gmail.com>
2020-09-21 18:42:57 +0200

    ROAD-288 - remove commands in favor of jobs package

--- Database/Migrations/2020_08_29_160303_seed_location_summaries_from_chargeables_table_data.php
+++ Database/Migrations/2020_08_29_160303_seed_location_summaries_from_chargeables_table_data.php
@@ -24,8 +24,6 @@ class SeedLocationSummariesFromChargeablesTableData extends Migration
      */
     public function up()
     {
-        Artisan::call('billing:migrate-all-location-services', [
-            'month' => '2020-08-01',
-        ]);
+        \CircleLinkHealth\CcmBilling\Jobs\MigrateChargeableServicesFromChargeablesToLocationSummariesTable::dispatch(\Carbon\Carbon::now()->startOfMonth());
     }
 }
