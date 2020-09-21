c100c50f3b4cc1ce71a722e6d4fc3904d5c11191
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-21 00:23:17 -0500

    composer

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Illuminate\Http\Middleware\FrameGuard::class,
+        \CircleLinkHealth\Core\Http\Middleware\SentryContext::class,
     ];
 
     /**
@@ -36,10 +38,16 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \CircleLinkHealth\Core\Http\Middleware\LogoutIfAccessDisabled::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+            \CircleLinkHealth\TwoFA\Http\Middleware\AuthyMiddleware::class,
+        ],
+        'sessions' => [
+            \Illuminate\Session\Middleware\StartSession::class,
         ],
 
         'api' => [
