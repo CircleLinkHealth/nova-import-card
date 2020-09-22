8a0111b45b4aff8b51237642bc30f56f3cb7eb84
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-22 13:29:34 -0500

    composer

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,11 +7,31 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     /**
+     * Indicates whether the XSRF-TOKEN cookie should be set on the response.
+     *
+     * @var bool
+     */
+    protected $addHttpCookie = true;
+    
+    /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array
      */
     protected $except = [
-        //
+        'enrollment/sms/reply',
+        '/sendgrid/status',
+        '/postmark/status',
+        '/postmark/inbound',
+        '/twilio/sms/status',
+        '/twilio/sms/inbound',
+        '/admin/reports/monthly-billing/v2/updateApproved',
+        '/admin/reports/monthly-billing/v2/updateRejected',
+        '/admin/reports/monthly-billing/v2/data',
+        '/admin/reports/monthly-billing/v2/storeProblem',
+        '/admin/reports/monthly-billing/v2/counts',
+        'api/v2.1/pagetimer',
+        'api/v2.1/time/patients',
+        'webhooks/on-sent-fax',
     ];
 }
