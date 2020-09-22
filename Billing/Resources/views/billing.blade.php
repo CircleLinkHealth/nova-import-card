243d0f1d4fbe43860d4a8539e38b051d4bc32e74
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-22 13:21:29 -0500

    Adds HTTPS for urls

--- Billing/Resources/views/billing.blade.php
+++ Billing/Resources/views/billing.blade.php
@@ -14,8 +14,6 @@
                 background-color: #ffff0052 !important;
             }
         </style>
-
-        <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
     @endpush
 
     <!-- Modal -->
