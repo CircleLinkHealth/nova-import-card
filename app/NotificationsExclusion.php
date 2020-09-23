86928428055de80331a9baea815841151fdcf17a
Pangratios Cosma <pagcosma@gmail.com>
2020-09-23 13:34:54 +0300

    do not send notification if in exclusions table + tests

---
+++ app/NotificationsExclusion.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App;
+
+use CircleLinkHealth\Customer\Entities\User;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\SmsExclusion.
+ *
+ * @property int                             $id
+ * @property int                             $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property User                            $user
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|NotificationsExclusion newModelQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|NotificationsExclusion newQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|NotificationsExclusion query()
+ * @mixin \Eloquent
+ * @property bool $sms
+ * @property bool $mail
+ */
+class NotificationsExclusion extends Model
+{
+    protected $casts = [
+        'sms'  => 'boolean',
+        'mail' => 'boolean',
+    ];
+    protected $fillable = [
+        'user_id',
+        'sms',
+        'mail',
+    ];
+    protected $table = 'notifications_exclusions';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    private static function isBlackListed($userId, string $channel): bool
+    {
+        return self::where('user_id', '=', $userId)
+            ->where($channel, '=', true)
+            ->exists();
+    }
+
+    public static function isMailBlackListed($userId): bool
+    {
+        return self::isBlackListed($userId, 'mail');
+    }
+
+    public static function isSmsBlackListed($userId): bool
+    {
+        return self::isBlackListed($userId, 'sms');
+    }
+}
