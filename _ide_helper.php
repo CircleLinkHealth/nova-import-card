ea9daf2a46c1a94d8055e3d9c4a063a68fe03a69
Pangratios Cosma <pagcosma@gmail.com>
2020-09-24 15:54:48 +0300

    update composer.json

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.17.2 on 2020-06-30 13:55:14.
+ * Generated for Laravel 7.17.2 on 2020-09-15 11:15:07.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
