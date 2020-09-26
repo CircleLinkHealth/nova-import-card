9789217c62d7ed27019bcfd3d31c2f89a97f9750
kakoushias <kakoushias@gmail.com>
2020-09-26 02:08:30 +0200

    ROAD- 288 - add missing repository method

--- Contracts/LocationProcessorRepository.php
+++ Contracts/LocationProcessorRepository.php
@@ -29,4 +29,6 @@ interface LocationProcessorRepository
     public function store(int $locationId, string $chargeableServiceCode, Carbon $month, float $amount = null): ChargeableLocationMonthlySummary;
 
     public function storeUsingServiceId(int $locationId, int $chargeableServiceId, Carbon $month, float $amount = null): ChargeableLocationMonthlySummary;
+    
+    public function hasServicesForMonth(int $locationId, Carbon $month): bool;
 }
