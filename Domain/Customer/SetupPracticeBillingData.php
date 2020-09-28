fa41cc3c9a7484f5be13b231023071ba9d615600
kakoushias <kakoushias@gmail.com>
2020-09-29 01:27:45 +0200

    ROAD-346 refresh practice Location summaries on practice CS change

--- Domain/Customer/SetupPracticeBillingData.php
+++ Domain/Customer/SetupPracticeBillingData.php
@@ -6,12 +6,15 @@
 
 namespace CircleLinkHealth\CcmBilling\Domain\Customer;
 
+use Carbon\Carbon;
+use CircleLinkHealth\CcmBilling\Entities\ChargeableLocationMonthlySummary;
 use CircleLinkHealth\CcmBilling\Jobs\MigrateChargeableServicesFromChargeablesToLocationSummariesTable;
 use CircleLinkHealth\CcmBilling\Jobs\MigratePracticeServicesFromChargeablesToLocationSummariesTable;
 use CircleLinkHealth\CcmBilling\Jobs\ProcessAllPracticePatientMonthlyServices;
 use CircleLinkHealth\CcmBilling\Jobs\ProcessPracticePatientMonthlyServices;
 use CircleLinkHealth\CcmBilling\Jobs\SeedCpmProblemChargeableServicesFromLegacyTables;
 use CircleLinkHealth\CcmBilling\Jobs\SeedPracticeCpmProblemChargeableServicesFromLegacyTables;
+use CircleLinkHealth\Customer\Entities\Location;
 
 class SetupPracticeBillingData
 {
@@ -27,5 +30,17 @@ class SetupPracticeBillingData
         MigratePracticeServicesFromChargeablesToLocationSummariesTable::dispatch($practiceId);
         SeedPracticeCpmProblemChargeableServicesFromLegacyTables::dispatch($practiceId);
         ProcessPracticePatientMonthlyServices::dispatch($practiceId);
+    }
+
+    public static function sync(int $practiceiD)
+    {
+        $locations = Location::where('practice_id', $practiceiD)
+            ->get();
+
+        ChargeableLocationMonthlySummary::whereIn('location_id', $locations->pluck('id')->toArray())
+            ->where('chargeable_month', Carbon::now()->startOfMonth())
+            ->delete();
+
+        self::forPractice($practiceiD);
     }
 }
