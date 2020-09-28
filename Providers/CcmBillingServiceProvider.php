adcd59872b7ee04da42b986c76d9ec90f750bda3
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-28 14:30:21 -0500

    Removes references to files that do not exist

--- Providers/CcmBillingServiceProvider.php
+++ Providers/CcmBillingServiceProvider.php
@@ -17,7 +17,6 @@ use CircleLinkHealth\CcmBilling\Repositories\LocationProcessorEloquentRepository
 use CircleLinkHealth\CcmBilling\Repositories\PatientProcessorEloquentRepository;
 use CircleLinkHealth\CcmBilling\Repositories\PatientServiceProcessorRepository;
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\ServiceProvider;
 
 class CcmBillingServiceProvider extends ServiceProvider implements DeferrableProvider
@@ -43,94 +42,11 @@ class CcmBillingServiceProvider extends ServiceProvider implements DeferrablePro
      */
     public function register()
     {
-        $this->registerTranslations();
-        $this->registerViews();
-
-        if ($this->app->runningInConsole()) {
-            $this->registerFactories();
-            $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-        }
-
-        //        $this->app->register(RouteServiceProvider::class);
-
         $this->app->bind(PatientMonthlyBillingProcessor::class, MonthlyProcessor::class);
 
         $this->app->singleton(PatientServiceRepositoryInterface::class, PatientServiceProcessorRepository::class);
         $this->app->singleton(LocationProblemServiceRepositoryInterface::class, LocationProblemServiceRepository::class);
         $this->app->singleton(LocationProcessorRepository::class, LocationProcessorEloquentRepository::class);
         $this->app->singleton(PatientProcessorEloquentRepositoryInterface::class, PatientProcessorEloquentRepository::class);
-
-//        $this->commands([]);
-    }
-
-    /**
-     * Register an additional directory of factories.
-     */
-    public function registerFactories()
-    {
-        if ( ! isProductionEnv()) {
-            app(Factory::class)->load(__DIR__.'/../Database/factories');
-        }
-    }
-
-    /**
-     * Register translations.
-     */
-    public function registerTranslations()
-    {
-        $langPath = resource_path('lang/modules/ccmbilling');
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, 'ccmbilling');
-        } else {
-            $this->loadTranslationsFrom(__DIR__.'/../Resources/lang', 'ccmbilling');
-        }
-    }
-
-    /**
-     * Register views.
-     */
-    public function registerViews()
-    {
-        $viewPath = resource_path('views/modules/ccmbilling');
-
-        $sourcePath = __DIR__.'/../Resources/views';
-
-        $this->publishes(
-            [
-                $sourcePath => $viewPath,
-            ],
-            'views'
-        );
-
-        $this->loadViewsFrom(
-            array_merge(
-                array_map(
-                    function ($path) {
-                        return $path.'/modules/ccmbilling';
-                    },
-                    \Config::get('view.paths')
-                ),
-                [$sourcePath]
-            ),
-            'ccmbilling'
-        );
-    }
-
-    /**
-     * Register config.
-     */
-    protected function registerConfig()
-    {
-        $this->publishes(
-            [
-                __DIR__.'/../Config/config.php' => config_path('ccmbilling.php'),
-            ],
-            'config'
-        );
-        $this->mergeConfigFrom(
-            __DIR__.'/../Config/config.php',
-            'ccmbilling'
-        );
     }
 }
