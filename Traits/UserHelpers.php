d3fd5ad589c291744bcad60a69c0936ddd69e90b
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-09-28 10:04:12 -0500

    removes duplicate USerHelpers trait

--- Traits/UserHelpers.php
+++ Traits/UserHelpers.php
@@ -14,13 +14,16 @@ use CircleLinkHealth\Customer\Entities\Nurse;
 use CircleLinkHealth\Customer\Entities\NurseContactWindow;
 use CircleLinkHealth\Customer\Entities\Patient;
 use CircleLinkHealth\Customer\Entities\PatientContactWindow;
+use CircleLinkHealth\Customer\Entities\PatientMonthlySummary;
 use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Customer\Entities\Role;
 use CircleLinkHealth\Customer\Entities\SaasAccount;
 use CircleLinkHealth\Customer\Entities\User;
 use CircleLinkHealth\Customer\Repositories\PatientWriteRepository;
 use CircleLinkHealth\Customer\Repositories\UserRepository;
+use CircleLinkHealth\Eligibility\Entities\PcmProblem;
 use CircleLinkHealth\NurseInvoices\Config\NurseCcmPlusConfig;
+use CircleLinkHealth\SharedModels\Entities\CarePlan;
 use CircleLinkHealth\SharedModels\Entities\CpmProblem;
 use Faker\Factory;
 use Illuminate\Support\Str;
@@ -37,30 +40,30 @@ trait UserHelpers
                 'service'     => 'phone',
                 'status'      => 'not reached',
                 'called_date' => '2016-07-16',
-
+                
                 'attempt_note' => '',
-
+                
                 'scheduler' => $scheduler->user->id,
-
+                
                 'inbound_phone_number' => '111-111-1111',
-
+                
                 'outbound_phone_number' => '',
-
+                
                 'inbound_cpm_id'  => $patient->user->id,
                 'outbound_cpm_id' => $scheduler->user->id,
-
+                
                 'call_time'  => 0,
                 'created_at' => Carbon::now()->toDateTimeString(),
-
+                
                 'scheduled_date' => '2016-12-01',
                 'window_start'   => '09:00',
                 'window_end'     => '10:00',
-
+                
                 'is_cpm_outbound' => true,
             ]
         );
     }
-
+    
     /**
      * @param int    $practiceId
      * @param string $roleName
@@ -73,15 +76,15 @@ trait UserHelpers
     ): User {
         $practiceId = parseIds($practiceId)[0];
         $roles      = [Role::whereName($roleName)->firstOrFail()->id];
-
+        
         //creates the User
         $user = $this->setupUser($practiceId, $roles, $ccmStatus);
-
+        
         $email     = $user->email;
         $locations = $user->locations->pluck('id')->all();
-
+        
         $isTest = method_exists($this, 'assertDatabaseHas');
-
+        
         if ($isTest) {
             foreach ($locations as $locId) {
                 $this->assertDatabaseHas(
@@ -93,12 +96,12 @@ trait UserHelpers
                 );
             }
         }
-
+        
         if ($isTest) {
             //check that it was created
             $this->assertDatabaseHas('users', ['email' => $email]);
         }
-
+        
         //check that the roles were created
         foreach ($roles as $role) {
             $is_admin = 1 == $role;
@@ -114,34 +117,35 @@ trait UserHelpers
                 );
             }
         }
-
-        if ('participant' == $roleName && isCpm()) {
-            $user->carePlan()->updateOrCreate(
+        
+        if ('participant' == $roleName) {
+            $user->ccdMedications()->create([
+                'name' => 'Test Aspirin',
+            ]);
+            CarePlan::updateOrCreate(
                 [
-                    'care_plan_template_id' => getDefaultCarePlanTemplate()->id,
+                    'user_id' => $user->id,
                 ],
                 [
-                    'status' => 'draft',
+                    'care_plan_template_id' => getDefaultCarePlanTemplate()->id,
+                    'status'                => 'draft',
                 ]
             );
+            $this->makePatientMonthlyRecord($user->patientInfo);
         }
-
-        $arr = ['practices', 'patientInfo'];
-        if (isCpm()) {
-            $arr[] = 'carePlan';
-        }
-        $user->load($arr);
-
+        
+        $user->load(['practices', 'patientInfo', 'carePlan']);
+        
         return $user;
     }
-
+    
     public function createWindowForNurse(
         Nurse $nurse,
         Carbon $st,
         Carbon $end
     ) {
         $window = timestampsToWindow($st, $end);
-
+        
         return NurseContactWindow::create(
             [
                 'date'              => $st->toDateString(),
@@ -153,9 +157,9 @@ trait UserHelpers
             ]
         );
     }
-
+    
     //NURSE TEST HELPERS
-
+    
     public function createWindowForPatient(
         Patient $patient,
         Carbon $st,
@@ -163,33 +167,45 @@ trait UserHelpers
         $dayOfWeek
     ) {
         $window = timestampsToWindow($st, $end);
-
+        
         return PatientContactWindow::create(
             [
                 'window_time_start' => $window['start'],
                 'window_time_end'   => $window['end'],
-
+                
                 'day_of_week' => $dayOfWeek,
-
+                
                 'patient_info_id' => $patient->id,
             ]
         );
     }
-
+    
     public function makePatientMonthlyRecord(Patient $patient)
     {
-        return (app(PatientWriteRepository::class))->updateCallLogs($patient, true);
+        return PatientMonthlySummary::updateOrCreate(
+            [
+                'month_year' => now()->startOfMonth()->toDateString(),
+                'patient_id' => $patient->user_id,
+            ],
+            [
+                'ccm_time'                            => 0,
+                'bhi'                                 => 0,
+                'no_of_calls'                         => 0,
+                'no_of_successful_calls'              => 0,
+                'no_call_attempts_since_last_success' => 0,
+            ]
+        );
     }
-
+    
     public function setupUser($practiceId, $roles, $ccmStatus = 'enrolled')
     {
         $faker = Factory::create();
-
+        
         $firstName = $faker->firstName;
         $lastName  = $faker->lastName;
         $email     = $faker->email;
         $workPhone = (new StringManipulation())->formatPhoneNumber($faker->phoneNumber);
-
+        
         $bag = new ParameterBag(
             [
                 'saas_account_id' => SaasAccount::firstOrFail()->id,
@@ -206,48 +222,49 @@ trait UserHelpers
                 'address2'          => '',
                 'city'              => $faker->city,
                 'state'             => 'AL',
-                'zip'               => '12345',
+                'zip'               => '55555',
                 'is_auto_generated' => true,
                 'roles'             => $roles,
                 'timezone'          => 'America/New_York',
-
+                
                 //provider Info
-                'prefix'     => 'Dr',
-                'suffix'     => 'MD',
-                'npi_number' => 1234567890,
-                'specialty'  => 'Unit Tester',
-
+                'prefix'                 => 'Dr',
+                'suffix'                 => 'MD',
+                'npi_number'             => 1234567890,
+                'specialty'              => 'Unit Tester',
+                'approve_own_care_plans' => true,
+                
                 //phones
                 'home_phone_number' => $workPhone,
-
+                
                 'ccm_status' => $ccmStatus,
                 'birth_date' => $faker->date('Y-m-d'),
             ]
         );
-
+        
         //create a user
         $user = (new UserRepository())->createNewUser($bag);
-
+        
         $practice = Practice::with('locations')->findOrFail($practiceId);
-
+        
         $locations = $practice->locations
             ->pluck('id')
             ->all();
-
+        
         $user->locations()->sync($locations);
-
+        
         if (array_key_exists(0, $locations) && is_numeric($locations[0])) {
             $user->setPreferredContactLocation($locations[0]);
         }
-
+        
         return $user;
     }
-
+    
     private function addWorkHours(User $nurse, Carbon $forDate, int $hours)
     {
         $workWeekStart = $forDate->copy()->startOfWeek()->toDateString();
         $dayOfWeek     = carbonToClhDayOfWeek($forDate->dayOfWeek);
-
+        
         $nurse->nurseInfo->windows()->updateOrCreate(
             [
                 'date' => $forDate->toDateString(),
@@ -259,7 +276,7 @@ trait UserHelpers
                 'repeat_frequency'  => 'does_not_repeat',
             ]
         );
-
+        
         $nurse->nurseInfo->workhourables()->updateOrCreate(
             [
                 'work_week_start' => $workWeekStart,
@@ -269,65 +286,83 @@ trait UserHelpers
             ]
         );
     }
-
+    
     private function setupNurse(
         User $nurse,
         bool $variableRate = true,
         float $hourlyRate = 29.0,
         bool $enableCcmPlus = false,
-        float $visitFee = null
+        float $visitFee = null,
+        Carbon $startDate = null
     ) {
+        if ( ! $startDate) {
+            $startDate = now()->startOfDay();
+        }
+        
+        $nurse->nurseInfo->start_date = $startDate;
+        
         $nurse->nurseInfo->is_variable_rate = $variableRate;
         $nurse->nurseInfo->hourly_rate      = $hourlyRate;
         $nurse->nurseInfo->high_rate        = 30.00;
         $nurse->nurseInfo->high_rate_2      = 28.00;
         $nurse->nurseInfo->high_rate_3      = 27.50;
-
+        
         $nurse->nurseInfo->low_rate = 10;
-
+        
         if ($visitFee) {
             $nurse->nurseInfo->visit_fee   = $visitFee;
             $nurse->nurseInfo->visit_fee_2 = 12.00;
             $nurse->nurseInfo->visit_fee_3 = 11.75;
         }
-
+        
         $nurse->nurseInfo->save();
-
+        
         AppConfig::set(NurseCcmPlusConfig::NURSE_CCM_PLUS_ENABLED_FOR_ALL, $enableCcmPlus
-                    ? 'true'
-                    : 'false');
-
+            ? 'true'
+            : 'false');
+        
+        //make sure this is false
+        AppConfig::set(NurseCcmPlusConfig::NURSE_CCM_PLUS_ALT_ALGO_ENABLED_FOR_ALL, 'false');
+        
         if ($enableCcmPlus && $visitFee) {
             $current = implode(',', NurseCcmPlusConfig::altAlgoEnabledForUserIds());
             AppConfig::set(NurseCcmPlusConfig::NURSE_CCM_PLUS_ALT_ALGO_ENABLED_FOR_USER_IDS, $current.(empty($current)
-                            ? ''
-                            : ',').$nurse->id);
-
+                    ? ''
+                    : ',').$nurse->id);
+            
             //hack for SmartCacheManager
             \Cache::store('array')->clear();
         }
-
+        
         return $nurse;
     }
-
+    
     private function setupPatient(Practice $practice, $isBhi = false, $pcmOnly = false)
     {
         $patient = $this->createUser($practice->id, 'participant');
         $patient->setPreferredContactLocation($this->location->id);
-
+        
         if ($isBhi) {
             $consentDate = Carbon::parse(Patient::DATE_CONSENT_INCLUDES_BHI);
             $consentDate->addDay();
             $patient->patientInfo->consent_date = $consentDate;
         }
-
+        
+        $patient->patientInfo->ccm_status = Patient::ENROLLED;
         $patient->patientInfo->save();
         $cpmProblems = CpmProblem::get();
-
+        
         //$pcmOnly means one ccm condition only
         if ($pcmOnly) {
             $ccdProblems = $patient->ccdProblems()->createMany([
-                ['name' => 'test'.Str::random(5)],
+                ['name' => 'test'.Str::random(5), 'code' => 'pcm_test'],
+            ]);
+            $patient->ccdProblems()->first()->codes()->create([
+                'code' => 'pcm_test',
+            ]);
+            PcmProblem::create([
+                'practice_id' => $practice->id,
+                'code'        => 'pcm_test',
             ]);
         } else {
             $ccdProblems = $patient->ccdProblems()->createMany([
@@ -336,7 +371,7 @@ trait UserHelpers
                 ['name' => 'test'.Str::random(5)],
             ]);
         }
-
+        
         $len = $ccdProblems->count();
         for ($i = 0; $i < $len; ++$i) {
             $problem = $ccdProblems->get($i);
@@ -344,11 +379,11 @@ trait UserHelpers
             if ($isLast && $isBhi) {
                 $problem->cpmProblem()->associate($cpmProblems->firstWhere('is_behavioral', '=', 1));
             } else {
-                $problem->cpmProblem()->associate($cpmProblems->random());
+                $problem->cpmProblem()->associate($cpmProblems->firstWhere('is_behavioral', '=', 0));
             }
             $problem->save();
         }
-
+        
         return $patient;
     }
 }
