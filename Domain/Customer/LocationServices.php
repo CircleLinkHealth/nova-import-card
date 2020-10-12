8e9f686a01f50ccf58f88a1249526f50fb2f60bd
kakoushias <kakoushias@gmail.com>
2020-10-12 15:48:43 +0200

    ROAD-346 Location Services action class more methods

--- Domain/Customer/LocationServices.php
+++ Domain/Customer/LocationServices.php
@@ -7,35 +7,49 @@
 namespace CircleLinkHealth\CcmBilling\Domain\Customer;
 
 use Carbon\Carbon;
-use CircleLinkHealth\CcmBilling\Entities\ChargeableLocationMonthlySummary;
+use CircleLinkHealth\CcmBilling\Contracts\LocationProcessorRepository;
 use CircleLinkHealth\Customer\Entities\ChargeableService;
 use Illuminate\Support\Facades\Cache;
 
 class LocationServices
 {
+    protected LocationProcessorRepository $repo;
+
+    public function __construct(LocationProcessorRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function hasCcmPlusCodes(int $locationId): bool
+    {
+        return $this->hasServicesForMonth($locationId, ChargeableService::CCM_PLUS_CODES);
+    }
+
     public static function hasCCMPlusServiceCode(int $locationId, $cacheIt = false): bool
     {
+        $static = new static(app(LocationProcessorRepository::class));
+
         if ($cacheIt) {
             return Cache::remember(
                 "location:{$locationId}:hasCcmPlus",
                 2,
-                function () use ($locationId) {
-                    return self::queryCcmPlusCodeExists($locationId);
+                function () use ($static, $locationId) {
+                    return $static->hasCcmPlusCodes($locationId);
                 }
             );
         }
 
-        return self::queryCcmPlusCodeExists($locationId);
+        return $static->hasCcmPlusCodes($locationId);
     }
 
-    public static function hasServicesForMonth(int $locationId, array $chargeableServiceCodes, Carbon $month = null): bool
+    public static function hasCodesForMonth(int $locationId, array $chargeableServiceCodes, Carbon $month = null)
     {
+        return (new static(app(LocationProcessorRepository::class)))
+            ->hasServicesForMonth($locationId, $chargeableServiceCodes, $month);
     }
 
-    public static function queryCcmPlusCodeExists(int $locationId): bool
+    public function hasServicesForMonth(int $locationId, array $chargeableServiceCodes, Carbon $month = null): bool
     {
-        return ChargeableLocationMonthlySummary::where('location_id', $locationId)
-            ->whereHas('chargeableService', fn ($cs) => $cs->whereIn('code', ChargeableService::CCM_PLUS_CODES))
-            ->exists();
+        return $this->repo->hasServicesForMonth($locationId, $chargeableServiceCodes, $month ?? Carbon::now()->startOfMonth());
     }
 }
