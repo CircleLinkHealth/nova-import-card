ff8a6bbf55c60ed20d725c4a17c3a0c659160ff7
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-10-13 10:07:10 -0500

    Increase timeout so we can download large Toledo eligibility report

--- Http/Controllers/EligibilityBatchController.php
+++ Http/Controllers/EligibilityBatchController.php
@@ -225,6 +225,8 @@ class EligibilityBatchController extends Controller
 
     public function downloadEligibleCsv(EligibilityBatch $batch)
     {
+        ini_set('max_execution_time', 300);
+    
         $practice = Practice::findOrFail($batch->practice_id);
         $fileName = $practice->display_name.'_'.Carbon::now()->toAtomString();
 
