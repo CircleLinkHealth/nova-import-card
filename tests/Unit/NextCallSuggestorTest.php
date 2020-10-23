af6cedb1764c9dd661c89a6bb618797b7e34f3c8
kakoushias <kakoushias@gmail.com>
2020-10-23 21:22:28 +0200

    ROAD-346 fix NextCallSuggestorTest.php (again but better)

--- tests/Unit/NextCallSuggestorTest.php
+++ tests/Unit/NextCallSuggestorTest.php
@@ -11,58 +11,43 @@ use App\Algorithms\Calls\NextCallSuggestor\Suggestion;
 use App\Algorithms\Calls\NextCallSuggestor\Suggestor;
 use App\Algorithms\Calls\NurseFinder\NurseFinderEloquentRepository;
 use App\Contracts\CallHandler;
-use App\Traits\Tests\PracticeHelpers;
 use Carbon\Carbon;
 use CircleLinkHealth\Customer\AppConfig\StandByNurseUser;
-use CircleLinkHealth\Customer\Entities\PatientContactWindow;
 use CircleLinkHealth\Customer\Entities\User;
-use CircleLinkHealth\Customer\Traits\UserHelpers;
 use Mockery;
 use Tests\TestCase;
 
 class NextCallSuggestorTest extends TestCase
 {
-    use PracticeHelpers;
-    use UserHelpers;
-    protected $location;
-    protected $patient;
-
-    protected $practice;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->practice = $this->setupPractice(true, true, true, true);
-        $this->patient  = $this->setupPatient($this->practice, true);
-    }
-
     public function test_it_returns_patient_associated_nurse_in_1st_week_of_month()
     {
         Carbon::setTestNow(now()->startOfMonth());
         $this->itShouldReturnPatientAssociatedNurse();
     }
-
+    
     public function test_it_returns_patient_associated_nurse_in_2nd_week_of_month()
     {
         Carbon::setTestNow(now()->startOfMonth()->addWeeks());
         $this->itShouldReturnPatientAssociatedNurse();
     }
-
+    
     public function test_it_returns_patient_associated_nurse_in_3rd_week_of_month()
     {
         Carbon::setTestNow(now()->startOfMonth()->addWeeks(2));
         $this->itShouldReturnPatientAssociatedNurse();
     }
-
+    
     public function test_it_returns_patient_associated_nurse_in_4th_week_of_month()
     {
         Carbon::setTestNow(now()->startOfMonth()->addWeeks(3));
         $this->itShouldReturnPatientAssociatedNurse();
     }
-
+    
     public function test_it_returns_standby_nurse_if_patient_doesnt_have_associated_nurse_and_standby_nurse_is_set()
     {
+        $patient = factory(User::class)->make([
+            'id' => rand(1, 9999999),
+        ]);
         $nurse = factory(User::class)->make([
             'id'           => 123456789,
             'display_name' => 'Soulla Masoulla',
@@ -70,28 +55,31 @@ class NextCallSuggestorTest extends TestCase
         $this->mock(StandByNurseUser::class, function ($mock) use ($nurse) {
             $mock->shouldReceive('user')->atLeast(1)->andReturn($nurse);
         });
-
-        $suggestion = (new Suggestor())->handle($this->patient, $handler = new SuccessfulCall());
-
-        self::assertValidNurseResponse($suggestion, $nurse, $this->patient, $handler);
+        
+        $suggestion = (new Suggestor())->handle($patient, $handler = new SuccessfulCall());
+        
+        self::assertValidNurseResponse($suggestion, $nurse, $patient, $handler);
     }
-
+    
     public function test_nurse_is_null_if_patient_doesnt_have_associated_nurse_and_standby_nurse_is_not_set()
     {
+        $patient = factory(User::class)->make([
+            'id' => rand(1, 9999999),
+        ]);
         $repo = Mockery::mock(NurseFinderEloquentRepository::class);
-
+        
         $repo->shouldReceive('find')
-            ->with($this->patient->id)
+            ->with($patient->id)
             ->once()
             ->andReturn(null);
-
+        
         $this->instance(NurseFinderEloquentRepository::class, $repo);
-
-        $suggestion = (new Suggestor())->handle($this->patient, $handler = new SuccessfulCall());
-
-        self::assertNullResponse($suggestion, $this->patient, $handler);
+        
+        $suggestion = (new Suggestor())->handle($patient, $handler = new SuccessfulCall());
+        
+        self::assertNullResponse($suggestion, $patient, $handler);
     }
-
+    
     private static function assertNullResponse(Suggestion $suggestion, User $patient, CallHandler $handler)
     {
         self::assertTrue($suggestion instanceof Suggestion);
@@ -113,18 +101,12 @@ class NextCallSuggestorTest extends TestCase
         self::assertEquals($patient, $suggestion->patient);
         self::assertEquals($handler->createSchedulerInfoString($suggestion), $suggestion->predicament);
         self::assertEquals(true, $suggestion->successful);
-        self::assertEquals(
-            Carbon::parse(PatientContactWindow::DEFAULT_WINDOW_TIME_END)->format('H:i'),
-            Carbon::parse($suggestion->window_end)->format('H:i')
-        );
+        self::assertEquals(Suggestor::DEFAULT_WINDOW_END, $suggestion->window_end);
         self::assertEquals('This patient has no assigned nurse in CPM.', $suggestion->window_match);
-        self::assertEquals(
-            Carbon::parse(PatientContactWindow::DEFAULT_WINDOW_TIME_START)->format('H:i'),
-            Carbon::parse($suggestion->window_start)->format('H:i')
-        );
+        self::assertEquals(Suggestor::DEFAULT_WINDOW_START, $suggestion->window_start);
         self::assertEquals(0, $suggestion->ccm_time_in_seconds);
     }
-
+    
     private static function assertValidNurseResponse(Suggestion $suggestion, User $nurse, User $patient, CallHandler $handler)
     {
         self::assertTrue($suggestion instanceof Suggestion);
@@ -147,35 +129,31 @@ class NextCallSuggestorTest extends TestCase
         self::assertEquals($patient, $suggestion->patient);
         self::assertEquals($handler->createSchedulerInfoString($suggestion), $suggestion->predicament);
         self::assertEquals(true, $suggestion->successful);
-        self::assertEquals(
-            Carbon::parse(PatientContactWindow::DEFAULT_WINDOW_TIME_END)->format('H:i'),
-            Carbon::parse($suggestion->window_end)->format('H:i')
-        );
+        self::assertEquals(Suggestor::DEFAULT_WINDOW_END, $suggestion->window_end);
         self::assertEquals("Assigning next call to $nurse->display_name.", $suggestion->window_match);
-        self::assertEquals(
-            Carbon::parse(PatientContactWindow::DEFAULT_WINDOW_TIME_START)->format('H:i'),
-            Carbon::parse($suggestion->window_start)->format('H:i')
-        );
+        self::assertEquals(Suggestor::DEFAULT_WINDOW_START, $suggestion->window_start);
         self::assertEquals(0, $suggestion->ccm_time_in_seconds);
     }
-
+    
     private function itShouldReturnPatientAssociatedNurse()
     {
+        $patient = factory(User::class)->make([
+            'id' => rand(1, 9999999),
+        ]);
         $nurse = factory(User::class)->make([
             'id' => rand(1, 9999999),
         ]);
-
         $repo = Mockery::mock(NurseFinderEloquentRepository::class);
-
+        
         $repo->shouldReceive('find')
-            ->with($this->patient->id)
+            ->with($patient->id)
             ->once()
             ->andReturn($nurse);
-
+        
         $this->instance(NurseFinderEloquentRepository::class, $repo);
-
-        $suggestion = (new Suggestor())->handle($this->patient, $handler = new SuccessfulCall());
-
-        self::assertValidNurseResponse($suggestion, $nurse, $this->patient, $handler);
+        
+        $suggestion = (new Suggestor())->handle($patient, $handler = new SuccessfulCall());
+        
+        self::assertValidNurseResponse($suggestion, $nurse, $patient, $handler);
     }
 }
