3e2ef297157a2c9184480690c00e4154fe57f5a0
kakoushias <kakoushias@gmail.com>
2020-10-26 18:51:24 +0100

    ROAD-346 add RPM processors, filter timetracker CS by problems too

--- Domain/Patient/PatientServicesForTimeTracker.php
+++ Domain/Patient/PatientServicesForTimeTracker.php
@@ -95,7 +95,7 @@ class PatientServicesForTimeTracker
     {
         return $summaries->reject(function (ChargeablePatientMonthlySummaryView $summary) {
             return in_array($summary->chargeable_service_name, self::NON_TIME_TRACKABLE_SERVICES);
-        });
+        })->filter(fn($summary) => PatientIsOfServiceCode::execute($summary->patient_user_id, $summary->chargeable_service_code));
     }
 
     private function repo(): PatientServiceProcessorRepository
