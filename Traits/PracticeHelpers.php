4feddca6d1da86c03251127e3b1f1b6940a5657c
kakoushias <kakoushias@gmail.com>
2020-10-29 16:31:01 +0100

    ROAD-346 Remove/Replace duplicate Practice Trait, update Customer one

--- Traits/PracticeHelpers.php
+++ Traits/PracticeHelpers.php
@@ -6,6 +6,7 @@
 
 namespace CircleLinkHealth\Customer\Traits;
 
+use CircleLinkHealth\CcmBilling\Domain\Customer\SetupPracticeBillingData;
 use CircleLinkHealth\Customer\Entities\ChargeableService;
 use CircleLinkHealth\Customer\Entities\Location;
 use CircleLinkHealth\Customer\Entities\Practice;
@@ -19,41 +20,41 @@ trait PracticeHelpers
         bool $addBhiService = false,
         bool $addPcmService = false
     ) {
-        $this->location = Location::where('practice_id', $practice->id)->first();
-
-        if ( ! $this->location) {
-            $this->location = factory(Location::class)->create(['practice_id' => $practice->id]);
+        if ( ! Location::where('practice_id', $practice->id)->exists()) {
+            factory(Location::class)->create(['practice_id' => $practice->id]);
         }
-
+        
         $sync = [];
-
+        
         if ($addCcmService) {
             $ccmService            = ChargeableService::where('code', '=', ChargeableService::CCM)->first();
             $sync[$ccmService->id] = ['amount' => 29.0];
         }
-
+        
         if ($addCcmPlusServices) {
             $ccmPlus40            = ChargeableService::where('code', '=', ChargeableService::CCM_PLUS_40)->first();
             $ccmPlus60            = ChargeableService::where('code', '=', ChargeableService::CCM_PLUS_60)->first();
             $sync[$ccmPlus40->id] = ['amount' => 28.0];
             $sync[$ccmPlus60->id] = ['amount' => 28.0];
         }
-
+        
         if ($addBhiService) {
             $bhi            = ChargeableService::where('code', '=', ChargeableService::BHI)->first();
             $sync[$bhi->id] = ['amount' => 28.0];
         }
-
+        
         if ($addPcmService) {
             $bhi            = ChargeableService::where('code', '=', ChargeableService::PCM)->first();
             $sync[$bhi->id] = ['amount' => 27.0];
         }
-
+        
         $practice->chargeableServices()->sync($sync);
-
+        
+        SetupPracticeBillingData::sync($practice->id);
+        
         return $practice;
     }
-
+    
     private function setupPractice(
         bool $addCcmService = false,
         bool $addCcmPlusServices = false,
@@ -61,7 +62,7 @@ trait PracticeHelpers
         bool $addPcmService = false
     ): Practice {
         $practice = factory(Practice::class)->create();
-
+        
         return $this->setupExistingPractice(
             $practice,
             $addCcmService,
