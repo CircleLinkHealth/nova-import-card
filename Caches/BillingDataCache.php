a97f7fb0e0599dc43d5fbb473bd80cbcdb563059
kakoushias <kakoushias@gmail.com>
2020-11-02 18:25:47 +0100

    ROAD-346 billing cache store location summaries

--- Caches/BillingDataCache.php
+++ Caches/BillingDataCache.php
@@ -7,13 +7,14 @@
 namespace CircleLinkHealth\CcmBilling\Caches;
 
 use CircleLinkHealth\Customer\Entities\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class BillingDataCache implements BillingCache
 {
-    protected array $locationServiceCache = [];
+    protected array $locationSummaryCache = [];
     protected array $patientCache         = [];
 
-    protected array $queriedLocationServices = [];
+    protected array $queriedLocationSummaries = [];
 
     protected array $queriedPatients = [];
 
@@ -22,31 +23,87 @@ class BillingDataCache implements BillingCache
         $this->patientCache    = [];
         $this->queriedPatients = [];
     }
+    
+    public function clearLocations(): void
+    {
+        $this->locationSummaryCache    = [];
+        $this->queriedLocationSummaries = [];
+    }
+    
+    public function forgetLocationSummaries(int $locationId) : void
+    {
+        $this->locationSummaryCache = collect($this->locationSummaryCache)
+            ->filter(fn($locationSummary) => $locationSummary->location_id != $locationId)
+            ->toArray();
+        
+        $this->queriedLocationSummaries = collect($this->queriedLocationSummaries)
+            ->filter(fn($id) => $id != $locationId)
+            ->toArray();
+    }
 
     public function forgetPatient(int $patientId): void
     {
-        $this->patientCache    = collect($this->patientCache)->filter(fn ($p) => $p->id != $patientId)->toArray();
-        $this->queriedPatients = collect($this->patientCache)->filter(fn ($id) => $id != $patientId)->toArray();
+        $this->patientCache    = collect($this->patientCache)
+            ->filter(fn ($p) => $p->id != $patientId)
+            ->toArray();
+        $this->queriedPatients = collect($this->patientCache)
+            ->filter(fn ($id) => $id != $patientId)
+            ->toArray();
     }
 
     public function getPatient(int $patientId): ?User
     {
         return collect($this->patientCache)->firstWhere('id', $patientId);
     }
+    
+    public function getLocationSummaries(int $locationId): ?Collection
+    {
+        return new Collection(
+            collect($this->locationSummaryCache)
+                ->where('location_id', $locationId)
+                ->toArray()
+        );
+    }
 
     public function patientExistsInCache(int $patientId): bool
     {
-        return collect($this->patientCache)->where('id', $patientId)->isNotEmpty();
+        return collect($this->patientCache)
+            ->where('id', $patientId)
+            ->isNotEmpty();
+    }
+    
+    public function locationSummariesExistInCache(int $locationId): bool
+    {
+        return collect($this->locationSummaryCache)
+            ->where('location_id', $locationId)
+            ->isNotEmpty();
     }
 
     public function patientWasQueried(int $patientId): bool
     {
         return in_array($patientId, $this->queriedPatients);
     }
+    
+    public function locationWasQueried(int $locationId): bool
+    {
+        return in_array($locationId, $this->queriedLocationSummaries);
+    }
 
     public function setPatientInCache(User $patientUser): void
     {
         $this->patientCache[] = $patientUser;
+    }
+    
+    public function setLocationSummariesInCache(Collection $summaries) : void
+    {
+        $this->locationSummaryCache = collect($this->locationSummaryCache)
+            ->merge($summaries)
+            ->toArray();
+    }
+    
+    public function setQueriedLocation(int $locationId):void
+    {
+        $this->queriedLocationSummaries[] = $locationId;
     }
 
     public function setQueriedPatient(int $patientId): void
