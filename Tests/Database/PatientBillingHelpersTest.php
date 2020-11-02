a3dbd2eaefd42b0596b040e5690b870f5ee3b8db
kakoushias <kakoushias@gmail.com>
2020-11-02 12:15:40 +0100

    ROAD-346 remove un-needed billing base test case, fix bhi helper test

--- Tests/Database/PatientBillingHelpersTest.php
+++ Tests/Database/PatientBillingHelpersTest.php
@@ -7,12 +7,33 @@
 namespace CircleLinkHealth\CcmBilling\Tests\Database;
 
 use Carbon\Carbon;
+use CircleLinkHealth\CcmBilling\Entities\BillingConstants;
+use CircleLinkHealth\CcmBilling\Events\PatientConsentedToService;
 use CircleLinkHealth\CcmBilling\Jobs\ProcessSinglePatientMonthlyServices;
 use CircleLinkHealth\Customer\Entities\ChargeableService;
 use CircleLinkHealth\Customer\Entities\Patient;
+use CircleLinkHealth\Customer\Traits\PracticeHelpers;
+use CircleLinkHealth\Customer\Traits\UserHelpers;
+use Facades\FriendsOfCat\LaravelFeatureFlags\Feature;
+use Tests\CustomerTestCase;
 
-class PatientBillingHelpersTest extends BillingTestCase
+class PatientBillingHelpersTest extends CustomerTestCase
 {
+    use UserHelpers;
+    use PracticeHelpers;
+    
+    protected $location;
+    
+    protected $practice;
+    
+    public function setUp(): void
+    {
+        parent::setUp();
+        
+        $this->practice = $this->setupPractice(true, true, true, true);
+    }
+    
+    
     public function test_patient_bhi_time_helper_uses_new_summaries()
     {
     }
@@ -23,25 +44,23 @@ class PatientBillingHelpersTest extends BillingTestCase
 
     public function test_patient_is_bhi_helper_uses_new_summaries()
     {
-        self::assertTrue(($patient = $this->patient())->ccdProblems->isEmpty());
-
-        $this->createPatientCcdProblemOfCode($patient = $this->patient(), $bhiCode = ChargeableService::BHI);
-
-        self::assertTrue(
-            $this->getLocation()
-                ->chargeableServiceSummaries
-                ->where('chargeable_service_id', ChargeableService::getChargeableServiceIdUsingCode($bhiCode))
-                ->isNotEmpty()
-        );
-
-        $patient->notes()->create([
+        $bhiPatient = $this->setupPatient($this->practice, true);
+        
+        $bhiPatient->notes()->create([
             'type'      => Patient::BHI_CONSENT_NOTE_TYPE,
             'author_id' => $this->careCoach()->id,
         ]);
-
-        ProcessSinglePatientMonthlyServices::dispatch($patient->id, Carbon::now()->startOfMonth());
-
-        self::assertTrue($patient->isBhi());
+        
+        event(new PatientConsentedToService($bhiPatient->id, $bhiCode = ChargeableService::BHI));
+        
+        self::assertTrue(Feature::isEnabled(BillingConstants::BILLING_REVAMP_FLAG));
+        self::assertTrue($bhiPatient->isBhi());
+        self::assertTrue(
+            $bhiPatient->chargeableMonthlySummariesView()
+                ->where('chargeable_service_code', $bhiCode)
+                ->where('chargeable_month', Carbon::now()->startOfMonth())
+        ->exists()
+        );
     }
 
     public function test_patient_is_pcm_helper_uses_new_summaries()
