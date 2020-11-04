2988edb81a96f272b80b864f75394bb4b73e2293
Pangratios Cosma <pagcosma@gmail.com>
2020-11-04 11:45:58 +0200

    ROAD-325 csId fix

--- Tests/Database/Repositories/CachedPatientServiceRepositoryTest.php
+++ Tests/Database/Repositories/CachedPatientServiceRepositoryTest.php
@@ -34,7 +34,7 @@ class CachedPatientServiceRepositoryTest extends PatientServiceRepositoryTest
 
     public function test_it_fetches_patient_from_cache_and_does_not_perform_multiple_queries_during_processing()
     {
-        $patient  = $this->setupPatientWithSummaries();
+        $patient = $this->setupPatientWithSummaries();
 
         $dto = (new PatientMonthlyBillingDTO())
             ->subscribe(
@@ -66,25 +66,25 @@ class CachedPatientServiceRepositoryTest extends PatientServiceRepositoryTest
 
     public function test_it_updates_cached_records_on_attach()
     {
-        $patient  = $this->setupPatientWithSummaries();
-    
+        $patient = $this->setupPatientWithSummaries();
+
         $serviceToAttach = ChargeableService::whereNotIn(
             'id',
             $this->repo->getChargeablePatientSummaries($patient->id, $thisMonth = Carbon::now()->startOfMonth())
                 ->pluck('id')
         )->first();
-        
+
         DB::enableQueryLog();
-        
+
         $this->repo->store($patient->id, $serviceToAttach->code, $thisMonth);
-        
+
         self::assertTrue(
             $this->repo->getChargeablePatientSummaries($patient->id, $thisMonth)
                 ->where('chargeable_service_id', $serviceToAttach->id)
                 ->isNotEmpty()
         );
-    
-        /**
+
+        /*
          * We expect 5 calls to the DB at this point
          * 1. Get Chargeable Service Id using Code (normally it will be cached)
          * 2. ServiceSummaryModel::updateOrCreate initial select query to see if model exists
@@ -92,26 +92,26 @@ class CachedPatientServiceRepositoryTest extends PatientServiceRepositoryTest
          * 4. Revisions insertion after insert query
          * 5. ServiceSummarySQLView select query to update cached data
          */
-        self::assertTrue(count(DB::getQueryLog()) === 5);
-        
+        self::assertTrue(5 === count(DB::getQueryLog()));
+
         DB::disableQueryLog();
     }
 
     public function test_it_updates_cached_records_on_fulfill()
     {
         $patient = $this->setupPatientWithSummaries();
-        
+
         $patientSummaries = $this->repo->getChargeablePatientSummaries($patient->id, $thisMonth = Carbon::now()->startOfMonth());
-        
+
         DB::enableQueryLog();
-        
+
         $this->repo->fulfill(
             $patient->id,
             $patientSummaries->first()->chargeable_service_code,
             $thisMonth
         );
-    
-        /**
+
+        /*
          * We expect 5 calls to the DB at this point
          * Get Chargeable Service Id using Code has already been called on processing and it's cached
          * 1. ServiceSummaryModel::updateOrCreate initial select query to see if model exists
@@ -119,16 +119,16 @@ class CachedPatientServiceRepositoryTest extends PatientServiceRepositoryTest
          * 3. Revisions insertion after insert query
          *
          */
-        self::assertTrue(count(DB::getQueryLog()) === 3);
-    
+        self::assertTrue(3 === count(DB::getQueryLog()));
+
         DB::disableQueryLog();
     }
 
     public function test_it_updates_cached_records_on_set_consent()
     {
     }
-    
-    private function setupPatientWithSummaries() : User
+
+    private function setupPatientWithSummaries(): User
     {
         return $this->setupPatient(
             $this->setupPractice(true, true, true, true),
