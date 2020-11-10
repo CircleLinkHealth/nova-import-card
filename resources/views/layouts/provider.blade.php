3e84a7d7092b09569d63d2e55edc1cc7e352661f
Pangratios Cosma <pagcosma@gmail.com>
2020-11-10 13:32:27 +0200

    we need wpstyle.css for nav bar

--- resources/views/layouts/provider.blade.php
+++ resources/views/layouts/provider.blade.php
@@ -15,13 +15,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css"
           integrity="sha256-/oZ7h/Jkj6AfibN/zTWrCoba0L+QhP9Tf/ZSgyZJCnY=" crossorigin="anonymous"/>
 
-    @if (Route::is('patientCallManagement.v2.index'))
-        <link href="{{ mix('/css/bootstrap.min.css') }}" rel="stylesheet">
-    @else
-        <link href="{{ mix('/css/wpstyle.css') }}" rel="stylesheet">
-    @endif
+    <link href="{{ mix('/css/wpstyle.css') }}" rel="stylesheet">
 
-    @if (! Route::is('patientCallManagement.v2.index') && \Illuminate\Support\Str::contains(optional(Route::getCurrentRoute())->getName(), 'admin'))
+    @if (Route::is('patientCallManagement.v2.index') || \Illuminate\Support\Str::contains(optional(Route::getCurrentRoute())->getName(), 'admin'))
         <link href="{{ mix('/css/bootstrap.min.css') }}" rel="stylesheet">
     @endif
 
