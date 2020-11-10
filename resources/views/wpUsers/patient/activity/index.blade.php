1127353345c6b9d3396c2bcaab3fd09c3d7a1fe1
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-11-10 14:24:35 -0600

    Show download butons

--- resources/views/wpUsers/patient/activity/index.blade.php
+++ resources/views/wpUsers/patient/activity/index.blade.php
@@ -293,7 +293,7 @@
 
                             </script>
                         @endpush
-                        @if( (auth()->user()->hasRole(array_merge(['administrator'], \App\Constants::PRACTICE_STAFF_ROLE_NAMES))) && (! auth()->user()->hasPermission('downloads.disable')) )
+                        @if( (auth()->user()->hasRole(array_merge(['administrator', 'software-only'], \App\Constants::PRACTICE_STAFF_ROLE_NAMES))) && (! auth()->user()->hasPermission('downloads.disable')) )
                             <input type="button" value="Export as PDF" class="btn btn-primary" style='margin:15px;'
                                    onclick="webix.toPDF($$(obs_alerts_dtable), {
                                            header:'CarePlanManager.com - Patient Activity Report <?= date('M d,Y'); ?>',
