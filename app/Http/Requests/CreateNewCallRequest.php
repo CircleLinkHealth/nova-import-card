9a899d6ef882a553c90d4b9b6ceb9fd42043efa4
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-11-09 18:25:37 -0600

    Fixowskii

--- app/Http/Requests/CreateNewCallRequest.php
+++ app/Http/Requests/CreateNewCallRequest.php
@@ -25,6 +25,9 @@ class CreateNewCallRequest extends FormRequest
         if (auth()->user()->isAdmin()) {
             return true;
         }
+        if (auth()->user()->isSoftwareOnly()) {
+            return true;
+        }
         if (auth()->user()->isCallbacksAdmin()) {
             return true;
         }
