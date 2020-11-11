a521c2ed88f29ecc84e4f703f04c322cd3a5fda3
kakoushias <kakoushias@gmail.com>
2020-11-11 13:43:33 +0100

    ROAD-346 fix bhi offline activity bug

--- Database/Migrations/2020_09_13_134756_unresolved_postmark_callbacks.php
+++ Database/Migrations/2020_09_13_134756_unresolved_postmark_callbacks.php
@@ -4,7 +4,6 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
