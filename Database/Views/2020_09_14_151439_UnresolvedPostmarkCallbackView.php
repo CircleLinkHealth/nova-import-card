46cf753d2622dd63910de5959afd0197c5f6bc6f
Pangratios Cosma <pagcosma@gmail.com>
2020-11-12 14:44:53 +0200

    do not run on AWV

--- Database/Views/2020_09_14_151439_UnresolvedPostmarkCallbackView.php
+++ Database/Views/2020_09_14_151439_UnresolvedPostmarkCallbackView.php
@@ -32,8 +32,8 @@ class UnresolvedPostmarkCallbackView extends BaseSqlView
         p.body as inbound_data,
         upc.created_at as date,
         upc.manually_resolved,
-       
-        
+
+
         CASE WHEN upc.unresolved_reason = '$notEnrolled' THEN 'Not enrolled'
         WHEN upc.unresolved_reason = '$queuedAndUnassigned' THEN 'Self enrollment queue - CA unassigned'
         WHEN upc.unresolved_reason = '$withdrawRequest' THEN 'Withdraw request'
@@ -41,45 +41,45 @@ class UnresolvedPostmarkCallbackView extends BaseSqlView
         WHEN upc.unresolved_reason = '$noNameSelfMatch' THEN 'Pt. SELF not matched'
         WHEN upc.unresolved_reason = '$notConsentedAndCAUnassigned' THEN 'Not consented - CA unassigned'
         END as unresolved_reason,
-        
- 
+
+
         CASE WHEN upc.user_id IS NULL
         THEN upc.suggestions
         ELSE NULL
         END
         as other_possible_matches,
- 
+
         CASE WHEN c.created_at >= upc.created_at
         AND c.sub_type = 'Call Back'
         THEN c.id
         END as call_id,
-        
+
         CASE WHEN c.id IS NOT NULL
         AND c.created_at >= upc.created_at
         AND c.sub_type = 'Call Back'
         THEN true
         ELSE false
         END as resolved,
-        
+
         CASE WHEN e.status = '$toCall' AND e.care_ambassador_user_id
         THEN true
         ELSE false
         END as assigned_to_ca
-        
+
         FROM
             unresolved_postmark_callbacks upc
-           
+
             left join calls c on upc.user_id = c.inbound_cpm_id
             left join postmark_inbound_mail p on upc.postmark_id = p.id
             left join enrollees e on upc.user_id = e.user_id
             left join users u on upc.user_id = u.id
-         
+
          WHERE 0 = (SELECT COUNT(c2.id)
              FROM calls c2
              WHERE c.inbound_cpm_id = c2.inbound_cpm_id
              AND c2.id < c.id)
-             
-             
+
+
          AND upc.deleted_at IS NULL
      ");
     }
@@ -90,5 +90,10 @@ class UnresolvedPostmarkCallbackView extends BaseSqlView
     public function getViewName(): string
     {
         return 'unresolved_postmark_callback_view';
+    }
+
+    public function shouldRun(): bool
+    {
+        return 'CarePlan Manager' === config('app.name');
     }
 }
