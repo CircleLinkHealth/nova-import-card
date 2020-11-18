18313ba98dc0e02503c4af3afadc6d83b84a2210
kountouris7 <kountouris7@gmail.com>
2020-11-18 17:31:11 +0200

    road_408_cameron_letter

---
+++ Database/Migrations/2020_11_18_102228_create_cameron_enrolment_letter.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCameronEnrolmentLetter extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+   //
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('db:seed', ['--class' => 'CircleLinkHealth\Eligibility\Database\Seeders\GenerateCameronLetter']);
+    }
+}
