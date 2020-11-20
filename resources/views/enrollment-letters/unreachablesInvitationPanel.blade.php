882bcd5e2e015133f317c56406ac4eb0cc75d267
kountouris7 <kountouris7@gmail.com>
2020-11-20 20:13:57 +0200

    road_408_ui change dash

--- resources/views/enrollment-letters/unreachablesInvitationPanel.blade.php
+++ resources/views/enrollment-letters/unreachablesInvitationPanel.blade.php
@@ -52,60 +52,8 @@
 
   </div>
   </div>
-{{--        <div class="row">--}}
-{{--            <div style="display: inline-flex;">--}}
-{{--          <div class="request-info-href">--}}
-{{--              <h5>Test Case 1.</h5>--}}
-{{--              <p>Send 1st. Reminder to non responding patients</p>--}}
-{{--              <ul class="browser-default">--}}
-{{--                  <li>--}}
-{{--                      For this to work,<br> you should just invite test patients and take no action after.--}}
-{{--                  </li>--}}
-{{--              </ul>--}}
-{{--            <div>--}}
-{{--                <a href="{{route('send.reminder.enrollee.qa')}}" target="_blank">--}}
-{{--                    <button type="button" class="btn btn-warning">--}}
-{{--                        Fast Forward 2 days and send reminders to Enrollees--}}
-{{--                    </button>--}}
-{{--                </a>--}}
-{{--            </div>--}}
-{{--<br>--}}
-{{--             <div>--}}
-{{--                 <a href="{{route('send.reminder.patient.qa')}}" target="_blank">--}}
-{{--                     <button type="button" class="btn btn-warning">--}}
-{{--                         Fast Forward 2 days and send reminders to Patients--}}
-{{--                     </button>--}}
-{{--                 </a>--}}
-{{--             </div>--}}
-{{--          </div>--}}
-
-{{--          <br>--}}
-
-{{--          <div class="request-info-href">--}}
-{{--              <h5>Test Case 2</h5>--}}
-{{--              <p>Take Final Action on Non Responding Patients</p>--}}
-{{--              <ul class="browser-default">--}}
-{{--                  <li>--}}
-{{--                      For this to work, you should just invite test patients and take no action after.--}}
-{{--                  </li>--}}
-{{--                  <li>--}}
-{{--                      Should go through "Test Case 1." first--}}
-{{--                  </li>--}}
-{{--              </ul>--}}
-{{--              <a href="{{route('final.action.qa')}}" target="_blank">--}}
-{{--                  <button type="button" class="btn btn-warning">--}}
-{{--                      Fast forward 4 days. Final Step<br>--}}
-{{--                  </button>--}}
-{{--              </a>--}}
-{{--              <p> After Final Step is done, you will be redirected to "Care Ambassador Director Panel"<br>--}}
-{{--                  You can check if patient is listed there marked as "Send Regular Mail" and "Auto enrollment triggered".</p>--}}
-{{--          </div>--}}
-{{--      </div>--}}
-
-{{--            <br>--}}
-
             <div class="request-info-href" style="padding-top: 20px;">
-                <h5>Enter patient 'ID' that got enrolled</h5>
+                <h5>Enter <strong>USER</strong> 'ID' that got enrolled</h5>
                 <form action="{{route('evaluate.survey.completed')}}" target="_blank">
                     <label for="enrolleeId">Enter Id:</label><br>
                     <input type="text" id="enrolleeId" name="enrolleeId" placeholder="ex.1616"><br>
@@ -113,46 +61,8 @@
                 </form>
             </div>
 
-{{--            <div class="request-info-href">--}}
-{{--                <a href="{{route('reset.test.qa')}}">--}}
-{{--                    <button type="button" class="btn btn-warning">Reset Test--}}
-{{--                    </button>--}}
-{{--                </a>--}}
-{{--            </div>--}}
         </div>
 
-{{--    <div>--}}
-{{--        <h5>Invited Patients credentials</h5>--}}
-{{--        @foreach($invitationData as $data)--}}
-{{--            @if($data['isEnrolleeClass'])--}}
-{{--                <ol>--}}
-{{--                    <strong> Type: Enrollee.</strong>--}}
-{{--                    <br>--}}
-{{--                    Invited Name: {{$data['name']}}--}}
-{{--                    <br>--}}
-{{--                    DOB: {{$data['dob']}}--}}
-{{--                    <br>--}}
-{{--                    <a href="{{$data['invitationUrl']}}" target="_blank">click here</a>--}}
-{{--                    or the link in Sms / Email. <br>--}}
-{{--                    Open in incognito window or log out from this browser.--}}
-{{--                </ol>--}}
-
-{{--            @elseif($data['isEnrolleeClass'] === false)--}}
-{{--                    <ol>--}}
-{{--                        <strong>Type: Unreachable Patient.</strong>--}}
-{{--                        <br>--}}
-{{--                        Invited Name: {{$data['name']}}--}}
-{{--                        <br>--}}
-{{--                        DOB: {{$data['dob']}}--}}
-{{--                        <br>--}}
-{{--                        You can <a href="{{$data['invitationUrl']}}" target="_blank">click here</a>--}}
-{{--                        or the link in Sms / Email. <br>--}}
-{{--                        Open in incognito window or log out from this browser.--}}
-{{--                    </ol>--}}
-{{--            @endif--}}
-
-{{--        @endforeach--}}
-{{--    </div>--}}
 
 </div>
 @endsection
