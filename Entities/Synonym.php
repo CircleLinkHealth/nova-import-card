b73588e96a7b229f0b8f034513286e5f47c0b0f5
kakoushias <kakoushias@gmail.com>
2020-11-21 16:16:09 +0100

    PAM: change views, drop un-needed

--- Entities/Synonym.php
+++ Entities/Synonym.php
@@ -8,6 +8,22 @@ namespace CircleLinkHealth\Synonyms\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * CircleLinkHealth\Synonyms\Entities\Synonym.
+ *
+ * @property int                             $id
+ * @property string                          $synonymable_type
+ * @property int                             $synonymable_id
+ * @property string                          $column
+ * @property string                          $synonym
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Eloquent|Model                 $synonymable
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|Synonym newModelQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|Synonym newQuery()
+ * @method   static                          \Illuminate\Database\Eloquent\Builder|Synonym query()
+ * @mixin \Eloquent
+ */
 class Synonym extends Model
 {
     protected $fillable = [
