a0a383d1006115dabe9701468d93248ce96805e2
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-11-26 18:32:43 -0600

    Moves routes

--- Routes/web.php
+++ Routes/web.php
@@ -134,3 +134,40 @@ Route::prefix('eligibility')->group(function () {
         });
     });
 });
+
+Route::group(function () {
+    Route::group([
+        'prefix'     => 'ehr-report-writer',
+        'middleware' => ['permission:ehr-report-writer-access'],
+    ], function () {
+        Route::get('index', [
+            'uses' => 'EhrReportWriterController@index',
+            'as'   => 'report-writer.dashboard',
+        ]);
+
+        Route::get('download-template/{name}', [
+            'uses' => 'EhrReportWriterController@downloadCsvTemplate',
+            'as'   => 'report-writer.download-template',
+        ]);
+
+        Route::post('validate', [
+            'uses' => 'EhrReportWriterController@validateJson',
+            'as'   => 'report-writer.validate',
+        ]);
+
+        Route::post('submit', [
+            'uses' => 'EhrReportWriterController@submitFile',
+            'as'   => 'report-writer.submit',
+        ]);
+
+        Route::post('notify', [
+            'uses' => 'EhrReportWriterController@notifyReportWriter',
+            'as'   => 'report-writer.notify',
+        ]);
+
+        Route::get('google-drive', [
+            'uses' => 'EhrReportWriterController@redirectToGoogleDriveFolder',
+            'as'   => 'report-writer.google-drive',
+        ]);
+    });
+})->middleware('auth');
