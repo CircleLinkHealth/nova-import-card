2b9197a026627c115d2066f5a88db127618ca687
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-11-27 02:43:07 -0600

    Rate limits jobs

--- Jobs/ProcessSinglePatientMonthlyServices.php
+++ Jobs/ProcessSinglePatientMonthlyServices.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use CircleLinkHealth\CcmBilling\Domain\Patient\ProcessPatientSummaries;
 use CircleLinkHealth\CcmBilling\Facades\BillingCache;
 use MichaelLedin\LaravelJob\Job;
+use Spatie\RateLimitedMiddleware\RateLimited;
 
 class ProcessSinglePatientMonthlyServices extends Job implements HasUniqueIdentifierForDebounce
 {
@@ -59,5 +60,20 @@ class ProcessSinglePatientMonthlyServices extends Job implements HasUniqueIdenti
         BillingCache::clearPatients([$this->getPatientId()]);
 
         (app(ProcessPatientSummaries::class))->execute($this->getPatientId(), $this->getMonth());
+    }
+    
+    public function middleware()
+    {
+        $rateLimitedMiddleware = (new RateLimited())
+            ->allow(30)
+            ->everySeconds(60)
+            ->releaseAfterSeconds(20);
+        
+        return [$rateLimitedMiddleware];
+    }
+    
+    public function retryUntil(): \DateTime
+    {
+        return now()->addDay();
     }
 }
