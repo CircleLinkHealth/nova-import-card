080d012a64f8ea4840399953d2732ee67e243d10
kakoushias <kakoushias@gmail.com>
2020-11-28 01:46:19 +0100

    billing-abp-toggle-off: add command to reset PMS CS

--- Providers/CcmBillingServiceProvider.php
+++ Providers/CcmBillingServiceProvider.php
@@ -8,6 +8,7 @@ namespace CircleLinkHealth\CcmBilling\Providers;
 
 use CircleLinkHealth\CcmBilling\Caches\BillingCache;
 use CircleLinkHealth\CcmBilling\Caches\BillingDataCache;
+use CircleLinkHealth\CcmBilling\Console\ResetPMSChargeableServicesForMonth;
 use CircleLinkHealth\CcmBilling\Console\SeedChargeableServices;
 use CircleLinkHealth\CcmBilling\Contracts\LocationProblemServiceRepository as LocationProblemServiceRepositoryInterface;
 use CircleLinkHealth\CcmBilling\Contracts\LocationProcessorRepository;
@@ -48,7 +49,9 @@ class CcmBillingServiceProvider extends ServiceProvider implements DeferrablePro
     {
         $this->commands([
             SeedChargeableServices::class,
+            ResetPMSChargeableServicesForMonth::class
         ]);
+        
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
 
         $this->app->singleton(PatientMonthlyBillingProcessor::class, MonthlyProcessor::class);
