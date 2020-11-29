8ba0232fb2e10316887c4da44c42851992dd8788
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-11-29 15:34:38 -0600

    Changes command

--- scripts/sh/create_module_branches.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# this command is not complete
-
-find ../../CircleLinkHealth/ -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && git checkout master && git pull && git checkout -b test-lint" \;
