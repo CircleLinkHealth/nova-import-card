af56c8ec255b14d4820cdf8ea35cc836faeb0942
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-11-29 18:30:24 -0600

    Shufflin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ use App\Http\Middleware\CheckPatientUserData;
 use App\Http\Middleware\EncryptCookies;
 use App\Http\Middleware\EnrollmentCenter;
 use CircleLinkHealth\Core\Http\Middleware\LogoutIfAccessDisabled;
-use App\Http\Middleware\PatientProgramSecurity;
+use CircleLinkHealth\Customer\Http\Middleware\PatientProgramSecurity;
 use CircleLinkHealth\Core\Http\Middleware\SentryContext;
 use App\Http\Middleware\VerifyCsrfToken;
 use CircleLinkHealth\TwoFA\Http\Middleware\AuthyMiddleware;
