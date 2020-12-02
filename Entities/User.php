e589a1ac4ef74e3cfd935ff36883e8280ae9f2c6
kountouris7 <kountouris7@gmail.com>
2020-12-02 14:40:08 +0200

    Road 404 Hotfix show diabetes on careplan_remove generic diabetes

--- Entities/User.php
+++ Entities/User.php
@@ -665,13 +665,9 @@ class User extends BaseModel implements AuthenticatableContract, CanResetPasswor
 
     public function billableProblems()
     {
-        $genericDiabetes = \CircleLinkHealth\SharedModels\Entities\CpmProblem::where('name', 'Diabetes')->firstOrFail(); // Is this needed now???
         //todo: use billing repo
         return $this->ccdProblems()
             ->whereNotNull('cpm_problem_id')
-            ->when($genericDiabetes, function ($p) use ($genericDiabetes) {
-                $p->where('cpm_problem_id', '!=', $genericDiabetes->id); // Is this needed now???
-            })
             ->with(['cpmProblem', 'icd10Codes'])
             ->where('is_monitored', true);
     }
