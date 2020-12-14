5ab66ff5192f5acd859f1c5a1d8b25d8519954f0
kakoushias <kakoushias@gmail.com>
2020-12-15 00:43:14 +0100

    Fix PAM patient links

--- Resources/views/partials/adminUI.blade.php
+++ Resources/views/partials/adminUI.blade.php
@@ -295,6 +295,7 @@
 <script>
     $(document).ready(function () {
         $('.select2').select2();
+        window['provider_app_url'] = @json(env('CPM_PROVIDER_APP_URL'))
     });
 </script>
 @stack('scripts')
