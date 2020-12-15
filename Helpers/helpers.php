9a0ac4e2ebb13970591fed11dc0573eba3ce36de
kakoushias <kakoushias@gmail.com>
2020-12-15 15:44:08 +0100

    vaporizing: move helper to core

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -375,3 +375,31 @@ if ( ! function_exists('capitalizeWords')) {
         return ucwords(strtolower($string), $delimiters);
     }
 }
+
+
+if ( ! function_exists('is_json')) {
+    /**
+     * Determine whether the given string is json.
+     *
+     * @param $string
+     *
+     * @return bool|null
+     *
+     * true: the string is valid json
+     * null: the string is an empty string, or not a string at all
+     * false: the string is invalid json
+     */
+    function is_json($string)
+    {
+        if ('' === $string || ! is_string($string)) {
+            return null;
+        }
+        
+        \json_decode($string);
+        if (\json_last_error()) {
+            return false;
+        }
+        
+        return true;
+    }
+}
