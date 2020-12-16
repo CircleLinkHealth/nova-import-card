10ef7e3378834bac070557521ee2db917cd8b7c9
kountouris7 <kountouris7@gmail.com>
2020-12-16 13:25:28 +0200

    Moving Self Enrollment files / renaming

--- Http/Controllers/Enrollment/AutoEnrollmentTestDashboard.php
+++ Http/Controllers/Enrollment/AutoEnrollmentTestDashboard.php
@@ -6,10 +6,9 @@
 
 namespace CircleLinkHealth\CpmAdmin\Http\Controllers\Enrollment;
 
-use Carbon\Carbon;
 use CircleLinkHealth\Customer\Entities\Practice;
 use CircleLinkHealth\Customer\Entities\User;
-use CircleLinkHealth\Eligibility\SelfEnrollment\Console\Commands\SendSelfEnrollmentReminders;
+use CircleLinkHealth\Eligibility\SelfEnrollment\Constants;
 use CircleLinkHealth\Eligibility\SelfEnrollment\Domain\InviteUnreachablePatients;
 use CircleLinkHealth\Eligibility\SelfEnrollment\Domain\UnreachablesFinalAction;
 use CircleLinkHealth\Eligibility\SelfEnrollment\Helpers;
@@ -42,7 +41,7 @@ class AutoEnrollmentTestDashboard extends Controller
      */
     public function finalActionTest()
     {
-        UnreachablesFinalAction::dispatch(now()->subDays(CpmConstants::DAYS_DIFF_FROM_FIRST_INVITE_TO_FINAL_ACTION));
+        UnreachablesFinalAction::dispatch(now()->subDays(Constants::DAYS_DIFF_FROM_FIRST_INVITE_TO_FINAL_ACTION));
 
         return redirect(route('ca-director.index'))->with('message', 'Reminders Sent Successfully');
     }
@@ -61,99 +60,11 @@ class AutoEnrollmentTestDashboard extends Controller
     }
 
     /**
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function resetEnrollmentTest()
-    {
-        $practice = $this->getDemoPractice();
-        // TEST ONLY
-        $users = User::withTrashed()
-            ->with('notifications', 'patientInfo', 'enrollee')
-            ->where('program_id', '=', $practice->id)
-            ->hasSelfEnrollmentInvite()
-            ->where('created_at', '>', Carbon::parse(now())->startOfMonth())
-            ->whereHas('patientInfo')
-            ->get();
-
-        $survey = Helpers::getEnrolleeSurvey();
-
-        foreach ($users as $user) {
-            $surveyInstance = DB::table('survey_instances')
-                ->where('survey_id', '=', $survey->id)
-                ->first();
-
-            if ($user->isSurveyOnly()) {
-                $this->deleteTestAwvUser($user, $surveyInstance);
-                $user->notifications()->delete();
-                $user->enrollee->enrollmentInvitationLinks()->delete();
-                $user->enrollee->enrollableInfoRequest()->delete();
-
-                DB::table('invitation_links')
-                    ->where('patient_info_id', $user->patientInfo()->withTrashed()->first()->id)
-                    ->delete();
-
-                DB::table('answers')
-                    ->where('user_id', $user->id)
-                    ->where('survey_instance_id', $surveyInstance->id)
-                    ->delete();
-            } else {
-                $this->deleteTestAwvUser($user, $surveyInstance);
-                $user->notifications()->delete();
-                $user->enrollmentInvitationLinks()->delete();
-                $user->enrollableInfoRequest()->delete();
-                $user->patientInfo()->update(
-                    [
-                        'ccm_status' => \CircleLinkHealth\Customer\Entities\Patient::UNREACHABLE,
-                    ]
-                );
-
-                $patientInfo = $user->patientInfo->withTrashed()->first();
-
-                DB::table('invitation_links')
-                    ->where('patient_info_id', $patientInfo->id)
-                    ->delete();
-
-                DB::table('answers')
-                    ->where('user_id', $user->id)
-                    ->where('survey_instance_id', $surveyInstance->id)
-                    ->delete();
-            }
-        }
-
-        return redirect()->back();
-    }
-
-    /**
-     * @return string
-     */
-    public function sendEnrolleesReminderTestMethod()
-    {
-        try {
-            SendSelfEnrollmentReminders::dispatchEnrolleeReminders($this->getDemoPractice()->id);
-        } catch (\Exception $e) {
-            return 'Something went wrong';
-        }
-
-        return 'Please check your email';
-    }
-
-    /**
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function sendInvitesPanelTest()
     {
-        return view('enrollment-letters.unreachablesInvitationPanel');
-    }
-
-    public function sendPatientsReminderTestMethod()
-    {
-//        try {
-//            SelfEnrollmentEnrolleesReminder::dispatch();
-//        } catch (\Exception $e) {
-//            return 'Something went wrong';
-//        }
-//
-//        return 'Please check your email';
+        return view('selfEnrollment.unreachablesInvitationPanel');
     }
 
     /**
@@ -168,24 +79,5 @@ class AutoEnrollmentTestDashboard extends Controller
         }
 
         return 'You can go back and proceed';
-    }
-
-    private function deleteTestAwvUser(User $user, $surveyInstance)
-    {
-        if ( ! Helpers::awvUserSurveyQuery($user, $surveyInstance)->exists()) {
-            return;
-        }
-
-        Helpers::awvUserSurveyQuery($user, $surveyInstance)->delete();
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model
-     */
-    private function getDemoPractice()
-    {
-        return \Cache::remember('demo_practice_object', 2, function () {
-            return Practice::where('name', '=', 'demo')->firstOrFail();
-        });
     }
 }
