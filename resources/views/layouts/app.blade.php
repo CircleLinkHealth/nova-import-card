3bf02316b5b6a6ae849981b96b8104b5f5cb0138
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-16 09:46:30 -0600

    fixes paths

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,10 @@
     <title>{{ config('app.name', 'AWV') }}</title>
 
     <!-- FAV ICON -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('../../../public/img/favicon/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('../../../public/img/favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('../../../public/img/favicon/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{asset('../../../public/img/favicon/site.webmanifest')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/img/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/img/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/img/favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('/img/favicon/site.webmanifest')}}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
