4c409c77ee39c83ee1a4d03f2655b13b6dd121b4
kountouris7 <kountouris7@gmail.com>
2020-12-17 12:30:45 +0200

    Moving Self Enrollment files / renaming / Last for now

--- SelfEnrollment/Entities/EnrollmentInvitationsBatch.php
+++ SelfEnrollment/Entities/EnrollmentInvitationsBatch.php
@@ -11,19 +11,8 @@ use CircleLinkHealth\Customer\EnrollableInvitationLink\EnrollableInvitationLink;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * App\EnrollmentInvitationsBatch.
- *
- * @property int                                                                                                                     $id
- * @property \Illuminate\Support\Carbon|null                                                                                         $created_at
- * @property \Illuminate\Support\Carbon|null                                                                                         $updated_at
- * @property \CircleLinkHealth\Customer\EnrollableInvitationLink\EnrollableInvitationLink[]|\Illuminate\Database\Eloquent\Collection $invitationLinks
- * @property int|null                                                                                                                $invitation_links_count
- * @method   static                                                                                                                  \Illuminate\Database\Eloquent\Builder|\App\EnrollmentInvitationsBatch newModelQuery()
- * @method   static                                                                                                                  \Illuminate\Database\Eloquent\Builder|\App\EnrollmentInvitationsBatch newQuery()
- * @method   static                                                                                                                  \Illuminate\Database\Eloquent\Builder|\App\EnrollmentInvitationsBatch query()
- * @mixin \Eloquent
- * @property int|null $practice_id
- * @property string   $type
+ * Class EnrollmentInvitationsBatch
+ * @package CircleLinkHealth\Eligibility\SelfEnrollment\Entities
  */
 class EnrollmentInvitationsBatch extends Model
 {
