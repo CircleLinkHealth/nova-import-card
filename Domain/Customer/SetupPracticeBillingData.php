c62b88993e1e25d82e4b128848a943a8a5d80a1b
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-18 00:39:36 -0600

    leave one default queue

--- Domain/Customer/SetupPracticeBillingData.php
+++ Domain/Customer/SetupPracticeBillingData.php
@@ -44,6 +44,6 @@ class SetupPracticeBillingData
             new ProcessPracticePatientMonthlyServices($practiceId),
         ])
             ->dispatch($practiceId)
-            ->onQueue('high');
+            ;
     }
 }
