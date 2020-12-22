e384e913754e9537abad89a5e306bc621e9e69b0
Pangratios Cosma <pagcosma@gmail.com>
2020-12-22 18:20:42 +0200

    disable CheckVoiceCalls scheduled command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -142,9 +142,11 @@ class Kernel extends ConsoleKernel
         $schedule->job(OverwritePatientMrnsFromSupplementalData::class)
             ->everyThirtyMinutes();
 
+        /*
         $schedule->command(CheckVoiceCalls::class, [now()->subHour()])
             ->hourly()
             ->between('7:00', '23:00');
+        */
 
         $schedule->command('schedule-monitor:clean')
             ->daily();
