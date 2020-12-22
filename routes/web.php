b181e51d366021129b7cc078a3f5ec08689818fd
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-22 02:18:03 -0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index');
 
 Route::get('ccd-importer', [
     'uses' => 'RedirectToOtherApp@ccdImporter',
