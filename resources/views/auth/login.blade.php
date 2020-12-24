2dd0d1b8892aa1aeb818fce60c579a240dde2ba5
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-23 21:28:11 -0600

    Styles admin app login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,19 @@
 @section('content')
     @include('core::partials.errors.errors')
 
-    <div class="container">
+    <div class="container-fluid" style="margin-top: 10%;">
         <section class="main-form col-lg-12 col-sm-11">
-            <div class="row">
-                <div class="col-lg-4 col-lg-offset-4">
-                    <img class="img-responsive" src="{{ asset('img/logos/LogoHorizontal_Color.svg') }}"
-                         alt="CarePlan Manager">
+            <div class="col-lg-4 col-lg-offset-4">
+                <div class="row">
+                    <div class="col-lg-6 col-lg-offset-3">
+                        <img class="img-responsive" src="{{ asset('img/logos/LogoHorizontal_Color.svg') }}"
+                             alt="CarePlan Manager">
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6 col-lg-offset-3">
+                <div class="row">
                     <div class="main-form-block main-form-horizontal main-form-primary-horizontal col-md-12">
-                        <form class="form-prevent-multi-submit" role="form" method="POST" action="{{ url('/auth/login') }}"
+                        <form class="form-prevent-multi-submit" role="form" method="POST"
+                              action="{{ url('/auth/login') }}"
                               autocomplete="off">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
