81eef28dc1ed435159b9687714bde9e7ff621fd4
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-26 18:08:28 -0600

    fix

--- Http/Controllers/OPCacheGUIController.php
+++ Http/Controllers/OPCacheGUIController.php
@@ -6,18 +6,15 @@
 
 namespace CircleLinkHealth\Core\Http\Controllers;
 
-use CircleLinkHealth\CpmAdmin\Http\Controllers\View;
 use Illuminate\Routing\Controller;
 
 class OPCacheGUIController extends Controller
 {
     /**
      * Show the status of OPCache.
-     *
-     * @return View the view from amnuts/opcache-gui
      */
     public function index()
     {
-        return view()->file(storage_path('../vendor/amnuts/opcache-gui/index.php'));
+        return view()->file(base_path('vendor/amnuts/opcache-gui/index.php'));
     }
 }
