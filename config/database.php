f7d1e510f0c5df834ea40eb997a6852141064227
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-27 22:30:46 -0600

    dev-laravel-8.0

--- config/database.php
+++ config/database.php
@@ -17,9 +17,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
+    
     'default' => env('DB_CONNECTION', 'mysql'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -35,7 +35,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
+    
     'connections' => [
         'sqlite' => [
             'driver'                  => 'sqlite',
@@ -44,24 +44,39 @@ return [
             'prefix'                  => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        
         'mysql' => CpmAppMySqlConfig::toArray(),
-
-        'pgsql' => [
-            'driver'         => 'pgsql',
-            'url'            => env('DATABASE_URL'),
-            'host'           => env('DB_HOST', '127.0.0.1'),
-            'port'           => env('DB_PORT', '5432'),
-            'database'       => env('DB_DATABASE', 'forge'),
-            'username'       => env('DB_USERNAME', 'forge'),
-            'password'       => env('DB_PASSWORD', ''),
-            'charset'        => 'utf8',
-            'prefix'         => '',
-            'prefix_indexes' => true,
-            'schema'         => 'public',
-            'sslmode'        => 'prefer',
+        
+        'test_suite' => [
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', '127.0.0.1'),
+            'port'        => env('DB_PORT', '3306'),
+            'database'    => env('TEST_SUITE_DB_DATABASE', 'cpm_test_suite'),
+            'username'    => env('DB_USERNAME', 'forge'),
+            'password'    => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => '',
+            'strict'      => false,
+            'engine'      => null,
         ],
-
+        
+        'pgsql' => [
+                'driver'         => 'pgsql',
+                'url'            => env('DATABASE_URL'),
+                'host'           => env('DB_HOST', '127.0.0.1'),
+                'port'           => env('DB_PORT', '5432'),
+                'database'       => env('DB_DATABASE', 'forge'),
+                'username'       => env('DB_USERNAME', 'forge'),
+                'password'       => env('DB_PASSWORD', ''),
+                'charset'        => 'utf8',
+                'prefix'         => '',
+                'prefix_indexes' => true,
+                'schema'         => 'public',
+                'sslmode'        => 'prefer',
+            ],
+        
         'sqlsrv' => [
             'driver'         => 'sqlsrv',
             'url'            => env('DATABASE_URL'),
@@ -75,7 +90,7 @@ return [
             'prefix_indexes' => true,
         ],
     ],
-
+    
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
@@ -86,9 +101,9 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
-
+    
     'migrations' => 'migrations',
-
+    
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -99,39 +114,42 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
-
     'redis' => [
-        'client' => env('ENCR_REDIS_CLIENT', env('REDIS_CLIENT', 'phpredis')),
-
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+        
         'options' => [
-            'cluster' => env('ENCR_REDIS_CLUSTER', env('REDIS_CLUSTER', 'redis')),
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix'  => 'cpm_database', //same as AWV
         ],
-
+        
         'default' => [
-            'url'      => env('ENCR_REDIS_URL', env('REDIS_URL')),
-            'host'     => env('ENCR_REDIS_HOST', env('REDIS_HOST', '127.0.0.1')),
-            'password' => env('ENCR_REDIS_PASSWORD', env('REDIS_PASSWORD', null)),
-            'port'     => env('ENCR_REDIS_PORT', env('REDIS_PORT', '6379')),
-            'database' => env('ENCR_REDIS_DB', env('REDIS_DB', '0')),
+            'url'      => env('REDIS_URL'),
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+            'context' => [
+//                 'auth' => ['username', 'secret'],
+                // 'stream' => ['verify_peer' => false],
+            ],
         ],
-
+        
         'cache' => [
-            'url'      => env('ENCR_REDIS_URL', env('REDIS_URL')),
-            'host'     => env('ENCR_REDIS_HOST', env('REDIS_HOST', '127.0.0.1')),
-            'password' => env('ENCR_REDIS_PASSWORD', env('REDIS_PASSWORD', null)),
-            'port'     => env('ENCR_REDIS_PORT', env('REDIS_PORT', '6379')),
-            'database' => env('ENCR_REDIS_CACHE_DB', env('REDIS_CACHE_DB', '1')),
+            'url'      => env('REDIS_URL'),
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
+        
         // need a specific connection for the pub/sub channel
         // otherwise, we get errors (tried both phpredis and predis)
         'pub_sub' => [
-            'url'                => env('ENCR_REDIS_URL', env('REDIS_URL')),
-            'host'               => env('ENCR_REDIS_HOST', env('REDIS_HOST', '127.0.0.1')),
-            'password'           => env('ENCR_REDIS_PASSWORD', env('REDIS_PASSWORD', null)),
-            'port'               => env('ENCR_REDIS_PORT', env('REDIS_PORT', 6379)),
-            'database'           => env('ENCR_REDIS_DB', env('REDIS_DB', 0)),
+            'url'                => env('REDIS_URL'),
+            'host'               => env('REDIS_HOST', '127.0.0.1'),
+            'password'           => env('REDIS_PASSWORD', null),
+            'port'               => env('REDIS_PORT', 6379),
+            'database'           => env('REDIS_DB', 0),
             'read_write_timeout' => -1,
         ],
     ],
