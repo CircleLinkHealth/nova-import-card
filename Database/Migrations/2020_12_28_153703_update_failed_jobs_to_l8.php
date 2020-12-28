62d42dfef9aa4d9561ee99be9fd081441a9dd371
Pangratios Cosma <pagcosma@gmail.com>
2020-12-28 18:45:04 +0200

    linting + remove validation that does not work on L8

--- Database/Migrations/2020_12_28_153703_update_failed_jobs_to_l8.php
+++ Database/Migrations/2020_12_28_153703_update_failed_jobs_to_l8.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
@@ -7,6 +11,15 @@ use Illuminate\Support\Facades\Schema;
 
 class UpdateFailedJobsToL8 extends Migration
 {
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
     /**
      * Run the migrations.
      *
@@ -17,21 +30,11 @@ class UpdateFailedJobsToL8 extends Migration
         Schema::table('failed_jobs', function (Blueprint $table) {
             $table->string('uuid')->after('id')->nullable()->unique();
         });
-    
+
         DB::table('failed_jobs')->whereNull('uuid')->cursor()->each(function ($job) {
             DB::table('failed_jobs')
-              ->where('id', $job->id)
-              ->update(['uuid' => (string) Illuminate\Support\Str::uuid()]);
+                ->where('id', $job->id)
+                ->update(['uuid' => (string) Illuminate\Support\Str::uuid()]);
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-    
     }
 }
