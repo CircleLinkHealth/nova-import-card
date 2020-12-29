d238b56a4db32ce9c48c69de2cbf562e932d3eba
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-28 18:08:30 -0600

    Resolves ROAD-503

--- Resources/views/partials/non-adminUI.blade.php
+++ Resources/views/partials/non-adminUI.blade.php
@@ -141,7 +141,7 @@
 
 <script src="{{asset('compiled/js/app-clh-admin-ui.js')}}"></script>
 <script src="{{asset('compiled/js/dependencies.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous"></script>
 
 <script>
     $(document).ready(function () {
