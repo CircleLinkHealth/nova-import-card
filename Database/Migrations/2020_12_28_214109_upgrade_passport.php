2147f4e33351e8776233a2d2a51274d184a7fe0f
Michalis Antoniou <michalisantoniou6@gmail.com>
2020-12-28 23:21:02 -0600

    L8 test suite fixes

--- Database/Migrations/2020_12_28_214109_upgrade_passport.php
+++ Database/Migrations/2020_12_28_214109_upgrade_passport.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,27 +11,29 @@ use Illuminate\Support\Facades\Schema;
 class UpgradePassport extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table(
-            'oauth_clients',
-            function (Blueprint $table) {
-                $table->string('provider')->after('secret')->nullable();
-            }
-        );
-    }
-    
-    /**
      * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        //
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('oauth_clients', 'provider')) {
+            return;
+        }
+        Schema::table(
+            'oauth_clients',
+            function (Blueprint $table) {
+                $table->string('provider')->after('secret')->nullable();
+            }
+        );
     }
 }
