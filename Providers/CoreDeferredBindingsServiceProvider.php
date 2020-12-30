656e8f7db4ba0b98697117bdba6366896119acdc
kakoushias <kakoushias@gmail.com>
2020-12-30 02:05:30 +0100

    vaporizing: move report formatter bind

--- Providers/CoreDeferredBindingsServiceProvider.php
+++ Providers/CoreDeferredBindingsServiceProvider.php
@@ -6,6 +6,7 @@
 
 namespace CircleLinkHealth\Core\Providers;
 
+use App\Formatters\WebixFormatter;
 use CircleLinkHealth\Core\Console\Commands\CreateAndSeedTestSuiteDB;
 use CircleLinkHealth\Core\Console\Commands\CreateMySqlDB;
 use CircleLinkHealth\Core\Console\Commands\CreateOpPreloadPreloadFilePlaceholder;
@@ -15,6 +16,7 @@ use CircleLinkHealth\Core\Console\Commands\HerokuOnRelease;
 use CircleLinkHealth\Core\Console\Commands\ReviewAppCreateDb;
 use CircleLinkHealth\Core\Console\Commands\ReviewAppPreDestroy;
 use CircleLinkHealth\Core\Console\Commands\ReviewAppSeedDb;
+use CircleLinkHealth\Core\Contracts\ReportFormatter;
 use CircleLinkHealth\Core\Entities\DatabaseNotification as CircleLinkDatabaseNotification;
 use CircleLinkHealth\Core\Notifications\Channels\DatabaseChannel as CircleLinkDatabaseChannel;
 use CircleLinkHealth\Core\Traits\HasDatabaseNotifications as CircleLinkHasDatabaseNotifications;
@@ -53,6 +55,7 @@ class CoreDeferredBindingsServiceProvider extends ServiceProvider implements Def
             CreateAndSeedTestSuiteDB::class,
             GenerateInboundCallbackDataFeedbackToTester::class,
             CreateOpPreloadPreloadFilePlaceholder::class,
+            ReportFormatter::class,
         ];
     }
 
@@ -82,5 +85,9 @@ class CoreDeferredBindingsServiceProvider extends ServiceProvider implements Def
         $this->app->bind(LaravelHasDatabaseNotifications::class, CircleLinkHasDatabaseNotifications::class);
         $this->app->bind(LaravelNotifiable::class, CircleLinkNotifiable::class);
         $this->app->bind(LaravelDatabaseNotification::class, CircleLinkDatabaseNotification::class);
+        $this->app->bind(
+            ReportFormatter::class,
+            WebixFormatter::class
+        );
     }
 }
