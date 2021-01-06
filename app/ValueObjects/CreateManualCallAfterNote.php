868484ce3f1329ad03fef96379fbf2027579f523
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-06 10:51:29 -0600

    - Adds fixes - Disables ProtectsPHI just for shits and giggles - Microoptimization on ManualCall

--- app/ValueObjects/CreateManualCallAfterNote.php
+++ app/ValueObjects/CreateManualCallAfterNote.php
@@ -13,6 +13,7 @@ use Illuminate\Contracts\Support\Arrayable;
 
 class CreateManualCallAfterNote implements Arrayable
 {
+    private User $patient;
     private int $patientId;
     private bool $reached;
 
@@ -44,7 +45,11 @@ class CreateManualCallAfterNote implements Arrayable
 
     public function getPatient(): User
     {
-        return User::find($this->patientId);
+        if ( ! $this->patient) {
+            $this->patient = User::without(['roles', 'perms'])->find($this->patientId);
+        }
+
+        return $this->patient;
     }
 
     /**
