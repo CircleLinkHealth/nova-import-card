af5b4bb9eb6a935396e541dd2743186bd95c6b44
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-12 14:13:44 -0600

    Merging Revisionable to master

---
+++ modules/Revisionable/Observers/RevisionObserver.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\Revisionable\Observers;
+
+use CircleLinkHealth\Revisionable\Entities\Revision;
+
+class RevisionObserver
+{
+    public function saving(Revision $revision)
+    {
+        $revision->is_phi = $this->isPhi($revision);
+    }
+
+    private function isPhi(Revision $revision)
+    {
+        if (class_exists($class = $revision->revisionable_type)) {
+            $revisedModel = app($class);
+
+            return in_array($revision->key, $revisedModel->phi);
+        }
+
+        return false;
+    }
+}
