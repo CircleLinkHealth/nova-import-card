2d91a81bd9183b17894064f712a7f73ffab843f9
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-12 14:02:25 -0600

    Merging caller-app to master

---
+++ apps/caller-app/app/Providers/TwilioClientServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace App\Providers;
+
+use App\Services\TwilioClientable;
+use App\Services\TwilioClientService;
+use Illuminate\Support\ServiceProvider;
+
+class TwilioClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->bind(TwilioClientable::class, function () {
+            return new TwilioClientService();
+        });
+    }
+}
