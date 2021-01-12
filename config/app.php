03f5db022f49941acf8da8dacdbf98652fc06b7f
kountouris7 <kountouris7@gmail.com>
2021-01-12 13:44:28 +0200

    Road 547 - Nova in BluePrint telika

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         DirectMailServiceProvider::class,
         FaxServiceProvider::class,
