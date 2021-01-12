4c35a7bb70a0bdb78d28b23ca71c774616288e1a
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-11 20:20:33 -0600

    Merging admin-app to master

---
+++ apps/admin/nova-components/GenerateSuccessStoriesReport/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+use Circlelinkhealth\GenerateSuccessStoriesReport\GenerateSuccessStoriesReportController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Card API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you may register API routes for your card. These routes
+| are loaded by the ServiceProvider of your card. You're free to add
+| as many additional routes to this file as your card may require.
+|
+*/
+
+Route::get('/generate-success-stories-report/{monthYear}', GenerateSuccessStoriesReportController::class.'@handle');
