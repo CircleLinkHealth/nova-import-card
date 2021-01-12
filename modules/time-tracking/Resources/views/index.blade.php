dbd2fd0aaa75cb531bc6bcd2f5e405c053c2e8f5
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-11 20:25:01 -0600

    Merging time-tracking-module to master

---
+++ modules/time-tracking/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('timetracking::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('timetracking.name') !!}
+    </p>
+@stop
