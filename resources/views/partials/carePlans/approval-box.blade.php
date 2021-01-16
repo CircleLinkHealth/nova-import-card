a93a96c01b7e76cd3842844065cc56a66f8248fb
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-15 21:48:52 -0600

    CU-jmu1vz[READY FOR QA]

--- resources/views/partials/carePlans/approval-box.blade.php
+++ resources/views/partials/carePlans/approval-box.blade.php
@@ -5,16 +5,15 @@
                 <i class="fas fa-exclamation" style="color: red"></i> This Care Plan is pending Dr. approval
             </div>
         </div>
-    @elseif($patient->carePlan->status === CircleLinkHealth\SharedModels\Entities\CarePlan::PROVIDER_APPROVED && $patient->carePlan->provider_approver_id && $patient->carePlan->provider_date)
+    @elseif($patient->carePlan->status === CircleLinkHealth\SharedModels\Entities\CarePlan::PROVIDER_APPROVED && $patient->carePlan->$patient->carePlan->providerApproverUser && $patient->carePlan->provider_date && $patient->getCcmStatus() !== 'patient_rejected')
         <div class="col-xs-12">
             <div class="pull-right print-row text-right">
                 {{ $patient->getCcmStatus() == 'patient_rejected' ? 'Rejected' : ($patient->patientIsUPG0506() ? 'Created' : 'Approved') }}
                 on {{$patient->carePlan->provider_date->format('m/d/Y')}}
                 at {{$patient->carePlan->provider_date->setTimezone($patient->timezone ?? 'America/New_York')->format('g:i A T')}}
-                by {{ $patient->getCcmStatus() == 'patient_rejected' ? \CircleLinkHealth\Customer\Entities\User::withTrashed()->find($patient->carePlan->user_id)->display_name : \CircleLinkHealth\Customer\Entities\User::withTrashed()->find($patient->carePlan->provider_approver_id)->display_name}}
+                by {{ $patient->carePlan->providerApproverUser->getDoctorFullNameWithSpecialty() }}
                 @if($patient->carePlan->wasApprovedViaNurse()) via {{$patient->carePlan->getNurseApproverName()}} @endif
             </div>
         </div>
     @endif
 @endif
-
