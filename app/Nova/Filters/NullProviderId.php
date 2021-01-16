3734b94b9e785f6d21a822a6fe22948736d0269d
kakoushias <kakoushias@gmail.com>
2021-01-16 14:09:53 +0100

    hotfix-ca-enrollee-practices: lint

--- app/Nova/Filters/NullProviderId.php
+++ app/Nova/Filters/NullProviderId.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 namespace App\Nova\Filters;
 
 use Illuminate\Http\Request;
@@ -18,21 +22,18 @@ class NullProviderId extends Filter
     /**
      * Apply the filter to the given query.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  mixed  $value
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @param  mixed                                 $value
      * @return \Illuminate\Database\Eloquent\Builder
      */
     public function apply(Request $request, $query, $value)
     {
-
         return $value['enabled'] ? $query->whereNull('provider_id') : $query;
     }
 
     /**
      * Get the filter's available options.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function options(Request $request)
