fab1afcf50a28abd52291435aea7cac24c31fcae
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-20 17:25:33 -0600

    Merge remote-tracking branch 'admin-app/development' into development
    # Conflicts:
    #	apps/admin-app/app/Console/Kernel.php
    #	apps/admin-app/composer.lock

--- apps/admin-app/routes/web.php
+++ apps/admin-app/routes/web.php
@@ -21,24 +21,16 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('home', 'HomeController@index');
 
 Route::get('ccd-importer', [
-    'uses' => 'RedirectToOtherApp@ccdImporter',
-    'as'   => 'import.ccd.remix',
+    'uses' => 'RedirectToProviderApp@ccdImporter',
+    'as' => 'import.ccd.remix',
 ]);
 
-Route::get('pinfo', [
-    'uses' => 'RedirectToOtherApp@pinfo',
-])->middleware(['auth', 'role:administrator']);
+Route::get('patient/{patientId}/notes', [
+    'uses' => 'RedirectToProviderApp@notesIndex',
+    'as' => 'patient.note.index',
+]);
 
-    Route::get('patient/{patientId}/notes', [
-        'uses' => 'RedirectToProviderApp@notesIndex',
-        'as'   => 'patient.note.index',
-    ]);
-
-    Route::get('patient/{patientId}/careplan', [
-        'uses' => 'RedirectToProviderApp@showCareplan',
-        'as'   => 'patient.careplan.print',
-    ]);
-});
-Route::get('config', [
-    'uses' => 'RedirectToOtherApp@config',
-])->middleware(['auth', 'role:administrator']);
+Route::get('patient/{patientId}/careplan', [
+    'uses' => 'RedirectToProviderApp@showCareplan',
+    'as' => 'patient.careplan.print',
+]);
