7ca27f4ec77e0990664559261e27e6b92048c0ce
kakoushias <kakoushias@gmail.com>
2021-01-21 21:40:59 +0100

    billing-force-cs: changes and additions on model relationships, pivot model

---
+++ Entities/PatientForcedChargeableService.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace CircleLinkHealth\CcmBilling\Entities;
+
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class PatientForcedChargeableService extends Pivot
+{
+    const FORCE_ACTION_TYPE = 'force';
+    const BLOCK_ACTION_TYPE = 'block';
+
+    protected $appends = [
+        'action_type',
+        'chargeable_month'
+    ];
+
+    protected $dates = [
+        'chargeable_month',
+        'created_at',
+        'updated_at'
+    ];
+
+    //todo: add logic on attach here?
+}
