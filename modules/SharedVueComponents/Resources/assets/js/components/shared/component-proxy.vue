cd79e604b01bf712cbcf673147aa86d7dfaee66b
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-21 10:42:36 -0600

    Merging SharedVueComponents to master

---
+++ modules/SharedVueComponents/Resources/assets/js/components/shared/component-proxy.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            props: {
+                type: Object,
+                default: () => {
+                }
+            }
+        },
+
+        render(createElem) {
+            return createElem(this.name, {
+                attrs: this.props
+            });
+        }
+    }
+</script>
