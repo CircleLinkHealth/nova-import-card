3ffa7ca530236982d36a86a59ea29ed1df880073
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-22 10:34:20 -0600

    Adds copy git config install step

--- bin/local-dev/install.sh
+++ bin/local-dev/install.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+cp -f "$PWD/bin/local-dev/git/config" "$PWD/.git/config"
 bash "$PWD/bin/local-dev/setup-symlinks.sh"
 bash "$PWD/bin/local-dev/setup-valet.sh"
 bash "$PWD/bin/local-dev/composer.sh"
