c8d16d45fcf54e28987c74c1369c69eff8e40df0
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-22 16:41:37 -0600

    Merging SharedVueComponents to master

---
+++ modules/SharedVueComponents/Resources/assets/js/components/pages/provider-admin-panel/manage-practice-locations.vue
@@ -0,0 +1,29 @@
+<template>
+    <component :is="componentName" :location="props" @update-view="updateView"></component>
+</template>
+
+<script>
+    import UpdateLocation from '../../practice/lodations/update.vue'
+    import IndexLocations from '../../practice/lodations/index.vue'
+
+    export default {
+        components: {
+            UpdateLocation,
+            IndexLocations
+        },
+
+        data() {
+            return {
+                componentName: 'index-locations',
+                props: {}
+            }
+        },
+
+        methods: {
+            updateView(componentName, props) {
+                this.componentName = componentName
+                this.props = props
+            }
+        }
+    }
+</script>
