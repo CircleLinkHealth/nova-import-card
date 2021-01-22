50fec7b48a98b04ab15d665a45ea07a32597e39b
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-01-22 11:30:59 -0600

    Improves gitconfig workflow

--- bin/local-dev/install.sh
+++ bin/local-dev/install.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-cp -f "$PWD/bin/local-dev/git/config" "$PWD/.git/config"
+git config --local include.path $PWD/.gitconfig
 bash "$PWD/bin/local-dev/setup-symlinks.sh"
 bash "$PWD/bin/local-dev/setup-valet.sh"
 bash "$PWD/bin/local-dev/composer.sh"
--- bin/monorepo/build/build.sh
+++ bin/monorepo/build/build.sh
@@ -5,3 +5,5 @@ export MONOREPO_NAME=cpm-monorepo
 cd ../
 
 cat ./$MONOREPO_NAME/repos.txt | ./tomono.sh --continue
+
+cp -f ./$MONOREPO_NAME/.git/config ./$MONOREPO_NAME/.gitconfig
