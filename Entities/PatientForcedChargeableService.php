222b43f967fd96b16c64072e92a89a8897006242
kakoushias <kakoushias@gmail.com>
2021-01-26 19:34:49 +0100

    billing-force-cs: make nova work with pivot model

--- Entities/PatientForcedChargeableService.php
+++ Entities/PatientForcedChargeableService.php
@@ -6,14 +6,27 @@
 namespace CircleLinkHealth\CcmBilling\Entities;
 
 
+use CircleLinkHealth\Customer\Entities\ChargeableService;
+use CircleLinkHealth\Customer\Entities\User;
 use CircleLinkHealth\Revisionable\RevisionableTrait;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class PatientForcedChargeableService extends Pivot
 {
+    protected $table = 'patient_forced_chargeable_services';
     //todo: test this works on pivot model, i.e: do the same model events fire for pivot models?
     //create Pivot revisionable if necessary
     use RevisionableTrait;
+
+    public static function boot()
+    {
+        parent::boot();
+
+//        static::saving(function ($item)  {
+//            // this will die and dump on the first element passed to ->sync()
+//            dd($item);
+//        });
+    }
 
     const FORCE_ACTION_TYPE = 'force';
     const BLOCK_ACTION_TYPE = 'block';
@@ -36,5 +49,17 @@ class PatientForcedChargeableService extends Pivot
         'updated_at'
     ];
 
-    //todo: add logic on attach here?
+    public function patient(){
+        return $this->belongsTo(User::class, 'patient_user_id', 'id');
+    }
+
+    public function chargeableService(){
+        return $this->belongsTo(ChargeableService::class, 'chargeable_service_id', 'id');
+    }
+
+    //todo: Hack for Laravel\Nova\Http\Controllers\ResourceAttachController@handle
+    public function attributesToArray()
+    {
+        return $this->getAttributes();
+    }
 }
