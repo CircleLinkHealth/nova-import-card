0907db9274a0903485d3599b489e82e6c2cababc
kakoushias <kakoushias@gmail.com>
2021-01-27 22:52:31 +0100

    billing-force-cs: add observer logic

--- app/Nova/PatientForcedChargeableService.php
+++ app/Nova/PatientForcedChargeableService.php
@@ -17,6 +17,13 @@ use Laravel\Nova\Fields\Text;
 class PatientForcedChargeableService extends Resource
 {
     /**
+     * Indicates if the resource should be displayed in the sidebar.
+     *
+     * @var bool
+     */
+    public static $displayInNavigation = false;
+
+    /**
      * The model the resource corresponds to.
      *
      * @var string
