c5c4bcea55ee723f8bf31522b19efab44d8d5682
kakoushias <kakoushias@gmail.com>
2021-01-27 16:46:35 +0100

    billing-force-cs: linting

--- Database/Migrations/2021_01_21_152403_rename_is_forced_column_on_patient_forced_chargeable_services_table.php
+++ Database/Migrations/2021_01_21_152403_rename_is_forced_column_on_patient_forced_chargeable_services_table.php
@@ -1,25 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class RenameIsForcedColumnOnPatientForcedChargeableServicesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('patient_forced_chargeable_services', function (Blueprint $table) {
-            $table->dropColumn('is_forced');
-
-            $table->enum('action_type', ['force', 'block'])->default('force')->after('chargeable_month');
-        });
-    }
-
     /**
      * Reverse the migrations.
      *
@@ -31,6 +21,20 @@ class RenameIsForcedColumnOnPatientForcedChargeableServicesTable extends Migrati
             $table->dropColumn('action_type');
 
             $table->boolean('is_forced')->default(true)->after('chargeable_month');
+        });
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patient_forced_chargeable_services', function (Blueprint $table) {
+            $table->dropColumn('is_forced');
+
+            $table->enum('action_type', ['force', 'block'])->default('force')->after('chargeable_month');
         });
     }
 }
