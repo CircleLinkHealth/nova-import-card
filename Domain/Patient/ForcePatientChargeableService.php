75d84f1a2e9d2206f8e3b0fbb73bd8af2e755b0a
kakoushias <kakoushias@gmail.com>
2021-01-27 16:29:36 +0100

    billing-force-cs: add DTO

--- Domain/Patient/ForcePatientChargeableService.php
+++ Domain/Patient/ForcePatientChargeableService.php
@@ -17,18 +17,20 @@ class ForcePatientChargeableService
     protected string $actionType;
     protected ?Carbon $month;
     protected int $patientUserId;
+    protected bool $isDetaching;
 
-    public function __construct(int $patientUserId, int $chargeableServiceId, string $actionType = PatientForcedChargeableService::FORCE_ACTION_TYPE, ?Carbon $month = null)
+    public function __construct(int $patientUserId, int $chargeableServiceId, string $actionType = PatientForcedChargeableService::FORCE_ACTION_TYPE, ?Carbon $month = null, bool $isDetaching)
     {
         $this->patientUserId       = $patientUserId;
         $this->chargeableServiceId = $chargeableServiceId;
         $this->actionType              = $actionType;
         $this->month               = $month;
+        $this->isDetaching            = $isDetaching;
     }
 
-    public static function execute(int $patientUserId, int $chargeableServiceId, ?Carbon $month = null): void
+    public static function execute(int $patientUserId, int $chargeableServiceId, ?Carbon $month = null, bool $isDetaching): void
     {
-        (new static($patientUserId, $chargeableServiceId, PatientForcedChargeableService::FORCE_ACTION_TYPE, $month))
+        (new static($patientUserId, $chargeableServiceId, PatientForcedChargeableService::FORCE_ACTION_TYPE, $month, $isDetaching))
             ->setForcedChargeableService()
             ->guaranteeHistoricallyAccurateRecords()
             ->reprocessPatientForBilling();
@@ -36,24 +38,43 @@ class ForcePatientChargeableService
 
     private function guaranteeHistoricallyAccurateRecords() : self
     {
-        self::processHistoricalRecords($this->patientUserId, $this->chargeableServiceId, $this->actionType, $this->month);
+        self::processHistoricalRecords($this->patientUserId, $this->chargeableServiceId, $this->actionType, $this->month, $this->isDetaching);
 
         return $this;
     }
 
-    public static function processHistoricalRecords(int $patientUserId, int $chargeableServiceId, string $actionType, ?Carbon $month) : void
+    public static function processHistoricalRecords(int $patientUserId, int $chargeableServiceId, string $actionType, ?Carbon $month, bool $isDetaching = false) : void
     {
         $patient = User::ofType('participant')
             ->with('forcedChargeableServices')
             ->findOrFail($patientUserId);
 
+        //if detaching forced CS
+        //1. IF detaching perma force or block, create historical records from created_at date until now  (maybe double check for intermediary opposite type entries for month)
+        //2. If for specific month just detach
+        if ($isDetaching){
+            if (is_null($month)){
+                $startingMonth = '';//need starting month
+            }
+            return;
+        }
+
+
+        //iF not detaching
+        //
+        //if type is force,
+        //1.remove block from month if exists
+        //2.if perma block, convert to historical records if you must
+        //3.if month, check for perma, and log error somewhere if perma exists.
+
+        //if type is block above or opposite
     }
 
 
 
-    public static function onPivotModelEvent(int $patientUserId, int $chargeableServiceId, string $actionType, ?Carbon $month):void
+    public static function onPivotModelEvent(int $patientUserId, int $chargeableServiceId, string $actionType, ?Carbon $month, bool $isDetaching):void
     {
-        self::processHistoricalRecords($patientUserId, $chargeableServiceId, $actionType, $month);
+        self::processHistoricalRecords($patientUserId, $chargeableServiceId, $actionType, $month, $isDetaching);
         ProcessPatientSummaries::wipeAndReprocessForMonth(
             $patientUserId,
             is_null($month) ? Carbon::now()->startOfMonth() : $month
