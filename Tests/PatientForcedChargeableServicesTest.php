d28ff5cdb6986e82a1e62f297c110e77cfeaddd7
kakoushias <kakoushias@gmail.com>
2021-01-28 22:27:59 +0100

    billing-force-cs: fixes and policy additions

---
+++ Tests/PatientForcedChargeableServicesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CircleLinkHealth\CcmBilling\Tests;
+
+use CircleLinkHealth\Customer\Tests\CustomerTestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PatientForcedChargeableServicesTest extends CustomerTestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
