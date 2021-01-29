dd81f01f413d482a3ff60a68fee81f44cbfc3c8f
kountouris7 <kountouris7@gmail.com>
2021-01-29 13:39:16 +0200

    Command to delete uploaded enrollees by zach

---
+++ app/Console/Commands/DeleteEnrolleesMarkedForSelfEnrollment.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use CircleLinkHealth\Customer\Entities\User;
+use CircleLinkHealth\SharedModels\Entities\Enrollee;
+use Illuminate\Console\Command;
+
+class DeleteEnrolleesMarkedForSelfEnrollment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete:enrollees-marked-for-selfEnrollment {practiceId} {updatedAt}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Delete all Enrollees with their User Patient and Role. updatedAt = in format '2021-01-28 16:00:00'";
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $updatedAtString = $this->argument('updatedAt');
+        $practiceId = intval($this->argument('practiceId'));
+
+
+        /** @var Carbon $updatedAt */
+        $updatedAt = Carbon::parse($updatedAtString);
+
+
+      User::with(['enrollee','patientInfo','roles'])
+            ->whereHas('enrollee', function ($enrollee) use ($updatedAt){
+                $enrollee->where('status', Enrollee::QUEUE_AUTO_ENROLLMENT);
+                $enrollee->where('updated_at','>', $updatedAt);
+            })->where('program_id', $practiceId)
+            ->chunk(50, function($users){
+                $users->each(function ($user){
+                    try {
+                        $user->enrollee->forceDelete();
+                        $user->patientInfo->forceDelete();
+                        $user->roles()->detach();
+                        $user->forceDelete();
+                        $this->info("User $user->id has been processed successfully.");
+                    }catch (\Exception $exception){
+                        $errorMessage = $exception->getMessage();
+                        $this->error("[Something went wrong with user $user->id], $errorMessage");
+                    }
+                });
+            });
+
+        $this->info("Done");
+    }
+}
