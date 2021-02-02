57844aa616aa15d867c1e736a684b40ee4cceba3
Pangratios Cosma <pagcosma@gmail.com>
2021-02-02 21:52:57 +0200

    linting

--- Providers/CommandsServiceProvider.php
+++ Providers/CommandsServiceProvider.php
@@ -8,8 +8,8 @@ namespace CircleLinkHealth\CpmAdmin\Providers;
 
 use CircleLinkHealth\CpmAdmin\Console\Commands\CountBillablePatientsForMonth;
 use CircleLinkHealth\CpmAdmin\Console\Commands\CountPatientMonthlySummaryCalls;
-use CircleLinkHealth\SelfEnrollment\Console\Commands\ManuallyCreateEnrollmentTestData;
 use CircleLinkHealth\CpmAdmin\Console\Commands\SyncNumberOfCallsForCurrentMonth;
+use CircleLinkHealth\SelfEnrollment\Console\Commands\ManuallyCreateEnrollmentTestData;
 use Illuminate\Support\ServiceProvider;
 
 class CommandsServiceProvider extends ServiceProvider
@@ -19,8 +19,8 @@ class CommandsServiceProvider extends ServiceProvider
         $this->commands([
             CountBillablePatientsForMonth::class,
             CountPatientMonthlySummaryCalls::class,
-            SyncNumberOfCallsForCurrentMonth::class,
             ManuallyCreateEnrollmentTestData::class,
+            SyncNumberOfCallsForCurrentMonth::class,
         ]);
     }
 }
