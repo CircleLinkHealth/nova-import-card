b6570872077700737d0e7ddc1e354e64c04489c6
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-02-02 20:39:04 -0600

    removes dependencies

--- Providers/CommandsServiceProvider.php
+++ Providers/CommandsServiceProvider.php
@@ -8,7 +8,6 @@ namespace CircleLinkHealth\CpmAdmin\Providers;
 
 use CircleLinkHealth\CpmAdmin\Console\Commands\CountBillablePatientsForMonth;
 use CircleLinkHealth\CpmAdmin\Console\Commands\CountPatientMonthlySummaryCalls;
-use CircleLinkHealth\SelfEnrollment\Console\Commands\ManuallyCreateEnrollmentTestData;
 use CircleLinkHealth\CpmAdmin\Console\Commands\SyncNumberOfCallsForCurrentMonth;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,7 +19,6 @@ class CommandsServiceProvider extends ServiceProvider
             CountBillablePatientsForMonth::class,
             CountPatientMonthlySummaryCalls::class,
             SyncNumberOfCallsForCurrentMonth::class,
-            ManuallyCreateEnrollmentTestData::class,
         ]);
     }
 }
