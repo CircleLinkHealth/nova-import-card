a29f0f460f799b97e1e7968c4bbbe5d15191683b
kakoushias <kakoushias@gmail.com>
2021-02-03 14:28:51 +0100

    hotfix-self-enrollment-controller-paths: rest of fixes

--- Database/Migrations/2020_05_25_050420_add_foreign_key_to_enrollment_invitation_links.php
+++ Database/Migrations/2020_05_25_050420_add_foreign_key_to_enrollment_invitation_links.php
@@ -4,10 +4,10 @@
  * This file is part of CarePlan Manager by CircleLink Health.
  */
 
-use CircleLinkHealth\Customer\EnrollableInvitationLink\EnrollableInvitationLink;
 use CircleLinkHealth\Customer\Entities\User;
-use CircleLinkHealth\Eligibility\SelfEnrollment\Entities\EnrollmentInvitationsBatch;
-use CircleLinkHealth\Eligibility\SelfEnrollment\Http\Controllers\SelfEnrollmentController;
+use CircleLinkHealth\SelfEnrollment\Entities\EnrollmentInvitationsBatch;
+use CircleLinkHealth\SelfEnrollment\Http\Controllers\SelfEnrollmentController;
+use CircleLinkHealth\SelfEnrollment\EnrollableInvitationLink\EnrollableInvitationLink;
 use CircleLinkHealth\SharedModels\Entities\Enrollee;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
