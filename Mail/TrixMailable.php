c43e3830d8e7ddfed712c253cdb08a04afa3af45
kakoushias <kakoushias@gmail.com>
2021-02-05 20:34:22 +0100

    hotfix-self-enrollment-controller-paths: fix

--- Mail/TrixMailable.php
+++ Mail/TrixMailable.php
@@ -79,7 +79,7 @@ class TrixMailable extends Mailable
 
     private function getPracticePhone(): string
     {
-        $phone = (new StringManipulation())->formatPhoneNumberWithNpaParenthesized($this->patient->primaryPractice->outgoing_phone);
+        $phone = (new StringManipulation())->formatPhoneNumberWithNpaParenthesized($this->patient->primaryPractice->outgoing_phone_number);
 
         if (empty($phone)) {
             sendSlackMessage('#cpm_general_alerts', "URGENT! Practice {$this->patient->primaryPractice->id}, does not have an outgoing phone number!");
