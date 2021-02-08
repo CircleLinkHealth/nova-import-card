7bc2cdd9fdf2a6e29c017192be6d77d2e59048a8
kakoushias <kakoushias@gmail.com>
2021-02-08 23:48:08 +0100

    billing-force-cs: add test, fixes

--- Tests/PatientForcedChargeableServicesTest.php
+++ Tests/PatientForcedChargeableServicesTest.php
@@ -6,6 +6,9 @@
 
 namespace CircleLinkHealth\CcmBilling\Tests;
 
+use Carbon\Carbon;
+use CircleLinkHealth\CcmBilling\Entities\PatientForcedChargeableService;
+use CircleLinkHealth\Customer\Entities\ChargeableService;
 use CircleLinkHealth\Customer\Tests\CustomerTestCase;
 
 class PatientForcedChargeableServicesTest extends CustomerTestCase
@@ -15,8 +18,24 @@ class PatientForcedChargeableServicesTest extends CustomerTestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_historical_records_are_created_for_deleted_permantent_forced_cs()
     {
-        $this->assertTrue(true);
+        PatientForcedChargeableService::unguard();
+        $forcedCs = PatientForcedChargeableService::create([
+            'patient_user_id'       => $this->patient()->id,
+            'action_type'           => PatientForcedChargeableService::FORCE_ACTION_TYPE,
+            'chargeable_month'      => null,
+            'chargeable_service_id' => $csId = ChargeableService::getChargeableServiceIdUsingCode(ChargeableService::CCM),
+            'created_at'            => Carbon::now()->subMonths(6)->startOfMonth(),
+        ]);
+
+        $forcedCs->delete();
+
+        $this->assertEquals(6,
+            PatientForcedChargeableService::where('chargeable_service_id', $csId)
+                                          ->where('patient_user_id', $this->patient()->id)
+                                          ->where('action_type', PatientForcedChargeableService::FORCE_ACTION_TYPE)
+                                          ->count()
+        );
     }
 }
