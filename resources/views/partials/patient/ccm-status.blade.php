7d78ed6e5a752b46793e15abd4c0221214aa2333
kountouris7 <kountouris7@gmail.com>
2021-02-09 20:18:13 +0200

    Separated logic

--- resources/views/partials/patient/ccm-status.blade.php
+++ resources/views/partials/patient/ccm-status.blade.php
@@ -1,36 +1,29 @@
-@php
-    use CircleLinkHealth\Customer\Entities\Patient;
 
-    $ccmStatus = $patient->getCcmStatus();
-    $isUnreachableAndSurveyOnly= $ccmStatus === Patient::UNREACHABLE && $patient->isSurveyOnly();
 
-    if ($isUnreachableAndSurveyOnly){
-        $ccmStatus = 'Enrollee';
-    }
-
-    $statusesForDropdown = [
-         Patient::ENROLLED => 'Enrolled',
-    ];
-
-    if (auth()->user()->isAdmin()) {
-        $statusesForDropdown[Patient::PAUSED] = 'Paused';
-    }
-
-    if ($ccmStatus == Patient::WITHDRAWN_1ST_CALL){
-        $statusesForDropdown[Patient::WITHDRAWN_1ST_CALL] = 'Wthdrn 1st Call';
-    }else{
-        $statusesForDropdown[Patient::WITHDRAWN] = 'Withdrawn';
-    }
-
-    if ($ccmStatus == Patient::UNREACHABLE){
-        $statusesForDropdown[Patient::UNREACHABLE] = 'Unreachable';
-    }
-
-    if ($isUnreachableAndSurveyOnly){
-        $statusesForDropdown[Patient::UNREACHABLE] = 'Unreachable';
-    }
-@endphp
 @if(Route::is('patient.note.create') || Route::is('patient.note.edit'))
+    @php
+        $model =  \CircleLinkHealth\Customer\Entities\Patient::class;
+
+        $ccmStatus = $patient->getCcmStatus();
+
+        $statusesForDropdown = [
+             $model::ENROLLED => 'Enrolled',
+        ];
+
+        if (auth()->user()->isAdmin()) {
+            $statusesForDropdown[$model::PAUSED] = 'Paused';
+        }
+
+        if ($ccmStatus == $model::WITHDRAWN_1ST_CALL){
+            $statusesForDropdown[$model::WITHDRAWN_1ST_CALL] = 'Wthdrn 1st Call';
+        }else{
+            $statusesForDropdown[$model::WITHDRAWN] = 'Withdrawn';
+        }
+
+        if ($ccmStatus == $model::UNREACHABLE){
+            $statusesForDropdown[$model::UNREACHABLE] = 'Unreachable';
+        }
+    @endphp
     <li class="inline-block">
         <select id="ccm_status" name="ccm_status" class="selectpickerX dropdownValid form-control"
                 data-size="2"
@@ -43,10 +36,162 @@
         </select>
     </li>
 @else
+    @php
+        $model =  \CircleLinkHealth\Customer\Entities\Patient::class;
+        $ccmStatus = $patient->getCcmStatus();
+        if ($ccmStatus === $model::UNREACHABLE && $patient->isSurveyOnly()){
+            $ccmStatus = 'Enrollee';
+        }
+
+    @endphp
     <li style="font-size: 18px"
         class="inline-block col-xs-pull-1 {{$ccmStatus}}">{{(empty($ccmStatus))
             ? 'N/A'
-            : (Patient::WITHDRAWN_1ST_CALL === $ccmStatus
+            : ($model::WITHDRAWN_1ST_CALL === $ccmStatus
                 ? 'Withdrawn 1st Call'
                 : ucwords($ccmStatus))}}</li>
 @endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
