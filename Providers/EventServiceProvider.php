80fa5aa17c20faf50a4efd1abeea428a192f6727
kountouris7 <kountouris7@gmail.com>
2021-02-10 15:05:00 +0200

    NotificationStatusUpdateTest.php

---
+++ Providers/EventServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace CircleLinkHealth\SelfEnrollment\Providers;
+
+use CircleLinkHealth\Core\Listeners\LogFailedNotification;
+use CircleLinkHealth\Core\Listeners\LogMailSmtpId;
+use CircleLinkHealth\Core\Listeners\LogSentMailNotification;
+use CircleLinkHealth\Core\Listeners\LogSentNotification;
+use CircleLinkHealth\Core\Listeners\PostmarkAddSmtpIdOnHeader;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Mail\Events\MessageSending;
+use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Mail\MailManager;
+use Illuminate\Notifications\Events\NotificationFailed;
+use Illuminate\Notifications\Events\NotificationSent;
+
+
+class EventServiceProvider extends  ServiceProvider
+{
+
+    protected $listen = [
+        MessageSending::class => [
+            LogMailSmtpId::class, //this needs to be first
+            CheckBeforeSendMessageListener::class,
+        ],
+        MessageSent::class => [
+            LogSentMailNotification::class,
+        ],
+        NotificationSent::class => [
+            LogSentNotification::class,
+        ],
+        NotificationFailed::class => [
+            LogFailedNotification::class,
+        ],
+    ];
+}
