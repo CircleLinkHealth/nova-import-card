ff3e54f0fd951d38ebc662037bea27d0facef226
Pangratios Cosma <pagcosma@gmail.com>
2021-02-12 11:28:43 +0200

    lighten up the job's message

--- Entities/PostmarkInboundMailRequest.php
+++ Entities/PostmarkInboundMailRequest.php
@@ -8,32 +8,46 @@ namespace CircleLinkHealth\SharedModels\Entities;
 
 class PostmarkInboundMailRequest
 {
-    public ?array $Attachments;
-    public ?string $Bcc;
-    public ?array $BccFull;
+    // public ?array $Attachments;
+    // public ?string $Bcc;
+    // public ?array $BccFull;
     public ?string $Cc;
-    public ?array $CcFull;
+    // public ?array $CcFull;
     public ?string $From;
-    public ?array $FromFull;
-    public ?string $FromName;
-    public ?array $Headers;
+    // public ?array $FromFull;
+    // public ?string $FromName;
+    // public ?array $Headers;
     public ?string $HtmlBody;
-    public ?string $MailboxHash;
-    public ?string $MailboxID;
-    public ?string $MessageStream;
-    public ?string $OriginalRecipient;
+    // public ?string $MailboxHash;
+    // public ?string $MailboxID;
+    // public ?string $MessageStream;
+    // public ?string $OriginalRecipient;
     public ?string $ReplyTo;
-    public ?string $StrippedTextReply;
+    // public ?string $StrippedTextReply;
     public ?string $Subject;
-    public ?string $Tag;
+    // public ?string $Tag;
     public ?string $TextBody;
     public ?string  $To;
-    public ?array $ToFull;
+    // public ?array $ToFull;
 
     public function __construct(array $input)
     {
-        foreach ($input as $key => $value) {
-            $this->$key = $value;
+        $this->setFields($input, [
+            'Cc',
+            'From',
+            'ReplyTo',
+            'Subject',
+            'TextBody',
+            'To',
+        ]);
+    }
+
+    private function setFields(array $input, array $fields)
+    {
+        foreach ($fields as $field) {
+            if (isset($input[$field])) {
+                $this->$field = $input[$field];
+            }
         }
     }
 }
