99f5d7399cbc98bb7ca484a8ba5b1a4dc49f44c2
kountouris7 <kountouris7@gmail.com>
2021-02-16 23:29:06 +0200

    Refactor and new test

--- tests/Feature/IntercomBubbleChatTest.php
+++ tests/Feature/IntercomBubbleChatTest.php
@@ -7,6 +7,7 @@
 namespace Tests\Feature;
 
 use CircleLinkHealth\Core\Entities\AppConfig;
+use CircleLinkHealth\Customer\Entities\Role;
 use CircleLinkHealth\Customer\Tests\CustomerTestCase;
 use CircleLinkHealth\Customer\Traits\UserHelpers;
 
@@ -15,7 +16,14 @@ class IntercomBubbleChatTest extends CustomerTestCase
     use UserHelpers;
 
     const CONFIG_KEY = 'roles_allowed_bubble_chat';
-
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        AppConfig::remove(self::CONFIG_KEY);
+        AppConfig::clearCache();
+    }
+    
     public function test_if_app_config_is_empty_no_user_can_see_bubble_chat()
     {
         self::assertTrue(empty(AppConfig::pull(self::CONFIG_KEY)));
@@ -36,7 +44,7 @@ class IntercomBubbleChatTest extends CustomerTestCase
     public function test_only_roles_in_app_config_are_able_to_see_bubble_chat()
     {
         $this->setChatAppConfig('care-center,care-ambassador');
-
+        self::assertFalse(empty(AppConfig::pull(self::CONFIG_KEY)));
         $careCoach      = $this->createUser(8, 'care-center');
         $careAmbassador = $this->createUser(8, 'care-ambassador');
         $provider       = $this->createUser(8, 'provider');
@@ -51,9 +59,21 @@ class IntercomBubbleChatTest extends CustomerTestCase
         self::assertFalse($patient->isAllowedToBubbleChat());
     }
 
+    public function test_user_with_multiple_roles_if_one_role_matches_config_role_then_will_return_true()
+    {
+        $this->setChatAppConfig('care-center, care-ambassador');
+        self::assertFalse(empty(AppConfig::pull(self::CONFIG_KEY)));
+
+        $adminCareAmbassador = $this->createUser(8, 'administrator');
+        $adminCareAmbassador->attachGlobalRole(Role::whereName('care-ambassador')->firstOrFail()->id);
+
+        self::assertTrue($adminCareAmbassador->hasRole('administrator'));
+        self::assertTrue($adminCareAmbassador->hasRole('care-ambassador'));
+        self::assertTrue($adminCareAmbassador->isAllowedToBubbleChat());
+    }
+
     private function setChatAppConfig(string $roles)
     {
-        AppConfig::clearCache();
         AppConfig::create(
             [
                 'config_key'   => self::CONFIG_KEY,
