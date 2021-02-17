2f4dbca8e6ceb1c8f3d4275b9cd57804c76e821c
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-02-17 16:46:06 -0600

    npm install

--- bin/local-dev/install.sh
+++ bin/local-dev/install.sh
@@ -7,3 +7,5 @@ bash "$PWD/bin/local-dev/setup-symlinks.sh"
 bash "$PWD/bin/local-dev/setup-valet.sh"
 bash "$PWD/bin/local-dev/composer.sh" install
 bash "$PWD/bin/local-dev/npm.sh"
+
+echo "CLH Monorepo installation finished! Build something awesome!"
