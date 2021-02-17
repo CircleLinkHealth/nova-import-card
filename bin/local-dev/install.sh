7e34219838249638eae1e73dad6e094e0facab28
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-02-17 16:26:50 -0600

    Adds install or update option to composer.sh. Default to install

--- bin/local-dev/install.sh
+++ bin/local-dev/install.sh
@@ -5,5 +5,5 @@ set -x
 git config --local include.path $PWD/.gitconfig
 bash "$PWD/bin/local-dev/setup-symlinks.sh"
 bash "$PWD/bin/local-dev/setup-valet.sh"
-bash "$PWD/bin/local-dev/composer.sh"
+bash "$PWD/bin/local-dev/composer.sh" install
 bash "$PWD/bin/local-dev/npm.sh"
