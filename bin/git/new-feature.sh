fa497d3ed9b7912746cb869422db3c9638125536
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-02-17 17:13:52 -0600

    adds

--- bin/git/new-feature.sh
+++ bin/git/new-feature.sh
@@ -5,4 +5,13 @@ set -x
 
 FEATURE=$1
 
+if [[ ! -z "$(git status --porcelain)" ]]
+then
+    echo "Your working directory is dirty. Did you forget to commit your changes?"
+
+    exit 1
+fi
+
+git checkout master
+git pull
 git checkout -b feature_$FEATURE
