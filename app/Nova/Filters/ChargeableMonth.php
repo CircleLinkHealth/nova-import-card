1c6a8e227ca79de79fa9c87b301aba842b37c3d4
kakoushias <kakoushias@gmail.com>
2021-02-24 02:02:32 +0200

    hotfix: attested problem nova resource

---
+++ app/Nova/Filters/ChargeableMonth.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class ChargeableMonth extends Filter
+{
+    const DEFAULT_MONTHS_TO_GO_BACK_T0 = 6;
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('chargeable_month', $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        $result         = [];
+        for ($i = 0; $i < self::DEFAULT_MONTHS_TO_GO_BACK_T0; ++$i) {
+            $month        = now()->subMonths($i);
+            $key          = $month->monthName.' '.$month->year;
+            $result[$key] = $month->startOfMonth()->toDateString();
+        }
+
+        return $result;
+    }
+}
