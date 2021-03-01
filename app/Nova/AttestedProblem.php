e993fa8a0d5cfa7628a7777cd72dd8343688e5ca
Pangratios Cosma <pagcosma@gmail.com>
2021-03-01 15:07:31 +0200

    composeros and linting

--- app/Nova/AttestedProblem.php
+++ app/Nova/AttestedProblem.php
@@ -1,9 +1,12 @@
 <?php
 
+/*
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
 namespace App\Nova;
 
 use App\Nova\Filters\ChargeableMonth;
-use App\Nova\Filters\MonthFilter;
 use CircleLinkHealth\CcmBilling\Entities\AttestedProblem as AttestedProblemModel;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
@@ -12,7 +15,6 @@ use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Http\Requests\NovaRequest;
 
 class AttestedProblem extends Resource
 {
@@ -22,13 +24,6 @@ class AttestedProblem extends Resource
      * @var string
      */
     public static $model = AttestedProblemModel::class;
-
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -47,9 +42,26 @@ class AttestedProblem extends Resource
      * @var array
      */
     public static $searchRelations = [
-        'patient' => ['display_name'],
-        'attestor' => ['display_name']
+        'patient'  => ['display_name'],
+        'attestor' => ['display_name'],
     ];
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
 
     /**
      * @return bool
@@ -76,9 +88,18 @@ class AttestedProblem extends Resource
     }
 
     /**
+     * Get the cards available for the request.
+     *
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
      * Get the fields displayed by the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function fields(Request $request)
@@ -88,57 +109,33 @@ class AttestedProblem extends Resource
 
             Number::make('patient_user_id')->sortable(),
             BelongsTo::make('patient', 'Patient', User::class)->searchable()->sortable(),
-            BelongsTo::make('attestor', 'Attestor',User::class)->searchable()->sortable(),
+            BelongsTo::make('attestor', 'Attestor', User::class)->searchable()->sortable(),
 
             Date::make('chargeable_month'),
             Text::make('ccd_problem_name'),
             Text::make('ccd_problem_icd_10_code'),
-            DateTime::make('created_at')
+            DateTime::make('created_at'),
         ];
-    }
-
-    /**
-     * Get the cards available for the request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function cards(Request $request)
-    {
-        return [];
     }
 
     /**
      * Get the filters available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function filters(Request $request)
     {
         return [
-            new ChargeableMonth()
+            new ChargeableMonth(),
         ];
     }
 
     /**
      * Get the lenses available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function lenses(Request $request)
-    {
-        return [];
-    }
-
-    /**
-     * Get the actions available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function actions(Request $request)
     {
         return [];
     }
