31cd4be47cd43617c9e3d522286d55283ef64739
kakoushias <kakoushias@gmail.com>
2021-03-01 20:21:22 +0200

    fix

--- apps/provider-app/app/Providers/CpmEventServiceProvider.php
+++ apps/provider-app/app/Providers/CpmEventServiceProvider.php
@@ -29,7 +29,6 @@ use App\Listeners\UserLoggedOut;
 use CircleLinkHealth\CcmBilling\Events\NurseAttestedToPatientProblems;
 use CircleLinkHealth\CcmBilling\Listeners\CreateAttestationRecords;
 use CircleLinkHealth\Core\Listeners\CheckBeforeSendMessageListener;
-use CircleLinkHealth\Core\Listeners\CheckBeforeSendMessageListener;
 use CircleLinkHealth\Core\Listeners\LogFailedNotification;
 use CircleLinkHealth\Core\Listeners\LogMailSmtpId;
 use CircleLinkHealth\Core\Listeners\LogSentMailNotification;
