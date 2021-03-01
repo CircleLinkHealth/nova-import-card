d6f12a1268fc233b06e180a10e16b6a1043cf26e
kakoushias <kakoushias@gmail.com>
2021-03-02 01:57:32 +0200

    fix migrate script

--- bin/local-dev/migrate.sh
+++ bin/local-dev/migrate.sh
@@ -3,11 +3,4 @@
 set -e
 set -x
 
-for SUBDOMAIN in $(ls "$PWD/apps/")
-do
-     APP_PATH="$PWD/apps/$SUBDOMAIN"
-     if [[ "$APP_PATH" = *provider-app ]];then
-          echo "$APP_PATH: Running migrations and generating view tables!"
-          (cd $APP_PATH && php artisan migrate && php artisan migrate:views)
-     fi
-done
+(cd "$PWD/apps/provider-app" && php artisan migrate && php artisan migrate:views)
