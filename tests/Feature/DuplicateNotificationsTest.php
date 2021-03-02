1d41b7a119f8f66e859831873ee42542c74da77f
Pangratios Cosma <pagcosma@gmail.com>
2021-03-02 15:36:28 +0200

    improved logic, more tests

--- tests/Feature/DuplicateNotificationsTest.php
+++ tests/Feature/DuplicateNotificationsTest.php
@@ -9,6 +9,7 @@ namespace Tests\Feature;
 use CircleLinkHealth\Core\Entities\AppConfig;
 use CircleLinkHealth\Core\Entities\DatabaseNotification;
 use CircleLinkHealth\Core\Notifications\Channels\DatabaseChannel;
+use CircleLinkHealth\Core\Notifications\Channels\DirectMailChannel;
 use CircleLinkHealth\Core\Notifications\DuplicateNotificationChecker;
 use CircleLinkHealth\Customer\Entities\Location;
 use CircleLinkHealth\Customer\Notifications\NoteForwarded;
@@ -23,15 +24,25 @@ class DuplicateNotificationsTest extends CustomerTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        AppConfig::set(DuplicateNotificationChecker::CONFIG_KEY, '*');
+        AppConfig::set(DuplicateNotificationChecker::TYPES_CONFIG_KEY, '*');
+        AppConfig::set(DuplicateNotificationChecker::MINUTES_CHECK_CONFIG_KEY, '10');
     }
 
     public function test_it_will_not_send_same_notification_to_user_twice()
     {
         $note     = $this->getNotification();
         $provider = $this->provider();
-        $this->storeInDb($provider, $note);
-        $result = DuplicateNotificationChecker::hasAlreadySentNotification($provider, $note);
+        $this->storeInDb($provider, $note, 'mail');
+        $result = DuplicateNotificationChecker::hasAlreadySentNotification($provider, $note, 'mail');
+        self::assertTrue($result);
+    }
+
+    public function test_it_will_not_send_same_notification_to_user_twice_2()
+    {
+        $note     = $this->getNotification();
+        $provider = $this->provider();
+        $this->storeInDb($provider, $note, DirectMailChannel::class);
+        $result = DuplicateNotificationChecker::hasAlreadySentNotification($provider, $note, DirectMailChannel::class);
         self::assertTrue($result);
     }
 
@@ -41,9 +52,8 @@ class DuplicateNotificationsTest extends CustomerTestCase
         $provider = $this->provider();
         /** @var Location $location */
         $location = $provider->locations->first();
-        $this->storeInDb($location, $note);
-
-        $result = DuplicateNotificationChecker::hasAlreadySentNotification($location, $note);
+        $this->storeInDb($location, $note, 'mail');
+        $result = DuplicateNotificationChecker::hasAlreadySentNotification($location, $note, 'mail');
         self::assertTrue($result);
     }
 
@@ -55,15 +65,24 @@ class DuplicateNotificationsTest extends CustomerTestCase
         $pLocationId = $providers[0]->locations->first()->id;
         self::assertEquals($pLocationId, $providers[1]->locations->first()->id);
 
-        $this->storeInDb($providers[0], $note);
+        $this->storeInDb($providers[0], $note, 'mail');
 
-        $result = DuplicateNotificationChecker::hasAlreadySentNotification($providers[0], $note);
+        $result = DuplicateNotificationChecker::hasAlreadySentNotification($providers[0], $note, 'mail');
         self::assertTrue($result);
 
         /** @var Location $location */
         $location = $providers[0]->locations->first();
-        $result   = DuplicateNotificationChecker::hasAlreadySentNotification($location, $note);
+        $result   = DuplicateNotificationChecker::hasAlreadySentNotification($location, $note, 'mail');
 
+        self::assertFalse($result);
+    }
+
+    public function test_it_will_send_same_notification_to_user_in_another_channel()
+    {
+        $note     = $this->getNotification();
+        $provider = $this->provider();
+        $this->storeInDb($provider, $note, 'mail');
+        $result = DuplicateNotificationChecker::hasAlreadySentNotification($provider, $note, DirectMailChannel::class);
         self::assertFalse($result);
     }
 
@@ -77,14 +96,20 @@ class DuplicateNotificationsTest extends CustomerTestCase
         return $notification;
     }
 
-    private function storeInDb($notifiable, Notification $notification)
+    private function storeInDb($notifiable, Notification $notification, string $channel)
     {
         $method = new ReflectionMethod(DatabaseChannel::class, 'buildPayload');
         $method->setAccessible(true);
-        $channel                    = new DatabaseChannel();
-        $payload                    = $method->invoke($channel, $notifiable, $notification);
+        $dbChannel                  = new DatabaseChannel();
+        $payload                    = $method->invoke($dbChannel, $notifiable, $notification);
         $payload['notifiable_id']   = $notifiable->id;
         $payload['notifiable_type'] = get_class($notifiable);
+        $payload['data']            = [
+            'status' => [
+                'database' => ['value' => 'pending'],
+                $channel   => ['value' => 'pending'],
+            ],
+        ];
 
         DatabaseNotification::create($payload);
     }
