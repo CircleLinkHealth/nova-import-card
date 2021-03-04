d0a49608a33a0f9d10c87ad89b7789a905ec463a
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-03-04 02:43:47 -0600

    Removes monorepo modules step

--- apps/superadmin-app/webpack.mix.js
+++ apps/superadmin-app/webpack.mix.js
@@ -55,8 +55,7 @@ mix.combine([
     'node_modules/select2/dist/js/select2.js'
 ], 'public/compiled/js/dependencies.js');
 
-mix.js('CircleLinkHealth/CpmAdmin/Resources/assets/js/app-provider-admin-panel-ui.js', 'public/compiled/js').sourceMaps();
-mix.js('CircleLinkHealth/CpmAdmin/Resources/assets/js/app-clh-admin-ui.js', 'public/compiled/js').sourceMaps();
+mix.js('vendor/circlelinkhealth/cpm-admin-module/Resources/assets/js/app-clh-admin-ui.js', 'public/compiled/js').sourceMaps();
 
 if (mix.inProduction()) {
     const ASSET_URL = process.env.ASSET_URL + "/";
