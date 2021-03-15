3ea4dda7b32ce60efe4c1a9efa33b7a4db359fd8
kakoushias <kakoushias@gmail.com>
2021-03-15 12:52:22 +0200

    automate-review-app: remove review apps command

---
+++ modules/VaporCli/src/Commands/Monorepo/DeleteReviewAppCommand.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace Laravel\VaporCli\Commands\Monorepo;
+
+
+use Laravel\VaporCli\Commands\Command;
+use Laravel\VaporCli\GitIgnore;
+use Laravel\VaporCli\Helpers;
+use Laravel\VaporCli\Manifest;
+use Laravel\VaporCli\Path;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Yaml\Yaml;
+
+class DeleteReviewAppCommand extends Command
+{
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $environment = $this->argument('environment');
+        $app         = array_reverse(explode('/', rtrim($_SERVER['PWD'], '-app')))[0];
+
+
+        if ( ! is_null($this->vapor->environmentNamed(Manifest::id(), $environment))) {
+            $this->vapor->deleteEnvironment(
+                Manifest::id(),
+                $environment,
+                );
+        }
+
+        Manifest::deleteEnvironment($environment);
+
+
+        if (file_exists("$environment.Dockerfile")) {
+            unlink("$environment.Dockerfile");
+        }
+
+        GitIgnore::remove(['.env.' . $environment]);
+
+        Helpers::info('Environment created successfully.');
+    }
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('delete-review-app')
+            ->addArgument('environment', InputArgument::REQUIRED, 'The review app name')
+            ->setDescription('Delete a review app');
+    }
+}
---
+++ modules/VaporCli/src/Commands/Monorepo/DeleteReviewAppsCommand.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of CarePlan Manager by CircleLink Health.
+ */
+
+namespace Laravel\VaporCli\Commands\Monorepo;
+
+
+use Laravel\VaporCli\Path;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Process\Process;
+
+class DeleteReviewAppsCommand extends InParallelCommand
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('delete-review-apps')
+            ->addArgument('environment', InputArgument::REQUIRED, 'The review app name')
+            ->addArgument('apps', InputArgument::IS_ARRAY, 'The apps to run the command for', self::CPM_APPS)
+            ->setDescription('Delete a review environment for the given apps');
+    }
+
+    public function createProcess(string $app): Process
+    {
+        $args = [
+            Path::current().'/modules/VaporCli/vapor',
+            'delete-review-app',
+            $this->argument('environment'),
+        ];
+
+        return new Process(
+            $args,
+            $this->appPath($app),
+            null,
+            null,
+            null
+        );
+    }
+}
