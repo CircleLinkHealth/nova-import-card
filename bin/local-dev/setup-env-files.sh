1c4ca0228311e54f6da6393e1ff42ef2e177a46a
kakoushias <kakoushias@gmail.com>
2021-03-18 19:39:29 +0200

    feature_billing-revamp: fix

--- bin/local-dev/setup-env-files.sh
+++ bin/local-dev/setup-env-files.sh
@@ -7,10 +7,11 @@ for SUBDOMAIN in $(ls "$PWD/apps/")
 do
     APP_PATH="$PWD/apps/$SUBDOMAIN"
     BLUEPRINT_ENV_FILE=".env.example"
+    ENV_FILE = ".env"
 
-    if [ ! -f "$APP_PATH/$BLUEPRINT_FILE" ]; then
+    if [ ! -f "$APP_PATH/$ENV_FILE" ]; then
         echo "$APP_PATH: .env not found!"
         echo "$APP_PATH: Creating .env from .env.example"
-        (cd $APP_PATH && (cp $BLUEPRINT_ENV_FILE .env))
+        (cd $APP_PATH && (cp $BLUEPRINT_ENV_FILE $ENV_FILE))
     fi
 done
