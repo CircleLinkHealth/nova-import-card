4ed4b99c62977f5aaaeaa299533d70032edd2569
Pangratios Cosma <pagcosma@gmail.com>
2021-04-01 23:15:36 +0300

    update k6 script

--- apps/provider-app/k6/script.js
+++ apps/provider-app/k6/script.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check, group } from 'k6';
+import {check, group} from 'k6';
 
 const baseUrl = "https://careplanmanager.com/";
 const routes = {
@@ -18,13 +18,13 @@ export const options = {
     vus: 10,
     duration: '60s',
     thresholds: {
-        'http_req_duration{url:https://careplanmanager.com/login}': ["avg<3000", "max<10000"],
-        'http_req_duration{url:https://careplanmanager.com/manage-patients/patient-call-list}': ["max<10000", "avg<8000"],
-        'http_req_duration{url:https://careplanmanager.com/manage-patients/326/careplan/demographics}': ["max<4000", "avg<3000"],
-        'http_req_duration{url:https://careplanmanager.com/manage-patients/326/view-careplan}': ["max<5000", "avg<4000"],
-        'http_req_duration{url:https://careplanmanager.com/manage-patients/326/activities}': ["max<3000", "avg<3000"],
-        'http_req_duration{url:https://careplanmanager.com/manage-patients/326/summary}': ["max<4000", "avg<3000"],
-        'http_req_duration{url:https://careplanmanager.com/manage-patients/326/notes}': ["max<4000", "avg<3000"]
+        'http_req_duration{name:ViewLogin}': ["avg<3000", "max<10000"],
+        'http_req_duration{name:PostLogin}': ["max<10000", "avg<8000"],
+        'http_req_duration{name:ViewDemographics}': ["max<4000", "avg<3000"],
+        'http_req_duration{name:ViewCarePlan}': ["max<5000", "avg<4000"],
+        'http_req_duration{name:ViewActivityReport}': ["max<3000", "avg<3000"],
+        'http_req_duration{name:ViewPatientProfile}': ["max<4000", "avg<3000"],
+        'http_req_duration{name:ViewNotes}': ["max<4000", "avg<3000"]
     }
 };
 
@@ -32,10 +32,12 @@ export default function () {
     let response;
 
     group('Login & View Patient Chart', function () {
-        response = http.get(baseUrl + routes['login']);
+        response = http.get(baseUrl + routes['login'], {
+            tags: {name: 'ViewLogin'}
+        });
 
         check(response, {
-            'View Login': (r) => r.status === 200,
+            'ViewLogin': (r) => r.status === 200,
         });
 
         /*
@@ -49,22 +51,24 @@ export default function () {
 
         response = response.submitForm({
             formSelector: 'form',
-            fields: { email: 'pagcosma nurse', password: '78Z43ZGsZ9p5!@#AA' },
+            fields: {email: 'pagcosma nurse', password: '78Z43ZGsZ9p5!@#AA'},
+        }, {
+            tags: {name: 'PostLogin'}
         });
 
         check(response, {
-            'POST Login': (r) => r.status === 200 && r.url === (baseUrl + routes['call-list'])
+            'PostLogin': (r) => r.status === 200 && r.url === (baseUrl + routes['call-list'])
         });
 
-        goToPage('View Notes', routes['notes']);
-        goToPage('View Demographics', routes['demographics']);
-        goToPage('View CarePlan', routes['care-plan']);
-        goToPage('View Patient Profile', routes['profile']);
-        goToPage('View Activity Report', routes['activities']);
+        goToPage('ViewNotes', routes['notes']);
+        goToPage('ViewDemographics', routes['demographics']);
+        goToPage('ViewCarePlan', routes['care-plan']);
+        goToPage('ViewPatientProfile', routes['profile']);
+        goToPage('ViewActivityReport', routes['activities']);
 
-        response = http.get(
-            baseUrl + routes['logout'],
-        );
+        response = http.get(baseUrl + routes['logout'], {
+            tags: {name: 'Logout'}
+        });
 
         check(response, {
             'Logout': (r) => r.status === 200,
@@ -73,9 +77,9 @@ export default function () {
 }
 
 function goToPage(description, url) {
-    const response = http.get(
-        baseUrl + url,
-    );
+    const response = http.get(baseUrl + url, {
+        tags: {name: description}
+    });
 
     check(response, {
         [description]: (r) => r.status === 200 && r.url === (baseUrl + url)
