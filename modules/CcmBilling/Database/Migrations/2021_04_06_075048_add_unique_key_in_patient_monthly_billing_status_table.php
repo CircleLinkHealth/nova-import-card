7828e22d59aa5359c6eac6a41d4a4daddd8ca60c
kakoushias <kakoushias@gmail.com>
2021-04-06 15:01:26 +0200

    billing-more-hotfixes-attestation: add unique key

---
+++ modules/CcmBilling/Database/Migrations/2021_04_06_075048_add_unique_key_in_patient_monthly_billing_status_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqueKeyInPatientMonthlyBillingStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patient_monthly_billing_statuses', function (Blueprint $table) {
+            $table->unique(['patient_user_id', 'chargeable_month'], 'billing_statuses_patient_id_month_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('patient_monthly_billing_statuses', function (Blueprint $table) {
+            $table->dropForeign('pmbs_patient_user_id_foreign');
+            $table->dropUnique('billing_statuses_patient_id_month_unique');
+            $table->foreign('patient_user_id', 'pmbs_patient_user_id_foreign')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+}
