4b9951c30863d83007d0c8f7318f5313fc62e4d4
kakoushias <kakoushias@gmail.com>
2021-04-12 15:15:14 +0200

    hotfix-billing-debouncing-and-duplicates: add patient info updated event

--- modules/CcmBilling/Providers/CcmBillingEventServiceProvider.php
+++ modules/CcmBilling/Providers/CcmBillingEventServiceProvider.php
@@ -9,6 +9,7 @@ namespace CircleLinkHealth\CcmBilling\Providers;
 use CircleLinkHealth\CcmBilling\Events\LocationServicesAttached;
 use CircleLinkHealth\CcmBilling\Events\PatientActivityCreated;
 use CircleLinkHealth\CcmBilling\Events\PatientConsentedToService;
+use CircleLinkHealth\CcmBilling\Events\PatientInfoUpdated;
 use CircleLinkHealth\CcmBilling\Events\PatientProblemsChanged;
 use CircleLinkHealth\CcmBilling\Events\PatientSuccessfulCallCreated;
 use CircleLinkHealth\CcmBilling\Listeners\ProcessLocationPatientServices;
@@ -31,6 +32,9 @@ class CcmBillingEventServiceProvider extends ServiceProvider
         ],
         PatientProblemsChanged::class => [
             ProcessPatientServices::class,
+        ],
+        PatientInfoUpdated::class => [
+            ProcessPatientServices::class
         ],
         PatientActivityCreated::class => [
             ProcessPatientServices::class,
