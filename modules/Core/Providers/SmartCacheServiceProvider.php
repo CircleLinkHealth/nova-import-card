0828157e1c07b41f4d617b0d7dcba996b3bace79
Pangratios Cosma <pagcosma@gmail.com>
2021-04-22 13:18:47 +0300

    fix cache.dynamodb.client

--- modules/Core/Providers/SmartCacheServiceProvider.php
+++ modules/Core/Providers/SmartCacheServiceProvider.php
@@ -7,12 +7,9 @@
 namespace CircleLinkHealth\Core\Providers;
 
 use CircleLinkHealth\Core\SmartCacheManager;
-use Illuminate\Cache\MemcachedConnector;
-use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Support\ServiceProvider;
-use Symfony\Component\Cache\Adapter\Psr16Adapter;
+use Illuminate\Cache\CacheServiceProvider;
 
-class SmartCacheServiceProvider extends ServiceProvider implements DeferrableProvider
+class SmartCacheServiceProvider extends CacheServiceProvider
 {
     /**
      * Get the services provided by the provider.
@@ -21,12 +18,7 @@ class SmartCacheServiceProvider extends ServiceProvider implements DeferrablePro
      */
     public function provides()
     {
-        return [
-            'cache',
-            'cache.store',
-            'cache.psr6',
-            'memcached.connector',
-        ];
+        return parent::provides();
     }
 
     /**
@@ -36,20 +28,10 @@ class SmartCacheServiceProvider extends ServiceProvider implements DeferrablePro
      */
     public function register()
     {
+        parent::register();
+
         $this->app->singleton('cache', function ($app) {
             return new SmartCacheManager($app);
-        });
-
-        $this->app->singleton('cache.store', function ($app) {
-            return $app['cache']->driver();
-        });
-
-        $this->app->singleton('cache.psr6', function ($app) {
-            return new Psr16Adapter($app['cache.store']);
-        });
-
-        $this->app->singleton('memcached.connector', function () {
-            return new MemcachedConnector();
         });
     }
 }
