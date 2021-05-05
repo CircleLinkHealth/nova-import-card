236d2520384bfad032e93b19071723448afac21f
Pangratios Cosma <pagcosma@gmail.com>
2021-05-05 14:35:08 +0300

    deploy prod: sqs fifo does not work

--- modules/CcmBilling/Services/PracticesInvoicesService.php
+++ modules/CcmBilling/Services/PracticesInvoicesService.php
@@ -55,9 +55,9 @@ class PracticesInvoicesService
         $jobs[] = new GeneratePracticesQuickbooksReportJob($practices, $date->toDateString(), $format, $requestedByUserId, $batchId);
 
         Bus::chain($jobs)
-            // ->onConnection('sync')
-            ->onConnection('sqs-fifo')
-            ->onQueue(getCpmQueueName(CpmConstants::FIFO_QUEUE))
+            ->onConnection('sync')
+            //->onConnection('sqs-fifo')
+            //->onQueue(getCpmQueueName(CpmConstants::FIFO_QUEUE))
             ->dispatch();
     }
 
