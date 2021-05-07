91f022c10d85e640c135286ca9e66a509ef68283
kakoushias <kakoushias@gmail.com>
2021-05-07 13:37:20 +0300

    CU-hx7e08-export-patient-problem-codes: chunk job

--- apps/superadmin-app/app/Nova/Actions/ExportPatientProblemCodes.php
+++ apps/superadmin-app/app/Nova/Actions/ExportPatientProblemCodes.php
@@ -2,17 +2,13 @@
 
 namespace App\Nova\Actions;
 
-use CircleLinkHealth\Core\Exports\FromArray;
-use CircleLinkHealth\Customer\Entities\User;
-use CircleLinkHealth\SharedModels\Entities\Problem;
+use CircleLinkHealth\Customer\CpmConstants;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use URL;
-
+use CircleLinkHealth\CcmBilling\Jobs\ExportPatientProblemCodes\ExportPatientProblemCodes as Job;
 
 class ExportPatientProblemCodes extends Action
 {
@@ -39,14 +35,6 @@ class ExportPatientProblemCodes extends Action
         return [];
     }
 
-    public function getFileName(): ?string
-    {
-        if ( ! $this->filename) {
-            $this->filename = 'patient-problem-codes' . now()->timestamp . '.csv';
-        }
-
-        return $this->filename;
-    }
 
     /**
      * Perform the action on the given models.
@@ -55,64 +43,9 @@ class ExportPatientProblemCodes extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
-        $resources = $this->resourcesToArray($models);
+        Job::dispatch($models->pluck('id')->toArray())
+           ->onQueue(getCpmQueueName(CpmConstants::LOW_QUEUE));
 
-        $response = (new FromArray($this->getFileName(), $resources,
-            array_keys($resources[0] ?? [])))->download($this->getFileName());
-
-        if ( ! $response instanceof BinaryFileResponse || $response->isInvalid()) {
-            return Action::danger(__('Resource could not be exported.'));
-        }
-
-        return Action::download(
-            $this->getDownloadUrl($response),
-            $this->getFileName()
-        );
-    }
-
-    protected function getDownloadUrl(BinaryFileResponse $response): string
-    {
-        return URL::temporarySignedRoute('laravel-nova-excel.download', now()->addMinutes(5), [
-            'path'     => encrypt($response->getFile()->getPathname()),
-            'filename' => $this->getFileName(),
-        ]);
-    }
-
-    private function resourcesToArray(Collection $models)
-    {
-        $array = [$this->headings];
-        foreach ($models as $practice) {
-            User::select(['id', 'display_name'])
-                ->ofPractice($practice->id)
-                ->ofType('participant')
-                ->whereHas('patientInfo', fn($q) => $q->enrolled())
-                ->with([
-                    'ccdProblems' => fn($q) => $q->forBilling(),
-                ])
-                ->each(function (User $user) use (&$array, $practice) {
-                    $array[] = [
-                        $practice->display_name,
-                        $user->id,
-                        $user->display_name,
-                        $this->formatProblemCodesForReport($user->ccdProblems)
-                    ];
-                });
-        }
-
-        return $array;
-    }
-
-    private function formatProblemCodesForReport(Collection $problems)
-    {
-        return $problems->isNotEmpty()
-            ?
-            $problems->map(
-                function (Problem $problem) {
-                    return $problem->icd10Code();
-                }
-            )->filter()
-                     ->unique()
-                     ->implode(', ')
-            : 'N/A';
+        return Action::message('Generating Patient Problem Codes report. A link to the report will be sent via email when job is completed.');
     }
 }
--- modules/CcmBilling/Jobs/ExportPatientProblemCodes/ExportPatientProblemCodesForPractice.php
+++ modules/CcmBilling/Jobs/ExportPatientProblemCodes/ExportPatientProblemCodesForPractice.php
@@ -2,14 +2,90 @@
 
 namespace CircleLinkHealth\CcmBilling\Jobs\ExportPatientProblemCodes;
 
-use Illuminate\Contracts\Queue\ShouldBeEncrypted;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use CircleLinkHealth\CcmBilling\Jobs\ChunksEloquentBuilderJob;
+use CircleLinkHealth\CcmBilling\Repositories\BatchableStoreRepository;
+use CircleLinkHealth\Customer\Entities\User;
+use CircleLinkHealth\SharedModels\Entities\Problem;
+use Illuminate\Bus\Queueable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 
 /**
  * This file is part of CarePlan Manager by CircleLink Health.
  */
-
-class ExportPatientProblemCodesForPractice implements ShouldQueue, ShouldBeEncrypted
+class ExportPatientProblemCodesForPractice extends ChunksEloquentBuilderJob
 {
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
+    protected int $practiceId;
+    protected string $batchId;
+
+    public function __construct(int $practiceId, string $batchId)
+    {
+        $this->practiceId = $practiceId;
+        $this->batchId    = $batchId;
+    }
+
+    public function getBuilder(): Builder
+    {
+        return User::select(['id', 'display_name'])
+                   ->ofPractice($this->practiceId)
+                   ->ofType('participant')
+                   ->whereHas('patientInfo', fn($q) => $q->enrolled())
+                   ->with([
+                       'ccdProblems'     => fn($q) => $q->forBilling(),
+                       'primaryPractice' => fn($q) => $q->select(['id', 'display_name']),
+                   ])
+                   ->offset($this->getOffset())
+                   ->limit($this->getLimit());
+    }
+
+    public function handle()
+    {
+        $offset = $this->getOffset();
+        $limit  = $this->getLimit();
+        $total  = $this->getTotal();
+        Log::info("Patient Problem Codes Report: Creating Patient Problem Codes report for practice[{$this->practiceId}] and batch[{$this->batchId}]. Total[$total] | Offset[$offset] | Limit[$limit] | Chunk[$this->chunkId]");
+        $data = $this->getBuilder()
+                     ->get()
+                     ->map(function (User $user) {
+                         return [
+                             $user->getPrimaryPracticeName(),
+                             $user->id,
+                             $user->display_name,
+                             $this->formatProblemCodesForReport($user->ccdProblems),
+                         ];
+                     });
+
+        $this->storeIntoCache($data);
+
+        Log::info("Patient Problem Codes Report: Creating Patient Problem Codes report for practice[{$this->practiceId}] and batch[{$this->batchId}]. Total[$total] | Offset[$offset] | Limit[$limit] | Chunk[$this->chunkId]");
+    }
+
+    private function storeIntoCache(Collection $data): void
+    {
+        app(BatchableStoreRepository::class)->store($this->batchId, BatchableStoreRepository::JSON_TYPE,
+            $data->toArray(), $this->chunkId);
+    }
+
+    private function formatProblemCodesForReport(Collection $problems): string
+    {
+        return $problems->isNotEmpty()
+            ?
+            $problems->map(
+                function (Problem $problem) {
+                    return $problem->icd10Code();
+                }
+            )->filter()
+                     ->unique()
+                     ->implode(', ')
+            : 'N/A';
+    }
 }
