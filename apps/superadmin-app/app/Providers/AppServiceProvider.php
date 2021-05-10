6eac16b8e8bf283b214238da10a946c3054d867b
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-05-10 18:58:12 +0300

    Adds logging

--- apps/superadmin-app/app/Providers/AppServiceProvider.php
+++ apps/superadmin-app/app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ namespace App\Providers;
 use CircleLinkHealth\LargePayloadSqsJob\LargeJobsDispatcherInterceptor;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrapThree();
 
         Queue::before(function (JobProcessing $event) {
+            Log::debug("Starting Job {$event->job->resolveName()}");
             (new LargeJobsDispatcherInterceptor())->handle($event->job);
         });
     }
