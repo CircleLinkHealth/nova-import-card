cef89ae39c94f0c6127d3626d6c0be0290bcaaf4
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-05-10 18:58:19 +0300

    lowers limit

--- modules/LargePayloadSqsJob/Constants.php
+++ modules/LargePayloadSqsJob/Constants.php
@@ -9,7 +9,7 @@ class Constants
     /**
      * The maximum size that SQS can accept in bytes (256 * 1024).
      */
-    const MAX_SQS_SIZE_BYTES = 262144;
+    const MAX_SQS_SIZE_BYTES = 62144;
     
     /**
      * The name of the custom SQS queue driver
