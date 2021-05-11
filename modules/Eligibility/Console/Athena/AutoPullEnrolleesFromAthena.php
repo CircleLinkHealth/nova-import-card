14730853d8b6c5f694e0fd0484303092e08be07c
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-05-11 17:04:15 +0300

    Pull more days

--- modules/Eligibility/Console/Athena/AutoPullEnrolleesFromAthena.php
+++ modules/Eligibility/Console/Athena/AutoPullEnrolleesFromAthena.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Bus;
 
 class AutoPullEnrolleesFromAthena extends Command
 {
-    const MAX_DAYS_TO_PULL_AT_ONCE = 1;
+    const MAX_DAYS_TO_PULL_AT_ONCE = 7;
     /**
      * The console command description.
      *
