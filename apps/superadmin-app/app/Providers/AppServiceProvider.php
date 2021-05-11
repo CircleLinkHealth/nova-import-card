a3f957c41cac2ab50ceee79b7b92ceecac036cc3
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-05-11 21:43:49 +0300

    deploy production: try13 - change default path back

--- apps/superadmin-app/app/Providers/AppServiceProvider.php
+++ apps/superadmin-app/app/Providers/AppServiceProvider.php
@@ -35,8 +35,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (isProductionEnv()) {
-            $this->app->instance('path.storage', sys_get_temp_dir());
-        }
+
     }
 }
