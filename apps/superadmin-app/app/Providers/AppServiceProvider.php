51f88f245169b5984117913cc5da797443d78e31
Michalis Antoniou <michalisantoniou6@gmail.com>
2021-05-11 17:21:24 +0300

    deploy production: try10
    - removes logs

--- apps/superadmin-app/app/Providers/AppServiceProvider.php
+++ apps/superadmin-app/app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,11 +28,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Log::debug("isProductionEnv".isProductionEnv());
-        Log::debug("sys_get_temp_dir".sys_get_temp_dir());
-//        Nothing changed. Maybe below doesn't work?
-//        if (isProductionEnv()) {
+        if (isProductionEnv()) {
             $this->app->instance('path.storage', sys_get_temp_dir());
-//        }
+        }
     }
 }
